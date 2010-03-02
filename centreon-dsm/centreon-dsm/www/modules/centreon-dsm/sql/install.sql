d7f09924f8107c9f24d6637582d08df59aca40a8
Julien Mathis <jmathis@merethis.com>
2010-03-02 09:27:38 +0000

    update sql schema
    git-svn-id: http://svn.modules.centreon.com/centreon-dsm/trunk@5 2223b321-f9ee-48cb-83f9-124db436e5d3

--- centreon-dsm/centreon-dsm/www/modules/centreon-dsm/sql/install.sql
+++ centreon-dsm/centreon-dsm/www/modules/centreon-dsm/sql/install.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `151_centreon`.`mod_dsm_pool` (
 `pool_id` INT NOT NULL AUTO_INCREMENT ,
-`pool_name2` VARCHAR( 63 ) NULL ,
+`pool_name` VARCHAR( 63 ) NULL ,
+`pool_host_id` INT NULL ,
 `pool_description` VARCHAR( 255 ) NULL ,
 `pool_number` INT NULL ,
 `pool_prefix` VARCHAR( 10 ) NULL ,
