centreon/centreon

commit eba06e5a1e5abb7675e6489e5bf96a3f5bf4e341

manual merge branches/centreon-2.1@9561-10313 into trunk codename: I hate this shit! * tactical overview and login not touched: DIY! * ignored ALL tree conflicts caused by double commits and delete+add
git-svn-id: http://svn.centreon.com/trunk/centreon@10318 6bcd3966-0018-0410-8128-fd23d134de7e

centreon/lib/identifyMetric.pm
@@ -35,14 +35,6 @@
 #
 ####################################################################################
 
-# identifier la metric
-# meed arguments 
-# - perfdata
-# - index_id
-# - status
-# - timestamp
-# - storage_type
-
 sub removeBackSpace($){
     $_[0] =~ s/\n//g;
     return $_[0];
@@ -81,25 +73,27 @@ sub insertMetrics($$$$$$$){
 sub updateMetricInformation($$$$$){
     my ($id, $warn, $crit, $min, $max) = @_;
 
-    if ($warn ne "" || $crit ne "" || $min ne "" || $max ne ""){
-	my $str = "";
-	$str .= "`warn` = '".$warn."'" if ($warn ne "");
-	if ($crit ne ""){
-	    $str .= ", " if ($str ne "");
-	    $str .= "`crit` = '".$crit."' ";
-	}
-	if ($min ne ""){
-	    $str .= ", " if ($str ne "");
-	    $str .= "`min` = '".$min."' ";
-	}
-	if ($max ne ""){
-	    $str .= ", " if ($str ne "");
-	    $str .= "`max` = '".$max."' ";
-	}
-	$sth1 = $con_ods->prepare("UPDATE `metrics` SET $str WHERE `metric_id` = '".$id."'");
-	writeLogFile("Error:" . $sth1->errstr . "\n") if (!$sth1->execute);
-	undef($sth1);
-	undef($str);
+    if ($warn ne "" || $crit ne "" || $min ne "" || $max ne "") {
+		my $str = "";
+		$str .= "`warn` = '".$warn."'" if ($warn ne "");
+		if ($crit ne ""){
+		    $str .= ", " if ($str ne "");
+		    $str .= "`crit` = '".$crit."' ";
+		}
+		if ($min ne ""){
+		    $str .= ", " if ($str ne "");
+		    $str .= "`min` = '".$min."' ";
+		}
+		if ($max ne ""){
+		    $str .= ", " if ($str ne "");
+		    $str .= "`max` = '".$max."' ";
+		}
+		$sth1 = $con_ods->prepare("UPDATE `metrics` SET $str WHERE `metric_id` = '".$id."'");
+		if (!$sth1->execute) {
+			writeLogFile("Error:" . $sth1->errstr . "\n");
+		}
+		undef($sth1);
+		undef($str);
     }		   	
 }
 
@@ -112,64 +106,84 @@ sub identify_metric($$$$$$$$){
     $generalcounter = $_[5];
     $just_insert = 0;
 
-
-    my $con_ods = CreateConnexionForCentstorage();
-    my $con_oreon = CreateConnexionForOreon();
+    my $con_ods 	= CreateConnexionForCentstorage();
+    my $con_oreon 	= CreateConnexionForOreon();
 
     # Cut perfdata    	
     my $metric = removeBackSpace($_[0]);
-  
-    while ($metric =~ m/\'?([a-zA-Z0-9\_\-\/\.\:\ \\]+)\'?\=([0-9\.\,\-]+)([a-zA-Z0-9\_\-\/\\\%]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)\s?/g){
-	my $metric_name = $1;
-    $metric_name =~ s/^\s+//;
-    $metric_name =~ s/\s+$//;
-    my $unit = "";
-    my $value = $2;
-    my $warn = "";
-    my $critical = "";
-    my $min = "";
-    my $max = "";
 
-    if (defined($3)){$unit = $3;}
-    if (defined($4)){$warn = $4;}
-    if (defined($5)){$critical = $5;}
-    if (defined($6)){$min = $6;}
-    if (defined($7)){$max = $7;}
+    while ($metric =~ m/\'?([a-zA-Z0-9\_\-\/\.\:\ \\]+)\'?\=([0-9\.\,\-]+)([a-zA-Z0-9\_\-\/\\\%]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)[\;]*([0-9\.\,\-]*)\s?/g) {
+		my $metric_name = $1;
+	    $metric_name =~ s/^\s+//;
+	    $metric_name =~ s/\s+$//;
+	    my $unit = "";
+	    my $value = $2;
+	    my $warn = "";
+	    my $critical = "";
+	    my $min = "";
+	    my $max = "";
 
-	my $cpt = 1;
-	my $x = 0;
-	while (defined($$cpt)){
-	    $data[$x] = $$cpt;
-	    $cpt++;
-	    $x++;
-	}
+	    if (defined($3)){$unit = $3;}
+	    if (defined($4)){$warn = $4;}
+	    if (defined($5)){$critical = $5;}
+	    if (defined($6)){$min = $6;}
+	    if (defined($7)){$max = $7;}
 	
-	@data = ($metric_name, $value, $unit, $warn, $critical, $min, $max); # metric, value, unit, warn, critical, min, max
-	if ($1 && defined($2)){			
-	    # Check if metric is known...
-	    $data[0] = removeSpecialCharInMetric($data[0]);
-
-	    my $sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
-	    writeLogFile("Error:" . $sth1->errstr . "\n") if (!$sth1->execute);
-
-	    if ($sth1->rows() eq 0){
-		$just_insert = 1;  
-		insertMetrics($_[1], $data[0], $data[2], $data[3], $data[4], $data[5], $data[6]);
-
-		# Get ID
-		$sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
-		if (!$sth1->execute) {
-		    writeLogFile("Error:" . $sth1->errstr . "\n");
+		my $cpt = 1;
+		my $x = 0;
+		while (defined($$cpt)){
+		    $data[$x] = $$cpt;
+		    $cpt++;
+		    $x++;
 		}
-	    }
-	    my $metric = $sth1->fetchrow_hashref();
-	    $sth1->finish();
 
-	    # Update metric attributs
-	    if ($just_insert || ($metric->{'unit_name'} ne $data[2])){
-		my $sth1 = $con_ods->prepare("UPDATE `metrics` SET `unit_name` = '".$data[2]."', `warn` = '".$data[3]."', `crit` = '".$data[4]."', `min` = '".$data[5]."', `max` = '".$data[6]."' WHERE `metric_id` = '".$metric->{'metric_id'}."'");
-		if (!$sth1->execute) {
-		    writeLogFile("Error:" . $sth1->errstr . "\n");
+		@data = ($metric_name, $value, $unit, $warn, $critical, $min, $max); # metric, value, unit, warn, critical, min, max
+		if (defined($metric_name) && $metric_name && defined($value)) {
+		    # Check if metric is known...
+		    $data[0] = removeSpecialCharInMetric($data[0]);
+	
+		    my $sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
+		    if (!$sth1->execute()) {
+			    writeLogFile("Error:" . $sth1->errstr . "\n");
+		    } else {
+			    if ($sth1->rows() eq 0) {
+					$just_insert = 1;  
+					insertMetrics($_[1], $data[0], $data[2], $data[3], $data[4], $data[5], $data[6]);
+			
+					# Get ID
+					$sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
+					if (!$sth1->execute) {
+					    writeLogFile("Error:" . $sth1->errstr . "\n");
+					}
+			    }
+			    my $metric = $sth1->fetchrow_hashref();
+			    $sth1->finish();
+	
+			    # Update metric attributs
+			    if ($just_insert || (defined($data[2]) && defined($metric->{'unit_name'}) && $metric->{'unit_name'} ne $data[2])) {
+					my $sth1 = $con_ods->prepare("UPDATE `metrics` SET `unit_name` = '".$data[2]."', `warn` = '".$data[3]."', `crit` = '".$data[4]."', `min` = '".$data[5]."', `max` = '".$data[6]."' WHERE `metric_id` = '".$metric->{'metric_id'}."'");
+					if (!$sth1->execute) {
+					    writeLogFile("Error:" . $sth1->errstr . "\n");
+					}
+					undef($sth1);
+			    }
+			
+		   	    updateMetricInformation($metric->{'metric_id'}, $data[3], $data[4], $data[5], $data[6]);
+
+			    # Check Storage Type
+			    # O -> BD Mysql & 1 -> RRDTool
+			    $begin = $_[3] - 200;
+		
+			    if (defined($data[1])) {
+					if (defined($_[4]) && $_[4] eq 0 && $_[6] eq 0){
+					    updateRRDDB($configuration->{'RRDdatabase_path'}, $metric->{'metric_id'}, $_[3], $data[1], $begin, $configuration->{'len_storage_rrd'}, $metric->{'metric_name'});
+					} elsif (defined($_[4]) && $_[4] eq 2) { 
+					    updateRRDDB($configuration->{'RRDdatabase_path'}, $metric->{'metric_id'}, $_[3], $data[1], $begin, $configuration->{'len_storage_rrd'}, $metric->{'metric_name'});
+					    updateMysqlDB($metric->{'metric_id'}, $_[3], $data[1], $status{$_[2]});
+					}
+			    }	    
+	    	}
+		    $just_insert = 0;
 		}
 		undef($sth1);
 	    }
@@ -230,35 +244,41 @@ sub identify_hidden_metric($$$$$$$$){ # perfdata index status time type counter 
 	    if (!defined($5)){$5 = "";}	
 	    @data = ($1, $2, $3, $4, $5); # metric, value, unit, warn, critical
 	}
-	if ($1 && defined($2)){			
+	if ($metric_name && defined($value)) {			
 	    # Check if metric is known...
 	    $data[0] =~ s/\//#S#/g;
 	    my $sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
 	    if (!$sth1->execute) {writeLogFile("Error:" . $sth1->errstr . "\n");}
 
-	    if ($sth1->rows() eq 0){
-		$just_insert = 1;   				
-		undef($sth1);
-				# Si pas connue -> insert
-		my $sth2 = $con_ods->prepare("INSERT INTO `metrics` (`index_id`, `metric_name`, `unit_name`) VALUES ('".$_[1]."', '".$data[0]."', '".$data[2]."')");
-		if (!$sth2->execute){writeLogFile("Error:" . $sth2->errstr . "\n");}
-		undef($sth2);
-		# Get ID
-		$sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
-		if (!$sth1->execute) {writeLogFile("Error:" . $sth1->errstr . "\n");}
-	    }
+	    if ($sth1->rows() eq 0) {
+			$just_insert = 1;   				
+			undef($sth1);
+			
+			# Si pas connue -> insert
+			my $sth2 = $con_ods->prepare("INSERT INTO `metrics` (`index_id`, `metric_name`, `unit_name`) VALUES ('".$_[1]."', '".$data[0]."', '".$data[2]."')");
+			if (!$sth2->execute){
+				writeLogFile("Error:" . $sth2->errstr . "\n");
+			}
+			undef($sth2);
+			# Get ID
+			$sth1 = $con_ods->prepare("SELECT * FROM `metrics` WHERE `index_id` = '".$_[1]."' AND `metric_name` = '".$data[0]."'");
+			if (!$sth1->execute) {
+				writeLogFile("Error:" . $sth1->errstr . "\n");
+			}
+		}
 	    my $metric = $sth1->fetchrow_hashref();
 	    undef($sth1);
 
-	    if ($just_insert || ($metric->{'unit_name'} ne $data[2])){
-		my $sth1 = $con_ods->prepare("UPDATE `metrics` SET `unit_name` = '".$data[2]."' WHERE `metric_id` = '".$metric->{'metric_id'}."'");
-		if (!$sth1->execute){
-		    writeLogFile("Error:" . $sth1->errstr . "\n");
-		}
-		undef($sth1);
+	    if ($just_insert || (defined($data[2]) && defined($metric->{'unit_name'}) && $metric->{'unit_name'} ne $data[2])) {
+			my $sth1 = $con_ods->prepare("UPDATE `metrics` SET `unit_name` = '".$data[2]."' WHERE `metric_id` = '".$metric->{'metric_id'}."'");
+			if (!$sth1->execute){
+			    writeLogFile("Error:" . $sth1->errstr . "\n");
+			}
+			undef($sth1);
 	    }
 
 	    $begin = $_[3] - 200;
+
 	    if (defined($data[1]) && defined($_[4])){
 		if ($_[6] eq 0){
 		    # no rebuild running

centreon/cron/centAcl-Func.php
@@ -36,89 +36,79 @@
  * 
  */
  
- /*
+ 	/*
 	 * Init functions
 	 */
 	function microtime_float2() 	{
 	   list($usec, $sec) = explode(" ", microtime());
 	   return ((float)$usec + (float)$sec);
 	}
-		
-	function getAuthorizedCategories2($groupstr, $res_id){
+	
+	/*
+	 * Return enable categories for this resource access
+	 */
+	function getAuthorizedCategories($groupstr, $res_id) {
 		global $pearDB;
 		
 		if (strlen($groupstr) == 0)
 			return array();
 			
-		$tab_categories = array();				
-		$DBRES =& $pearDB->query("SELECT acl_res_id FROM `acl_resources` WHERE acl_res_id = '".$res_id."' AND acl_res_activate = '1'");
-			
-		if (!$DBRES->numRows())
-			return array();
+		$tab_categories = array();
 		$request = "SELECT sc_id " .
-					"FROM acl_resources_sc_relations, acl_res_group_relations " .
+					"FROM acl_resources_sc_relations, acl_res_group_relations, acl_resources " .
 					"WHERE acl_resources_sc_relations.acl_res_id = acl_res_group_relations.acl_res_id " .
-					"AND acl_res_group_relations.acl_group_id IN (".$groupstr.") " .
-					"AND acl_resources_sc_relations.acl_res_id = '$res_id'";
-					
+						"AND acl_res_group_relations.acl_group_id IN (".$groupstr.") " .
+						"AND acl_resources_sc_relations.acl_res_id = acl_resources.acl_res_id " .
+						"AND acl_resources.acl_res_id = '".$res_id."' " .
+						"AND acl_res_activate = '1'";
 		$DBRESULT =& $pearDB->query($request);
-		while ($res =& $DBRESULT->fetchRow())			
-			$tab_categories[$res["sc_id"]] = $res["sc_id"];		
+		while ($res =& $DBRESULT->fetchRow()) {
+			$tab_categories[$res["sc_id"]] = $res["sc_id"];			
+		}
+		$DBRESULT->free();
 	  	unset($res);
 	  	unset($DBRESULT);		
 	  	return $tab_categories;
 	}
 	
-	function getServiceTemplateList3($service_id = NULL)	{
-		global $pearDB;
+	function getServiceTemplateCategoryList($service_id = NULL)	{
+		global $pearDB, $svcTplCache, $svcCatCache;
 
+		$tabCategory = array();
+		
 		if (!$service_id) 
 			return;
+		
+		if (isset($svcCatCache[$service_id])) {
+			foreach ($svcCatCache[$service_id] as $ct_id => $flag) {
+				$tabCategory[$ct_id] = $ct_id;
+			} 
+		}
+		
 		/*
 		 * Init Table of template
 		 */
-		$strTemplate = "'$service_id'";
-		while (1)	{
-			/*
-			 * Get template Informations
-			 */
-			$DBRESULT =& $pearDB->query("SELECT service_template_model_stm_id FROM service WHERE service_id = '".$service_id."' LIMIT 1");
-			if (PEAR::isError($DBRESULT))
-				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-			$row =& $DBRESULT->fetchRow();
-			if (isset($row["service_template_model_stm_id"]) && $row["service_template_model_stm_id"]){
-				if ($strTemplate)
-					$strTemplate .= ', ';
-				$strTemplate .= "'".$row["service_template_model_stm_id"]."'";
-				$service_id = $row["service_template_model_stm_id"];
-			} else
-				return $strTemplate;
-		}
-	}
-	
-	function getMyServiceName2($service_id = NULL)	{
-		if (!$service_id) return;
-		global $pearDB;
 		while (1) {
-			$DBRESULT =& $pearDB->query("SELECT service_description, service_template_model_stm_id FROM service WHERE service_id = '".$service_id."' LIMIT 1");
-			if (PEAR::isError($DBRESULT))
-				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-			$row =& $DBRESULT->fetchRow();
-			if ($row["service_description"])	{
-				$row["service_description"] = str_replace('#S#', "/", $row["service_description"]);
-				$row["service_description"] = str_replace('#BS#', "\\", $row["service_description"]);
-				return html_entity_decode($row["service_description"], ENT_QUOTES);
-			} else if ($row["service_template_model_stm_id"])
-				$service_id = $row["service_template_model_stm_id"];
-			else
-				break;
+			if (isset($svcTplCache[$service_id]) && $svcTplCache[$service_id]) {
+				//print "svc => $service_id => ".$svcTplCache[$service_id]." \n";
+				//print_r($svcCatCache[$service_id]);
+				if (isset($svcCatCache[$service_id])) {
+					foreach ($svcCatCache[$service_id] as $ct_id => $flag) {
+						$tabCategory[$ct_id] = $ct_id;
+					} 
+				}
+				$service_id = $svcTplCache[$service_id];
+			} else {
+				return $tabCategory;
+			}	
 		}
 	}
 	
-	function getACLSGForHost2($pearDB, $host_id, $groupstr){
+	function getACLSGForHost($pearDB, $host_id, $groupstr){
+		global $svcCache;
+		
 		if (!$pearDB || !isset($host_id))
 			return ;
-
 
 		/*
 		 * Init Acl Table
@@ -133,10 +123,10 @@
 			$condition = " WHERE `acl_group_id` IN (".$groupstr.") AND ";			
 		else
 			$condition = " WHERE ";
+		
 		$DBRESULT =& $pearDB->query("SELECT argr.`acl_res_id` FROM `acl_res_group_relations` argr, `acl_resources` ar ".$condition." " .
 									"argr.acl_res_id = ar.acl_res_id " .
 									"AND ar.acl_res_activate = '1'");
-				
 		while ($res =& $DBRESULT->fetchRow()) {
 			$DBRESULT2 =& $pearDB->query(	"SELECT `service_service_id` " .
 											"FROM `servicegroup`, `acl_resources_sg_relations`, `servicegroup_relation` " .
@@ -144,58 +134,45 @@
 											"AND `acl_resources_sg_relations`.`sg_id` = `servicegroup`.`sg_id` " .
 											"AND `servicegroup_relation`.`servicegroup_sg_id` = `servicegroup`.`sg_id` " .
 											"AND `servicegroup_relation`.`host_host_id` = '".$host_id."'");	
-			if (PEAR::isError($DBRESULT2))
-				print "DB Error : ".$DBRESULT2->getDebugInfo()."<br />";
-			while ($service =& $DBRESULT2->fetchRow())
-				$svc[getMyServiceName2($service["service_service_id"])] = $service["service_service_id"];
+			while ($service =& $DBRESULT2->fetchRow()) {
+				if (isset($svcCache[$service["service_service_id"]])) {
+					$svc[$svcCache[$service["service_service_id"]]] = $service["service_service_id"];
+				}
+			}
 			$DBRESULT2->free();
 		}
 		$DBRESULT->free();
 		return $svc;
 	}
 	
-	function getServicesCategories2($str){
-		global $pearDB;
+	function getAuthorizedServicesHost($host_id, $groupstr, $res_id, $authorizedCategories){
+		global $pearDB, $svcCache, $hostCache;
 		
-		$tab = array();
-		$DBRESULT =& $pearDB->query("SELECT `sc_id` FROM `service_categories_relation` WHERE `service_service_id` IN (".$str.")");
-		while ($res =& $DBRESULT->fetchRow())
-			$tab[$res["sc_id"]] = $res["sc_id"];
-		unset($res);		
-		unset($DBRESULT);
-		return $tab;
-	}
-	
-	function getAuthorizedServicesHost2($host_id, $groupstr, $res_id){
-		global $pearDB;
+		$tab_svc 	= getMyHostServicesByName($host_id);
 		
-		$tab_svc 	= getMyHostServicesByName2($host_id);
-
-		/*
-		 * Get categories
-		 */
-		$tab_cat    = getAuthorizedCategories2($groupstr, $res_id);		
-
-
 		/*
 		 * Get Service Groups
 		 */
-		$svc_SG 	= getACLSGForHost2($pearDB, $host_id, $groupstr);
+		$svc_SG 	= getACLSGForHost($pearDB, $host_id, $groupstr);
 		
 		$tab_services = array();
-		if (count($tab_cat)){
+		if (count($authorizedCategories)) {
 			if ($tab_svc) {
-				foreach ($tab_svc as $svc_descr => $svc_id){
-					$tmp = getServiceTemplateList3($svc_id);
-					$tab = getServicesCategories2($tmp);					
+				foreach ($tab_svc as $svc_descr => $svc_id) {
+					//print "Service : $host_id (".$hostCache[$host_id].") ".$svc_id ." | ".$svcCache[$svc_id]."\n";
+					$tab = getServiceTemplateCategoryList($svc_id);
+					if (count($tab) && 0) {
+						print "------------------\n";
+						print_r($tab);
+					}
 					foreach ($tab as $t){
-						if (isset($tab_cat[$t])) {							
+						if (isset($authorizedCategories[$t])) {							
 							$tab_services[$svc_descr] = $svc_id;
 						}
 					}
 				}
 			}			
-		} elseif (hostIsAuthorized($host_id, $groupstr)){
+		} else if (hostIsAuthorized($host_id, $groupstr)){
 			$tab_services = $tab_svc;
 			if ($svc_SG) {
 				foreach ($svc_SG as $key => $value) {
@@ -203,7 +180,7 @@
 				}
 			}	
 		}
-	  	return $tab_services;
+		return $tab_services;
 	}
 	
 	function hostIsAuthorized($host_id, $group_id) {
@@ -240,43 +217,24 @@
 		
 		return false;
 	}
-	
-	function getMyHostServicesByName2($host_id = NULL)	{
-		global $pearDB;
+
+	/*
+	 * Retreive service description
+	 */	
+	function getMyHostServicesByName($host_id = NULL)	{
+		global $pearDB, $hsRelation, $svcCache;
 		
 		if (!$host_id) 
 			return;
-
-		$hSvs = array();
-		/*
-		 * Service By Host
-		 */
-		$DBRESULT =& $pearDB->query("SELECT service_id, service_description FROM service, host_service_relation hsr WHERE hsr.host_host_id = '".$host_id."' AND hsr.service_service_id = service_id AND service_activate = '1'");
-		if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-		while ($elem =& $DBRESULT->fetchRow())	{
-			$elem["service_description"] = str_replace('#S#', '/', $elem["service_description"]);
-			$elem["service_description"] = str_replace('#BS#', '\\', $elem["service_description"]);
-			$hSvs[$elem["service_description"]] = html_entity_decode($elem["service_id"], ENT_QUOTES);
-		}
-		$DBRESULT->free();
 		
-		/*
-		 * Service By Hostgroup
-		 */
-		$DBRESULT =& $pearDB->query("SELECT service_id, service_description FROM hostgroup_relation hgr, service, host_service_relation hsr" .
-				" WHERE hgr.host_host_id = '".$host_id."' AND hsr.hostgroup_hg_id = hgr.hostgroup_hg_id" .
-				" AND service_id = hsr.service_service_id");
-		if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-		while ($elem =& $DBRESULT->fetchRow()) {
-			$elem["service_description"] = str_replace('#S#', '/', $elem["service_description"]);
-			$elem["service_description"] = str_replace('#BS#', '\\', $elem["service_description"]);
-			$hSvs[$elem["service_description"]]	= html_entity_decode($elem["service_id"], ENT_QUOTES);
+		$hSvs = array();
+		if (isset($hsRelation[$host_id])) {
+			foreach ($hsRelation[$host_id] as $service_id => $flag) {
+				$service_description = str_replace('#S#', '/', $svcCache[$service_id]);
+				$service_description = str_replace('#BS#', '\\', $service_description);
+				$hSvs[$service_description] = html_entity_decode($service_id, ENT_QUOTES);
+			}
 		}
-		$DBRESULT->free();
 		return $hSvs;
 	}
- 
- 
- ?>
+?>

commit 900055b4dd3eb95fb65482adccbe20f7a5accce6

fix bracket errors caused by merge in r10318
git-svn-id: http://svn.centreon.com/trunk/centreon@10704 6bcd3966-0018-0410-8128-fd23d134de7e

centreon/lib/identifyMetric.pm
@@ -186,7 +186,7 @@ sub identify_metric($$$$$$$$){
 		    $just_insert = 0;
 		}
 		undef($sth1);
-	    }
+
 
 	    updateMetricInformation($metric->{'metric_id'}, $data[3], $data[4], $data[5], $data[6]);
 
@@ -204,7 +204,7 @@ sub identify_metric($$$$$$$$){
 		}
 	    }
 	    $just_insert = 0;
-	}
+
 	undef(@data);
     }
     undef($metric_name);