206700379c8d6c168ed724e52be401a28ed527f3
Julien Mathis <jmathis@merethis.com>
2011-10-26 12:34:20 +0000

    prepare version 1.2
    git-svn-id: http://svn.modules.centreon.com/centreon-dsm/trunk@82 2223b321-f9ee-48cb-83f9-124db436e5d3

--- centreon-dsm/centreon-dsm/www/modules/centreon-dsm/conf.php
+++ centreon-dsm/centreon-dsm/www/modules/centreon-dsm/conf.php
@@ -2,11 +2,11 @@
 // Be Carefull with internal_name, it's case sensitive (with directory module name)
 $module_conf['centreon-dsm']["name"] = "centreon-dsm";
 $module_conf['centreon-dsm']["rname"] = "Dynamic Services Management";
-$module_conf['centreon-dsm']["mod_release"] = "1.1";
+$module_conf['centreon-dsm']["mod_release"] = "1.2";
 $module_conf['centreon-dsm']["infos"] = "Dynamic system management for passif return without definition";
 $module_conf['centreon-dsm']["is_removeable"] = "1";
 $module_conf['centreon-dsm']["author"] = "Merethis Team";
-$module_conf['centreon-dsm']["lang_files"] = "1";
+$module_conf['centreon-dsm']["lang_files"] = "0";
 $module_conf['centreon-dsm']["sql_files"] = "1";
-$module_conf['centreon-dsm']["php_files"] = "1";
+$module_conf['centreon-dsm']["php_files"] = "0";
 ?>
