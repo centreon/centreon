0fb757701c6ab2adbc02adfa867b8055b36e785e
Maximilien Bersoult <mbersoult@merethis.com>
2012-02-22 11:15:41 +0000

    * Fix update version 1/2
    git-svn-id: http://svn.modules.centreon.com/centreon-dsm/trunk@88 2223b321-f9ee-48cb-83f9-124db436e5d3

--- centreon-dsm/centreon-dsm/www/modules/centreon-dsm/UPGRADE/centreon-dsm-1.2/conf.php
+++ centreon-dsm/centreon-dsm/www/modules/centreon-dsm/UPGRADE/centreon-dsm-1.2/conf.php
@@ -1,7 +1,7 @@
 <?php
 $upgrade_conf['centreon-dsm']["rname"] = "centreon-dsm";
-$upgrade_conf['centreon-dsm']["release_from"] = "1.2";
-$upgrade_conf['centreon-dsm']["release_to"] = "1.3";
+$upgrade_conf['centreon-dsm']["release_from"] = "1.1";
+$upgrade_conf['centreon-dsm']["release_to"] = "1.2";
 $upgrade_conf['centreon-dsm']["is_removeable"] = "1";
 $upgrade_conf['centreon-dsm']["author"] = "Merethis";
 $upgrade_conf['centreon-dsm']["lang_files"] = "0";
