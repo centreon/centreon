a5d7c061c0183964bf50421ea2e9444fee4d2b0f
Julien Mathis <jmathis@merethis.com>
2012-12-19 09:16:45 +0000

    improve configuration file
    git-svn-id: http://svn.modules.centreon.com/centreon-dsm/trunk@127 2223b321-f9ee-48cb-83f9-124db436e5d3

--- centreon-dsm/centreon-dsm/etc/conf_dsm.pm
+++ centreon-dsm/centreon-dsm/etc/conf_dsm.pm
@@ -1,14 +1,12 @@
 # String for exclusion
 $EXCLUDESTR = "";
 
-$LOG = "@CENTREON_LOG@/dynamicTrap.log";
+$LOG = "@CENTREON_LOG@/centreon-dsm.log";
 
 $NAGIOSCMD = "@NAGIOS_CMD@/nagios.cmd";
 $CECORECMD = "@CENTREON_VARLIB@/centcore.cmd";
 
-$LOCKDIR = "@CENTREON_VARLIB@/centreon-dsm/tmp/";
-$CACHEDIR = "@CENTREON_VARLIB@/centreon-dsm/cache/";
-$MAXDATAAGE = 5;
+$MAXDATAAGE = 15;
 
 # Force recovery if the service is ok 
 $FORCEFREE = 1;
@@ -28,9 +26,8 @@ $ACTION_ALARM = 0;
 #$action_list[$ACTION_ALARM]->{'host'}->{'run'} = 'rename';
 #$action_list[$ACTION_ALARM]->{'host'}->{'pattern'} = "";
 
-
 # Application conf
 $DEBUG_ENABLED = 0;
-$USE_LONG_OPT = 0;
+$USE_LONG_OPT = 1;
 
 1;
