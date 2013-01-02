e2528372485c41ccda581c8c64bb0f4065e592bf
Julien Mathis <jmathis@merethis.com>
2013-01-02 14:54:48 +0000

    prepare release 2.0.0b2
    git-svn-id: http://svn.modules.centreon.com/centreon-dsm/trunk@142 2223b321-f9ee-48cb-83f9-124db436e5d3

---
+++ centreon-dsm/centreon-dsm/www/modules/centreon-dsm/UPGRADE/centreon-dsm-2.0.0b2/conf.php
@@ -0,0 +1,10 @@
+<?php
+$upgrade_conf['centreon-dsm']["rname"] = "centreon-dsm";
+$upgrade_conf['centreon-dsm']["release_from"] = "2.0.0b1";
+$upgrade_conf['centreon-dsm']["release_to"] = "2.0.0b2";
+$upgrade_conf['centreon-dsm']["is_removeable"] = "1";
+$upgrade_conf['centreon-dsm']["author"] = "Merethis";
+$upgrade_conf['centreon-dsm']["lang_files"] = "0";
+$upgrade_conf['centreon-dsm']["sql_files"] = "1";
+$upgrade_conf['centreon-dsm']["php_files"] = "0";
+?>
