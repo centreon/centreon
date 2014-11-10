572fdcf4835e50cb39a65ce9a5176417787893ff
julienmathis <julien.mathis@gmail.com>
2014-11-10 15:59:39 +0100

    change criticities by severities and add export function

--- widgets/centreon-widget-service-monitoring/service-monitoring/src/export.php
+++ widgets/centreon-widget-service-monitoring/service-monitoring/src/export.php
@@ -268,7 +268,7 @@ if (isset($preferences['order_by']) && $preferences['order_by'] != "") {
     $orderby = $preferences['order_by'];
 }
 $query .= "ORDER BY $orderby";
-$query .= " LIMIT ".($page * $preferences['entries']).",".$preferences['entries'];
+
 $res = $dbb->query($query);
 $nbRows = $dbb->numberRows();
 $data = array();
@@ -299,7 +299,7 @@ while ($row = $res->fetchRow()) {
             $value = $svcObj->replaceMacroInString($service_id, $value);
         } elseif ($key == "criticality_id" && $value != '') {
 	  $critData = $criticality->getData($row["criticality_id"], 1);
-	  $value = "<img src='../../img/media/".$media->getFilename($critData['icon_id'])."' title='".$critData["hc_name"]."'>";        
+	  $value = $critData["hc_name"];        
 	}
         $data[$row['host_id']."_".$row['service_id']][$key] = $value;
     }
@@ -307,48 +307,5 @@ while ($row = $res->fetchRow()) {
 $template->assign('centreon_web_path', trim($centreon->optGen['oreon_web_path'], "/"));
 $template->assign('preferences', $preferences);
 $template->assign('data', $data);
-$template->display('index.ihtml');
-?>
-<script type="text/javascript">
-var nbRows = <?php echo $nbRows;?>;
-var currentPage = <?php echo $page;?>;
-var orderby = '<?php echo $orderby;?>';
-var nbCurrentItems = <?php echo count($data);?>;
+$template->display('export.ihtml');
 
-$(function () {
-    $("#HostTable").styleTable();
-    if (nbRows > itemsPerPage) {
-      $("#pagination").pagination(nbRows, {
-	items_per_page	: itemsPerPage,
-	    current_page : pageNumber,
-	    callback : paginationCallback
-	    }).append("<br/>");
-    }
-    
-    $("#nbRows").html(nbCurrentItems+"/"+nbRows);
-    
-    $(".selection").each(function() {
-	var curId = $(this).attr('id');
-	if (typeof(clickedCb[curId]) != 'undefined') {
-	  this.checked = clickedCb[curId];
-	}
-      });
-    
-    var tmp = orderby.split(' ');
-    var icn = 'n';
-    if (tmp[1] == "DESC") {
-      icn = 's';
-    }
-    $("[name="+tmp[0]+"]").append('<span style="position: relative; float: right;" class="ui-icon ui-icon-triangle-1-'+icn+'"></span>');
-
-});
-
-function paginationCallback(page_index, jq)
-{
-  if (page_index != pageNumber) {
-    pageNumber = page_index;
-    clickedCb = new Array();
-    loadPage();
-  }
-}
-</script>
