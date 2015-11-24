ef85a2067d11b763963db93f21e8be5f5bf54dec
ClaireGizard <cgizard@centreon.com>
2015-11-24 16:59:26 +0100

    modif name and preferences

--- widgets/centreon-widget-engine-status/engine-status/src/data.php
+++ widgets/centreon-widget-engine-status/engine-status/src/data.php
@@ -69,7 +69,7 @@ if (CentreonSession::checkSession(session_id(), $db_centreon) == 0) {
 error_log("Debut widget claire");
 
 // Configure new smarty object
-$path = $centreon_path . "www/widgets/widget-engine-status/src/";
+$path = $centreon_path . "www/widgets/engine-status/src/";
 $template = new Smarty();
 $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
 
