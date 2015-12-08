59adec880ecb604088bee461611288585f1c24ba
ClaireGizard <cgizard@centreon.com>
2015-12-08 10:20:27 +0100

    mise en forme code

--- widgets/centreon-widget-live-top10-memory-usage/live-top10-memory-usage/index.php
+++ widgets/centreon-widget-live-top10-memory-usage/live-top10-memory-usage/index.php
@@ -33,34 +33,41 @@
  *
  */
 
+ini_set("log_error",1);
+ini_set("error_log", "/tmp/php-error.log");
+
 require_once "../require.php";
 require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
 require_once $centreon_path . 'www/class/centreonDB.class.php';
 require_once $centreon_path . 'www/class/centreonWidget.class.php';
+require_once "/etc/centreon/centreon.conf.php";
+require_once $centreon_path . 'www/class/centreonDuration.class.php';
+require_once $centreon_path . 'www/class/centreonUtils.class.php';
+require_once $centreon_path . 'www/class/centreonACL.class.php';
+require_once $centreon_path . 'www/class/centreonHost.class.php';
+
 
 session_start();
+
+//load smarty
+require_once $centreon_path . 'GPL_LIB/Smarty/libs/Smarty.class.php';
 
 if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
     exit;
 }
+
 $centreon = $_SESSION['centreon'];
 $widgetId = $_REQUEST['widgetId'];
 
 try {
     global $pearDB;
 
-    $db = new CentreonDB();
-    $db2 = new CentreonDB("centstorage");
-    $pearDB = $db;
+    $db_centreon = new CentreonDB();
+    $db = new CentreonDB("centstorage");
+    $pearDB = $db_centreon;
 
-    if ($centreon->user->admin == 0) {
-        $access = new CentreonACL($centreon->user->get_id());
-        $grouplist = $access->getAccessGroups();
-        $grouplistStr = $access->getAccessGroupsString();
-    }
-
-    $widgetObj = new CentreonWidget($centreon, $db);
+    $widgetObj = new CentreonWidget($centreon, $db_centreon);
     $preferences = $widgetObj->getWidgetPreferences($widgetId);
     $autoRefresh = 0;
     if (isset($preferences['refresh_interval'])) {
@@ -71,61 +78,101 @@ try {
     echo $e->getMessage() . "<br/>";
     exit;
 }
-?>
-<html>
-    <head>
-    	<title>top10-memory</title>
-        <link href="<?php echo '../../Themes/Centreon-2/Color/blue_css.php';?>" rel="stylesheet" type="text/css"/>
-        <link href="../../Theme/Centreon-2/style.css" type="text/css" />
-        <link href="src/top10.css" rel="styleheet" type="text/css"/>
-        <script type="text/javascript" src="../../include/common/javascript/jquery/jquery.js"></script>
-    	<script type="text/javascript" src="../../include/common/javascript/jquery/jquery-ui.js"></script>
-        <script type="text/javascript" src="../../include/common/javascript/widgetUtils.js"></script>
-  <style type="text/css">
-    .ListTable {font-size:11px;border-color: #BFD0E2;}                                                                                                                                                      
-   </style>
-  </head>
-    <body>
-	<?php 
-    if (/* isset($preferences['']) */ /* && */ $preferences['service_description'] != '' && $preferences['metric_name'] != '' && $preferences['nb_lin'] != '') {
-            print "<div id=\"top10memory\"></div>";         
-        } else {
-            print "<center><div class='update' style='text-align:center;width:350px;'>"._("Please select a metric_name, nb_lin, service_description")."</div></center>";
-        } 
-	?>
 
-    </body>
-<script type="text/javascript">
-    var widgetId = <?php echo $widgetId; ?>;
-    var autoRefresh = <?php echo $autoRefresh;?>;
-    var timeout;
-    
-jQuery(function() {
-    console.log("jQuery function");
-        loadTop10();
-});
-    
-function loadTop10() {
-    jQuery.ajax("./src/data.php?widgetId="+widgetId, {
-        success : function(htmlData) {
-             console.log("Success load");
-            jQuery("#top10memory").html("");
-            jQuery("#top10memory").html(htmlData);
-            var h = document.getElementById("top10memory").scrollHeight + 10;
-            if(h){
-                console.log("h recupérée");
-                parent.iResize(window.name, h);
-            }else{
-                parent.iResize(window.name, 200);
-            }
-        }
-    });
-    if (autoRefresh) {
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-        timeout = setTimeout(loadTop10, (autoRefresh * 1000));
+ if ($centreon->user->admin == 0) {
+        $access = new CentreonACL($centreon->user->get_id());
+        $grouplist = $access->getAccessGroups();
+        $grouplistStr = $access->getAccessGroupsString();
     }
+
+$path = $centreon_path . "www/widgets/live-top10-memory-usage/src/";
+$template = new Smarty();
+$template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
+
+$data = array();
+
+if ($preferences['host_group'] == ''){
+$query = "select DISTINCT T1.service_id, T1.host_id, T1.host_name, T1.service_description, current_value/max as ratio, max-current_value as remaining_space, T3.state as status           
+from index_data T1, metrics T2, services T3, hosts T4  " .($centreon->user->admin == 0 ? ", centreon_acl acl" : ""). "                                                                    
+where T1.service_description like '%".$preferences['service_description']."%'                                                                                                             
+AND metric_name like '%".$preferences['metric_name']."%'                                                                                                                                  
+and T2.index_id = id                                                                                                                                                                      
+and T1.service_id = T3.service_id                                                                                                                                                         
+and T1.host_id = T3.host_id                                                                                                                                                               
+AND Max is not null                                                                                                                                                                       
+and T2.index_id = id                                                                                                                                                                      
+and T1.host_name = T4.name                                                                                                                                                                
+and T4.enabled = 1                                                                                                                                                                        
+" .($centreon->user->admin == 0 ? " AND T1.host_id = acl.host_id AND T1.service_id = acl.service_id AND acl.group_id IN (" .($grouplistStr != "" ? $grouplistStr : 0). ")" : ""). "       
+order by ratio desc limit ".$preferences['nb_lin'].";";
+
+} else {
+
+$query = "select T2.service_id, T2.host_id, T2.host_name, T2.service_description, current_value/Max as ratio, Max-current_value as remaining_space, T1.state as status                    
+from services T1, index_data T2, metrics T3, hosts_hostgroups T5, hosts T6 " .($centreon->user->admin == 0 ? ", centreon_acl acl" : ""). "                                                
+where T2.service_description like '%".$preferences['service_description']."%'                                                                                                             
+AND Max is not null                                                                                                                                                                       
+and T3.index_id = id                                                                                                                                                                      
+and T2.service_id = T1.service_id                                                                                                                                                         
+and T2.host_id = T1.host_id                                                                                                                                                               
+and metric_name like '%".$preferences['metric_name']."%'                                                                                                                                  
+and T5.hostgroup_id = ".$preferences['host_group']."                                                                                                                                      
+and T1.host_id = T5.host_id                                                                                                                                                               
+and T2.host_name = T6.name                                                                                                                                                                
+and T6.enabled = 1                                                                                                                                                                        
+and T6.host_id = T5.host_id                                                                                                                                                               
+" .($centreon->user->admin == 0 ? " AND T1.host_id = acl.host_id AND T1.service_id = acl.service_id AND acl.group_id AND T4.hg_id AND T5.hostgroup_id IN (" .($grouplistStr != "" ? $grouplistStr : 0). ")" : ""). " 
+group by T2.host_id order by ratio desc limit ".$preferences['nb_lin'].";";
 }
-</script>
-</html>
+
+error_log($query);
+$numLine = 1;
+$in = 0;
+
+function getUnit($in)
+{
+  if ($in == 0) {
+    $return = "B";
+    return $return;
+  }
+  else if ($in == 1) {
+    $return = "KB";
+    return $return;
+  }
+  else if ($in == 2) {
+    $return = "MB";
+    return $return;
+  }
+  else if ($in == 3) {
+    $return = "GB";
+    return $return;
+  }
+  else if ($in == 4) {
+    $return = "TB";
+    return $return;
+  }
+}
+
+error_log('ICI');
+$res = $db->query($query);
+while ($row = $res->fetchRow()) {
+  $row['numLin'] = $numLine;
+  while ($row['remaining_space'] >= 1024) {
+    $row['remaining_space'] = $row['remaining_space'] / 1024;
+    $in = $in + 1;
+  }
+  $row['unit'] = getUnit($in);
+  $in = 0;
+  $row['remaining_space'] = round($row['remaining_space']);
+  $row['ratio'] = ceil($row['ratio'] * 100);
+  $data[] = $row;
+  $numLine++;
+}
+
+$template->assign('preferences', $preferences);
+$template->assign('widgetID', $widgetId);
+$template->assign('preferences', $preferences);
+$template->assign('data', $data);
+$template->display('table_top10memory.ihtml');
+?>
+
