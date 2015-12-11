b9dbdd0f2ebfc79aca596ba698814a13c11c6e63
ClaireGizard <cgizard@centreon.com>
2015-12-11 11:12:05 +0100

    Revert "modif beug ajax"
    This reverts commit 6b8f92500ebff38c3850c99c58fad2b120f2a075.

--- widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/src/data_js.js
+++ widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/src/data_js.js
@@ -1,23 +1,23 @@
 var timeout;
 
 jQuery(function() {
-        console.log("jQuery function");
+	console.log("jQuery function");
         loadTop10();
     });
 
 function loadTop10() {
-    jQuery.ajax("./index.php", {
-            success : function(htmlData) {
-                jQuery("#infoAjax").html("");
-                jQuery("#infoAjax").html(htmlData);
-                var h = document.getElementById("top-10-memory").scrollHeight + 10;
-                if(h){
-                    parent.iResize(window.name, h);
-                }else{
-                    parent.iResize(window.name, 200);
-                }
-            }
-        });
+    jQuery.ajax("./ajax.php", {
+	    success : function(htmlData) {
+		jQuery("#infoAjax").html("");
+		jQuery("#infoAjax").html(htmlData);
+		var h = document.getElementById("top-10-cpu").scrollHeight + 10;
+		if(h){
+		    parent.iResize(window.name, h);
+		}else{
+		    parent.iResize(window.name, 200);
+		}
+	    }
+	});
     if (autoRefresh) {
         if (timeout) {
             clearTimeout(timeout);
