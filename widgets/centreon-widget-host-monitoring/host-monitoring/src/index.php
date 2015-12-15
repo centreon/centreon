d6ac2063342a3c7b66b038831b9f6bd4dfeedb5e
ClaireGizard <cgizard@centreon.com>
2015-12-15 17:46:31 +0100

    mise au propre

--- widgets/centreon-widget-host-monitoring/host-monitoring/src/index.php
+++ widgets/centreon-widget-host-monitoring/host-monitoring/src/index.php
@@ -257,6 +257,14 @@ while ($row = $res->fetchRow()) {
 }
 
 $aColorHost = array(0 => 'host_up', 1 => 'host_down', 2 => 'host_unreachable', 4 => 'host_pending');
+
+$autoRefresh = $preferences['refresh_interval'];
+$template->assign('widgetId', $widgetId);
+$template->assign('autoRefresh', $autoRefresh);
+$template->assign('preferences', $preferences);
+$template->assign('page', $page);
+$template->assign('dataJS', count($data));
+$template->assign('nbRows', $nbRows);
 $template->assign('aColorHost', $aColorHost);
 $template->assign('centreon_web_path', trim($centreon->optGen['oreon_web_path'], "/"));
 $template->assign('preferences', $preferences);
@@ -264,47 +272,7 @@ $template->assign('data', $data);
 $template->assign('broker', "broker");
 $template->assign('title_graph', _('See Graphs of this host'));
 $template->assign('title_flapping', _('Host is flapping'));
-$template->display('index.ihtml');
 
+$template->display('table.ihtml');
 ?>
-<script type="text/javascript">
-    var nbRows = <?php echo $nbRows;?>;
-    var currentPage = <?php echo $page;?>;
-    var orderby = '<?php echo $orderby;?>';
-    var nbCurrentItems = <?php echo count($data);?>;
 
-    $(function () {
-        if (nbRows > itemsPerPage) {
-            $("#pagination").pagination(nbRows, {
-                items_per_page	: itemsPerPage,
-                current_page	: pageNumber,
-                callback	: paginationCallback
-            }).append("<br/>");
-        }
-
-        $("#nbRows").html(nbCurrentItems+"/"+nbRows);
-
-        $(".selection").each(function() {
-            var curId = $(this).attr('id');
-            if (typeof(clickedCb[curId]) != 'undefined') {
-                this.checked = clickedCb[curId];
-            }
-        });
-
-        var tmp = orderby.split(' ');
-        var icn = 'n';
-        if (tmp[1] == "DESC") {
-            icn = 's';
-        }
-        $("[name="+tmp[0]+"]").append('<span style="position: relative; float: right;" class="ui-icon ui-icon-triangle-1-'+icn+'"></span>');
-    });
-
-    function paginationCallback(page_index, jq)
-    {
-        if (page_index != pageNumber) {
-            pageNumber = page_index;
-            clickedCb = new Array();
-            loadPage();
-        }
-    }
-</script>
