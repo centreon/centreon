616ba931477f5c8fd39d6acd23b1139f18563a73
Toufik MECHOUET <mtoufik77dz@yahoo.fr>
2016-02-16 16:12:09 +0100

    Resize height : let us choose #37

--- widgets/centreon-widget-service-monitoring/service-monitoring/data.js
+++ widgets/centreon-widget-service-monitoring/service-monitoring/data.js
@@ -1,21 +1,21 @@
-jQuery(function() {
-	loadPage();
-    });
+jQuery(function () {
+    loadPage();
+});
 
 /**
  * Load Page
  */
 function loadPage()
 {
-    jQuery.ajax("./src/index.php?widgetId="+widgetId+"&page="+pageNumber, {
-	    success : function(htmlData) {
-		jQuery("#serviceMonitoringTable").empty().append(htmlData);
-		var hostMonitoringTable = jQuery("#serviceMonitoringTable").find("img, style, script, link").load(function() {
-			var h = document.getElementById("serviceMonitoringTable").scrollHeight +50;
-			ResizeFrame(window.name, h);
-		    });
-	    }
-	});
+    jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
+        success: function (htmlData) {
+            jQuery("#serviceMonitoringTable").empty().append(htmlData);
+            var hostMonitoringTable = jQuery("#serviceMonitoringTable").find("img, style, script, link").load(function () {
+                var h = document.getElementById("serviceMonitoringTable").scrollHeight + 50;
+                parent.iResize(window.name, h);
+            });
+        }
+    });
     if (autoRefresh) {
         if (timeout) {
             clearTimeout(timeout);
@@ -30,33 +30,24 @@ function loadPage()
 function loadToolBar()
 {
     jQuery("#toolBar").load(
-			    "./src/toolbar.php",
-			    {
-				widgetId : widgetId
-				    }
-			    );
+            "./src/toolbar.php",
+            {
+                widgetId: widgetId
+            }
+    );
 }
 
-jQuery(function() {
+jQuery(function () {
     loadToolBar();
     loadPage();
     $('.checkall').live('click', function () {
-            var chck = this.checked;
-            $(this).parents().find(':checkbox').each(function() {
-                    $(this).attr('checked', chck);
-                    clickedCb[$(this).attr('id')] = chck;
-                });
+        var chck = this.checked;
+        $(this).parents().find(':checkbox').each(function () {
+            $(this).attr('checked', chck);
+            clickedCb[$(this).attr('id')] = chck;
         });
-    $(".selection").live('click', function() {
-            clickedCb[$(this).attr('id')] = this.checked;
-        });
+    });
+    $(".selection").live('click', function () {
+        clickedCb[$(this).attr('id')] = this.checked;
+    });
 });
-
-
-function ResizeFrame(ifrm, height)
-{
-    if (height < 150) {
-        height = 150;
-    }
-    jQuery("[name=" + ifrm + "]").height(height);
-}
