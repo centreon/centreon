1f543843c1dcab8adb98cef298b583b6263fb151
garnier-quentin <qgarnier@centreon.com>
2016-02-24 10:16:20 +0100

    + Update licence

--- centreon-open-tickets/www/modules/centreon-open-tickets/providers/register.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/providers/register.php
@@ -1,15 +1,23 @@
 <?php
 /*
- * CENTREON
+ * Copyright 2015 Centreon (http://www.centreon.com/)
  *
- * Source Copyright 2005-2015 CENTREON
+ * Centreon is a full-fledged industry-strength solution that meets 
+ * the needs in IT infrastructure and application monitoring for 
+ * service performance.
  *
- * Unauthorized reproduction, copy and distribution
- * are not allowed.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * For more information : contact@centreon.com
+ *    http://www.apache.org/licenses/LICENSE-2.0  
  *
-*/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,*
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 $register_providers = array();
 
