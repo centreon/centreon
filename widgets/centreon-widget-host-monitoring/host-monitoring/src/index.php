df57ef98ebbcf20f332962b2433cd9788253015b
Kevin Duret <kduret@centreon.com>
2016-02-29 14:27:47 +0100

    #8 Fix https urls

--- widgets/centreon-widget-host-monitoring/host-monitoring/src/index.php
+++ widgets/centreon-widget-host-monitoring/host-monitoring/src/index.php
@@ -236,8 +236,10 @@ while ($row = $res->fetchRow()) {
         } elseif ($key == "output") {
             $value = substr($value, 0, $outputLength);
         } elseif (($key == "action_url" || $key == "notes_url") && !empty($value)) {
-            $value = str_replace("http://", "", $value);
-            $value = urlencode($hostObj->replaceMacroInString($row['host_name'], $value));
+            if (!preg_match("/(^http[s]?)|(^\/\/)/", $value)) {
+                $value = '//' . $value;
+            }
+            $value = CentreonUtils::escapeSecure($hostObj->replaceMacroInString($row['host_name'], $value));
         } elseif ($key == "criticality" && $value != '') {
             $critData = $criticality->getData($row["criticality_id"]);
             $value = "<img src='../../img/media/".$media->getFilename($critData['icon_id'])."' title='".$critData["hc_name"]."' width='16' height='16'>";
