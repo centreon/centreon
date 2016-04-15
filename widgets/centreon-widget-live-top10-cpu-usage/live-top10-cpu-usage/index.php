5671a6b33bfd423134f007176458802e386d6357
Florian Asche <github@florian-asche.de>
2016-04-15 14:23:54 +0200

    Update index.php
    This require is no more nessesary due to ../require.php and this widget is not working, with own build rpm in custom path.

--- widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/index.php
+++ widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/index.php
@@ -40,7 +40,6 @@ require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
 require_once $centreon_path . 'www/class/centreonDB.class.php';
 require_once $centreon_path . 'www/class/centreonWidget.class.php';
-require_once "/etc/centreon/centreon.conf.php";
 require_once $centreon_path . 'www/class/centreonDuration.class.php';
 require_once $centreon_path . 'www/class/centreonUtils.class.php';
 require_once $centreon_path . 'www/class/centreonACL.class.php';
