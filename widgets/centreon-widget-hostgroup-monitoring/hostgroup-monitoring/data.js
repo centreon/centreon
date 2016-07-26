93873e00d25615ab50372d0cfa1d9683c1c091b1
Guillaume28 <guillaume-baldo@hotmail.fr>
2016-07-26 11:07:02 +0200

    Add csv export

--- widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/data.js
+++ widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/data.js
@@ -1,23 +1,22 @@
-jQuery(function () {
+jQuery(function() {
     loadPage();
 });
 
-/**                                                                                                                                                                                                          
- * Load page                                                                                                                                                                                                 
+/**
+ * Load page
  */
 function loadPage()
 {
-    jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
-        success: function (htmlData) {
+    jQuery.ajax("./src/index.php?widgetId="+widgetId+"&page="+pageNumber, {
+        success : function(htmlData) {
             jQuery("#hgMonitoringTable").html("");
             jQuery("#hgMonitoringTable").html(htmlData);
             var h = document.getElementById("hgMonitoringTable").scrollHeight + 10;
-            ResizeFrame(window.name, h);
-            jQuery("#hgMonitoringTable").find("img, style, script, link").load(function () {
+            parent.iResize(window.name, h);
+            jQuery("#hgMonitoringTable").find("img, style, script, link").load(function(){
                 var h = document.getElementById("hgMonitoringTable").scrollHeight + 10;
-                ResizeFrame(window.name, h);
+                parent.iResize(window.name, h);
             });
-
         }
     });
     if (autoRefresh) {
@@ -27,12 +26,3 @@ function loadPage()
         timeout = setTimeout(loadPage, (autoRefresh * 1000));
     }
 }
-
-function ResizeFrame(ifrm, height)
-{
-    if (height < 150) {
-            height = 150;
-    }
-    jQuery("[name="+ifrm+"]").height(height);
-}
-
