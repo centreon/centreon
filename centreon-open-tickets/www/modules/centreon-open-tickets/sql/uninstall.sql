fe0422f1825166ea69383206256b39a32599adc8
garnier-quentin <qgarnier@centreon.com>
2016-10-12 15:07:51 +0200

    + add uninstall page

--- centreon-open-tickets/www/modules/centreon-open-tickets/sql/uninstall.sql
+++ centreon-open-tickets/www/modules/centreon-open-tickets/sql/uninstall.sql
@@ -8,4 +8,7 @@ DELETE FROM topology WHERE topology_page = '60420' AND topology_name = 'Rules';
 DELETE FROM topology WHERE topology_parent = '604' AND topology_name = 'Open Tickets';
 DELETE FROM topology_JS WHERE id_page = '60420';
 
+DELETE FROM topology WHERE topology_page = '20320' AND topology_name = 'Ticket Logs';
+DELETE FROM topology_JS WHERE id_page = '20320';
+
 DELETE FROM widget_parameters_field_type WHERE ft_typename = 'openTicketsRule';
