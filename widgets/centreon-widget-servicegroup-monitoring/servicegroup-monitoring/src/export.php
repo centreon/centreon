da77c9c7e1769fcd575ad34f5e1240ca654c2d23
Julien Mathis <jmathis@centreon.com>
2016-11-25 18:52:49 +0100

    improve widget

--- widgets/centreon-widget-servicegroup-monitoring/servicegroup-monitoring/src/export.php
+++ widgets/centreon-widget-servicegroup-monitoring/servicegroup-monitoring/src/export.php
@@ -33,9 +33,8 @@
  *
  */
 
-ini_set("log_errors",1);
-ini_set("error_log", "/tmp/php-error.log");
-
+header('Content-type: application/csv');
+header('Content-Disposition: attachment; filename="servicegroups-monitoring.csv"');
 
 require_once "../../require.php";
 require_once $centreon_path . 'www/class/centreon.class.php';
@@ -48,7 +47,7 @@ require_once $centreon_path . 'www/class/centreonACL.class.php';
 require_once $centreon_path . 'www/widgets/servicegroup-monitoring/src/class/ServicegroupMonitoring.class.php';
 
 session_start();
-if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId']) || !isset($_REQUEST['page'])) {
+if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
     exit;
 }
 $db = new CentreonDB();
@@ -56,7 +55,7 @@ if (CentreonSession::checkSession(session_id(), $db) == 0) {
     exit;
 }
 
-require_once $centreon_path . "GPL_LIB/Smarty/libs/Smarty.class.php";
+require_once $centreon_path ."GPL_LIB/Smarty/libs/Smarty.class.php";
 $path = $centreon_path . "www/widgets/servicegroup-monitoring/src/";
 $template = new Smarty();
 $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
@@ -71,36 +70,53 @@ $sgMonObj = new ServicegroupMonitoring($dbb);
 $preferences = $widgetObj->getWidgetPreferences($widgetId);
 $pearDB = $db;
 $aclObj = new CentreonACL($centreon->user->user_id, $centreon->user->admin);
-$nbRows = $preferences['entries'];
 
+$res = $db->query("SELECT `key`, `value` FROM `options` WHERE `key` LIKE 'color%'");
+$hostStateColors = array(0 => "#19EE11",
+                         1 => "#F91E05",
+                         2 => "#82CFD8",
+                         4 => "#2AD1D4");
 
-$hostStateColors = array(
-    0 => "#88B917",
-    1 => "#F91E05",
-    2 => "#82CFD8",
-    4 => "#2AD1D4");
+$serviceStateColors = array(0 => "#13EB3A",
+                            1 => "#F8C706",
+                            2 => "#F91D05",
+                            3 => "#DCDADA",
+                            4 => "#2AD1D4");
 
-$serviceStateColors = array(
-    0 => "#88B917",
-    1 => "#FF9A13",
-    2 => "#E00B3D",
-    3 => "#DCDADA",
-    4 => "#2AD1D4");
+while ($row = $res->fetchRow()) {
+    if ($row['key'] == "color_up") {
+        $hostStateColors[0] = $row['value'];
+    } elseif ($row['key'] == "color_down") {
+        $hostStateColors[1] = $row['value'];
+    } elseif ($row['key'] == "color_unreachable") {
+        $hostStateColors[2] = $row['value'];
+    } elseif ($row['key'] == "color_pending") {
+        $hostStateColors[4] = $row['value'];
+    } elseif ($row['key'] == "color_ok") {
+        $serviceStateColors[0] = $row['value'];
+    } elseif ($row['key'] == "color_warning") {
+        $serviceStateColors[1] = $row['value'];
+    } elseif ($row['key'] == "color_critical") {
+        $serviceStateColors[2] = $row['value'];
+    } elseif ($row['key'] == "color_unknown") {
+        $serviceStateColors[3] = $row['value'];
+    } elseif ($row['key'] == "color_pending") {
+        $serviceStateColors[4] = $row['value'];
+    }
+}
 
-$aColorHost = array(0 => 'host_up', 1 => 'host_down', 2 => 'host_unreachable', 4 => 'host_pending');
-$aColorService = array(0 => 'service_ok', 1 => 'service_warning', 2 => 'service_critical', 3 => 'service_unknown', 4 => 'pending');
 $hostStateLabels = array(0 => "Up",
-    1 => "Down",
-    2 => "Unreachable",
-    4 => "Pending");
+                         1 => "Down",
+                         2 => "Unreachable",
+                         4 => "Pending");
 
 $serviceStateLabels = array(0 => "Ok",
-    1 => "Warning",
-    2 => "Critical",
-    3 => "Unknown",
-    4 => "Pending");
+                            1 => "Warning",
+                            2 => "Critical",
+                            3 => "Unknown",
+                            4 => "Pending");
 
-$query = "SELECT SQL_CALC_FOUND_ROWS DISTINCT name, servicegroup_id ";
+$query = "SELECT SQL_CALC_FOUND_ROWS DISTINCT name ";
 $query .= "FROM servicegroups ";
 if (isset($preferences['sg_name_search']) && $preferences['sg_name_search'] != "") {
     $tab = split(" ", $preferences['sg_name_search']);
@@ -109,12 +125,14 @@ if (isset($preferences['sg_name_search']) && $preferences['sg_name_search'] != "
         $search = $tab[1];
     }
     if ($op && isset($search) && $search != "") {
-        $query = CentreonUtils::conditionBuilder($query, "name " . CentreonUtils::operandToMysqlFormat($op) . " '" . $dbb->escape($search) . "' ");
+        $query = CentreonUtils::conditionBuilder($query, "name ".CentreonUtils::operandToMysqlFormat($op)." '".$dbb->escape($search)."' ");
     }
 }
 if (!$centreon->user->admin) {
-    $query = CentreonUtils::conditionBuilder($query, "name IN (" . $aclObj->getServiceGroupsString("NAME") . ")");
+    $query = CentreonUtils::conditionBuilder($query, "name IN (".$aclObj->getServiceGroupsString("NAME").")");
 }
+
+$query = CentreonUtils::conditionBuilder($query, "enabled=1");
 
 
 $orderby = "name ASC";
@@ -122,10 +140,9 @@ if (isset($preferences['order_by']) && $preferences['order_by'] != "") {
     $orderby = $preferences['order_by'];
 }
 $query .= "ORDER BY $orderby";
-$query .= " LIMIT " . ($page * $preferences['entries']) . "," . $preferences['entries'];
-$res = $dbb->query($query);
+$query .= " LIMIT ".($page * $preferences['entries']).",".$preferences['entries'];
 
-error_log($query);
+$res = $dbb->query($query);
 $nbRows = $dbb->numberRows();
 $data = array();
 $detailMode = false;
@@ -133,37 +150,19 @@ if (isset($preferences['enable_detailed_mode']) && $preferences['enable_detailed
     $detailMode = true;
 }
 while ($row = $res->fetchRow()) {
-    $data[$row['name']] = array('name'          => $row['name'],
-                                'svc_id'        => $row['servicegroup_id'],
-                                'sgurl'         => "main.php?p=20201&search=0&host_search=0&output_search=0&hg=0&sg=" .$row['servicegroup_id'],
-                                'hosturl'       => "main.php?p=20202",
-                                'host_state'    => $sgMonObj->getHostStates($row['name'], $detailMode, $centreon->user->admin, $aclObj, $preferences),
-                                'service_state' => $sgMonObj->getServiceStates($row['name'], $detailMode, $centreon->user->admin, $aclObj, $preferences));
+    foreach ($row as $key => $value) {
+        $data[$row['name']][$key] = $value;
+        $data[$row['name']]['host_state'] = $sgMonObj->getHostStates($row['name'], $detailMode, $centreon->user->admin, $aclObj, $preferences);
+        $data[$row['name']]['service_state'] = $sgMonObj->getServiceStates($row['name'], $detailMode, $centreon->user->admin, $aclObj, $preferences);
+    }
 }
-
-$autoRefresh = $preferences['refresh_interval'];
-$template->assign('widgetId', $widgetId);
-$template->assign('autoRefresh', $autoRefresh);
-$template->assign('preferences', $preferences);
-$template->assign('nbRows', $nbRows);
-$template->assign('page', $page);
-$template->assign('orderby', $orderby);
-$template->assign('data', $data);
-$template->assign('dataJS', count($data));
-$template->assign('aColorHost', $aColorHost);
-$template->assign('aColorService', $aColorService);
 $template->assign('preferences', $preferences);
 $template->assign('hostStateLabels', $hostStateLabels);
 $template->assign('hostStateColors', $hostStateColors);
 $template->assign('serviceStateLabels', $serviceStateLabels);
 $template->assign('serviceStateColors', $serviceStateColors);
-$template->assign('centreon_web_path', trim($centreon->optGen['oreon_web_path'], "/"));
-$template->assign('centreon_path', $centreon_path);
+$template->assign('centreon_web_path', $centreon->optGen['oreon_web_path']);
 
-$bMoreViews = 0;
-if ($preferences['more_views']) {
-    $bMoreViews = $preferences['more_views'];
-}
-$template->assign('more_views', $bMoreViews);
+$template->assign('data', $data);
 
-$template->display('table.ihtml');
+$template->display('export.ihtml');
