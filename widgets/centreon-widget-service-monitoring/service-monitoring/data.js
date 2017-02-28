047c3edaf774560c7ad2c4e24a670f223af42f2b
Guillaume28 <Guillaume28@users.noreply.github.com>
2017-02-28 17:18:40 +0100

    Problem about the size done (#73)
    Fix widget init size

--- widgets/centreon-widget-service-monitoring/service-monitoring/data.js
+++ widgets/centreon-widget-service-monitoring/service-monitoring/data.js
@@ -43,8 +43,7 @@ function loadPage()
 {
     jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
         success: function (htmlData) {
-            jQuery("#serviceMonitoringTable").empty().append(htmlData);
-            var hostMonitoringTable = jQuery("#serviceMonitoringTable").find("img, style, script, link").load(function () {
+            jQuery("#serviceMonitoringTable").empty().append(htmlData).append(function() {
                 var h = document.getElementById("serviceMonitoringTable").scrollHeight + 0;
                 parent.iResize(window.name, h);
             });
