780e143c8bdcb773e9176475824009bd93bb9a5a
garnier-quentin <qgarnier@centreon.com>
2017-03-20 16:21:58 +0100

    + add an extra config file. Example of "/etc/centreon/centreon_dsmclient.pm":
    %dsmclient_config = (
           "centreon_db" => "centreon",
           "centstorage_db" => "centreon_storage",
           "db_host" => "localhost:3306",
           "db_user" => "centreon",
           "db_passwd" => "xxxx"
    );
    
    1;

--- centreon-dsm/bin/dsmclient.pl
+++ centreon-dsm/bin/dsmclient.pl
@@ -13,29 +13,32 @@ use centreon::script;
 
 use base qw(centreon::script);
 
+use vars qw(%dsmclient_config);
+
 sub new {
     my $class = shift;
     my $self = $class->SUPER::new("dsmclient",
         centreon_db_conn => 0, # use my connection because i don't need lock!!
-        centstorage_db_conn => 1,
+        centstorage_db_conn => 0,
     );
     bless $self, $class;
 
     $self->add_options(
-        "host-id:s"     => \$self->{host_id},
-        "Host:s"        => \$self->{host},
-        "H:s"           => \$self->{host}, 
-        "id:s"          => \$self->{id},
-        "i:s"           => \$self->{id},
-        "status:s"      => \$self->{status},
-        "s:s"           => \$self->{status},
-        "time:s"        => \$self->{time},
-        "t:s"           => \$self->{time},
-        "output:s"      => \$self->{output},
-        "o:s"           => \$self->{output},
-        "macro:s"       => \$self->{macro},
-        "m:s"           => \$self->{macro},
-        "pool-prefix:s" => \$self->{pool_prefix},   
+        "host-id:s"         => \$self->{host_id},
+        "Host:s"            => \$self->{host},
+        "H:s"               => \$self->{host}, 
+        "id:s"              => \$self->{id},
+        "i:s"               => \$self->{id},
+        "status:s"          => \$self->{status},
+        "s:s"               => \$self->{status},
+        "time:s"            => \$self->{time},
+        "t:s"               => \$self->{time},
+        "output:s"          => \$self->{output},
+        "o:s"               => \$self->{output},
+        "macro:s"           => \$self->{macro},
+        "m:s"               => \$self->{macro},
+        "pool-prefix:s"     => \$self->{pool_prefix},
+        "config-extra:s"    => \$self->{opt_extra},
     );
     
     return $self;
@@ -59,16 +62,26 @@ sub init {
     if (!defined($self->{time}) || $self->{time} !~ /\d+/) {
         $self->{time} = time();
     }
+    
+    if (!defined($self->{opt_extra})) {
+        $self->{opt_extra} = "/etc/centreon/centreon_dsmclient.pm";
+    }
+    if (-f $self->{opt_extra}) {
+        require $self->{opt_extra};
+    } else {
+        $self->{logger}->writeLogInfo("Can't find extra config file $self->{opt_extra}");
+    }
+    $self->{centreon_config} = {%{$self->{centreon_config}}, %dsmclient_config};
 }
 
 sub get_pool_prefix {
     my ($self, %options) = @_;
     
-    my $query = "SELECT pool_prefix FROM mod_dsm_pool WHERE pool_host_id = " . $self->{centreon_db_centreon}->quote($options{host_id}) . " AND pool_activate = '1'";
+    my $query = "SELECT pool_prefix FROM mod_dsm_pool WHERE pool_host_id = " . $self->{db_centreon}->quote($options{host_id}) . " AND pool_activate = '1'";
     if (defined($self->{pool_prefix}) && $self->{pool_prefix} ne '') {
-        $query .= "AND pool_prefix = " . $self->{centreon_db_centreon}->quote($self->{pool_prefix});
+        $query .= "AND pool_prefix = " . $self->{db_centreon}->quote($self->{pool_prefix});
     }
-    my ($status, $sth) = $self->{centreon_db_centreon}->query($query);
+    my ($status, $sth) = $self->{db_centreon}->query($query);
     if ($status == -1) {
         $self->{logger}->writeLogError("Cannot get pool prefix from database");
         exit 1;
@@ -88,8 +101,8 @@ sub get_hosts {
     
     my $host_id = $self->{host_id};
     if (defined($self->{host}) && $self->{host} ne '') {
-        my $query = "SELECT host_id FROM host WHERE host_name = " . $self->{centreon_db_centreon}->quote($self->{host}) . " OR host_address = " . $self->{centreon_db_centreon}->quote($self->{host});
-        my ($status, $sth) = $self->{centreon_db_centreon}->query($query);
+        my $query = "SELECT host_id FROM host WHERE host_name = " . $self->{db_centreon}->quote($self->{host}) . " OR host_address = " . $self->{db_centreon}->quote($self->{host});
+        my ($status, $sth) = $self->{db_centreon}->query($query);
         if ($status == -1) {
             $self->{logger}->writeLogError("Cannot get host ID from database");
             exit 1;
@@ -120,23 +133,33 @@ sub run {
 
     $self->SUPER::run();
     
-    $self->{centreon_db_centreon} = centreon::common::db->new(db => $self->{centreon_config}->{centreon_db},
+    $self->{db_centreon} = centreon::common::db->new(db => $self->{centreon_config}->{centreon_db},
                                                      host => $self->{centreon_config}->{db_host},
                                                      port => $self->{centreon_config}->{db_port},
                                                      user => $self->{centreon_config}->{db_user},
                                                      password => $self->{centreon_config}->{db_passwd},
                                                      force => 1,
                                                      logger => $self->{logger});
-    $self->{centreon_db_centreon}->connect();
+    $self->{db_centreon}->connect();
+    
+    $self->{db_centstorage} = centreon::common::db->new(
+        db => $self->{centreon_config}->{centstorage_db},
+        host => $self->{centreon_config}->{db_host},
+        port => $self->{centreon_config}->{db_port},
+        user => $self->{centreon_config}->{db_user},
+        password => $self->{centreon_config}->{db_passwd},
+        force => 1,
+        logger => $self->{logger}
+    );
+    $self->{db_centstorage}->connect();
 
     my $host_id = $self->get_hosts();
     my $pool_prefix = $self->get_pool_prefix(host_id => $host_id);
-    $self->{csdb}->connect();
     my $query = sprintf("INSERT INTO mod_dsm_cache (`host_id`, `ctime`, `status`, `pool_prefix`, `id`, `macros`, `output`) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-        $self->{csdb}->quote($host_id), $self->{csdb}->quote($self->{time}), $self->{csdb}->quote($self->{status}),
-        $self->{csdb}->quote($pool_prefix), $self->{csdb}->quote($self->{id}), $self->{csdb}->quote($self->{macros}),
-        $self->{csdb}->quote($self->{output}));
-    my ($status, $sth) = $self->{csdb}->query($query);
+        $self->{db_centstorage}->quote($host_id), $self->{db_centstorage}->quote($self->{time}), $self->{db_centstorage}->quote($self->{status}),
+        $self->{db_centstorage}->quote($pool_prefix), $self->{db_centstorage}->quote($self->{id}), $self->{db_centstorage}->quote($self->{macros}),
+        $self->{db_centstorage}->quote($self->{output}));
+    my ($status, $sth) = $self->{db_centstorage}->query($query);
     if ($status == -1) {
         $self->{logger}->writeLogError("Cannot write in database");
         exit 1;
@@ -160,6 +183,11 @@ dsmclient.pl [options]
 =item B<--config>
 
 Specify the path to the main configuration file (default: /etc/centreon/conf.pm).
+
+=item B<--config-extra>
+
+Specify the extra configuration file (default: /etc/centreon/centreon_dsmclient.pm).
+Can be used to overload database configuration.
 
 =item B<--help>
 
