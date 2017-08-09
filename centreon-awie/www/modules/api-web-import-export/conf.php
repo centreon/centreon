5c920e627a5d177040b53245c18afccef11f48da
loiclau <loic.lau@gmail.com>
2017-08-09 14:04:17 +0200

    * add element

---
+++ centreon-awie/www/modules/api-web-import-export/conf.php
@@ -0,0 +1,10 @@
+<?php
+$module_conf['api-web-import-export']['rname'] = 'Api Web Import Export';
+$module_conf['api-web-import-export']['name'] = 'api-web-import-export';
+$module_conf['api-web-import-export']["mod_release"] = "1.0.0";
+$module_conf['api-web-import-export']["infos"] = "api-web-import-export";
+$module_conf['api-web-import-export']["is_removeable"] = "1";
+$module_conf['api-web-import-export']["author"] = "Centreon";
+$module_conf['api-web-import-export']["lang_files"] = "0";
+$module_conf['api-web-import-export']["sql_files"] = "1";
+$module_conf['api-web-import-export']["php_files"] = "0";
---
+++ centreon-awie/www/modules/api-web-import-export/core/DB-Func.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CENTREON
+ *
+ * Source Copyright 2005-2015 CENTREON
+ *
+ * Unauthorized reproduction, copy and distribution
+ * are not allowed.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
