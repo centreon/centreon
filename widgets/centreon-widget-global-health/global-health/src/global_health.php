2d635d4553a6e451f75c3fdceab713b7f1635629
Laurent Pinsivy <lpinsivy@gmail.com>
2017-10-24 13:32:01 +0200

    correct #2 to display result for non admin users

--- widgets/centreon-widget-global-health/global-health/src/global_health.php
+++ widgets/centreon-widget-global-health/global-health/src/global_health.php
@@ -55,6 +55,7 @@ if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
 }
 
 $db = new CentreonDB();
+$pearDB = $db;
 if (CentreonSession::checkSession(session_id(), $db) == 0) {
     exit;
 }
