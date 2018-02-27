5f39271b4ff10b15da061623f58d1784d02f2c81
Loic Laurent <loic.lau@gmail.com>
2018-02-27 10:51:36 +0100

    feat(acceptance) add export test

---
+++ centreon-awie/features/bootstrap/CentreonAwieContext.php
@@ -0,0 +1,44 @@
+<?php
+/*
+** Copyright 2018 Centreon
+**
+** All rights reserved.
+*/
+
+use Centreon\Test\Behat\CentreonContext;
+
+class CentreonAwieContext extends CentreonContext
+{
+
+    /**
+     *  @Given I am logged in a Centreon server with Awie installed
+     */
+    public function iAmLoggedInACentreonServerWithAWIEInstalled()
+    {
+        $this->launchCentreonWebContainer('awie');
+        $this->iAmLoggedIn();
+    }
+
+    public function iAmOnTheExportPage()
+    {
+        $this->visit('main.php?p=61201');
+        // Check that page is valid for this class.
+        $mythis = $this;
+        $this->spin(
+            function ($context) use ($mythis) {
+                return $context->getSession()->getPage()->has('css', '#poller');
+            }
+        );
+    }
+
+    public function iAmOnTheImportPage()
+    {
+        $this->visit('main.php?p=61202');
+        $mythis = $this;
+        $this->spin(
+            function ($context) use ($mythis) {
+                return $context->getSession()->getPage()->has('css', '#file');
+            }
+        );
+    }
+}
---
+++ centreon-awie/features/bootstrap/ImportExportContext.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Class ImportExportContext
+ */
+class ImportExportContext extends CentreonAwieContext
+{
+    /**
+     * @When I export an object
+     */
+    public function iExportAnObject()
+    {
+        $this->iAmOnTheExportPage();
+        $this->assertFind('css', '#contact')->click();
+        $this->assertFind('css', '.bt_success')->click();
+    }
+
+    /**
+     * @Then I have a file
+     */
+    public function iHaveAFile()
+    {
+        $mythis = $this;
+        $this->spin(
+            function ($context) use ($mythis) {
+                return !$context->assertFind('css', '.loadingWrapper')->isVisible();
+            }
+        );
+        $cmd = 'ls /tmp';
+        $output = $this->container->execute(
+            $cmd,
+            'web'
+        );
+        $output = explode("\n", $output['output']);
+        $fileCreate = false;
+        foreach ($output as $file) {
+            if (substr("$file", -3) == 'zip') {
+                $fileCreate = true;
+            }
+        }
+
+        if (!$fileCreate) {
+            throw new \Exception('File not create');
+        }
+    }
+}
