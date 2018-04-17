8369492b2cd76871b3787fcaedccd15138c2e60d
Guillaume28 <Guillaume28@users.noreply.github.com>
2018-04-17 13:50:12 +0200

    Pdo (#21)
    * * use pdo
    
    * Refacto for more transparency (#17)
    
    * Refacto for more transparency
    
    * Update data.js
    
    * fix(various): Useless variable
    
    * fix(various) : Useless variable
    
    * Update data.js
    
    * Update index.php
    
    * Update export.php
    
    * Update export.php

--- widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/src/export.php
+++ widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/src/export.php
@@ -37,9 +37,9 @@ header('Content-type: application/csv');
 header('Content-Disposition: attachment; filename="hostgroups-monitoring.csv"');
 
 require_once "../../require.php";
+require_once $centreon_path . 'bootstrap.php';
 require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
-require_once $centreon_path . 'www/class/centreonDB.class.php';
 require_once $centreon_path . 'www/class/centreonWidget.class.php';
 require_once $centreon_path . 'www/class/centreonDuration.class.php';
 require_once $centreon_path . 'www/class/centreonUtils.class.php';
@@ -55,7 +55,7 @@ if (CentreonSession::checkSession(session_id(), $db) == 0) {
     exit;
 }
 
-require_once $centreon_path ."GPL_LIB/Smarty/libs/Smarty.class.php";
+require_once $centreon_path . "GPL_LIB/Smarty/libs/Smarty.class.php";
 $path = $centreon_path . "www/widgets/hostgroup-monitoring/src/";
 $template = new Smarty();
 $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
@@ -63,26 +63,27 @@ $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
 $centreon = $_SESSION['centreon'];
 $widgetId = $_REQUEST['widgetId'];
 
-$dbb = new CentreonDB("centstorage");
+$dbb = $dependencyInjector['realtime_db'];
 $widgetObj = new CentreonWidget($centreon, $db);
 $sgMonObj = new HostgroupMonitoring($dbb);
 $preferences = $widgetObj->getWidgetPreferences($widgetId);
-$pearDB = $db;
 $aclObj = new CentreonACL($centreon->user->user_id, $centreon->user->admin);
 
 $res = $db->query("SELECT `key`, `value` FROM `options` WHERE `key` LIKE 'color%'");
 $hostStateColors = array(
-			 0 => "#19EE11",
-			 1 => "#F91E05",
-			 2 => "#82CFD8",
-			 4 => "#2AD1D4");
+    0 => "#19EE11",
+    1 => "#F91E05",
+    2 => "#82CFD8",
+    4 => "#2AD1D4"
+);
 
 $serviceStateColors = array(
-			    0 => "#13EB3A",
-			    1 => "#F8C706",
-			    2 => "#F91D05",
-			    3 => "#DCDADA",
-			    4 => "#2AD1D4");
+    0 => "#13EB3A",
+    1 => "#F8C706",
+    2 => "#F91D05",
+    3 => "#DCDADA",
+    4 => "#2AD1D4"
+);
 
 while ($row = $res->fetchRow()) {
     if ($row['key'] == "color_up") {
@@ -107,17 +108,19 @@ while ($row = $res->fetchRow()) {
 }
 
 $hostStateLabels = array(
-			 0 => "Up",
-			 1 => "Down",
-			 2 => "Unreachable",
-			 4 => "Pending");
+    0 => "Up",
+    1 => "Down",
+    2 => "Unreachable",
+    4 => "Pending"
+);
 
 $serviceStateLabels = array(
-			    0 => "Ok",
-			    1 => "Warning",
-			    2 => "Critical",
-			    3 => "Unknown",
-			    4 => "Pending");
+    0 => "Ok",
+    1 => "Warning",
+    2 => "Critical",
+    3 => "Unknown",
+    4 => "Pending"
+);
 
 $query = "SELECT SQL_CALC_FOUND_ROWS DISTINCT name ";
 $query .= "FROM hostgroups ";
@@ -128,11 +131,14 @@ if (isset($preferences['hg_name_search']) && $preferences['hg_name_search'] != "
         $search = $tab[1];
     }
     if ($op && isset($search) && $search != "") {
-        $query = CentreonUtils::conditionBuilder($query, "name ".CentreonUtils::operandToMysqlFormat($op)." '".$dbb->escape($search)."' ");
+        $query = CentreonUtils::conditionBuilder(
+            $query,
+            "name " . CentreonUtils::operandToMysqlFormat($op) . " '" . $dbb->escape($search) . "' "
+        );
     }
 }
 if (!$centreon->user->admin) {
-    $query = CentreonUtils::conditionBuilder($query, "name IN (".$aclObj->getHostGroupsString("NAME").")");
+    $query = CentreonUtils::conditionBuilder($query, "name IN (" . $aclObj->getHostGroupsString("NAME") . ")");
 }
 $orderby = "name ASC";
 
@@ -142,12 +148,7 @@ if (isset($preferences['order_by']) && $preferences['order_by'] != "") {
 $query .= "ORDER BY $orderby";
 
 $res = $dbb->query($query);
-
-if (PEAR::isError($res)) {
-    die('Error in SQL queries');
-}
-
-$nbRows = $dbb->numberRows();
+$nbRows = $res->rowCount();
 $detailMode = false;
 if (isset($preferences['enable_detailed_mode']) && $preferences['enable_detailed_mode']) {
     $detailMode = true;
@@ -162,7 +163,7 @@ while ($row = $res->fetchRow()) {
 
 $sgMonObj->getHostStates($data, $detailMode, $centreon->user->admin, $aclObj, $preferences);
 $sgMonObj->getServiceStates($data, $detailMode, $centreon->user->admin, $aclObj, $preferences);
-   
+
 $template->assign('preferences', $preferences);
 $template->assign('hostStateLabels', $hostStateLabels);
 $template->assign('hostStateColors', $hostStateColors);
--- widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/src/index.php
+++ widgets/centreon-widget-hostgroup-monitoring/hostgroup-monitoring/src/index.php
@@ -34,9 +34,9 @@
  */
 
 require_once "../../require.php";
+require_once $centreon_path . 'bootstrap.php';
 require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
-require_once $centreon_path . 'www/class/centreonDB.class.php';
 require_once $centreon_path . 'www/class/centreonWidget.class.php';
 require_once $centreon_path . 'www/class/centreonDuration.class.php';
 require_once $centreon_path . 'www/class/centreonUtils.class.php';
@@ -48,13 +48,12 @@ CentreonSession::start(1);
 if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId']) || !isset($_REQUEST['page'])) {
     exit;
 }
-
-$db = new CentreonDB();
+$db = $dependencyInjector['configuration_db'];
 if (CentreonSession::checkSession(session_id(), $db) == 0) {
     exit;
 }
 
-require_once $centreon_path ."GPL_LIB/Smarty/libs/Smarty.class.php";
+require_once $centreon_path . "GPL_LIB/Smarty/libs/Smarty.class.php";
 $path = $centreon_path . "www/widgets/hostgroup-monitoring/src/";
 $template = new Smarty();
 $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
@@ -63,39 +62,52 @@ $centreon = $_SESSION['centreon'];
 $widgetId = $_REQUEST['widgetId'];
 $page = $_REQUEST['page'];
 
-$dbb = new CentreonDB("centstorage");
+$dbb = $dependencyInjector['realtime_db'];
 $widgetObj = new CentreonWidget($centreon, $db);
 $hgMonObj = new HostgroupMonitoring($dbb);
 $preferences = $widgetObj->getWidgetPreferences($widgetId);
-$pearDB = $db;
 $aclObj = new CentreonACL($centreon->user->user_id, $centreon->user->admin);
 
 $aColorHost = array(0 => 'host_up', 1 => 'host_down', 2 => 'host_unreachable', 4 => 'host_pending');
-$aColorService = array(0 => 'service_ok', 1 => 'service_warning', 2 => 'service_critical', 3 => 'service_unknown', 4 => 'pending');
+$aColorService = array(
+    0 => 'service_ok',
+    1 => 'service_warning',
+    2 => 'service_critical',
+    3 => 'service_unknown',
+    4 => 'pending'
+);
 
 
-$hostStateColors = array(0 => "#88b917",
+$hostStateColors = array(
+    0 => "#88b917",
     1 => "#e00b3d",
     2 => "#82CFD8",
-    4 => "#2ad1d4");
+    4 => "#2ad1d4"
+);
 
-$serviceStateColors = array(0 => "#88b917",
+$serviceStateColors = array(
+    0 => "#88b917",
     1 => "#F8C706",
     2 => "#e00b3d",
     3 => "#DCDADA",
-    4 => "#2ad1d4");
+    4 => "#2ad1d4"
+);
 
 
-$hostStateLabels = array(0 => "Up",
+$hostStateLabels = array(
+    0 => "Up",
     1 => "Down",
     2 => "Unreachable",
-    4 => "Pending");
+    4 => "Pending"
+);
 
-$serviceStateLabels = array(0 => "Ok",
+$serviceStateLabels = array(
+    0 => "Ok",
     1 => "Warning",
     2 => "Critical",
     3 => "Unknown",
-    4 => "Pending");
+    4 => "Pending"
+);
 
 $query = "SELECT SQL_CALC_FOUND_ROWS DISTINCT name, hostgroup_id ";
 $query .= "FROM hostgroups ";
@@ -107,35 +119,40 @@ if (isset($preferences['hg_name_search']) && $preferences['hg_name_search'] != "
         $search = $tab[1];
     }
     if ($op && isset($search) && $search != "") {
-        $query = CentreonUtils::conditionBuilder($query, "name ".CentreonUtils::operandToMysqlFormat($op)." '".$dbb->escape($search)."' ");
+        $query = CentreonUtils::conditionBuilder(
+            $query,
+            "name " . CentreonUtils::operandToMysqlFormat($op) . " '" . $dbb->escape($search) . "' "
+        );
     }
 }
 
 if (!$centreon->user->admin) {
-    $query = CentreonUtils::conditionBuilder($query, "name IN (".$aclObj->getHostGroupsString("NAME").")");
+    $query = CentreonUtils::conditionBuilder($query, "name IN (" . $aclObj->getHostGroupsString("NAME") . ")");
 }
 
 $orderby = "name ASC";
-if (isset($preferences['order_by']) && $preferences['order_by'] != "") {
+if (isset($preferences['order_by']) && trim($preferences['order_by']) != "") {
     $orderby = $preferences['order_by'];
 }
 
 $query .= "ORDER BY $orderby";
-$query .= " LIMIT ".($page * $preferences['entries']).",".$preferences['entries'];
+$query .= " LIMIT " . ($page * $preferences['entries']) . "," . $preferences['entries'];
 $res = $dbb->query($query);
-$nbRows = $dbb->numberRows();
+$nbRows = $res->rowCount();
 $data = array();
 $detailMode = false;
 if (isset($preferences['enable_detailed_mode']) && $preferences['enable_detailed_mode']) {
     $detailMode = true;
 }
 while ($row = $res->fetchRow()) {
-    $data[$row['name']] = array('name'          => $row['name'],
-        'hg_id'         => $row['hostgroup_id'],
-        'hgurl'         => "main.php?p=20201&o=svc&hg=" .$row['hostgroup_id'],
-        "hgurlhost"     => "main.php?p=20202&o=h&hostgroups=" . $row['hostgroup_id'],
-        'host_state'    => array(),
-        'service_state' => array());
+    $data[$row['name']] = array(
+        'name' => $row['name'],
+        'hg_id' => $row['hostgroup_id'],
+        'hgurl' => "main.php?p=20201&o=svc&hg=" . $row['hostgroup_id'],
+        "hgurlhost" => "main.php?p=20202&o=h&hostgroups=" . $row['hostgroup_id'],
+        'host_state' => array(),
+        'service_state' => array()
+    );
 }
 $hgMonObj->getHostStates($data, $detailMode, $centreon->user->admin, $aclObj, $preferences);
 $hgMonObj->getServiceStates($data, $detailMode, $centreon->user->admin, $aclObj, $preferences);
