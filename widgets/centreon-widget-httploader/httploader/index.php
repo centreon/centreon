769328751d4446b39401b1fba4bd0b2a635ad789
Matthieu Kermagoret <ganoze@gmail.com>
2018-06-22 11:29:24 +0200

    refactor: use dependency injector

--- widgets/centreon-widget-httploader/httploader/index.php
+++ widgets/centreon-widget-httploader/httploader/index.php
@@ -36,8 +36,9 @@
 require_once "../require.php";
 require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
-require_once $centreon_path . 'www/class/centreonDB.class.php';
 require_once $centreon_path . 'www/class/centreonWidget.class.php';
+require_once $centreon_path . 'bootstrap.php';
+
 
 session_start();
 if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
@@ -47,7 +48,7 @@ $centreon = $_SESSION['centreon'];
 $widgetId = $_REQUEST['widgetId'];
 
 try {
-    $db = new CentreonDB();
+    $db = $dependencyInjector['configuration_db'];
     $widgetObj = new CentreonWidget($centreon, $db);
     $preferences = $widgetObj->getWidgetPreferences($widgetId);
     $autoRefresh = 0;
