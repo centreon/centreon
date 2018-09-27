3f6a77d174081b60167650ea2d65ea1187ee76b7
sc979 <schapron@centreon.com>
2018-09-27 18:02:11 +0200

    fix: scrollHeight

--- widgets/centreon-widget-host-monitoring/host-monitoring/data.js
+++ widgets/centreon-widget-host-monitoring/host-monitoring/data.js
@@ -40,7 +40,7 @@ function loadPage()
     jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
         success: function (htmlData) {
             jQuery("#hostMonitoringTable").empty().append(htmlData).append(function() {
-                var h = document.getElementById("hostMonitoringTable").scrollHeight;
+                var h = jQuery("hostMonitoringTable").prop("scrollHeight");
                 parent.iResize(window.name, h);
             });
             jQuery('.checkall').on('change', function() {
