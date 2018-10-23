333a262e2e6bd75f15af01d74dbfc7e7bd83971d
sc979 <schapron@centreon.com>
2018-10-23 12:04:37 +0200

    fix: remove warning in logs

--- widgets/centreon-widget-host-monitoring/host-monitoring/src/action.php
+++ widgets/centreon-widget-host-monitoring/host-monitoring/src/action.php
@@ -146,10 +146,8 @@ try {
             $template->assign('daysLabel', _("days"));
             $template->assign('hoursLabel', _("hours"));
             $template->assign('minutesLabel', _("minutes"));
-            $template->assign('defaultStart', $dateStart);
             $template->assign('defaultHourStart',$hourStart);
             $template->assign('defaultMinuteStart', $minuteStart);
-            $template->assign('defaultEnd', $dateEnd);
             $template->assign('defaultHourEnd', $hourEnd);
             $template->assign('defaultMinuteEnd', $minuteEnd);
 
