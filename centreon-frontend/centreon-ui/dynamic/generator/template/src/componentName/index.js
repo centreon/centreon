7bff7dba36a19c98abcc948f5a5c80666c223d97
Danijel Chalupka <danijel@simple-task.com>
2019-01-24 12:35:28 +0100

    global components generator

---
+++ centreon-frontend/centreon-ui/dynamic/generator/template/src/componentName/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class COMPONENT_FUNCTIONAL_NAME extends Component{
+
+    render(){
+        return <h1>COMPONENT_CAPITALIZED_NAME</h1>
+    }
+
+}
+
+export default COMPONENT_FUNCTIONAL_NAME;
