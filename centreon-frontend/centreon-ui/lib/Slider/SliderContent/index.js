bfc004b4d78bd2782fb751451e13ef63525fcbcd
Nikola Lovic <nikola.lovic@simple-task.com>
2019-02-11 10:18:03 +0100

    fix(extensions): Insert default image for slider.

--- centreon-frontend/centreon-ui/lib/Slider/SliderContent/index.js
+++ centreon-frontend/centreon-ui/lib/Slider/SliderContent/index.js
@@ -123,7 +123,7 @@ var SliderContent = function (_Component) {
     };
 
     _this.state = {
-      images: ["https://static.centreon.com/wp-content/uploads/2018/09/plugin-banner-it-operatio" + "ns-management.png", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg"],
+      images: ["https://res.cloudinary.com/dezez0fsn/image/upload/v1549874437/slider-default-image.png", "https://static.centreon.com/wp-content/uploads/2018/09/plugin-banner-it-operatio" + "ns-management.png", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg"],
       currentIndex: 0,
       translateValue: 0
     };
--- centreon-frontend/centreon-ui/src/Slider/SliderContent/index.js
+++ centreon-frontend/centreon-ui/src/Slider/SliderContent/index.js
@@ -12,6 +12,7 @@ class SliderContent extends Component {
 
     this.state = {
       images: [
+        "https://res.cloudinary.com/dezez0fsn/image/upload/v1549874437/slider-default-image.png",
         "https://static.centreon.com/wp-content/uploads/2018/09/plugin-banner-it-operatio" +
           "ns-management.png",
         "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg",
