890f1031bf4b0a37b1ba8d493ccce24ad0ed69b5
Nikola Lovic <nikola.lovic@simple-task.com>
2019-02-12 11:44:18 +0100

    fix(submenu): Submenu align fix.

--- centreon-frontend/centreon-ui/lib/Title/index.js
+++ centreon-frontend/centreon-ui/lib/Title/index.js
@@ -15,10 +15,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var Title = function Title(_ref) {
   var icon = _ref.icon,
       label = _ref.label,
-      titleColor = _ref.titleColor;
+      titleColor = _ref.titleColor,
+      onClick = _ref.onClick;
   return _react2.default.createElement(
     "h2",
-    { className: "custom-title" },
+    { className: "custom-title",
+      onClick: onClick
+    },
     icon ? _react2.default.createElement("span", { className: "custom-title-icon custom-title-icon-" + icon }) : null,
     _react2.default.createElement(
       "span",
