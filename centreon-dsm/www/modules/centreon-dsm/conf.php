e8515e6e479e450587ecaac8fe3ea9687347e322
Laurent Pinsivy <lpinsivy@centreon.com>
2019-04-04 12:50:26 +0100

    enh(install): Improve description of the module

--- centreon-dsm/www/modules/centreon-dsm/conf.php
+++ centreon-dsm/www/modules/centreon-dsm/conf.php
@@ -3,10 +3,21 @@
 $module_conf['centreon-dsm']["name"] = "centreon-dsm";
 $module_conf['centreon-dsm']["rname"] = "Dynamic Services Management";
 $module_conf['centreon-dsm']["mod_release"] = "19.04.0";
-$module_conf['centreon-dsm']["infos"] = "Dynamic system management for passif return without definition";
+$module_conf['centreon-dsm']["infos"] = "Centreon Dynamic Service Management (Centreon-DSM) is a module to manage alarms with an event logs system. With DSM, Centreon can receive events such as SNMP traps resulting from the detection of a problem and assign events dynamically to a slot defined in Centreon, like a tray events.
+
+A resource has a set number of “slots” (containers) on which alerts will be assigned (stored). While this event has not been taken into account by a human action, it will remain visible in the interface Centreon. When event is acknowledged manually or by a recovery event, the slot becomes available for new events.
+
+The goal of this module is to overhead the basic trap management system of Centreon. The basic function run with a single service and alarm crashed by successive alarms.
+";
 $module_conf['centreon-dsm']["is_removeable"] = "1";
 $module_conf['centreon-dsm']["author"] = "Centreon";
 $module_conf['centreon-dsm']["lang_files"] = "0";
 $module_conf['centreon-dsm']["sql_files"] = "1";
 $module_conf['centreon-dsm']["php_files"] = "0";
+$module_conf['centreon-dsm']["stability"] = "stable";
+$module_conf['centreon-dsm']["last_update"] = "2019-04-04";
+$module_conf['centreon-dsm']["release_note"] = "https://documentation.centreon.com/docs/centreon-dsm/en/latest/";
+$module_conf['centreon-dsm']["images"] = [
+    'images/dsm_snmp_events_tray.png'
+];
 ?>
