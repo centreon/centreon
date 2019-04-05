3fc61c008f6c30400861da0cd1cc587f2e0d98cd
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-04-05 14:54:24 +0200

    refactor(upgrade): refactor upgrade scripts to match new Centreon Web update system.
    There is no more /source/ and /destination/ releases in upgrade scripts. The
    sorting is made by Centreon Web update system.
    
    Refs centreon/centreon#7321.

--- centreon-awie/www/modules/centreon-awie/upgrade/19.04.0/conf.php
+++ centreon-awie/www/modules/centreon-awie/upgrade/19.04.0/conf.php
@@ -12,8 +12,7 @@
  */
 $upgrade_conf['centreon-awie']["rname"] = "Centreon Api Web Import Export";
 $upgrade_conf['centreon-awie']["name"] = "centreon-awie";
-$upgrade_conf['centreon-awie']["release_from"] = "18.10.0";
-$upgrade_conf['centreon-awie']["release_to"] = "19.04.0";
+$upgrade_conf['centreon-awie']["mod_release"] = "19.04.0";
 $upgrade_conf['centreon-awie']["is_removeable"] = "1";
 $upgrade_conf['centreon-awie']["author"] = "Centreon";
 $upgrade_conf['centreon-awie']["infos"] = "Centreon Api Web Import Export";
