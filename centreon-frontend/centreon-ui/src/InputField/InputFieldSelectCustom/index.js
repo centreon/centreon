bb53e6fd63265f1bce3d0747d752b97ee905a0da
Danijel Chalupka <danijel@simple-task.com>
2019-04-10 14:55:22 +0200

    icons in select

--- centreon-frontend/centreon-ui/src/InputField/InputFieldSelectCustom/index.js
+++ centreon-frontend/centreon-ui/src/InputField/InputFieldSelectCustom/index.js
@@ -1,83 +1,109 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import styles from './input-field.scss';
-import ScrollBar from '../../ScrollBar';
-import CustomIconWithText from '../../Custom/CustomIconWithText';
-import IconToggleSubmenu from '../../Icon/IconToggleSubmenu';
+import React, { Component } from "react";
+import classnames from "classnames";
+import styles from "./input-field.scss";
+import ScrollBar from "../../ScrollBar";
+import CustomIconWithText from "../../Custom/CustomIconWithText";
+import IconToggleSubmenu from "../../Icon/IconToggleSubmenu";
 
 class InputFieldSelectCustom extends Component {
-
   state = {
     active: false,
     selected: {}
-  }
+  };
 
   toggleSelect = () => {
     const { active } = this.state;
     this.setState({
       active: !active
-    })
-  }
+    });
+  };
 
   componentWillMount = () => {
-    const {value, options} = this.props;
-    for(let i = 0; i < options.length;i++){
-      if(options[i].id == value) {
+    const { value, options } = this.props;
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].id == value) {
         this.setState({
-          selected:options[i]
-        })
+          selected: options[i]
+        });
       }
     }
-  }
+  };
 
-  componentWillReceiveProps = (nextProps) => {
-    const {value} = nextProps;
-    const {options} = this.props;
-    for(let i = 0; i < options.length;i++){
-      if(options[i].id == value) {
+  componentWillReceiveProps = nextProps => {
+    const { value } = nextProps;
+    const { options } = this.props;
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].id == value) {
         this.setState({
-          selected:options[i]
-        })
+          selected: options[i]
+        });
       }
     }
-  }
+  };
 
-  optionChecked = (option) => {
-    const {onChange} = this.props;
-    this.setState({
-      selected: option,
-      active:false
-    },()=> {
-      if(onChange){
-        onChange(option.id)
+  optionChecked = option => {
+    const { onChange } = this.props;
+    this.setState(
+      {
+        selected: option,
+        active: false
+      },
+      () => {
+        if (onChange) {
+          onChange(option.id);
+        }
       }
-    })
-  }
+    );
+  };
 
   render() {
     const { active, selected } = this.state;
-    const { size, label, error, options } = this.props;
+    const { size, label, error, options, icons, domainPath } = this.props;
     return (
-      <div className={classnames(styles["input-select"], styles[size ? size : ''], styles[active ? "active" : ''], error ? styles['has-danger'] : '')}>
+      <div
+        className={classnames(
+          styles["input-select"],
+          styles[size ? size : ""],
+          styles[active ? "active" : ""],
+          error ? styles["has-danger"] : ""
+        )}
+      >
         <div className={classnames(styles["input-select-wrap"])}>
-          <span className={classnames(styles["input-select-field"])}>{selected.name}</span>
-          <IconToggleSubmenu iconPosition="icons-toggle-position-multiselect" iconType="arrow" onClick={this.toggleSelect.bind(this)} />
+          <span className={classnames(styles["input-select-field"])}>
+            {selected.name}
+          </span>
+          <IconToggleSubmenu
+            iconPosition="icons-toggle-position-multiselect"
+            iconType="arrow"
+            onClick={this.toggleSelect.bind(this)}
+          />
         </div>
-        {
-          active ?
-            <div className={classnames(styles["input-select-dropdown"])}>
-              {
-                options ? options.map((option) => (
-                  <span onClick={this.optionChecked.bind(this, option)} className={classnames(styles["input-select-label"])}>{option.name}</span>
-                )) : null
-              }
-            </div>
-            : null
-        }
-        {error ? (
-          <div className={classnames(styles["form-error"])}>
-            {error}
+        {active ? (
+          <div className={classnames(styles["input-select-dropdown"])}>
+            {options
+              ? options.map(option => (
+                  <React.Fragment>
+                    {icons ? (
+                      <CustomIconWithText
+                        label={option.name}
+                        onClick={this.optionChecked.bind(this, option)}
+                        image={`${domainPath}/${option.preview}`}
+                      />
+                    ) : (
+                      <span
+                        onClick={this.optionChecked.bind(this, option)}
+                        className={classnames(styles["input-select-label"])}
+                      >
+                        {option.name}
+                      </span>
+                    )}
+                  </React.Fragment>
+                ))
+              : null}
           </div>
+        ) : null}
+        {error ? (
+          <div className={classnames(styles["form-error"])}>{error}</div>
         ) : null}
       </div>
     );
