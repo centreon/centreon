4e3808c8a4d4c5d97625608be113a46822063db1
Kevin Duret <duret.kevin@gmail.com>
2019-04-16 14:09:53 +0200

    fix(front/extensions): fix extensions screenshots position (#43)

--- centreon-frontend/centreon-ui/src/Slider/SliderContent/ContentSliderItem.js
+++ centreon-frontend/centreon-ui/src/Slider/SliderContent/ContentSliderItem.js
@@ -2,21 +2,14 @@ import React from "react";
 import classnames from 'classnames';
 import styles2 from './content-slider.scss';
 
-const ContentSliderItem = ({ image, isActive }) => {
-  const styles = {
-    backgroundImage: `url(${image})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "50% 60%"
-  };
-
-  return (
-    <div
-      alt="Slider image"
-      className={classnames(styles2["content-slider-item"], styles2[isActive ? "active-slide" : ""])}
-      style={styles}
-    />
-  );
-};
+const ContentSliderItem = ({ image, isActive }) => (
+  <div
+    alt="Slider image"
+    className={classnames(styles2["content-slider-item"], styles2[isActive ? "active-slide" : ""])}
+    style={{
+      backgroundImage: `url(${image})`
+    }}
+  />
+);
 
 export default ContentSliderItem;
