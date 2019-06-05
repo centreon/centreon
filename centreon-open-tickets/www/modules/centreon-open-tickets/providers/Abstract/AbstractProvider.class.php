6ee828fc61f173d7691024cc4cd65df07b37e2ff
tanguyvda <tcharles@centreon.com>
2019-06-05 11:16:33 +0200

    fix(code) initialize $placeholder var

--- centreon-open-tickets/www/modules/centreon-open-tickets/providers/Abstract/AbstractProvider.class.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/providers/Abstract/AbstractProvider.class.php
@@ -891,6 +891,7 @@ Output: {$service.output|substr:0:1024}
 
                 $id = '-1';
                 $value = '';
+                $placeholder = '';
                 $matches = array();
                 if (preg_match('/^(.*?)___(.*?)___(.*)$/', $this->_submitted_config['select_' . $values['Id']], $matches)) {
                     $id = $matches[1];
@@ -900,7 +901,7 @@ Output: {$service.output|substr:0:1024}
                     $id = $matches[1];
                     $value = $matches[2];
                 }
-                if (!empty($placeholder) && isset($placeholder)) {
+                if (!empty($placeholder)) {
                     $select_lists[$values['Id']] = array('label' => _($values['Label']), 'id' => $id, 'value' => $value, 'placeholder' => $placeholder);
                 } else {
                     $select_lists[$values['Id']] = array('label' => _($values['Label']), 'id' => $id, 'value' => $value);
