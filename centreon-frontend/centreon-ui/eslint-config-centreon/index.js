b449b0b55cbab8d4036a552220c22192e880c2f1
Bruno d'Auria <bdauria@centreon.com>
2019-06-11 15:09:18 +0200

    Add eslint-config-centreon.

---
+++ centreon-frontend/centreon-ui/eslint-config-centreon/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-parens": ["error", "always"],
+    quotes: ["error", "single"]
+  }
+};
