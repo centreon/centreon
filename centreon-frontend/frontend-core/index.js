eae40975575497b394a072dd8de908dcbb72f906
Bruno d'Auria <bdauria@centreon.com>
2019-07-11 09:04:35 +0200

    enh(core): Add eslint-plugin-jest. (#2)

--- centreon-frontend/frontend-core/index.js
+++ centreon-frontend/frontend-core/index.js
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'jest'],
   rules: {
     'prettier/prettier': [
       'error',
