0d57607b699a5ac9706e5886c4e6f58a17078cbe
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-10-04 15:34:40 +0200

    chore: remove all conf.php files from upgrade directories.

--- centreon-dsm/www/modules/centreon-dsm/upgrade/1.2/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-$upgrade_conf['centreon-dsm']["rname"] = "centreon-dsm";
-$upgrade_conf['centreon-dsm']["mod_release"] = "1.2";
-$upgrade_conf['centreon-dsm']["is_removeable"] = "1";
-$upgrade_conf['centreon-dsm']["author"] = "Merethis";
-$upgrade_conf['centreon-dsm']["lang_files"] = "0";
-$upgrade_conf['centreon-dsm']["sql_files"] = "1";
-$upgrade_conf['centreon-dsm']["php_files"] = "0";
-?>
--- centreon-dsm/www/modules/centreon-dsm/upgrade/19.04.0/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$upgrade_conf['centreon-dsm']["rname"] = "centreon-dsm";
-$upgrade_conf['centreon-dsm']["mod_release"] = "19.04.0";
-$upgrade_conf['centreon-dsm']["is_removeable"] = "1";
-$upgrade_conf['centreon-dsm']["author"] = "Centreon";
-$upgrade_conf['centreon-dsm']["lang_files"] = "0";
-$upgrade_conf['centreon-dsm']["sql_files"] = "0";
-$upgrade_conf['centreon-dsm']["php_files"] = "0";
