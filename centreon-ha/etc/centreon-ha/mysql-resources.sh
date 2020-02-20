a07e5f92c81a84031b9ee4b2246af05455f7fb0e
omercier <omercier@centreon.com>
2020-02-20 09:38:04 +0100

    clean(script+conf): remove useless var SAVE_DIR from conf and remove failover-files-check.sh

--- centreon-ha/etc/centreon-ha/mysql-resources.sh
+++ centreon-ha/etc/centreon-ha/mysql-resources.sh
@@ -10,7 +10,6 @@ DBREPLUSER="centreon-repl"
 DBREPLPASSWORD="mysqlreplpw"
 DBROOTUSER="centreon"
 DBROOTPASSWORD="mysqlrootpw"
-SAVE_DIR="/tmp"
 CENTREON_DB="centreon"
 CENTREON_STORAGE_DB="centreon_storage"
 
