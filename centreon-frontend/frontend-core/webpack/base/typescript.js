2adbd2f09ad3e15af658ff2fdeb7ab6cb8c0a65b
Kevin Duret <kduret@centreon.com>
2020-03-16 15:15:29 +0100

    enh(typescript): replace awesome-typescript-loader by ts-loader (#25)

--- centreon-frontend/frontend-core/webpack/base/typescript.js
+++ centreon-frontend/frontend-core/webpack/base/typescript.js
@@ -1,4 +1,5 @@
 const merge = require('webpack-merge');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const webpackConfig = require('.');
 
@@ -10,9 +11,25 @@ module.exports = merge(webpackConfig, {
     rules: [
       {
         test: /\.tsx?$/,
-        exclude: /node_modules(?!(\\|\/)@centreon(\\|\/)ui)/,
-        use: ['babel-loader', 'awesome-typescript-loader'],
+        exclude: /node_modules(\\|\/)(?!(@centreon(\\|\/)ui))/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              allowTsInNodeModules: true,
+              transpileOnly: true,
+              experimentalWatchApi: true,
+            }
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      useTypescriptIncrementalApi: true,
+      checkSyntacticErrors: true,
+    }),
+  ],
 });
