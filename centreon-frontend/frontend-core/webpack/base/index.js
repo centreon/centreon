914f02584bf3c54315b13f0d35d8395712729a33
Bruno d'Auria <bdauria@centreon.com>
2020-03-20 14:05:12 +0100

    enh(webpack): enhance and simplify webpack configuration (#27)

--- centreon-frontend/frontend-core/webpack/base/index.js
+++ centreon-frontend/frontend-core/webpack/base/index.js
@@ -1,8 +1,9 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   output: {
     libraryTarget: 'umd',
@@ -17,14 +18,36 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      useTypescriptIncrementalApi: true,
+      checkSyntacticErrors: true,
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules(?!(\\|\/)@centreon(\\|\/)ui)/,
-        use: ['babel-loader'],
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules(\\|\/)(?!(@centreon(\\|\/)ui))/,
+        use: [
+          { loader: 'cache-loader' },
+          {
+              loader: 'thread-loader',
+              options: {
+                  workers: require('os').cpus().length - 1,
+              },
+          },
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              allowTsInNodeModules: true,
+              transpileOnly: true,
+              experimentalWatchApi: true,
+              happyPackMode: true,
+            }
+          },
+        ],
       },
     ],
-  }
+  },
 };
