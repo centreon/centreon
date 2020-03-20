b7c388a0cd4ab336f096ed95142765c382520ca5
Tom Darneix <tomdar87@outlook.com>
2020-03-20 14:03:49 +0100

    Use theme background color as title background (#198)

--- centreon-frontend/centreon-ui/src/HorizontalLines/HorizontalLineContent/index.js
+++ centreon-frontend/centreon-ui/src/HorizontalLines/HorizontalLineContent/index.js
@@ -3,22 +3,34 @@
 
 import React from 'react';
 import clsx from 'clsx';
+
+import { makeStyles } from '@material-ui/core';
+
 import styles from './content-horizontal-line.scss';
 
-const HorizontalLineContent = ({ hrTitle, hrColor, hrTitleColor }) => (
-  <div
-    className={clsx(styles['content-hr'], {
-      [styles[`content-hr-${hrColor}`]]: hrColor,
-    })}
-  >
-    <span
-      className={clsx(styles['content-hr-title'], {
-        [styles[`content-hr-title-${hrTitleColor}`]]: hrTitleColor,
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundColor: theme.palette.background.default,
+  },
+}));
+
+const HorizontalLineContent = ({ hrTitle, hrColor, hrTitleColor }) => {
+  const classes = useStyles();
+  return (
+    <div
+      className={clsx(styles['content-hr'], {
+        [styles[`content-hr-${hrColor}`]]: hrColor,
       })}
     >
-      {hrTitle}
-    </span>
-  </div>
-);
+      <span
+        className={clsx(styles['content-hr-title'], classes.background, {
+          [styles[`content-hr-title-${hrTitleColor}`]]: hrTitleColor,
+        })}
+      >
+        {hrTitle}
+      </span>
+    </div>
+  );
+};
 
 export default HorizontalLineContent;
