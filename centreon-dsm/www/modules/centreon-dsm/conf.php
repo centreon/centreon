7f69e4d0176c9ad962f8ca2acc0280340f12229b
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-10-14 13:59:03 +0200

    chore(install): set version to 20.10.0-beta.2.

--- centreon-dsm/www/modules/centreon-dsm/conf.php
+++ centreon-dsm/www/modules/centreon-dsm/conf.php
@@ -2,7 +2,7 @@
 // Be Carefull with internal_name, it's case sensitive (with directory module name)
 $module_conf['centreon-dsm']["name"] = "centreon-dsm";
 $module_conf['centreon-dsm']["rname"] = "Dynamic Services Management";
-$module_conf['centreon-dsm']["mod_release"] = "20.10.0-beta.1";
+$module_conf['centreon-dsm']["mod_release"] = "20.10.0-beta.2";
 $module_conf['centreon-dsm']["infos"] = "Centreon Dynamic Service Management (Centreon-DSM) is a module to manage " .
     "alarms with an event logs system. With DSM, Centreon can receive events such as SNMP traps resulting from the " .
     "detection of a problem and assign events dynamically to a slot defined in Centreon, like a tray events.
@@ -17,7 +17,7 @@ The goal of this module is to overhead the basic trap management system of Centr
 $module_conf['centreon-dsm']["is_removeable"] = "1";
 $module_conf['centreon-dsm']["author"] = "Centreon";
 $module_conf['centreon-dsm']["stability"] = "stable";
-$module_conf['centreon-dsm']["last_update"] = "2020-06-04";
+$module_conf['centreon-dsm']["last_update"] = "2020-10-14";
 $module_conf['centreon-dsm']["release_note"] = "https://documentation.centreon.com/20.10/en/releases/centreon-os-extensions.html";
 $module_conf['centreon-dsm']["images"] = [
     'images/dsm_snmp_events_tray.png'
