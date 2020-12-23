135c57a3f322545a62482c8cd9a3a739df25af02
sc979 <34628915+sc979@users.noreply.github.com>
2020-12-23 15:40:33 +0100

    enh(chore): add CI requirements for github violation (#18)

--- widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/index.php
+++ widgets/centreon-widget-live-top10-cpu-usage/live-top10-cpu-usage/index.php
@@ -1,6 +1,7 @@
 <?php
-/**
- * Copyright 2005-2019 Centreon
+
+/*
+ * Copyright 2005-2020 Centreon
  * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
@@ -19,11 +20,11 @@
  * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
  *
- * As a special exception, the copyright holders of this program give MERETHIS
+ * As a special exception, the copyright holders of this program give Centreon
  * permission to link this program with independent modules to produce an executable,
  * regardless of the license terms of these independent modules, and to copy and
- * distribute the resulting executable under terms of MERETHIS choice, provided that
- * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * distribute the resulting executable under terms of Centreon choice, provided that
+ * Centreon also meet, for each linked independent module, the terms  and conditions
  * of the license of that module. An independent module is a module which is not
  * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
@@ -100,9 +101,9 @@ $query = "SELECT
     FROM
         metrics m,
         hosts h "
-        . ($preferences['host_group'] ? ", hosts_hostgroups hg " : "")
-        . ($centreon->user->admin == 0 ? ", centreon_acl acl " : "")
-        . " , index_data i
+    . ($preferences['host_group'] ? ", hosts_hostgroups hg " : "")
+    . ($centreon->user->admin == 0 ? ", centreon_acl acl " : "")
+    . " , index_data i
     LEFT JOIN services s ON s.service_id  = i.service_id AND s.enabled = 1
     WHERE i.service_description LIKE '%" . $preferences['service_description'] . "%'
     AND i.id = m.index_id
