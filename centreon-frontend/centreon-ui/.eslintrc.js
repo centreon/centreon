b118d8d365c867ae0a4d257895e6118aaa283898
Squalli Karim <karim.squalli@outlook.fr>
2021-02-09 09:29:17 +0100

    enh(chore): Update eslintrc config to use new config from frontend-core (#317)

--- centreon-frontend/centreon-ui/.eslintrc.js
+++ centreon-frontend/centreon-ui/.eslintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: './node_modules/@centreon/frontend-core/eslint/typescript',
+  extends:
+    './node_modules/@centreon/frontend-core/eslint/react/typescript.eslintrc.js',
 };
