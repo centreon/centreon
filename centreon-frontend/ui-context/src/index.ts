f8b22f6f258159733111a44aa9195472e63d686f
karimsx <karim.squalli@outlook.fr>
2021-03-24 11:42:31 +0100

    Revert "fix type export"
    This reverts commit 258ea2cbdf82399eea55c3275866bfd1b69333c3.

--- centreon-frontend/ui-context/src/index.ts
+++ centreon-frontend/ui-context/src/index.ts
@@ -4,4 +4,4 @@ export { default as useAcl } from './useAcl';
 export { default as useDowntime } from './useDowntime';
 export { default as useRefreshInterval } from './useRefreshInterval';
 
-export type { User, UserContext, ActionAcl, Actions, Downtime } from './types';
+export { User, UserContext, ActionAcl, Actions, Downtime } from './types';
