cf8d0b5e49c1ed14c82d4d2e529f1e70bb578322
Kevin Duret <kduret@centreon.com>
2021-04-20 10:35:59 +0200

    chore(version): upgrade to 21.04.0 (#147)

--- centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
@@ -22,7 +22,7 @@
 
 $module_conf['centreon-open-tickets']["rname"] = "Centreon Open Tickets";
 $module_conf['centreon-open-tickets']["name"] = "centreon-open-tickets";
-$module_conf['centreon-open-tickets']["mod_release"] = "21.04.0-beta.2";
+$module_conf['centreon-open-tickets']["mod_release"] = "21.04.0";
 $module_conf['centreon-open-tickets']["infos"] = "Centreon Open Tickets is a community module developed to " .
     "create tickets to your favorite ITSM tools using API.
 
@@ -37,7 +37,7 @@ Regarding the widget configuration, it is possible to see the created tickets by
 $module_conf['centreon-open-tickets']["is_removeable"] = "1";
 $module_conf['centreon-open-tickets']["author"] = "Centreon";
 $module_conf['centreon-open-tickets']["stability"] = "stable";
-$module_conf['centreon-open-tickets']["last_update"] = "2021-04-13";
+$module_conf['centreon-open-tickets']["last_update"] = "2021-04-20";
 $module_conf['centreon-open-tickets']["release_note"] =
     "https://docs.centreon.com/21.04/en/releases/centreon-os-extensions.html";
 $module_conf['centreon-open-tickets']["images"] = [
