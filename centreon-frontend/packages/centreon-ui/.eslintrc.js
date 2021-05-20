41a8f323a6d854ad32696c57ec6a4edb233720da
Tom Darneix <tomdar87@outlook.com>
2021-05-20 22:24:33 +0200

    Fix eslint

--- centreon-frontend/packages/centreon-ui/.eslintrc.js
+++ centreon-frontend/packages/centreon-ui/.eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   extends:
     './node_modules/@centreon/frontend-config/eslint/react/typescript.eslintrc.js',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      settings: {
+        'import/resolver': {
+          alias: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            map: [
+              ['@centreon/ui-context', './node_modules/@centreon/ui-context'],
+            ],
+          },
+        },
+      },
+    },
+  ],
 };
