6256e7674c78ad838496e0c5fc49aabbb5bab73d
Tom Darneix <tomdar87@outlook.com>
2021-05-21 10:34:11 +0200

    Add module resolution for jest

--- centreon-frontend/packages/frontend-config/jest/index.js
+++ centreon-frontend/packages/frontend-config/jest/index.js
@@ -6,6 +6,8 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!@centreon/ui).+\\.jsx?$'],
   moduleNameMapper: {
     '\\.(s?css|png|svg)$': 'identity-obj-proxy',
+    '@centreon/ui': '<rootDir>/node_modules/@centreon/centreon-frontend/packages/centreon-ui',
+    '@centreon/ui-context': '<rootDir>/node_modules/@centreon/centreon-frontend/packages/ui-context'
   },
   testPathIgnorePatterns: ['/node_modules/'],
 };
