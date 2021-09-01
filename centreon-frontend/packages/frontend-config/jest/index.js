da931792c57f1fd46e88a9142af8e38d30b16f76
Tom Darneix <tomdar87@outlook.com>
2021-09-01 09:43:07 +0200

    chore(Jest): Provide testEnvironment #41

--- centreon-frontend/packages/frontend-config/jest/index.js
+++ centreon-frontend/packages/frontend-config/jest/index.js
@@ -1,13 +1,18 @@
 module.exports = {
+  moduleNameMapper: {
+    '@centreon/ui':
+      '<rootDir>/node_modules/@centreon/centreon-frontend/packages/centreon-ui',
+    '@centreon/ui-context':
+      '<rootDir>/node_modules/@centreon/centreon-frontend/packages/ui-context',
+    '\\.(s?css|png|svg)$': 'identity-obj-proxy',
+  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
-  transformIgnorePatterns: ['/node_modules/(?!@centreon/centreon-frontend/packages/(centreon-ui|ui-context)).+\\.jsx?$'],
-  moduleNameMapper: {
-    '\\.(s?css|png|svg)$': 'identity-obj-proxy',
-    '@centreon/ui': '<rootDir>/node_modules/@centreon/centreon-frontend/packages/centreon-ui',
-    '@centreon/ui-context': '<rootDir>/node_modules/@centreon/centreon-frontend/packages/ui-context'
-  },
-  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!@centreon/centreon-frontend/packages/(centreon-ui|ui-context)).+\\.jsx?$',
+  ],
 };
