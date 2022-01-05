e04390ba3012a5a8ccba52dc4189058e7aa78fe8
Tom Darneix <tomdar87@outlook.com>
2022-01-05 12:55:02 +0100

    Clean up webpack Module patch config (#98)

--- centreon-frontend/packages/frontend-config/webpack/patch/module.js
+++ centreon-frontend/packages/frontend-config/webpack/patch/module.js
@@ -7,39 +7,7 @@ module.exports = ({ assetPublicPath, outputPath }) => ({
     react: 'React',
     'react-dom': 'ReactDOM',
     'react-i18next': 'ReactI18Next',
-    'react-redux': 'ReactRedux',
-    'react-redux-i18n': 'ReactReduxI18n',
     'react-router-dom': 'ReactRouterDom',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(c|sa|sc)ss$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { modules: true } },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                modules: true,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif|off|woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: assetPublicPath,
-            },
-          },
-        ],
-      },
-    ],
   },
   output: {
     library: '[chunkhash:8]',
@@ -48,10 +16,7 @@ module.exports = ({ assetPublicPath, outputPath }) => ({
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        `${outputPath}/**/*.js`,
-        `${outputPath}/**/*.css`,
-      ],
+      cleanOnceBeforeBuildPatterns: [`${outputPath}/**/*.js`],
       dangerouslyAllowCleanPatternsOutsideProject: true,
       dry: false,
     }),
