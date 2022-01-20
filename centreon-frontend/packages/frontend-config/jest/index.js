7b0e70de07879c0ca238d445e28a3644a87388b9
jcaro <jcaro@centreon.com>
2022-01-20 14:50:32 +0100

    fix(UI): Fix alias for jest into centreon-frontend (#104)
    * Fix alias for jest into centreon-frontend
    
    * Fix version of eslint-config-airbnb

--- centreon-frontend/packages/frontend-config/jest/index.js
+++ centreon-frontend/packages/frontend-config/jest/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   moduleNameMapper: {
     '@centreon/ui':
-      '<rootDir>/node_modules/@centreon/centreon-frontend/packages/centreon-ui',
+      '<rootDir>/node_modules/centreon-frontend/packages/centreon-ui',
     '@centreon/ui-context':
-      '<rootDir>/node_modules/@centreon/centreon-frontend/packages/ui-context',
+      '<rootDir>/node_modules/centreon-frontend/packages/ui-context',
     '\\.(s?css|png|svg)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
@@ -13,6 +13,6 @@ module.exports = {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!@centreon/centreon-frontend/packages/(centreon-ui|ui-context)).+\\.jsx?$',
+    '/node_modules/(?!centreon-frontend/packages/(centreon-ui|ui-context)).+\\.jsx?$',
   ],
 };
