10d76f74615a7ffb9b5eaeba2e5b010c44250d45
Bruno d'Auria <bdauria@centreon.com>
2022-02-07 10:11:06 +0100

    Fix Jotai provider order

--- centreon-frontend/packages/centreon-ui/src/testRenderer.tsx
+++ centreon-frontend/packages/centreon-ui/src/testRenderer.tsx
@@ -1,19 +1,26 @@
 import React, { ReactElement } from 'react';
 
+import { Provider as JotaiProvider } from 'jotai';
 import {
   render as rtlRender,
   RenderOptions,
   RenderResult,
 } from '@testing-library/react';
 
-import ThemeProvider from './ThemeProvider';
+import { ThemeMode } from '@centreon/ui-context';
+
+import ThemeProvider from './StoryBookThemeProvider';
 
 interface Props {
   children: React.ReactChild;
 }
 
 const ThemeProviderWrapper = ({ children }: Props): JSX.Element => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <JotaiProvider scope="ui-context">
+      <ThemeProvider themeMode={ThemeMode.light}>{children}</ThemeProvider>
+    </JotaiProvider>
+  );
 };
 
 const render = (
