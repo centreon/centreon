b3c177a74ac70a1cf58ddcc3a45e2ed1487ceb5e
Tom Darneix <tomdar87@outlook.com>
2022-05-16 09:58:08 +0200

    enh(Listing Page): Add automatic scroll on listing page (#163)
    * Add automatic page scroll on listing page
    
    * Add snpashots

--- centreon-frontend/packages/centreon-ui/src/ListingPage/index.tsx
+++ centreon-frontend/packages/centreon-ui/src/ListingPage/index.tsx
@@ -1,8 +1,8 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 
 import clsx from 'clsx';
 
-import { Theme } from '@mui/material';
+import { Box, Theme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 
 import WithPanel from '../Panel/WithPanel';
@@ -16,7 +16,6 @@ const useStyles = makeStyles<Theme>((theme) => {
       zIndex: 4,
     },
     listing: {
-      height: '100%',
       marginLeft: theme.spacing(2),
       marginRight: theme.spacing(2),
     },
@@ -33,6 +32,7 @@ const useStyles = makeStyles<Theme>((theme) => {
 interface Props {
   filter: JSX.Element;
   listing: JSX.Element;
+  listingScrollOffset?: number;
   pageClassName?: string;
   panel?: JSX.Element;
   panelFixed?: boolean;
@@ -46,19 +46,50 @@ const ListingPage = ({
   panelOpen = false,
   panelFixed = false,
   pageClassName,
+  listingScrollOffset = 16,
 }: Props): JSX.Element => {
   const classes = useStyles();
+  const [listingHeight, setListingHeight] = useState(0);
+  const listingRef = useRef<HTMLDivElement | null>(null);
+  const filtersRef = useRef<HTMLDivElement | null>(null);
+
+  const resize = (): void => {
+    setListingHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resize);
+
+    setListingHeight(window.innerHeight);
+
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+
+  const listingContainerHeight =
+    listingHeight -
+    (filtersRef.current?.getBoundingClientRect().height || 0) -
+    (filtersRef.current?.getBoundingClientRect().top || 0) -
+    listingScrollOffset;
 
   return (
     <div className={clsx(classes.page, pageClassName)}>
-      <div className={classes.filters}>
+      <div className={classes.filters} ref={filtersRef}>
         <Suspense fallback={<FilterSkeleton />}>{filter}</Suspense>
       </div>
 
       <WithPanel fixed={panelFixed} open={panelOpen} panel={panel}>
-        <div className={classes.listing}>
+        <Box
+          className={classes.listing}
+          ref={listingRef}
+          sx={{
+            maxHeight: listingContainerHeight,
+            overflowY: 'auto',
+          }}
+        >
           <Suspense fallback={<ListingSkeleton />}>{listing}</Suspense>
-        </div>
+        </Box>
       </WithPanel>
     </div>
   );
