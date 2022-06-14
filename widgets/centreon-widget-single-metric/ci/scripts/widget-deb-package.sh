09a7a66869be27d56e56a64be845c38dd5ec8b14
Luiz Costa <luizgustavo@luizgustavo.pro.br>
2022-06-14 08:56:23 +0100

    update changes for make debian pkg (#17)

--- widgets/centreon-widget-single-metric/ci/scripts/widget-deb-package.sh
+++ widgets/centreon-widget-single-metric/ci/scripts/widget-deb-package.sh
@@ -23,6 +23,7 @@ mkdir -p /build/$PROJECT
 (cd /src && tar czvpf - $PROJECT) | dd of=$PROJECT-$VERSION.tar.gz
 cp -rv /src/$PROJECT /build/
 cp -rv /src/$PROJECT/ci/debian /build/$PROJECT/
+sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" /build/$PROJECT/debian/substvars
 
 ls -lart
 cd /build/$PROJECT
