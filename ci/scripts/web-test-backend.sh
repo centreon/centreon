dd54b322cd92c6a0b6a502104426b15c8b9bae60
Zakaria GUENNOUNE <zguennoune@centreon.com>
2022-07-13 14:45:37 +0200

    fix

--- ci/scripts/web-test-backend.sh
+++ ci/scripts/web-test-backend.sh
@@ -4,6 +4,6 @@ cd centreon
 mkdir build
 composer install --optimize-autoloader
 XDEBUG_MODE=coverage composer run-script test:ci
-pwd
+phpcs --config-set ignore_warnings_on_exit 1
 composer run-script codestyle:ci
 #composer run-script phpstan:ci > build/phpstan.xml
