aab2eb12f346155fbc192f72d2d63fbe246265c7
Zakaria GUENNOUNE <zguennoune@centreon.com>
2022-07-13 10:07:33 +0200

    fix

--- ci/scripts/frontend-test-frontend.sh
+++ ci/scripts/frontend-test-frontend.sh
@@ -5,7 +5,10 @@ npm ci --legacy-peer-deps
 
 cd packages/ui-context
 npm ci --legacy-peer-deps
-
-
-# Run frontend unit tests and code style.
 npm run eslint -- -o checkstyle.xml -f checkstyle
+
+cd ../centreon-ui
+npm ci --legacy-peer-deps
+npm run eslint -- -o checkstyle.xml -f checkstyle
+npm run build:storybook
+npm t -- --ci --reporters=jest-junit
