4dda2a2fc48be15314099dd5b52e82d4cd713e58
Zakaria GUENNOUNE <zguennoune@centreon.com>
2022-07-15 14:28:28 +0200

    fix

--- ci/scripts/web-test-frontend.sh
+++ ci/scripts/web-test-frontend.sh
@@ -1,15 +1,7 @@
 #!/bin/bash 
 set -ex
 
-cd centreon-frontend
-npm ci --legacy-peer-deps
-
-cd packages/ui-context
-npm ci --legacy-peer-deps
-npm run eslint -- -o checkstyle.xml -f checkstyle
-
-cd ../centreon-ui
-npm ci --legacy-peer-deps
-npm run eslint -- -o checkstyle.xml -f checkstyle
-npm run build:storybook
-npm t -- --ci --reporters=jest-junit
+cd centreon
+npm ci
+npm run eslint -- -o checkstyle-fe.xml -f checkstyle
+npm run test:coverage -- --ci --reporters=jest-junit
--- ci/scripts/web-test-web.sh
+++ ci/scripts/web-test-web.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 
-cd centreon
-npm ci
-npm run eslint -- -o checkstyle-fe.xml -f checkstyle
-npm run test:coverage -- --ci --reporters=jest-junit
+set -ex
+
+cd centreon-frontend
+npm ci --legacy-peer-deps
+
+cd packages/ui-context
+npm ci --legacy-peer-deps
+npm run eslint -- -o checkstyle.xml -f checkstyle
+
+cd ../centreon-ui
+npm ci --legacy-peer-deps
+npm run eslint -- -o checkstyle.xml -f checkstyle
+npm t -- --ci --reporters=jest-junit
+npm run build:storybook
