83a0926a9918055ac622d9125854ede5e69d1764
Elmahdi ABBASSI <108519266+emabassi-ext@users.noreply.github.com>
2022-08-22 12:31:59 +0100

    disabling the use of colors imported from database (#148)

--- widgets/centreon-widget-host-monitoring/host-monitoring/src/DB-Func.php
+++ widgets/centreon-widget-host-monitoring/host-monitoring/src/DB-Func.php
@@ -43,19 +43,6 @@ function getColors($db): array
         4 => "#2ad1d4"
     );
 
-    // Get configured colors
-    $res = $db->query("SELECT `key`, `value` FROM `options` WHERE `key` LIKE 'color%'");
-    while ($row = $res->fetchRow()) {
-        if ($row['key'] === "color_up") {
-            $stateColors[0] = $row['value'];
-        } elseif ($row['key'] === "color_down") {
-            $stateColors[1] = $row['value'];
-        } elseif ($row['key'] === "color_unreachable") {
-            $stateColors[2] = $row['value'];
-        } elseif ($row['key'] === "color_pending") {
-            $stateColors[4] = $row['value'];
-        }
-    }
 
     return $stateColors;
 }
