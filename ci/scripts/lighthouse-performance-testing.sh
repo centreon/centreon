12214ede8714a35e28e7b4c7ddc713e79e1e767c
rem31 <73845199+rem31@users.noreply.github.com>
2022-08-31 12:54:00 +0200

    update monorepoc open source

---
+++ ci/scripts/lighthouse-performance-testing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+cd $MODULE_PATH
+rm -rf centreon-injector
+git clone https://github.com/centreon/centreon-injector.git
+docker cp centreon-injector lighthouse-tests-centreon-autodiscovery:/usr/share
+# docker exec lighthouse-tests-centreon-autodiscovery sed -i 's/127.0.0.1/localhost/g' /usr/share/centreon-injector/.env
+docker exec lighthouse-tests-centreon-autodiscovery bash -c "cd /usr/share/centreon-injector && bin/console centreon:inject-data" || true
+docker exec lighthouse-tests-centreon-autodiscovery bash -c "centreon -u admin -p Centreon\!2021 -a APPLYCFG -v 1"
+npm ci --legacy-peer-deps
+
+npm start
