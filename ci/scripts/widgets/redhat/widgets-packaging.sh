d4c9cf9f257e089aef0eae13c481099a96430a55
rem31 <73845199+rem31@users.noreply.github.com>
2022-08-31 15:31:46 +0200

    update monorepoc open source

--- ci/scripts/widgets/redhat/widgets-packaging.sh
+++ ci/scripts/widgets/redhat/widgets-packaging.sh
@@ -23,12 +23,12 @@ ls -1 | sed '/centreon-widget.spectemplate/d' | while read PROJECT; do
     export WIDGET="$(echo $PROJECT | sed 's/centreon-widget-//')"
     export SUMMARY="$(find . -name configs.xml | xargs sed -n 's|\s*<description>\(.*\)</description>|\1|p'
  2>/dev/null)"
-    rpmbuild -ba centreon-widget.spectemplate \
+    rpmbuild -ba ../centreon-widget.spectemplate \
         -D "VERSION $VERSION" \
         -D "RELEASE $RELEASE" \
         -D "SUMMARY $SUMMARY" \
         -D "PROJECT $PROJECT" \
-        -D "WIDGET_SUB_DIR $WIDGET"
+        -D "WIDGET $WIDGET"
     cp -r /root/rpmbuild/RPMS/noarch/*.rpm .
     chmod 777 *.rpm
     cd ..
