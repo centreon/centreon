f9836e4c8b2dfa1540a33242a930e284fdbdea10
Bruno d'Auria <bdauria@centreon.com>
2022-09-11 20:41:27 +0200

    Quiet mode

--- ci/scripts/cypress-e2e-testing.sh
+++ ci/scripts/cypress-e2e-testing.sh
@@ -2,10 +2,11 @@
 set -ex
 
 export CYPRESS_CACHE_FOLDER=$PWD/cypress_cache
+export CYPRESS_VERIFY_TIMEOUT=100000
 export HOME=$PWD/cache
 
 cd $MODULE/tests/e2e
 
 npm ci
 
-$(npm bin)/cypress run --browser chrome
+$(npm bin)/cypress run --browser chrome --quiet
