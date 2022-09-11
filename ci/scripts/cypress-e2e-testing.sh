46c43b57078854d37797614931788741d55137f2
Bruno d'Auria <bdauria@centreon.com>
2022-09-11 22:11:59 +0200

    fix eof

--- ci/scripts/cypress-e2e-testing.sh
+++ ci/scripts/cypress-e2e-testing.sh
@@ -4,9 +4,10 @@ set -ex
 export CYPRESS_CACHE_FOLDER=$PWD/cypress_cache
 export CYPRESS_VERIFY_TIMEOUT=100000
 export HOME=$PWD/cache
+export DEBUG="cypress:server:browsers,cypress:server:video"
 
 cd $MODULE/tests/e2e
 
-npm ci
+# npm ci
 
-$(npm bin)/cypress run --browser chrome --quiet
+$(npm bin)/cypress run --quiet --browser chrome
