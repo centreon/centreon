9eba83d0a0052b39a4109c7cd026eab6b8a43ea7
Bruno d'Auria <bdauria@centreon.com>
2022-09-12 22:13:53 +0200

    Integrate ESLint (#25)

--- centreon/cypress/plugins/index.ts
+++ centreon/cypress/plugins/index.ts
@@ -2,19 +2,20 @@
 /* eslint-disable global-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const {
-  addMatchImageSnapshotPlugin,
-} = require('cypress-image-snapshot/plugin');
-
-module.exports = (on, config) => {
-  if (config.testingType === 'component') {
-    addMatchImageSnapshotPlugin(on, config);
-
-    const { startDevServer } = require('@cypress/webpack-dev-server');
-
-    const webpackConfig = require('../../webpack.config.dev');
-
-    on('dev-server:start', (options) =>
-      startDevServer({ options, webpackConfig }),
-    );
-  }
-};
+    addMatchImageSnapshotPlugin,
+  } = require('cypress-image-snapshot/plugin');
+  
+  module.exports = (on, config) => {
+    if (config.testingType === 'component') {
+      addMatchImageSnapshotPlugin(on, config);
+  
+      const { startDevServer } = require('@cypress/webpack-dev-server');
+  
+      const webpackConfig = require('../../webpack.config.dev');
+  
+      on('dev-server:start', (options) =>
+        startDevServer({ options, webpackConfig }),
+      );
+    }
+  };
+  
