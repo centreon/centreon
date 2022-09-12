a819ee1e79acef9edda200b8693a53ea6476730a
Bruno d'Auria <bdauria@centreon.com>
2022-09-12 09:27:44 +0200

    Add composer install

--- ci/scripts/lighthouse-performance-testing.sh
+++ ci/scripts/lighthouse-performance-testing.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -ex
 cd $MODULE_PATH
+
 rm -rf centreon-injector
 git clone https://github.com/centreon/centreon-injector.git
+composer install
+
 docker cp centreon-injector lighthouse-tests-centreon:/usr/share
 docker exec lighthouse-tests-centreon sed -i 's/127.0.0.1/localhost/g' /usr/share/centreon-injector/.env
 docker exec lighthouse-tests-centreon bash -c "cd /usr/share/centreon-injector && bin/console centreon:inject-data" || true
