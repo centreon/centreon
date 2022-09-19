0c975954521194aef0f0bc48aedda62e227429aa
Bruno d'Auria <bdauria@centreon.com>
2022-09-19 23:32:02 +0200

    Refacto

--- ci/scripts/web-lint-backend-phpstan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-cd centreon
-composer install
-cd ..
-ln -s ./centreon/vendor ./vendor
-./centreon/vendor/bin/phpstan analyse -c ./centreon/phpstan.neon --level 6 --memory-limit=512M --error-format=checkstyle --no-interaction --no-progress $1 $2 > checkstyle-phpstan.xml
