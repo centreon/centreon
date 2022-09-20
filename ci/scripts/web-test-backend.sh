874e48df9efacb200cef84fce6b863d415372234
Bruno d'Auria <bdauria@centreon.com>
2022-09-20 14:17:16 +0200

    Rework centreon-web pipelines and use GH runners when possible (#30)

--- ci/scripts/web-test-backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-cd centreon
-mkdir build
-composer install --optimize-autoloader
-#XDEBUG_MODE=coverage composer run-script test:ci
-#composer run-script codestyle:ci
-#composer run-script phpstan:ci > build/phpstan.xml
