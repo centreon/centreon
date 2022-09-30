ad01d711e8cc71e68dbda73ee19b4cce2621620e
Pavel Napolov <pnapolov-ext@centreon.com>
2022-09-30 14:57:53 +0000

    check docker compose files

--- ci/scripts/behat-acceptance-testing.sh
+++ ci/scripts/behat-acceptance-testing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-WEB_IMAGE="$REGISTRY/$PROJECT-$DISTRIB:$RELEASE"
-WEB_FRESH_IMAGE="$REGISTRY/$PROJECT-fresh-$DISTRIB:$RELEASE"
+WEB_IMAGE="$REGISTRY/$PROJECT-$DISTRIB:$IMAGE_VERSION"
+WEB_FRESH_IMAGE="$REGISTRY/$PROJECT-fresh-$DISTRIB:$IMAGE_VERSION"
 WEB_WIDGETS_IMAGE="$REGISTRY/packaging-widgets-$DISTRIB:22.10"
 WEBDRIVER_IMAGE="$REGISTRY/standalone-chrome:3.141.59-oxygen"
 
@@ -19,7 +19,7 @@ sed 's#@WEB_IMAGE@#'$WEB_FRESH_IMAGE'#g' < `dirname $0`/../docker/compose/docker
 sed 's#@WEB_IMAGE@#'$WEB_WIDGETS_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-web.yml.in > "$COMPOSE_DIR/docker-compose-web-widgets.yml"
 
 sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-squid-simple.yml.in > "$COMPOSE_DIR/docker-compose-web-squid-simple.yml"
-sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/docker-compose-squid-basic-auth.yml.in > "$COMPOSE_DIR/docker-compose-web-squid-basic-auth.yml"
-sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/docker-compose-mediawiki.yml.in > "$COMPOSE_DIR/docker-compose-web-kb.yml"
-sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../../containers/openldap/docker-compose.yml.in > "$COMPOSE_DIR/docker-compose-web-openldap.yml"
-sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../../containers/web/22.10/docker-compose-influxdb.yml.in > "$COMPOSE_DIR/docker-compose-web-influxdb.yml"
+sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-squid-basic-auth.yml.in > "$COMPOSE_DIR/docker-compose-web-squid-basic-auth.yml"
+sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-mediawiki.yml.in > "$COMPOSE_DIR/docker-compose-web-kb.yml"
+sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-openldap.yml.in > "$COMPOSE_DIR/docker-compose-web-openldap.yml"
+sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../docker/compose/docker-compose-influxdb.yml.in > "$COMPOSE_DIR/docker-compose-web-influxdb.yml"
