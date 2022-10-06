89e9675d535054a24e4c370249cd70389114aced
Luiz Costa <me@luizgustavo.pro.br>
2022-10-07 00:05:21 +0100

    fix name

--- ci/scripts/centreon-plugin-packs/redhat/centreon-plugin-packs-packaging.sh
+++ ci/scripts/centreon-plugin-packs/redhat/centreon-plugin-packs-packaging.sh
@@ -37,7 +37,7 @@ BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 BuildArch:      noarch
 
 %prep
-%setup -q -n packs
+%setup -q -n centreon-plugin-packs
 
 %description
 Centreon Pack root package. Not meant to be installed.
