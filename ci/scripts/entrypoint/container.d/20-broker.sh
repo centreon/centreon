d6d88c0fc4ac3bbb37c39da8ce81e364215b4aba
Kevin Duret <kduret@centreon.com>
2022-10-17 23:20:22 +0200

    test

--- ci/scripts/entrypoint/container.d/20-broker.sh
+++ ci/scripts/entrypoint/container.d/20-broker.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# Copy configuration files
-rm -rf /etc/centreon-broker/*
-cp -R /var/cache/centreon/config/broker/1/* /etc/centreon-broker/
-chown -R apache. /etc/centreon-broker/*
-chmod -R 664 /etc/centreon-broker/*
-
 # Start Centreon Broker daemons.
-service cbd start
-service cbd reload
+/etc/init.d/cbd restart
--- ci/scripts/entrypoint/container.d/30-engine.sh
+++ ci/scripts/entrypoint/container.d/30-engine.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# Copy configuration files
-rm -rf /etc/centreon-engine/*
-cp -R /var/cache/centreon/config/engine/1/* /etc/centreon-engine/
-chown -R apache. /etc/centreon-engine/*
-chmod -R 664 /etc/centreon-engine/*
-
 # Start Centreon Engine daemon.
-service centengine start
-service centengine reload
+/etc/init.d/centengine restart
