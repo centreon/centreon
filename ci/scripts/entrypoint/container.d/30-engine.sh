151f35587cb44338770982cd406b158a2ea897b3
Kevin Duret <kduret@centreon.com>
2022-10-17 19:52:08 +0200

    fix

--- ci/scripts/entrypoint/container.d/30-engine.sh
+++ ci/scripts/entrypoint/container.d/30-engine.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Copy configuration files
+rm -rf /etc/centreon-engine/*
+cp -R /var/cache/centreon/config/engine/1/* /etc/centreon-engine/
+chown -R apache. /etc/centreon-engine/*
+chmod -R 664 /etc/centreon-engine/*
+
 # Start Centreon Engine daemon.
 /etc/init.d/centengine start
