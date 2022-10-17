84732df026a72f15aa41a3814df70ea8266b609d
Kevin Duret <kduret@centreon.com>
2022-10-17 17:57:52 +0200

    update selenium

--- ci/scripts/entrypoint/container.d/60-gorgone.sh
+++ ci/scripts/entrypoint/container.d/60-gorgone.sh
@@ -2,3 +2,6 @@
 
 # Run gorgone in background.
 su - centreon-gorgone -c "/usr/bin/perl /usr/bin/gorgoned --config=/etc/centreon-gorgone/config.yaml --logfile=/var/log/centreon-gorgone/gorgoned.log --severity=info" &
+
+# Wait for centreon-engine configuration files to be exported
+timeout 10 bash -c -- "while [ ! -f /etc/centreon-engine/tags.cfg ]; do sleep 1; done"
