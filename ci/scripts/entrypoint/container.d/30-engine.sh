e53bb48d580bcbd6f6808315839bc42cd5ecc4af
Kevin Duret <kduret@centreon.com>
2022-10-18 08:15:20 +0200

    test

--- ci/scripts/entrypoint/container.d/30-engine.sh
+++ ci/scripts/entrypoint/container.d/30-engine.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 # Start Centreon Engine daemon.
-while [ -s /var/log/centreon-engine/centengine.log ]; do
+while [ `cat /var/log/centreon-engine/centengine.log | wc -l` -lt 1 ]; do
+    echo "centreon-engine log file is empty, restarting centengine"
     /etc/init.d/centengine restart
     sleep 1
-    if [ -s /var/log/centreon-engine/centengine.log ]; then
-        break ;
-    fi
-    echo "centreon-engine log file is still empty"
 done
