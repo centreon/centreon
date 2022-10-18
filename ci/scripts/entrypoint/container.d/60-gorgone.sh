3b7f1d5dd65869a5a1003fc5e8ff305efa975492
Kevin Duret <kduret@centreon.com>
2022-10-18 12:56:00 +0200

    test

--- ci/scripts/entrypoint/container.d/60-gorgone.sh
+++ ci/scripts/entrypoint/container.d/60-gorgone.sh
@@ -2,6 +2,3 @@
 
 # Run gorgone in background.
 su - centreon-gorgone -c "/usr/bin/perl /usr/bin/gorgoned --config=/etc/centreon-gorgone/config.yaml --logfile=/var/log/centreon-gorgone/gorgoned.log --severity=debug" &
-while [ `cat /var/log/centreon-gorgone/gorgoned.log | grep "Setcoreid changed 1" | wc -l` -lt 1 ]; do
-    sleep 1
-done
