bbc85a475c93df97463a1dcd272daa635202fbc5
Kevin Duret <kduret@centreon.com>
2022-10-18 11:44:00 +0200

    test

--- ci/scripts/entrypoint/container.d/60-gorgone.sh
+++ ci/scripts/entrypoint/container.d/60-gorgone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run gorgone in background.
-su - centreon-gorgone -c "/usr/bin/perl /usr/bin/gorgoned --config=/etc/centreon-gorgone/config.yaml --logfile=/var/log/centreon-gorgone/gorgoned.log --severity=info" &
+su - centreon-gorgone -c "/usr/bin/perl /usr/bin/gorgoned --config=/etc/centreon-gorgone/config.yaml --logfile=/var/log/centreon-gorgone/gorgoned.log --severity=debug" &
 while [ `cat /var/log/centreon-gorgone/gorgoned.log | grep "Create module 'legacycmd' process" | wc -l` -lt 1 ]; do
     sleep 1
 done
