54c4050064827c1a1fd262fb234f9d1b1b541c57
Kevin Duret <kduret@centreon.com>
2022-10-18 10:29:47 +0200

    test

--- ci/scripts/entrypoint/container.d/30-engine.sh
+++ ci/scripts/entrypoint/container.d/30-engine.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-
+/etc/init.d/centengine restart
 
 # Start Centreon Engine daemon.
 while [ `cat /var/log/centreon-engine/centengine.log | grep "initialized successfully" | wc -l` -lt 1 ]; do
     echo "centreon-engine log file is empty, restarting centengine"
-    /etc/init.d/centengine restart
     sleep 0.5
     /etc/init.d/centengine reload
     sleep 0.5
--- ci/scripts/entrypoint/container.d/60-gorgone.sh
+++ ci/scripts/entrypoint/container.d/60-gorgone.sh
@@ -2,3 +2,6 @@
 
 # Run gorgone in background.
 su - centreon-gorgone -c "/usr/bin/perl /usr/bin/gorgoned --config=/etc/centreon-gorgone/config.yaml --logfile=/var/log/centreon-gorgone/gorgoned.log --severity=info" &
+while [ `cat /var/log/centreon-gorgone/gorgoned.log | grep "Create module 'legacycmd' process" | wc -l` -lt 1 ]; do
+    sleep 1
+done
