80c775cad27246de02385558ad395e68cbb43bcf
Kevin Duret <kduret@centreon.com>
2022-10-21 16:23:35 +0200

    rename

--- ci/scripts/widget-packaging-deb.sh
+++ ci/scripts/widget-packaging-deb.sh
@@ -4,6 +4,7 @@ set -ex
 
 VERSION=$1
 COMMIT=$2
+WIDGET_NAME=$3
 now=`date +%s`
 
 export RELEASE="$now.$COMMIT"
@@ -12,34 +13,32 @@ AUTHOR="Luiz Costa"
 AUTHOR_EMAIL="me@luizgustavo.pro.br"
 
 cd /src/widgets
-ls -1 | sed '/centreon-widget.spectemplate/d' | while read PROJECT; do
-    if [ -d /build ]; then
-        rm -rf /build
-    fi
-    mkdir -p /build
-    cd /build
+if [ -d /build ]; then
+    rm -rf /build
+fi
+mkdir -p /build
+cd /build
 
-    mkdir -p /build/$PROJECT
-    (cd /src/widgets && tar czvpf - $PROJECT) | dd of=$PROJECT-$VERSION.tar.gz
-    cp -rv /src/widgets/$PROJECT /build/
-    cp -rv /src/widgets/$PROJECT/ci/debian /build/$PROJECT/
+mkdir -p /build/$WIDGET_NAME
+(cd /src/widgets && tar czvpf - $WIDGET_NAME) | dd of=$WIDGET_NAME-$VERSION.tar.gz
+cp -rv /src/widgets/$WIDGET_NAME /build/
+cp -rv /src/widgets/$WIDGET_NAME/ci/debian /build/$WIDGET_NAME/
 
-    ls -lart
-    cd /build/$PROJECT
-    if [ -e /build/$PROJECT/debian/substvars ]; then
-        sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" /build/$PROJECT/debian/substvars
-    fi
-    debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -y -r "$RELEASE"
-    debuild-pbuilder
-    cd /build
+ls -lart
+cd /build/$WIDGET_NAME
+if [ -e /build/$WIDGET_NAME/debian/substvars ]; then
+    sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" /build/$WIDGET_NAME/debian/substvars
+fi
+debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -y -r "$RELEASE"
+debuild-pbuilder
+cd /build
 
-    if [ -d "$RELEASE" ] ; then
-        rm -rf "$RELEASE"
-    fi
-    mkdir $RELEASE
-    mv /build/*.deb $RELEASE/
-    mv /build/$RELEASE/*.deb /src
-done
+if [ -d "$RELEASE" ] ; then
+    rm -rf "$RELEASE"
+fi
+mkdir $RELEASE
+mv /build/*.deb $RELEASE/
+mv /build/$RELEASE/*.deb /src
 
 find /src -iname '*.deb'
 
--- ci/scripts/widget-packaging-rpm.sh
+++ ci/scripts/widget-packaging-rpm.sh
@@ -4,32 +4,31 @@ set -ex
 
 VERSION=$1
 RELEASE=$2
+WIDGET_NAME=$3
 
 if [ ! -d /root/rpmbuild/SOURCES ] ; then
     mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 fi
 
 cd /src/widgets
-ls -1 | sed '/centreon-widget.spectemplate/d' | while read PROJECT; do
-    rm -rf /tmp/$PROJECT-$VERSION
-    mkdir /tmp/$PROJECT-$VERSION
-    cp -rp $PROJECT/* /tmp/$PROJECT-$VERSION/
-    (cd /tmp && tar czf /root/rpmbuild/SOURCES/$PROJECT-$VERSION.tar.gz $PROJECT-$VERSION)
-    rm -rf /root/rpmbuild/RPMS/*
-    cd $PROJECT
-    export WIDGET="$(echo $PROJECT | sed 's/centreon-widget-//')"
-    export SUMMARY="$(find . -name configs.xml | xargs sed -n 's|\s*<description>\(.*\)</description>|\1|p'
- 2>/dev/null)"
-    rpmbuild -ba ../centreon-widget.spectemplate \
-        -D "version $VERSION" \
-        -D "release $RELEASE" \
-        -D "summary $SUMMARY" \
-        -D "name $PROJECT" \
-        -D "widget_sub_dir $WIDGET"
-    cp -r /root/rpmbuild/RPMS/noarch/*.rpm .
-    chmod 777 *.rpm
-    cd ..
-done
+rm -rf /tmp/$WIDGET_NAME-$VERSION
+mkdir /tmp/$WIDGET_NAME-$VERSION
+cp -rp $WIDGET_NAME/* /tmp/$WIDGET_NAME-$VERSION/
+(cd /tmp && tar czf /root/rpmbuild/SOURCES/$WIDGET_NAME-$VERSION.tar.gz $WIDGET_NAME-$VERSION)
+rm -rf /root/rpmbuild/RPMS/*
+cd $WIDGET_NAME
+export WIDGET="$(echo $WIDGET_NAME | sed 's/centreon-widget-//')"
+export SUMMARY="$(find . -name configs.xml | xargs sed -n 's|\s*<description>\(.*\)</description>|\1|p'
+2>/dev/null)"
+rpmbuild -ba ../centreon-widget.spectemplate \
+    -D "version $VERSION" \
+    -D "release $RELEASE" \
+    -D "summary $SUMMARY" \
+    -D "name $WIDGET_NAME" \
+    -D "widget_sub_dir $WIDGET"
+cp -r /root/rpmbuild/RPMS/noarch/*.rpm .
+chmod 777 *.rpm
+cd ..
 
 find /src -type f -iname '*.rpm' | xargs cp -vt /src/
 
