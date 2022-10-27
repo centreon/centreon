e49ae61357d0829c587efe00cf5b9738e54e0fcf
Tom Darneix <tomdar87@outlook.com>
2022-10-27 18:13:19 +0200

    chore(dep): Replace centreon frontend dependency url develop (#82)

--- centreon/jest.config.js
+++ centreon/jest.config.js
@@ -4,6 +4,8 @@ module.exports = mergeDeepRight(
   require('centreon-frontend/packages/frontend-config/jest'),
   {
     moduleNameMapper: {
+      '\\.(s?css|png|svg|jpg)$':
+        '<rootDir>/www/front_src/src/__mocks__/image.js',
       'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
     },
     roots: ['<rootDir>/www/front_src/src/'],
