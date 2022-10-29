60cee394a008b10b5cc469dd606e223b6d3ee0db
Tom Darneix <tomdar87@outlook.com>
2022-10-29 16:01:11 +0200

    Fix eslint

--- centreon/lighthouse/lighthouseReport.ts
+++ centreon/lighthouse/lighthouseReport.ts
@@ -20,11 +20,7 @@ const createReportFile = (report): void => {
 
 const captureReport = async (): Promise<void> => {
   const browser = await puppeteer.launch({
-    args: [
-      '--lang=en-US,en',
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
+    args: ['--lang=en-US,en', '--no-sandbox', '--disable-setuid-sandbox'],
     headless: true,
   });
   const page = await browser.newPage();
