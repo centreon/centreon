1e9a4b5e7fdeba6b359f4df9b8be130ec823e2bc
Bruno d'Auria <bdauria@centreon.com>
2022-10-31 14:51:55 +0100

    Try fixing tests

--- centreon/www/front_src/src/Resources/Actions/index.test.tsx
+++ centreon/www/front_src/src/Resources/Actions/index.test.tsx
@@ -482,8 +482,11 @@ describe(Actions, () => {
 
     await findByText(labelDowntimeByAdmin);
 
-    userEvent.clear(getByLabelText(labelEndTime));
-    userEvent.type(getByLabelText(labelEndTime), dayjs(mockNow).format('L LT'));
+    // userEvent.clear(getByLabelText(labelEndTime));
+    userEvent.type(
+      getByLabelText(labelEndTime),
+      `{selectall}${dayjs(mockNow).format('L LT')}`,
+    );
 
     await waitFor(() =>
       expect(
@@ -515,7 +518,7 @@ describe(Actions, () => {
 
     await findByText(labelDowntimeByAdmin);
 
-    userEvent.type(getByLabelText(labelStartTime), '{backspace}l');
+    userEvent.type(getByLabelText(labelStartTime), '{backspace}{backspace}');
 
     await waitFor(() => {
       expect(
