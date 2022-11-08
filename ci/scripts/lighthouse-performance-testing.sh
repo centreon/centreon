d0305daab83bad1ef51491335dff15ddbb7b91a5
Tom Darneix <tomdar87@outlook.com>
2022-11-08 06:16:30 +0000

    Fix path

--- ci/scripts/lighthouse-performance-testing.sh
+++ ci/scripts/lighthouse-performance-testing.sh
@@ -14,7 +14,7 @@ composer install
 
 cd ../lighthouse
 
-docker cp centreon-injector lighthouse-tests-centreon:/usr/share
+docker cp ../centreon-injector lighthouse-tests-centreon:/usr/share
 docker exec lighthouse-tests-centreon sed -i 's/127.0.0.1/localhost/g' /usr/share/centreon-injector/.env
 docker exec lighthouse-tests-centreon bash -c "cd /usr/share/centreon-injector && bin/console centreon:inject-data" || true
 docker exec lighthouse-tests-centreon bash -c "centreon -u admin -p Centreon\!2021 -a APPLYCFG -v 1"
