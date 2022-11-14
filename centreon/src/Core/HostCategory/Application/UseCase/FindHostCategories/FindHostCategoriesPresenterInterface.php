1906eb7eb40b164fb6f816807e81320045f26bd7
alaunois <alaunois@centreon.com>
2022-11-14 16:38:03 +0100

    add findHostCategories endpoint

--- centreon/src/Core/HostCategory/Application/UseCase/FindHostCategories/FindHostCategoriesPresenterInterface.php
+++ centreon/src/Core/HostCategory/Application/UseCase/FindHostCategories/FindHostCategoriesPresenterInterface.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+* Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-* https://www.apache.org/licenses/LICENSE-2.0
+* http://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,10 +23,8 @@ declare(strict_types=1);
 
 namespace Core\HostCategory\Application\UseCase\FindHostCategories;
 
-final class FindHostCategoriesResponse
+use Core\Application\Common\UseCase\PresenterInterface;
+
+interface FindHostCategoriesPresenterInterface extends PresenterInterface
 {
-    /**
-     * @var array<array{id:int,name:string,alias:string,is_activated:bool,comment:string|null}>
-     */
-    public array $hostCategories = [];
 }
--- centreon/src/Core/HostCategory/Domain/Model/Host.php
+++ centreon/src/Core/HostCategory/Domain/Model/Host.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * https://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,27 +23,21 @@ declare(strict_types=1);
 
 namespace Core\HostCategory\Domain\Model;
 
-class HostCategory extends NewHostCategory
+class Host
 {
-    /**
-     * @param int $id
-     * @param string $name
-     * @param string $alias
-     */
     public function __construct(
         private int $id,
-        string $name,
-        string $alias
+        private string $name
     ) {
-        $this->id = $id;
-        parent::__construct($name, $alias);
     }
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
     }
 }
