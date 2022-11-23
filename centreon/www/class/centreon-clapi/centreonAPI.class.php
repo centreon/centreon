f10d15f6bc889dc057f40272b988d23a77663a6b
Kevin Duret <kduret@centreon.com>
2022-11-23 23:32:19 +0100

    Fix(security): XSS security vulnerabilities (#147)
    Co-authored-by: YAHIAOUI Hamza <hyahiaoui-ext@centreon.com>

--- centreon/www/class/centreon-clapi/centreonAPI.class.php
+++ centreon/www/class/centreon-clapi/centreonAPI.class.php
@@ -660,7 +660,7 @@ class CentreonAPI
      */
     public function launchAction($exit = true)
     {
-        $action = strtoupper($this->action);
+        $action = htmlspecialchars(strtoupper($this->action), ENT_QUOTES, 'UTF-8');
 
         /**
          * Debug
@@ -747,9 +747,13 @@ class CentreonAPI
                 $i++;
                 $tab = preg_split('/;/', $string);
                 if (strlen(trim($string)) != 0 && !preg_match('/^\{OBJECT_TYPE\}/', $string)) {
-                    $this->object = trim($tab[0]);
-                    $this->action = trim($tab[1]);
-                    $this->variables = trim(substr($string, strlen($tab[0] . ";" . $tab[1] . ";")));
+                    $this->object = htmlspecialchars(trim($tab[0]), ENT_QUOTES, 'UTF-8');
+                    $this->action = htmlspecialchars(trim($tab[1]), ENT_QUOTES, 'UTF-8');
+                    $this->variables = htmlspecialchars(
+                        trim(substr($string, strlen($tab[0] . ";" . $tab[1] . ";"))),
+                        ENT_QUOTES,
+                        'UTF-8'
+                    );
                     if ($this->debug == 1) {
                         print "Object : " . $this->object . "\n";
                         print "Action : " . $this->action . "\n";
@@ -774,7 +778,7 @@ class CentreonAPI
 
     public function launchActionForImport()
     {
-        $action = strtoupper($this->action);
+        $action = htmlspecialchars(strtoupper($this->action), ENT_QUOTES, 'UTF-8');
         /**
          * Debug
          */
