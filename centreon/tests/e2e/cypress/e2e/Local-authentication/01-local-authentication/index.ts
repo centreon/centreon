4212f929f448e9b7e09432cd12cfc4748aa75155
SoldierBoy31 <lthomas@centreon.com>
2022-11-23 09:22:08 +0100

    Upadate to Cypress 11

--- centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
+++ centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
@@ -1,4 +1,4 @@
-import { When, Then, Given } from 'cypress-cucumber-preprocessor/steps';
+import { When, Then, Given } from '@badeball/cypress-cucumber-preprocessor';
 
 import {
   getUserContactId,
