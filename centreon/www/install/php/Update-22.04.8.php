f6b35bec839e7df812030cb1d0268e1bea9966cd
hyahiaoui-ext <97593234+hyahiaoui-ext@users.noreply.github.com>
2022-11-24 10:29:04 +0100

    Fix(security): XSS security vulnerabilities 22.04.x (#296)
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- centreon/www/install/php/Update-22.04.8.php
+++ centreon/www/install/php/Update-22.04.8.php
@@ -27,6 +27,12 @@ $centreonLog = new CentreonLog();
 $versionOfTheUpgrade = 'UPGRADE - 22.04.8: ';
 
 try {
+    $errorMessage = "Impossible to delete color picker topology_js entries";
+    $pearDB->query(
+        "DELETE FROM `topology_JS`
+        WHERE `PathName_js` = './include/common/javascript/color_picker_mb.js'"
+    );
+
     // Transactional queries
     $pearDB->beginTransaction();
 
