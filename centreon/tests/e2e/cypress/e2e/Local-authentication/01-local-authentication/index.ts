4fcb804538132f23099f3a80d983c5c6a584e680
Tom Darneix <tomdar87@outlook.com>
2022-12-05 21:24:14 +0100

    Trigger commit

--- centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
+++ centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
@@ -329,7 +329,7 @@ Then('user can not reuse the last passwords more than 3 times', () => {
     .find('#Form')
     .find('#tab1')
     .parent()
-    .contains('Your password has already been used.');
+    .contains('Your password has already been used');
 
   cy.get('@user1Id').then((idUser) => {
     cy.get('@user1CreationPasswordDate').then((userPasswordCreationDate) => {
