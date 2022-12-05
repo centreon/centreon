36994ef9703a2315b6f5720cb727a19bcb2de071
nouha <nelabrouki-ext@centreon.com>
2022-12-05 14:58:20 +0000

    tests part 2

--- centreon/www/front_src/src/Resources/Graph/Performance/TimePeriods/index.cypress.spec.tsx
+++ centreon/www/front_src/src/Resources/Graph/Performance/TimePeriods/index.cypress.spec.tsx
@@ -6,7 +6,6 @@ import localizedFormatPlugin from 'dayjs/plugin/localizedFormat';
 import timezonePlugin from 'dayjs/plugin/timezone';
 import utcPlugin from 'dayjs/plugin/utc';
 import { useAtomValue } from 'jotai';
-import { equals } from 'ramda';
 
 import { LocalizationProvider } from '@mui/x-date-pickers';
 
@@ -21,7 +20,7 @@ dayjs.extend(timezonePlugin);
 dayjs.extend(utcPlugin);
 dayjs.extend(localizedFormatPlugin);
 
-const date = '2023-02-01T12:59:41.041Z';
+const initialDate = '2023-01-01T12:59:41.041Z';
 const timeZoneParis = 'Europe/Paris';
 const numberDaysInWeek = 7;
 
@@ -38,15 +37,98 @@ const days = [
 const months2023 = [
   {
     January: {
-      firstDay: { day: days[0], value: 1 },
-      lastDay: { day: days[6], value: 31 },
+      date: '2023-01-01T12:59:41.041Z',
+      firstDay: { day: days[0], indexDayInRowWeek: 0, value: 1 },
+      lastDay: { day: days[2], indexDayInRowWeek: 2, value: 31 },
+      numberWeeks: 5,
     },
   },
   {
     February: {
+      date: '2023-02-01T12:59:41.041Z',
       firstDay: { day: days[3], indexDayInRowWeek: 3, value: 1 },
       lastDay: { day: days[2], indexDayInRowWeek: 2, value: 28 },
       numberWeeks: 5,
+    },
+  },
+  {
+    March: {
+      date: '2023-03-01T12:59:41.041Z',
+      firstDay: { day: days[3], indexDayInRowWeek: 3, value: 1 },
+      lastDay: { day: days[5], indexDayInRowWeek: 5, value: 31 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    April: {
+      date: '2023-04-01T12:59:41.041Z',
+      firstDay: { day: days[6], indexDayInRowWeek: 6, value: 1 },
+      lastDay: { day: days[0], indexDayInRowWeek: 0, value: 30 },
+      numberWeeks: 6,
+    },
+  },
+  {
+    May: {
+      date: '2023-05-01T12:59:41.041Z',
+      firstDay: { day: days[1], indexDayInRowWeek: 1, value: 1 },
+      lastDay: { day: days[3], indexDayInRowWeek: 3, value: 31 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    June: {
+      date: '2023-06-01T12:59:41.041Z',
+      firstDay: { day: days[4], indexDayInRowWeek: 4, value: 1 },
+      lastDay: { day: days[5], indexDayInRowWeek: 5, value: 30 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    July: {
+      date: '2023-07-01T12:59:41.041Z',
+      firstDay: { day: days[6], indexDayInRowWeek: 6, value: 1 },
+      lastDay: { day: days[1], indexDayInRowWeek: 1, value: 31 },
+      numberWeeks: 6,
+    },
+  },
+  {
+    August: {
+      date: '2023-08-01T12:59:41.041Z',
+      firstDay: { day: days[2], indexDayInRowWeek: 2, value: 1 },
+      lastDay: { day: days[4], indexDayInRowWeek: 4, value: 31 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    September: {
+      date: '2023-09-01T12:59:41.041Z',
+      firstDay: { day: days[5], indexDayInRowWeek: 5, value: 1 },
+      lastDay: { day: days[6], indexDayInRowWeek: 6, value: 30 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    October: {
+      date: '2023-10-01T12:59:41.041Z',
+      firstDay: { day: days[0], indexDayInRowWeek: 0, value: 1 },
+      lastDay: { day: days[2], indexDayInRowWeek: 2, value: 31 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    November: {
+      date: '2023-11-01T12:59:41.041Z',
+      firstDay: { day: days[3], indexDayInRowWeek: 3, value: 1 },
+      lastDay: { day: days[4], indexDayInRowWeek: 4, value: 30 },
+      numberWeeks: 5,
+    },
+  },
+  {
+    December: {
+      date: '2023-12-01T12:59:41.041Z',
+      firstDay: { day: days[5], indexDayInRowWeek: 5, value: 1 },
+      lastDay: { day: days[0], indexDayInRowWeek: 0, value: 31 },
+      numberWeeks: 6,
     },
   },
 ];
@@ -73,7 +155,7 @@ describe('calendar for timeZone=Europe/Paris', () => {
         <LocalizationProvider adapterLocale="en" dateAdapter={Adapter}>
           <DateTimePickerInput
             changeDate={changeDate}
-            date={new Date(date)}
+            date={new Date(initialDate)}
             property={CustomTimePeriodProperty.start}
             setDate={setStart}
           />
@@ -82,40 +164,60 @@ describe('calendar for timeZone=Europe/Paris', () => {
     });
   });
 
-  // it('date must be correct in input calendar', () => {
-  //   const currentDateByTimeZone = dayjs(date).tz(timeZoneParis);
+  it('input calendar value contains correct date', () => {
+    const currentDateByTimeZone = dayjs(initialDate).tz(timeZoneParis);
 
-  //   const dateInput = currentDateByTimeZone.format('L hh:mm A');
-  //   cy.get('input').should('have.value', dateInput);
-  // });
+    const dateInput = currentDateByTimeZone.format('L hh:mm A');
+    cy.get('input').should('have.value', dateInput);
+  });
 
-  it.only('the first/last day of selected month ,must correspond to the correct beginning/end of the week', () => {
-    const { firstDay, lastDay, numberWeeks } = Object.values(months2023[1])[0];
-
+  it('the first/last day of the current month ,must correspond to the  beginning/end of the week to this current month , when clicking on nextMonth button ', () => {
     cy.get('input').click();
 
-    cy.get('[role="rowgroup"]').children().as('listWeeks');
+    months2023.forEach((data) => {
+      const { firstDay, lastDay, numberWeeks } = Object.values(data)[0];
 
-    cy.get('@listWeeks').should('have.length', numberWeeks);
-    cy.get('@listWeeks').eq(0).as('firstWeek');
-    cy.get('@firstWeek').children().as('listDaysInFirstWeek');
+      cy.get('[role="rowgroup"]').children().as('listWeeks');
 
-    cy.get('@listDaysInFirstWeek').should('have.length', numberDaysInWeek);
-    cy.get('@listDaysInFirstWeek')
-      .eq(firstDay.indexDayInRowWeek)
-      .as('firstDayInWeek');
-    cy.get('@firstDayInWeek').contains(firstDay.value);
+      cy.get('@listWeeks').should('have.length', numberWeeks);
+      cy.get('@listWeeks').eq(0).as('firstWeek');
+      cy.get('@firstWeek').children().as('listDaysInFirstWeek');
 
-    cy.get('@listWeeks')
-      .eq(numberWeeks - 1)
-      .as('lastWeek');
+      cy.get('@listDaysInFirstWeek').should('have.length', numberDaysInWeek);
+      cy.get('@listDaysInFirstWeek')
+        .eq(firstDay.indexDayInRowWeek)
+        .as('firstDayInWeek');
+      cy.get('@firstDayInWeek').contains(firstDay.value);
 
-    cy.get('@lastWeek').children().as('listDaysInLastWeek');
-    cy.get('@listDaysInLastWeek').should('have.length', numberDaysInWeek);
+      cy.get('@listWeeks')
+        .eq(numberWeeks - 1)
+        .as('lastWeek');
 
-    cy.get('@listDaysInLastWeek')
-      .eq(lastDay.indexDayInRowWeek)
-      .as('lastDayInWeek');
-    cy.get('@lastDayInWeek').contains(lastDay.value);
+      cy.get('@lastWeek').children().as('listDaysInLastWeek');
+      cy.get('@listDaysInLastWeek').should('have.length', numberDaysInWeek);
+
+      cy.get('@listDaysInLastWeek')
+        .eq(lastDay.indexDayInRowWeek)
+        .as('lastDayInWeek');
+      cy.get('@lastDayInWeek').contains(lastDay.value);
+
+      cy.get('[aria-label="Next month"]').click();
+    });
   });
+
+  it('the correspond month and year must be displayed in the calendars header ,when clicking on nextMonth button', () => {
+    cy.get('input').click();
+
+    months2023.forEach((data) => {
+      const { date } = Object.values(data)[0];
+
+      const monthAndYear = dayjs(date).tz(timeZoneParis).format('MMMM YYYY');
+
+      cy.contains(monthAndYear);
+      cy.get('[aria-label="Next month"]').click();
+    });
+  });
+
+  // days should be displayed
+  // days should not be duplicated
 });
