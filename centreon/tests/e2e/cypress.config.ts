8f8a4760373304b37a201037457af0f7899ff7c2
YAHIAOUI Hamza <hamza.yahiaoui@goandev.net>
2022-12-15 11:38:14 +0100

    adding properties to cypress config

--- centreon/tests/e2e/cypress.config.ts
+++ centreon/tests/e2e/cypress.config.ts
@@ -6,11 +6,13 @@ import { defineConfig } from 'cypress';
 import setupNodeEvents from './cypress/plugins';
 
 export default defineConfig({
+  chromeWebSecurity: false,
   defaultCommandTimeout: 6000,
   e2e: {
     baseUrl: 'http://localhost:4000',
 
     excludeSpecPattern: ['*.js', '*.ts', '*.md'],
+    experimentalSessionAndOrigin: true,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents,
