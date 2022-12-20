069b71a95d178e03fc0f0bc31f60639a75815148
SoldierBoy31 <lthomas@centreon.com>
2022-12-20 11:23:34 +0100

    test with commands deletion

--- centreon/www/front_src/src/Resources/Listing/index.cypress.spec.tsx
+++ centreon/www/front_src/src/Resources/Listing/index.cypress.spec.tsx
@@ -209,7 +209,8 @@ describe('column sorting', () => {
 
   columnToClick.forEach(({ id, label, sortField }) => {
     it(`executes a listing request with sort_by param and stores the order parameter in the URL when ${label} column is clicked`, () => {
-      cy.waitFiltersAndListingRequests();
+      cy.waitForRequest('@filterRequest');
+      cy.waitForRequest('@dataToListingTable');
 
       const sortBy = (sortField || id) as string;
 
