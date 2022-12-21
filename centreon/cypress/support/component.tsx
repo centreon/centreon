83cc9b1d922adfe54384672f1940f2f7c13ca230
Tom Darneix <tomdar87@outlook.com>
2022-12-21 18:33:24 +0100

    Enable visual testing while running cypress in run mode

--- centreon/cypress/support/component.tsx
+++ centreon/cypress/support/component.tsx
@@ -2,10 +2,22 @@ import './commands';
 
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
-addMatchImageSnapshotCommand({
-  capture: 'viewport',
-  customDiffConfig: { threshold: 0.1 },
-  customSnapshotsDir: './cypress/visual-testing-snapshots',
-  failureThreshold: 0.06,
-  failureThresholdType: 'percent'
-});
+const enableVisualTesting = (): void => {
+  if (Cypress.config('isInteractive')) {
+    Cypress.Commands.add('matchImageSnapshot', () => {
+      cy.log('Skipping snapshot');
+    });
+
+    return;
+  }
+
+  addMatchImageSnapshotCommand({
+    capture: 'viewport',
+    customDiffConfig: { threshold: 0.1 },
+    customSnapshotsDir: './cypress/visual-testing-snapshots',
+    failureThreshold: 0.06,
+    failureThresholdType: 'percent'
+  });
+};
+
+enableVisualTesting();
