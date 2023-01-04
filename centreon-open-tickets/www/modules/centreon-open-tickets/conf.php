e5fcdcd37deee7294de21d4d8961dc3a2bd4f8fc
tuntoja <tuntoja@centreon.com>
2023-01-04 15:23:35 +0100

    chore: bump all versions 22.10

--- centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
@@ -22,7 +22,7 @@
 
 $module_conf['centreon-open-tickets']["rname"] = "Centreon Open Tickets";
 $module_conf['centreon-open-tickets']["name"] = "centreon-open-tickets";
-$module_conf['centreon-open-tickets']["mod_release"] = "22.10.0";
+$module_conf['centreon-open-tickets']["mod_release"] = "22.10.1";
 $module_conf['centreon-open-tickets']["infos"] = "Centreon Open Tickets is a community module developed to " .
     "create tickets to your favorite ITSM tools using API.
 
@@ -37,7 +37,7 @@ Regarding the widget configuration, it is possible to see the created tickets by
 $module_conf['centreon-open-tickets']["is_removeable"] = "1";
 $module_conf['centreon-open-tickets']["author"] = "Centreon";
 $module_conf['centreon-open-tickets']["stability"] = "stable";
-$module_conf['centreon-open-tickets']["last_update"] = "2022-10-24";
+$module_conf['centreon-open-tickets']["last_update"] = "2022-01-04";
 $module_conf['centreon-open-tickets']["release_note"] =
     "https://docs.centreon.com/22.10/en/releases/centreon-os-extensions.html";
 $module_conf['centreon-open-tickets']["images"] = [
