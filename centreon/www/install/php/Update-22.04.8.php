55889e1869822ab576f3b2635243f69489d3129c
jeremyjaouen <jjaouen@centreon.com>
2023-01-06 14:49:11 +0100

    fix(upgrade): Move alter table before transaction and add missing request

--- centreon/www/install/php/Update-22.04.8.php
+++ centreon/www/install/php/Update-22.04.8.php
@@ -27,8 +27,18 @@ $centreonLog = new CentreonLog();
 $versionOfTheUpgrade = 'UPGRADE - 22.04.8: ';
 
 try {
+    if ($pearDB->isColumnExist('remote_servers', 'app_key') === 1) {
+        $errorMessage = "Unable to remove app_key column";
+        $pearDB->query("ALTER TABLE `remote_servers` DROP COLUMN `app_key`");
+    }
+
     // Transactional queries
     $pearDB->beginTransaction();
+    $errorMessage = "Impossible to delete color picker topology_js entries";
+    $pearDB->query(
+        "DELETE FROM `topology_JS`
+        WHERE `PathName_js` = './include/common/javascript/color_picker_mb.js'"
+    );
 
     $errorMessage = 'Unable to update illegal characters fields from engine configuration of pollers';
     decodeIllegalCharactersNagios($pearDB);
