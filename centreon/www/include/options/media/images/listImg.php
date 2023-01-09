42560ae95e436ea7d4fa7de4fbbfd56cce5450a2
elmahdiabbassi <emabbassi-ext@centreon.com>
2023-01-09 11:51:49 +0100

    calculate free space and display on ui

--- centreon/www/include/options/media/images/listImg.php
+++ centreon/www/include/options/media/images/listImg.php
@@ -148,6 +148,20 @@ for ($i = 0; $elem = $res->fetchRow(); $i++) {
 }
 
 $tpl->assign("elemArr", $elemArr);
+
+/*
+ * Calculate availiable disk's space
+ */
+
+$diskStmnt = $pearDB->query("SELECT value FROM options AS o WHERE  o.key = 'nagios_path_img'")->fetch();
+
+$bytes = disk_free_space($diskStmnt["value"]);
+$si_prefix = array ('B', 'KB', 'MB', 'GB', 'TB');
+$base = 1024;
+$class = min((int)log($bytes, $base), count($si_prefix) - 1);
+$availiableSpace = sprintf('%1.2f', $bytes / pow($base, $class)) . ' ' . $si_prefix[$class];
+$tpl->assign("availiableSpace",$availiableSpace);
+
 /*
  * Different messages we put in the template
  */
