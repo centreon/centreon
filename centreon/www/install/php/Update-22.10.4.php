7a15cf4f5b17be99f6431f47054653cd0e1024b1
Kevin Duret <kduret@centreon.com>
2023-01-10 13:54:53 +0100

    fix(upgrade): redo missing upgrade script 22.10.2 (#662)
    Refs: MON-16485

--- centreon/www/install/php/Update-22.10.4.php
+++ centreon/www/install/php/Update-22.10.4.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ require_once __DIR__ . '/../../class/centreonLog.class.php';
 $centreonLog = new CentreonLog();
 
 //error specific content
-$versionOfTheUpgrade = 'UPGRADE - 22.10.2: ';
+$versionOfTheUpgrade = 'UPGRADE - 22.10.4: ';
 $errorMessage = '';
 
 try {
