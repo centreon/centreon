b9d4afe8dd098d623585a1964a55cedcc310a337
Kevin Duret <kduret@centreon.com>
2023-01-12 16:28:21 +0100

    limit token usage

--- centreon/tests/php/Centreon/Domain/Authentication/UseCase/LogoutTest.php
+++ centreon/tests/php/Centreon/Domain/Authentication/UseCase/LogoutTest.php
@@ -65,7 +65,7 @@ class LogoutTest extends TestCase
         $this->authenticationRepository
             ->expects($this->once())
             ->method('deleteSecurityToken')
-            ->with('abc123');
+            ->with('abc123aaaaa');
 
         $logout->execute($logoutRequest);
     }
