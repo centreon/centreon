726f26e09d7261e6e9769168b7a20a2aed962d31
Tom Darneix <tomdar87@outlook.com>
2023-01-19 17:41:15 +0100

    Add action to retrieve beta branch version

---
+++ .github/actions/get-latest-npm-beta-branch-version/index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+
+const getPackageInformations = async () => {
+  const package = core.getInput('package');
+
+  const response = await fetch(`https://registry.npmjs.org/@centreon/${package}`);
+  return await response.json();
+}
+
+try {
+  const branchName = core.getInput('branch_name');
+
+  getPackageInformations().then((package) => {
+    core.setOutput("package_version", package['dist-tags'][branchName])
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
