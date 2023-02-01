6aeaad80160ec25ac0ba00740837e2977adbd47e
s-duret <sduret@centreon.com>
2023-02-01 16:27:29 +0100

    fix(clapi) do not encode CLAPI variables

--- centreon/www/class/centreon-clapi/centreonAPI.class.php
+++ centreon/www/class/centreon-clapi/centreonAPI.class.php
@@ -805,11 +805,7 @@ class CentreonAPI
                 if (strlen(trim($string)) != 0 && !preg_match('/^\{OBJECT_TYPE\}/', $string)) {
                     $this->object = htmlspecialchars(trim($tab[0]), ENT_QUOTES, 'UTF-8');
                     $this->action = htmlspecialchars(trim($tab[1]), ENT_QUOTES, 'UTF-8');
-                    $this->variables = htmlspecialchars(
-                        trim(substr($string, strlen($tab[0] . ";" . $tab[1] . ";"))),
-                        ENT_QUOTES,
-                        'UTF-8'
-                    );
+                    $this->variables = trim(substr($string, strlen($tab[0] . ";" . $tab[1] . ";")));
                     if ($this->debug == 1) {
                         print "Object : " . $this->object . "\n";
                         print "Action : " . $this->action . "\n";
