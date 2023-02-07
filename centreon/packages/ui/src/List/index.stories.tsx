a450c6b38348b850e7b3a992bc391b0417f4ef99
Mohamed El Meziani <melmeziani-ext@centreon.com>
2023-02-07 11:03:58 +0000

    add storie for list component

---
+++ centreon/packages/ui/src/List/index.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CopyIcon from '@mui/icons-material/ContentCopy';
+
+import List from '.';
+
+const actions = [
+  {
+    Icon: CopyIcon,
+    label: 'duplicate',
+    onClick: (): void => undefined
+  },
+  {
+    Icon: EditIcon,
+    label: 'Edit',
+    onClick: (): void => undefined
+  },
+  {
+    Icon: DeleteIcon,
+    label: 'Delete',
+    onClick: (): void => undefined
+  }
+];
+
+export default {
+  argTypes: {},
+  component: List,
+  title: 'List'
+} as ComponentMeta<typeof List>;
+
+const Template: ComponentStory<typeof List> = (args) => <List {...args} />;
+
+export const Playground = Template.bind({});
+
+Playground.args = {
+  actions
+};
