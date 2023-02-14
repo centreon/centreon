be6b14ff1d2ae9aae449f4d3ff03db0fd7c2cdb3
Thebarda <tomdar87@outlook.com>
2023-02-14 13:30:53 +0000

    Fix tooltip position

--- centreon/packages/ui/src/RichTextEditor/utils/getDOMRangeRect.ts
+++ centreon/packages/ui/src/RichTextEditor/utils/getDOMRangeRect.ts
@@ -1,7 +1,13 @@
+import { isNil } from 'ramda';
+
 export const getDOMRangeRect = (
-  nativeSelection: Selection,
-  rootElement: HTMLElement
-): DOMRect => {
+  nativeSelection: Selection | null,
+  rootElement: HTMLElement | null
+): DOMRect | null => {
+  if (isNil(nativeSelection) || isNil(rootElement)) {
+    return null;
+  }
+
   const domRange = nativeSelection.getRangeAt(0);
   if (nativeSelection.anchorNode === rootElement) {
     let inner = rootElement;
