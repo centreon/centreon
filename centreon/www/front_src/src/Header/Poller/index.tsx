45e3277fe9f66756689b7d995324a0d599497009
gael-boyenval <gaelboyenval@icloud.com>
2023-02-21 10:40:30 +0100

    enh(web): refactor headers improve responsive design and fix bugs (#858)
    Co-authored-by: Kevin Duret <kduret@centreon.com>

---
+++ centreon/www/front_src/src/Header/Poller/index.tsx
@@ -0,0 +1,37 @@
+import PollerIcon from '@mui/icons-material/DeviceHub';
+
+import { MenuSkeleton } from '@centreon/ui';
+
+import ItemLayout from '../sharedUI/ItemLayout';
+
+import PollerStatusIcon from './PollerStatusIcon';
+import { PollerSubMenu } from './PollerSubMenu/PollerSubMenu';
+import { usePollerData } from './usePollerData';
+import { labelPollers } from './translatedLabels';
+
+const ServiceStatusCounter = (): JSX.Element | null => {
+  const { isLoading, data, isAllowed } = usePollerData();
+
+  if (isLoading) {
+    return <MenuSkeleton width={20} />;
+  }
+
+  if (!isAllowed || !data) {
+    return null;
+  }
+
+  return (
+    <ItemLayout
+      Icon={PollerIcon}
+      renderIndicators={(): JSX.Element => (
+        <PollerStatusIcon iconSeverities={data.iconSeverities} />
+      )}
+      renderSubMenu={({ closeSubMenu }): JSX.Element => (
+        <PollerSubMenu {...data.subMenu} closeSubMenu={closeSubMenu} />
+      )}
+      title={labelPollers}
+    />
+  );
+};
+
+export default ServiceStatusCounter;
