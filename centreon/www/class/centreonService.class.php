d5f0a45638e003ee1a6997b1b5e421f7bc84ebfa
Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
2023-02-23 10:04:58 +0100

    fix(addDowntime): fix SQL requests when adding downtime (#968) (#997)

--- centreon/www/class/centreonService.class.php
+++ centreon/www/class/centreonService.class.php
@@ -61,6 +61,9 @@ class CentreonService
      */
     protected $instanceObj;
 
+    private const TABLE_SERVICE_CONFIGURATION = 'service',
+                  TABLE_SERVICE_REALTIME = 'services';
+
     /**
      *  Constructor
      *
@@ -1691,37 +1694,39 @@ class CentreonService
      * Returns service details
      *
      * @param int $id
+     * @param array $parameters
+     * @param boolean $monitoringDB
+     *
      * @return array
      */
-    public function getParameters($id, $parameters = array(), $monitoringDB = false)
+    public function getParameters($id, $parameters = [], $monitoringDB = false)
     {
-        $sElement = "*";
-        $arr = array();
-        if (empty($id)) {
-            return array();
-        }
-        if (count($parameters) > 0) {
-            $sElement = implode(",", $parameters);
+        if ((int) $id <= 0) {
+            return [];
         }
 
-        $table = 'service';
-        $db = $this->db;
-        if ($monitoringDB) {
-            $table = 'services';
-            $db = $this->dbMon;
-        }
+        $searchColumns = (count($parameters) > 0) ? implode(',', $parameters) : '*';
+        $searchTable = ($monitoringDB === true) ? self::TABLE_SERVICE_REALTIME : self::TABLE_SERVICE_CONFIGURATION;
+        $database = ($monitoringDB === true) ? $this->dbMon : $this->db;
 
-        $res = $db->query(
-            "SELECT " . $sElement . " "
-            . "FROM " . $table . " "
-            . "WHERE service_id = " . $db->escape($id)
+        $statement = $database->prepare(<<<SQL
+            SELECT
+                $searchColumns
+            FROM
+                $searchTable
+            WHERE
+                service_id = :serviceId
+            SQL
         );
+        $statement->bindValue(':serviceId', (int) $id, \PDO::PARAM_INT);
+        $statement->execute();
 
-        if ($res->rowCount()) {
-            $arr = $res->fetchRow();
+        $result = [];
+        if ($statement->rowCount()) {
+            $result = $statement->fetchRow();
         }
 
-        return $arr;
+        return $result;
     }
 
     /**
