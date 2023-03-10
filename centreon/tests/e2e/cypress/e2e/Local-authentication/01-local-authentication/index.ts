c7af19be0ec44f933ee531328988f2f6c56fa904
Tom Darneix <tomdar87@outlook.com>
2023-03-10 21:10:35 +0100

    Fix user is blocked text detection

--- centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
+++ centreon/tests/e2e/cypress/e2e/Local-authentication/01-local-authentication/index.ts
@@ -466,7 +466,8 @@ Then('the user is locked after reaching the number of allowed attempts', () => {
   cy.loginByTypeOfUser({
     jsonName: 'user-non-admin-with-wrong-password',
     preserveToken: false
-  }).contains('User is blocked');
+  })
+  cy.contains('User is blocked').should('exist');
 
   cy.reload();
 });
