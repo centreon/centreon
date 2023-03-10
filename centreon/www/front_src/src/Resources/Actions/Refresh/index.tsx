dde63d065d4d8ef90b3fa9547319b026fd88a338
gael-boyenval <gaelboyenval@icloud.com>
2023-03-10 15:16:24 +0100

    add string type

--- centreon/www/front_src/src/Resources/Actions/Refresh/index.tsx
+++ centreon/www/front_src/src/Resources/Actions/Refresh/index.tsx
@@ -67,11 +67,11 @@ const RefreshActions = ({ onRefresh }: Props): JSX.Element => {
     <Grid container spacing={1}>
       <Grid item>
         <IconButton
-          ariaLabel={t(labelRefresh)}
+          ariaLabel={t(labelRefresh) as string}
           data-testid="Refresh"
           disabled={sending}
           size="small"
-          title={t(labelRefresh)}
+          title={t(labelRefresh) as string}
           onClick={onRefresh}
         >
           <IconRefresh />
