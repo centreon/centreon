f6ec05593f7419595788888476df2ad5c460c035
gael-boyenval <gaelboyenval@icloud.com>
2023-03-14 14:06:59 +0100

    Apply suggestions from code review
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>

--- centreon/www/front_src/src/Header/Resources/Service/translatedLabels.ts
+++ centreon/www/front_src/src/Header/Resources/Service/translatedLabels.ts
@@ -3,11 +3,11 @@ export const labelServices = 'Services';
 export const labelCriticalStatusServices = 'Critical status services';
 export const labelWarningStatusServices = 'Warning status services';
 export const labelUnknownStatusServices = 'Unknown status services';
-export const labelOkStatusServices = 'Ok status services';
+export const labelOkStatusServices = 'OK status services';
 
 export const labelAll = 'All';
 export const labelCritical = 'Critical';
 export const labelWarning = 'Warning';
 export const labelPending = 'Pending';
 export const labelUnknown = 'Unknown';
-export const labelOk = 'Ok';
+export const labelOk = 'OK';
