6fc3b03fd897c3d7f1a66633668f2b060349f38b
Tom Darneix <tomdar87@outlook.com>
2023-03-15 11:31:25 +0100

    enh(CI): Improve Cypress reports (#1044)

--- centreon/tests/e2e/cypress/e2e/OpenID-connect/02-oidc-authentication-only/index.ts
+++ centreon/tests/e2e/cypress/e2e/OpenID-connect/02-oidc-authentication-only/index.ts
@@ -1,4 +1,4 @@
-  import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 import {
   initializeOIDCUserAndGetLoginPage,
