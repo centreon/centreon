6342b7c94d5eb9e143c498da27baaa97b46008f2
jeremyjaouen <jjaouen@centreon.com>
2023-03-20 10:26:26 +0100

    add svg to container

--- .github/docker/sql/media.sql
+++ .github/docker/sql/media.sql
@@ -8,10 +8,10 @@ VALUES ('logos', 'logos', 'my logos');
 -- Contenu de la table view_img_dir
 --
 INSERT INTO `view_img` (`img_name`, `img_path`, `img_comment`)
-VALUES ('centreon', 'logo-centreon-colors.png', 'centreon logo colors');
+VALUES ('centreon', 'logo-centreon-colors.svg', 'centreon logo colors');
 
 INSERT INTO `view_img` (`img_name`, `img_path`, `img_comment`)
-VALUES ('centreon white', 'logo-centreon-white.png', 'centreon logo white');
+VALUES ('centreon white', 'logo-centreon-white.svg', 'centreon logo white');
 
 --
 -- Contenu de la table view_img_dir_relation
