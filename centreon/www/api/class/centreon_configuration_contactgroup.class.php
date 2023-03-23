098a1a793fa62955749a61400d608ae551ce60a0
s-duret <sduret@centreon.com>
2023-03-23 17:26:44 +0100

    fix(web): users should not be able to add LDAP groups to a contact

--- centreon/www/api/class/centreon_configuration_contactgroup.class.php
+++ centreon/www/api/class/centreon_configuration_contactgroup.class.php
@@ -99,6 +99,10 @@ class CentreonConfigurationContactgroup extends CentreonConfigurationObjects
 
         $contactgroupList = array();
         foreach ($aclCgs['items'] as $id => $contactgroup) {
+            if (($this->arguments['type'] == 'local') && ($contactgroup['cg_type'] == 'ldap')) {
+                $aclCgs['total'] -= 1;
+                continue;
+            }
             $sText = $contactgroup['cg_name'];
             if ($contactgroup['cg_type'] == 'ldap') {
                 $sText .= " (LDAP : " . $contactgroup['ar_name'] . ")";
@@ -111,23 +115,25 @@ class CentreonConfigurationContactgroup extends CentreonConfigurationObjects
         }
 
         # get Ldap contactgroups
-        $ldapCgs = array();
-        if (isset($this->arguments['page_limit']) && isset($this->arguments['page'])) {
-            $maxItem = $this->arguments['page_limit'] * $this->arguments['page'];
-            if ($aclCgs['total'] <= $maxItem) {
+        if ($this->arguments['type'] != 'local') {
+            $ldapCgs = array();
+            if (isset($this->arguments['page_limit']) && isset($this->arguments['page'])) {
+                $maxItem = $this->arguments['page_limit'] * $this->arguments['page'];
+                if ($aclCgs['total'] <= $maxItem) {
+                    $ldapCgs = $cg->getLdapContactgroups($ldapFilter);
+                }
+            } else {
                 $ldapCgs = $cg->getLdapContactgroups($ldapFilter);
             }
-        } else {
-            $ldapCgs = $cg->getLdapContactgroups($ldapFilter);
-        }
 
-        foreach ($ldapCgs as $key => $value) {
-            $sTemp = $value;
-            if (!$this->uniqueKey($sTemp, $contactgroupList)) {
-                $contactgroupList[] = array(
-                    'id' => $key,
-                    'text' => $value
-                );
+            foreach ($ldapCgs as $key => $value) {
+                $sTemp = $value;
+                if (!$this->uniqueKey($sTemp, $contactgroupList)) {
+                    $contactgroupList[] = array(
+                        'id' => $key,
+                        'text' => $value
+                    );
+                }
             }
         }
 
