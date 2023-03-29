a62f2c1faa291dc67ff4e83bc8ad30c0ad22efce
Tom Darneix <tomdar87@outlook.com>
2023-03-29 18:41:52 +0200

    Trigger workflow

--- centreon/packages/ui/src/InputField/Select/Autocomplete/Connected/index.tsx
+++ centreon/packages/ui/src/InputField/Select/Autocomplete/Connected/index.tsx
@@ -37,7 +37,6 @@ export interface ConnectedAutoCompleteFieldProps<TData> {
   labelKey?: string;
   searchConditions?: Array<ConditionsSearchParameter>;
 }
-const t = 't';
 
 const ConnectedAutocompleteField = (
   AutocompleteField: (props) => JSX.Element,
