8bb9b5f3ed0ad265491c59b2931f24f90188c5c9
nouha <nelabrouki-ext@centreon.com>
2023-03-30 11:55:20 +0000

    apply feedback

--- centreon/www/front_src/src/Resources/Actions/Resource/Check/Text.tsx
+++ centreon/www/front_src/src/Resources/Actions/Resource/Check/Text.tsx
@@ -1,13 +1,12 @@
 import { makeStyles } from 'tss-react/mui';
 import { useTranslation } from 'react-i18next';
-import { equals } from 'ramda';
 
 import { Typography } from '@mui/material';
 
 const useStyles = makeStyles()((theme) => ({
   description: {
     fontSize: '0.8rem',
-    whiteSpace: 'pre-line'
+    maxWidth: theme.spacing(31)
   },
   title: {
     color: theme.palette.info.main,
@@ -24,22 +23,10 @@ const Text = ({ title, description }: Props): JSX.Element => {
   const { classes } = useStyles();
   const { t } = useTranslation();
 
-  const formatDescription = (data: string): string => {
-    const words = data.split(' ');
-
-    const wordsWithLineBreak = words.map((word, index) => {
-      return equals(index, Math.round(words.length / 2)) ? `${word}\n` : word;
-    });
-
-    return wordsWithLineBreak.join(' ');
-  };
-
   return (
     <>
       <Typography className={classes.title}>{t(title)}</Typography>
-      <Typography className={classes.description}>
-        {formatDescription(t(description))}
-      </Typography>
+      <Typography className={classes.description}>{t(description)}</Typography>
     </>
   );
 };
