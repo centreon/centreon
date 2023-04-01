b375d74ac7d5e3f8314f76d9695762319ef3113f
Mohamed El Meziani <melmeziani-ext@centreon.com>
2023-04-01 13:25:12 +0000

    fix the with of export button

--- centreon/www/front_src/src/Header/PollerMenu/ExportConfiguration/index.tsx
+++ centreon/www/front_src/src/Header/PollerMenu/ExportConfiguration/index.tsx
@@ -41,6 +41,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     fontSize: theme.typography.body2.fontSize,
     marginTop: theme.spacing(1),
+    width : "100%"
   },
   pollerText: {
     margin: theme.spacing(0),
