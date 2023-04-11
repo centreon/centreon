64752101d2fa7482d31e281268663b7e83f5d6ef
SoldierBoy31 <lthomas@centreon.com>
2023-04-11 14:49:35 +0200

    fix eslint errors

--- centreon/packages/ui/src/Colors/index.stories.tsx
+++ centreon/packages/ui/src/Colors/index.stories.tsx
@@ -1,73 +1,123 @@
-import { ThemeMode } from '@centreon/ui-context';
-import { Paper } from '@mui/material';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import { ColorStory } from '.';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Paper } from '@mui/material';
+
+import { ThemeMode } from '@centreon/ui-context';
+
 import StoryBookThemeProvider from '../StoryBookThemeProvider';
 
+import { ColorStory } from '.';
+
 export default {
-  argTypes: {
-  },
+  argTypes: {},
   component: ColorStory,
   title: 'Colors'
 } as ComponentMeta<typeof ColorStory>;
 
-const TemplateColorStory: ComponentStory<typeof ColorStory> = (args) => (
-  <StoryBookThemeProvider themeMode={args.themeMode || ThemeMode.light}>
-    <Paper sx={{
-      padding: 2
-    }}>
-    <ColorStory {...args} />
-    </Paper>
-  </StoryBookThemeProvider>
+const TemplateColorStory: ComponentStory<typeof ColorStory> = (args) => {
+  const { themeMode } = args;
 
-);
+  return (
+    <StoryBookThemeProvider themeMode={themeMode || ThemeMode.light}>
+      <Paper
+        sx={{
+          padding: 2
+        }}
+      >
+        <ColorStory {...args} />
+      </Paper>
+    </StoryBookThemeProvider>
+  );
+};
 
 export const primaryLight = (): JSX.Element => (
-  <TemplateColorStory paletteKey="primary" title="primary" themeMode={ThemeMode.light} />
+  <TemplateColorStory
+    paletteKey="primary"
+    themeMode={ThemeMode.light}
+    title="primary"
+  />
 );
 
 export const secondaryLight = (): JSX.Element => (
-  <TemplateColorStory paletteKey="secondary" title="secondary" themeMode={ThemeMode.light}/>
+  <TemplateColorStory
+    paletteKey="secondary"
+    themeMode={ThemeMode.light}
+    title="secondary"
+  />
 );
 
 export const statusLight = (): JSX.Element => (
-  <TemplateColorStory isGrouped title="Status" themeMode={ThemeMode.light}/>
+  <TemplateColorStory isGrouped themeMode={ThemeMode.light} title="Status" />
 );
 
 export const textLight = (): JSX.Element => (
-  <TemplateColorStory isText paletteKey="text" title="Text" themeMode={ThemeMode.light}/>
+  <TemplateColorStory
+    isText
+    paletteKey="text"
+    themeMode={ThemeMode.light}
+    title="Text"
+  />
 );
 
 export const backgroundLight = (): JSX.Element => (
-  <TemplateColorStory paletteKey="background" title="Background" themeMode={ThemeMode.light}/>
+  <TemplateColorStory
+    paletteKey="background"
+    themeMode={ThemeMode.light}
+    title="Background"
+  />
 );
 
 export const actionLight = (): JSX.Element => (
-  <TemplateColorStory paletteKey="action" title="Action" themeMode={ThemeMode.light}/>
+  <TemplateColorStory
+    paletteKey="action"
+    themeMode={ThemeMode.light}
+    title="Action"
+  />
 );
 
 export const primaryDark = (): JSX.Element => (
-  <TemplateColorStory paletteKey="primary" title="primary" themeMode={ThemeMode.dark} />
+  <TemplateColorStory
+    paletteKey="primary"
+    themeMode={ThemeMode.dark}
+    title="primary"
+  />
 );
 
 export const secondaryDark = (): JSX.Element => (
-  <TemplateColorStory paletteKey="secondary" title="secondary" themeMode={ThemeMode.dark}/>
+  <TemplateColorStory
+    paletteKey="secondary"
+    themeMode={ThemeMode.dark}
+    title="secondary"
+  />
 );
 
 export const statusDark = (): JSX.Element => (
-  <TemplateColorStory isGrouped title="Status" themeMode={ThemeMode.dark}/>
+  <TemplateColorStory isGrouped themeMode={ThemeMode.dark} title="Status" />
 );
 
 export const textDark = (): JSX.Element => (
-  <TemplateColorStory isText paletteKey="text" title="Text" themeMode={ThemeMode.dark}/>
+  <TemplateColorStory
+    isText
+    paletteKey="text"
+    themeMode={ThemeMode.dark}
+    title="Text"
+  />
 );
 
 export const backgroundDark = (): JSX.Element => (
-  <TemplateColorStory paletteKey="background" title="Background" themeMode={ThemeMode.dark}/>
+  <TemplateColorStory
+    paletteKey="background"
+    themeMode={ThemeMode.dark}
+    title="Background"
+  />
 );
 
 export const actionDark = (): JSX.Element => (
-  <TemplateColorStory paletteKey="action" title="Action" themeMode={ThemeMode.dark}/>
+  <TemplateColorStory
+    paletteKey="action"
+    themeMode={ThemeMode.dark}
+    title="Action"
+  />
 );
