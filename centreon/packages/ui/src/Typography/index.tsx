a86619cd75a8c8df349c8de3a6c35cb0bd79ab14
SoldierBoy31 <lthomas@centreon.com>
2023-04-12 14:22:40 +0200

    apply suggestion to Typography storie

--- centreon/packages/ui/src/Typography/index.tsx
+++ centreon/packages/ui/src/Typography/index.tsx
@@ -1,22 +1,33 @@
 import { makeStyles } from 'tss-react/mui';
+import { toPairs } from 'ramda';
 
-import { Typography, Box } from '@mui/material';
+import {
+  useTheme,
+  Typography,
+  Box,
+  Stack,
+  CardContent,
+  Card,
+  CardHeader
+} from '@mui/material';
 
 const useStyles = makeStyles()((theme) => ({
   headerContainer: {
+    alignItems: 'center',
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    margin: theme.spacing(3)
   },
   itemContainer: {
     borderRadius: 1,
     margin: theme.spacing(3),
-    padding: theme.spacing(1),
     textAlign: 'center'
   }
 }));
 
 const TypographyStory = (): JSX.Element => {
   const { classes } = useStyles();
+  const { typography } = useTheme();
 
   const variants = [
     'h1',
@@ -38,7 +49,12 @@ const TypographyStory = (): JSX.Element => {
     <Box sx={{ width: '100%' }}>
       <Box className={classes.itemContainer}>
         {variants.map((typographyVariant) => (
-          <div className={classes.headerContainer} key={typographyVariant}>
+          <Stack
+            className={classes.headerContainer}
+            direction="row"
+            key={typographyVariant}
+            spacing={10}
+          >
             <Typography
               gutterBottom
               display="block"
@@ -46,7 +62,28 @@ const TypographyStory = (): JSX.Element => {
             >
               {typographyVariant}
             </Typography>
-          </div>
+            <Card sx={{ minWidth: 545 }}>
+              <CardHeader title={typographyVariant} />
+              <CardContent>
+                {toPairs(typography[typographyVariant]).map(([key, value]) => (
+                  <Box
+                    key={key}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between'
+                    }}
+                  >
+                    <Typography color="text.secondary" variant="subtitle1">
+                      {key}
+                    </Typography>
+                    <Typography color="text.secondary" variant="body2">
+                      {value}
+                    </Typography>
+                  </Box>
+                ))}
+              </CardContent>
+            </Card>
+          </Stack>
         ))}
       </Box>
     </Box>
