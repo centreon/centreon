cc7ca343a2ab7fb9abcaff2a7b9316def76ca1ea
jeremyjaouen <jjaouen@centreon.com>
2023-04-17 10:53:37 +0200

    add lexik

--- centreon/config/bundles.php
+++ centreon/config/bundles.php
@@ -30,4 +30,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
