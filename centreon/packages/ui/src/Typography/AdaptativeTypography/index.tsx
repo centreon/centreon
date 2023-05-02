f46df2663bf516e549df6bf08d32ff4d153a4967
Tom Darneix <tomdar87@outlook.com>
2023-05-02 23:47:37 +0200

    Fix

--- centreon/packages/ui/src/Typography/AdaptativeTypography/index.tsx
+++ centreon/packages/ui/src/Typography/AdaptativeTypography/index.tsx
@@ -1,30 +1,34 @@
 import { Responsive } from '@visx/visx';
 import { omit } from 'ramda';
 import ScaleText from 'react-scale-text';
+import { makeStyles } from 'tss-react/mui';
 
-import { TypographyProps, useTheme } from '@mui/material';
+import { TypographyProps } from '@mui/material';
 
-export interface AdaptativeTypographyProps
-  extends Pick<TypographyProps, 'variant'> {
+type CustomTypographyProps = Required<Pick<TypographyProps, 'variant'>>;
+export interface AdaptativeTypographyProps extends CustomTypographyProps {
+  className: string;
   text: string;
 }
 
+const useStyles = makeStyles<CustomTypographyProps>()((theme, { variant }) => ({
+  container: {
+    ...omit(['fontSize'], theme.typography[variant])
+  }
+}));
+
 const AdaptativeTypography = ({
   text,
-  variant = 'body1'
+  variant = 'body1',
+  className
 }: AdaptativeTypographyProps): JSX.Element => {
-  const theme = useTheme();
+  const { classes, cx } = useStyles({ variant });
 
   return (
     <Responsive.ParentSizeModern>
       {(): JSX.Element => {
         return (
-          <span
-            style={{
-              ...omit(['fontSize'], theme.typography[variant]),
-              lineHeight: 1.2
-            }}
-          >
+          <span className={cx(classes.container, className)}>
             <ScaleText>{text}</ScaleText>
           </span>
         );
