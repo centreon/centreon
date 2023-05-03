b130c0f6b437e5f09cb466bd6dede806f804830d
Tom Darneix <tomdar87@outlook.com>
2023-05-03 19:33:58 +0200

    Inser to the topology

---
+++ centreon/www/install/sql/centreon/Update-DB-23.10.0.sql
@@ -0,0 +1,1 @@
+INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_group`, `topology_order`) VALUES ('Dashboard (beta)', '/home/dashboards', '1', '1', 1, 104, 1, 2);
