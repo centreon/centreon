bebb9ac38a6ced3f24f1a1116c2d8b7c66b1c697
nouha <nelabrouki-ext@centreon.com>
2023-05-03 18:10:37 +0000

    apply 3rd feedback

--- centreon/packages/ui/src/Graph/InteractionWithGraph/index.tsx
+++ centreon/packages/ui/src/Graph/InteractionWithGraph/index.tsx
@@ -1,19 +1,74 @@
+import { useState } from 'react';
+
+import { useUpdateAtom } from 'jotai/utils';
+import { makeStyles } from 'tss-react/mui';
+
+import Bar from './Bar';
 import ZoomPreview from './ZoomPreview';
 import { ZoomPreviewData } from './ZoomPreview/models';
+import { eventMouseMovingAtom } from './interactionWithGraphAtoms';
 
-interface Props {
-  renderAreaToInteractWith: JSX.Element;
-  zoomPreviewData: ZoomPreviewData;
+const useStyles = makeStyles()(() => ({
+  overlay: {
+    cursor: 'crosshair'
+  }
+}));
+
+interface CommonData {
+  graphHeight: number;
+  graphWidth: number;
 }
 
-const InteractionWithGraph = ({
-  zoomPreviewData,
-  renderAreaToInteractWith
-}: Props): JSX.Element => {
+interface Props {
+  commonData: CommonData;
+  zoomData: Pick<ZoomPreviewData, 'positionX' | 'xScale'>;
+}
+
+const InteractionWithGraph = ({ zoomData, commonData }: Props): JSX.Element => {
+  const { classes } = useStyles();
+
+  const [eventMouseDown, setEventMouseDown] = useState<null | MouseEvent>(null);
+  const setEventMouseMoving = useUpdateAtom(eventMouseMovingAtom);
+
+  const { graphHeight, graphWidth } = commonData;
+
+  const mouseLeave = (): void => {
+    setEventMouseMoving(null);
+    setEventMouseDown(null);
+  };
+
+  const mouseUp = (): void => {
+    setEventMouseDown(null);
+  };
+
+  const mouseMove = (event): void => {
+    setEventMouseMoving(event);
+  };
+
+  const mouseDown = (event): void => {
+    setEventMouseDown(event);
+  };
+
   return (
     <g>
-      <ZoomPreview {...zoomPreviewData} />
-      {renderAreaToInteractWith}
+      <Bar
+        className={classes.overlay}
+        fill="transparent"
+        height={graphHeight}
+        width={graphWidth}
+        x={0}
+        y={0}
+        onMouseDown={mouseDown}
+        onMouseLeave={mouseLeave}
+        onMouseMove={mouseMove}
+        onMouseUp={mouseUp}
+      />
+      <ZoomPreview
+        {...zoomData}
+        eventMouseDown={eventMouseDown}
+        graphHeight={graphHeight}
+        graphWidth={graphWidth}
+      />
     </g>
   );
 };
---
+++ centreon/packages/ui/src/Graph/InteractionWithGraph/interactionWithGraphAtoms.ts
@@ -0,0 +1,3 @@
+import { atom } from 'jotai';
+
+export const eventMouseMovingAtom = atom<null | MouseEvent>(null);
