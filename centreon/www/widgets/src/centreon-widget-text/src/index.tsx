41884a2a61ea22b671669285d04a867f1a7d32c1
Tom Darneix <tomdar87@outlook.com>
2023-05-05 12:05:27 +0200

    Add Module on widgets

--- centreon/www/widgets/src/centreon-widget-text/src/index.tsx
+++ centreon/www/widgets/src/centreon-widget-text/src/index.tsx
@@ -1,16 +1,24 @@
-import { FluidTypography } from '@centreon/ui';
+import { createStore } from 'jotai';
 
-const Text = (): JSX.Element => {
+import { FluidTypography, Module } from '@centreon/ui';
+
+interface Props {
+  store: ReturnType<typeof createStore>;
+}
+
+const Text = ({ store }: Props): JSX.Element => {
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, minmax(60px, auto))'
-      }}
-    >
-      <FluidTypography text="Hello world" />
-      <FluidTypography text="Hello world" variant="h2" />
-    </div>
+    <Module maxSnackbars={1} seedName="text" store={store}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, minmax(60px, auto))'
+        }}
+      >
+        <FluidTypography text="Hello world" />
+        <FluidTypography text="Hello world" variant="h2" />
+      </div>
+    </Module>
   );
 };
 
--- centreon/www/widgets/src/centreon-widget-text2/src/index.tsx
+++ centreon/www/widgets/src/centreon-widget-text2/src/index.tsx
@@ -1,4 +1,8 @@
+import { createStore } from 'jotai';
+
 import { Typography, TextField, Box } from '@mui/material';
+
+import { Module } from '@centreon/ui';
 
 interface Options {
   input: string;
@@ -6,18 +10,21 @@ interface Options {
 interface Props {
   panelOptions?: Options;
   setPanelOptions: (options: Options) => void;
+  store: ReturnType<typeof createStore>;
 }
 
-const Text = ({ panelOptions, setPanelOptions }: Props): JSX.Element => {
+const Text = ({ panelOptions, setPanelOptions, store }: Props): JSX.Element => {
   const changeInput = (event): void => {
     setPanelOptions({ input: event.target.value });
   };
 
   return (
-    <Box>
-      <TextField value={panelOptions?.input} onChange={changeInput} />
-      <Typography>{panelOptions?.input}</Typography>
-    </Box>
+    <Module maxSnackbars={1} seedName="text2" store={store}>
+      <Box>
+        <TextField value={panelOptions?.input} onChange={changeInput} />
+        <Typography>{panelOptions?.input}</Typography>
+      </Box>
+    </Module>
   );
 };
 
