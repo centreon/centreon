centreon/centreon

commit 512e624f756d9d6ac5e9522a636a87ae391250f1

fix(clapi): duplicates in the table host_service_relation (#1386)

centreon/www/class/centreon-clapi/centreonServiceTemplate.class.php
@@ -942,11 +942,11 @@ class CentreonServiceTemplate extends CentreonObject
 
                         $relationTable[] = $tab[0];
                     }
-                    if ($matches[1] == "set") {
+                    if ($matches[1] === "set" || $matches[1] === "add") {
                         $relobj->delete(null, $serviceId);
                     }
 
-                    if ((strtolower($matches[2]) === 'hosttemplate') && (strtolower($matches[1]) === 'add')) {
+                    if ($matches[2] === 'hosttemplate' && $matches[1] === 'add') {
                         $existingRelationIds = $relobj->getTargetIdFromSourceId(
                             $relobj->getSecondKey(),
                             $relobj->getFirstKey(),

centreon/lib/Centreon/Object/Relation/Service/Template/Host.php
@@ -63,28 +63,6 @@ class Centreon_Object_Relation_Service_Template_Host extends Centreon_Object_Rel
     }
 
     /**
-     * Get target id from source id
-     *
-     * @param int $sourceKey
-     * @param int $targetKey
-     * @param array $sourceId
-     * @return array
-     */
-    public function getTargetIdFromSourceId($sourceKey, $targetKey, $sourceId)
-    {
-        if (!is_array($sourceId)) {
-            $sourceId = array($sourceId);
-        }
-        $sql = "SELECT $targetKey FROM $this->relationTable WHERE $sourceKey = ? ";
-        $result = $this->getResult($sql, $sourceId);
-        $tab = array();
-        foreach ($result as $rez) {
-            $tab[] = $rez[$targetKey];
-        }
-        return $tab;
-    }
-
-    /**
      * Get Merged Parameters from seperate tables
      *
      * @param array $firstTableParams

commit 33c00dfe86785c48f9ef9068ff4e01aca1a0845b

fix(clapi): gethosttemplate of service template
gethosttemplate has been broken by https://github.com/centreon/centreon/commit/512e624f756d9d6ac5e9522a636a87ae391250f1

centreon/www/class/centreon-clapi/centreonServiceTemplate.class.php
@@ -913,11 +913,19 @@ class CentreonServiceTemplate extends CentreonObject
                 $relobj = new $relclass($this->dependencyInjector);
                 $obj = new $class($this->dependencyInjector);
                 if ($matches[1] == "get") {
-                    $tab = $relobj->getTargetIdFromSourceId(
-                        $relobj->getFirstKey(),
-                        $relobj->getSecondKey(),
-                        $serviceId
-                    );
+                    if ($matches[2] === 'hosttemplate') {
+                        $tab = $relobj->getTargetIdFromSourceId(
+                            $relobj->getSecondKey(),
+                            $relobj->getFirstKey(),
+                            $serviceId
+                        );
+                    } else {
+                        $tab = $relobj->getTargetIdFromSourceId(
+                            $relobj->getFirstKey(),
+                            $relobj->getSecondKey(),
+                            $serviceId
+                        );
+                    }
                     echo "id" . $this->delim . "name" . "\n";
                     foreach ($tab as $value) {
                         $tmp = $obj->getParameters($value, array($obj->getUniqueLabelField()));