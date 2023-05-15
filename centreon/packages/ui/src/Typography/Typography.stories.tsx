ddaefd1d15fc1514be6a148aa595cf7f101ed3cc
Tom Darneix <tomdar87@outlook.com>
2023-05-15 22:09:15 +0200

    Fix typo

--- centreon/packages/ui/src/Typography/Typography.stories.tsx
+++ centreon/packages/ui/src/Typography/Typography.stories.tsx
@@ -1,16 +1,16 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import TypographtStory from './story.utils';
+import TypographyStory from './story.utils';
 
 export default {
   argTypes: {},
-  component: TypographtStory,
+  component: TypographyStory,
   title: 'Typography'
-} as ComponentMeta<typeof TypographtStory>;
+} as ComponentMeta<typeof TypographyStory>;
 
-const TemplateTypographtStory: ComponentStory<typeof TypographtStory> = (
+const TemplateTypographtStory: ComponentStory<typeof TypographyStory> = (
   args
-) => <TypographtStory {...args} />;
+) => <TypographyStory {...args} />;
 
 export const typography = (): JSX.Element => (
   <TemplateTypographtStory text="Hello world" />
