48cf72812c1be123d6aa549c2674d817b87a5eb3
Tom Darneix <tomdar87@outlook.com>
2023-05-16 10:01:38 +0200

    enh(core): Migrate to MUI X v6 (#1391)

--- centreon/packages/ui/src/InputField/Text/index.tsx
+++ centreon/packages/ui/src/InputField/Text/index.tsx
@@ -12,7 +12,7 @@ import {
   Typography
 } from '@mui/material';
 
-import getNormalizedId from '../../utils/getNormalizedId';
+import { getNormalizedId } from '../../utils';
 
 import useAutoSize from './useAutoSize';
 
