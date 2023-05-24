253797e47b1713769ac6f6b1224fac16cae679dc
Tom Darneix <tomdar87@outlook.com>
2023-05-24 14:29:04 +0200

    Remove divider

--- centreon/packages/ui/src/components/Header/Header.tsx
+++ centreon/packages/ui/src/components/Header/Header.tsx
@@ -18,7 +18,6 @@ const Header = ({ title, nav }: HeaderProps): ReactElement => {
         <Typography variant="h5">{title}</Typography>
         {nav && <nav>{nav}</nav>}
       </div>
-      <Divider className={classes.divider} orientation="horizontal" />
     </header>
   );
 };
