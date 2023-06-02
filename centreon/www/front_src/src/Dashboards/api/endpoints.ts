aea8d0baa437c24a8ad2181a2bee123a94900431
Tom Darneix <tomdar87@outlook.com>
2023-06-02 18:06:50 +0200

    Update centreon/www/front_src/src/Dashboards/api/endpoints.ts
    Co-authored-by: Giovanni Carnel <479046+g10@users.noreply.github.com>

--- centreon/www/front_src/src/Dashboards/api/endpoints.ts
+++ centreon/www/front_src/src/Dashboards/api/endpoints.ts
@@ -1,3 +1,4 @@
-export const baseEndpoint = 'http://localhost:5005/centreon/api/latest';
+export const baseEndpoint = './api/latest';
+
 
 export const dashboardsEndpoint = `${baseEndpoint}/configuration/dashboards`;
