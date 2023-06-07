986e6c691998451e9fbf59c2f3a708fd556e1b6a
giovanni carnel <gcarnel@centreon.com>
2023-06-07 09:46:04 +0100

    chore: fix jest.config.js formatting

--- centreon/packages/ui/jest.config.js
+++ centreon/packages/ui/jest.config.js
@@ -1,24 +1,24 @@
-const baseConfig = require("../js-config/jest");
+const baseConfig = require('../js-config/jest');
 
 module.exports = {
   ...baseConfig,
   moduleNameMapper: {
-    "\\.(s?css|png|svg)$": "identity-obj-proxy",
-    "^@centreon/ui/fonts(.*)$": "<rootDir>/public/fonts$1",
-    "^react($|/.+)": "<rootDir>/../../node_modules/react$1",
+    '\\.(s?css|png|svg)$': 'identity-obj-proxy',
+    '^@centreon/ui/fonts(.*)$': '<rootDir>/public/fonts$1',
+    '^react($|/.+)': '<rootDir>/../../node_modules/react$1'
   },
-  reporters: ["default", ["jest-junit", { outputName: "junit.xml" }]],
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
+  reporters: ['default', ['jest-junit', { outputName: 'junit.xml' }]],
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   setupFilesAfterEnv: [
-    "<rootDir>/test/setupTests.js",
-    "@testing-library/jest-dom/extend-expect",
+    '<rootDir>/test/setupTests.js',
+    '@testing-library/jest-dom/extend-expect'
   ],
-  testResultsProcessor: "jest-junit",
+  testResultsProcessor: 'jest-junit',
   transform: {
     ...baseConfig.transform,
-    "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
+    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx'
   },
   transformIgnorePatterns: [
-    "<rootDir>/../../node_modules/(?!d3-array|d3-scale|d3-interpolation|d3-interpolate)",
-  ],
+    '<rootDir>/../../node_modules/(?!d3-array|d3-scale|d3-interpolation|d3-interpolate)'
+  ]
 };
