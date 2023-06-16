e3f9a35f115991a24734f4707e1f5009cb70693b
Thebarda <tomdar87@outlook.com>
2023-06-16 09:07:05 +0000

    Add react query dev tools

--- centreon/www/front_src/src/Main/Provider.tsx
+++ centreon/www/front_src/src/Main/Provider.tsx
@@ -3,6 +3,7 @@ import { lazy, useEffect } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { not, startsWith, tail } from 'ramda';
 import { createStore } from 'jotai';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { Module } from '@centreon/ui';
 
@@ -69,7 +70,10 @@ const Provider = (): JSX.Element | null => {
 
   return (
     <Module maxSnackbars={2} seedName="centreon" store={store}>
-      <RouterProvider router={router} />
+      <>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </>
     </Module>
   );
 };
