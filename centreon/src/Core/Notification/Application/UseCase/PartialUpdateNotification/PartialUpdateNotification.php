ca69430beb835a59c6af63691619f40d9fa05f60
dmyios <diosypenko@centreon.com>
2023-06-21 16:22:49 +0200

    started implementing PATCH endpoint

---
+++ centreon/src/Core/Notification/Application/UseCase/PartialUpdateNotification/PartialUpdateNotification.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : user@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Notification\Application\UseCase\PartialUpdateNotification;
+
+use Centreon\Domain\Contact\Contact;
+use Centreon\Domain\Contact\Interfaces\ContactInterface;
+use Centreon\Domain\Log\LoggerTrait;
+use CentreonMap\Common\Application\UseCase\ForbiddenResponse;
+use Core\Application\Common\UseCase\ErrorResponse;
+use Core\Application\Common\UseCase\ResponseStatusInterface;
+use Core\Notification\Application\Exception\NotificationException;
+use Core\Notification\Application\Repository\ReadNotificationRepositoryInterface;
+use Core\Notification\Application\Repository\WriteNotificationRepositoryInterface;
+
+final class PartialUpdateNotification
+{
+    use LoggerTrait;
+
+    /**
+     * @param ContactInterface $contact
+     * @param ReadNotificationRepositoryInterface $readRepository
+     * @param WriteNotificationRepositoryInterface $writeRepository
+     */
+    public function __construct(
+        private readonly ContactInterface $contact,
+        private readonly ReadNotificationRepositoryInterface $readRepository,
+        private readonly WriteNotificationRepositoryInterface $writeRepository
+    ) {
+    }
+
+    /**
+     * @param PartialUpdateNotificationRequest $request
+     * @param PartialUpdateNotificationPresenterInterface $presenter
+     * @param int $notificationId
+     */
+    public function __invoke(
+        PartialUpdateNotificationRequest $request,
+        PartialUpdateNotificationPresenterInterface $presenter,
+        int $notificationId
+    ): void {
+        try {
+            if ($this->contactCanExecuteUseCase()) {
+                $response = $this->partiallyUpdateNotification($request, $notificationId);
+            } else {
+                $this->error(
+                    "User doesn't have sufficient rights to delete notifications",
+                    ['user_id' => $this->contact->getId()]
+                );
+                $response = new ForbiddenResponse(NotificationException::partialUpdateNotAllowed()->getMessage());
+            }
+        } catch (\Throwable $ex) {
+            $this->error($ex->getMessage(), ['trace' => $ex->getTraceAsString()]);
+            $response = new ErrorResponse(NotificationException::errorWhilePartiallyUpdatingObject());
+        }
+
+        $presenter->presentResponse($response);
+    }
+
+    /**
+     * @return bool
+     */
+    private function contactCanExecuteUseCase(): bool
+    {
+        return $this->contact->hasTopologyRole(Contact::ROLE_CONFIGURATION_NOTIFICATIONS_READ_WRITE);
+    }
+
+    /**
+     * @param int $notificationId
+     *
+     * @throws \Throwable
+     *
+     * @return ResponseStatusInterface
+     */
+    private function partiallyUpdateNotification(
+        PartialUpdateNotificationRequest $resuest,
+        int $notificationId
+    ): ResponseStatusInterface {
+        if (!($notification = $this->readRepository->findById($notificationId))) {
+            $this->error('Notification not found', ['notififcation_id' => $notificationId])
+        }
+    }
+}
---
+++ centreon/src/Core/Notification/Infrastructure/API/PartialUpdateNotification/PartialUpdateNotificationController.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Notification\Infrastructure\API\PartialUpdateNotification;
+
+use Centreon\Domain\Log\LoggerTrait;
+use Symfony\Component\HttpFoundation\{Request, Response};
+use Centreon\Application\Controller\AbstractController;
+use Core\Application\Common\UseCase\{ErrorResponse, InvalidArgumentResponse};
+use Core\Notification\Application\Exception\NotificationException;
+use Core\Notification\Application\UseCase\PartialUpdateNotification\{
+    PartialUpdateNotification,
+    PartialUpdateNotificationRequest
+};
+
+final class PartialUpdateNotificationController extends AbstractController
+{
+    use LoggerTrait;
+
+    /**
+     * @param Request $request
+     * @param PartialUpdateNotification $useCase
+     * @param PartialUpdateNotificationPresenter $presenter
+     * @param int $notificationId
+     *
+     * @return Response
+     */
+    public function __invoke(
+        Request $request,
+        PartialUpdateNotification $useCase,
+        PartialUpdateNotificationPresenter $presenter,
+        int $notificationId
+    ): Response {
+        $this->denyAccessUnlessGrantedForApiConfiguration();
+
+        try {
+            $data = $this->validateAndRetrieveDataSent($request, __DIR__ . '/PartialUpdateNotificationSchema.json');
+
+            $requestDto = new PartialUpdateNotificationRequest();
+
+            if (\array_key_exists('is_activated', $data)) {
+                $requestDto->isActivated = $data['is_activated'];
+            }
+
+            $useCase($requestDto, $presenter, $notificationId);
+        } catch (\InvalidArgumentException $ex) {
+            $this->error($ex->getMessage(), ['trace' => $ex->getTraceAsString()]);
+            $presenter->setResponseStatus(new InvalidArgumentResponse($ex));
+        } catch (\Throwable $ex) {
+            $this->error($ex->getMessage(), ['trace' => $ex->getTraceAsString()]);
+            $presenter->setResponseStatus(
+                new ErrorResponse(NotificationException::errorWhilePartiallyUpdatingObject())
+            );
+        }
+
+        return $presenter->show();
+    }
+}
