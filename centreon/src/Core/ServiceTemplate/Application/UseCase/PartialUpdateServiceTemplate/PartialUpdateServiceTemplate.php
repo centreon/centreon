d5fce1d1c77ce98e99c6c903f93e26358636c6f8
Laurent Calvet <lcalvet@centreon.com>
2023-06-26 09:42:29 +0200

    enh (api/service template): Update service categories from the PATCH API endpoint (#1738)

--- centreon/src/Core/ServiceTemplate/Application/UseCase/PartialUpdateServiceTemplate/PartialUpdateServiceTemplate.php
+++ centreon/src/Core/ServiceTemplate/Application/UseCase/PartialUpdateServiceTemplate/PartialUpdateServiceTemplate.php
@@ -35,18 +35,30 @@ use Core\Application\Common\UseCase\NotFoundResponse;
 use Core\Application\Common\UseCase\PresenterInterface;
 use Core\Common\Application\Type\NoValue;
 use Core\HostTemplate\Application\Repository\ReadHostTemplateRepositoryInterface;
+use Core\Security\AccessGroup\Application\Repository\ReadAccessGroupRepositoryInterface;
+use Core\Security\AccessGroup\Domain\Model\AccessGroup;
+use Core\ServiceCategory\Application\Repository\ReadServiceCategoryRepositoryInterface;
+use Core\ServiceCategory\Application\Repository\WriteServiceCategoryRepositoryInterface;
+use Core\ServiceCategory\Domain\Model\ServiceCategory;
 use Core\ServiceTemplate\Application\Exception\ServiceTemplateException;
 use Core\ServiceTemplate\Application\Repository\ReadServiceTemplateRepositoryInterface;
 use Core\ServiceTemplate\Application\Repository\WriteServiceTemplateRepositoryInterface;
+use Utility\Difference\BasicDifference;
 
 class PartialUpdateServiceTemplate
 {
     use LoggerTrait;
 
+    /** @var AccessGroup[] */
+    private array $accessGroups;
+
     public function __construct(
         private readonly ReadServiceTemplateRepositoryInterface $readRepository,
         private readonly WriteServiceTemplateRepositoryInterface $writeRepository,
         private readonly ReadHostTemplateRepositoryInterface $readHostTemplateRepository,
+        private readonly ReadServiceCategoryRepositoryInterface $readServiceCategoryRepository,
+        private readonly WriteServiceCategoryRepositoryInterface $writeServiceCategoryRepository,
+        private readonly ReadAccessGroupRepositoryInterface $readAccessGroupRepository,
         private readonly ContactInterface $user,
         private readonly DataStorageEngineInterface $storageEngine,
     ) {
@@ -77,10 +89,12 @@ class PartialUpdateServiceTemplate
                 return;
             }
 
-            if (! ($request->hostTemplates instanceof NoValue)) {
-                $this->assertHostTemplateIds($request);
-                $this->linkServiceTemplateToHostTemplates($request);
+            if (! $this->user->isAdmin()) {
+                $this->accessGroups = $this->readAccessGroupRepository->findByContact($this->user);
             }
+
+            $this->assertAllProperties($request);
+            $this->updatePropertiesInTransaction($request);
 
             $presenter->setResponseStatus(new NoContentResponse());
         } catch (ServiceTemplateException $ex) {
@@ -109,9 +123,33 @@ class PartialUpdateServiceTemplate
         if (is_array($request->hostTemplates)) {
             $hostTemplateIds = array_unique($request->hostTemplates);
             $hostTemplateIdsFound = $this->readHostTemplateRepository->findAllExistingIds($hostTemplateIds);
-            if ([] !== ($diff = array_diff($hostTemplateIds, $hostTemplateIdsFound))) {
-                throw ServiceTemplateException::idsDoesNotExist('host_templates', $diff);
+            if ([] !== ($idsNotFound = array_diff($hostTemplateIds, $hostTemplateIdsFound))) {
+                throw ServiceTemplateException::idsDoesNotExist('host_templates', $idsNotFound);
             }
+        }
+    }
+
+    /**
+     * @param int[] $serviceCategoriesIds
+     *
+     * @throws ServiceTemplateException
+     * @throws \Throwable
+     */
+    private function assertServiceCategories(array $serviceCategoriesIds): void
+    {
+        if ($this->user->isAdmin()) {
+            $serviceCategoriesIdsFound = $this->readServiceCategoryRepository->findAllExistingIds(
+                $serviceCategoriesIds
+            );
+        } else {
+            $serviceCategoriesIdsFound = $this->readServiceCategoryRepository->findAllExistingIdsByAccessGroups(
+                $serviceCategoriesIds,
+                $this->accessGroups
+            );
+        }
+
+        if ([] !== ($idsNotFound = array_diff($serviceCategoriesIds, $serviceCategoriesIdsFound))) {
+            throw ServiceTemplateException::idsDoesNotExist('service_categories', $idsNotFound);
         }
     }
 
@@ -122,28 +160,106 @@ class PartialUpdateServiceTemplate
      */
     private function linkServiceTemplateToHostTemplates(PartialUpdateServiceTemplateRequest $request): void
     {
-        if (is_array($request->hostTemplates)) {
-            $this->storageEngine->startTransaction();
-            try {
-                $this->debug('Start transaction');
-                $this->info('Unlink existing host templates from service template', [
-                    'service_template_id' => $request->id,
-                    'host_templates' => $request->hostTemplates,
-                ]);
-                $this->writeRepository->unlinkHostTemplates($request->id);
-                $this->info('Link host templates to service template', [
-                    'service_template_id' => $request->id,
-                    'host_templates' => $request->hostTemplates,
-                ]);
-                $this->writeRepository->linkToHostTemplates($request->id, $request->hostTemplates);
-                $this->debug('Commit transaction');
-                $this->storageEngine->commitTransaction();
-            } catch (\Throwable $ex) {
-                $this->debug('Rollback transaction');
-                $this->storageEngine->rollbackTransaction();
+        if (! is_array($request->hostTemplates)) {
+            return;
+        }
 
-                throw $ex;
-            }
+        $this->info('Unlink existing host templates from service template', [
+            'service_template_id' => $request->id,
+            'host_templates' => $request->hostTemplates,
+        ]);
+        $this->writeRepository->unlinkHosts($request->id);
+        $this->info('Link host templates to service template', [
+            'service_template_id' => $request->id,
+            'host_templates' => $request->hostTemplates,
+        ]);
+        $this->writeRepository->linkToHosts($request->id, $request->hostTemplates);
+    }
+
+    /**
+     * @param PartialUpdateServiceTemplateRequest $request
+     *
+     * @throws \Throwable
+     */
+    private function linkServiceTemplateToServiceCategories(PartialUpdateServiceTemplateRequest $request): void
+    {
+        if (! is_array($request->serviceCategories)) {
+            return;
+        }
+
+        if ($this->user->isAdmin()) {
+            $originalServiceCategories = $this->readServiceCategoryRepository->findByService($request->id);
+        } else {
+            $originalServiceCategories = $this->readServiceCategoryRepository->findByServiceAndAccessGroups(
+                $request->id,
+                $this->accessGroups
+            );
+        }
+        $this->info('Original service categories found', ['service_categories' => $originalServiceCategories]);
+
+        $originalServiceCategoriesIds = array_map(
+            static fn(ServiceCategory $serviceCategory): int => $serviceCategory->getId(),
+            $originalServiceCategories
+        );
+
+        $serviceCategoryDifferences = new BasicDifference(
+            $originalServiceCategoriesIds,
+            array_unique($request->serviceCategories)
+        );
+
+        $serviceCategoriesToAdd = $serviceCategoryDifferences->getAdded();
+        $serviceCategoriesToRemove = $serviceCategoryDifferences->getRemoved();
+
+        $this->info(
+            'Unlink existing service categories from service',
+            ['service_categories' => $serviceCategoriesToRemove]
+        );
+        $this->writeServiceCategoryRepository->unlinkFromService($request->id, $serviceCategoriesToRemove);
+
+        $this->info(
+            'Link existing service categories to service',
+            ['service_categories' => $serviceCategoriesToAdd]
+        );
+        $this->writeServiceCategoryRepository->linkToService($request->id, $serviceCategoriesToAdd);
+    }
+
+    /**
+     * @param PartialUpdateServiceTemplateRequest $request
+     *
+     * @throws ServiceTemplateException
+     * @throws \Throwable
+     */
+    private function updatePropertiesInTransaction(PartialUpdateServiceTemplateRequest $request): void
+    {
+        $this->debug('Start transaction');
+        $this->storageEngine->startTransaction();
+        try {
+            $this->linkServiceTemplateToHostTemplates($request);
+            $this->linkServiceTemplateToServiceCategories($request);
+
+            $this->debug('Commit transaction');
+            $this->storageEngine->commitTransaction();
+        } catch (\Throwable $ex) {
+            $this->debug('Rollback transaction');
+            $this->storageEngine->rollbackTransaction();
+
+            throw $ex;
+        }
+    }
+
+    /**
+     * @param PartialUpdateServiceTemplateRequest $request
+     *
+     * @throws ServiceTemplateException
+     * @throws \Throwable
+     */
+    private function assertAllProperties(PartialUpdateServiceTemplateRequest $request): void
+    {
+        if (! ($request->hostTemplates instanceof NoValue)) {
+            $this->assertHostTemplateIds($request);
+        }
+        if (! ($request->serviceCategories instanceof NoValue)) {
+            $this->assertServiceCategories($request->serviceCategories);
         }
     }
 }
