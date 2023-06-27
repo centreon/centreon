fe659c55895013f09abbe444538729069485d8e1
Arnaud Buathier <abuathier@centreon.com>
2023-06-27 10:07:11 +0200

    fix: PlatformController

--- centreon/src/Centreon/Application/Controller/PlatformController.php
+++ centreon/src/Centreon/Application/Controller/PlatformController.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,94 +23,63 @@ declare(strict_types=1);
 
 namespace Centreon\Application\Controller;
 
+use Centreon\Domain\Platform\Interfaces\PlatformServiceInterface;
+use Centreon\Domain\Platform\PlatformException;
+use Centreon\Domain\PlatformInformation\Model\PlatformInformationDtoValidator;
+use Centreon\Domain\PlatformInformation\UseCase\V20\UpdatePartiallyPlatformInformation;
 use Centreon\Domain\VersionHelper;
+use Core\Common\Infrastructure\FeatureFlags;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Centreon\Domain\Platform\PlatformException;
-use Centreon\Domain\PlatformInformation\UseCase\V20\UpdatePartiallyPlatformInformation;
-use Centreon\Domain\Platform\Interfaces\PlatformServiceInterface;
-use Centreon\Domain\PlatformInformation\Model\PlatformInformationDtoValidator;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * This controller is designed to manage API requests concerning the versions of the different modules, widgets on the
  * Centreon platform.
- *
- * @package Centreon\Application\Controller
  */
 class PlatformController extends AbstractController
 {
-    /**
-     * @var PlatformServiceInterface
-     */
-    private $informationService;
-
     public function __construct(
-        PlatformServiceInterface $informationService
+        private readonly PlatformServiceInterface $informationService,
+        private readonly FeatureFlags $flags
     ) {
-        $this->informationService = $informationService;
     }
 
     /**
      * Retrieves the version of modules, widgets, remote pollers from the Centreon Platform.
      *
-     * @return View
      * @throws PlatformException
+     *
+     * @return View
      */
-    public function getVersions(): View
+    public function getVersions(): Response
     {
         $webVersion = $this->informationService->getWebVersion();
         $modulesVersion = $this->informationService->getModulesVersion();
         $widgetsVersion = $this->informationService->getWidgetsVersion();
 
-        return $this->view(
+        return new JsonResponse(
             [
-                'web' => $this->extractVersion($webVersion),
-                'modules' => array_map(
-                    function ($version) {
-                        return $this->extractVersion($version);
-                    },
-                    $modulesVersion
-                ),
-                'widgets' => array_map(
-                    function ($version) {
-                        return $this->extractVersion($version);
-                    },
-                    $widgetsVersion
-                ),
-                'is_cloud_platform' => $_SERVER['IS_CLOUD_PLATFORM'] || false
+                'web' => (object) $this->extractVersion($webVersion),
+                'modules' => (object) array_map($this->extractVersion(...), $modulesVersion),
+                'widgets' => (object) array_map($this->extractVersion(...), $widgetsVersion),
+                'is_cloud_platform' => $this->flags->isCloudPlatform(),
+                'feature_flags' => (object) array_filter($this->flags->getAll()),
             ]
         );
     }
 
     /**
-     * Extract the major, minor and fix number from the version.
+     * Update the platform.
      *
-     * @param string $version Version to analyse (ex: 1.2.09)
-     * @return array<string, string> (ex: [ 'major' => '1', 'minor' => '2', 'fix' => '09'])
-     */
-    private function extractVersion(string $version): array
-    {
-        list($major, $minor, $fix) = explode(
-            '.',
-            VersionHelper::regularizeDepthVersion($version),
-            3
-        );
-        return [
-            'version' => $version,
-            'major' => $major,
-            'minor' => $minor,
-            'fix' => $fix
-        ];
-    }
-
-    /**
-     * Update the platform
      * @param Request $request
      * @param UpdatePartiallyPlatformInformation $updatePartiallyPlatformInformation
-     * @return View
+     *
      * @throws \Throwable
+     *
+     * @return View
      */
     public function updatePlatform(
         Request $request,
@@ -123,16 +92,40 @@ class PlatformController extends AbstractController
                 new PlatformInformationDtoValidator(
                     $this->getParameter('centreon_path')
                     . 'config/json_validator/latest/Centreon/PlatformInformation/Update.json'
-                )
+                ),
             ]
         );
 
         $request = json_decode((string) $request->getContent(), true);
-        if (!is_array($request)) {
+        if (! \is_array($request)) {
             throw new BadRequestHttpException(_('Error when decoding sent data'));
         }
 
         $updatePartiallyPlatformInformation->execute($request);
+
         return $this->view(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Extract the major, minor and fix number from the version.
+     *
+     * @param string $version Version to analyse (ex: 1.2.09)
+     *
+     * @return array{
+     *     version: string,
+     *     major: string,
+     *     minor: string,
+     *     fix: string
+     * } (ex: [ 'version' => '1.2.09', 'major' => '1', 'minor' => '2', 'fix' => '09'])
+     */
+    private function extractVersion(string $version): array
+    {
+        [$major, $minor, $fix] = explode(
+            '.',
+            VersionHelper::regularizeDepthVersion($version),
+            3
+        );
+
+        return compact('version', 'major', 'minor', 'fix');
     }
 }
