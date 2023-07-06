694f227433a0736d27641eea94097b5e2bc4d12a
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2023-07-06 16:35:42 +0200

    enh(notification): handle contact groups in update notification endpoint (#1841)

--- centreon/src/Core/Notification/Infrastructure/Repository/DbWriteNotificationRepository.php
+++ centreon/src/Core/Notification/Infrastructure/Repository/DbWriteNotificationRepository.php
@@ -220,6 +220,52 @@ class DbWriteNotificationRepository extends AbstractRepositoryRDB implements Wri
     /**
      * @inheritDoc
      */
+    public function deleteContactGroups(int $notificationId): void
+    {
+        $statement = $this->db->prepare($this->translateDbName(
+            <<<'SQL'
+                DELETE FROM `:db`.notification_contactgroup_relation
+                WHERE notification_id = :notificationId
+            SQL
+        ));
+        $statement->bindValue(':notificationId', $notificationId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteContactGroupsByNotificationAndContactGroupIds(
+        int $notificationId,
+        array $contactGroupsIds
+    ): void {
+        if ($contactGroupsIds === []) {
+            return;
+        }
+        $bindValues = [];
+        foreach ($contactGroupsIds as $contactGroupId) {
+            $bindValues[':contactgroup_' . $contactGroupId] = $contactGroupId;
+        }
+
+        $bindToken = implode(", ", array_keys($bindValues));
+
+        $statement = $this->db->prepare($this->translateDbName(
+            <<<SQL
+                    DELETE FROM `:db`.notification_contactgroup_relation
+                    WHERE notification_id = :notificationId
+                    AND contactgroup_id IN ($bindToken)
+                SQL
+        ));
+        $statement->bindValue(':notificationId', $notificationId, \PDO::PARAM_INT);
+        foreach($bindValues as $token => $value) {
+            $statement->bindValue($token, $value, \PDO::PARAM_INT);
+        }
+        $statement->execute();
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function delete(int $notificationId): int
     {
         $request = <<<'SQL'
