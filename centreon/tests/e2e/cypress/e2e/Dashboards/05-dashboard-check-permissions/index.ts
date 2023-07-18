add9904eb8f33b2b7c7b16d793dc1ac66e3f9161
Paul Oureib <poureib@centreon.com>
2023-07-18 17:53:54 +0200

    small fix

--- centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
+++ centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
@@ -11,11 +11,11 @@ before(() => {
     method: 'GET',
     url: '/centreon/api/latest/configuration/dashboards?'
   }).as('listAllDashboards');
-  cy.startWebContainer({ version: 'develop' });
-  /* cy.execInContainer({
+  cy.startWebContainer();
+  cy.execInContainer({
     command: `sed -i 's@"dashboard": 0@"dashboard": 3@' /usr/share/centreon/config/features.json`,
     name: Cypress.env('dockerName')
-  }); */
+  });
   cy.executeCommandsViaClapi(
     'resources/clapi/config-ACL/dashboard-check-permissions.json'
   );
