91911fbd357537dfe70fd9f4ca314500c5687343
Paul Oureib <poureib@centreon.com>
2023-07-20 17:18:32 +0200

    removed comments

--- centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
+++ centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
@@ -3,14 +3,14 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 import dashboards from '../../../fixtures/dashboards/check-permissions/dashboards.json';
 
 before(() => {
-  cy.startWebContainer({ version: 'develop' });
-  /* cy.execInContainer({
+  cy.startWebContainer();
+  cy.execInContainer({
     command: `sed -i 's@"dashboard": 0@"dashboard": 3@' /usr/share/centreon/config/features.json`,
     name: Cypress.env('dockerName')
   });
   cy.executeCommandsViaClapi(
     'resources/clapi/config-ACL/dashboard-check-permissions.json'
-  ); */
+  );
 });
 
 beforeEach(() => {
@@ -39,9 +39,9 @@ beforeEach(() => {
   });
 });
 
-/* after(() => {
+after(() => {
   cy.stopWebContainer();
-}); */
+});
 
 afterEach(() => {
   cy.requestOnDatabase({
