fc68ffdc5f01b7d5423bb8ee24f1b4a0a7b094aa
Paul Oureib <poureib@centreon.com>
2023-07-24 13:25:00 +0200

    refactored the two first scenarios + implemented the third one

--- centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions.feature
+++ centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions.feature
@@ -15,3 +15,12 @@ Scenario: Creating a new dashboard as an admin user
   Given an admin user on the dashboards library
   When the admin user creates a new dashboard
   Then the dashboard is created and is noted as the creation of the admin user
+
+Scenario: Accessing all dashboards as a non-admin dashboard administrator user
+  Given a non-admin user with the dashboard administrator role is logged in on a platform with dashboards
+  When the dashboard administrator user accesses the dashboards library
+  Then the dashboard administrator user can consult all the dashboards configured on the platform
+  When the dashboard administrator user clicks on a dashboard
+  Then the dashboard administrator user is redirected to the detail page for this dashboard
+  And the dashboard administrator user is allowed to access the edit mode for this dashboard
+  And the dashboard administrator user is allowed to update the dashboard's properties
--- centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
+++ centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
@@ -1,6 +1,9 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 import dashboards from '../../../fixtures/dashboards/check-permissions/dashboards.json';
+import dashboardAdministratorUser from '../../../fixtures/users/user-dashboard-administrator.json';
+import dashboardCreatorUser from '../../../fixtures/users/user-dashboard-creator.json';
+import dashboardViewerUser from '../../../fixtures/users/user-dashboard-viewer.json';
 
 before(() => {
   cy.startWebContainer();
@@ -27,20 +30,20 @@ beforeEach(() => {
     url: '/centreon/api/latest/configuration/dashboards'
   }).as('createDashboard');
   cy.loginByTypeOfUser({
-    jsonName: 'user-dashboard-administrator',
+    jsonName: dashboardAdministratorUser.login,
     loginViaApi: true
   });
   cy.insertDashboard({ ...dashboards.fromAdministratorUser });
   cy.logoutViaAPI();
   cy.loginByTypeOfUser({
-    jsonName: 'user-dashboard-creator',
+    jsonName: dashboardCreatorUser.login,
     loginViaApi: true
   });
   cy.insertDashboard({ ...dashboards.fromCreatorUser });
   cy.shareDashboardToUser({
     dashboardName: dashboards.fromCreatorUser.name,
     role: 'viewer',
-    userName: 'user-dashboard-viewer'
+    userName: dashboardViewerUser.login
   });
   cy.logoutViaAPI();
 });
@@ -76,6 +79,13 @@ Then(
       tag: 'button'
     })
       .contains(dashboards.fromAdministratorUser.name)
+      .should('exist');
+
+    cy.getByLabel({
+      label: 'view',
+      tag: 'button'
+    })
+      .contains(dashboards.fromCreatorUser.name)
       .should('exist');
   }
 );
@@ -157,7 +167,7 @@ When('the admin user creates a new dashboard', () => {
     dashboards.fromCurrentUser.name
   );
   cy.getByLabel({ label: 'Description', tag: 'textarea' }).type(
-    dashboards.fromCurrentUser.description
+    'by admin user'
   );
   cy.getByTestId({ testId: 'submit' }).click();
   cy.wait('@createDashboard');
@@ -172,12 +182,110 @@ Then(
     );
     cy.getByLabel({ label: 'page header description' }).should(
       'contain.text',
-      dashboards.fromCurrentUser.description
+      'by admin user'
     );
     cy.getByLabel({ label: 'Exit', tag: 'button' }).click();
     cy.getByLabel({ label: 'share', tag: 'button' }).click();
     cy.contains('admin admin').should('be.visible');
     cy.getByTestId({ testId: 'role-input' }).should('contain.text', 'editor');
     cy.getByLabel({ label: 'Cancel', tag: 'button' }).click();
+  }
+);
+
+Given(
+  'a non-admin user with the dashboard administrator role is logged in on a platform with dashboards',
+  () => {
+    cy.loginByTypeOfUser({
+      jsonName: dashboardAdministratorUser.login,
+      loginViaApi: false
+    });
+  }
+);
+
+When('the dashboard administrator user accesses the dashboards library', () => {
+  cy.visit(`${Cypress.config().baseUrl}/centreon/home/dashboards`);
+});
+
+Then(
+  'the dashboard administrator user can consult all the dashboards configured on the platform',
+  () => {
+    cy.getByLabel({
+      label: 'view',
+      tag: 'button'
+    })
+      .contains(dashboards.fromAdministratorUser.name)
+      .should('exist');
+
+    cy.getByLabel({
+      label: 'view',
+      tag: 'button'
+    })
+      .contains(dashboards.fromCreatorUser.name)
+      .should('exist');
+  }
+);
+
+When('the dashboard administrator user clicks on a dashboard', () => {
+  cy.getByLabel({
+    label: 'view',
+    tag: 'button'
+  })
+    .contains(dashboards.fromAdministratorUser.name)
+    .click();
+});
+
+Then(
+  'the dashboard administrator user is redirected to the detail page for this dashboard',
+  () => {
+    cy.location('pathname')
+      .should('include', '/dashboards/')
+      .invoke('split', '/')
+      .should('not.be.empty')
+      .then(Cypress._.last)
+      .then(Number)
+      .should('not.be', 'dashboards')
+      .should('be.a', 'number');
+
+    cy.getByLabel({ label: 'page header title' }).should(
+      'contain.text',
+      dashboards.fromAdministratorUser.name
+    );
+  }
+);
+
+Then(
+  'the dashboard administrator user is allowed to access the edit mode for this dashboard',
+  () => {
+    cy.getByTestId({ testId: 'edit_dashboard' }).click();
+    cy.location('search').should('include', 'edit=true');
+    cy.getByLabel({ label: 'add widget', tag: 'button' }).should('be.enabled');
+    cy.getByLabel({ label: 'Exit', tag: 'button' }).click();
+  }
+);
+
+Then(
+  "the dashboard administrator user is allowed to update the dashboard's properties",
+  () => {
+    cy.getByLabel({ label: 'edit', tag: 'button' }).click();
+
+    cy.getByLabel({ label: 'Name', tag: 'input' }).clear();
+    cy.getByLabel({ label: 'Name', tag: 'input' }).type(
+      `${dashboards.fromAdministratorUser.name}-edited`
+    );
+    cy.getByLabel({ label: 'Description', tag: 'textarea' }).clear();
+    cy.getByLabel({ label: 'Description', tag: 'textarea' }).type(
+      `${dashboards.fromAdministratorUser.description} and ${dashboardAdministratorUser.login}`
+    );
+
+    cy.getByLabel({ label: 'Update', tag: 'button' }).should('be.enabled');
+    cy.getByLabel({ label: 'Update', tag: 'button' }).click();
+
+    cy.reload();
+    cy.contains(`${dashboards.fromAdministratorUser.name}-edited`).should(
+      'exist'
+    );
+    cy.contains(
+      `${dashboards.fromAdministratorUser.description} and ${dashboardAdministratorUser.login}`
+    ).should('exist');
   }
 );
