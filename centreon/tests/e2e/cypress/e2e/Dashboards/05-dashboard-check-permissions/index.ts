5fc08c07fe742276970c501e0d9a1c6ed485ce56
Paul Oureib <poureib@centreon.com>
2023-07-25 10:50:31 +0200

    decommented code that wasn't meant to be commented

--- centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
+++ centreon/tests/e2e/cypress/e2e/Dashboards/05-dashboard-check-permissions/index.ts
@@ -7,13 +7,13 @@ import dashboardViewerUser from '../../../fixtures/users/user-dashboard-viewer.j
 
 before(() => {
   cy.startWebContainer();
-  /* cy.execInContainer({
+  cy.execInContainer({
     command: `sed -i 's@"dashboard": 0@"dashboard": 3@' /usr/share/centreon/config/features.json`,
     name: Cypress.env('dockerName')
   });
   cy.executeCommandsViaClapi(
     'resources/clapi/config-ACL/dashboard-check-permissions.json'
-  ); */
+  );
 });
 
 beforeEach(() => {
@@ -48,9 +48,9 @@ beforeEach(() => {
   cy.logoutViaAPI();
 });
 
-/* after(() => {
+after(() => {
   cy.stopWebContainer();
-}); */
+});
 
 afterEach(() => {
   cy.visit(`${Cypress.config().baseUrl}/centreon/home/dashboards`);
