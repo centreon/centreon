f9357af1c136946179bef64339e8b4b3473c78d5
cbadr-ext <cbadr-ext@centreon.com>
2023-07-25 11:07:53 +0100

    upgrade and fix legacy test

--- centreon/src/Core/Security/Authentication/Application/UseCase/Login/Login.php
+++ centreon/src/Core/Security/Authentication/Application/UseCase/Login/Login.php
@@ -132,6 +132,7 @@ final class Login
         } catch (AuthenticationException $e) {
             $this->error('An error occurred during authentication', ['trace' => (string) $e]);
             $presenter->setResponseStatus(new UnauthorizedResponse($e->getMessage()));
+
             return;
         } catch (AclConditionsException $e) {
             $this->error('An error occured while matching your ACL conditions', ['trace' => (string) $e]);
@@ -139,10 +140,12 @@ final class Login
         } catch (AuthenticationConditionsException $ex) {
             $this->error('An error occured while matching your authentication conditions', ['trace' => (string) $ex]);
             $presenter->setResponseStatus(new ErrorAuthenticationConditionsResponse($ex->getMessage()));
+
             return;
         } catch (\Throwable $ex) {
             $this->error('An error occurred during authentication', ['trace' => (string) $ex]);
             $presenter->setResponseStatus(new ErrorResponse('An error occurred during authentication'));
+            
             return;
         }
     }
