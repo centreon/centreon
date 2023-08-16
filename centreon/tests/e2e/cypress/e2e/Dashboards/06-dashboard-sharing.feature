aa4538d4fa4c641929766884c9d72284d22e96c1
Paul Oureib <poureib@centreon.com>
2023-08-16 17:01:54 +0200

    added implementation of third scenario

--- centreon/tests/e2e/cypress/e2e/Dashboards/06-dashboard-sharing.feature
+++ centreon/tests/e2e/cypress/e2e/Dashboards/06-dashboard-sharing.feature
@@ -3,16 +3,25 @@ Feature: As a Centreon User with dashboard administration rights,
   so I can provision accesses or re-direct ownership if a user is not supposed to use Centreon anymore
 
 Scenario: Accessing the sharing list of a dashboard
-    Given a non-admin user who is on a list of shared dashboards
-    When the user selects the share option on a dashboard
-    Then the user is redirected to the sharing list of the dashboard
-    And the creator of the dashboard is listed as its sole editor
+  Given a non-admin user who is on a list of shared dashboards
+  When the user selects the share option on a dashboard
+  Then the user is redirected to the sharing list of the dashboard
+  And the creator of the dashboard is listed as its sole editor
 
 Scenario: Adding a viewer user to a dashboard
-    Given a non-admin user who has update rights on a dashboard
-    When the editor user sets another user as a viewer on the dashboard
-    Then the viewer user is listed as a viewer in the dashboard's share list
-    When the viewer user logs in on the platform
-    Then the dashboard is featured in the viewer user's dashboards library
-    When the viewer user clicks on the dashboard
-    Then the viewer user can visualize the dashboard's layout but cannot update or share it
+  Given a non-admin user who has update rights on a dashboard
+  When the editor user sets another user as a viewer on the dashboard
+  Then the viewer user is listed as a viewer in the dashboard's share list
+  When the viewer user logs in on the platform
+  Then the dashboard is featured in the viewer user's dashboards library
+  When the viewer user clicks on the dashboard
+  Then the viewer user can visualize the dashboard's layout but cannot share it or update its properties
+
+Scenario: Adding an editor user to a dashboard
+  Given a non-admin user with the dashboard administrator role is logged in on a platform with dashboards
+  When the dashboard administrator user sets another user as a second editor on a dashboard
+  Then the second editor user is listed as an editor in the dashboard's share list
+  When the second editor user logs in on the platform
+  Then the dashboard is featured in the second editor user's dashboards library
+  When the second editor user clicks on the dashboard
+  Then the second editor can visualize the dashboard's layout and can share it or update its properties
--- centreon/tests/e2e/cypress/e2e/Dashboards/06-dashboard-sharing/index.ts
+++ centreon/tests/e2e/cypress/e2e/Dashboards/06-dashboard-sharing/index.ts
@@ -142,9 +142,12 @@ Then(
     cy.get('*[class^="MuiList-root"]')
       .eq(1)
       .children()
-      .contains('user-dashboard-viewer')
+      .contains(dashboardViewerUser.login)
       .should('exist');
-
+    cy.getByTestId({ testId: 'role-input' })
+      .eq(0)
+      .contains('viewer')
+      .should('exist');
     cy.get('[data-state="added"]').should('not.exist');
     cy.getByLabel({ label: 'Cancel', tag: 'button' }).click();
     cy.logout();
@@ -182,7 +185,7 @@ When('the viewer user clicks on the dashboard', () => {
 });
 
 Then(
-  "the viewer user can visualize the dashboard's layout but cannot update or share it",
+  "the viewer user can visualize the dashboard's layout but cannot share it or update its properties",
   () => {
     cy.location('pathname')
       .should('include', '/dashboards/')
@@ -195,5 +198,113 @@ Then(
 
     cy.getByTestId({ testId: 'edit' }).should('not.exist');
     cy.getByTestId({ testId: 'share' }).should('not.exist');
+  }
+);
+
+Given(
+  'a non-admin user with the dashboard administrator role is logged in on a platform with dashboards',
+  () => {
+    cy.loginByTypeOfUser({
+      jsonName: dashboardAdministratorUser.login,
+      loginViaApi: false
+    });
+
+    cy.visit(`${Cypress.config().baseUrl}/centreon/home/dashboards`);
+  }
+);
+
+When(
+  'the dashboard administrator user sets another user as a second editor on a dashboard',
+  () => {
+    cy.getByLabel({
+      label: 'view',
+      tag: 'button'
+    })
+      .contains(dashboards.fromDashboardAdministratorUser.name)
+      .click();
+    cy.getByLabel({ label: 'share', tag: 'button' }).click();
+    cy.getByLabel({ label: 'Open', tag: 'button' }).click();
+    cy.contains(dashboardCreatorUser.login).click();
+    cy.getByTestId({ testId: 'add' }).should('be.enabled');
+    cy.getByTestId({ testId: 'role-input' }).eq(0).click();
+    cy.get('[role="listbox"]').contains('editor').click();
+    cy.getByTestId({ testId: 'add' }).click();
+
+    cy.get('*[class^="MuiList-root"]')
+      .eq(1)
+      .children()
+      .its('length')
+      .should('eq', 2);
+    cy.get('*[class^="MuiList-root"]')
+      .eq(1)
+      .children()
+      .eq(0)
+      .should('contain', `${dashboardCreatorUser.login}`);
+
+    cy.getByLabel({ label: 'Update', tag: 'button' }).click();
+  }
+);
+
+Then(
+  "the second editor user is listed as an editor in the dashboard's share list",
+  () => {
+    cy.getByLabel({ label: 'share', tag: 'button' }).click();
+    cy.get('*[class^="MuiList-root"]')
+      .eq(1)
+      .children()
+      .contains(dashboardCreatorUser.login)
+      .should('exist');
+
+    cy.get('[data-state="added"]').should('not.exist');
+    cy.getByLabel({ label: 'Cancel', tag: 'button' }).click();
+    cy.logout();
+    cy.getByLabel({ label: 'Alias', tag: 'input' }).should('exist');
+  }
+);
+
+When('the second editor user logs in on the platform', () => {
+  cy.loginByTypeOfUser({
+    jsonName: dashboardCreatorUser.login,
+    loginViaApi: false
+  });
+
+  cy.visit(`${Cypress.config().baseUrl}/centreon/home/dashboards`);
+});
+
+Then(
+  "the dashboard is featured in the second editor user's dashboards library",
+  () => {
+    cy.getByLabel({
+      label: 'view',
+      tag: 'button'
+    })
+      .contains(dashboards.fromDashboardAdministratorUser.name)
+      .should('exist');
+  }
+);
+
+When('the second editor user clicks on the dashboard', () => {
+  cy.getByLabel({
+    label: 'view',
+    tag: 'button'
+  })
+    .contains(dashboards.fromDashboardCreatorUser.name)
+    .click();
+});
+
+Then(
+  "the second editor can visualize the dashboard's layout and can share it or update its properties",
+  () => {
+    cy.location('pathname')
+      .should('include', '/dashboards/')
+      .invoke('split', '/')
+      .should('not.be.empty')
+      .then(last)
+      .then(Number)
+      .should('not.be', 'dashboards')
+      .should('be.a', 'number');
+
+    cy.getByTestId({ testId: 'edit' }).should('be.enabled');
+    cy.getByTestId({ testId: 'share' }).should('be.enabled');
   }
 );
