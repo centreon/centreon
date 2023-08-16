16edee5b97d82a8b129f6043ad9c9af0d164c4d9
Arnaud Buathier <abuathier@centreon.com>
2023-08-16 11:03:56 +0200

    try fix front

---
+++ centreon/www/front_src/src/Main/usePlatformFeatures.ts
@@ -0,0 +1,36 @@
+import { useCallback } from 'react';
+
+import { useAtom } from 'jotai';
+
+import { getData, useRequest } from '@centreon/ui';
+
+import { platformFeaturesEndpoint } from '../api/endpoint';
+import { PlatformFeatures } from '../api/models';
+import { platformFeaturesDecoder } from '../api/decoders';
+
+import { platformFeaturesAtom } from './atoms/platformFeaturesAtom';
+
+interface UsePlatformFeaturesState {
+  getPlatformFeatures: () => void;
+}
+
+const usePlatformFeatures = (): UsePlatformFeaturesState => {
+  const { sendRequest: sendPlatformFeatures } = useRequest<PlatformFeatures>({
+    decoder: platformFeaturesDecoder,
+    request: getData
+  });
+
+  const [, setPlatformFeatures] = useAtom(platformFeaturesAtom);
+
+  const getPlatformFeatures = useCallback((): void => {
+    sendPlatformFeatures({ endpoint: platformFeaturesEndpoint }).then(
+      setPlatformFeatures
+    );
+  }, []);
+
+  return {
+    getPlatformFeatures
+  };
+};
+
+export default usePlatformFeatures;
