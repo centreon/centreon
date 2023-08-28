117fa40513433c685f50bdd55a7438711ae403b2
Kevin Duret <kduret@centreon.com>
2023-08-28 13:19:20 +0200

    fix(test): fix dashboard check permission flaky test (#2072)

--- centreon/tests/e2e/cypress/support/commands.ts
+++ centreon/tests/e2e/cypress/support/commands.ts
@@ -121,13 +121,6 @@ Cypress.Commands.add('logoutViaAPI', (): Cypress.Chainable => {
     .getByLabel({ label: 'Alias', tag: 'input' });
 });
 
-Cypress.Commands.add('logoutViaAPI', (): Cypress.Chainable => {
-  return cy.request({
-    method: 'GET',
-    url: '/centreon/authentication/logout'
-  });
-});
-
 Cypress.Commands.add('removeACL', (): Cypress.Chainable => {
   return cy.setUserTokenApiV1().then(() => {
     cy.executeActionViaClapi({
