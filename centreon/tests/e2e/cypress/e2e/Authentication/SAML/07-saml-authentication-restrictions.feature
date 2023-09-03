f9723d6fbbd55688257195e82edfec5988af9803
YAHIAOUI Hamza <hyahiaoui-ext@centreon.com>
2023-09-03 19:38:22 +0100

    add implementation to conditional auth saml

---
+++ centreon/tests/e2e/cypress/e2e/Authentication/SAML/07-saml-authentication-restrictions.feature
@@ -0,0 +1,9 @@
+Feature: SAML authentication
+    As an admin of Centreon Platform
+    I want to be able to make use of an external authentication provider
+    So that Platform users can use existing authentication services to authenticate
+
+Scenario: Authorize access to Centreon application 
+    Given an administrator is logged on the platform
+    When the administrator sets valid settings in the authentication conditions and saves
+    Then the users can access to Centreon UI only if all conditions matche
---
+++ centreon/tests/e2e/cypress/e2e/Authentication/SAML/07-saml-authentication-restrictions/index.ts
@@ -0,0 +1,103 @@
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
+
+import {
+  configureSAML,
+  initializeSAMLUser,
+  navigateToSAMLConfigPage
+} from '../common';
+
+before(() => {
+  cy.startWebContainer()
+    .startOpenIdProviderContainer()
+    .then(() => {
+      initializeSAMLUser();
+    });
+});
+
+beforeEach(() => {
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/internal.php?object=centreon_topology&action=navigationList'
+  }).as('getNavigationList');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/latest/administration/authentication/providers/saml'
+  }).as('getSAMLProvider');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/latest/authentication/providers/configurations'
+  }).as('getCentreonAuthConfigs');
+  cy.intercept({
+    method: 'PUT',
+    url: '/centreon/api/latest/administration/authentication/providers/saml'
+  }).as('updateSAMLProvider');
+  cy.intercept({
+    method: 'POST',
+    url: '/centreon/api/latest/authentication/providers/configurations/local'
+  }).as('postLocalAuthentification');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/include/common/userTimezone.php'
+  }).as('getTimeZone');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/latest/configuration/access-groups?page=1&sort_by=%7B%22name%22%3A%22ASC%22%7D&search=%7B%22%24and%22%3A%5B%5D%7D'
+  }).as('getListAccessGroup');
+});
+
+Given('an administrator is logged on the platform', () => {
+  cy.loginByTypeOfUser({ jsonName: 'admin' });
+});
+
+When(
+  'the administrator sets valid settings in the authentication conditions and saves',
+  () => {
+    navigateToSAMLConfigPage();
+
+    cy.getByLabel({
+      label: 'Enable SAMLv2 authentication',
+      tag: 'input'
+    }).check();
+
+    configureSAML();
+
+    cy.getByLabel({ label: 'Authentication conditions' }).click();
+
+    cy.getByLabel({
+      label: 'Enable conditions on identity provider',
+      tag: 'input'
+    }).check();
+
+    cy.getByLabel({
+      label: 'Conditions attribute path',
+      tag: 'input'
+    }).type('{selectall}{backspace}preferred_username');
+
+    cy.getByLabel({
+      label: 'Condition value',
+      tag: 'input'
+    }).type('{selectall}{backspace}saml');
+
+    cy.getByLabel({ label: 'save button', tag: 'button' }).click();
+
+    cy.wait('@updateSAMLProvider').its('response.statusCode').should('eq', 204);
+  }
+);
+
+Then(
+  'the users can access to Centreon UI only if all conditions matche',
+  () => {
+    const username = 'user-non-admin-for-SAML-authentication';
+
+    cy.session(username, () => {
+      cy.visit('/').getByLabel({ label: 'Login with SAML', tag: 'a' }).click();
+      cy.loginKeycloak(username)
+        .url()
+        .should('include', '/monitoring/resources')
+    });
+  }
+);
+
+after(() => {
+  cy.stopWebContainer().stopOpenIdProviderContainer();
+});
