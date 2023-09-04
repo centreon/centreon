3862056714ec341b5f22a9afb07f637bbc25e871
Tom Darneix <tomdar87@outlook.com>
2023-09-04 16:49:24 +0200

    Fix import path

--- centreon/packages/ui/src/Graph/common/utils.ts
+++ centreon/packages/ui/src/Graph/common/utils.ts
@@ -14,7 +14,7 @@ import {
 
 import { Theme } from '@mui/material';
 
-import { Thresholds } from '../Gauge/models';
+import { Thresholds } from './models';
 
 interface GetColorFromDataAndThresholdsProps {
   data: number;
