739d0986d99a0fd77f8c501661ed1501a57f4115
Kevin Duret <kduret@centreon.com>
2023-09-04 17:01:56 +0200

    retrieve workflows

--- centreon/tests/e2e/cypress.config.ts
+++ centreon/tests/e2e/cypress.config.ts
@@ -3,7 +3,7 @@ import configuration from '@centreon/js-config/cypress/e2e/configuration';
 export default configuration({
   dockerName: 'e2e-tests-centreon',
   env: {
-    OPENID_IMAGE_URL: 'http://172.17.0.3:8080'
+    OPENID_IMAGE_URL: 'http://localhost:8080'
   },
   specPattern: 'cypress/e2e/**/*.feature'
 });
