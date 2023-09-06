b07d3234fabdf9549828f47c38a5efcf283a7f2f
Tom Darneix <tomdar87@outlook.com>
2023-09-06 16:35:11 +0200

    Update centreon/www/widgets/src/centreon-widget-graph/src/translatedLabels.ts

--- centreon/www/widgets/src/centreon-widget-graph/src/translatedLabels.ts
+++ centreon/www/widgets/src/centreon-widget-graph/src/translatedLabels.ts
@@ -1,5 +1,3 @@
 export const labelNoDataFound = 'No data found';
 export const labelWarningThreshold = 'Warning threshold';
 export const labelCriticalThreshold = 'Critical threshold';
-export const labelValueDefinedByMetric = 'Value defined by metric';
-export const labelValueCustomized = 'Value customized';
