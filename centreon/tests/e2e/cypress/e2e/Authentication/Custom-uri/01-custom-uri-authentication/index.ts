c103b0b7b2ff418fa18d1c0f3c07faa459c6dea0
YAHIAOUI Hamza <hamza.yahiaoui@goandev.net>
2023-09-18 03:11:44 +0100

    apply commands for debian

--- centreon/tests/e2e/cypress/e2e/Authentication/Custom-uri/01-custom-uri-authentication/index.ts
+++ centreon/tests/e2e/cypress/e2e/Authentication/Custom-uri/01-custom-uri-authentication/index.ts
@@ -1,5 +1,7 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor';
 
+import { reloadWebServer, updateWebServerConfig } from '../common';
+
 const service = 'Ping';
 const host = 'Centreon-Server';
 
@@ -25,19 +27,12 @@ beforeEach(() => {
 When(
   'I update the base uri within the corresponding web server configuration file',
   () => {
-    cy.execInContainer({
-      command:
-        'bash -c "sed -i \'0,/centreon/s//monitor/\' /etc/httpd/conf.d/10-centreon.conf"',
-      name: Cypress.env('dockerName')
-    });
+    updateWebServerConfig();
   }
 );
 
 When('I reload the web server', () => {
-  cy.execInContainer({
-    command: 'systemctl reload httpd',
-    name: Cypress.env('dockerName')
-  });
+  reloadWebServer();
 });
 
 Then('I can authenticate to the centreon platform', () => {
---
+++ centreon/tests/e2e/cypress/e2e/Authentication/Custom-uri/common.ts
@@ -0,0 +1,31 @@
+const reloadWebServer = (): Cypress.Chainable => {
+  if (!Cypress.env('WEB_IMAGE_OS').includes('alma')) {
+    return cy.execInContainer({
+      command: 'systemctl reload httpd',
+      name: Cypress.env('dockerName')
+    });
+  }
+
+  return cy.execInContainer({
+    command: 'systemctl reload apache2',
+    name: Cypress.env('dockerName')
+  });
+};
+
+const updateWebServerConfig = (): Cypress.Chainable => {
+  if (!Cypress.env('WEB_IMAGE_OS').includes('alma')) {
+    return cy.execInContainer({
+      command:
+        'bash -c "sed -i \'0,/centreon/s//monitor/\' /etc/httpd/conf.d/10-centreon.conf"',
+      name: Cypress.env('dockerName')
+    });
+  }
+
+  return cy.execInContainer({
+    command:
+      'bash -c "sed -i \'0,/centreon/s//monitor/\' /etc/apache2/sites-available/centreon.conf"',
+    name: Cypress.env('dockerName')
+  });
+};
+
+export { reloadWebServer, updateWebServerConfig };
