46c22fa985ea86b0379dbefa2fa15e9b21d47cf1
Mohamed El Meziani <melmeziani-ext@centreon.com>
2023-09-19 15:31:16 +0000

    fixes

--- centreon/packages/ui/src/InputField/Text/index.tsx
+++ centreon/packages/ui/src/InputField/Text/index.tsx
@@ -142,7 +142,7 @@ const TextField = forwardRef(
     }, [innerValue, debounced, defaultValue]);
 
     return (
-      <Box sx={{ width: autoSize ? 'auto' : 'enherit' }}>
+      <Box sx={{ width: autoSize ? 'auto' : '100%' }}>
         <Tooltip placement="top" title={tooltipTitle}>
           <MuiTextField
             data-testid={dataTestId}
