f45ee6257feeec3c8b1a1bc8e170157bd9fef68b
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2023-09-19 16:45:10 +0100

    feat (feature flags/ frontend) : Manage the feature flag endpoint and add a FF for the new resource status enhancements. (#2154)
    Co-authored-by: Adrien Morais <amorais@centreon.com>

--- centreon/www/front_src/src/Main/useMain.ts
+++ centreon/www/front_src/src/Main/useMain.ts
@@ -18,6 +18,7 @@ import { platformInstallationStatusAtom } from './atoms/platformInstallationStat
 import useUser, { areUserParametersLoadedAtom } from './useUser';
 import usePlatformVersions from './usePlatformVersions';
 import useInitializeTranslation from './useInitializeTranslation';
+import usePlatformFeatures from './usePlatformFeatures';
 
 export const router = {
   useNavigate
@@ -48,6 +49,7 @@ const useMain = (): void => {
   const navigate = router.useNavigate();
   const [searchParameter] = useSearchParams();
   const { getPlatformVersions } = usePlatformVersions();
+  const { getPlatformFeatures } = usePlatformFeatures();
   useFederatedModules();
   useFederatedWidgets();
 
@@ -78,6 +80,8 @@ const useMain = (): void => {
         return;
       }
       loadUser();
+
+      getPlatformFeatures();
       getPlatformVersions();
     });
   }, []);
