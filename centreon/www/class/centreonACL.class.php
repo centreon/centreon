ce7461978203eabdd77f7fa16874ff8b7d515a98
Exodiaaaa <imran.ouizerht@gmail.com>
2023-09-20 10:43:31 +0100

    Fix Query of getHostServicesName method

--- centreon/www/class/centreonACL.class.php
+++ centreon/www/class/centreonACL.class.php
@@ -1646,14 +1646,16 @@ class CentreonACL
             . "LEFT JOIN services s "
             . "ON h.host_id = s.host_id "
             . $joinAcl
-            . "WHERE h.name = '" . CentreonDB::escape($host_name) . "' "
+            . "WHERE h.name = :host_name "
             . "AND s.service_id IS NOT NULL "
             . "ORDER BY h.name, s.description ";
-        $DBRESULT = $pearDBndo->query($query);
-        while ($row = $DBRESULT->fetchRow()) {
+        $statement = $pearDBndo->prepare($query);
+        $statement->bindValue(':host_name', $host_name, \PDO::PARAM_STR);
+        $statement->execute();
+        while ($row = $statement->fetchRow()) {
             $tab[$row['service_id']] = $row['description'];
         }
-        $DBRESULT->closeCursor();
+        $statement->closeCursor();
 
         return $tab;
     }
