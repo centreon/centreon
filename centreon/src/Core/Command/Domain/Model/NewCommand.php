d1cab576d2954b134ec8f7832ab90c6566ce4d1a
alaunois <alaunois@centreon.com>
2023-10-06 15:28:51 +0200

    feat(api) new endpoint: AddCommand

---
+++ centreon/src/Core/Command/Domain/Model/NewCommand.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Command\Domain\Model;
+
+use Assert\AssertionFailedException;
+use Centreon\Domain\Common\Assertion\Assertion;
+use Core\CommandMacro\Domain\Model\NewCommandMacro;
+use Core\Common\Domain\TrimmedString;
+
+class NewCommand
+{
+    public const COMMAND_MAX_LENGTH = 65535;
+    public const EXAMPLE_MAX_LENGTH = 255;
+    public const NAME_MAX_LENGTH = 200;
+
+    /**
+     * @param TrimmedString $name
+     * @param TrimmedString $commandLine
+     * @param CommandType $type
+     * @param bool $isShellEnabled
+     * @param TrimmedString $argumentExample
+     * @param Argument[] $arguments
+     * @param NewCommandMacro[] $macros
+     * @param null|int $connectorId
+     * @param null|int $graphTemplateId
+     *
+     * @throws AssertionFailedException
+     */
+    public function __construct(
+        private readonly TrimmedString $name,
+        private readonly TrimmedString $commandLine,
+        private readonly CommandType $type = CommandType::Check,
+        private readonly bool $isShellEnabled = false,
+        private readonly TrimmedString $argumentExample = new TrimmedString(''),
+        private readonly array $arguments = [],
+        private readonly array $macros = [],
+        private readonly ?int $connectorId = null,
+        private readonly ?int $graphTemplateId = null,
+    ) {
+        Assertion::notEmptyString($name->value, 'NewCommand::name');
+        Assertion::notEmptyString($commandLine->value, 'NewCommand::commandLine');
+
+        Assertion::maxLength($commandLine->value, self::NAME_MAX_LENGTH, 'NewCommand::name');
+        Assertion::maxLength($commandLine->value, self::COMMAND_MAX_LENGTH, 'NewCommand::commandLine');
+        Assertion::maxLength($argumentExample->value, self::EXAMPLE_MAX_LENGTH, 'NewCommand::argumentExample');
+
+        if ($connectorId !== null) {
+            Assertion::positiveInt($connectorId, 'NewCommand::connectorId');
+        }
+        if ($graphTemplateId !== null) {
+            Assertion::positiveInt($graphTemplateId, 'NewCommand::graphTemplateId');
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name->value;
+    }
+
+    public function getCommandLine(): string
+    {
+        return $this->commandLine->value;
+    }
+
+    public function getType(): CommandType
+    {
+        return $this->type;
+    }
+
+    public function isShellEnabled(): bool
+    {
+        return $this->isShellEnabled;
+    }
+
+    public function getArgumentExample(): string
+    {
+        return $this->argumentExample->value;
+    }
+
+    /**
+     * @return Argument[]
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return NewCommandMacro[]
+     */
+    public function getMacros(): array
+    {
+        return $this->macros;
+    }
+
+    public function getConnectorId(): ?int
+    {
+        return $this->connectorId;
+    }
+
+    public function getGraphTemplateId(): ?int
+    {
+        return $this->graphTemplateId;
+    }
+}
