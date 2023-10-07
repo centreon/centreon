487d8e56dc63f37e8efee5ce89afd6c889d30f16
tuntoja <58987095+tuntoja@users.noreply.github.com>
2023-10-07 21:38:59 +0200

    Update unattended.sh

--- centreon/unattended.sh
+++ centreon/unattended.sh
@@ -1245,7 +1245,7 @@ if [ "$operation" == "install" ]; then
 			centreon_admin_password=${ENV_CENTREON_ADMIN_PASSWD:-"$(genpasswd "Centreon user: admin")"}
 		else
 			test_password_policy
-   			echo "Random password generated for user [Centreon user] is [$centreon_admin_password]" >>$tmp_passwords_file
+   			echo "Random password generated for user [Centreon user: admin] is [$centreon_admin_password]" >>$tmp_passwords_file
 		fi
 		# Set from ENV or Administrator first name
 		centreon_admin_firstname=${ENV_CENTREON_ADMIN_FIRSTNAME:-"John"}
