522375066159b55c061cf6c29c1625513d5637ed
Tom Darneix <tomdar87@outlook.com>
2023-10-07 17:45:56 +0200

    Allow generic text to be sized by 1 row/column

--- centreon/www/front_src/src/Dashboards/Dashboard/atoms.ts
+++ centreon/www/front_src/src/Dashboards/Dashboard/atoms.ts
@@ -116,11 +116,19 @@ export const addPanelDerivedAtom = atom(
       ? 3
       : columnsFromScreenSize;
 
-    const panelWidth = width || panelConfiguration?.panelMinWidth || maxColumns;
+    const panelWidth =
+      Math.max(width || 0, panelConfiguration?.panelMinWidth || 0) ||
+      maxColumns;
+
+    const widgetHeight = Math.max(
+      height || 1,
+      panelConfiguration?.panelMinHeight || 1,
+      3
+    );
 
     const basePanelLayout = {
       data,
-      h: height || panelConfiguration?.panelMinHeight || 3,
+      h: widgetHeight,
       i: id,
       minH: panelConfiguration?.panelMinHeight || 3,
       minW: panelConfiguration?.panelMinWidth || 3,
