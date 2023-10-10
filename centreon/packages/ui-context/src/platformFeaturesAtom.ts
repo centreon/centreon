1a3e55095202e499b1814ccb28000cba28014e2a
Med1233 <melmeziani-ext@centreon.com>
2023-10-10 18:26:13 +0100

    trigger

--- centreon/packages/ui-context/src/platformFeaturesAtom.ts
+++ centreon/packages/ui-context/src/platformFeaturesAtom.ts
@@ -3,6 +3,7 @@ import { atom } from 'jotai';
 import { FeatureFlags, PlatformFeatures } from './types';
 
 export const platformFeaturesAtom = atom<PlatformFeatures | null>(null);
+
 export const featureFlagsDerivedAtom = atom<FeatureFlags | null>(
   (get): FeatureFlags => {
     const platformFeatures = get(platformFeaturesAtom);
