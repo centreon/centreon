256f7d4608a5753a422bf86a7a18b4f4f311274f
Tom Darneix <tomdar87@outlook.com>
2023-10-10 12:01:23 +0200

    Fix tests

--- centreon/www/widgets/src/centreon-widget-singlemetric/src/SingleMetric.cypress.spec.tsx
+++ centreon/www/widgets/src/centreon-widget-singlemetric/src/SingleMetric.cypress.spec.tsx
@@ -364,6 +364,11 @@ describe('Single metric Widget', () => {
 
       cy.contains('34%').should('have.css', 'fill', 'rgb(253, 155, 39)');
       cy.findByTestId('34-bar-#FD9B27').should('be.visible');
+      cy.findByTestId('34-bar-#FD9B27').should(
+        'have.css',
+        'width',
+        '465.69696044921875px'
+      );
 
       cy.makeSnapshot();
     });
@@ -378,7 +383,7 @@ describe('Single metric Widget', () => {
 
       cy.contains('34%').should('have.css', 'fill', 'rgb(255, 74, 74)');
       cy.findByTestId('34-bar-#FF4A4A').should('be.visible');
-      cy.findByTestId('34-bar-#FF4A4A').should('have.css', 'width', '1366px');
+      cy.findByTestId('34-bar-#FF4A4A').should('have.css', 'width', '1356px');
 
       cy.findByTestId('warning-line-10-tooltip').trigger('mouseover');
       cy.contains('Warning threshold: 10%. Custom value').should('be.visible');
