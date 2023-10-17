372109f498c0d5dee329b56faebd54dd95c94a46
Vincent Untz <vuntz@centreon.com>
2023-10-17 18:20:00 +0200

    fix(web): Log about starting partitioning only after initial check in cron script

--- centreon/cron/centreon-partitioning.php
+++ centreon/cron/centreon-partitioning.php
@@ -41,8 +41,6 @@ require_once _CENTREON_PATH_ . '/www/class/centreon-partition/config.class.php';
 require_once _CENTREON_PATH_ . '/www/class/centreon-partition/mysqlTable.class.php';
 require_once _CENTREON_PATH_ . '/www/class/centreon-partition/options.class.php';
 
-echo "[" . date(DATE_RFC822) . "] PARTITIONING STARTED\n";
-
 /* Create partitioned tables */
 $centreonDb = new CentreonDB('centreon');
 $centstorageDb = new CentreonDB('centstorage', 3);
@@ -53,6 +51,8 @@ if (!$partEngine->isCompatible($centstorageDb)) {
          . "CRITICAL: MySQL server is not compatible with partitionning. MySQL version must be greater or equal to 5.1\n";
     exit(1);
 }
+
+echo "[" . date(DATE_RFC822) . "] PARTITIONING STARTED\n";
 
 $tables = [
     'data_bin',
