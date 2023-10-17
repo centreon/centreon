b653d7a8ee6f6a9a0ab29c0561da0aa308c971ac
Vincent Untz <vuntz@centreon.com>
2023-10-17 18:12:01 +0200

    fix(web): Avoid crash in partitioning script/cron if server is not compatible
    There is no exitProcess function to be used in these scripts.
    
    Refs: MON-18861

--- centreon/cron/centreon-partitioning.php
+++ centreon/cron/centreon-partitioning.php
@@ -49,12 +49,9 @@ $centstorageDb = new CentreonDB('centstorage', 3);
 $partEngine = new PartEngine();
 
 if (!$partEngine->isCompatible($centstorageDb)) {
-    exitProcess(
-        PROCESS_ID,
-        1,
-        "[" . date(DATE_RFC822) . "] "
-        . "CRITICAL: MySQL server is not compatible with partitionning. MySQL version must be greater or equal to 5.1\n"
-    );
+    echo "[" . date(DATE_RFC822) . "] "
+         . "CRITICAL: MySQL server is not compatible with partitionning. MySQL version must be greater or equal to 5.1\n";
+    exit(1);
 }
 
 $tables = [
