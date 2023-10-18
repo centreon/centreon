fcc8492306d9004fd30c9c6d9a822c12df2eae2f
alaunois <alaunois@centreon.com>
2023-10-18 09:31:47 +0200

    feat(api) new endpoint: FindConnectors

---
+++ centreon/src/Core/Connector/Domain/Model/Connector.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Connector\Domain\Model;
+
+use Assert\AssertionFailedException;
+use Centreon\Domain\Common\Assertion\Assertion;
+
+class Connector
+{
+    public const COMMAND_MAX_LENGTH = 512;
+    public const NAME_MAX_LENGTH = 255;
+    public const DESCRIPTION_MAX_LENGTH = 255;
+
+    /**
+     * Summary of __construct.
+     *
+     * @param int $id
+     * @param string $name
+     * @param string $commandLine
+     * @param string $description
+     * @param int[] $commandIds
+     * @param bool $isActivated
+     *
+     * @throws AssertionFailedException
+     */
+    public function __construct(
+        private readonly int $id,
+        private string $name,
+        private string $commandLine,
+        private string $description = '',
+        private array $commandIds = [],
+        private bool $isActivated = true,
+    )
+    {
+        Assertion::positiveInt($id, 'Connector::id');
+
+        Assertion::notEmptyString($name, 'Connector::name');
+        Assertion::maxLength($name, self::NAME_MAX_LENGTH, 'Connector::name');
+
+        Assertion::notEmptyString($commandLine, 'Connector::commandLine');
+        Assertion::maxLength($commandLine, self::COMMAND_MAX_LENGTH, 'Connector::commandLine');
+
+        Assertion::maxLength($description, self::DESCRIPTION_MAX_LENGTH, 'Connector::description');
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCommandLine(): string
+    {
+        return $this->commandLine;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function isActivated(): bool
+    {
+        return $this->isActivated;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getCommandIds(): array
+    {
+        return $this->commandIds;
+    }
+}
