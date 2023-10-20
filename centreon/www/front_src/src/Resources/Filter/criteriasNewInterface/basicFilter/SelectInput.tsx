3ff671278808af72cc32770fee4dad6100e3303c
nouha <nelabrouki-ext@centreon.com>
2023-10-20 16:27:56 +0000

    implement deletion

--- centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/basicFilter/SelectInput.tsx
+++ centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/basicFilter/SelectInput.tsx
@@ -18,7 +18,7 @@ import {
 import { ChangedCriteriaParams, SectionType } from '../model';
 import useInputCurrentValues from '../useInputCurrentValues';
 import useInputData from '../useInputsData';
-import { findData, removeDuplicateFromObjectArray } from '../utils';
+import { removeDuplicateFromObjectArray } from '../utils';
 
 import useSectionsData from './sections/useSections';
 
@@ -59,14 +59,17 @@ const SelectInput = ({
     return null;
   }
 
-  const handleSearchData = (updatedValue): SearchData | undefined => {
-    const values = dataByFilterName?.search_data?.values;
+  const search = searchData?.search;
+  const setSearch = searchData?.setSearch;
+  const searchDataItem = dataByFilterName?.search_data;
+  const searchDataValues = dataByFilterName?.search_data?.values;
 
+  const handleSearchData = (updatedValue): SearchData | undefined => {
     const formattedUpdatedValues = updatedValue.map((item) => {
       return { id: resourceType, value: item.name, valueId: item?.id };
     });
 
-    const filteredFormattedUpdatedValues = values?.filter(
+    const filteredFormattedUpdatedValues = searchDataValues?.filter(
       (item) => item?.id !== resourceType
     );
 
@@ -75,39 +78,80 @@ const SelectInput = ({
       : formattedUpdatedValues;
 
     return {
-      ...dataByFilterName?.search_data,
+      ...searchDataItem,
       values: result as Array<SearchedDataValue>
     } as SearchData;
   };
 
-  const handleValues = (): Array<SelectEntry> | [] => {
-    const selectedValue = {
-      id: resourceType,
-      name: findData({
-        data: dataByFilterName?.options,
-        filterName: resourceType,
-        findBy: 'id'
-      })?.name
-    };
+  const handleValues = (data): Array<SelectEntry> | [] => {
+    const selectedValues = new Set(data?.values?.map((element) => element?.id));
+    const updatedValues = Array.from(selectedValues).filter((item) => item);
 
-    const dataByFilterNameValue = dataByFilterName?.value;
-
-    return removeDuplicateFromObjectArray({
-      array: dataByFilterNameValue
-        ? [...dataByFilterNameValue, selectedValue]
-        : [selectedValue],
-      byFields: ['id']
-    }) as Array<SelectEntry>;
+    return updatedValues.map((item) => ({
+      id: item,
+      name: dataByFilterName.options?.find((element) => element.id === item)
+        ?.name
+    }));
   };
 
-  const handleChange = (_, updatedValue): void => {
+  const handleChange = (event, updatedValue): void => {
+    const selectedValue = event?.target?.innerText;
+
+    const searchedSelectedValue = updatedValue.find(
+      (item) => item.name === selectedValue
+    );
+
+    if (!searchedSelectedValue) {
+      onDelete(null, { name: selectedValue });
+
+      return;
+    }
+
     const search_data = handleSearchData(updatedValue);
-    const selectedValues = handleValues();
+    const selectedValues = handleValues(search_data);
 
     changeCriteria({
       filterName,
       search_data,
       updatedValue: selectedValues
+    });
+  };
+
+  const updateSearchInput = (option: SelectEntry): void => {
+    const field = searchDataItem?.field as string;
+
+    const result = getFoundFields({
+      fields: [field],
+      value: search as string
+    });
+    const oldFormattedValue = `${result[0].field}:${result[0].value}`;
+
+    const newValues = result[0]?.value
+      ?.split(',')
+      ?.filter((item) => item !== option?.name);
+
+    const newFormattedValue =
+      newValues?.length > 0 ? `${field}:${newValues.join(',')}` : '';
+
+    const newSearch = search?.replace(oldFormattedValue, newFormattedValue);
+
+    setSearch(newSearch);
+  };
+
+  const onDelete = (_, option): void => {
+    updateSearchInput(option);
+
+    const search_data = {
+      ...searchDataItem,
+      values: searchDataValues?.filter((item) => item.value !== option.name)
+    } as SearchData;
+
+    const updatedValue = handleValues(search_data);
+
+    changeCriteria({
+      filterName,
+      search_data,
+      updatedValue
     });
   };
 
@@ -133,41 +177,6 @@ const SelectInput = ({
     return isNil(option)
       ? false
       : equals(option.name.toString(), selectedValue.name.toString());
-  };
-
-  const onDelete = (_, option): void => {
-    const field = dataByFilterName.search_data?.field as string;
-    const values = dataByFilterName.search_data?.values;
-
-    const search = searchData?.search;
-    const setSearch = searchData?.setSearch;
-
-    const result = getFoundFields({
-      fields: [field],
-      value: search
-    });
-    const oldFormattedValue = `${result[0].field}:${result[0].value}`;
-
-    const newValues = result[0]?.value
-      ?.split(',')
-      ?.filter((item) => item !== option?.name);
-
-    const newFormattedValue = `${field}:${newValues.join(',')}`;
-
-    const newSearch = search?.replace(oldFormattedValue, newFormattedValue);
-
-    setSearch(newSearch);
-
-    const search_data = {
-      ...dataByFilterName.search_data,
-      values: values?.filter((item) => item.value !== option.name)
-    } as SearchData;
-
-    changeCriteria({
-      filterName,
-      search_data,
-      updatedValue: dataByFilterName.value
-    });
   };
 
   return (
