ac6a9a8a8be1f9bb6f85cc006fbfd9a0dc98eca0
YAHIAOUI Hamza <hamza.yahiaoui@goandev.net>
2023-10-23 10:51:27 +0100

    fix

--- centreon/tests/e2e/integration/Authentication/SAML/07-saml-authentication-restrictions/index.ts
+++ centreon/tests/e2e/integration/Authentication/SAML/07-saml-authentication-restrictions/index.ts
@@ -5,7 +5,7 @@ import {
   initializeSAMLUser,
   navigateToSAMLConfigPage
 } from '../common';
-import { configureProviderAcls } from '../../../../../commons';
+import { configureProviderAcls } from '../../../../commons';
 
 before(() => {
   cy.startWebContainer()
