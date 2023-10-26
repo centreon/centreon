0aee902649ecdac767815f2a8aaab7bf773f4ef0
Tom Darneix <tomdar87@outlook.com>
2023-10-26 19:13:52 +0200

    Disable the save button when filter is not updated

--- centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/actions/Save.tsx
+++ centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/actions/Save.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import { equals, isEmpty, omit, propEq, reject } from 'ramda';
+import { useAtomValue } from 'jotai';
+
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { Popper } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -10,6 +13,13 @@ import MenuList from '@mui/material/MenuList';
 import Paper from '@mui/material/Paper';
 
 import { labelSave, labelSaveAsNew } from '../../../translatedLabels';
+import { currentFilterAtom, customFiltersAtom } from '../../filterAtoms';
+import {
+  allFilter,
+  resourceProblemsFilter,
+  unhandledProblemsFilter
+} from '../../models';
+import { Criteria } from '../../Criterias/models';
 
 import { useStyles } from './actions.style';
 
@@ -27,6 +37,14 @@ interface Save {
   getIsUpdateFilter: (value: boolean) => void;
 }
 
+const getSelectableCriterias = (
+  criterias: Array<Criteria>
+): Array<Criteria> => {
+  const filteredCriterias = reject<Criteria>(propEq('name', 'sort'))(criterias);
+
+  return filteredCriterias.map(omit(['search_data']));
+};
+
 const Save = ({
   canSaveFilterAsNew,
   canSaveFilter,
@@ -39,6 +57,30 @@ const Save = ({
   const [selectedOption, setSelectedOption] = useState<Option | undefined>();
   const [anchorEl, setAnchorEl] = useState<null | HTMLDivElement>(null);
   const openPopper = Boolean(anchorEl);
+
+  const currentFilter = useAtomValue(currentFilterAtom);
+  const customFilters = useAtomValue(customFiltersAtom);
+
+  const baseFilters = [
+    unhandledProblemsFilter,
+    resourceProblemsFilter,
+    allFilter
+  ];
+
+  const selectableFilters = [...baseFilters, ...customFilters];
+
+  const isNewFilter = isEmpty(currentFilter.id);
+
+  const selectedCustomFilter = isNewFilter
+    ? null
+    : selectableFilters.find(propEq('id', currentFilter.id));
+
+  const saveButtonDisabled =
+    !isNewFilter &&
+    equals(
+      getSelectableCriterias(currentFilter.criterias),
+      getSelectableCriterias(selectedCustomFilter?.criterias || [])
+    );
 
   const handleToggle = (event): void => {
     setOpen(!open);
@@ -83,7 +125,12 @@ const Save = ({
 
   return (
     <ClickAwayListener onClickAway={handleClose}>
-      <ButtonGroup aria-label="split button" color="primary" size="small">
+      <ButtonGroup
+        aria-label="split button"
+        color="primary"
+        disabled={saveButtonDisabled}
+        size="small"
+      >
         <div className={classes.container}>
           <div className={classes.subContainer}>
             <Button
