e509b862d3449ada50cc6af173f88235fde58eaa
Tom Darneix <tomdar87@outlook.com>
2023-10-26 21:32:40 +0200

    Change save buttons

--- centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/actions/Save.tsx
+++ centreon/www/front_src/src/Resources/Filter/criteriasNewInterface/actions/Save.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react';
-
 import { equals, isEmpty, omit, propEq, reject } from 'ramda';
 import { useAtomValue } from 'jotai';
+import { useTranslation } from 'react-i18next';
 
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { Popper } from '@mui/material';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import Paper from '@mui/material/Paper';
+import SaveIcon from '@mui/icons-material/Save';
+import EditIcon from '@mui/icons-material/Edit';
 
-import { labelSave, labelSaveAsNew } from '../../../translatedLabels';
 import { currentFilterAtom, customFiltersAtom } from '../../filterAtoms';
 import {
   allFilter,
@@ -20,14 +13,7 @@ import {
   unhandledProblemsFilter
 } from '../../models';
 import { Criteria } from '../../Criterias/models';
-
-import { useStyles } from './actions.style';
-
-interface Option {
-  disabled: boolean;
-  onClick: () => void;
-  title: string;
-}
+import { labelSaveAs, labelUpdate } from '../translatedLabels';
 
 interface Save {
   canSaveFilter: boolean;
@@ -52,11 +38,7 @@ const Save = ({
   getIsUpdateFilter,
   closePopover
 }: Save): JSX.Element => {
-  const { classes } = useStyles();
-  const [open, setOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState<Option | undefined>();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLDivElement>(null);
-  const openPopper = Boolean(anchorEl);
+  const { t } = useTranslation();
 
   const currentFilter = useAtomValue(currentFilterAtom);
   const customFilters = useAtomValue(customFiltersAtom);
@@ -82,16 +64,6 @@ const Save = ({
       getSelectableCriterias(selectedCustomFilter?.criterias || [])
     );
 
-  const handleToggle = (event): void => {
-    setOpen(!open);
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
-
-  const handleClose = (): void => {
-    setOpen(false);
-    setAnchorEl(null);
-  };
-
   const saveAsNew = (): void => {
     getIsCreateFilter(true);
     closePopover?.();
@@ -102,82 +74,25 @@ const Save = ({
     closePopover?.();
   };
 
-  const options = [
-    {
-      disabled: !canSaveFilterAsNew,
-      onClick: saveAsNew,
-      title: labelSaveAsNew
-    },
-    {
-      disabled: !canSaveFilter,
-      onClick: saveAs,
-      title: labelSave
-    }
-  ];
-
-  const selectOption = (event, option: Option): void => {
-    setSelectedOption(option);
-    handleToggle(event);
-  };
-  const defaultCurrentOption =
-    options.find(({ disabled }) => !disabled) ||
-    options.find(({ title }) => title === labelSaveAsNew);
-
   return (
-    <ClickAwayListener onClickAway={handleClose}>
-      <ButtonGroup
-        aria-label="split button"
-        color="primary"
-        disabled={saveButtonDisabled}
-        size="small"
+    <>
+      <Button
+        disabled={saveButtonDisabled || !canSaveFilterAsNew}
+        startIcon={<SaveIcon fontSize="small" />}
+        variant="outlined"
+        onClick={saveAsNew}
       >
-        <div className={classes.container}>
-          <div className={classes.subContainer}>
-            <Button
-              color="primary"
-              disabled={
-                selectedOption?.disabled ?? defaultCurrentOption?.disabled
-              }
-              size="small"
-              onClick={selectedOption?.onClick || defaultCurrentOption?.onClick}
-            >
-              {selectedOption?.title || defaultCurrentOption?.title}
-            </Button>
-            <Button
-              color="primary"
-              disabled={options.every(({ disabled }) => disabled)}
-              size="small"
-              onClick={handleToggle}
-            >
-              <ArrowDropDownIcon />
-            </Button>
-          </div>
-        </div>
-        {open && (
-          <Popper
-            anchorEl={anchorEl}
-            className={classes.popperButtonGroup}
-            id="popperButtonGroup"
-            open={openPopper}
-            placement="bottom-end"
-          >
-            <Paper>
-              <MenuList autoFocusItem id="split-button-menu">
-                {options.map((option) => (
-                  <MenuItem
-                    disabled={option.disabled}
-                    key={option.title}
-                    onClick={(event) => selectOption(event, option)}
-                  >
-                    {option.title}
-                  </MenuItem>
-                ))}
-              </MenuList>
-            </Paper>
-          </Popper>
-        )}
-      </ButtonGroup>
-    </ClickAwayListener>
+        {t(labelSaveAs)}
+      </Button>
+      <Button
+        disabled={saveButtonDisabled || !canSaveFilter}
+        startIcon={<EditIcon fontSize="small" />}
+        variant="outlined"
+        onClick={saveAs}
+      >
+        {t(labelUpdate)}
+      </Button>
+    </>
   );
 };
 
