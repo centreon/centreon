4381d0bb857f2f2f9cf4179d519a121126910881
Med1233 <melmeziani-ext@centreon.com>
2023-10-31 12:34:16 +0100

    adapt refresh icon color to other icons

--- centreon/www/front_src/src/Dashboards/Dashboard/Dashboard.tsx
+++ centreon/www/front_src/src/Dashboards/Dashboard/Dashboard.tsx
@@ -102,7 +102,7 @@ const Dashboard = (): ReactElement => {
                 data-testid="refresh"
                 icon={<RefreshIcon />}
                 size="small"
-                variant="ghost"
+                variant="primary"
                 onClick={refreshAllWidgets}
               />
             </span>
