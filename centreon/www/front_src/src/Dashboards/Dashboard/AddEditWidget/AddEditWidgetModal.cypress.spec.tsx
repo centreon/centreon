9782a0b05b0527c2af4c879e0bb4313ee8b68c03
Med1233 <melmeziani-ext@centreon.com>
2023-11-02 13:47:23 +0100

    update tests

--- centreon/www/front_src/src/Dashboards/Dashboard/AddEditWidget/AddEditWidgetModal.cypress.spec.tsx
+++ centreon/www/front_src/src/Dashboards/Dashboard/AddEditWidget/AddEditWidgetModal.cypress.spec.tsx
@@ -530,7 +530,7 @@ describe('AddEditWidgetModal', () => {
         cy.makeSnapshot();
       });
 
-      it('hides the delete button when there is only one resource ', () => {
+      it.only('hides the delete button when there is only one resource ', () => {
         cy.findByLabelText(labelWidgetType).click();
         cy.contains('Generic data (example)').click();
 
@@ -556,7 +556,7 @@ describe('AddEditWidgetModal', () => {
 
         cy.findByLabelText(labelSave).should('be.enabled');
 
-        cy.findAllByLabelText(labelDelete).should('have.length', 1);
+        cy.findAllByLabelText(labelDelete).eq(0).should('not.be.visible');
 
         cy.makeSnapshot();
       });
