2c77036bce375c33c6aafc92c88edd44b52617ec
hamzabessa <hbessa@ext.centreon.com>
2023-11-03 10:18:56 +0100

    fix: modify scripts for centreon-gorgone

--- centreon-gorgone/packaging/scripts/centreon-gorgone-preinstall.sh
+++ centreon-gorgone/packaging/scripts/centreon-gorgone-preinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if ! /usr/bin/getent group centreon-gorgone &>/dev/null; then
-    /usr/sbin/groupadd -r centreon-gorgone
+  /usr/sbin/groupadd -r centreon-gorgone
 fi
 
 # Check if the centreon-gorgone user exists, and create it if not
 if ! /usr/bin/getent passwd centreon-gorgone &>/dev/null; then
-    /usr/sbin/useradd -g centreon-gorgone -m -d /var/lib/centreon-gorgone -r centreon-gorgone 2> /dev/null
+  /usr/sbin/useradd -g centreon-gorgone -m -d /var/lib/centreon-gorgone -r centreon-gorgone 2> /dev/null
 fi
