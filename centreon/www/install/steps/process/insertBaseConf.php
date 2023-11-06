8b7545dd9fa2625e7cbe40cde531cae2323266e9
Kevin Duret <kduret@centreon.com>
2023-11-06 13:36:37 +0100

    chore(widgets): move widgets in centreon web package

--- centreon/www/install/steps/process/insertBaseConf.php
+++ centreon/www/install/steps/process/insertBaseConf.php
@@ -38,6 +38,8 @@ session_start();
 require_once __DIR__ . '/../../../../bootstrap.php';
 require_once '../functions.php';
 
+use CentreonModule\ServiceProvider;
+
 $return = array(
     'id' => 'baseconf',
     'result' => 1,
@@ -86,6 +88,12 @@ try {
     $utils->executeSqlFile(__DIR__ . '/../../var/baseconf/centreon-broker.sql', $macros);
     $utils->executeSqlFile(__DIR__ . '/../../insertTopology.sql', $macros);
     $utils->executeSqlFile(__DIR__ . '/../../insertBaseConf.sql', $macros);
+
+    /**
+     * @var CentreonModuleService
+     */
+    $moduleService = \Centreon\LegacyContainer::getInstance()[ServiceProvider::CENTREON_MODULE];
+    $entity = $moduleService->install('engine-status', 'widget');
 } catch (\Exception $e) {
     $return['msg'] = $e->getMessage();
     echo json_encode($return);
