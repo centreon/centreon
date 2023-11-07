6391c9303f2d469963811c14a843089a249a57e0
hamzabessa <hbessa@ext.centreon.com>
2023-11-07 11:44:28 +0100

    fix: fixing paths and selinux commands

--- centreon-gorgone/packaging/scripts/centreon-gorgone-selinux-postinstall.sh
+++ centreon-gorgone/packaging/scripts/centreon-gorgone-selinux-postinstall.sh
@@ -1,1 +1,29 @@
-semodule -i %{_datadir}/selinux/packages/ent/ent-gorgoned.pp > /dev/null 2>&1
+
+
+
+#!/bin/sh
+
+install() {
+  semodule -i /usr/share/selinux/packages/centreon/centreon-gorgoned.pp > /dev/null 2>&1 || :
+
+}
+
+upgrade() {
+  semodule -i /usr/share/selinux/packages/centreon/centreon-gorgoned.pp > /dev/null 2>&1 || :
+}
+
+action="$1"
+if  [ "$1" = "configure" ] && [ -z "$2" ]; then
+  action="install"
+elif [ "$1" = "configure" ] && [ -n "$2" ]; then
+  action="upgrade"
+fi
+
+case "$action" in
+  "1" | "install")
+    install
+    ;;
+  "2" | "upgrade")
+    upgrade
+    ;;
+esac
