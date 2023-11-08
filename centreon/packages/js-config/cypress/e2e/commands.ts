016b5ad4f5ee6c951a25f7c01c5e1a79c98dacf9
Kevin Duret <kduret@centreon.com>
2023-11-08 15:56:09 +0100

    add logs

--- centreon/packages/js-config/cypress/e2e/commands.ts
+++ centreon/packages/js-config/cypress/e2e/commands.ts
@@ -5,7 +5,7 @@ import './commands/monitoring';
 
 import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector';
 
-installLogsCollector();
+installLogsCollector({ enableExtendedCollector: true });
 
 const apiLoginV2 = '/centreon/authentication/providers/configurations/local';
 
