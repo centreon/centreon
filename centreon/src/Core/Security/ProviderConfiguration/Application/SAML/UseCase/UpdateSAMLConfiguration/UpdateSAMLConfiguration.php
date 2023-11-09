874d4ceaf0b61b137f75e384eb854881af6fe82e
Laurent Pinsivy <lpinsivy@centreon.com>
2023-11-09 08:59:25 +0100

    fix(logs): do not log error when all access groups are found (#2147) (#2346)
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- centreon/src/Core/Security/ProviderConfiguration/Application/SAML/UseCase/UpdateSAMLConfiguration/UpdateSAMLConfiguration.php
+++ centreon/src/Core/Security/ProviderConfiguration/Application/SAML/UseCase/UpdateSAMLConfiguration/UpdateSAMLConfiguration.php
@@ -203,10 +203,13 @@ class UpdateSAMLConfiguration
         foreach ($foundAccessGroups as $foundAccessGroup) {
             $foundAccessGroupsId[] = $foundAccessGroup->getId();
         }
+
         $nonExistentAccessGroupsIds = array_diff($accessGroupIdsFromRequest, $foundAccessGroupsId);
-        $this->error("Access Groups not found", [
-            "access_group_ids" => implode(', ', $nonExistentAccessGroupsIds)
-        ]);
+        if (! empty($nonExistentAccessGroupsIds)) {
+            $this->error('Access Groups not found', [
+                'access_group_ids' => implode(', ', $nonExistentAccessGroupsIds),
+            ]);
+        }
     }
 
     /**
