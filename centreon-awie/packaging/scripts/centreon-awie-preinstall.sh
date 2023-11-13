2b10aa5aa2291f0a844fa6c5e2cd04fa39e917ae
hamzabessa <hbessa@ext.centreon.com>
2023-11-13 14:51:11 +0100

    fix: fixing pre-install script

--- centreon-awie/packaging/scripts/centreon-awie-preinstall.sh
+++ centreon-awie/packaging/scripts/centreon-awie-preinstall.sh
@@ -1,11 +1,11 @@
 find .          \
         -type f \
-        -exec %{__grep} -qE '(@CENTREON_ETC@)|(@BIN_DIR@)|(CENTREON_DIR)|(CENTREON_LOG)|(CENTREON_VARLIB)|(MODULE_NAME)|(DB_CENTSTORAGE)' {} ';'   \
-        -exec %{__sed} -i -e "s|@CENTREON_ETC@|%{centreon_etc}|g" \
-                          -e "s|@BIN_DIR@|%{_bindir}|g" \
-                          -e "s|@CENTREON_DIR@|%{centreon_dir}|g" \
-                          -e "s|@CENTREON_LOG@|%{centreon_log}|g" \
-                          -e "s|@CENTREON_VARLIB@|%{centreon_varlib}|g" \
-                          -e "s|@MODULE_NAME@|%{name}|g" \
+        -exec grep -qE '(@CENTREON_ETC@)|(@BIN_DIR@)|(CENTREON_DIR)|(CENTREON_LOG)|(CENTREON_VARLIB)|(MODULE_NAME)|(DB_CENTSTORAGE)' {} ';'   \
+        -exec sed -i -e "s|@CENTREON_ETC@|${centreon_etc}|g" \
+                          -e "s|@BIN_DIR@|/usr/bin|g" \
+                          -e "s|@CENTREON_DIR@|${centreon_dir}|g" \
+                          -e "s|@CENTREON_LOG@|${centreon_log}|g" \
+                          -e "s|@CENTREON_VARLIB@|${centreon_varlib}|g" \
+                          -e "s|@MODULE_NAME@|centreon-awie|g" \
 			  -e "s|@DB_CENTSTORAGE@|centreon_storage|g" \
                        {} ';'
