8726e496680cb3a92c07b16699b19bd54b9f5977
Kevin Duret <kduret@centreon.com>
2023-11-16 10:18:26 +0100

    Update centreon/www/front_src/src/Resources/Listing/ResourceListing.cypress.spec.tsx

--- centreon/www/front_src/src/Resources/Listing/ResourceListing.cypress.spec.tsx
+++ centreon/www/front_src/src/Resources/Listing/ResourceListing.cypress.spec.tsx
@@ -656,7 +656,7 @@ describe('Display additional columns', () => {
   });
 });
 
-describe.only('Notification column', () => {
+describe('Notification column', () => {
   it('displays notification column if the cloud notification feature is disabled', () => {
     store.set(
       platformFeaturesAtom,
