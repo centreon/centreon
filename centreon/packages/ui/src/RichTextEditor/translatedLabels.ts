a286972e82ae74790dab0cce614bf9139d46fb9d
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2023-11-20 10:07:15 +0100

    enh(UI) : refacto the Rich Text Editor (#2536)
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>

--- centreon/packages/ui/src/RichTextEditor/translatedLabels.ts
+++ centreon/packages/ui/src/RichTextEditor/translatedLabels.ts
@@ -3,3 +3,6 @@ export const labelRedo = 'Redo';
 export const labelInputLink = 'Insert link';
 export const labelSavedLink = 'Saved link';
 export const labelEditLink = 'Edit link';
+export const labelAlignPicker = 'align picker';
+export const labelUnorderedList = 'Unordered List';
+export const labelOrderedList = 'Ordered List';
