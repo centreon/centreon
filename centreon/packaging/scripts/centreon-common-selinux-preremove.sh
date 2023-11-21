bce01b55e6f98f71847b4041f52feb3992b0bf24
Kevin Duret <kduret@centreon.com>
2023-11-21 15:41:45 +0100

    selinux

---
+++ centreon/packaging/scripts/centreon-common-selinux-preremove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" -lt "1" ]; then
+  setsebool -P daemons_enable_cluster_mode off > /dev/null 2>&1 || :
+  setsebool -P cluster_can_network_connect off > /dev/null 2>&1 || :
+  setsebool -P cluster_manage_all_files off > /dev/null 2>&1 || :
+  semodule -r centreon-common > /dev/null 2>&1 || :
+fi
