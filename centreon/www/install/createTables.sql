4327ee187bd9f50e922544c00517bbfa17182795
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2023-11-30 13:52:50 +0100

    feat(dashboards): implements an endpoint to update Playlist Shares (#2753)

--- centreon/www/install/createTables.sql
+++ centreon/www/install/createTables.sql
@@ -2670,6 +2670,8 @@ CREATE TABLE IF NOT EXISTS `dashboard_playlist_relation` (
 CREATE TABLE IF NOT EXISTS `dashboard_playlist_contact_relation` (
   `contact_id` INT(11) NOT NULL,
   `playlist_id` INT UNSIGNED NOT NULL,
+  `role` enum('viewer','editor') NOT NULL DEFAULT 'viewer',
+  KEY `role_index` (`role`),
   UNIQUE KEY(`contact_id`, `playlist_id`),
   CONSTRAINT `dashboard_playlist_contact_relation_contact_id`
     FOREIGN KEY (`contact_id`)
@@ -2682,6 +2684,8 @@ CREATE TABLE IF NOT EXISTS `dashboard_playlist_contact_relation` (
 CREATE TABLE IF NOT EXISTS `dashboard_playlist_contactgroup_relation` (
   `contactgroup_id` INT(11) NOT NULL,
   `playlist_id` INT UNSIGNED NOT NULL,
+  `role` enum('viewer','editor') NOT NULL DEFAULT 'viewer',
+  KEY `role_index` (`role`),
   UNIQUE KEY(`contactgroup_id`, `playlist_id`),
   CONSTRAINT `dashboard_playlist_contactgroup_relation_contactgroup_id`
     FOREIGN KEY (`contactgroup_id`)
