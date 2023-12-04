e6dcabfce6727942709508fca4740b6c9d6fc26f
Jérémy Jaouen <jjaouen@centreon.com>
2023-12-04 08:56:58 +0100

    documentation and translation

--- centreon/src/Core/Dashboard/Playlist/Application/Exception/PlaylistException.php
+++ centreon/src/Core/Dashboard/Playlist/Application/Exception/PlaylistException.php
@@ -157,7 +157,7 @@ class PlaylistException extends \Exception
      */
     public static function playlistNotShared(int $playlistId): self
     {
-        return new self(sprintf(_('The following playlist is not shared with you: [%d].'), $playlistId));
+        return new self(sprintf(_('The following playlist is not shared with you: [%d]'), $playlistId));
     }
 
     /**
