304c43ccd8b8f06af6248bc024bd0a85b232ffc5
hamzabessa <hbessa@ext.centreon.com>
2023-12-05 17:25:48 +0100

    fix: dsm server postinstall script

--- centreon-dsm/packaging/scripts/centreon-dsm-server-postinstall.sh
+++ centreon-dsm/packaging/scripts/centreon-dsm-server-postinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-/sbin/chkconfig --add dsmd
-/sbin/chkconfig --level 345 dsmd on
-service dsmd start > /dev/null 2>&1 || echo "Failed to start dsmd service"
+systemctl daemon-reload ||:
+systemctl unmask dsmd.service ||:
+systemctl preset dsmd.service ||:
+systemctl enable dsmd.service ||:
+systemctl restart dsmd.service ||:
