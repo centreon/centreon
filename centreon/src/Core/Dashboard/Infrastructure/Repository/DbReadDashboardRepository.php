b07998ab86fa0d81f56aa97901cb31ff8024badd
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2023-12-06 13:44:26 +0100

    enh(dashboards): refactor create playlist to handle dashboards in a proper way (#2807)

--- centreon/src/Core/Dashboard/Infrastructure/Repository/DbReadDashboardRepository.php
+++ centreon/src/Core/Dashboard/Infrastructure/Repository/DbReadDashboardRepository.php
@@ -81,6 +81,9 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
         return (bool) $statement->fetchColumn();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function findOne(int $dashboardId): ?Dashboard
     {
         $query = <<<'SQL'
@@ -146,6 +149,9 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
         return (bool) $statement->fetchColumn();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function findOneByContact(int $dashboardId, ContactInterface $contact): ?Dashboard
     {
         $query = <<<'SQL'
@@ -190,6 +196,9 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
         return $data ? $this->createDashboardFromArray($data) : null;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function findByRequestParameter(
         ?RequestParametersInterface $requestParameters
     ): array {
@@ -199,6 +208,9 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
         );
     }
 
+    /**
+     * @inheritDoc
+     */
     public function findByRequestParameterAndContact(
         ?RequestParametersInterface $requestParameters,
         ContactInterface $contact
@@ -207,6 +219,53 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
             $this->getFindDashboardConcatenator($contact->getId()),
             $requestParameters
         );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findByIds(array $ids): array
+    {
+        $bind = [];
+        foreach ($ids as $key => $id) {
+            $bind[':id_' . $key] = $id;
+        }
+        if ([] === $bind) {
+            return [];
+        }
+
+        $dashboardIdsAsString = implode(', ', array_keys($bind));
+
+        $query = <<<SQL
+            SELECT
+                d.id,
+                d.name,
+                d.description,
+                d.created_by,
+                d.updated_by,
+                d.created_at,
+                d.updated_at,
+                d.refresh_type,
+                d.refresh_interval
+            FROM `:db`.`dashboard` d
+            WHERE d.id IN ({$dashboardIdsAsString})
+            SQL;
+
+        $statement = $this->db->prepare($this->translateDbName($query));
+        foreach ($bind as $token => $id) {
+            $statement->bindValue($token, $id, \PDO::PARAM_INT);
+        }
+        $statement->setFetchMode(\PDO::FETCH_ASSOC);
+        $statement->execute();
+
+        // Retrieve data
+        $dashboards = [];
+        foreach ($statement as $result) {
+            /** @var DashboardResultSet $result */
+            $dashboards[] = $this->createDashboardFromArray($result);
+        }
+
+        return $dashboards;
     }
 
     /**
@@ -251,6 +310,11 @@ class DbReadDashboardRepository extends AbstractRepositoryRDB implements ReadDas
         return $dashboards;
     }
 
+    /**
+     * @param int|null $contactId
+     *
+     * @return SqlConcatenator
+     */
     private function getFindDashboardConcatenator(?int $contactId): SqlConcatenator
     {
         $concatenator = (new SqlConcatenator())
