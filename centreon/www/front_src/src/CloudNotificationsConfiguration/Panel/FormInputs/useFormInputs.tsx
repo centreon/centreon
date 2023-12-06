e856d2486499f7d4986e8118cb701b1d81211c33
dmyios <diosypenko@centreon.com>
2023-12-06 15:57:24 +0100

    removed contact groups selector from ui

--- centreon/www/front_src/src/CloudNotificationsConfiguration/Panel/FormInputs/useFormInputs.tsx
+++ centreon/www/front_src/src/CloudNotificationsConfiguration/Panel/FormInputs/useFormInputs.tsx
@@ -20,9 +20,7 @@ import {
   labelTimePeriod,
   labelSearchHostGroups,
   labelSearchServiceGroups,
-  // labelContactsGroups,
   labelContacts,
-  // labelSearchContactsGroups,
   labelSearchContacts,
   labelSearchBusinessViews,
   labelBusinessViews,
@@ -31,7 +29,6 @@ import {
 import { hostEvents, serviceEvents } from '../utils';
 import {
   businessViewsEndpoint,
-  // contactGroupsEndpoint,
   hostsGroupsEndpoint,
   serviceGroupsEndpoint,
   usersEndpoint
@@ -328,39 +325,6 @@ const useFormInputs = ({
       label: '',
       type: InputType.Grid
     },
-    // {
-    //   additionalLabel: t(labelContactsGroups),
-    //   additionalLabelClassName: classes.additionalLabel,
-    //   fieldName: '',
-    //   grid: {
-    //     columns: [
-    //       {
-    //         connectedAutocomplete: {
-    //           additionalConditionParameters: [],
-    //           endpoint: contactGroupsEndpoint
-    //         },
-    //         dataTestId: 'Search contact groups',
-    //         fieldName: 'contactgroups',
-    //         label: t(labelSearchContactsGroups),
-    //         required: true,
-    //         type: InputType.MultiConnectedAutocomplete
-    //       },
-    //       {
-    //         custom: {
-    //           Component: Box
-    //         },
-    //         fieldName: '',
-    //         label: '',
-    //         type: InputType.Custom
-    //       }
-    //     ],
-    //     gridTemplateColumns: handleGridTemplate(panelWidth)
-    //   },
-    //   group: basicFormGroups[1].name,
-    //   inputClassName: classes.input,
-    //   label: '',
-    //   type: InputType.Grid
-    // },
 
     {
       fieldName: '',
