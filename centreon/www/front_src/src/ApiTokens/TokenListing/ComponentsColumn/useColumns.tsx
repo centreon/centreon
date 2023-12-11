e8e19e6b643917c354eb3e512bc3ba7c24bb2d3c
nouha <nelabrouki-ext@centreon.com>
2023-12-11 13:03:09 +0000

    enhancements

--- centreon/www/front_src/src/ApiTokens/TokenListing/ComponentsColumn/useColumns.tsx
+++ centreon/www/front_src/src/ApiTokens/TokenListing/ComponentsColumn/useColumns.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 
 import { useAtom, useAtomValue } from 'jotai';
+import { useTranslation } from 'react-i18next';
 
 import { Column, ColumnType, useLocaleDateTimeFormat } from '@centreon/ui';
 import { userAtom } from '@centreon/ui-context';
@@ -9,11 +10,13 @@ import { labelActive, labelRevoked } from '../../translatedLabels';
 import { selectedColumnIdsAtom } from '../atoms';
 
 import ActionsColumn from './ActionsColumn';
+import Information from './Information';
 import { Columns, UseColumns, defaultSelectedColumnIds } from './models';
 
 const dateFormat = 'L';
 
 export const useColumns = (): UseColumns => {
+  const { t } = useTranslation();
   const { format } = useLocaleDateTimeFormat();
 
   const [selectedColumnIds, setSelectedColumnIds] = useAtom(
@@ -30,65 +33,74 @@ export const useColumns = (): UseColumns => {
     setSelectedColumnIds(defaultSelectedColumnIds);
   };
 
-  const columns: Array<Column> = useMemo(
-    () => [
+  const columns: Array<Column> = useMemo(() => {
+    return [
       {
-        getFormattedString: ({ is_revoked }) =>
-          is_revoked ? labelRevoked : labelActive,
+        Component: ({ row }) => (
+          <Information
+            data={row.is_revoked ? t(labelRevoked) : t(labelActive)}
+          />
+        ),
         id: 'status',
         label: Columns.Status,
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
-        getFormattedString: ({ name }) => name,
+        Component: ({ row }) => {
+          return <Information data={row.name} />;
+        },
         id: 'token_name',
         label: Columns.Name,
         sortField: 'token_name',
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
-        getFormattedString: ({ creation_date }) => {
-          return format({
-            date: creation_date,
-            formatString: dateFormat
-          });
-        },
+        Component: ({ row }) => (
+          <Information
+            data={format({
+              date: row.creation_date,
+              formatString: dateFormat
+            })}
+          />
+        ),
         id: 'creation_date',
         label: Columns.CreationDate,
         sortField: 'creation_date',
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
-        getFormattedString: ({ expiration_date }) => {
-          return format({
-            date: expiration_date,
-            formatString: dateFormat
-          });
-        },
+        Component: ({ row }) => (
+          <Information
+            data={format({
+              date: row.expiration_date,
+              formatString: dateFormat
+            })}
+          />
+        ),
         id: 'expiration_date',
         label: Columns.ExpirationDate,
         sortField: 'expiration_date',
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
-        getFormattedString: ({ user }) => user.name,
+        Component: ({ row }) => <Information data={row.user.name} />,
         id: 'user_name',
         label: Columns.User,
         sortField: 'user.name',
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
-        getFormattedString: ({ creator }) => creator.name,
+        Component: ({ row }) => <Information data={row.creator.name} />,
         id: 'creator_name',
         label: Columns.Creator,
         sortField: 'creator.name',
         sortable: true,
-        type: ColumnType.string
+        type: ColumnType.component
       },
       {
         Component: ActionsColumn,
@@ -96,9 +108,8 @@ export const useColumns = (): UseColumns => {
         label: Columns.Actions,
         type: ColumnType.component
       }
-    ],
-    [timezone]
-  );
+    ];
+  }, [timezone]);
 
   return { columns, onResetColumns, onSelectColumns, selectedColumnIds };
 };
