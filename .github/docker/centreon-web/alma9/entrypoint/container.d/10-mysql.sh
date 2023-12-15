3d7a93745ee8d969b521112dd09da31322454e68
Kevin Duret <kduret@centreon.com>
2023-12-15 12:18:09 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
@@ -23,7 +23,7 @@ mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} < /usr/local/src/centreon_
 
 sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/centreon.conf.php
 sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/conf.pm
-mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'host'"
+mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
 
 #mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' IDENTIFIED BY 'centreon' WITH GRANT OPTION"
 mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
--- .github/docker/centreon-web/alma9/entrypoint/container.d/20-configuration_files.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/20-configuration_files.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Copy engine configuration files
-rm -rf /etc/centreon-engine/*
-cp -R /var/cache/centreon/config/engine/1/* /etc/centreon-engine/
-chown -R apache. /etc/centreon-engine/*
-chmod -R 664 /etc/centreon-engine/*
+# rm -rf /etc/centreon-engine/*
+# cp -R /var/cache/centreon/config/engine/1/* /etc/centreon-engine/
+# chown -R apache. /etc/centreon-engine/*
+# chmod -R 664 /etc/centreon-engine/*
 
-# Copy broker configuration files
-rm -rf /etc/centreon-broker/*
-cp -R /var/cache/centreon/config/broker/1/* /etc/centreon-broker/
-chown -R apache. /etc/centreon-broker/*
-chmod -R 664 /etc/centreon-broker/*
+# # Copy broker configuration files
+# rm -rf /etc/centreon-broker/*
+# cp -R /var/cache/centreon/config/broker/1/* /etc/centreon-broker/
+# chown -R apache. /etc/centreon-broker/*
+# chmod -R 664 /etc/centreon-broker/*
 
-su - apache -s /bin/bash -c "centreon -d -u admin -p Centreon\!2021 -a POLLERGENERATE -v 1"
-su - apache -s /bin/bash -c "centreon -d -u admin -p Centreon\!2021 -a POLLERTEST -v 1"
-su - apache -s /bin/bash -c "centreon -d -u admin -p Centreon\!2021 -a POLLERMOVE -v 1"
+centreon -u admin -p Centreon\!2021 -a POLLERGENERATE -v 1
+# centreon -u admin -p Centreon\!2021 -a POLLERTEST -v 1
+centreon -u admin -p Centreon\!2021 -a CFGMOVE -v 1
