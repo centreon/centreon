7c25c84bc6382ffd0224fd45957c0f10ea1da4b6
Kevin Duret <kduret@centreon.com>
2023-12-15 15:35:21 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
@@ -18,14 +18,30 @@
 #   fi
 # done
 
-# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} < /usr/local/src/centreon.sql
-# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} < /usr/local/src/centreon_storage.sql
-mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD}  <<< "SET autocommit=0; source /usr/local/src/centreon.sql; COMMIT;"
-mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD}  <<< "SET autocommit=0; source /usr/local/src/centreon_storage.sql; COMMIT;"
+# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD}  <<< "SET autocommit=0; source /usr/local/src/centreon.sql; COMMIT;"
+# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD}  <<< "SET autocommit=0; source /usr/local/src/centreon_storage.sql; COMMIT;"
 
-sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/centreon.conf.php
-sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/conf.pm
-mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
+# sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/centreon.conf.php
+# sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/conf.pm
+# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
 
-#mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' IDENTIFIED BY 'centreon' WITH GRANT OPTION"
-mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
+# #mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' IDENTIFIED BY 'centreon' WITH GRANT OPTION"
+# mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
+
+sed -i "s/localhost/${MYSQL_HOST}/g" /usr/share/centreon/www/install/tmp/database.json
+cd /usr/share/centreon/www/install/steps/process
+su apache -s /bin/bash -c "php configFileSetup.php"
+su apache -s /bin/bash -c "php installConfigurationDb.php"
+su apache -s /bin/bash -c "php installStorageDb.php"
+su apache -s /bin/bash -c "php createDbUser.php"
+su apache -s /bin/bash -c "SERVER_ADDR='127.0.0.1' php insertBaseConf.php"
+su apache -s /bin/bash -c "php partitionTables.php"
+su apache -s /bin/bash -c "php generationCache.php"
+su apache -s /bin/bash -c "rm -rf /usr/share/centreon/www/install"
+
+mysql -pcentreon -e "GRANT ALL ON *.* to 'root'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION"
+mysql -e "GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY 'centreon' WITH GRANT OPTION"
+
+mysql centreon < /tmp/sql/standard.sql
+mysql centreon < /tmp/sql/media.sql
+mysql centreon < /tmp/sql/openldap.sql
