e78de31d3ee82cf62921376e09ffbd35c10b5533
Tom Darneix <tomdar87@outlook.com>
2023-12-19 11:27:42 +0100

    Fix

--- centreon/packages/ui/src/Module/LicensedModule/index.tsx
+++ centreon/packages/ui/src/Module/LicensedModule/index.tsx
@@ -5,13 +5,19 @@ import LicenseCheck, { LicenseCheckProps } from './LicenseCheck';
 type Props = ModuleProps & LicenseCheckProps;
 
 const LicensedModule = ({
+  isFederatedComponent,
   moduleName,
   children,
   ...props
 }: Props): JSX.Element => {
   return (
     <Module {...props}>
-      <LicenseCheck moduleName={moduleName}>{children}</LicenseCheck>
+      <LicenseCheck
+        isFederatedComponent={isFederatedComponent}
+        moduleName={moduleName}
+      >
+        {children}
+      </LicenseCheck>
     </Module>
   );
 };
