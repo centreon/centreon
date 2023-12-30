93e6d9272f1b4c8de4b9995f11a090fa6f5d0c40
Kevin Duret <kduret@centreon.com>
2023-12-30 14:58:35 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# Delete ib_logfiles to avoid read/rebuild time on startup (~9s)
-# rm -f /var/lib/mysql/ib_logfile*
-
-# # Start database server.
-# service mysql start
-
-# # Wait for the database to be up and running.
-# while true ; do
-#   timeout 10 mysql -e 'SELECT id FROM nagios_server' centreon
-#   retval=$?
-#   if [ "$retval" = 0 ] ; then
-#     break ;
-#   else
-#     echo 'DB server is not yet responding.'
-#     sleep 1
-#   fi
-# done
-
 # Wait for the database to be up and running.
 while true ; do
   timeout 20 mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e 'SELECT User FROM user' mysql
@@ -32,7 +14,6 @@ done
 
 DATABASES_DUMP_DIR="/usr/local/src/sql/databases"
 for file in `ls $DATABASES_DUMP_DIR` ; do
-  # mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} <<< "SET autocommit=0; source $DATABASES_DUMP_DIR/$file; COMMIT;"
   myloader -h ${MYSQL_HOST} -u root -p ${MYSQL_ROOT_PASSWORD} -P 3306 -s $file -o -d $DATABASES_DUMP_DIR/$file
 done
 
@@ -45,7 +26,6 @@ fi
 
 mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
 
-#mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' IDENTIFIED BY 'centreon' WITH GRANT OPTION"
 mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
 
 sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/centreon.conf.php
