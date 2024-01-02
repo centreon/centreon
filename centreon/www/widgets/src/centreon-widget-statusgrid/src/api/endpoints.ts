03226b5f29f5acfd34cb6014d12ecf3759c896eb
Med1233 <melmeziani-ext@centreon.com>
2024-01-02 13:41:31 +0100

    change sort order to desc

--- centreon/www/widgets/src/centreon-widget-statusgrid/src/api/endpoints.ts
+++ centreon/www/widgets/src/centreon-widget-statusgrid/src/api/endpoints.ts
@@ -76,7 +76,7 @@ export const buildResourcesEndpoint = ({
         conditions: flatten(searchConditions)
       },
       sort: {
-        [sortBy]: 'ASC'
+        [sortBy]: 'DESC'
       }
     }
   });
