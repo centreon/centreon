1e1bac455b1f6f87548d5eb2a8800912c851475f
Exodiaaaa <imran.ouizerht@gmail.com>
2024-01-02 11:23:51 +0100

    Fix : Revert allow to manage groups and contacts

--- centreon/www/include/configuration/configObject/contactgroup/formContactGroup.php
+++ centreon/www/include/configuration/configObject/contactgroup/formContactGroup.php
@@ -178,8 +178,8 @@ $tpl = initSmartyTpl($path, $tpl);
 $tpl->assign(
     "helpattr",
     'TITLE, "' . _("Help") . '", CLOSEBTN, true, FIX, [this, 0, 5], BGCOLOR, "#ffff99", BORDERCOLOR, '
-    . '"orange", TITLEFONTCOLOR, "black", TITLEBGCOLOR, "orange", CLOSEBTNCOLORS, ["","black", "white", "red"],'
-    . ' WIDTH, -300, SHADOW, true, TEXTALIGN, "justify"'
+        . '"orange", TITLEFONTCOLOR, "black", TITLEBGCOLOR, "orange", CLOSEBTNCOLORS, ["","black", "white", "red"],'
+        . ' WIDTH, -300, SHADOW, true, TEXTALIGN, "justify"'
 );
 # prepare help texts
 $helptext = "";
