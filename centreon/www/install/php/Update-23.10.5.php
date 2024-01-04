4203a04dde1d7ecc6706269f33b9a0f58305ef0e
dmyios <diosypenko@centreon.com>
2024-01-04 11:01:26 +0100

    backport dev-23.10.x

--- centreon/www/install/php/Update-23.10.5.php
+++ centreon/www/install/php/Update-23.10.5.php
@@ -18,3 +18,54 @@
  * For more information : contact@centreon.com
  *
  */
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+$centreonLog = new CentreonLog();
+
+//error specific content
+$versionOfTheUpgrade = 'UPGRADE - 23.10.5: ';
+$errorMessage = '';
+
+// ------------ INSERT / UPDATE / DELETE
+$updateTopologyForCloudNotifications = function(CentreonDB $pearDB): void {
+    $statement = $pearDB->query(
+        <<<'SQL'
+            SELECT 1 FROM `topology` WHERE `topology_url` = '/configuration/notifications'
+            SQL
+    );
+
+    if (false === (bool) $statement->fetch(\PDO::FETCH_COLUMN)) {
+        $pearDB->query(
+            <<<'SQL'
+                INSERT INTO `topology`
+                    (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`,
+                    `topology_order`, `topology_group`,`topology_feature_flag`)
+                VALUES
+                    ('Notifications', '/configuration/notifications', '1', '1', 604, 60413, 1, 1,'notification')
+                SQL
+        );
+    }
+};
+
+try {
+    $errorMessage = '';
+    // Transactional queries
+    if (! $pearDB->inTransaction()) {
+        $pearDB->beginTransaction();
+    }
+    $errorMessage = 'Unable to update topology for Cloud Notifications';
+    $updateTopologyForCloudNotifications($pearDB);
+} catch (\Exception $ex) {
+    if ($pearDB->inTransaction()) {
+        $pearDB->rollBack();
+    }
+
+    $centreonLog->insertLog(
+        4,
+        $versionOfTheUpgrade . $errorMessage
+        . ' - Code : ' . (int) $ex->getCode()
+        . ' - Error : ' . $ex->getMessage()
+        . ' - Trace : ' . $ex->getTraceAsString()
+    );
+
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int) $ex->getCode(), $ex);
+}
