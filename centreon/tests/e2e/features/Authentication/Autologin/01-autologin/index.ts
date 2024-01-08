7f13980c1ecdead9a8e58e8a325e4c0bcedf23be
a-eljazouly <aeljazouly-ext@centreon.com>
2024-01-08 11:12:20 +0100

    trigger e2e test to fail

--- centreon/tests/e2e/features/Authentication/Autologin/01-autologin/index.ts
+++ centreon/tests/e2e/features/Authentication/Autologin/01-autologin/index.ts
@@ -8,18 +8,7 @@ before(() => {
 });
 
 beforeEach(() => {
-  cy.intercept({
-    method: 'GET',
-    url: '/centreon/api/internal.php?object=centreon_topology&action=navigationList'
-  }).as('getNavigationList');
-  cy.intercept({
-    method: 'GET',
-    url: '/centreon/include/common/userTimezone.php'
-  }).as('getTimeZone');
-  cy.intercept({
-    method: 'GET',
-    url: '/centreon/api/latest/users/filters/events-view?page=1&limit=100'
-  }).as('getLastestUserFilters');
+ 
 });
 
 Given('an administrator is logged in the platform', () => {
