aad0258bc0361eb23bb76ab85d087c5a62ef6220
Kevin Duret <kduret@centreon.com>
2024-01-11 17:47:37 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/10-mysql.sh
@@ -5,6 +5,7 @@ while true ; do
   timeout 20 mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} -e 'SELECT User FROM user' mysql
   retval=$?
   if [ "$retval" = 0 ] ; then
+    echo 'DB server is running.'
     break ;
   else
     echo 'DB server is not yet responding.'
