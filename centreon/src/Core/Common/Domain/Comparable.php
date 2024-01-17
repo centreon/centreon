fe374296af33a07f38041b95d7ed8eaaa10726d5
Laurent Calvet <lcalvet@centreon.com>
2024-01-17 08:33:14 +0100

    First update

--- centreon/src/Core/Common/Domain/Comparable.php
+++ centreon/src/Core/Common/Domain/Comparable.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,14 @@
  * For more information : contact@centreon.com
  *
  */
-declare(strict_types=1);
 
-namespace Centreon\Domain\Repository;
+declare(strict_types = 1);
 
-class RepositoryException extends \Exception
+namespace Core\Common\Domain;
+
+interface Comparable
 {
-    public static function notYetImplemented(): self
-    {
-        return new self(_('Not yet implemented'));
-    }
+    public function isEqual(object $object): bool;
+
+    public function getEqualityHash(): string;
 }
