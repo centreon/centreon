8c867110277e3a2325762d4b8278d8c3f3389a83
Arnaud Buathier <abuathier@centreon.com>
2024-01-26 11:37:29 +0100

    chore(phpstan): setup phpstan on centreon-open-tickets (#3075)

--- centreon-open-tickets/www/modules/centreon-open-tickets/views/rules/ajax/actions/closeTicket.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/views/rules/ajax/actions/closeTicket.php
@@ -64,7 +64,7 @@ $centreon_provider = new $classname(
 
 // We get Host or Service
 $selected_values = explode(',', $get_information['form']['selection']);
-$db_storage = new centreonDBManager('centstorage');
+$db_storage = new CentreonDBManager('centstorage');
 
 $problems = array();
 $tickets = array();
@@ -144,7 +144,7 @@ try {
     $centreon_provider->closeTicket($tickets);
     require_once $centreon_path . 'www/class/centreonExternalCommand.class.php';
     $oreon = $_SESSION['centreon'];
-    $external_cmd = new CentreonExternalCommand($oreon);
+    $external_cmd = new CentreonExternalCommand();
     $method_external_name = 'set_process_command';
     if (method_exists($external_cmd, $method_external_name) == false) {
         $method_external_name = 'setProcessCommand';
