75cef323613606ff8ab184be5825959a95093200
Nouha-ElAbrouki <97687698+Noha-ElAbrouki@users.noreply.github.com>
2024-01-30 14:57:25 +0100

    Update centreon/www/front_src/src/ApiTokens/translatedLabels.ts
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>

--- centreon/www/front_src/src/ApiTokens/translatedLabels.ts
+++ centreon/www/front_src/src/ApiTokens/translatedLabels.ts
@@ -22,6 +22,6 @@ export const labelTokenCreated = 'Token has been created';
 export const labelSecurityToken =
   'For security reasons, the token can only be displayed once. Store it carefully.';
 export const labelMsgConfirmationDeletionToken =
-  'You are about to delete the token <b> {{tokenName}}</b>. This is an irreversible <br> action. If you process, all requests made with this token will be <br> rejected. Do you want to process anyway ?';
+  'You are about to delete the token <b> {{tokenName}}</b>. This action <br> cannot be undone. If you proceed, all requests made using this token will be <br> rejected. Do you want to delete the token?';
 export const labelDeleteToken = 'Delete Token';
 export const labelTokenDeletedSuccessfully = 'Token deleted successfully';
