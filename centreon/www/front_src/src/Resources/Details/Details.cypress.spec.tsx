76a80591230c7b8ae73f02e24b7fb9198f3c6da7
nouha <nelabrouki-ext@centreon.com>
2024-01-30 12:38:31 +0000

    stabilize cp test

--- centreon/www/front_src/src/Resources/Details/Details.cypress.spec.tsx
+++ centreon/www/front_src/src/Resources/Details/Details.cypress.spec.tsx
@@ -580,6 +580,7 @@ describe('Details', () => {
     cy.findByTestId('commentArea').should('be.visible');
     cy.findByTestId(labelCancel).should('be.visible').should('be.enabled');
     cy.findByTestId(labelSave).should('be.visible').should('be.disabled');
+    cy.findByTestId('addComment').scrollIntoView();
 
     cy.makeSnapshot();
   });
@@ -617,6 +618,7 @@ describe('Details', () => {
     });
 
     cy.contains(labelYourCommentSent);
+    cy.findByTestId('addComment').scrollIntoView();
 
     cy.makeSnapshot();
   });
@@ -643,6 +645,7 @@ describe('Details', () => {
       .click();
 
     cy.findByTestId('commentArea').should('not.exist');
+    cy.findByTestId('addComment').scrollIntoView();
 
     cy.makeSnapshot();
   });
