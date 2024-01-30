ff7130a35ac8bd80e34913ba56b1bfb06faf5bbd
dmyios <diosypenko@centreon.com>
2024-01-30 17:36:30 +0100

    Edit Rule: added click in listing & get initial values

--- centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInitialValues/initialValues.ts
+++ centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInitialValues/initialValues.ts
@@ -1,13 +1,50 @@
-import { ResourceAccessRule } from '../../models';
+/* eslint-disable no-param-reassign */
+import { equals, prop } from 'ramda';
 
-export const getEmptyInitialValues = (): ResourceAccessRule => ({
+import {
+  Dataset,
+  DatasetFilter,
+  ResourceAccessRule,
+  ResourceTypeEnum
+} from '../../models';
+
+export const getEmptyInitialValues = (): Omit<ResourceAccessRule, 'id'> => ({
   contactGroups: [],
   contacts: [],
-  datasetFilters: [[{ resourceType: undefined, resources: [] }]],
+  datasetFilters: [[{ resourceType: ResourceTypeEnum.Empty, resources: [] }]],
   description: '',
   isActivated: true,
   name: ''
 });
+
+const formatDatasetFilter = (datasetFilter: DatasetFilter): Array<Dataset> => {
+  let datasets: Array<Dataset> = [];
+  while (!equals(prop('datasetFilter', datasetFilter), null)) {
+    datasets = [
+      ...datasets,
+      {
+        resourceType: datasetFilter.resourceType,
+        resources: datasetFilter.resources
+      }
+    ];
+    datasetFilter = prop('datasetFilter', datasetFilter);
+  }
+
+  datasets = [
+    ...datasets,
+    {
+      resourceType: datasetFilter.resourceType,
+      resources: datasetFilter.resources
+    }
+  ];
+
+  return datasets;
+};
+
+const formatDatasetFilters = (
+  datasetFilters: Array<DatasetFilter>
+): Array<Array<Dataset>> =>
+  datasetFilters.map((datasetFilter) => formatDatasetFilter(datasetFilter));
 
 export const getInitialValues = ({
   contactGroups,
@@ -16,10 +53,10 @@ export const getInitialValues = ({
   description,
   isActivated,
   name
-}): ResourceAccessRule => ({
+}): Omit<ResourceAccessRule, 'id'> => ({
   contactGroups,
   contacts,
-  datasetFilters,
+  datasetFilters: formatDatasetFilters(datasetFilters),
   description,
   isActivated,
   name
--- centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInputs/styles/AddDatasetButton.styles.ts
+++ centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInputs/styles/AddDatasetButton.styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from 'tss-react/mui';
 
 export const useAddDatasetButtonStyles = makeStyles()((theme) => ({
   addDatasetButton: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(2),
     width: '25%'
   }
 }));
