d1c90cf0a0ee758ce8f05b591b36119949fb54db
dmyios <diosypenko@centreon.com>
2024-02-09 10:18:52 +0100

    added check for contact_oreon array key

--- centreon/www/include/configuration/configObject/contact/DB-Func.php
+++ centreon/www/include/configuration/configObject/contact/DB-Func.php
@@ -635,7 +635,7 @@ function insertContact($ret = array())
         $ret = $form->getSubmitValues();
     }
     $ret["contact_name"] = $centreon->checkIllegalChar($ret["contact_name"]);
-    if ($ret['contact_oreon']['contact_oreon'] === '1') {
+    if (isset($ret['contact_oreon']) && $ret['contact_oreon']['contact_oreon'] === '1') {
         $ret['reach_api_rt']['reach_api_rt'] = '1';
     }
 
