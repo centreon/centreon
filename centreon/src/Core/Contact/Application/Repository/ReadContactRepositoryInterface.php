6ac5b08d5d387ac0c5632791cda209c5871c4f47
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2024-02-12 09:42:01 +0100

    fix(dashboards): fix filter of contacts and contactgroups on dashboard listing and detail (#3312)

--- centreon/src/Core/Contact/Application/Repository/ReadContactRepositoryInterface.php
+++ centreon/src/Core/Contact/Application/Repository/ReadContactRepositoryInterface.php
@@ -107,7 +107,6 @@ interface ReadContactRepositoryInterface
      * @throws \Throwable
      *
      * @return Contact[]
-     *                   /
      */
     public function findAdminsByIds(array $contactIds): array;
 }
