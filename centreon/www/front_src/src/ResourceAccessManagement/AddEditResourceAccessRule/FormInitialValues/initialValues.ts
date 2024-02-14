595815ac02f2a5a4fb4a320a0da411d661ed6ef2
Dmytro Iosypenko <108675430+dmyios@users.noreply.github.com>
2024-02-14 13:27:08 +0100

    feat(Resource Access Management): Edit Resource Access Rule implementation (#3223)
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    Co-authored-by: jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>

--- centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInitialValues/initialValues.ts
+++ centreon/www/front_src/src/ResourceAccessManagement/AddEditResourceAccessRule/FormInitialValues/initialValues.ts
@@ -1,13 +1,57 @@
-import { ResourceAccessRule } from '../../models';
+/* eslint-disable no-param-reassign */
+import { equals, prop } from 'ramda';
 
-export const getEmptyInitialValues = (): ResourceAccessRule => ({
+import {
+  Dataset,
+  DatasetFilter,
+  ResourceAccessRule,
+  ResourceTypeEnum
+} from '../../models';
+
+export const getEmptyInitialValues = (): Omit<ResourceAccessRule, 'id'> => ({
   contactGroups: [],
   contacts: [],
-  datasetFilters: [[{ resourceType: undefined, resources: [] }]],
+  datasetFilters: [[{ resourceType: ResourceTypeEnum.Empty, resources: [] }]],
   description: '',
   isActivated: true,
   name: ''
 });
+
+const nestedObjectToArray = (
+  element: DatasetFilter,
+  datasets: Array<Dataset>
+): Array<Dataset> => {
+  if (equals(prop('datasetFilter', element), null)) {
+    return [
+      ...datasets,
+      {
+        resourceType: element.resourceType,
+        resources: element.resources
+      }
+    ];
+  }
+
+  datasets = [
+    ...datasets,
+    {
+      resourceType: element.resourceType,
+      resources: element.resources
+    }
+  ];
+
+  return nestedObjectToArray(prop('datasetFilter', element), datasets);
+};
+
+const formatDatasetFilter = (datasetFilter: DatasetFilter): Array<Dataset> => {
+  const datasets: Array<Dataset> = [];
+
+  return nestedObjectToArray(datasetFilter, datasets);
+};
+
+const formatDatasetFilters = (
+  datasetFilters: Array<DatasetFilter>
+): Array<Array<Dataset>> =>
+  datasetFilters.map((datasetFilter) => formatDatasetFilter(datasetFilter));
 
 export const getInitialValues = ({
   contactGroups,
@@ -16,10 +60,10 @@ export const getInitialValues = ({
   description,
   isActivated,
   name
-}): ResourceAccessRule => ({
+}): Omit<ResourceAccessRule, 'id'> => ({
   contactGroups,
   contacts,
-  datasetFilters,
+  datasetFilters: formatDatasetFilters(datasetFilters),
   description,
   isActivated,
   name
