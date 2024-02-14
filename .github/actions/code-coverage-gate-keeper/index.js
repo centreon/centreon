2c3b598b63f22ea2151e907d7a190725159b4a4e
Tom Darneix <tomdar87@outlook.com>
2024-02-14 11:31:23 +0100

    Fix typo

--- .github/actions/code-coverage-gate-keeper/index.js
+++ .github/actions/code-coverage-gate-keeper/index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 const fs = require('fs');
-const { exeSync } = require('child_process');
+const { execSync } = require('child_process');
 
 try {
   const baseCodeCoveragePercentage = core.getInput('base_code_coverage_percentage');
 
-  exeSync('pnpx nyc report --reporter json --report-dir /tmp')
+  execSync('pnpx nyc report --reporter json --report-dir /tmp')
 
   const coverageFile = fs.readFileSync('/tmp/coverage-final.json');
   const coverage = JSON.parse(coverageFile)
