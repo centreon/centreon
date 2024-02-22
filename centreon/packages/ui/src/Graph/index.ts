7950434acab5ce5a4ce88d99affaa8df7f15f374
Tom Darneix <tomdar87@outlook.com>
2024-02-22 11:48:45 +0100

    enh(core): Improve code coverage for web and Centreon-ui (#3331)

--- centreon/packages/ui/src/Graph/index.ts
+++ centreon/packages/ui/src/Graph/index.ts
@@ -2,4 +2,5 @@ export { default as LineChart } from './LineChart';
 export { Gauge } from './Gauge';
 export { SingleBar } from './SingleBar';
 export { Text as GraphText } from './Text';
+
 export { HeatMap } from './HeatMap';
