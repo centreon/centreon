3a3dba91200ea93c5a211285862f5ab2e969021a
nouha <nelabrouki-ext@centreon.com>
2024-02-26 14:42:31 +0000

    add tr

--- centreon/www/front_src/src/ApiTokens/TokenListing/Actions/Filter/DateInput/DateInputWrapper.tsx
+++ centreon/www/front_src/src/ApiTokens/TokenListing/Actions/Filter/DateInput/DateInputWrapper.tsx
@@ -1,11 +1,17 @@
 import { useAtom } from 'jotai';
+import { useTranslation } from 'react-i18next';
 
 import { creationDateAtom, expirationDateAtom } from '../atoms';
 import { Property } from '../models';
+import {
+  labelCreationDate,
+  labelExpirationDate
+} from '../../../../translatedLabels';
 
 import DateInput from './DateInput';
 
 const DateInputWrapper = (): JSX.Element => {
+  const { t } = useTranslation();
   const [creationDate, setCreationDate] = useAtom(creationDateAtom);
   const [expirationDate, setExpirationDate] = useAtom(expirationDateAtom);
 
@@ -13,12 +19,12 @@ const DateInputWrapper = (): JSX.Element => {
     <>
       <DateInput
         dataDate={{ date: creationDate, setDate: setCreationDate }}
-        label="Creation date"
+        label={t(labelCreationDate)}
         property={Property.last}
       />
       <DateInput
         dataDate={{ date: expirationDate, setDate: setExpirationDate }}
-        label="Expiration date"
+        label={t(labelExpirationDate)}
         property={Property.in}
       />
     </>
