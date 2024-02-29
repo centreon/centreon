119300f6b85a3859c104ef96139379bfa3603fdb
Tom Darneix <tomdar87@outlook.com>
2024-02-29 16:11:56 +0100

    enh(UI): Use modal for delete actions on Dashboard (#3450)
    Co-authored-by: smau <102975713+smau-centreon@users.noreply.github.com>
    Co-authored-by: Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>

--- centreon/www/front_src/src/Dashboards/atoms.ts
+++ centreon/www/front_src/src/Dashboards/atoms.ts
@@ -3,3 +3,4 @@ import { atom } from 'jotai';
 import { Dashboard } from './api/models';
 
 export const isSharesOpenAtom = atom<Dashboard | null>(null);
+export const dashboardToDeleteAtom = atom<Dashboard | null>(null);
