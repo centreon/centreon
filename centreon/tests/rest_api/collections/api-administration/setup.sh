7160a5ef63ff07a0dd555d5f95871ac68d0a4bee
Kevin Duret <kduret@centreon.com>
2024-03-01 14:30:16 +0100

    fix

--- centreon/tests/rest_api/collections/api-administration/setup.sh
+++ centreon/tests/rest_api/collections/api-administration/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml cp centreon.png web:/usr/share/centreon/www/img/media/test/centreon.png
+docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml cp $(dirname $0)/images/centreon.png web:/usr/share/centreon/www/img/media/test/centreon.png
