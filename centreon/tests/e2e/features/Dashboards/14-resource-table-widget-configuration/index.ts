ac476f1f2b915ce390f50f25a61514264164a4f1
MedWassCh <wchaabane@centreon.com>
2024-03-01 10:22:04 +0100

    fix issue

--- centreon/tests/e2e/features/Dashboards/14-resource-table-widget-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/14-resource-table-widget-configuration/index.ts
@@ -368,6 +368,7 @@ Given('a dashboard featuring two resource table widgets', () => {
   cy.wait('@resourceRequest');
   cy.getByTestId({ testId: 'MoreHorizIcon' }).click();
   cy.getByTestId({ testId: 'ContentCopyIcon' }).click();
+  cy.wait('@resourceRequest');
 });
 
 When('the dashboard administrator user deletes one of the widgets', () => {
