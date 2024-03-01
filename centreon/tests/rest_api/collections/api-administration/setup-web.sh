4bce0a994de3716d20f6a4eb9cea9f146ee6c6ac
Kevin Duret <kduret@centreon.com>
2024-03-01 14:44:27 +0100

    fix

--- centreon/tests/rest_api/collections/api-administration/setup-web.sh
+++ centreon/tests/rest_api/collections/api-administration/setup-web.sh
@@ -3,5 +3,11 @@
 mysql -hdb -uroot -pcentreon centreon -e "UPDATE options SET options.value = 1 WHERE options.key = 'inheritance_mode'"
 
 mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir (dir_name, dir_alias, dir_comment) VALUES ('test', 'test', 'my test')"
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img (img_name, img_path, img_comment) VALUES ('centreon', 'centreon.png', 'centreon test image')"
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir_relation (dir_dir_parent_id, img_img_id) VALUES (SELECT id FROM view_img_dir WHERE dir_name = 'test' LIMIT 1, SELECT id FROM view_img WHERE img_name = 'centreon' LIMIT 1)"
+mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img (img_name, img_path, img_comment) VALUES ('Admin-Potter', 'Admin-Potter.png', 'Admin-Potter test image')"
+mysql -hdb -uroot -pcentreon centreon -e "
+INSERT INTO view_img_dir_relation (dir_dir_parent_id, img_img_id)
+SELECT vid.dir_id, vi.img_id FROM view_img_dir vid, view_img vi
+WHERE vid.dir_name = 'test'
+AND vi.img_name = 'Admin-Potter'
+"
+
