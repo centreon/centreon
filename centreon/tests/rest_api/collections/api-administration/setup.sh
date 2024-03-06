507d9182c001ac0d02366341dbe11427d06e2334
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2024-03-06 22:25:28 +0100

    fix path img naming on the tmp directory and test empty data

--- centreon/tests/rest_api/collections/api-administration/setup.sh
+++ centreon/tests/rest_api/collections/api-administration/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml exec web mkdir -p /usr/share/centreon/www/img/media/test
-docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml cp $(dirname $0)/images/Admin-Potter.png web:/tmp
+docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml cp $(dirname $0)/images/Admin-Potter.png web:/tmp/Admin-Potter.png
