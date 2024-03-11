71c302a5518a5f68d00550ea9008f57074a487ab
Med1233 <melmeziani-ext@centreon.com>
2024-03-11 21:01:23 +0000

    fix tests

--- centreon/packages/ui/src/components/DataTable/DataTable.cypress.spec.tsx
+++ centreon/packages/ui/src/components/DataTable/DataTable.cypress.spec.tsx
@@ -12,11 +12,7 @@ const data = Array(5)
     title: `Entity ${idx}`
   }));
 
-const initializeDataTableGrid = ({
-  hasActions,
-  hasCardAction,
-  canDelete
-}): void => {
+const initializeDataTableGrid = ({ hasActions, hasCardAction }): void => {
   cy.viewport(1200, 590);
   cy.mount({
     Component: (
@@ -27,14 +23,18 @@ const initializeDataTableGrid = ({
             hasActions={hasActions}
             hasCardAction={hasCardAction}
             key={title}
-            labelsDelete={{
-              cancel: 'Cancel',
-              confirm: {
-                label: 'Delete'
-              }
+            labels={{
+              labelDelete: 'Delete',
+              labelDuplicate: 'Duplicate',
+              labelEditProperties: 'Edit Properties',
+              labelMoreActions: 'More actions',
+              labelShare: 'Share'
             }}
             title={title}
-            onDelete={canDelete ? cy.stub() : undefined}
+            onDelete={() => cy.stub()}
+            onDuplicate={() => cy.stub()}
+            onEdit={() => cy.stub()}
+            onEditAccessRights={() => cy.stub()}
           />
         ))}
       </DataTable>
@@ -94,7 +94,6 @@ const initializeDataTableListing = (): void => {
 describe('DataTable: Grid', () => {
   it('displays items with title and description only', () => {
     initializeDataTableGrid({
-      canDelete: false,
       hasActions: false,
       hasCardAction: false
     });
@@ -109,32 +108,33 @@ describe('DataTable: Grid', () => {
 
   it('displays items with actions', () => {
     initializeDataTableGrid({
-      canDelete: false,
       hasActions: true,
       hasCardAction: false
     });
 
-    cy.findAllByLabelText('edit access rights').should('have.length', 5);
-    cy.findAllByLabelText('edit').should('have.length', 5);
+    cy.findAllByLabelText('Share').should('have.length', 5);
+    cy.findAllByLabelText('More actions').should('have.length', 5);
 
     cy.makeSnapshot();
   });
 
-  it('displays items with delete action', () => {
+  it('displays actions when more actions button was clicked', () => {
     initializeDataTableGrid({
-      canDelete: true,
       hasActions: true,
       hasCardAction: false
     });
 
-    cy.findAllByLabelText('delete').should('have.length', 5);
+    cy.findAllByLabelText('More actions').eq(0).click();
+
+    cy.findByLabelText('Duplicate').should('be.visible');
+    cy.findByLabelText('Delete').should('be.visible');
+    cy.findByLabelText('Edit Properties').should('be.visible');
 
     cy.makeSnapshot();
   });
 
   it('displays items with card action only', () => {
     initializeDataTableGrid({
-      canDelete: false,
       hasActions: false,
       hasCardAction: true
     });
@@ -146,15 +146,13 @@ describe('DataTable: Grid', () => {
 
   it('displays items with card action and bottom actions', () => {
     initializeDataTableGrid({
-      canDelete: true,
       hasActions: true,
       hasCardAction: true
     });
 
     cy.findAllByLabelText('view').should('have.length', 5);
-    cy.findAllByLabelText('delete').should('have.length', 5);
-    cy.findAllByLabelText('edit access rights').should('have.length', 5);
-    cy.findAllByLabelText('edit').should('have.length', 5);
+    cy.findAllByLabelText('Share').should('have.length', 5);
+    cy.findAllByLabelText('More actions').should('have.length', 5);
 
     cy.makeSnapshot();
   });
