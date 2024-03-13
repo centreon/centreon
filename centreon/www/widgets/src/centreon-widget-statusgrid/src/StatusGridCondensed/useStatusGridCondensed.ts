1023f6b785b0d8865ca6cbc0a512f24ec1d5bd07
Thebarda <tomdar87@outlook.com>
2024-03-13 17:55:19 +0100

    Fix condensed view

--- centreon/www/widgets/src/centreon-widget-statusgrid/src/StatusGridCondensed/useStatusGridCondensed.ts
+++ centreon/www/widgets/src/centreon-widget-statusgrid/src/StatusGridCondensed/useStatusGridCondensed.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 
-import { filter, isNil, map, pipe } from 'ramda';
+import { filter, intersection, isNil, map, pipe, toUpper } from 'ramda';
 
 import { SeverityCode, useFetchQuery, useRefreshInterval } from '@centreon/ui';
 
@@ -13,6 +13,7 @@ import {
 import { buildResourcesEndpoint } from '../api/endpoints';
 
 import { getStatusesEndpoint } from './api/endpoints';
+import { getStatusNamesPerResourceType } from './utils';
 
 interface FormattedStatus {
   count: StatusDetail;
@@ -46,14 +47,19 @@ export const useStatusGridCondensed = ({
     refreshIntervalCustom
   });
 
+  const statusesToUse = pipe(
+    getStatusNamesPerResourceType,
+    map(toUpper),
+    intersection(statuses)
+  )(resourceType);
+
   const { data, isLoading } = useFetchQuery<StatusType>({
     getEndpoint: () =>
       buildResourcesEndpoint({
         baseEndpoint: getStatusesEndpoint(resourceType),
-        page: undefined,
+        page: 0,
         resources,
-        statuses,
-        type: resourceType
+        statuses: statusesToUse
       }),
     getQueryKey: () => [
       'statusgrid',
