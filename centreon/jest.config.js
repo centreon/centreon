fc87b6c26c3cbc8b170fb7f70b83a040d9f1ede4
nouha <nelabrouki-ext@centreon.com>
2024-03-15 15:24:47 +0000

    fixes

--- centreon/jest.config.js
+++ centreon/jest.config.js
@@ -1,16 +1,14 @@
 const { mergeDeepRight } = require('ramda');
 
-module.exports = mergeDeepRight(
-  require('@centreon/js-config/jest'),
-  {
-    moduleNameMapper: {
-      'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
-    },
-    roots: ['<rootDir>/www/front_src/src/'],
-    setupFilesAfterEnv: [
-      '@testing-library/jest-dom/extend-expect',
-      '<rootDir>/setupTest.js',
-    ],
-    testEnvironment: 'jsdom',
+module.exports = mergeDeepRight(require('@centreon/js-config/jest'), {
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+    'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
   },
-);
+  roots: ['<rootDir>/www/front_src/src/'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    '<rootDir>/setupTest.js',
+  ],
+  testEnvironment: 'jsdom',
+});
