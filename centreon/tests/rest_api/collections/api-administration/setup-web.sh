aa366ca8336637711a04c04dd4a6d71953790b7e
Marion Debauchez <mdebauchez@centreon.com>
2024-03-27 17:30:44 +0100

    fix

--- centreon/tests/rest_api/collections/api-administration/setup-web.sh
+++ centreon/tests/rest_api/collections/api-administration/setup-web.sh
@@ -3,20 +3,3 @@
 mysql -hdb -uroot -pcentreon centreon -e "UPDATE options SET options.value = 1 WHERE options.key = 'inheritance_mode'"
 
 mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir (dir_name, dir_alias, dir_comment) VALUES ('ppm', 'ppm', 'ppm directory')"
-
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir (dir_name, dir_alias, dir_comment) VALUES ('ppm', 'ppm', 'ppm directory')"
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img (img_name, img_path, img_comment) VALUES ('Carapuce.png', 'Carapuce.png', 'Carapuce test image')"
-mysql -hdb -uroot -pcentreon centreon -e "
-INSERT INTO view_img_dir_relation (dir_dir_parent_id, img_img_id)
-SELECT vid.dir_id, vi.img_id FROM view_img_dir vid, view_img vi
-WHERE vid.dir_name = 'ppm'
-AND vi.img_name = 'Carapuce.png'
-"
-
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img (img_name, img_path, img_comment) VALUES ('Herbizarre.png', 'Herbizarre.png', 'Herbizarre test image')"
-mysql -hdb -uroot -pcentreon centreon -e "
-INSERT INTO view_img_dir_relation (dir_dir_parent_id, img_img_id)
-SELECT vid.dir_id, vi.img_id FROM view_img_dir vid, view_img vi
-WHERE vid.dir_name = 'ppm'
-AND vi.img_name = 'Herbizarre.png'
-"
