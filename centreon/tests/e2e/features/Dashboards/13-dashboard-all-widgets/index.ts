38887ae4b974e4df8bf886f9b3ab39885a661386
MedWassCh <wchaabane@centreon.com>
2024-03-28 11:49:36 +0100

    some fix

--- centreon/tests/e2e/features/Dashboards/13-dashboard-all-widgets/index.ts
+++ centreon/tests/e2e/features/Dashboards/13-dashboard-all-widgets/index.ts
@@ -154,13 +154,7 @@ before(() => {
   ]);
 
   cy.logoutViaAPI();
-  const apacheUser = Cypress.env('WEB_IMAGE_OS').includes('alma')
-    ? 'apache'
-    : 'www-data';
-  cy.execInContainer({
-    command: `su -s /bin/sh ${apacheUser} -c "/usr/bin/env php -q /usr/share/centreon/cron/centAcl.php"`,
-    name: 'web'
-  });
+  cy.applyAcl();
 });
 
 beforeEach(() => {
