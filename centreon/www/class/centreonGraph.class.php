9c433217990de4c5e69cbbecabd705be748b5747
cbadr-ext <cbadr-ext@centreon.com>
2024-04-03 10:39:51 +0000

    use the heredoc /nowdoc

--- centreon/www/class/centreonGraph.class.php
+++ centreon/www/class/centreonGraph.class.php
@@ -398,7 +398,7 @@ class CentreonGraph
      */
     private static function quote($elem)
     {
-        return $elem ;
+        return "'" . $elem . "'";
     }
 
     /**
@@ -408,7 +408,7 @@ class CentreonGraph
      */
     private static function vquote($elem)
     {
-        return  substr($elem, 1, strlen($elem) - 1) ;
+        return "'" . substr($elem, 1, strlen($elem) - 1) . "'";
     }
 
 
@@ -514,7 +514,7 @@ class CentreonGraph
             $query = <<<SQL
                 SELECT 
                     vmetric_id,
-                FROM virtual_metrics,
+                FROM virtual_metrics
                 WHERE $queryCondition
                 ORDER BY vmetric_name
               SQL;
