d9a78e2bf87cc51f150a7f02414778d6b08eaee1
Kevin Duret <kduret@centreon.com>
2024-04-04 11:45:41 +0200

    fix

---
+++ centreon/src/Migrations/Migration000020040500.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+require_once __DIR__  . '/../../www/class/centreonLog.class.php';
+
+use Centreon\Domain\Log\LoggerTrait;
+use Core\Migration\Application\Repository\LegacyMigrationInterface;
+use Core\Migration\Infrastructure\Repository\AbstractCoreMigration;
+use Pimple\Container;
+
+class Migration000020040500 extends AbstractCoreMigration implements LegacyMigrationInterface
+{
+    use LoggerTrait;
+
+    private const VERSION = '20.04.5';
+
+    public function __construct(
+        private readonly Container $dependencyInjector,
+    ) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getVersion(): string
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription(): string
+    {
+        return sprintf(_('Update to %s'), self::VERSION);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function up(): void
+    {
+        $pearDB = $this->dependencyInjector['configuration_db'];
+
+
+        /* Update-DB-20.04.5.sql */
+
+        $pearDB->query(
+            <<<'SQL'
+                CREATE TABLE `user_filter` (
+                    `id` int(11) NOT NULL AUTO_INCREMENT,
+                    `name` varchar(255) NOT NULL,
+                    `user_id` int(11) NOT NULL,
+                    `page_name` varchar(255) NOT NULL,
+                    `criterias` text,
+                    PRIMARY KEY (`id`),
+                    CONSTRAINT `filter_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+                SQL
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function down(): void
+    {
+        // nothing
+    }
+}
