c28a2f458aa620c6feb6c82c72bc380cfe6fe542
Thebarda <tomdar87@outlook.com>
2024-04-08 10:55:44 +0200

    Fix lint

--- centreon/www/front_src/src/federatedModules/atoms.ts
+++ centreon/www/front_src/src/federatedModules/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from 'jotai';
 
-import { FederatedModule, FederatedWidgetProperties } from './models';
+import { FederatedWidgetProperties } from './models';
 
 export const federatedWidgetsPropertiesAtom =
   atom<Array<FederatedWidgetProperties> | null>(null);
