41a83930c9fe2528d8763c3a9a0bb683a016d492
Thebarda <tomdar87@outlook.com>
2024-04-09 10:04:28 +0200

    Widget optimization

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Layout/Panel/PanelHeader.tsx
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Layout/Panel/PanelHeader.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { equals } from 'ramda';
 import { Link } from 'react-router-dom';
 
-import { CardHeader } from '@mui/material';
+import { CardHeader, Typography } from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import DvrIcon from '@mui/icons-material/Dvr';
 
@@ -100,7 +100,11 @@ const PanelHeader = ({
         )
       }
       className={classes.panelHeader}
-      title={panel?.options?.name || ''}
+      title={
+        <Typography className={classes.panelTitle}>
+          {panel?.options?.name || ''}
+        </Typography>
+      }
     />
   );
 };
