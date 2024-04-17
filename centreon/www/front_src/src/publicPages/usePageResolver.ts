7e004b5e50d614fa458fdc93e50649d2bf088a9a
Thebarda <tomdar87@outlook.com>
2024-04-17 10:34:43 +0200

    Improve page resolver about dynamic segments

--- centreon/www/front_src/src/publicPages/usePageResolver.ts
+++ centreon/www/front_src/src/publicPages/usePageResolver.ts
@@ -1,27 +1,63 @@
-import { LazyExoticComponent, useMemo } from 'react';
+import { ComponentType, LazyExoticComponent, useMemo, useRef } from 'react';
 
 import { useLocation } from 'react-router-dom';
+import { fromPairs, replace } from 'ramda';
 
 import { routes } from './routes';
 
-export const usePageResolver = ():
-  | [string, LazyExoticComponent<() => JSX.Element>]
-  | undefined => {
+const getURLMatchRegExp = (url: string): RegExp => {
+  const regexp = replace(/\[(\w+|\w+(-|_)\w+)\]/g, '(\\w+)', url);
+
+  return new RegExp(`^${regexp}$`, 'g');
+};
+
+interface UsePageResolverState {
+  matchedRoute?: [string, LazyExoticComponent<ComponentType>];
+  parameters: {
+    [x: string]: unknown;
+    [x: number]: unknown;
+    [x: symbol]: unknown;
+  };
+}
+
+export const usePageResolver = (): UsePageResolverState => {
   const { pathname } = usePageResolver.useLocation();
+  const parametersNamesRef = useRef([]);
 
   const formattedPathname = pathname.replace('/public', '');
 
   const matchedRoute = useMemo(
     () =>
       Object.entries(routes).find(([key]) => {
-        const regexp = key.replace(/\[\S+\]/, '\\w+');
+        parametersNamesRef.current = key
+          .matchAll(/\[(?<param>\w+|\w+(-|_)\w+)\]/g)
+          .toArray()
+          .map((match) => match[1]);
 
-        return formattedPathname.match(new RegExp(`^${regexp}$`));
+        return formattedPathname.match(getURLMatchRegExp(key));
       }),
     [formattedPathname]
   );
 
-  return matchedRoute;
+  const parameterValues = matchedRoute
+    ? formattedPathname
+        .matchAll(getURLMatchRegExp(matchedRoute[0]))
+        .toArray()[0]
+        .slice(1)
+        .filter((v) => v)
+    : [];
+
+  const parameters = fromPairs(
+    parameterValues.map((value, index) => [
+      parametersNamesRef.current[index],
+      value
+    ])
+  );
+
+  return {
+    matchedRoute,
+    parameters
+  };
 };
 
 usePageResolver.useLocation = useLocation;
