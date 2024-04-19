7c308bc39e4c39159ea141540ba0c343943e8cdd
jeremyjaouen <jjaouen@centreon.com>
2024-04-19 12:45:39 +0200

    start implementing normalisation

---
+++ centreon/src/Core/Host/Infrastructure/API/FindHosts/FindHostsPresenter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\Host\Infrastructure\API\FindHosts;
+
+class FindHostsPresenter
+{
+
+}
---
+++ centreon/src/Core/Host/Infrastructure/API/FindHosts/FindHostsResponseNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Core\Host\Infrastructure\API\FindHosts;
+
+use Core\Host\Application\UseCase\FindHosts\FindHostsResponse;
+use Core\Host\Application\UseCase\FindHosts\HostDto;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+/**
+ * @method array getSupportedTypes(?string $format)
+ */
+class FindHostsNormalizer implements NormalizerInterface
+{
+
+    public function __construct(private readonly ObjectNormalizer $normalizer)
+    {
+    }
+    /**
+     * @inheritDoc
+     */
+    public function normalize(mixed $object, ?string $format = null, array $context = [])
+    {
+        return [
+            'result' => array_map(
+                function (HostDto $hostDto) {
+                    return $this->normalizer->normalize($hostDto, null, ['groups' => $context['groups']]);
+                },
+                $object->hostDto
+            ),
+            'meta' => $context['request_parameters']
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsNormalization(mixed $data, ?string $format = null)
+    {
+        return $data instanceof FindHostsResponse;
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        // TODO: Implement @method array getSupportedTypes(?string $format)
+    }
+}
