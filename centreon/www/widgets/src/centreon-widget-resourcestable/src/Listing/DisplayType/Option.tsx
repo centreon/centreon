e23246015db9ed71e02eb0d5d5b02f22f4eb5d96
Med1233 <melmeziani-ext@centreon.com>
2024-05-16 08:36:53 +0100

    add data-icon

--- centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/DisplayType/Option.tsx
+++ centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/DisplayType/Option.tsx
@@ -37,6 +37,7 @@ const Option = ({
     <IconButton
       ariaLabel={t(title)}
       className={classes.iconButton}
+      data-icon={title}
       data-testid={title}
       title={t(title)}
       tooltipClassName={classes.tooltipClassName}
