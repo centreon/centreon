e4541fb8ffa5a57d246a8606cf682f7129e829c7
MedWassCh <144020537+MedWassCh@users.noreply.github.com>
2024-05-22 15:09:09 +0200

    chore(E2E):fix auth test on sync-release-24041 (#4167)

--- centreon/tests/e2e/support/e2e.ts
+++ centreon/tests/e2e/support/e2e.ts
@@ -16,7 +16,8 @@ Cypress.on('uncaught:exception', (err) => {
   if (
     err.message.includes('Request failed with status code 401') ||
     err.message.includes('Request failed with status code 403') ||
-    err.message.includes('undefined')
+    err.message.includes('undefined') ||
+    err.message.includes('canceled')
   ) {
     return false;
   }
