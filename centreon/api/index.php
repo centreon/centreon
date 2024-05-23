ad7febeee8912f094d70c2b4267a9d5cbce71a12
Paul Oureib <poureib@centreon.com>
2024-05-23 14:52:22 +0200

    FORCE_JAVASCRIPT_ACTIONS_TO_NODE20 as env instead of github output

--- centreon/api/index.php
+++ centreon/api/index.php
@@ -23,7 +23,6 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
