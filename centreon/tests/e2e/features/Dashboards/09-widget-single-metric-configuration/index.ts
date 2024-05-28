ba289257e0d1316d0e4ccb5ea1519a671c37f3b9
MedWassCh <144020537+MedWassCh@users.noreply.github.com>
2024-05-28 17:04:17 +0200

    fix(E2E): fix flaky tests of dashboard (#4219)

--- centreon/tests/e2e/features/Dashboards/09-widget-single-metric-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/09-widget-single-metric-configuration/index.ts
@@ -65,7 +65,6 @@ beforeEach(() => {
     jsonName: dashboardAdministratorUser.login,
     loginViaApi: false
   });
-  cy.visit('/centreon/home/dashboards');
 });
 
 afterEach(() => {
@@ -83,7 +82,11 @@ Given(
   "a dashboard in the dashboard administrator user's dashboard library",
   () => {
     cy.insertDashboard({ ...dashboards.default });
-    cy.visit('/centreon/home/dashboards');
+    cy.navigateTo({
+      page: 'Dashboards',
+      rootItemNumber: 0
+    });
+    cy.wait('@listAllDashboards');
     cy.contains(dashboards.default.name).click();
   }
 );
@@ -148,7 +151,10 @@ Then('the information about the selected metric is displayed', () => {
 
 // Given('a dashboard featuring a single Single Metric widget', () => {
 //   cy.insertDashboardWithWidget(dashboards.default, singleMetricPayload);
-//   cy.visit('/centreon/home/dashboards');
+//   cy.navigateTo({
+//   page: 'Dashboards',
+//   rootItemNumber: 0
+// });
 //   cy.contains(dashboards.default.name).click();
 // });
 
@@ -159,9 +165,9 @@ Then('the information about the selected metric is displayed', () => {
 //       label: 'Edit dashboard',
 //       tag: 'button'
 //     }).click();
-//     cy.getByTestId({ testId: 'MoreHorizIcon' }).click();
+//     cy.getByTestId({ testId: 'More actions' }).click();
 //     cy.getByTestId({ testId: 'RefreshIcon' }).click();
-//     cy.getByTestId({ testId: 'MoreHorizIcon' }).click({ force: true });
+//     cy.getByTestId({ testId: 'More actions' }).click({ force: true });
 //     cy.getByTestId({ testId: 'ContentCopyIcon' }).click();
 //   }
 // );
@@ -187,14 +193,17 @@ Given(
   'a dashboard with a Single Metric widget displaying a human-readable value format',
   () => {
     cy.insertDashboardWithWidget(dashboards.default, singleMetricPayloadRta);
-    cy.visit('/centreon/home/dashboards');
+    cy.navigateTo({
+      page: 'Dashboards',
+      rootItemNumber: 0
+    });
     cy.wait('@listAllDashboards');
     cy.contains(dashboards.default.name).click();
     cy.getByLabel({
       label: 'Edit dashboard',
       tag: 'button'
     }).click();
-    cy.getByTestId({ testId: 'MoreHorizIcon' }).click();
+    cy.getByTestId({ testId: 'More actions' }).click();
     cy.getByLabel({
       label: 'Edit widget',
       tag: 'li'
@@ -225,7 +234,10 @@ Then(
 
 Given('a dashboard containing a Single Metric widget', () => {
   cy.insertDashboardWithWidget(dashboards.default, singleMetricPayloadRta);
-  cy.visit('/centreon/home/dashboards');
+  cy.navigateTo({
+    page: 'Dashboards',
+    rootItemNumber: 0
+  });
   cy.wait('@listAllDashboards');
   cy.contains(dashboards.default.name).click();
   cy.getByLabel({
@@ -277,7 +289,10 @@ Then(
 
 Given('a dashboard featuring a Single Metric widget', () => {
   cy.insertDashboardWithWidget(dashboards.default, singleMetricPayloadRta);
-  cy.visit('/centreon/home/dashboards');
+  cy.navigateTo({
+    page: 'Dashboards',
+    rootItemNumber: 0
+  });
   cy.wait('@listAllDashboards');
   cy.contains(dashboards.default.name).click();
   cy.getByLabel({
@@ -318,7 +333,10 @@ Then(
 
 Given('a dashboard featuring two Single Metric widgets', () => {
   cy.insertDashboardWithWidget(dashboards.default, singleMetricDoubleWidgets);
-  cy.visit('/centreon/home/dashboards');
+  cy.navigateTo({
+    page: 'Dashboards',
+    rootItemNumber: 0
+  });
   cy.wait('@listAllDashboards');
   cy.contains(dashboards.default.name).click();
   cy.getByLabel({
