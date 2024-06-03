83ab988abbba703d9e6198cdc3921e45fc1cc8bf
hamzabessa <hbessa@ext.centreon.com>
2024-06-03 15:47:04 +0100

    add err msg

--- centreon/tests/e2e/support/e2e.ts
+++ centreon/tests/e2e/support/e2e.ts
@@ -16,11 +16,12 @@ before(() => {
 
 Cypress.on('uncaught:exception', (err) => {
   if (
-    err.message.includes("Request failed with status code 401") ||
-    err.message.includes("Request failed with status code 403") ||
-    err.message.includes("undefined") ||
-    err.message.includes("postMessage") ||
-    err.message.includes("canceled")
+    err.message.includes('Request failed with status code 401') ||
+    err.message.includes('Request failed with status code 403') ||
+    err.message.includes('undefined') ||
+    err.message.includes('postMessage') ||
+    err.message.includes('canceled') ||
+    err.message.includes('{revert:true}')
   ) {
     return false;
   }
