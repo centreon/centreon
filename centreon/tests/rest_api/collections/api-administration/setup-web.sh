fe0cfeade2a89383f735b1c7ea47a87b28933dd3
Marion Debauchez <mdebauchez@centreon.com>
2024-06-04 11:32:44 +0200

    Fix

--- centreon/tests/rest_api/collections/api-administration/setup-web.sh
+++ centreon/tests/rest_api/collections/api-administration/setup-web.sh
@@ -3,3 +3,5 @@
 mysql -hdb -uroot -pcentreon centreon -e "UPDATE options SET options.value = 1 WHERE options.key = 'inheritance_mode'"
 
 mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir (dir_name, dir_alias, dir_comment) VALUES ('ppm', 'ppm', 'ppm directory')"
+
+sed -i 's@"vault": 0@"vault": 3@' /usr/share/centreon/config/features.json
