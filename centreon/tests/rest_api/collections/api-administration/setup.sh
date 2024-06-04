6abc86f75b18ba2142e128b89ecf6c1d3d5a8488
Marion Debauchez <mdebauchez@centreon.com>
2024-06-04 10:56:56 +0200

    Add Vault_Configuration and Clapi scripts

--- centreon/tests/rest_api/collections/api-administration/setup.sh
+++ centreon/tests/rest_api/collections/api-administration/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml exec web mkdir -p /usr/share/centreon/www/img/media/test
+
+sed -i 's@"vault": 0@"vault": 3@' /usr/share/centreon/config/features.json
