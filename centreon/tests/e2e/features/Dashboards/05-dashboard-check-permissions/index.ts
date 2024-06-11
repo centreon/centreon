8d1fa089054f7328f049a54c5972cc37c7622b3f
MedWassCh <wchaabane@centreon.com>
2024-06-11 11:00:11 +0200

    fix test 05

--- centreon/tests/e2e/features/Dashboards/05-dashboard-check-permissions/index.ts
+++ centreon/tests/e2e/features/Dashboards/05-dashboard-check-permissions/index.ts
@@ -81,7 +81,7 @@ Given('an admin user is logged in on a platform with dashboards', () => {
 });
 
 When('the admin user accesses the dashboards library', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 });
 
 Then(
@@ -139,7 +139,6 @@ Then("the admin user is allowed to update the dashboard's properties", () => {
   cy.getByLabel({ label: 'Name', tag: 'input' }).type(
     `{selectall}{backspace}${dashboards.fromAdminUser.name}-edited`
   );
-
   cy.getByLabel({ label: 'Description', tag: 'textarea' }).type(
     `{selectall}{backspace}${dashboards.fromAdminUser.description}, edited by ${adminUser.login}`
   );
@@ -167,7 +166,7 @@ Given('an admin user on the dashboards library', () => {
 });
 
 When('the admin user creates a new dashboard', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
   cy.getByTestId({ testId: 'create-dashboard' }).eq(0).click();
   cy.getByLabel({ label: 'Name', tag: 'input' }).type(
     dashboards.fromCurrentUser.name
@@ -204,10 +203,10 @@ Then(
 Given('an admin user who has just created a dashboard', () => {
   cy.loginByTypeOfUser({
     jsonName: adminUser.login,
-    loginViaApi: false
+    loginViaApi: true
   });
 
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 
   cy.contains(dashboards.fromCurrentUser.name).should('exist');
 });
@@ -243,7 +242,7 @@ Given(
 );
 
 When('the dashboard administrator user accesses the dashboards library', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 });
 
 Then(
@@ -337,7 +336,7 @@ Given(
 );
 
 When('the dashboard administrator user creates a new dashboard', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
   cy.getByTestId({ testId: 'create-dashboard' }).eq(0).click();
   cy.getByLabel({ label: 'Name', tag: 'input' }).type(
     dashboards.fromCurrentUser.name
@@ -373,11 +372,10 @@ Then(
 Given('a dashboard administrator user who has just created a dashboard', () => {
   cy.loginByTypeOfUser({
     jsonName: dashboardAdministratorUser.login,
-    loginViaApi: false
+    loginViaApi: true
   });
 
-  cy.visit('/centreon/home/dashboards');
-
+  cy.visitDashboards();
   cy.contains(dashboards.fromCurrentUser.name).should('exist');
 });
 
@@ -415,7 +413,7 @@ Given(
 );
 
 When('the dashboard editor user accesses the dashboards library', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 });
 
 Then(
@@ -474,17 +472,18 @@ Then(
   () => {
     cy.getByLabel({ label: 'edit', tag: 'button' }).click();
 
-    cy.getByLabel({ label: 'Name', tag: 'input' }).type(
+    cy.get('div.MuiDialog-container input[aria-label="Name"]').type(
       `{selectall}{backspace}${dashboards.fromDashboardCreatorUser.name}-edited`
     );
 
-    cy.getByLabel({ label: 'Description', tag: 'textarea' }).type(
+    cy.get('div.MuiDialog-container textarea[aria-label="Description"]').type(
       `{selectall}{backspace}${dashboards.fromDashboardCreatorUser.description}, edited by ${dashboardCreatorUser.login}`
     );
 
-    cy.getByLabel({ label: 'Update', tag: 'button' }).should('be.enabled');
+    cy.getByLabel({ label: 'Update', tag: 'button' })
+      .should('be.enabled')
+      .click();
 
-    cy.getByLabel({ label: 'Update', tag: 'button' }).click();
     cy.getByLabel({ label: 'page header title' })
       .should(
         'contain.text',
@@ -509,7 +508,11 @@ Given('a non-admin user with the editor role on the dashboard feature', () => {
 });
 
 When('the dashboard editor user creates a new dashboard', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.navigateTo({
+    page: 'Dashboards',
+    rootItemNumber: 0
+  });
+  cy.wait('@listAllDashboards');
   cy.getByTestId({ testId: 'create-dashboard' }).eq(0).click();
   cy.getByLabel({ label: 'Name', tag: 'input' }).type(
     dashboards.fromCurrentUser.name
@@ -548,8 +551,7 @@ Given('a dashboard editor user who has just created a dashboard', () => {
     jsonName: dashboardCreatorUser.login,
     loginViaApi: false
   });
-
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 
   cy.contains(dashboards.fromCurrentUser.name).should('exist');
 });
@@ -581,8 +583,7 @@ Given(
       jsonName: dashboardAdministratorUser.login,
       loginViaApi: true
     });
-    cy.visit('/centreon/home/dashboards');
-    cy.contains(dashboards.fromDashboardCreatorUser.name).click();
+    cy.visitDashboard(dashboards.fromDashboardCreatorUser.name);
     cy.getByLabel({ label: 'share', tag: 'button' }).click();
     cy.getByLabel({ label: 'Open', tag: 'button' }).click();
     cy.contains(dashboardViewerUser.login).click();
@@ -658,7 +659,11 @@ Given('a non-admin user with the viewer role on the dashboard feature', () => {
 });
 
 When('the dashboard viewer accesses the dashboards library', () => {
-  cy.visit('/centreon/home/dashboards');
+  cy.navigateTo({
+    page: 'Dashboards',
+    rootItemNumber: 0
+  });
+  cy.wait('@listAllDashboards');
 });
 
 Then('the option to create a new dashboard is not displayed', () => {
@@ -668,10 +673,10 @@ Then('the option to create a new dashboard is not displayed', () => {
 Given('a dashboard viewer user who could not create a dashboard', () => {
   cy.loginByTypeOfUser({
     jsonName: dashboardViewerUser.login,
-    loginViaApi: false
+    loginViaApi: true
   });
 
-  cy.visit('/centreon/home/dashboards');
+  cy.visitDashboards();
 });
 
 When('the dashboard viewer user tries to delete a dashboard', () => {
