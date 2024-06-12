ffbb9ca2a37e972cdd43afceebac70aa7519dbb1
Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
2024-06-12 15:11:12 +0200

    fix(servicegroups): error fixed during servicegroup creation on cloud it2 (#4403)

--- centreon/www/include/configuration/configObject/servicegroup/DB-Func.php
+++ centreon/www/include/configuration/configObject/servicegroup/DB-Func.php
@@ -305,9 +305,11 @@ function updateServiceGroupAcl(int $serviceGroupId, array $submittedValues = [])
          * if so then add the new servicegroup to the dataset
          * otherwise create a new dataset for this servicegroup
          */
-        $serviceGroupDatasetFilters = array_filter(
-            $datasets,
-            fn (array $dataset) => $dataset['dataset_filter_type'] === 'servicegroup'
+        $serviceGroupDatasetFilters = array_values(
+            array_filter(
+                $datasets,
+                fn (array $dataset) => $dataset['dataset_filter_type'] === 'servicegroup'
+            )
         );
 
         // No dataset_filter of type service group found. Create a new one
