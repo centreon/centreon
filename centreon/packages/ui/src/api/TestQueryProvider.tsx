cfa07913bb20de294e7614d6ae5db43dd5f2b9cb
dmyios <diosypenko@centreon.com>
2024-06-13 11:39:39 +0200

    lint

--- centreon/packages/ui/src/api/TestQueryProvider.tsx
+++ centreon/packages/ui/src/api/TestQueryProvider.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const client = new QueryClient({
-
   defaultOptions: {
     queries: {
       gcTime: 0,
@@ -20,7 +19,6 @@ const TestQueryProvider = ({
   children
 }: TestQueryProviderProps): JSX.Element => (
   <QueryClientProvider client={client}>{children}</QueryClientProvider>
-
 );
 
 export default TestQueryProvider;
