cadab1dc03c7477029f5faa856bcd16f5a6e1455
MedWassCh <wchaabane@centreon.com>
2024-06-19 10:48:53 +0200

    some fix

--- centreon/packages/js-config/cypress/e2e/configuration.ts
+++ centreon/packages/js-config/cypress/e2e/configuration.ts
@@ -76,6 +76,19 @@ export default ({
         configFile: `${__dirname}/reporter-config.js`
       },
       setupNodeEvents: async (on, config) => {
+        // Configure pre-processors and plugins
+        await esbuildPreprocessor(on, config);
+        tasks(on);
+
+        // Register event handler for after:spec
+        on('after:spec', async (spec, results) => {
+          await handleTestResults(results);
+        });
+
+        // Install logs printer or any other necessary setup
+        installLogsPrinter(on);
+
+        // Return the configured plugins
         return plugins(on, config);
       },
       specPattern,
--- centreon/packages/js-config/cypress/e2e/plugins.ts
+++ centreon/packages/js-config/cypress/e2e/plugins.ts
@@ -1,43 +1,12 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable default-param-last */
 /* eslint-disable global-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-param-reassign */
 
-import fs from 'fs';
-import path from 'path';
-
-import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
-
-import tasks from './tasks';
-import esbuildPreprocessor from './esbuild-preprocessor';
-
-const handleTestResults = async (results: CypressCommandLine.RunResult) => {
-  const testRetries: { [key: string]: boolean } = {};
-  if (results && results.tests) {
-    results.tests.forEach((test) => {
-      if (test.attempts && test.attempts.length > 1) {
-        const testTitle = test.title.join(' > ');
-        testRetries[testTitle] = true;
-      }
-    });
-  }
-
-  console.log('Test retries:', testRetries);
-  if (Object.keys(testRetries).length > 0) {
-    const resultFilePath = path.join(
-      __dirname,
-      '../../../../tests/e2e/results',
-      'hasRetries.json'
-    );
-    fs.writeFileSync(resultFilePath, JSON.stringify(testRetries, null, 2));
-  }
-};
-
-export default async (
+export default (
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> => {
+): Cypress.PluginConfigOptions => {
   on('before:browser:launch', (browser, launchOptions) => {
     const width = 1920;
     const height = 1080;
@@ -62,15 +31,6 @@ export default async (
 
     return launchOptions;
   });
-
-  on('after:spec', async (spec, results) => {
-    await handleTestResults(results);
-  });
-
-  installLogsPrinter(on);
-
-  await esbuildPreprocessor(on, config);
-  tasks(on);
 
   return config;
 };
