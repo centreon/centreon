301b5c29bfb8c67b786ea92e2d2d3d06ab306aa7
MedWassCh <wchaabane@centreon.com>
2024-06-19 14:21:04 +0200

    some fix

--- centreon/packages/js-config/cypress/e2e/configuration.ts
+++ centreon/packages/js-config/cypress/e2e/configuration.ts
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable consistent-return */
-/* eslint-disable no-restricted-syntax */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 
 import { execSync } from 'child_process';
-import fs from 'fs';
-import path from 'path';
 
 import { defineConfig } from 'cypress';
 import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
@@ -42,34 +37,6 @@ export default ({
     .toString('utf8')
     .replace(/[\n\r\s]+$/, '');
 
-  const handleTestResults = async (results: CypressCommandLine.RunResult) => {
-    try {
-      const testRetries: { [key: string]: boolean } = {};
-      if (results && results.tests) {
-        results.tests.forEach((test) => {
-          if (test.attempts && test.attempts.length > 1) {
-            const testTitle = test.title.join(' > ');
-            testRetries[testTitle] = true;
-          }
-        });
-      }
-
-      console.log('Test retries:', testRetries);
-      if (Object.keys(testRetries).length > 0) {
-        const resultFilePath = path.join(
-          __dirname,
-          '../../../../tests/e2e/results',
-          'hasRetries.json'
-        );
-        fs.writeFileSync(resultFilePath, JSON.stringify(testRetries, null, 2));
-      }
-    } catch (error) {
-      console.error('Erreur lors du traitement des résultats:', error);
-      // Gérer l'erreur selon les besoins
-      throw error;
-    }
-  };
-
   return defineConfig({
     chromeWebSecurity: false,
     defaultCommandTimeout: 20000,
@@ -82,9 +49,6 @@ export default ({
         configFile: `${__dirname}/reporter-config.js`
       },
       setupNodeEvents: async (on, config) => {
-        on('after:spec', async (spec, results) => {
-          await handleTestResults(results);
-        });
         installLogsPrinter(on);
 
         await esbuildPreprocessor(on, config);
@@ -105,10 +69,6 @@ export default ({
     },
     execTimeout: 60000,
     requestTimeout: 20000,
-    retries: {
-      openMode: 0,
-      runMode: 2
-    },
     screenshotsFolder: `${resultsFolder}/screenshots`,
     video: isDevelopment,
     videoCompression: 0,
