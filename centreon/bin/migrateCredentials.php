97c23ed6108ab625894cea3a95023ba06aec7f49
jeremyjaouen <jjaouen@centreon.com>
2024-06-25 13:27:04 +0200

    feat(vault): create a script to manage migrations

--- centreon/bin/migrateCredentials.php
+++ centreon/bin/migrateCredentials.php
@@ -1,12 +1,29 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/centreon.config.php';
+require_once __DIR__ . '/../www/include/common/vault-functions.php';
 
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-if (posix_getuid() !== 0) {
-    throw new \RuntimeException('This script is only available using root user.');
+$kernel = \App\Kernel::createForWeb();
+$readVaultConfigurationRepository = $kernel->getContainer()->get(
+    Core\Security\Vault\Application\Repository\ReadVaultConfigurationRepositoryInterface::class
+);
+$vaultConfiguration = $readVaultConfigurationRepository->find();
+/**
+ * @var \Centreon\Domain\Log\Logger $logger
+ */
+$logger = $kernel->getContainer()->get(\Centreon\Domain\Log\Logger::class);
+$httpClient = new CentreonRestHttp();
+
+try {
+    echo('Migration of database credentials');
+    migrateDatabaseCredentialsToVault($vaultConfiguration, $logger, $httpClient);
+    echo('Migration of database credentials done');
+} catch (Throwable $e) {
+    echo((string) $e);
 }
 
 $process = Process::fromShellCommandline(
@@ -21,6 +38,18 @@ if (! $process->isSuccessful()) {
 }
 
 preg_match_all('/((.*)credentials:migrate-vault)(.*)/', $process->getOutput(), $matches);
-foreach ($matches as $match) {
-    var_dump(trim($match[1]));
+$migrationCommands = array_map(fn(string $command): string => trim($command), $matches[1]);
+
+foreach ($migrationCommands as $migrationCommand) {
+    $process = Process::fromShellCommandline(
+        'sudo -u apache php /usr/share/centreon/bin/console ' . $migrationCommand
+    );
+    $process->setWorkingDirectory('/usr/share/centreon');
+    $process->run();
+
+    if (! $process->isSuccessful()) {
+        throw new ProcessFailedException($process);
+    }
+
+    echo ($process->getOutput() . PHP_EOL);
 }
--- centreon/www/include/common/vault-functions.php
+++ centreon/www/include/common/vault-functions.php
@@ -37,6 +37,7 @@
 use Centreon\Domain\Log\Logger;
 use Utility\Interfaces\UUIDGeneratorInterface;
 use Core\Security\Vault\Domain\Model\VaultConfiguration;
+use Utility\UUIDGenerator;
 
 const VAULT_PATH_REGEX = '^secret::[^:]*::';
 const SNMP_COMMUNITY_MACRO_NAME = '_HOSTSNMPCOMMUNITY';
@@ -1730,4 +1731,200 @@ function writeKnowledgeBasePasswordInVault(
 
         throw $ex;
     }
+}
+
+/**
+ * Migrate database credentials to Vault and update the different config files.
+ *
+ * @throws Throwable
+ */
+function migrateDatabaseCredentialsToVault(VaultConfiguration $vaultConfiguration, Logger $logger, CentreonRestHttp $httpClient): void
+{
+    $vaultToken = authenticateToVault($vaultConfiguration, $logger, $httpClient);
+    $vaultPath = migrateDatabaseCredentials($vaultConfiguration, $vaultToken, $httpClient);
+    updateConfigFilesWithVaultPath($vaultPath);
+}
+
+/**
+ * Migrate database credentials to Vault.
+ *
+ * @param VaultConfiguration $vaultConfiguration
+ * @param string $token
+ * @param CentreonRestHttp $httpClient
+ *
+ * @throws RestBadRequestException
+ * @throws RestConflictException
+ * @throws RestForbiddenException
+ * @throws RestInternalServerErrorException
+ * @throws RestMethodNotAllowedException
+ * @throws RestNotFoundException
+ * @throws RestUnauthorizedException
+ *
+ * @return string
+ */
+function migrateDatabaseCredentials(
+    VaultConfiguration $vaultConfiguration,
+    string $token,
+    CentreonRestHttp $httpClient
+): string {
+    $uuidGenerator = new UUIDGenerator();
+    $uuid = $uuidGenerator->generateV4();
+    $vaultPathUri = $vaultConfiguration->getRootPath() . '/data/database/' . $uuid;
+    $vaultPath = 'secret::hashicorp_vault::' . $vaultPathUri;
+    $credentials = retrieveDatabaseCredentialsFromConfigFile();
+    $url = 'https://' . $vaultConfiguration->getAddress() . ':' . $vaultConfiguration->getPort()
+        . '/v1/' . $vaultPathUri;
+    $headers = [
+        'X-Vault-Token: ' . $token,
+    ];
+
+    $body = [
+        'data' => ['_DBUSERNAME' => $credentials['username'], '_DBPASSWORD' => $credentials['password']],
+    ];
+
+    $httpClient->call($url, 'POST', $body, $headers);
+
+    return $vaultPath;
+}
+
+/**
+ * Retrieve database credentials from config file.
+ *
+ * @throws Exception
+ *
+ * @return array{username: string, password: string
+ */
+function retrieveDatabaseCredentialsFromConfigFile(): array
+{
+    if (! file_exists(_CENTREON_ETC_ . '/centreon.conf.php')
+        || ($content = file_get_contents(_CENTREON_ETC_ . '/centreon.conf.php')) === false
+    ) {
+        throw new Exception('Unable to retrieve content of file: ' . _CENTREON_ETC_ . '/centreon.conf.php');
+    }
+
+    preg_match(
+        '/\$conf_centreon\[[\'\"]user[\'\"]\]\s*=\s*[\'\"](.*)[\'\"]\s*;/',
+        $content,
+        $matches
+    );
+
+    $userContent = $matches[1];
+
+    preg_match(
+        '/\$conf_centreon\[[\'\"]password[\'\"]\]\s*=\s*[\'\"](.*)[\'\"]\s*;/',
+        $content,
+        $matches
+    );
+
+    $passwordContent = $matches[1];
+
+    return ['username' => $userContent, 'password' => $passwordContent];
+}
+
+/**
+ * Update the different config files with the vault path.
+ *
+ * @param $vaultPath
+ *
+ * @throws Exception
+ */
+function updateConfigFilesWithVaultPath($vaultPath): void
+{
+    updateCentreonConfPhpFile($vaultPath);
+    updateCentreonConfPmFile($vaultPath);
+    updateDatabaseYamlFile($vaultPath);
+}
+
+/**
+ * @param string $vaultPath
+ *
+ * @throws Exception
+ */
+function updateCentreonConfPhpFile(string $vaultPath): void
+{
+    if (! file_exists(_CENTREON_ETC_ . '/centreon.conf.php')
+        || ($content = file_get_contents(_CENTREON_ETC_ . '/centreon.conf.php')) === false
+    ) {
+        throw new Exception('Unable to retrieve content of file: ' . _CENTREON_ETC_ . '/centreon.conf.php');
+    }
+
+    $newContentPhp = preg_replace(
+        '/\$conf_centreon\[[\'\"]user[\'\"]\]\s*=\s*(.*)/',
+        '\$conf_centreon[\'user\'] = \'' . $vaultPath . '\';',
+        $content
+    );
+    $newContentPhp = preg_replace(
+        '/\$conf_centreon\[[\'\"]password[\'\"]\]\s*=\s*(.*)/',
+        '\$conf_centreon[\'password\'] = \'' . $vaultPath . '\';',
+        $newContentPhp
+    );
+
+    file_put_contents(_CENTREON_ETC_ . '/centreon.conf.php', $newContentPhp)
+        ?: throw new Exception('Unable to update file: ' . _CENTREON_ETC_ . '/centreon.conf.php');
+}
+
+/**
+ * @param string $vaultPath
+ *
+ * @throws Exception
+ */
+function updateCentreonConfPmFile(string $vaultPath): void
+{
+    if (! file_exists(_CENTREON_ETC_ . '/conf.pm')
+        || ($content = file_get_contents(_CENTREON_ETC_ . '/conf.pm')) === false
+    ) {
+        throw new Exception('Unable to retrieve content of file: ' . _CENTREON_ETC_ . '/conf.pm');
+    }
+
+    $newContentPm = preg_replace(
+        '/"db_user"\s*=>\s*(.*)/',
+        '"db_user" => "' . $vaultPath .'",',
+        $content
+    );
+    $newContentPm = preg_replace(
+        '/"db_passwd"\s*=>\s*(.*)/',
+        '"db_passwd" => "' . $vaultPath .'"',
+        $newContentPm
+    );
+    $newContentPm = preg_replace(
+        '/\$mysql_user\s*=\s*(.*)/',
+        '$mysql_user = "' . $vaultPath .'";',
+        $newContentPm
+    );
+    $newContentPm = preg_replace(
+        '/\$mysql_passwd\s*=\s*(.*)/',
+        '$mysql_passwd = "' . $vaultPath .'";',
+        $newContentPm
+    );
+
+    file_put_contents(_CENTREON_ETC_ . '/conf.pm', $newContentPm)
+        ?: throw new Exception('Unable to update file: ' . _CENTREON_ETC_ . '/conf.pm');
+}
+
+/**
+ * @param string $vaultPath
+ *
+ * @throws Exception
+ */
+function updateDatabaseYamlFile(string $vaultPath): void
+{
+    if (! file_exists(_CENTREON_ETC_ . '/config.d/10-database.yaml')
+        || ($content = file_get_contents(_CENTREON_ETC_ . '/config.d/10-database.yaml')) === false
+    ) {
+        throw new Exception('Unable to retrieve content of file: ' . _CENTREON_ETC_
+            . '/config.d/10-database.yaml');
+    }
+    $newContentYaml = preg_replace(
+        '/username: (.*)/',
+        'username: "' . $vaultPath . '"',
+        $content
+    );
+    $newContentYaml = preg_replace(
+        '/password: (.*)/',
+        'password: "' . $vaultPath . '"',
+        $newContentYaml
+    );
+
+    file_put_contents(_CENTREON_ETC_ . '/config.d/10-database.yaml', $newContentYaml)
+        ?: throw new Exception('Unable to update file: ' . _CENTREON_ETC_ . '/config.d/10-database.yaml');
 }
