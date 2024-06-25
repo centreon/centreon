029b96850bb28edb6049ceca05f8f8b2c44a097f
jeremyjaouen <jjaouen@centreon.com>
2024-06-25 09:20:14 +0200

    check for root user

--- centreon/bin/migrateCredentials.php
+++ centreon/bin/migrateCredentials.php
@@ -9,12 +9,17 @@ if (posix_getuid() !== 0) {
     throw new \RuntimeException('This script is only available using root user.');
 }
 
-$process = Process::fromShellCommandline('sudo -u apache php /usr/share/centreon/bin/console list | grep credentials:migrate-vault');
+$process = Process::fromShellCommandline(
+    'sudo -u apache php /usr/share/centreon/bin/console list '
+    . '| grep credentials:migrate-vault'
+);
 $process->setWorkingDirectory('/usr/share/centreon');
 $process->run();
-if (!$process->isSuccessful()) {
+
+if (! $process->isSuccessful()) {
     throw new ProcessFailedException($process);
 }
+
 preg_match_all('/((.*)credentials:migrate-vault)(.*)/', $process->getOutput(), $matches);
 foreach ($matches as $match) {
     var_dump(trim($match[1]));
