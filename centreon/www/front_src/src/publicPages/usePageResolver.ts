d0132809913511d20b4ea63d7bb010db44d27959
Thebarda <tomdar87@outlook.com>
2024-07-01 15:51:24 +0200

    Fix page resolver

--- centreon/www/front_src/src/publicPages/usePageResolver.ts
+++ centreon/www/front_src/src/publicPages/usePageResolver.ts
@@ -19,17 +19,16 @@ interface UsePageResolverState {
 
 export const usePageResolver = (): UsePageResolverState => {
   const { pathname } = usePageResolver.useLocation();
-  const parametersNamesRef = useRef([]);
+  const parametersNamesRef = useRef<Array<string>>([]);
 
   const formattedPathname = pathname.replace('/public', '');
 
   const matchedRoute = useMemo(
     () =>
       Object.entries(routes).find(([key]) => {
-        parametersNamesRef.current = key
-          .matchAll(/\[(?<param>\w+|\w+(-|_)\w+)\]/g)
-          .toArray()
-          .map((match) => match[1]);
+        parametersNamesRef.current = Array.from(
+          key.matchAll(/\[(?<param>\w+|\w+(-|_)\w+)\]/g) || []
+        ).map((match) => match[1]);
 
         return formattedPathname.match(getURLMatchRegExp(key));
       }),
