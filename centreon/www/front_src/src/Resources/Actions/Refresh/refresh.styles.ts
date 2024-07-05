ef84a8d3013263b7d9026172823abfeecdab0fbc
Nouha-ElAbrouki <97687698+Noha-ElAbrouki@users.noreply.github.com>
2024-07-05 08:59:21 +0100

    Update centreon/www/front_src/src/Resources/Actions/Refresh/refresh.styles.ts
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>

--- centreon/www/front_src/src/Resources/Actions/Refresh/refresh.styles.ts
+++ centreon/www/front_src/src/Resources/Actions/Refresh/refresh.styles.ts
@@ -1,7 +1,7 @@
 import { makeStyles } from 'tss-react/mui';
 
-export const useStyles = makeStyles()(() => ({
+export const useStyles = makeStyles()({
   container: {
     flexWrap: 'nowrap'
   }
-}));
+});
