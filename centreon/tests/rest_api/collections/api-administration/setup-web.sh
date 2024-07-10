0b5b6e4cef4c93e9c0769406b2c56e94fc402b19
mdebauchez <78470741+mdebauchez@users.noreply.github.com>
2024-07-10 09:53:14 +0200

    feat(test): Add Monitoring Postman scripts to replace Behat (#3146)
    Co-authored-by: Jérémy Delpierre <jdelpierre@users.noreply.github.com>
    Co-authored-by: dmyios <diosypenko@centreon.com>

--- centreon/tests/rest_api/collections/api-administration/setup-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysql -hdb -uroot -pcentreon centreon -e "UPDATE options SET options.value = 1 WHERE options.key = 'inheritance_mode'"
-
-mysql -hdb -uroot -pcentreon centreon -e "INSERT INTO view_img_dir (dir_name, dir_alias, dir_comment) VALUES ('ppm', 'ppm', 'ppm directory')"
