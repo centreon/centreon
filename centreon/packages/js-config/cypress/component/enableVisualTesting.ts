ee360a60b7aa8403b9d2b6c50acfd328963de03c
Kevin Duret <kduret@centreon.com>
2024-07-15 11:59:23 +0200

    fix snapshot

--- centreon/packages/js-config/cypress/component/enableVisualTesting.ts
+++ centreon/packages/js-config/cypress/component/enableVisualTesting.ts
@@ -13,7 +13,6 @@ const enableVisualTesting = (cypressFolder = 'cypress'): void => {
     capture: 'viewport',
     customDiffConfig: { threshold: 0.01 },
     customSnapshotsDir: `${cypressFolder}/visual-testing-snapshots`,
-    dumpInlineDiffToConsole: true,
     failureThreshold: Cypress.env('updateSnapshots') === true ? 0 : 0.07,
     failureThresholdType: 'percent'
   });
