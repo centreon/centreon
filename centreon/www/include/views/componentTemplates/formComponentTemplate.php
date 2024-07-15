8a7e572116328c3a0394b35ad258bc0d88d20071
Anthony Graule <agraule@centreon.com>
2024-07-15 17:44:35 +0200

    In the form of virtual metrics, adding events to adjust the field of list of known metrics in function of the field of linked host services

--- centreon/www/include/views/componentTemplates/formComponentTemplate.php
+++ centreon/www/include/views/componentTemplates/formComponentTemplate.php
@@ -477,7 +477,7 @@ if ($o === MODIFY_COMPONENT_TEMPLATE || $o === WATCH_COMPONENT_TEMPLATE) {
             const j_divLinkedHostServices = $('#host_service_id');
             const divListKnownMetrics = document.querySelector('#sl_list_metrics');
             const j_divListKnownMetrics = $('#sl_list_metrics');
-            if (divLinkedHostServices.value !== '') {
+            if (divLinkedHostServices.value === '') {
                 divListKnownMetrics.disabled = true;
             }
             j_divLinkedHostServices.on("change", function (e) {
