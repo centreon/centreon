bb9bdb5dfc6bb760fe0cdd987ac298088a7a75b6
Marion Debauchez <mdebauchez@centreon.com>
2024-07-17 09:43:14 +0200

    Complete index.ts

--- centreon/tests/e2e/features/Training/01-test/index.ts
+++ centreon/tests/e2e/features/Training/01-test/index.ts
@@ -1,3 +1,5 @@
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
+
 before(() => {
   cy.startContainers();
 });
@@ -7,6 +9,14 @@ beforeEach(() => {
     method: 'GET',
     url: '/centreon/api/internal.php?object=centreon_topology&action=navigationList'
   }).as('getNavigationList');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/latest/configuration/dashboards?'
+  }).as('getDashboardPage');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/latest/configuration/dashboards/1'
+  }).as('getDashboardDetail');
 });
 
 after(() => {
@@ -14,11 +24,22 @@ after(() => {
 });
 
 Given('the administrator is logged in', () => {
-  cy.getByLabel({ label: 'Alias', tag: 'input' })
+  cy.getByLabel({ label: 'Alias', tag: 'input' }).type('admin');
+  cy.getByLabel({ label: 'Password', tag:'input' }).type('Centreon!2021');
+  cy.getByLabel({ label: 'Connect', tag: 'button' }).click();
+  cy.wait('@getNavigationList');
 });
 
 When('the admin user visits dashboard page', () => {
+  cy.visit('/centreon/home/dashboards');
+  cy.wait('@getDashboardPage');
 });
 
 Then('the admin user could create a new dashboard', () => {
+  cy.getByLabel({ label: 'create', tag: 'button' }).click();
+  cy.contains('Create dashboard').should('be.visible');
+  cy.getByLabel({ label: 'Name', tag: 'input' }).type('Dashboard-001');
+  cy.getByLabel({ label: 'Description', tag: 'input' }).type('Hello there');
+  cy.getByLabel({ label: 'Create', tag: 'button' }).click();
+  cy.wait('@getDashboardDetail');
 });
