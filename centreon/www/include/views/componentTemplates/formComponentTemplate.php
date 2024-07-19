f3b2890fd41a8c868b05786f032b5dbaf4b1fcb9
Anthony Graule <agraule@centreon.com>
2024-07-19 13:52:45 +0200

    fix(Graph,Virtual Metrics) : List of metrics is empty in curve configuration form fixed (#4670)

--- centreon/www/include/views/componentTemplates/formComponentTemplate.php
+++ centreon/www/include/views/componentTemplates/formComponentTemplate.php
@@ -455,7 +455,7 @@ if ($o === MODIFY_COMPONENT_TEMPLATE || $o === WATCH_COMPONENT_TEMPLATE) {
             multiple: false,
             allowClear: true,
             additionnalFilters: {
-                id: '#host_id',
+                id: '#host_service_id',
             }
         });
         // color picker change event in form
@@ -469,5 +469,23 @@ if ($o === MODIFY_COMPONENT_TEMPLATE || $o === WATCH_COMPONENT_TEMPLATE) {
                 divColorPickerBlock.style.backgroundColor = (newColor !== '') ? newColor : oldColor;
             })
         });
+        // disable/enable List of known metrics in function of Linked Host Services to avoid an error 400
+        // tip to check if we display the form
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has("compo_id")) {
+            const divLinkedHostServices = document.querySelector('#host_service_id');
+            const j_divLinkedHostServices = $('#host_service_id');
+            const divListKnownMetrics = document.querySelector('#sl_list_metrics');
+            const j_divListKnownMetrics = $('#sl_list_metrics');
+            if (divLinkedHostServices.value === '') {
+                divListKnownMetrics.disabled = true;
+            }
+            j_divLinkedHostServices.on("change", function (e) {
+                e.stopPropagation();
+                j_divListKnownMetrics.val(null).trigger("change");
+                let hasService = divLinkedHostServices.value !== '';
+                divListKnownMetrics.disabled = !hasService;
+            });
+        }
     });
 </script>
