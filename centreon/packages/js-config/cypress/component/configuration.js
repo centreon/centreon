e13e0fab617252f3758fa3e24738a0a5110d9c41
Tom Darneix <tomdar87@outlook.com>
2024-07-22 14:15:20 +0200

    chore(core): Revert changes about using electron on cypress components (#4711)

--- centreon/packages/js-config/cypress/component/configuration.js
+++ centreon/packages/js-config/cypress/component/configuration.js
@@ -70,7 +70,7 @@ module.exports = ({
     },
     video: true,
     videosFolder: `${mainCypressFolder}/results/videos`,
-    viewportHeight: 1400,
-    viewportWidth: 1200
+    viewportHeight: 590,
+    viewportWidth: 1280
   });
 };
