34530c9d1791d15c84913c2e53b3cb20022f6073
hamzabessa <hbessa@ext.centreon.com>
2024-07-25 16:33:13 +0100

    fix(test): flaky e2e test

--- centreon/tests/e2e/features/Dashboards/10-widget-metric-graph-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/10-widget-metric-graph-configuration/index.ts
@@ -636,20 +636,28 @@ Then(
   }
 );
 
-When('the dashboard administrator clicks the "Display as Bar Chart" button', () => {
-  cy.getByTestId({ testId: '-summary' }).eq(2).click();
-  cy.getByLabel({
-    label: 'Bar',
-    tag: 'div'
-  }).click();
-});
+When(
+  'the dashboard administrator clicks the "Display as Bar Chart" button',
+  () => {
+    cy.getByTestId({ testId: '-summary' }).eq(2).click();
+    cy.getByLabel({
+      label: 'Bar',
+      tag: 'div'
+    }).click();
+  }
+);
 
 Then('the graph should be displayed as a bar chart', () => {
   cy.get('rect[data-testid*="single-bar-"]').each(($el) => {
-    const dataTestId = $el.attr("data-testid");
-    const height = $el.attr("height");
-    if (!dataTestId.endsWith("null") && height !== "0") {
-      cy.wrap($el).should("exist").and("be.visible");
-    }
+    cy.wrap($el)
+      .invoke('attr', 'data-testid')
+      .then((dataTestId) => {
+        if (dataTestId !== undefined) {
+          const height = $el.attr('height');
+          if (!dataTestId.endsWith('null') && height !== '0') {
+            cy.wrap($el).should('exist').and('be.visible');
+          }
+        }
+      });
   });
 });
