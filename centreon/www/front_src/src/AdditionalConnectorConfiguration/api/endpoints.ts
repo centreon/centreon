a42d73dcaf644ae5892f0ee2b2ee10801936a317
Med1233 <melmeziani-ext@centreon.com>
2024-07-28 23:23:13 +0100

    fix endpoints

--- centreon/www/front_src/src/AdditionalConnectorConfiguration/api/endpoints.ts
+++ centreon/www/front_src/src/AdditionalConnectorConfiguration/api/endpoints.ts
@@ -1,17 +1,16 @@
 import { buildListingEndpoint } from '@centreon/ui';
 
 export const additionalConnectorsEndpoint =
-  'http://localhost:3000/api/latest/configuration/additional-connectors';
+  '/configuration/additional-connectors';
 
 export const getAdditionalConnectorEndpoint = (id): string =>
-  `http://localhost:3000/api/latest/configuration/additional-connectors/${id}`;
+  `/configuration/additional-connectors/${id}`;
 
 export const pollersEndpoint = '/configuration/monitoring-servers';
 
 export const getPollersForConnectorTypeEndpoint = ({
   type = 'vmware_v6'
-}): string =>
-  `http://localhost:3000/api/latest/configuration/additional-connectors/pollers/${type}`;
+}): string => `/configuration/additional-connectors/pollers/${type}`;
 
 export const getPollersEndpoint = (parameters): string =>
   buildListingEndpoint({
