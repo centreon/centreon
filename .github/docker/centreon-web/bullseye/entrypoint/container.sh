cf01adc60432a85f341e1f8e9385804be25f57fd
Paul Oureib <poureib@centreon.com>
2024-08-05 15:13:05 +0200

    first information about vault container

--- .github/docker/centreon-web/bullseye/entrypoint/container.sh
+++ .github/docker/centreon-web/bullseye/entrypoint/container.sh
@@ -9,3 +9,6 @@ BASEDIR="/usr/share/centreon/container.d"
 for file in `ls $BASEDIR` ; do
   . "$BASEDIR/$file"
 done
+
+# PUT HERE: Commands that will register Central server to Vault
+# Should be conditionned to Web being up-and-running (health check to container)
