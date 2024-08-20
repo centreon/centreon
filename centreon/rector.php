d4bbc719f9d97d342a190a34674b734857173cbf
Anthony Graule <agraule@centreon.com>
2024-08-20 08:18:50 +0200

    install rector to migrate php to 8.2

--- centreon/rector.php
+++ centreon/rector.php
@@ -3,22 +3,31 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonySetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\Set\ValueObject\LevelSetList;
 
-return RectorConfig::configure()
-    ->withPaths([
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/api',
-        __DIR__ . '/config',
-        __DIR__ . '/src',
-    ])
-    // uncomment to reach your current PHP version
-    //->withPhpSets(php81: true)
-    ->withRules([
-        AddVoidReturnTypeWhereNoReturnRector::class,
-    ])
-    ->withSets([
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+//        __DIR__ . '/config',
+//        __DIR__ . '/src',
+//        __DIR__ . '/www',
     ]);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82]);
+};
+
+//return RectorConfig::configure()
+//    ->withPaths([
+//        __DIR__ . '/api',
+//        __DIR__ . '/config',
+//        __DIR__ . '/src',
+//    ])
+//    // uncomment to reach your current PHP version
+//    //->withPhpSets(php81: true)
+//    ->withRules([
+//        AddVoidReturnTypeWhereNoReturnRector::class,
+//    ])
+//    ->withSets([
+//        SymfonySetList::SYMFONY_64,
+//        SymfonySetList::SYMFONY_CODE_QUALITY,
+//        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+//    ]);
