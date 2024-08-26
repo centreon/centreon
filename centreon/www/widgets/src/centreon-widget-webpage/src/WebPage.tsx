e94169c97b8faa87365503ae5994f790467e6135
Med1233 <melmeziani-ext@centreon.com>
2024-08-26 12:10:06 +0100

    linting

--- centreon/www/widgets/src/centreon-widget-webpage/src/WebPage.tsx
+++ centreon/www/widgets/src/centreon-widget-webpage/src/WebPage.tsx
@@ -1,13 +1,13 @@
-const WebPage = ():JSX.Element => {
-    return(
-        <div style={{ marginTop: '20px' }}>
-        <iframe
-          src="https://driouchcity.net/"
-          style={{ width: '100%', height: '600px', border: 'none' }}
-          title="Webpage Display"
-        />
-      </div>
-    )
-}
+const WebPage = (): JSX.Element => {
+  return (
+    <div style={{ marginTop: '20px' }}>
+      <iframe
+        src="https://driouchcity.net/"
+        style={{ width: '100%', height: '600px', border: 'none' }}
+        title="Webpage Display"
+      />
+    </div>
+  );
+};
 
 export default WebPage;
--- centreon/www/widgets/src/centreon-widget-webpage/src/index.tsx
+++ centreon/www/widgets/src/centreon-widget-webpage/src/index.tsx
@@ -3,20 +3,16 @@ import { Module } from '@centreon/ui';
 // import ResourcesTable from './ResourcesTable';
 // import { ResourcesTableProps } from './models';
 
-import Webpage from "./WebPage"
+import Webpage from './WebPage';
 
-const Widget = ({
-  store,
-  queryClient,
-  ...props
-}): JSX.Element => (
+const Widget = ({ store, queryClient, ...props }): JSX.Element => (
   <Module
     maxSnackbars={1}
     queryClient={queryClient}
     seedName="widget-resourcetable"
     store={store}
   >
-  <Webpage/>
+    <Webpage {...props} />
   </Module>
 );
 
