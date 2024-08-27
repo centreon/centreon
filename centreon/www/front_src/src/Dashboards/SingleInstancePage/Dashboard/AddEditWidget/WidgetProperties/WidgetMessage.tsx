a195aaeca501626f411ae33eab0668b611172f81
Med1233 <melmeziani-ext@centreon.com>
2024-08-28 00:55:29 +0100

    add Widget message

---
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/WidgetMessage.tsx
@@ -0,0 +1,41 @@
+import { Box, SvgIcon, Typography } from '@mui/material';
+import { useAtomValue } from 'jotai';
+import { useTranslation } from 'react-i18next';
+import { widgetPropertiesAtom } from '../atoms';
+import { useWidgetMessageStyles } from './widgetProperties.styles';
+
+import parse from 'html-react-parser';
+
+const WidgetMessage = (): JSX.Element => {
+  const { classes } = useWidgetMessageStyles();
+
+  const { t } = useTranslation();
+
+  const selectedWidgetProperties = useAtomValue(widgetPropertiesAtom);
+
+  const message = selectedWidgetProperties?.message;
+
+  if (!message) {
+    return <div />;
+  }
+
+  return (
+    <Box className={classes.container}>
+      {message?.icon && (
+        <SvgIcon
+          className={classes.icon}
+          color="inherit"
+          data-icon={message.label}
+          viewBox="0 0 20 20"
+        >
+          {parse(message.icon)}
+        </SvgIcon>
+      )}
+      <Typography variant="h6" className={classes.label}>
+        {t(message.label)}
+      </Typography>
+    </Box>
+  );
+};
+
+export default WidgetMessage;
--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/index.ts
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/index.ts
@@ -2,3 +2,4 @@ export { default as WidgetSelection } from './WidgetSelection';
 export { default as WidgetProperties } from './WidgetProperties';
 export { default as Preview } from './Preview';
 export { default as WidgetData } from './WidgetData';
+export { default as WidgetMessage } from './WidgetMessage';
