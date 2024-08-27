eae7e864ae2e335dab1585a313fda892d9d013a2
MedWassCh <wchaabane@centreon.com>
2024-08-27 12:37:42 +0200

    implement tests

---
+++ centreon/tests/e2e/features/Backup-configuration/01-backup-properties.feature
@@ -0,0 +1,23 @@
+@REQ_MON-16477
+Feature: Creating a Notification Rule
+  As a Centreon admin user,
+  I want to verify that the backup settings are properly configured
+  So that I can ensure that all necessary backup options are enabled and correctly set.
+
+  Scenario: Verify Backup Configuration in the UI
+    Given an admin user is logged in
+    When the admin user acces to the backup page
+    Then backup is enable in UI
+    And backup directory is set
+    And backup temporary is set
+    And database backup options is set
+    And Mysql configuration file path is set
+
+  Scenario: Run the full backup
+    Given an admin user is logged in
+    When the admin user acces to the backup page
+    And the admin user enables backup for all configuration files
+    And the admin user selects full backup day
+    And the admin user saves the backup configuration and export the configuration
+    And after the scheduled cron job has run
+    Then the database backups and configuration files should be present in the backup directory
---
+++ centreon/tests/e2e/features/Backup-configuration/01-backup-properties/index.ts
@@ -0,0 +1,162 @@
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
+
+before(() => {
+  cy.startContainers();
+});
+
+after(() => {
+  cy.stopContainers();
+});
+
+beforeEach(() => {
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/api/internal.php?object=centreon_topology&action=navigationList'
+  }).as('getNavigationList');
+  cy.intercept({
+    method: 'GET',
+    url: '/centreon/include/common/userTimezone.php'
+  }).as('userTimeZone');
+});
+
+Given('an admin user is logged in', () => {
+  cy.loginByTypeOfUser({
+    jsonName: 'admin',
+    loginViaApi: false
+  });
+});
+
+When('the admin user acces to the backup page', () => {
+  cy.navigateTo({
+    page: 'Backup',
+    rootItemNumber: 4,
+    subMenu: 'Parameters'
+  });
+  cy.wait('@userTimeZone');
+});
+
+Then('backup is enable in UI', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('input[name="backup_enabled[backup_enabled]"]').should('be.checked');
+  });
+});
+
+Then('backup directory is set', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('input[name="backup_backup_directory"]').should(
+      'have.value',
+      '/var/cache/centreon/backup'
+    );
+  });
+});
+
+Then('backup temporary is set', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('input[name="backup_tmp_directory"]').should(
+      'have.value',
+      '/tmp/backup'
+    );
+  });
+});
+
+Then('database backup options is set', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('input[name="backup_database_centreon"]').should('be.checked');
+  });
+});
+
+Then('Mysql configuration file path is set', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('input[name="backup_retention"]').should('have.value', '7');
+    cy.get('input[name="backup_configuration_files"]').should('be.checked');
+    cy.get('input[name="backup_mysql_conf"]').should(
+      'have.value',
+      '/etc/my.cnf.d/centreon.cnf'
+    );
+    cy.get(
+      'input[name="backup_export_scp_enabled[backup_export_scp_enabled]"]'
+    ).should('be.checked');
+  });
+});
+
+Then('the admin user enables backup for all configuration files', () => {
+  cy.enterIframe('#main-content').within(() => {
+    cy.get('label')
+      .contains('Yes')
+      .prev('input[type="radio"]')
+      .check({ force: true });
+    cy.get('label')
+      .contains('Dump')
+      .prev('input[type="radio"]')
+      .check({ force: true });
+  });
+});
+
+Then('the admin user selects full backup day', () => {
+  cy.selectCurrentDayCheckbox();
+});
+
+Then(
+  'the admin user saves the backup configuration and export the configuration',
+  () => {
+    cy.enterIframe('#main-content').within(() => {
+      cy.get('input[name="backup_mysql_conf"]')
+        .clear()
+        .type('/etc/my.cnf.d/container.cnf');
+      cy.get('input[name="submitC"]').click();
+    });
+    cy.exportConfig();
+  }
+);
+
+Then('after the scheduled cron job has run', () => {
+  cy.exec('docker ps --format "{{.Names}}"').then((result) => {
+    cy.log(result.stdout);
+
+    const containerNames = result.stdout.split('\n');
+    const targetContainer = containerNames.find((name) =>
+      name.includes('docker-web-1')
+    );
+
+    expect(targetContainer).to.exist;
+
+    if (targetContainer) {
+      cy.exec(
+        `docker exec ${targetContainer} /usr/share/centreon/cron/centreon-backup.pl`
+      ).then(() => {});
+    }
+  });
+});
+
+Then(
+  'the database backups and configuration files should be present in the backup directory',
+  () => {
+    cy.exec('docker ps --format "{{.Names}}"').then((result) => {
+      cy.log(result.stdout);
+
+      const containerNames = result.stdout.split('\n');
+      const targetContainer = containerNames.find((name) =>
+        name.includes('docker-web-1')
+      );
+
+      expect(targetContainer).to.exist;
+
+      if (targetContainer) {
+        cy.exec(
+          `docker exec ${targetContainer} sh -c "cd /var/cache/centreon/backup && ls"`
+        ).then((lsResult) => {
+          cy.log(lsResult.stdout);
+          const todayDate = new Date().toISOString().split('T')[0];
+          const expectedFiles = [
+            `${todayDate}-centreon_storage.sql.gz`,
+            `${todayDate}-centreon.sql.gz`
+          ];
+
+          expectedFiles.forEach((file) => {
+            expect(lsResult.stdout).to.contain(file);
+          });
+        });
+      }
+    });
+  }
+);
