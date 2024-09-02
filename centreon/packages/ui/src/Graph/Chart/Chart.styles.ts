85d0eb5d8fcbb9ea6291bdf7fa97e2159e46bf67
nouha <nelabrouki-ext@centreon.com>
2024-09-02 17:29:30 +0200

    fix tooltip display

--- centreon/packages/ui/src/Graph/Chart/Chart.styles.ts
+++ centreon/packages/ui/src/Graph/Chart/Chart.styles.ts
@@ -1,6 +1,9 @@
 import { makeStyles } from "tss-react/mui";
 
 export const useChartStyles = makeStyles()({
+	baseWrapper: {
+		position: "relative",
+	},
 	tooltipChildren: { height: "100%", width: "100%" },
 	wrapperContainer: {
 		height: "100%",
