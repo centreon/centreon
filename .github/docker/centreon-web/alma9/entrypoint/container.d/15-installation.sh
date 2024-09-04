f6c5700f69de6fe96747660e7d24b93a1f977eac
Kevin Duret <kduret@centreon.com>
2024-09-04 09:55:49 +0200

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/15-installation.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/15-installation.sh
@@ -12,8 +12,8 @@ su apache -s /bin/bash -c "php partitionTables.php"
 su apache -s /bin/bash -c "php generationCache.php"
 su apache -s /bin/bash -c "rm -rf /usr/share/centreon/www/install"
 
-# sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/centreon.conf.php
-# sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/conf.pm
-# sed -i "s/localhost/${MYSQL_HOST}/g" /etc/centreon/config.d/10-database.yaml
+sed -i 's#severity=error#severity=debug#' /etc/sysconfig/gorgoned
+sed -i "5s/.*/    id: 1/" /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+sed -i 's#enable: true#enable: false#' /etc/centreon-gorgone/config.d/50-centreon-audit.yaml
 
 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
