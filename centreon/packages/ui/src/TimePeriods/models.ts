f87ed7d009a7e7222497879d86fa5d673731ccbc
nouha <nelabrouki-ext@centreon.com>
2024-09-06 15:07:54 +0200

    fixes

--- centreon/packages/ui/src/TimePeriods/models.ts
+++ centreon/packages/ui/src/TimePeriods/models.ts
@@ -1,113 +1,113 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react';
 
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 import {
-	label1Day,
-	label7Days,
-	label31Days,
-	labelLast7Days,
-	labelLast31Days,
-	labelLastDay,
-} from "./translatedLabels";
+  label1Day,
+  label7Days,
+  label31Days,
+  labelLast7Days,
+  labelLast31Days,
+  labelLastDay
+} from './translatedLabels';
 
-export const dateFormat = "L";
-export const timeFormat = "LT";
+export const dateFormat = 'L';
+export const timeFormat = 'LT';
 export const dateTimeFormat = `${dateFormat} ${timeFormat}`;
 
 export enum CustomTimePeriodProperty {
-	end = "end",
-	start = "start",
+  end = 'end',
+  start = 'start'
 }
 
 export interface TimePeriod {
-	dateTimeFormat: string;
-	getStart: () => Date;
-	id: string;
-	largeName: string;
-	name: string;
-	timelineEventsLimit: number;
+  dateTimeFormat: string;
+  getStart: () => Date;
+  id: string;
+  largeName: string;
+  name: string;
+  timelineEventsLimit: number;
 }
 
 export const lastDayPeriod: TimePeriod = {
-	dateTimeFormat: timeFormat,
-	getStart: (): Date => dayjs(Date.now()).subtract(24, "hour").toDate(),
-	id: "last_24_h",
-	largeName: labelLastDay,
-	name: label1Day,
-	timelineEventsLimit: 20,
+  dateTimeFormat: timeFormat,
+  getStart: (): Date => dayjs(Date.now()).subtract(24, 'hour').toDate(),
+  id: 'last_24_h',
+  largeName: labelLastDay,
+  name: label1Day,
+  timelineEventsLimit: 20
 };
 
 export const last7Days: TimePeriod = {
-	dateTimeFormat: dateFormat,
-	getStart: (): Date => dayjs(Date.now()).subtract(7, "day").toDate(),
-	id: "last_7_days",
-	largeName: labelLast7Days,
-	name: label7Days,
-	timelineEventsLimit: 100,
+  dateTimeFormat: dateFormat,
+  getStart: (): Date => dayjs(Date.now()).subtract(7, 'day').toDate(),
+  id: 'last_7_days',
+  largeName: labelLast7Days,
+  name: label7Days,
+  timelineEventsLimit: 100
 };
 
 export const last31Days: TimePeriod = {
-	dateTimeFormat: dateFormat,
-	getStart: (): Date => dayjs(Date.now()).subtract(31, "day").toDate(),
-	id: "last_31_days",
-	largeName: labelLast31Days,
-	name: label31Days,
-	timelineEventsLimit: 500,
+  dateTimeFormat: dateFormat,
+  getStart: (): Date => dayjs(Date.now()).subtract(31, 'day').toDate(),
+  id: 'last_31_days',
+  largeName: labelLast31Days,
+  name: label31Days,
+  timelineEventsLimit: 500
 };
 
 export const timePeriods: Array<TimePeriod> = [
-	lastDayPeriod,
-	last7Days,
-	last31Days,
+  lastDayPeriod,
+  last7Days,
+  last31Days
 ];
 
 export interface TimePeriodById {
-	id: string;
-	timePeriods: Array<TimePeriod>;
+  id: string;
+  timePeriods: Array<TimePeriod>;
 }
 
 export interface CustomTimePeriod {
-	end: Date;
-	start: Date;
-	timelineEventsLimit: number;
+  end: Date;
+  start: Date;
+  timelineEventsLimit: number;
 }
 
 export interface DateTimePickerInputModel {
-	changeDate: (props) => void;
-	date: Date | null;
-	disabled?: boolean;
-	maxDate?: Date;
-	minDate?: Date;
-	property: CustomTimePeriodProperty;
+  changeDate: (props) => void;
+  date: Date | null;
+  disabled?: boolean;
+  maxDate?: Date;
+  minDate?: Date;
+  property: CustomTimePeriodProperty;
 }
 
 export interface TimeLineAxisTickFormat extends CustomTimePeriod {
-	timeLineLimit: number;
-	xAxisTickFormat: string;
+  timeLineLimit: number;
+  xAxisTickFormat: string;
 }
 
 export interface GraphQueryParametersProps {
-	endDate?: Date;
-	startDate?: Date;
-	timePeriod?: TimePeriod | null;
+  endDate?: Date;
+  startDate?: Date;
+  timePeriod?: TimePeriod | null;
 }
 
 export interface EndStartInterval {
-	end: string;
-	start: string;
+  end: string;
+  start: string;
 }
 
 export interface Parameters extends EndStartInterval {
-	timelineEventsLimit: number;
+  timelineEventsLimit: number;
 }
 
 export interface WrapperTimePeriodProps {
-	adjustTimePeriodData?: Omit<CustomTimePeriod, "timelineEventsLimit">;
-	disabled?: boolean;
-	extraTimePeriods?: Array<Omit<TimePeriod, "timelineEventsLimit">>;
-	getIsError?: (value: boolean) => void;
-	getParameters?: ({ start, end, timelineEventsLimit }: Parameters) => void;
-	renderExternalComponent?: ReactNode;
-	skeletonHeight?: number;
+  adjustTimePeriodData?: Omit<CustomTimePeriod, 'timelineEventsLimit'>;
+  disabled?: boolean;
+  extraTimePeriods?: Array<Omit<TimePeriod, 'timelineEventsLimit'>>;
+  getIsError?: (value: boolean) => void;
+  getParameters?: ({ start, end, timelineEventsLimit }: Parameters) => void;
+  renderExternalComponent?: ReactNode;
+  skeletonHeight?: number;
 }
