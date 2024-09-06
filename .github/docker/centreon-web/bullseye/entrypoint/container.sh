67fabef305d8e96427ee163d2653a924a2b3aae3
Paul Oureib <poureib@centreon.com>
2024-09-06 16:54:05 +0200

    add vault agent configuration

--- .github/docker/centreon-web/bullseye/entrypoint/container.sh
+++ .github/docker/centreon-web/bullseye/entrypoint/container.sh
@@ -12,3 +12,5 @@ done
 
 # PUT HERE: Commands that will register Central server to Vault
 # Should be conditionned to Web being up-and-running (health check to container)
+      export VAULT_ROLE_ID=$(curl --silent -H 'X-Vault-Token: $VAULT_TOKEN' $VAULT_ADDR/v1/auth/approle/role/my-role/role-id | jq -r '.data.role_id') &&
+      export VAULT_SECRET_ID=$(curl --silent -H 'X-Vault-Token: $VAULT_TOKEN' $VAULT_ADDR/v1/auth/approle/role/my-role/secret-id | jq -r '.data.secret_id') &&
