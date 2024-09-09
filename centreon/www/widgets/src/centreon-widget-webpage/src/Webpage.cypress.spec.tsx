ba5ce2624ea91b486476db8fd2f421ac526278b0
Med1233 <melmeziani-ext@centreon.com>
2024-09-09 02:14:33 +0100

    add more tests

--- centreon/www/widgets/src/centreon-widget-webpage/src/Webpage.cypress.spec.tsx
+++ centreon/www/widgets/src/centreon-widget-webpage/src/Webpage.cypress.spec.tsx
@@ -4,21 +4,25 @@ import { createStore } from 'jotai';
 import Widget from '.';
 import { labelWebPagePreview } from './translatedLabels';
 
-const initialize = ({ url }): void => {
+const initialize = ({   widgetId = "1" , url,globalRefreshInterval = {
+  interval: null,
+  type: 'manual',
+
+} }): void => {
   const store = createStore();
 
+  cy.clock(); 
+  
   cy.mount({
     Component: (
       <div style={{ height: '100vh', position: 'relative', width: '100%' }}>
         <Widget
           dashboardId={1}
-          globalRefreshInterval={{
-            interval: null,
-            type: 'manual'
-          }}
+          globalRefreshInterval={globalRefreshInterval}
           hasDescription={false}
-          id="dashboard"
-          panelOptions={{ url }}
+          id={widgetId}
+          panelOptions={{ url 
+           }}
           queryClient={new QueryClient()}
           refreshCount={0}
           store={store}
@@ -48,5 +52,38 @@ describe('Web page', () => {
     initialize({ url: 'docs.centreon.com' });
 
     cy.findByTestId('Webpage Display').should('be.visible');
+  });
+
+  it('refreshes the iframe content at the specified interval', () => {
+    initialize({
+      url: 'docs.centreon.com',
+      globalRefreshInterval: { interval: 1000, type: 'automatic' }
+    });
+
+    cy.findByTestId('Webpage Display').as('iframe');
+
+    // Wait for the interval to pass and check if the iframe's src is refreshed
+    cy.tick(1000);
+    cy.get('@iframe').should('have.attr', 'src', 'http://docs.centreon.com');
+
+    cy.tick(1000);
+    cy.get('@iframe').should('have.attr', 'src', 'http://docs.centreon.com');
+  });
+
+  it('generates the correct iframe ID based on widgetId', () => {
+    const widgetId = '1';
+    initialize({
+      url: 'docs.centreon.com',
+      globalRefreshInterval: { interval: null, type: 'manual' },
+      widgetId
+    });
+
+    cy.get(`#Webpage_${widgetId}`).should('be.visible');
+  });
+
+  it('transforms a URL correctly when missing http prefix', () => {
+    initialize({ url: 'docs.centreon.com', globalRefreshInterval: { interval: null, type: 'manual' } });
+
+    cy.findByTestId('Webpage Display').should('have.attr', 'src', 'http://docs.centreon.com');
   });
 });
