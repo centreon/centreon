7848920ec914d422621d157b5746fe510d314acd
s-duret <sduret@centreon.com>
2024-09-11 16:19:58 +0200

    fix(dashboard): do not take into account UNDETERMINED day in calculations

--- centreon/www/include/reporting/dashboard/csvExport/csv_ServiceLogs.php
+++ centreon/www/include/reporting/dashboard/csvExport/csv_ServiceLogs.php
@@ -201,7 +201,13 @@ $dbResult->bindValue(':endDate', $endDate, PDO::PARAM_INT);
 $dbResult->execute();
 
 while ($row = $dbResult->fetch()) {
-    $duration = $row["date_end"] - $row["date_start"];
+    $duration = $row["OKTimeScheduled"]
+        + $row["WARNINGTimeScheduled"]
+        + $row["UNKNOWNTimeScheduled"]
+        + $row["CRITICALTimeScheduled"];
+    if ($duration == 0) {
+        continue;
+    }    
     /* Percentage by status */
     $duration = $row["OKTimeScheduled"]
         + $row["WARNINGTimeScheduled"]
