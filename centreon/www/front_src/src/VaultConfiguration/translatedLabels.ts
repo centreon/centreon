b780f8f6a51c25236cb8e4ae39e4200fb40ae00d
Thebarda <tomdar87@outlook.com>
2024-09-11 09:55:32 +0200

    fix labels

--- centreon/www/front_src/src/VaultConfiguration/translatedLabels.ts
+++ centreon/www/front_src/src/VaultConfiguration/translatedLabels.ts
@@ -17,10 +17,10 @@ export const labelVaultConfigurationUpdate = 'Vault configuration updated';
 export const labelPortMustStartFrom1 = 'Port must start from 1';
 export const labelMigrate = 'Migrate';
 export const labelMigrationScript = 'Migration script';
-export const labelExecuteThisCommandAsRoot = 'Execute this script as root';
+export const labelExecuteThisCommandAsRoot = 'Execute this script as root.';
 export const labelMigrationScriptExportCredentials =
   'The following script will migrate all your credentials from Centreon, Auto-discovery and the database.';
 export const labelByExecutingThisScript =
-  'By executing this script, your platform will be in a locked mode and you not be able to do <strong>anything</strong> (example: exporting you configuration, updating administration parameters, etc...)';
+  'By executing this script, your platform will be in a locked mode and you not be able to do <strong>anything</strong> (example: exporting you configuration, updating administration parameters, etc...).';
 export const labelMigrationCanTakeSeveralMinutes =
-  'The migration process can take several minutes depending on your configuration';
+  'The migration process can take several minutes depending on your configuration.';
