07575071dd0e79f0559242eabee1af3e9c86b947
Marion Debauchez <mdebauchez@centreon.com>
2024-09-11 13:59:32 +0200

    Fix

--- centreon/tests/e2e/features/Additional-connectors/02-ACC-listing/index.ts
+++ centreon/tests/e2e/features/Additional-connectors/02-ACC-listing/index.ts
@@ -5,14 +5,6 @@ before(() => {
   cy.setUserTokenApiV1().executeCommandsViaClapi('resources/clapi/config-ACL/acc-acl-user.json');
   cy.setUserTokenApiV1().executeCommandsViaClapi('resources/clapi/pollers/poller-1.json');
   cy.setUserTokenApiV1().executeCommandsViaClapi('resources/clapi/pollers/poller-2.json');
-  cy.execInContainer({
-    command: `mysql centreon -uroot -pcentreon`,
-    name: 'db'
-  });
-  cy.execInContainer({
-    command: `UPDATE topology SET topology_show = '1' WHERE topology_name = 'Additional Connector Configuration';`,
-    name: 'db'
-  });
 });
 
 beforeEach(() => {
