58fdcf89762d7130442539d472356b51d6b17c7b
Paul Oureib <poureib@centreon.com>
2024-09-12 17:33:14 +0200

    add login commands

--- .github/docker/centreon-web/alma8/entrypoint/container.sh
+++ .github/docker/centreon-web/alma8/entrypoint/container.sh
@@ -9,3 +9,34 @@ BASEDIR="/usr/share/centreon/container.d"
 for file in `ls $BASEDIR` ; do
   . "$BASEDIR/$file"
 done
+
+# Vault configuration
+dnf -y install jq
+. /tmp/shared-volume/vault-ids
+
+## Get Centreon token
+LOGIN_RESPONSE=$(curl -s -w "%{http_code}" \
+     -H 'Content-Type:application/json' \
+     -H 'Accept:application/json' \
+     -d '{"security":{"credentials":{"login":"admin","password":"'"$CENTREON_CENTRAL_ADMIN_PASSWORD"'"}}}' \
+     -L "http://$CENTRAL_PRIVATE_IP/centreon/api/latest/login")
+
+TOKEN=$(echo "$RESPONSE" | head -c -4 | jq -r '.security.token')
+
+## Register Vault
+curl -X POST \
+  -H 'Content-Type:application/json' \
+  -H X-AUTH-TOKEN:$TOKEN \
+  -H 'XDEBUG_SESSION:XDEBUG_KEY' \
+  -L 'http://localhost:8080/centreon/api/latest/administration/vaults/configurations'
+{
+  "name": "hashicorp_vault",
+  "address": "vault-custom.url.com",
+  "port": 443,
+  "root_path": "custom_root_path",
+  "role_id": "$VAULT_ROLE_ID",
+  "secret_id": "$VAULT_SECRET_ID"
+}
+
+## Migrate existing credentials
+sudo -u apache php /usr/share/centreon/bin/console credentials:migrate-vault
