1b7d866c13c4365ae8c37bc06f560c63f61c84a2
Tom Darneix <tomdar87@outlook.com>
2024-09-12 17:02:36 +0200

    Apply suggestions from code review
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>

--- centreon/www/front_src/src/VaultConfiguration/translatedLabels.ts
+++ centreon/www/front_src/src/VaultConfiguration/translatedLabels.ts
@@ -12,9 +12,9 @@ export const labelCancel = 'Cancel';
 export const labelResetConfiguration = 'Reset configuration';
 export const labelFormWillBeCleared =
   'The form will be cleared and all data will be lost. Do you want to proceed?';
-export const labelPortExpectedAtMost = 'Port expected at most 65535';
+export const labelPortExpectedAtMost = 'Port number must be at most 65535';
 export const labelVaultConfigurationUpdate = 'Vault configuration updated';
-export const labelPortMustStartFrom1 = 'Port must start from 1';
+export const labelPortMustStartFrom1 = 'Port number must be at least 1';
 export const labelMigrate = 'Migrate';
 export const labelMigrationScript = 'Migration script';
 export const labelExecuteThisCommandAsRoot =
@@ -22,6 +22,6 @@ export const labelExecuteThisCommandAsRoot =
 export const labelMigrationScriptExportCredentials =
   'The following script will migrate all your credentials from Centreon, Auto-discovery and the database.';
 export const labelByExecutingThisScript =
-  'By executing this script, your platform will be in a locked mode and you not be able to do <strong>anything</strong> during the migration process (example: exporting your configuration, updating administration parameters, etc...).';
+  'By executing this script, your platform will be in a locked mode and you will not be able to do <strong>anything</strong> during the migration process (example: deploying your configuration, updating administration parameters, etc...).';
 export const labelMigrationCanTakeSeveralMinutes =
   'The migration process can take several minutes depending on your configuration.';
