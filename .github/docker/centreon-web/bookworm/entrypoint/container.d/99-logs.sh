f9f12fc7ef79e38f1be31f2e2ce6b5b9d3aac108
Kevin Duret <kduret@centreon.com>
2024-09-13 14:04:12 +0200

    chore(ci): prepare docker images and packaging for php 8.2

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/99-logs.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/99-logs.sh
@@ -5,5 +5,5 @@ echo "Centreon is ready"
 
 tail -f \
   /var/log/apache2/error.log \
-  /var/log/php8.1-fpm-centreon-error.log \
+  /var/log/php2-fpm-centreon-error.log \
   /var/log/apache2/other_vhosts_access.log
