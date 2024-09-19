00db8fba60756876e15680f425857bb25e85ef3e
Anthony Graule <agraule@centreon.com>
2024-09-19 10:27:06 +0200

    centreon-awie phpstan fixed

--- centreon-awie/www/modules/centreon-awie/core/generateExport.php
+++ centreon-awie/www/modules/centreon-awie/core/generateExport.php
@@ -81,7 +81,7 @@ foreach ($_POST as $object => $value) {
                 if (!empty($generateContent['error'])) {
                     $ajaxReturn['error'][] = $generateContent['error'];
                 }
-                if (!is_null($generateContent['result'])) {
+                if (isset($generateContent['result'])) {
                     $scriptContent[] = $generateContent['result'];
                 }
             }
@@ -90,7 +90,7 @@ foreach ($_POST as $object => $value) {
             if (!empty($generateContent['error'])) {
                 $ajaxReturn['error'][] = $generateContent['error'];
             }
-            if (!is_null($generateContent['result'])) {
+            if (isset($generateContent['result'])) {
                 $scriptContent[] = $generateContent['result'];
             }
         }
