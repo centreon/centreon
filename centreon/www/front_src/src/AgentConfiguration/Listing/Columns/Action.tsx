3d459df5971b6c1eb58ae4c446e6409996c9a643
Tom Darneix <tomdar87@outlook.com>
2024-09-25 10:34:44 +0200

    enh(UI): Display agents configurations UI (#5133)
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>

---
+++ centreon/www/front_src/src/AgentConfiguration/Listing/Columns/Action.tsx
@@ -0,0 +1,43 @@
+import { IconButton } from '@centreon/ui/components';
+import { DeleteOutline } from '@mui/icons-material';
+import { useSetAtom } from 'jotai';
+import { isNotNil, pick } from 'ramda';
+import { useTranslation } from 'react-i18next';
+import { itemToDeleteAtom } from '../../atoms';
+import { AgentConfigurationListing } from '../../models';
+import { labelDelete } from '../../translatedLabels';
+
+interface Props {
+  row: AgentConfigurationListing & {
+    internalListingParentId?: number;
+    internalListingParentRow: AgentConfigurationListing;
+  };
+}
+
+const Action = ({ row }: Props): JSX.Element => {
+  const { t } = useTranslation();
+
+  const setItemToDelete = useSetAtom(itemToDeleteAtom);
+
+  const askBeforeDelete = (): void => {
+    setItemToDelete({
+      agent: isNotNil(row.internalListingParentId)
+        ? pick(['id', 'name'], row.internalListingParentRow)
+        : pick(['id', 'name'], row),
+      poller: isNotNil(row.internalListingParentId)
+        ? pick(['id', 'name'], row)
+        : undefined
+    });
+  };
+
+  return (
+    <IconButton
+      size="small"
+      icon={<DeleteOutline fontSize="small" color="error" />}
+      onClick={askBeforeDelete}
+      title={t(labelDelete)}
+    />
+  );
+};
+
+export default Action;
