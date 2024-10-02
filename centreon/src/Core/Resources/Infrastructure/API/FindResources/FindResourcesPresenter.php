463a65c558ca0441ea60491e37c4139029473dd1
Marion DESNAULT <mdesnault@chu-reims.fr>
2024-10-02 19:36:36 +0200

    fix(api): resource notification enabled status
    Reflect the notification enabled status of a resource in the API.

--- centreon/src/Core/Resources/Infrastructure/API/FindResources/FindResourcesPresenter.php
+++ centreon/src/Core/Resources/Infrastructure/API/FindResources/FindResourcesPresenter.php
@@ -178,7 +178,7 @@ class FindResourcesPresenter extends AbstractPresenter implements FindResourcesP
                     'tries' => $resource->tries,
                     'information' => $resource->information,
                     'performance_data' => null,
-                    'is_notification_enabled' => false,
+                    'is_notification_enabled' => $resource->areNotificationsEnabled(),
                     'severity' => $severity,
                     'links' => $links,
                     'extra' => $resource->resourceId !== null
