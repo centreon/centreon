67f43fa36480f0d8381ab2496cc057e3064c8232
Med1233 <melmeziani-ext@centreon.com>
2024-10-08 21:51:56 +0100

    trigger

--- centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/translatedLabels.ts
+++ centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/translatedLabels.ts
@@ -17,4 +17,5 @@ export const labelListView = 'View as list';
 export const labelCardsView = 'View as cards';
 export const labelMoreActions = 'More actions';
 export const labelShareWithContacts = 'Share with contacts';
+
 export const labelClearFilter = 'Clear';
