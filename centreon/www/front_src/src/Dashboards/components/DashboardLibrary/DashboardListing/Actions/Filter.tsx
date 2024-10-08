e5a10133aff71945dce045af54da09090e08497e
Med1233 <melmeziani-ext@centreon.com>
2024-10-08 21:38:18 +0100

    fix

--- centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/Actions/Filter.tsx
+++ centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/Actions/Filter.tsx
@@ -1,32 +1,50 @@
-import { useRef } from 'react';
-
-import { useSetAtom } from 'jotai';
-import { useTranslation } from 'react-i18next';
-
+import { IconButton, SearchField } from '@centreon/ui';
+import CloseIcon from '@mui/icons-material/Close';
 import debounce from '@mui/utils/debounce';
-
-import { SearchField } from '@centreon/ui';
-
+import { useAtom } from 'jotai';
+import { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { searchAtom } from '../atom';
-import { labelSearch } from '../translatedLabels';
-
+import { labelClearFilter, labelSearch } from '../translatedLabels';
 import { useActionsStyles } from './useActionsStyles';
+
+export const renderEndAdornmentFilter = (onClear) => (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <IconButton
+      ariaLabel={t(labelClearFilter) as string}
+      data-testid={labelClearFilter}
+      size="small"
+      title={t(labelClearFilter) as string}
+      onClick={onClear}
+    >
+      <CloseIcon color="action" fontSize="small" />
+    </IconButton>
+  );
+};
 
 const Filter = (): JSX.Element => {
   const { classes } = useActionsStyles();
-
   const { t } = useTranslation();
 
-  const setSearchValue = useSetAtom(searchAtom);
+  const [searchValue, setSearchValue] = useAtom(searchAtom);
+  const [inputValue, setInputValue] = useState(searchValue);
 
   const searchDebounced = useRef(
-    debounce<(search) => void>((debouncedSearch): void => {
+    debounce<(debouncedSearch: string) => void>((debouncedSearch): void => {
       setSearchValue(debouncedSearch);
     }, 500)
   );
 
   const onChange = ({ target }): void => {
+    setInputValue(target.value);
     searchDebounced.current(target.value);
+  };
+
+  const clearFilter = (): void => {
+    setInputValue('');
+    setSearchValue('');
   };
 
   return (
@@ -36,7 +54,9 @@ const Filter = (): JSX.Element => {
       className={classes.filter}
       dataTestId={t(labelSearch)}
       placeholder={t(labelSearch)}
+      value={inputValue}
       onChange={onChange}
+      EndAdornment={renderEndAdornmentFilter(clearFilter)}
     />
   );
 };
