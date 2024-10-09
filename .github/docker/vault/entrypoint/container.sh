5942b9b626ec87f7e8f42c472ee79bd7769098ca
Paul Oureib <poureib@centreon.com>
2024-10-09 16:23:18 +0200

    fixes

--- .github/docker/vault/entrypoint/container.sh
+++ .github/docker/vault/entrypoint/container.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-jq --version
-
 export VAULT_TOKEN=${VAULT_DEV_ROOT_TOKEN_ID}
-export VAULT_ADDR='https://127.0.0.1:8200'
+export VAULT_ADDR='https://vault:8200'
 vault secrets enable -path=centreon kv
 vault auth enable approle
 
