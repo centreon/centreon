0c1fd8311dc6c5998b32623d0b65521063caa723
s-elhassani <selhassani-ext@centreon.com>
2024-10-10 17:51:11 +0200

    [E2E] Rewrite HostGroupConfiguration.feature (#5317)

--- centreon/tests/e2e/features/Hosts/commands.ts
+++ centreon/tests/e2e/features/Hosts/commands.ts
@@ -33,6 +33,47 @@ Cypress.Commands.add(
   }
 );
 
+Cypress.Commands.add('checkLegacyRadioButton', (label: string) => {
+  cy.getIframeBody().contains('label', label)
+    .should('exist')
+    .then(($label) => {
+      const radioId = $label.attr('for');
+      cy.getIframeBody().find(`input[type="radio"][id="${radioId}"]`)
+        .should('be.checked');
+    });
+});
+
+Cypress.Commands.add('updateHostGroupViaApi', (body: HostGroup, hostGroup_name: string) => {
+  let query =
+    `SELECT h.hg_id from hostgroup as h WHERE h.hg_name='${hostGroup_name}'`;
+  cy.requestOnDatabase({
+    database: 'centreon',
+    query
+  }).then(([rows]) => {
+    cy.request({
+      body: body,
+      method: 'PUT',
+      url: `/centreon/api/beta/configuration/hosts/groups/${rows[0].hg_id}`
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+
+});
+
+interface HostGroup {
+  name: string,
+  alias: string,
+  notes: string,
+  notes_url: string,
+  action_url: string,
+  icon_id: number,
+  icon_map_id: number,
+  geo_coords: string,
+  rrd: number,
+  comment: string,
+  is_activated: boolean
+}
 
 declare global {
   namespace Cypress {
@@ -41,6 +82,8 @@ declare global {
         iframeSelector: string,
         elementSelector: string
       ) => Cypress.Chainable;
+      checkLegacyRadioButton: (label: string) => Cypress.Chainable;
+      updateHostGroupViaApi: (body: HostGroup, name: string) => Cypress.Chainable;
     }
   }
 }
