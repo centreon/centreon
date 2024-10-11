e8e2ce800bb315a953f3a9e8ff59715a3841e34b
Paul Oureib <poureib@centreon.com>
2024-10-11 16:06:44 +0200

    fix requests, debug

--- .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
@@ -7,14 +7,14 @@ done
 
 . /tmp/shared-volume/vault-ids
 
-RESPONSE=$(curl -s -w "%{http_code}" -H 'Content-Type:application/json' -H 'Accept:application/json' -d '{"security":{"credentials":{"login":"admin","password":"Centreon!2021"}}}' -L "http://localhost:8080/centreon/api/latest/login")
+RESPONSE=$(curl -s -w "%{http_code}" -H 'Content-Type:application/json' -H 'Accept:application/json' -d '{"security":{"credentials":{"login":"admin","password":"Centreon!2021"}}}' -L "http://localhost:80/centreon/api/latest/login")
 TOKEN=$(echo "$RESPONSE" | head -c -4 | jq -r '.security.token')
 
-RESPONSE=$(curl -X POST \
+RESPONSE=$(curl -X PUT \
      -H "Content-Type: application/json" \
      -H "X-AUTH-TOKEN: $TOKEN" \
-     -L "http://localhost:8080/centreon/api/latest/administration/vaults/configurations"
-     --data '{"name": "hashicorp_vault", "address": "vault", "port": 443, "root_path": "centreon/*", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
+     -L "http://localhost:80/centreon/api/latest/administration/vaults/configurations" \
+     --data '{"address": "vault", "port": 443, "root_path": "centreon/", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
 
 STATUS=$(echo "$RESPONSE" | tr -d '\n' | tail -c 3)
 
