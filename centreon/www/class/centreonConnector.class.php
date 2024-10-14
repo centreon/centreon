dee96ddc73089a9e43de3d79d745f876d5ddef1a
Anthony Graule <agraule@centreon.com>
2024-10-14 09:18:27 +0200

    rollback of a deleted method and a bug has been fixed

--- centreon/www/class/centreonConnector.class.php
+++ centreon/www/class/centreonConnector.class.php
@@ -578,4 +578,45 @@ class CentreonConnector
         return $parameters;
     }
 
+    /**
+     * @param array $values
+     * @param array $options
+     *
+     * @return array
+     * @throws PDOException
+     */
+    public function getObjectForSelect2($values = [], $options = [])
+    {
+        $items = [];
+        $listValues = '';
+        $queryValues = [];
+        if (!empty($values)) {
+            foreach ($values as $k => $v) {
+                $listValues .= ':id' . $v . ',';
+                $queryValues['id' . $v] = (int)$v;
+            }
+            $listValues = rtrim($listValues, ',');
+        } else {
+            $listValues .= '""';
+        }
+
+        # get list of selected connectors
+        $query = "SELECT id, name FROM connector " .
+            "WHERE id IN (" . $listValues . ") ORDER BY name ";
+
+        $stmt = $this->dbConnection->prepare($query);
+
+        if ($queryValues !== []) {
+            foreach ($queryValues as $key => $id) {
+                $stmt->bindValue(':' . $key, $id, PDO::PARAM_INT);
+            }
+        }
+        $stmt->execute();
+
+        while ($row = $stmt->fetch()) {
+            $items[] = ['id' => $row['id'], 'text' => $row['name']];
+        }
+
+        return $items;
+    }
 }
