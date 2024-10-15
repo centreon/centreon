3d52ee9a12cf990f9860eb46b808b57f1bbd411e
hamzabessa <hbessa@ext.centreon.com>
2024-10-15 11:03:42 +0100

    tests implementation

--- centreon/tests/e2e/features/Services-configuration/03-service-configuration/index.ts
+++ centreon/tests/e2e/features/Services-configuration/03-service-configuration/index.ts
@@ -1,7 +1,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 beforeEach(() => {
-//   cy.startContainers();
+  cy.startContainers();
   cy.intercept({
     method: "GET",
     url: "/centreon/include/common/userTimezone.php",
@@ -17,38 +17,27 @@ Given('a user is logged in Centreon', () => {
 });
 
 When('a service is configured', () => {
-//   cy.setUserTokenApiV1();
-//   cy.addHost({
-//   hostGroup: "Linux-Servers",
-//   name: "host_1",
-//   template: "generic-host",
-//   })
-//   .addService({
-//   activeCheckEnabled: false,
-//   host: "host_1",
-//   maxCheckAttempts: 1,
-//   name: "kiwi",
-//   template: "Ping-LAN",
-// });
+  cy.setUserTokenApiV1();
+  cy.addHost({
+  hostGroup: "Linux-Servers",
+  name: "host_1",
+  template: "generic-host",
+  })
+  .addService({
+  activeCheckEnabled: false,
+  host: "host_1",
+  maxCheckAttempts: 1,
+  name: "kiwi",
+  template: "Ping-LAN",
+});
 });
 
 When('the user changes the properties of a service', () => {
-    // cy.addService({
-    //   activeCheckEnabled: true, // Enable active checks
-    //   checkCommand: "check_http", // Set a check command
-    //   checkPeriod: "24x7", // Set the check period
-    //   host: "host_1", // Name of the host
-    //   maxCheckAttempts: 3, // Set maximum check attempts
-    //   name: "service", // Name of the service
-    //   passiveCheckEnabled: false, // Disable passive checks
-    //   template: "generic-service", // Leave the template empty as you're not adding a new service
-    // });
   cy.navigateTo({
     page: "Services by host",
     rootItemNumber: 3,
     subMenu: "Services",
   });
-  //check properties of the service created
   cy.get("iframe#main-content")
     .its("0.contentDocument.body")
     .find("table.ListTable")
@@ -56,16 +45,16 @@ When('the user changes the properties of a service', () => {
     .find("td.ListColLeft")
     .contains("kiwi")
     .click();
-  cy.getIframeBody()
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
     .find("table.formTable")
     .find("tr.list_two")
     .find("td.FormRowValue")
     .find('input[name="service_description"]')
     .clear()
     .type("kiwi_modified");
-  cy.get("iframe#main-content")
-    .its("0.contentDocument.body")
-    .find("table tr.list_two")
+  cy.getIframeBody()
+    .find("table tr.list_one")
     .find("td.FormRowValue")
     .find("select#service_template_model_stm_id")
     .next()
@@ -74,38 +63,142 @@ When('the user changes the properties of a service', () => {
   cy.getIframeBody()
     .find("div#validForm")
     .find("p.oreonbutton")
-    .find('.btc.bt_success[name="submitA"]')
+    .find('.btc.bt_success[name="submitC"]')
     .click();
 });
 
 Then('the properties are updated', () => {
     cy.get("iframe#main-content")
       .its("0.contentDocument.body")
-      .find("table tbody")
+      .find("table.ListTable")
+      .find("tr.list_one")
+      .find("td.ListColLeft")
       .contains("kiwi")
       .click();
     cy.get("iframe#main-content")
       .its("0.contentDocument.body")
-      .find("table tr.list_two td.FormRowValue")
+      .find("table.formTable")
+      .find("tr.list_two")
+      .find("td.FormRowValue")
       .find('input[name="service_description"]')
-      .contains("kiwi_modified");
-
+      .should('have.value',"kiwi_modified");
+  cy.getIframeBody()
+    .find("table tr.list_one")
+    .find("td.FormRowValue")
+    .find("select#service_template_model_stm_id")
+    .contains("Ping-WAN")
+    .should("exist");
 });
 
 When('the user duplicates a service', () => {
-
+  cy.navigateTo({
+    page: "Services by host",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+cy.get("iframe#main-content")
+  .its("0.contentDocument.body")
+  .find("table tbody")
+  .find("tr.list_one")
+  .each(($row) => {
+    cy.wrap($row)
+      .find("td.ListColLeft")
+      .then(($td) => {
+        if ($td.text().includes("host_1")) {
+          cy.wrap($row)
+            .find("td.ListColPicker")
+            .find("div.md-checkbox")
+            .click();
+        }
+      });
+  });
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .invoke(
+        "attr",
+        "onchange",
+        "javascript: { setO(this.form.elements['o2'].value); this.form.submit(); }",
+    );
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .select("Duplicate");
 });
 
 Then('the new service has the same properties', () => {
-
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ListTable")
+    .find("tr.list_two")
+    .find("td.ListColLeft")
+    .contains("kiwi_modified_1")
+    .click();
+    cy.get("iframe#main-content")
+      .its("0.contentDocument.body")
+      .find("table.formTable")
+      .find("tr.list_two")
+      .find("td.FormRowValue")
+      .find('input[name="service_description"]')
+      .should("have.value", "kiwi_modified_1");
+    cy.getIframeBody()
+      .find("table tr.list_one")
+      .find("td.FormRowValue")
+      .find("select#service_template_model_stm_id")
+      .contains("Ping-WAN")
+      .should("exist");
 });
 
 When('the user deletes a service', () => {
-
+  cy.navigateTo({
+    page: "Services by host",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+cy.get("iframe#main-content")
+  .its("0.contentDocument.body")
+  .find("table tbody")
+  .find("tr.list_one")
+  .each(($row) => {
+    cy.wrap($row)
+      .find("td.ListColLeft")
+      .then(($td) => {
+        if ($td.text().includes("host_1")) {
+          cy.wrap($row)
+            .find("td.ListColPicker")
+            .find("div.md-checkbox")
+            .click();
+        }
+      });
+  });
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .invoke(
+      "attr",
+      "onchange",
+      "javascript: { setO(this.form.elements['o2'].value); this.form.submit(); }",
+    );
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .select("Delete");
 });
 
 Then('the deleted service is not displayed in the service list', () => {
-
+  cy.get("iframe#main-content")
+    .its("0.contentDocument.body")
+    .find("table.ListTable tbody")
+    .contains("kiwi")
+    .should("not.exist");
 });
 
 afterEach(() => {
