5ef4fc87b66ba4ecfc5f5113dddc825e2b046d73
Nouha-ElAbrouki <97687698+Noha-ElAbrouki@users.noreply.github.com>
2024-10-15 15:11:58 +0200

    Update index.ts

--- centreon/tests/e2e/features/Poller-configuration/01-poller-configuration/index.ts
+++ centreon/tests/e2e/features/Poller-configuration/01-poller-configuration/index.ts
@@ -114,7 +114,7 @@ When('I select some pollers', () => {
 
 When('I click on the Export configuration button', () => {
   cy.getIframeBody()
-    .find('form button[name="apply_configuration"]')
+    .find('#exportConfigurationLink')
     .contains('Export configuration')
     .click({ force: true });
 });
@@ -124,6 +124,7 @@ Then('I am redirected to generate page', () => {
 });
 
 Then('the selected poller names are displayed', () => {
+  cy.reload()
   cy.get<string>('@pollerName').then((pollerName) => {
     cy.getIframeBody()
       .find('form span[class="selection"]')
