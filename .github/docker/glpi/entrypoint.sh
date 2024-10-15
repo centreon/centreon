d9fdc7d92d2cb7bc160ff736dca4daf7e3965932
Kevin Duret <kduret@centreon.com>
2024-10-15 09:18:50 +0200

    dynamic entrypoint

--- .github/docker/glpi/entrypoint.sh
+++ .github/docker/glpi/entrypoint.sh
@@ -17,8 +17,8 @@ su - www-data -s /bin/bash -c "php /var/www/html/glpi/bin/console database:insta
 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" glpi <<EOF
   UPDATE glpi_configs SET value = '1' WHERE context = 'core' AND name = 'enable_api';
   UPDATE glpi_configs SET value = 'http://glpi/api' WHERE context = 'core' AND name = 'url_base_api';
-  UPDATE glpi_apiclients SET name = 'full access', ipv4_range_start = NULL, ipv4_range_end = NULL, ipv6 = NULL, app_token = 'Ns4CuByx9MBIZhkO83mMaKYrceFJ21YNmGDw59K8', app_token_date = '2024-10-14 12:33:47';
-  UPDATE glpi_users SET api_token = 'R0MIlEL91Hhh2OJIGIS9y43TDFWCRX0r2aClU7sI', api_token_date = '2024-10-14 12:33:47' WHERE name = 'glpi';
+  UPDATE glpi_apiclients SET name = 'full access', ipv4_range_start = NULL, ipv4_range_end = NULL, ipv6 = NULL, app_token = '${GLPI_APP_TOKEN}', app_token_date = '2024-10-14 12:33:47';
+  UPDATE glpi_users SET api_token = '${GLPI_USER_TOKEN}', api_token_date = '2024-10-14 12:33:47' WHERE name = 'glpi';
 EOF
 
 service cron start
