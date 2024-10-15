5951317c5b38f3087eddd778ebf0d808c97fe2b1
selfakiri <selfakiri@ext.centreon.com>
2024-10-15 09:54:49 +0100

    fix(configuration): remove deprecated macros & add new ones (#5187) (#5275)

--- centreon/www/install/php/Update-23.10.18.php
+++ centreon/www/install/php/Update-23.10.18.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
@@ -17,3 +18,81 @@
  * For more information : contact@centreon.com
  *
  */
+
+require_once __DIR__ . '/../../../bootstrap.php';
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+
+$centreonLog = new CentreonLog();
+
+//error specific content
+$versionOfTheUpgrade = 'UPGRADE - 23.10.18: ';
+$errorMessage = '';
+
+$updateNagiosMacros =  function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to check for existing macros in nagios_macro table';
+    $statement = $pearDB->executeQuery(
+        <<<'SQL'
+            SELECT COUNT(*) FROM `nagios_macro`
+            WHERE `macro_name` IN (
+                '$NOTIFICATIONAUTHOR$',
+                '$NOTIFICATIONAUTHORNAME$',
+                '$NOTIFICATIONAUTHORALIAS$',
+                '$NOTIFICATIONCOMMENT$'
+            )
+        SQL
+    );
+
+    $errorMessage = 'Unable to insert new macros into nagios_macro table';
+    if (0 === (int) $statement->fetch(\PDO::FETCH_COLUMN)) {
+        $pearDB->executeQuery(
+        <<<'SQL'
+            INSERT INTO `nagios_macro` (`macro_name`)
+            VALUES
+                ('$NOTIFICATIONAUTHOR$'),
+                ('$NOTIFICATIONAUTHORNAME$'),
+                ('$NOTIFICATIONAUTHORALIAS$'),
+                ('$NOTIFICATIONCOMMENT$')
+            SQL
+        );
+    }
+    
+    $errorMessage = 'Unable to delete deprecated macros from nagios_macro table';
+    $pearDB->executeQuery(
+    <<<'SQL'
+        DELETE FROM `nagios_macro`
+        WHERE `macro_name` IN (
+            '$HOSTACKAUTHOR$',
+            '$HOSTACKCOMMENT$',
+            '$SERVICEACKAUTHOR$',
+            '$SERVICEACKCOMMENT$'
+        )
+        SQL
+    );
+};
+
+try {
+    // Transactional queries
+    if (! $pearDB->inTransaction()) {
+        $pearDB->beginTransaction();
+    }
+
+    $updateNagiosMacros($pearDB);
+
+    $pearDB->commit();
+} catch (\Exception $e) {
+
+    if ($pearDB->inTransaction()) {
+        $pearDB->rollBack();
+    }
+
+    $centreonLog->log(
+        4,
+        strtoupper($centreonLog::LEVEL_ERROR),
+        $versionOfTheUpgrade . $errorMessage
+        . ' - Code : ' . (int) $e->getCode()
+        . ' - Error : ' . $e->getMessage()
+        . ' - Trace : ' . $e->getTraceAsString()
+    );
+
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int) $e->getCode(), $e);
+}
