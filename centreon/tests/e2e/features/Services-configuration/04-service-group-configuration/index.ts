8a879085c858137536681d6010c860f04e329881
hamzabessa <hbessa@ext.centreon.com>
2024-10-16 16:21:11 +0100

    implement tests

--- centreon/tests/e2e/features/Services-configuration/04-service-group-configuration/index.ts
+++ centreon/tests/e2e/features/Services-configuration/04-service-group-configuration/index.ts
@@ -1,7 +1,9 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+import data from "../../../fixtures/services/service.json";
+
 beforeEach(() => {
-//   cy.startContainers();
+  //   cy.startContainers();
   cy.intercept({
     method: "GET",
     url: "/centreon/include/common/userTimezone.php",
@@ -18,49 +20,210 @@ Given("a user is logged in Centreon", () => {
 
 Then("a service group is configured", () => {
   cy.setUserTokenApiV1();
-  cy.addHost({
-    activeCheckEnabled: false,
-    checkCommand: "check_centreon_cpu",
-    hostGroup: '',
-    name: 'host_1',
-    template: "generic-host",
+  cy.addHostGroup({
+    name: data.hostGroups.hostGroup1.name,
   })
+    .addHost({
+      activeCheckEnabled: false,
+      checkCommand: "check_centreon_cpu",
+      hostGroup: data.hostGroups.hostGroup1.name,
+      name: data.hosts.host1.name,
+      template: "generic-host",
+    })
     .addService({
       activeCheckEnabled: false,
-      host: "host_1",
+      host: data.hosts.host1.name,
       maxCheckAttempts: 1,
-      name: "test",
+      name: data.services.service1.name,
       template: "Ping-LAN",
     })
     .addServiceGroup({
-      name: "sg",
-      hostsAndServices: null,
+      name: data.service_group.service1.name,
+      hostsAndServices: [[data.hosts.host1.name, data.services.service1.name]],
     });
 });
 
-When("the user changes the properties of a service group", () => {});
+When("the user changes the properties of a service group", () => {
+  cy.navigateTo({
+    page: "Service Groups",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+  cy.enterIframe("iframe#main-content")
+    .find("table.ListTable")
+    .find("tr.list_one")
+    .find("td.ListColLeft")
+    .contains(data.service_group.service1.name)
+    .click();
+  cy.enterIframe("iframe#main-content")
+    .find("table.formTable")
+    .find("tr.list_one")
+    .find("td.FormRowValue")
+    .find('input[name="sg_name"]')
+    .clear()
+    .type("test_modified");
+  cy.enterIframe("iframe#main-content")
+    .find("table.formTable")
+    .find("tr.list_two")
+    .find("td.FormRowValue")
+    .find('input[name="sg_alias"]')
+    .clear()
+    .type("description_modified");
+  cy.enterIframe("iframe#main-content")
+    .find("td.FormRowValue")
+    .find("select#sg_hServices")
+    .next()
+    .click();
+  cy.getIframeBody().contains("Centreon-Server - Memory").click();
+  cy.getIframeBody()
+    .find("div#validForm")
+    .find("p.oreonbutton")
+    .find('.btc.bt_success[name="submitC"]')
+    .click();
+});
 
 Then("the properties of the service group are updated", () => {
-
+  cy.navigateTo({
+    page: "Service Groups",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+  cy.enterIframe("iframe#main-content")
+    .find("table.ListTable")
+    .find("tr.list_one")
+    .find("td.ListColLeft")
+    .contains("test_modified")
+    .click();
+ cy.enterIframe("iframe#main-content")
+   .find("table.formTable")
+   .find("tr.list_one")
+   .find("td.FormRowValue")
+   .find('input[name="sg_name"]')
+   .should("have.value", "test_modified");
+ cy.enterIframe("iframe#main-content")
+   .find("table.formTable")
+   .find("tr.list_two")
+   .find("td.FormRowValue")
+   .find('input[name="sg_alias"]')
+   .should("have.value", "description_modified");
+  cy.enterIframe("iframe#main-content")
+    .find("table tr.list_one")
+    .find("td.FormRowValue")
+    .find("select#sg_hServices")
+    .contains("Centreon-Server - Memory")
+    .should("exist");
 });
 
-// Scenario: Duplicate one existing service group
-When("the user duplicates a service group", () => {});
+When("the user duplicates a service group", () => {
+  cy.navigateTo({
+    page: "Service Groups",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+  cy.enterIframe("iframe#main-content")
+    .find("table tbody")
+    .find("tr.list_one")
+    .each(($row) => {
+      cy.wrap($row)
+        .find("td.ListColLeft")
+        .then(($td) => {
+          if ($td.text().includes(data.service_group.service1.name)) {
+            cy.wrap($row)
+              .find("td.ListColPicker")
+              .find("div.md-checkbox")
+              .click();
+          }
+        });
+    });
+  cy.enterIframe("iframe#main-content")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .invoke(
+      "attr",
+      "onchange",
+      "javascript: { setO(this.form.elements['o2'].value); this.form.submit(); }",
+    );
+  cy.enterIframe("iframe#main-content")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .select("Duplicate");
+});
 
 Then("the new service group has the same properties", () => {
-
+  cy.enterIframe("iframe#main-content")
+    .find("table.ListTable")
+    .find("tr.list_two")
+    .find("td.ListColLeft")
+    .contains(`${data.service_group.service1.name}_1`)
+    .click();
+  cy.enterIframe("iframe#main-content")
+    .find("table.formTable")
+    .find("tr.list_one")
+    .find("td.FormRowValue")
+    .find('input[name="sg_name"]')
+    .should("have.value", `${data.service_group.service1.name}_1`);
+ cy.enterIframe("iframe#main-content")
+   .find("table.formTable")
+   .find("tr.list_two")
+   .find("td.FormRowValue")
+   .find('input[name="sg_alias"]')
+   .should("have.value", "description_modified");
+ cy.enterIframe("iframe#main-content")
+   .find("table tr.list_one")
+   .find("td.FormRowValue")
+   .find("select#sg_hServices")
+   .contains("Centreon-Server - Memory")
+   .should("exist");
 });
 
-// Scenario: Delete one existing service group
-When("the user deletes a service group", () => {});
+When("the user deletes a service group", () => {
+  cy.navigateTo({
+    page: "Service Groups",
+    rootItemNumber: 3,
+    subMenu: "Services",
+  });
+  cy.enterIframe("iframe#main-content")
+    .find("table tbody")
+    .find("tr.list_one")
+    .each(($row) => {
+      cy.wrap($row)
+        .find("td.ListColLeft")
+        .then(($td) => {
+          if ($td.text().includes(data.service_group.service1.name)) {
+            cy.wrap($row)
+              .find("td.ListColPicker")
+              .find("div.md-checkbox")
+              .click();
+          }
+        });
+    });
+  cy.enterIframe("iframe#main-content")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .invoke(
+      "attr",
+      "onchange",
+      "javascript: { setO(this.form.elements['o2'].value); this.form.submit(); }",
+    );
+  cy.enterIframe("iframe#main-content")
+    .find("table.ToolbarTable tbody")
+    .find("td.Toolbar_TDSelectAction_Bottom")
+    .find("select")
+    .select("Delete");
+});
 
 Then(
   "the deleted service group is not displayed in the service group list",
   () => {
-
+  cy.enterIframe("iframe#main-content")
+    .find("table.ListTable tbody")
+    .contains(data.service_group.service1.name)
+    .should("not.exist");
   },
 );
-
 
 afterEach(() => {
   cy.stopContainers();
