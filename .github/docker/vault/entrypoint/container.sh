72e4507afdbab69a6b1d50fa3f277dbcb6a0c10b
Paul Oureib <poureib@centreon.com>
2024-10-17 15:49:44 +0200

    fix listen address

--- .github/docker/vault/entrypoint/container.sh
+++ .github/docker/vault/entrypoint/container.sh
@@ -25,7 +25,7 @@ storage "raft" {
 }
 
 listener "tcp" {
-  address       = "127.0.0.1:8200"
+  address       = "0.0.0.0:8200"
   tls_cert_file = "/opt/vault/tls/tls.crt"
   tls_key_file  = "/opt/vault/tls/tls.key"
 }
