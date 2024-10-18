a4bf7659916ef8cdf8cef9c001a4937091b71f6f
Paul Oureib <poureib@centreon.com>
2024-10-18 16:55:27 +0200

    fix port

--- .github/docker/vault/entrypoint/container.sh
+++ .github/docker/vault/entrypoint/container.sh
@@ -19,8 +19,7 @@ listener "tcp" {
 }
 
 disable_mlock = true
-api_addr      = "https://0.0.0.0:8200"
-cluster_addr  = "https://127.0.0.1:8201"
+api_addr      = "https://0.0.0.0:8202"
 ui            = true
 EOM
 
