699a1b6b1be98a68cce721f93cb57b2d7d61c0f9
Kevin Duret <kduret@centreon.com>
2024-10-18 10:08:43 +0200

    enh(ci): refactor get-version workflow

--- centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/conf.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2016-2021 Centreon (http://www.centreon.com/)
+ * Copyright 2016-2024 Centreon (http://www.centreon.com/)
  *
  * Centreon is a full-fledged industry-strength solution that meets
  * the needs in IT infrastructure and application monitoring for
