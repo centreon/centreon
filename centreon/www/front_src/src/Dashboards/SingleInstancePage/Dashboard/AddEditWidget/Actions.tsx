e1cb239c998ababed0af6ad075657ad006ec243b
Thebarda <tomdar87@outlook.com>
2024-10-22 10:37:09 +0200

    fix lint

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/Actions.tsx
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/Actions.tsx
@@ -2,12 +2,8 @@ import { useFormikContext } from 'formik';
 import { useTranslation } from 'react-i18next';
 
 import { Modal } from '@centreon/ui/components';
-
-import { useAtomValue } from 'jotai';
-import { equals } from 'ramda';
 import { useCanEditProperties } from '../hooks/useCanEditDashboard';
 import { labelCancel, labelSave } from '../translatedLabels';
-import { widgetFormInitialDataAtom } from './atoms';
 
 interface Props {
   closeModal: (shouldAskForClosingConfirmation: boolean) => void;
@@ -16,21 +12,13 @@ interface Props {
 const Actions = ({ closeModal }: Props): JSX.Element | null => {
   const { t } = useTranslation();
 
-  const widgetFormInitialData = useAtomValue(widgetFormInitialDataAtom);
-
-  const { handleSubmit, isValid, dirty, isSubmitting, values } =
-    useFormikContext();
+  const { handleSubmit, isValid, dirty, isSubmitting } = useFormikContext();
 
   const { canEdit, canEditField } = useCanEditProperties();
 
   if (!canEdit || !canEditField) {
     return null;
   }
-  console.log(
-    equals(values, widgetFormInitialData),
-    values,
-    widgetFormInitialData
-  );
 
   const isDisabled = isSubmitting || !dirty || !isValid;
 
