849b4f081525a5d5f3dbb03fd24a24ff95c92710
Paul Oureib <poureib@centreon.com>
2024-10-23 16:31:50 +0200

    use 8200 port instead of 443

--- .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
@@ -14,7 +14,7 @@ RESPONSE=$(curl -X PUT \
      -H "Content-Type: application/json" \
      -H "X-AUTH-TOKEN: $TOKEN" \
      -L "http://localhost:80/centreon/api/latest/administration/vaults/configurations" \
-     --data '{"address": "vault", "port": 443, "root_path": "centreon/", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
+     --data '{"address": "vault", "port": 8200, "root_path": "centreon/", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
 
 STATUS=$(echo "$RESPONSE" | tr -d '\n' | tail -c 3)
 
