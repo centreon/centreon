1850c6c8bd58efa34388383210a28b8ed158afb1
Paul Oureib <poureib@centreon.com>
2024-10-23 16:06:55 +0200

    vault_addr to https

--- .github/docker/vault/entrypoint/container.sh
+++ .github/docker/vault/entrypoint/container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_ADDR='https://127.0.0.1:8200'
 export VAULT_SKIP_VERIFY=true
 export VAULT_TOKEN=${VAULT_DEV_ROOT_TOKEN_ID}
 
