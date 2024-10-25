fe3332e1cb09e6c302138e7dfeb28c60d3881836
Tom Darneix <tomdar87@outlook.com>
2024-10-25 13:19:32 +0200

    Fix

--- centreon/www/front_src/src/Resources/Actions/Visualization/useVisualization.tsx
+++ centreon/www/front_src/src/Resources/Actions/Visualization/useVisualization.tsx
@@ -1,16 +1,13 @@
-import { useSetAtom, useAtomValue, useAtom } from 'jotai';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { cond, equals, has } from 'ramda';
 
-import { selectedVisualizationAtom } from '../actionsAtoms';
-import { ResourceListing, Visualization } from '../../models';
-import { setCriteriaAndNewFilterDerivedAtom } from '../../Filter/filterAtoms';
-import { CriteriaNames } from '../../Filter/Criterias/models';
-import { listingAtom, selectedColumnIdsAtom } from '../../Listing/listingAtoms';
-import {
-  defaultSelectedColumnIds,
-  defaultSelectedColumnIdsforViewByHost
-} from '../../Listing/columns/index';
 import { platformVersionsAtom } from '../../../Main/atoms/platformVersionsAtom';
+import { CriteriaNames } from '../../Filter/Criterias/models';
+import { setCriteriaAndNewFilterDerivedAtom } from '../../Filter/filterAtoms';
+import { defaultSelectedColumnIdsforViewByHost } from '../../Listing/columns/index';
+import { listingAtom, selectedColumnIdsAtom } from '../../Listing/listingAtoms';
+import { ResourceListing, Visualization } from '../../models';
+import { selectedVisualizationAtom } from '../actionsAtoms';
 
 interface Props {
   type: Visualization;
@@ -70,7 +67,6 @@ const useVisualization = ({ type }: Props): State => {
 
       return;
     }
-    setSelectedColumnIds(defaultSelectedColumnIds);
   };
 
   const updateSearchAndSortValues = (): void => {
