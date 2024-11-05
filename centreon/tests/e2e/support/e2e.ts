b5cc6428d706a3195c1aa51b0b6c3067278861d3
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2024-11-05 10:22:38 +0100

    feat(dashboards): Library as cards: add thumbnail (#5150)
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>
    Co-authored-by: Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
    Co-authored-by: jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
    Co-authored-by: May <110405507+mushroomempires@users.noreply.github.com>
    Co-authored-by: Adrien Morais <amorais@centreon.com>
    Co-authored-by: MedWassCh <wchaabane@centreon.com>
    Co-authored-by: Anthony Graule <agraule@centreon.com>

--- centreon/tests/e2e/support/e2e.ts
+++ centreon/tests/e2e/support/e2e.ts
@@ -9,7 +9,6 @@ import './commands';
 
 before(() => {
   Cypress.config('baseUrl', 'http://127.0.0.1:4000');
-  Cypress.config('baseUrl', 'http://127.0.0.1:4000');
 
   cy.intercept('/waiting-page', {
     headers: { 'content-type': 'text/html' },
