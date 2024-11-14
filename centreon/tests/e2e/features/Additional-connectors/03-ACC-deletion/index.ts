0759099f326b4f86b2ccd6bc229e277c2591f2c9
s-elhassani <selhassani-ext@centreon.com>
2024-11-14 17:51:29 +0100

    fix e2e test

--- centreon/tests/e2e/features/Additional-connectors/03-ACC-deletion/index.ts
+++ centreon/tests/e2e/features/Additional-connectors/03-ACC-deletion/index.ts
@@ -54,6 +54,7 @@ Given('an additional connector configuration is already created', () => {
   cy.get('#mui-component-select-type').should('have.text', 'VMWare 6/7');
   cy.getByLabel({ label: 'Select poller(s)', tag: 'input' }).click();
   cy.contains('Central').click();
+  cy.get('#Selectpollers').click();
   cy.get('#vCenternamevalue').clear().type('vCenter-001');
   cy.get('#URLvalue').clear().type('https://10.0.0.0/sdk');
   cy.get('#Usernamevalue').type('admin');
