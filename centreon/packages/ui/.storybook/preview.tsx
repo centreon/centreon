2b52e9615c0d1f00d24a53f19668e1ce17bacfac
Thebarda <tomdar87@outlook.com>
2024-11-18 16:22:52 +0100

    add dayjs extension

--- centreon/packages/ui/.storybook/preview.tsx
+++ centreon/packages/ui/.storybook/preview.tsx
@@ -1,13 +1,33 @@
-import { useDarkMode } from "storybook-dark-mode";
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import isBetween from 'dayjs/plugin/isBetween';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isToday from 'dayjs/plugin/isToday';
+import isYesterday from 'dayjs/plugin/isYesterday';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import timezonePlugin from 'dayjs/plugin/timezone';
+import utcPlugin from 'dayjs/plugin/utc';
+import weekday from 'dayjs/plugin/weekday';
 import { initialize, mswLoader } from 'msw-storybook-addon';
+import { useDarkMode } from 'storybook-dark-mode';
 
-import { ThemeMode } from "@centreon/ui-context";
+dayjs.extend(localizedFormat);
+dayjs.extend(utcPlugin);
+dayjs.extend(timezonePlugin);
+dayjs.extend(isToday);
+dayjs.extend(isYesterday);
+dayjs.extend(weekday);
+dayjs.extend(isBetween);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(duration);
 
-import StoryBookThemeProvider from "../src/StoryBookThemeProvider";
-import QueryProvider from "../src/api/QueryProvider";
+import { ThemeMode } from '@centreon/ui-context';
+
+import { Decorator, Preview } from '@storybook/react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import StoryBookThemeProvider from '../src/StoryBookThemeProvider';
+import QueryProvider from '../src/api/QueryProvider';
 import { allModes } from './modes';
-import { Decorator, Preview } from "@storybook/react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 initialize();
 
@@ -29,51 +49,48 @@ const withQueryProvider: Decorator = (story, context): JSX.Element => (
 const preview: Preview = {
   loaders: [mswLoader],
 
-  decorators: [
-    withThemeProvider,
-    withQueryProvider,
-  ],
+  decorators: [withThemeProvider, withQueryProvider],
 
   globalTypes: {
     reactquerydevtools: {
-      description: "React-Query devtools",
+      description: 'React-Query devtools',
       defaultValue: false,
       toolbar: {
-        title: "React-Query",
+        title: 'React-Query',
         items: [
-          { value: true, icon: "circle", title: "Enable devtools" },
-          { value: false, icon: "circlehollow", title: "Disable devtools" },
+          { value: true, icon: 'circle', title: 'Enable devtools' },
+          { value: false, icon: 'circlehollow', title: 'Disable devtools' }
         ],
-        dynamicTitle: false,
-      },
-    },
+        dynamicTitle: false
+      }
+    }
   },
 
   parameters: {
     // actions: { argTypesRegex: "^on[A-Z].*" },
     a11y: {
-      manual: true,
+      manual: true
     },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/
       }
     },
     chromatic: {
       diffThreshold: 0.1,
       delay: 300,
       modes: {
-        desktop: allModes.desktop,
-      },
+        desktop: allModes.desktop
+      }
     },
     viewport: {
       defaultViewport: 'tablet',
       defaultOrientation: 'landscape'
-    },
+    }
   },
 
-  tags: ["autodocs"]
+  tags: ['autodocs']
 };
 
 export default preview;
