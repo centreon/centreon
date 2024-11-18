7f7a09bd138bdb79fd8bc4c551ae17a391ccc288
jeremyjaouen <jjaouen@centreon.com>
2024-11-18 09:36:16 +0100

    fix braces

--- centreon/www/include/configuration/configObject/service_categories/DB-Func.php
+++ centreon/www/include/configuration/configObject/service_categories/DB-Func.php
@@ -212,6 +212,7 @@ function multipleServiceCategorieInDB($sc = [], $nbrDup = [])
                         );
 
                         throw $ex;
+                    }
                 }
             }
         }
