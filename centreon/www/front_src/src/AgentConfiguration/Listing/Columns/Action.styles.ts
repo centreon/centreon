c66c4ff7a4dff6d0d07301f2df558a09cfaf3610
Med1233 <melmeziani-ext@centreon.com>
2024-11-19 16:14:44 +0100

    fix

--- centreon/www/front_src/src/AgentConfiguration/Listing/Columns/Action.styles.ts
+++ centreon/www/front_src/src/AgentConfiguration/Listing/Columns/Action.styles.ts
@@ -1,13 +1,6 @@
 import { makeStyles } from 'tss-react/mui';
 
-export const useColumnStyles = makeStyles()((theme) => ({
-  actions: {
-    display: 'flex',
-    gap: theme.spacing(1)
-  },
-  icon: {
-    fontSize: theme.spacing(2)
-  },
+export const useStyles = makeStyles()((theme) => ({
   removeButton: {
     color: theme.palette.primary.main,
     '& :hover': {
