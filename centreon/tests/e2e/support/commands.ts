6b86d29efca1a60209cbc8038f61c0d22658048e
s-elhassani <selhassani-ext@centreon.com>
2024-11-20 12:42:39 +0100

    [E2E backport] Rewrite ServiceConfiguration.feature on dev-24.04.x

--- centreon/tests/e2e/support/commands.ts
+++ centreon/tests/e2e/support/commands.ts
@@ -60,6 +60,11 @@ Cypress.Commands.add('removeACL', (): Cypress.Chainable => {
   });
 });
 
+Cypress.Commands.add("enterIframe", (iframeSelector): Cypress.Chainable => {
+  return cy.get(iframeSelector)
+    .its("0.contentDocument");
+});
+
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -71,6 +76,7 @@ declare global {
       removeResourceData: () => Cypress.Chainable;
       startOpenIdProviderContainer: () => Cypress.Chainable;
       stopOpenIdProviderContainer: () => Cypress.Chainable;
+      enterIframe: (iframeSelector: string) => Cypress.Chainable;
     }
   }
 }
