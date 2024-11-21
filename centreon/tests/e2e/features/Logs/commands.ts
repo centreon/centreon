255a58002c561ce326ac5c229684be7a747ec906
s-elhassani <selhassani-ext@centreon.com>
2024-11-21 19:38:02 +0100

    Automated E2E - Administration Logs on changes

--- centreon/tests/e2e/features/Logs/commands.ts
+++ centreon/tests/e2e/features/Logs/commands.ts
@@ -11,6 +11,20 @@ Cypress.Commands.add('addTimePeriodViaApi', (payload: TimePeriod) => {
       });
 });
 
+Cypress.Commands.add('addHostSeverityViaAPIv2', (payload: HostSeverity) => {
+  cy.request({
+    body: payload,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    url: '/centreon/api/latest/configuration/hosts/severities'
+  }).then((response) => {
+    expect(response.status).to.eq(201);
+  });
+
+});
+
 Cypress.Commands.add('updateTimePeriodViaApi', (name: string, payload: TimePeriod) => {
   cy.requestOnDatabase({
     database: 'centreon',
@@ -30,6 +44,19 @@ Cypress.Commands.add('updateTimePeriodViaApi', (name: string, payload: TimePerio
   });
 });
 
+Cypress.Commands.add('updateHostSeverityViaAPIv2', (id: number, payload: HostSeverity) => {
+  cy.request({
+      body: payload,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'PUT',
+      url: `/centreon/api/latest/configuration/hosts/severities/${id}`
+  }).then((response) => {
+      expect(response.status).to.eq(204);
+  });
+});
+
 Cypress.Commands.add('deleteTimePeriodViaApi', (name: string) => {
   cy.requestOnDatabase({
     database: 'centreon',
@@ -45,6 +72,18 @@ Cypress.Commands.add('deleteTimePeriodViaApi', (name: string) => {
     }).then((response) => {
       expect(response.status).to.eq(204);
     });
+  });
+});
+
+Cypress.Commands.add('deleteHostSeverityViaAPIv2', (id: number) => {
+  cy.request({
+    headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'DELETE',
+      url: `/centreon/api/latest/configuration/hosts/severities/${id}`
+    }).then((response) => {
+      expect(response.status).to.eq(204);
   });
 });
 
@@ -87,6 +126,14 @@ interface TimePeriod {
   exceptions: IEDyas[],
 }
 
+interface HostSeverity {
+  name: string,
+  alias: string,
+  level: number,
+  icon_id: number,
+  is_activated?: boolean
+}
+
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -94,6 +141,9 @@ declare global {
       updateTimePeriodViaApi: (name: string, body: TimePeriod) => Cypress.Chainable;
       deleteTimePeriodViaApi: (name: string) => Cypress.Chainable;
       checkLogDetails: (tableIndex: number, trIndex:number, firstTd:string, secondTd:string, thirdTd:string) => Cypress.Chainable;
+      addHostSeverityViaAPIv2: (payload: HostSeverity) => Cypress.Chainable;
+      deleteHostSeverityViaAPIv2: (id: number) => Cypress.Chainable;
+      updateHostSeverityViaAPIv2: (id: number, payload: HostSeverity) => Cypress.Chainable;
     }
   }
 }
