77fe9e215aa95e0ee57acf60926053568719869e
Med1233 <melmeziani-ext@centreon.com>
2024-11-22 11:01:13 +0100

    update request params

--- centreon/www/widgets/src/centreon-widget-statusgrid/src/StatusGridCondensed/Tooltip/useLoadResources.tsx
+++ centreon/www/widgets/src/centreon-widget-statusgrid/src/StatusGridCondensed/Tooltip/useLoadResources.tsx
@@ -1,12 +1,14 @@
+import { equals, flatten } from 'ramda';
+
 import { useInfiniteScrollListing } from '@centreon/ui';
 
-import { ResourceStatus } from '../../StatusGridStandard/models';
+import { Resource } from '../../../../models';
 import { tooltipPageAtom } from '../../StatusGridStandard/Tooltip/atoms';
+import { ResourceStatus } from '../../StatusGridStandard/models';
 import {
   getListingCustomQueryParameters,
   resourcesEndpoint
 } from '../../api/endpoints';
-import { Resource } from '../../../../models';
 
 interface UseLoadResourcesProps {
   bypassRequest: boolean;
@@ -28,6 +30,30 @@ export const useLoadResources = ({
   status,
   bypassRequest
 }: UseLoadResourcesProps): UseLoadResourcesState => {
+  const formattedResources = resources.map((resource) => {
+    if (!equals(resourceType, resource.resourceType)) {
+      return {
+        ...resource,
+        resourceType: equals(resource.resourceType, 'host')
+          ? 'parent_name'
+          : `${resource.resourceType.replace('-', '_')}.name`
+      };
+    }
+
+    return { ...resource, resourceType: 'name' };
+  });
+
+  const resourcesSearchConditions = formattedResources.map(
+    ({ resourceType: type, resources: resourcesToApply }) => {
+      return resourcesToApply.map((resource) => ({
+        field: type,
+        values: {
+          $rg: `^${resource.name}$`
+        }
+      }));
+    }
+  );
+
   const { elementRef, elements, isLoading, total } =
     useInfiniteScrollListing<ResourceStatus>({
       customQueryParameters: getListingCustomQueryParameters({
@@ -40,9 +66,12 @@ export const useLoadResources = ({
       limit: 10,
       pageAtom: tooltipPageAtom,
       parameters: {
+        search: {
+          conditions: flatten(resourcesSearchConditions)
+        },
         sort: { status: 'DESC' }
       },
-      queryKeyName: `statusgrid_condensed_${status}`,
+      queryKeyName: `statusgrid_condensed_${status}_${JSON.stringify(resources)}_${resourceType}`,
       suspense: false
     });
 
