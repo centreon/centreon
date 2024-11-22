7b6f8e5e79585ef8774b3d00100ff7625caa49b3
alaunois <alaunois@centreon.com>
2024-11-22 14:56:51 +0100

    Move sql script to 24.10.3 update file

---
+++ centreon/www/install/php/Update-24.10.3.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+require_once __DIR__ . '/../../../bootstrap.php';
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+
+//error specific content
+$versionOfTheUpgrade = 'UPGRADE - 24.10.3: ';
+$errorMessage = '';
+
+// Agent Configuration
+$createAgentConfiguration = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to create agent_configuration table';
+    $pearDB->executeQuery(
+        <<<'SQL'
+            CREATE TABLE IF NOT EXISTS `agent_configuration` (
+                `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                `type` enum('telegraf', 'centreon-agent') NOT NULL,
+                `name` varchar(255) NOT NULL,
+                `configuration` JSON NOT NULL,
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `name_unique` (`name`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+            SQL
+    );
+
+    $errorMessage = 'Unable to create ac_poller_relation table';
+    $pearDB->executeQuery(
+        <<<'SQL'
+            CREATE TABLE IF NOT EXISTS `ac_poller_relation` (
+                `ac_id` INT UNSIGNED NOT NULL,
+                `poller_id` INT(11) NOT NULL,
+                UNIQUE KEY `rel_unique` (`ac_id`, `poller_id`),
+                CONSTRAINT `ac_id_contraint`
+                    FOREIGN KEY (`ac_id`)
+                    REFERENCES `agent_configuration` (`id`) ON DELETE CASCADE,
+                CONSTRAINT `ac_poller_id_contraint`
+                    FOREIGN KEY (`poller_id`)
+                    REFERENCES `nagios_server` (`id`) ON DELETE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+            SQL
+    );
+};
+
+$insertAgentConfigurationTopology = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to retrieve data from topology table';
+    $statement = $pearDB->executeQuery(
+        <<<'SQL'
+            SELECT 1 FROM `topology` WHERE `topology_name` = 'Agent configurations'
+            SQL
+    );
+    $topologyAlreadyExists = (bool) $statement->fetch(\PDO::FETCH_COLUMN);
+
+    $errorMessage = 'Unable to retrieve data from informations table';
+    $statement = $pearDB->executeQuery(
+        <<<'SQL'
+            SELECT `value` FROM `informations` WHERE `key` = 'isCentral'
+            SQL
+    );
+    $isCentral = $statement->fetch(\PDO::FETCH_COLUMN);
+
+    $errorMessage = 'Unable to insert data into table topology';
+    if (false === $topologyAlreadyExists) {
+        $constraintStatement = $pearDB->prepareQuery(
+            <<<'SQL'
+                INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_show`, `is_react`)
+                VALUES (92,'Agent configurations',609,60905,50,1,'/configuration/pollers/agent-configurations', :show, '1');
+                SQL
+        );
+        $pearDB->executePreparedQuery($constraintStatement, [':show' => $isCentral === 'yes' ? '1' : '0']);
+    }
+};
+
+// DDL statements
+$addAllContactsColumnToAclGroups = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to add the colum all_contacts to the table acl_groups';
+    if (! $pearDB->isColumnExist(table: 'acl_groups', column: 'all_contacts')) {
+        $pearDB->exec('ALTER TABLE `acl_groups` ADD COLUMN `all_contacts` TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+};
+
+$addAllContactGroupsColumnToAclGroups = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to add the colum all_contact_groups to the table acl_groups';
+    if (! $pearDB->isColumnExist(table: 'acl_groups', column: 'all_contact_groups')) {
+        $pearDB->exec('ALTER TABLE `acl_groups` ADD COLUMN `all_contact_groups` TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+};
+
+try {
+    $createAgentConfiguration($pearDB);
+    // DDL statements
+    $addAllContactsColumnToAclGroups($pearDB);
+    $addAllContactGroupsColumnToAclGroups($pearDB);
+
+    // Transactional queries
+    if (! $pearDB->inTransaction()) {
+        $pearDB->beginTransaction();
+    }
+
+    $insertAgentConfigurationTopology($pearDB);
+
+    $pearDB->commit();
+} catch (\Exception $e) {
+    CentreonLog::create()->error(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: $versionOfTheUpgrade . $errorMessage,
+        customContext: ['trace' => $e->getTraceAsString()],
+        exception: $e
+    );
+
+    if ($pearDB->inTransaction()) {
+        try {
+            $pearDB->rollBack();
+        } catch (PDOException $e) {
+            CentreonLog::create()->error(
+                logTypeId: CentreonLog::TYPE_UPGRADE,
+                message: "{$versionOfTheUpgrade} error while rolling back the upgrade operation",
+                exception: $e
+            );
+        }
+    }
+
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int) $e->getCode(), $e);
+}
