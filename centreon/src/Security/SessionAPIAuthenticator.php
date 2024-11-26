ed8ee6654dfc0e099389b0db1f21f1884d2d0bc9
Laurent Calvet <lcalvet@centreon.com>
2024-11-26 09:42:15 +0100

    fix (OpenID): fix random OpenID disconnection (#4068) (#5700)

--- centreon/src/Security/SessionAPIAuthenticator.php
+++ centreon/src/Security/SessionAPIAuthenticator.php
@@ -26,7 +26,6 @@ namespace Security;
 use Centreon\Domain\Contact\Interfaces\ContactRepositoryInterface;
 use Centreon\Domain\Exception\ContactDisabledException;
 use Security\Domain\Authentication\Interfaces\AuthenticationServiceInterface;
-use Security\Domain\Authentication\Interfaces\SessionRepositoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -46,30 +45,16 @@ use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPasspor
  */
 class SessionAPIAuthenticator extends AbstractAuthenticator
 {
-    /** @var AuthenticationServiceInterface */
-    private $authenticationService;
-
-    /** @var ContactRepositoryInterface */
-    private $contactRepository;
-
-    /** @var SessionRepositoryInterface */
-    private $sessionRepository;
-
     /**
      * SessionAPIAuthenticator constructor.
      *
      * @param AuthenticationServiceInterface $authenticationService
      * @param ContactRepositoryInterface $contactRepository
-     * @param SessionRepositoryInterface $sessionRepository
      */
     public function __construct(
-        AuthenticationServiceInterface $authenticationService,
-        ContactRepositoryInterface $contactRepository,
-        SessionRepositoryInterface $sessionRepository
+        private AuthenticationServiceInterface $authenticationService,
+        private ContactRepositoryInterface $contactRepository,
     ) {
-        $this->authenticationService = $authenticationService;
-        $this->contactRepository = $contactRepository;
-        $this->sessionRepository = $sessionRepository;
     }
 
     /**
@@ -145,8 +130,6 @@ class SessionAPIAuthenticator extends AbstractAuthenticator
     private function getUserAndUpdateSession(string $sessionId): UserInterface
     {
         $isValidToken = $this->authenticationService->isValidToken($sessionId);
-
-        $this->sessionRepository->deleteExpiredSession();
 
         if (! $isValidToken) {
             throw new BadCredentialsException();
