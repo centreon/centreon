cd5338d3a163f523a739febfc8d05c4f9a62f759
Med1233 <melmeziani-ext@centreon.com>
2024-11-26 23:27:35 +0100

    handle status grid

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-statusgrid/src/StatusGridStandard/Tile.tsx
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-statusgrid/src/StatusGridStandard/Tile.tsx
@@ -101,7 +101,8 @@ const Tile = ({
     );
   }
 
-  const displayStatusTile = data.is_acknowledged || data.is_in_downtime;
+  const displayStatusTile =
+    data.is_acknowledged || data.is_in_downtime || data.is_in_flapping;
 
   if (isSmallestSize && !isNil(data)) {
     return (
@@ -117,6 +118,7 @@ const Tile = ({
               isAcknowledged={data.is_acknowledged}
               isCompact={isSmallestSize}
               isInDowntime={data.is_in_downtime}
+              isInFlapping={data.is_in_flapping}
               type={type}
             />
           )}
@@ -138,6 +140,7 @@ const Tile = ({
             isAcknowledged={data.is_acknowledged}
             isCompact={isSmallestSize}
             isInDowntime={data.is_in_downtime}
+            isInFlapping={data.is_in_flapping}
           />
         )}
         <div className={classes.resourceTypeIcon}>
