0ca230eecfa608194a8bd8d148cf5a6af6ef35c0
s-elhassani <selhassani-ext@centreon.com>
2024-11-29 11:11:13 +0100

    some fix

--- centreon/tests/e2e/support/commands.ts
+++ centreon/tests/e2e/support/commands.ts
@@ -7,9 +7,6 @@ import '../features/Dashboards/commands';
 import '../features/Hosts/commands';
 import '../features/HostGroups/commands';
 import '../features/Contacts/commands';
-import '../features/Ldaps/commands';
-import '../features/Services-configuration/commands';
-import '../features/Agent-configuration/commands';
 import '../features/Logs/commands';
 
 Cypress.Commands.add('refreshListing', (): Cypress.Chainable => {
