9ba3a3e2cadbc2ea49d6388db97c8b771592b2c2
Kevin Duret <kduret@centreon.com>
2024-12-02 11:42:22 +0100

    enh(vault): remove docker volume usage

--- .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-while [ ! -f "/tmp/shared-volume/vault-ids" ]; do
-  sleep 5
-  ls -lah /tmp/shared-volume
-done
+# while [ ! -f "/tmp/shared-volume/vault-ids" ]; do
+#   sleep 5
+#   ls -lah /tmp/shared-volume
+# done
 
-. /tmp/shared-volume/vault-ids
+# . /tmp/shared-volume/vault-ids
 
-RESPONSE=$(curl -s -w "%{http_code}" -H 'Content-Type:application/json' -H 'Accept:application/json' -d '{"security":{"credentials":{"login":"admin","password":"Centreon!2021"}}}' -L "http://localhost:80/centreon/api/latest/login")
-TOKEN=$(echo "$RESPONSE" | head -c -4 | jq -r '.security.token')
+if [ ! -z ${GLPI_HOST} ] && getent hosts ${GLPI_HOST}; then
+  RESPONSE=$(curl -s -w "%{http_code}" -H 'Content-Type:application/json' -H 'Accept:application/json' -d '{"security":{"credentials":{"login":"admin","password":"Centreon!2021"}}}' -L "http://localhost:80/centreon/api/latest/login")
+  TOKEN=$(echo "$RESPONSE" | head -c -4 | jq -r '.security.token')
 
-RESPONSE=$(curl -X PUT \
-     -H "Content-Type: application/json" \
-     -H "X-AUTH-TOKEN: $TOKEN" \
-     -L "http://localhost:80/centreon/api/latest/administration/vaults/configurations" \
-     --data '{"address": "vault", "port": 8200, "root_path": "centreon/", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
+  RESPONSE=$(curl -X PUT \
+      -H "Content-Type: application/json" \
+      -H "X-AUTH-TOKEN: $TOKEN" \
+      -L "http://localhost:80/centreon/api/latest/administration/vaults/configurations" \
+      --data '{"address": "vault", "port": 8200, "root_path": "centreon/", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
 
-STATUS=$(echo "$RESPONSE" | tr -d '\n' | tail -c 3)
+  STATUS=$(echo "$RESPONSE" | tr -d '\n' | tail -c 3)
 
-if [[ $STATUS -eq 200 ]]; then
-  sudo -u apache php /usr/share/centreon/bin/console credentials:migrate-vault
+  if [[ $STATUS -eq 200 ]]; then
+    sudo -u apache php /usr/share/centreon/bin/console credentials:migrate-vault
+  fi
 fi
