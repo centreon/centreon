579674d4b4d89c7ce4025b4819bbd0d11516aafc
Kevin Duret <kduret@centreon.com>
2024-12-03 10:12:57 +0100

    enh(vault): remove docker volume usage

--- .github/docker/vault/entrypoint/container.sh
+++ .github/docker/vault/entrypoint/container.sh
@@ -4,7 +4,27 @@ set -x
 # export VAULT_SKIP_VERIFY=true
 # export VAULT_TOKEN=${VAULT_DEV_ROOT_TOKEN_ID}
 
-vault server -dev-tls -non-interactive -tls-skip-verify -dev-listen-address="0.0.0.0:8200" -log-file=/vault/logs/vault.log &
+cat <<EOM >>/vault/config/vault.hcl
+listener "tcp" {
+  address       = "0.0.0.0:8200"
+}
+
+disable_mlock = true
+api_addr      = "https://0.0.0.0:8200"
+cluster_addr  = "https://0.0.0.0:8201"
+ui            = true
+
+log_file      = "/vault/logs/vault.log"
+log_level     = "info"
+EOM
+
+vault server \
+  -config="/vault/config" \
+  -dev-tls -non-interactive \
+  -tls-skip-verify \
+  -dev-listen-address="0.0.0.0:8200" \
+  -log-file=/vault/logs/vault.log &
+
 # vault secrets enable pki
 # vault write pki/roles/vault-role \
 #     allow_subdomains=true \
@@ -21,19 +41,7 @@ vault server -dev-tls -non-interactive -tls-skip-verify -dev-listen-address="0.0
 for i in $(seq 1 30); do grep -q 'successful mount' /vault/logs/vault.log && break || sleep 1; done
 
 vault secrets enable -path=centreon kv
-vault auth enable -address=https://0.0.0.0:8200 approle
-
-# mkdir -p /etc/vault.d
-
-cat <<EOM >>/vault/config/vault.hcl
-listener "tcp" {
-  address       = "0.0.0.0:8200"
-}
-disable_mlock = true
-api_addr      = "https://0.0.0.0:8200"
-cluster_addr  = "https://0.0.0.0:8201"
-ui            = true
-EOM
+vault auth enable approle
 
 cat <<EOM >>/vault/config/central_policy.hcl
 path "centreon/*" {
@@ -44,22 +52,6 @@ EOM
 vault policy write central /vault/config/central_policy.hcl
 vault write auth/approle/role/central token_policies="central" \
   token_ttl=1h token_max_ttl=4h
-
-# while [[ -z $VAULT_ROLE_ID ]] || [[ -z $VAULT_SECRET_ID ]]; do
-#   export VAULT_ROLE_ID=$(vault read auth/approle/role/central/role-id -format=json | jq -r '.data.role_id')
-#   export VAULT_SECRET_ID=$(vault write -force auth/approle/role/central/secret-id -format=json | jq -r '.data.secret_id')
-#   sleep 5
-# done
-
-# if [ -f /tmp/shared-volume/vault-ids ]; then
-#   rm -f /tmp/shared-volume/vault-ids
-# fi
-
-# # Export role IDs/secret IDs to the shared volume so that they are used by the central server in the registration process
-# if [ ! -f /tmp/shared-volume/vault-ids ] && [[ -n $VAULT_ROLE_ID ]] && [[ -n $VAULT_SECRET_ID ]]; then
-#   echo "VAULT_ROLE_ID=$VAULT_ROLE_ID" >> /tmp/shared-volume/vault-ids
-#   echo "VAULT_SECRET_ID=$VAULT_SECRET_ID" >> /tmp/shared-volume/vault-ids
-# fi
 
 vault write auth/approle/role/central token_policies="central" token_ttl=1h token_max_ttl=4h role_id=$VAULT_ROLE_ID
 vault write auth/approle/role/central/custom-secret-id ttl=0 secret_id=$VAULT_SECRET_ID
