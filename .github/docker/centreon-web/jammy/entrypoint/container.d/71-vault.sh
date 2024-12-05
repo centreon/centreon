a450348728452222d4869272586a08be9c141930
Kevin Duret <kduret@centreon.com>
2024-12-05 15:09:31 +0100

    fix php

--- .github/docker/centreon-web/jammy/entrypoint/container.d/71-vault.sh
+++ .github/docker/centreon-web/jammy/entrypoint/container.d/71-vault.sh
@@ -4,7 +4,7 @@ if [ ! -z ${VAULT_HOST} ] && getent hosts ${VAULT_HOST}; then
   sed -i 's@"vault": [0-3]@"vault": 3@' /usr/share/centreon/config/features.json
   sed -i 's@"vault_broker": [0-3]@"vault_broker": 3@' /usr/share/centreon/config/features.json
   sed -i 's@"vault_gorgone": [0-3]@"vault_gorgone": 3@' /usr/share/centreon/config/features.json
-  sed -i 's@new CurlHttpClient\(\)@new CurlHttpClient(["verify_peer" => false, "verify_host" => false])@' /usr/share/centreon/config/centreon.config.php
+  sed -i 's@new CurlHttpClient()@new CurlHttpClient(["verify_peer" => false, "verify_host" => false])@' /usr/share/centreon/config/centreon.config.php
   sed -i 's/default_options:/&\n            verify_host: false/' /usr/share/centreon/config/packages/framework.yaml
   sed -i 's/default_options:/&\n            verify_peer: false/' /usr/share/centreon/config/packages/framework.yaml
   sudo -u apache php /usr/share/centreon/bin/console cache:clear
