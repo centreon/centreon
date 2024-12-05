75b0b39e6061d177fd837384ffa45ac203d641a1
nouha <nelabrouki-ext@centreon.com>
2024-12-05 15:16:21 +0100

    enhances

--- centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/Actions/favoriteFilter/favoriteFilter.styles.ts
+++ centreon/www/front_src/src/Dashboards/components/DashboardLibrary/DashboardListing/Actions/favoriteFilter/favoriteFilter.styles.ts
@@ -2,7 +2,9 @@ import { makeStyles } from 'tss-react/mui';
 
 const useFavoriteFilterStyles = makeStyles()((theme) => ({
   container: {
-    display: 'flex'
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center'
   },
   label: {
     paddingLeft: theme.spacing(0.25)
