b9d13d76da2b8219681e27a3c0fe3009a2b92fef
Kevin Duret <kduret@centreon.com>
2024-12-05 11:54:59 +0100

    fix php

--- .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/71-vault.sh
@@ -11,6 +11,7 @@ if [ ! -z ${VAULT_HOST} ] && getent hosts ${VAULT_HOST}; then
   sed -i 's@"vault": [0-3]@"vault": 3@' /usr/share/centreon/config/features.json
   sed -i 's@"vault_broker": [0-3]@"vault_broker": 3@' /usr/share/centreon/config/features.json
   sed -i 's@"vault_gorgone": [0-3]@"vault_gorgone": 3@' /usr/share/centreon/config/features.json
+  sed -i 's@new CurlHttpClient\(\)@new CurlHttpClient(["verify_peer" => false,"verify_host" => false])@' /usr/share/centreon/config/centreon.config.php
   sed -i 's/default_options:/&\n            verify_host: false/' /usr/share/centreon/config/packages/framework.yaml
   sed -i 's/default_options:/&\n            verify_peer: false/' /usr/share/centreon/config/packages/framework.yaml
   sudo -u apache php /usr/share/centreon/bin/console cache:clear
@@ -18,16 +19,11 @@ if [ ! -z ${VAULT_HOST} ] && getent hosts ${VAULT_HOST}; then
   RESPONSE=$(curl -s -w "%{http_code}" -H 'Content-Type:application/json' -H 'Accept:application/json' -d '{"security":{"credentials":{"login":"admin","password":"Centreon!2021"}}}' -L "http://localhost:80/centreon/api/latest/login")
   TOKEN=$(echo "$RESPONSE" | head -c -4 | jq -r '.security.token')
 
-  RESPONSE=$(curl -X PUT \
+  curl -X PUT \
       -H "Content-Type: application/json" \
       -H "X-AUTH-TOKEN: $TOKEN" \
       -L "http://localhost:80/centreon/api/latest/administration/vaults/configurations" \
-      --data '{"address": "vault", "port": 8200, "root_path": "centreon", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}')
+      --data '{"address": "vault", "port": 8200, "root_path": "centreon", "role_id": "'"$VAULT_ROLE_ID"'", "secret_id": "'"$VAULT_SECRET_ID"'"}'
 
-  STATUS=$(echo "$RESPONSE" | tr -d '\n' | tail -c 3)
-
-  if [[ $STATUS -eq 200 ]]; then
-    php /usr/share/centreon/bin/migrateCredentials.php
-    # sudo -u apache php /usr/share/centreon/bin/console vault:migrate-credentials:web
-  fi
+  php /usr/share/centreon/bin/migrateCredentials.php
 fi
