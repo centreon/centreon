acb8ded9e6ece42e1ab5b8921fc48d77dcf429d9
selfakiri <selfakiri@ext.centreon.com>
2024-12-05 19:02:53 +0100

    fix(configuration): improve input sanitization for contact form

--- centreon/www/include/configuration/configObject/contact/DB-Func.php
+++ centreon/www/include/configuration/configObject/contact/DB-Func.php
@@ -648,7 +648,10 @@ function insertContact($ret = [])
     if (isset($ret['contact_oreon']['contact_oreon']) && $ret['contact_oreon']['contact_oreon'] === '1') {
         $ret['reach_api_rt']['reach_api_rt'] = '1';
     }
-
+    // Filter fields to only include whitelisted fields for non-admin users
+    if (($centreon->user->admin ?? false) !== true) {
+        $ret = filterNonAdminFields($ret);
+    }
     $bindParams = sanitizeFormContactParameters($ret);
     $params = [];
     foreach (array_keys($bindParams) as $token) {
@@ -700,12 +703,16 @@ function updateContact($contactId = null)
         return;
     }
     $ret = $form->getSubmitValues();
-    // remove illegal chars in data sent by the user
+    // Filter fields to only include whitelisted fields for non-admin users
+    if (($centreon->user->admin ?? false) !== true) {
+        $ret = filterNonAdminFields($ret);
+    }
+    // Remove illegal chars in data sent by the user
     $ret['contact_name'] = CentreonUtils::escapeSecure($ret['contact_name'], CentreonUtils::ESCAPE_ILLEGAL_CHARS);
     $ret['contact_alias'] = CentreonUtils::escapeSecure($ret['contact_alias'], CentreonUtils::ESCAPE_ILLEGAL_CHARS);
     $bindParams = sanitizeFormContactParameters($ret);
 
-    //Build Query with only setted values.
+    // Build Query with only setted values.
     $rq = "UPDATE contact SET ";
     foreach (array_keys($bindParams) as $token) {
         $rq .= ltrim($token, ':') . " = " . $token . ", ";
@@ -758,6 +765,10 @@ function updateContact_MC($contact_id = null)
         if (is_string($value) && empty($value)) {
             unset($ret[$name]);
         }
+    }
+    // Filter fields to only include whitelisted fields for non-admin users
+    if (($centreon->user->admin ?? false) !== true) {
+        $ret = filterNonAdminFields($ret);
     }
 
     $bindParams = sanitizeFormContactParameters($ret);
@@ -1671,4 +1682,31 @@ function validateAutologin(array $fields)
     }
 
     return $errors !== [] ? $errors : true;
+}
+
+/**
+ * Filter the fields in the $ret array to only include whitelisted fields for non-admin users.
+ *
+ * @param array $ret
+ * @return array
+ */
+function filterNonAdminFields(array $ret): array
+{
+    $allowedFields = [
+        'contact_alias', 'contact_name', 'contact_email', 'contact_pager',
+        'contact_cgNotif', 'contact_enable_notifications', 'contact_hostNotifOpts',
+        'timeperiod_tp_id', 'contact_hostNotifCmds', 'contact_svNotifOpts',
+        'timeperiod_tp_id2', 'contact_svNotifCmds', 'contact_oreon', 'contact_passwd', 'contact_passwd2',
+        'contact_lang', 'default_page', 'contact_location', 'contact_autologin_key', 'contact_auth_type',
+        'contact_acl_groups', 'contact_address1', 'contact_address2', 'contact_address3', 'contact_address4',
+        'contact_address5', 'contact_address6', 'contact_comment', 'submitC', 'contact_register', 'contact_activate',
+        'contact_id', 'o', 'initialValues', 'centreon_token'
+    ];
+    foreach ($ret as $field => $value) {
+        if (!in_array($field, $allowedFields, true)) {
+            unset($ret[$field]);
+        }
+    }
+
+    return $ret;
 }
