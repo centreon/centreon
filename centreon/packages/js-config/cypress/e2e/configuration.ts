04989205afcfa17247e08c065b806b5f172715a1
Nouha-ElAbrouki <97687698+Noha-ElAbrouki@users.noreply.github.com>
2024-12-12 14:12:41 +0100

    reset

--- centreon/packages/js-config/cypress/e2e/configuration.ts
+++ centreon/packages/js-config/cypress/e2e/configuration.ts
@@ -49,7 +49,7 @@ export default ({
         configFile: `${__dirname}/reporter-config.js`
       },
       setupNodeEvents: async (cypressOn, config) => {
-        const on = require('cypress-on-fix')(cypressOn);
+        const on = require('cypress-on-fix')(cypressOn)
         installLogsPrinter(on);
         await esbuildPreprocessor(on, config);
         tasks(on);
