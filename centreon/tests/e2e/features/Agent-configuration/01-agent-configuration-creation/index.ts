188d720c51e791c194ad9be41b2aab994a15650c
s-elhassani <selhassani-ext@centreon.com>
2024-12-19 15:04:18 +0100

    some fix

--- centreon/tests/e2e/features/Agent-configuration/01-agent-configuration-creation/index.ts
+++ centreon/tests/e2e/features/Agent-configuration/01-agent-configuration-creation/index.ts
@@ -51,7 +51,7 @@ Given('a non-admin user is in the Agents Configuration page', () => {
 });
 
 When('the user clicks on Add', () => {
-  cy.contains('button', 'Add poller/agent configuration').click();
+  cy.contains('button', 'Add').click();
 });
 
 Then('a pop-up menu with the form is displayed', () => {
