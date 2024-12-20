8aedb957115bf6b98a21ba698a7d8fee9e9e0b50
Thebarda <tomdar87@outlook.com>
2024-12-20 09:43:07 +0100

    fix?

--- centreon/packages/ui/cypress/support/commands.tsx
+++ centreon/packages/ui/cypress/support/commands.tsx
@@ -1,3 +1,3 @@
 import '@centreon/js-config/cypress/component/commands';
 
-Cypress.Commands.add('adjustViewport', () => cy.viewport(1280, 581));
+Cypress.Commands.add('adjustViewport', () => cy.viewport(1280, 590));
