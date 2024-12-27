c178d81745f6abda8c2078dd67e1a7e45501254e
Kevin Duret <kduret@centreon.com>
2024-12-27 10:37:42 +0100

    fix(ci): fix upload of cypress component test results

--- centreon/www/front_src/src/Login/Login.cypress.spec.tsx
+++ centreon/www/front_src/src/Login/Login.cypress.spec.tsx
@@ -247,7 +247,6 @@ describe('Login Page', () => {
     cy.findByLabelText(labelPassword).should('be.visible');
     cy.findByLabelText(labelConnect).should('be.visible');
     cy.contains(labelPoweredByCentreon).should('be.visible');
-
     cy.contains('v. 21.10.1').should('be.visible');
 
     cy.findByTestId('Login with openid').should(
