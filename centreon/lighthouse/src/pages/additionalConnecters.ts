9507ccdce4b59be43a0feafea594fe258d29246a
Med1233 <melmeziani-ext@centreon.com>
2024-12-31 08:43:22 +0100

    ACC & PAC performance tests

---
+++ centreon/lighthouse/src/pages/additionalConnecters.ts
@@ -0,0 +1,34 @@
+import { baseUrl } from '../defaults';
+import additionalConnectors from '../fixtures/additionalConnectors';
+import { GenerateReportForPageProps } from '../models';
+
+export const generateReportForACCsPage = async ({
+  navigate,
+  snapshot,
+  page
+}: GenerateReportForPageProps): Promise<void> => {
+  const cookies = await page.cookies();
+
+  await fetch(
+    `${baseUrl}api/latest/configuration/additional-connector-configurations`,
+    {
+      method: 'POST',
+      body: JSON.stringify(additionalConnectors),
+      headers: {
+        Cookie: `${cookies[0].name}=${cookies[0].value}`
+      }
+    }
+  );
+
+  await navigate({
+    name: 'ACCs page Cold navigation',
+    url: `${baseUrl}configuration/additional-connector-configurations`
+  });
+
+  await navigate({
+    name: 'ACCs page Warm navigation',
+    url: `${baseUrl}configuration/additional-connector-configurations`
+  });
+
+  await snapshot('ACCs Snapshot');
+};
