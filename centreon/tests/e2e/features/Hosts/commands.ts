39c4beb52f0db62bf64e52463b8eede5bbeebb0a
s-elhassani <selhassani-ext@centreon.com>
2024-12-31 11:52:35 +0100

    enh (host-group): migrate the configuration of the host groups dependencies behat test to a cypress test

--- centreon/tests/e2e/features/Hosts/commands.ts
+++ centreon/tests/e2e/features/Hosts/commands.ts
@@ -61,6 +61,68 @@ Cypress.Commands.add('updateHostGroupViaApi', (body: HostGroup, hostGroup_name: 
 
 });
 
+Cypress.Commands.add('addHostGroupDependency', (body: HostGroupDependency) => {
+  cy.waitForElementInIframe('#main-content', 'input[name="dep_name"]');
+  cy.getIframeBody()
+    .find('input[name="dep_name"]')
+    .type(body.name);
+  cy.getIframeBody()
+    .find('input[name="dep_description"]')
+    .type(body.description);
+  cy.getIframeBody().find('label[for="eDown"]').click({ force: true });
+  cy.getIframeBody().find('label[for="nPending"]').click({ force: true });
+  cy.getIframeBody().find('input[class="select2-search__field"]').eq(0).click();
+  cy.getIframeBody().find(`div[title="${body.hostGrpsNames[0]}"]`).click();
+  cy.getIframeBody().find('input[class="select2-search__field"]').eq(1).click();
+  cy.getIframeBody().find(`div[title="${body.dependentHostGrpsNames[0]}"]`).click();
+  cy.getIframeBody()
+    .find('textarea[name="dep_comment"]')
+    .type(body.comment);
+  cy.getIframeBody().find('input.btc.bt_success[name^="submit"]').eq(0).click();
+});
+
+Cypress.Commands.add('updateHostGroupDependency', (body: HostGroupDependency) => {
+  cy.waitForElementInIframe('#main-content', 'input[name="dep_name"]');
+    cy.getIframeBody()
+      .find('input[name="dep_name"]')
+      .clear()
+      .type(body.name);
+    cy.getIframeBody()
+      .find('input[name="dep_description"]')
+      .clear()
+      .type(body.description);
+    cy.getIframeBody().find('label[for="eDown"]').click({ force: true });
+    cy.getIframeBody().find('label[for="eUp"]').click({ force: true });
+
+    cy.getIframeBody().find('label[for="nPending"]').click({ force: true });
+    cy.getIframeBody().find('label[for="nDown"]').click({ force: true });
+    cy.getIframeBody().find('span[title="Clear field"]').eq(0).click();
+    cy.getIframeBody()
+      .find('input[class="select2-search__field"]')
+      .eq(0)
+      .click();
+    cy.getIframeBody().find(`div[title="${body.hostGrpsNames[0]}"]`).click();
+    cy.getIframeBody()
+      .find('input[class="select2-search__field"]')
+      .eq(0)
+      .click();
+    cy.getIframeBody().find(`div[title="${body.hostGrpsNames[1]}"]`).click();
+    cy.getIframeBody().find('span[title="Clear field"]').eq(1).click();
+    cy.getIframeBody()
+      .find('input[class="select2-search__field"]')
+      .eq(1)
+      .type(body.dependentHostGrpsNames[0]);
+    cy.getIframeBody().find(`div[title="${body.dependentHostGrpsNames[0]}"]`).click();
+    cy.getIframeBody()
+      .find('textarea[name="dep_comment"]')
+      .clear()
+      .type(body.comment);
+    cy.getIframeBody()
+      .find('input.btc.bt_success[name^="submit"]')
+      .eq(0)
+      .click();
+})
+
 interface HostGroup {
   name: string,
   alias: string,
@@ -75,6 +137,25 @@ interface HostGroup {
   is_activated: boolean
 }
 
+interface HostGroupDependency {
+  name: string,
+  description: string,
+  parent_relationship: number,
+  execution_fails_on_ok: number,
+  execution_fails_on_down: number,
+  execution_fails_on_unreachable: number,
+  execution_fails_on_pending: number,
+  execution_fails_on_none: number,
+  notification_fails_on_none: number,
+  notification_fails_on_ok: number,
+  notification_fails_on_down: number,
+  notification_fails_on_unreachable: number,
+  notification_fails_on_pending: number,
+  hostGrpsNames: string[],
+  dependentHostGrpsNames: string[],
+  comment: string
+}
+
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -84,6 +165,8 @@ declare global {
       ) => Cypress.Chainable;
       checkLegacyRadioButton: (label: string) => Cypress.Chainable;
       updateHostGroupViaApi: (body: HostGroup, name: string) => Cypress.Chainable;
+      addHostGroupDependency: (body: HostGroupDependency) => Cypress.Chainable;
+      updateHostGroupDependency: (body: HostGroupDependency) => Cypress.Chainable;
     }
   }
 }
