2c681b567b27ca23bab3510c414e9acc576695a5
nouha <nelabrouki-ext@centreon.com>
2025-01-09 13:55:16 +0100

    test

--- centreon/www/front_src/src/AdditionalConnectorConfiguration/Modal/Parameters/Port.tsx
+++ centreon/www/front_src/src/AdditionalConnectorConfiguration/Modal/Parameters/Port.tsx
@@ -37,7 +37,7 @@ const Port = (): ReactElement => {
         required
         dataTestId={`${labelPort}_value`}
         error={error as string}
-        label={t(labelPort)}
+        // label={t(labelPort)}
         slotProps={{
           htmlInput: {
             'data-testId':'numberFieldInput',
