82ff03076c7e5945dca34d192c1aa149249c1c4e
Adrien Morais <amorais@centreon.com>
2025-01-09 11:25:41 +0100

    enh(dashboard): add SQL upgrade to enable favorite management feature

--- centreon/www/install/php/Update-25.01.0.php
+++ centreon/www/install/php/Update-25.01.0.php
@@ -1,6 +1,7 @@
 <?php
+
 /*
- * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2024 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,3 +18,67 @@
  * For more information : contact@centreon.com
  *
  */
+
+require_once __DIR__ . '/../../../bootstrap.php';
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+
+$centreonLog = CentreonLog::create();
+
+// error specific content
+$versionOfTheUpgrade = 'UPGRADE - 25.01.0: ';
+$errorMessage = '';
+
+$createUserProfileTable = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to add table user_profile';
+    $pearDB->executeQuery(
+        <<<SQL
+        CREATE TABLE IF NOT EXISTS `user_profile` (
+          `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+          `contact_id` INT(11) NOT NULL,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `unique_user_profile` (`id`, `contact_id`),
+          CONSTRAINT `fk_user_profile_contact_id`
+            FOREIGN KEY (`contact_id`)
+            REFERENCES `contact` (`contact_id`) ON DELETE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        SQL
+    );
+};
+
+$createUserProfileFavoriteDashboards = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to add table user_profile_favorite_dashboards';
+    $pearDB->executeQuery(
+        <<<SQL
+        CREATE TABLE IF NOT EXISTS `user_profile_favorite_dashboards` (
+          `profile_id` INT UNSIGNED NOT NULL,
+          `dashboard_id` INT UNSIGNED NOT NULL,
+          CONSTRAINT `fk_user_profile_favorite_dashboards_profile_id`
+            FOREIGN KEY (`profile_id`)
+            REFERENCES `user_profile` (`id`) ON DELETE CASCADE,
+          CONSTRAINT `fk_user_profile_favorite_dashboards_dashboard_id`
+            FOREIGN KEY (`dashboard_id`)
+            REFERENCES `dashboard` (`id`) ON DELETE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        SQL
+    );
+};
+
+try {
+    $createUserProfileTable($pearDB);
+    $createUserProfileFavoriteDashboards($pearDB);
+} catch (Exception $e) {
+    if ($pearDB->inTransaction()) {
+        $pearDB->rollBack();
+    }
+
+    $centreonLog->log(
+        CentreonLog::TYPE_UPGRADE,
+        CentreonLog::LEVEL_ERROR,
+        $versionOfTheUpgrade . $errorMessage
+        . ' - Code : ' . (int) $e->getCode()
+        . ' - Error : ' . $e->getMessage()
+        . ' - Trace : ' . $e->getTraceAsString()
+    );
+
+    throw new Exception($versionOfTheUpgrade . $errorMessage, (int) $e->getCode(), $e);
+}
