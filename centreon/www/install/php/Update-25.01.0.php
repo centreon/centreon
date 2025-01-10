9027426794bbdb2b017bda6f2c061c5a4d68cd9d
Thebarda <tomdar87@outlook.com>
2025-01-10 15:45:49 +0100

    add update script

--- centreon/www/install/php/Update-25.01.0.php
+++ centreon/www/install/php/Update-25.01.0.php
@@ -17,3 +17,52 @@
  * For more information : contact@centreon.com
  *
  */
+
+
+require_once __DIR__ . '/../../../bootstrap.php';
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+
+$centreonLog = new CentreonLog();
+
+// error specific content
+$versionOfTheUpgrade = 'UPGRADE - 25.01.0: ';
+$errorMessage = '';
+
+// Dashboard
+$updatePanelsLayout = function (CentreonDB $pearDB) use (&$errorMessage): void {
+    $errorMessage = 'Unable to update table dashboard_panel';
+    $pearDB->executeQuery(
+        <<<'SQL'
+            UPDATE `dashboard_panel`
+            SET `layout_x` = `layout_x` * 2,
+                `layout_width` = `layout_width` * 2 
+            SQL
+    );
+};
+
+
+try {
+    // Transactional queries
+    if (! $pearDB->inTransaction()) {
+        $pearDB->beginTransaction();
+    }
+
+    $updatePanelsLayout($pearDB);
+
+    $pearDB->commit();
+} catch (Exception $e) {
+
+    if ($pearDB->inTransaction()) {
+        $pearDB->rollBack();
+    }
+
+    $centreonLog->insertLog(
+        4,
+        $versionOfTheUpgrade . $errorMessage
+            . ' - Code : ' . (int) $e->getCode()
+            . ' - Error : ' . $e->getMessage()
+            . ' - Trace : ' . $e->getTraceAsString()
+    );
+
+    throw new Exception($versionOfTheUpgrade . $errorMessage, (int) $e->getCode(), $e);
+}
