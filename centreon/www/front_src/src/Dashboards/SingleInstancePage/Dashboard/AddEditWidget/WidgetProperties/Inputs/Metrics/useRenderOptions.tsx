ce43b850e767f398e353924752981a51290741b2
nouha <nelabrouki-ext@centreon.com>
2025-01-10 15:35:10 +0100

    fiiix

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/Inputs/Metrics/useRenderOptions.tsx
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/Inputs/Metrics/useRenderOptions.tsx
@@ -347,7 +347,7 @@ export const useRenderOptions = ({
       !isEmpty(currentMetricValue?.excludedMetrics);
 
     return (
-      <ListItem disableGutters>
+      <ListItem disableGutters key={option?.id}>
         <CollapsibleItem
           compact
           dataTestId={option.name}
