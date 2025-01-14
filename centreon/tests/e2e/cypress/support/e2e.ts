6fd11de9ff895a1832d8f6468a602e3a8f87e209
MedWassCh <wchaabane@centreon.com>
2025-01-14 11:39:20 +0100

    adding another exception

--- centreon/tests/e2e/cypress/support/e2e.ts
+++ centreon/tests/e2e/cypress/support/e2e.ts
@@ -5,6 +5,7 @@ Cypress.on('uncaught:exception', (err) => {
   if (
     err.message.includes('Request failed with status code 401') ||
     err.message.includes('Request failed with status code 403') ||
+    err.message.includes('Request failed with status code 500') ||
     err.message.includes('undefined') ||
     err.message.includes('postMessage') ||
     err.message.includes('canceled') ||
