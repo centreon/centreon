5f8c70ada642ea15a6169d94e5c0bbba7c59ffab
Med1233 <melmeziani-ext@centreon.com>
2025-01-16 11:34:24 +0100

    undo

--- centreon/packages/ui/src/Dialog/UnsavedChanges/translatedLabels.ts
+++ centreon/packages/ui/src/Dialog/UnsavedChanges/translatedLabels.ts
@@ -1,13 +1,24 @@
 export const labelDiscard = 'Discard';
-export const labelIfYouClickOnDiscard =
-  'If you click on Discard, your changes will not be saved.';
 export const labelSave = 'Save';
 
+export const labelLeave = 'Leave';
+export const labelStay = 'Stay';
+
+export const labelIfYouClickOnDiscard =
+  'If you click on Discard, your changes will not be saved.';
 export const labelDoYouWantToQuit = 'Do you want to leave this page?';
 
 export const labelYourFormHasUnsavedChanges =
   'Your changes will not be saved if you leave this page.';
 export const labelDoYouWantToSaveChanges = 'Do you want to save the changes?';
 
-export const labelLeave = 'Leave';
-export const labelStay = 'Stay';
+export default {
+  labelDiscard,
+  labelSave,
+  labelLeave,
+  labelStay,
+  labelIfYouClickOnDiscard,
+  labelDoYouWantToQuit,
+  labelYourFormHasUnsavedChanges,
+  labelDoYouWantToSaveChanges
+};
