242197464f152543104f6a14de765b3e7f43b1b2
Kevin Duret <kduret@centreon.com>
2025-01-27 14:21:59 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/80-register_pollers_background.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/80-register_pollers_background.sh
@@ -2,15 +2,19 @@
 
 while true ; do
   sleep 10
+
   for var in $(printenv | grep -Eo '^CENTREON_POLLER_[0-9]+='); do
     var_name=$(echo $var | sed 's/=//')
-    printenv $var_name
-
-    SQL_RESULT=$(timeout 5 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" centreon_storage -e "SELECT instance_id FROM instances WHERE name = '$(printenv $var_name)'")
-    if [[ "$SQL_RESULT" != *"instance_id"* ]] ; then
-      echo "Poller $(printenv $var_name) is not in monitoring database. Restarting gorgoned to register it."
-      systemctl restart gorgoned
-      sleep 60
+    poller_name=$(printenv $var_name)
+    if getent hosts $poller_name; then
+      echo "Poller $poller_name is reachable by central server."
     fi
   done
+
+  SQL_RESULT=$(timeout 5 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" centreon -e "SELECT id FROM nagios_server WHERE name NOT IN (SELECT name from centreon_storage.instances)")
+  if [[ "$SQL_RESULT" == *"id"* ]] ; then
+    echo "Restarting gorgoned to register new pollers."
+    systemctl restart gorgoned
+    sleep 60
+  fi
 done
