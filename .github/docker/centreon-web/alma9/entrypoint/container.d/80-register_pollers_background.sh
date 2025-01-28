c527084d7f8783b784e2203d2bb3c6b44a37f318
Kevin Duret <kduret@centreon.com>
2025-01-28 09:23:48 +0100

    fix

--- .github/docker/centreon-web/alma9/entrypoint/container.d/80-register_pollers_background.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/80-register_pollers_background.sh
@@ -3,18 +3,10 @@
 while true ; do
   sleep 10
 
-  for var in $(printenv | grep -Eo '^CENTREON_POLLER_[0-9]+='); do
-    var_name=$(echo $var | sed 's/=//')
-    poller_name=$(printenv $var_name)
-    if getent hosts $poller_name; then
-      echo "Poller $poller_name is reachable by central server."
-    fi
-  done
-
   SQL_RESULT=$(timeout 5 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" centreon -e "SELECT id FROM nagios_server WHERE name NOT IN (SELECT name from centreon_storage.instances)")
   if [[ "$SQL_RESULT" == *"id"* ]] ; then
     echo "Restarting gorgoned to register new pollers."
-    systemctl restart gorgoned
+    systemctl reload gorgoned
     sleep 60
   else
     echo "No new pollers to register."
