c7b6bbc547611f74a7933d906def4575ae982c2b
MedWassCh <wchaabane@centreon.com>
2025-01-28 14:19:53 +0100

    Adding new command

--- centreon/tests/e2e/support/commands.ts
+++ centreon/tests/e2e/support/commands.ts
@@ -15,6 +15,7 @@ import '../features/Services-configuration/commands';
 import '../features/Agent-configuration/commands';
 import '../features/Logs/commands';
 import '../features/Notifications/commands';
+import '../features/Resources-status/commands';
 
 Cypress.Commands.add('refreshListing', (): Cypress.Chainable => {
   return cy.get(refreshButton).click();
