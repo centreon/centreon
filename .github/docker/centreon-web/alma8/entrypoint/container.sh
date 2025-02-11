bb638c7c915df8379707777c8874e93eae13244a
Kevin Duret <kduret@centreon.com>
2025-02-11 17:53:48 +0100

    fix

--- .github/docker/centreon-web/alma8/entrypoint/container.sh
+++ .github/docker/centreon-web/alma8/entrypoint/container.sh
@@ -7,5 +7,17 @@ set -x
 # ls is required to ensure that the scripts are properly sorted by name.
 BASEDIR="/usr/share/centreon/container.d"
 for file in `ls $BASEDIR` ; do
-  . "$BASEDIR/$file"
+  case "$file" in
+    *_background*)
+      # Execute background script and store PID
+      . "$BASEDIR/$file" &
+      echo $! >> /tmp/background_pids
+      ;;
+    *)
+      if ! . "$BASEDIR/$file"; then
+        echo "Error executing $file"
+        exit 1
+      fi
+      ;;
+  esac
 done
