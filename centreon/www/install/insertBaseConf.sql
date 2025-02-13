56d9914c4e258c534bb3bc615c5da5d8e90b59f3
pkippes <144150042+pkippes@users.noreply.github.com>
2025-02-13 11:55:28 +0100

    chore(release) bump web to 23.10.22

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '23.10.21');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '23.10.22');
 
 --
 -- Contenu de la table `contact`
