0253feb0bb7bf4b9964aa4f94b056cb1fbd7405d
pkippes <144150042+pkippes@users.noreply.github.com>
2025-02-14 16:34:27 +0100

    chore(release) bump 22.10 OSS for february on prem release (#6666)

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '22.10.28');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '22.10.29');
 
 --
 -- Contenu de la table `contact`
