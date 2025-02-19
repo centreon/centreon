42fb707758082255e476d2b27884700740559f68
Anthony Graule <agraule@centreon.com>
2025-02-19 11:23:40 +0100

    unit test to execute query on several databases for CentreonDB.php and DatabaseConnection.php added

--- centreon/tests/php/Centreon/Infrastructure/DatabaseConnectionTest.php
+++ centreon/tests/php/Centreon/Infrastructure/DatabaseConnectionTest.php
@@ -1374,6 +1374,28 @@ if (! is_null($dbConfigCentreon) && hasConnectionDb($dbConfigCentreon)) {
             expect($successClose)->toBeTrue();
         }
     );
+
+    // ----------------------------------- QUERY ON SEVERAL DATABASES -------------------------------------
+
+    it(
+        'test DatabaseConnection : execute query on several databases with success',
+        function () use ($dbConfigCentreon): void {
+            $db = DatabaseConnection::createFromConfig(connectionConfig: $dbConfigCentreon);
+            // get log actions done by admin
+            $sql = <<<'SQL'
+                SELECT * FROM `centreon_storage`.`log_action` AS la 
+                    INNER JOIN `centreon`.`contact` AS c
+                        ON la.log_contact_id = c.contact_id
+                WHERE c.contact_id = :contact_id;
+                SQL;
+            $logActions = $db->fetchAllAssociative(
+                $sql,
+                QueryParameters::create([QueryParameter::int('contact_id', 1)])
+            );
+            expect($logActions)->toBeArray()->toHaveCount(11);
+        }
+    );
+
 } else {
     it('no centreon database available for testing the DatabaseConnection connector, so these tests were ignored');
 }
