eeb07270bd88ba79cdc70d8fcae782557817c8b9
Med1233 <melmeziani-ext@centreon.com>
2025-02-20 22:46:09 +0100

    clean up

--- centreon/www/front_src/src/Configuration/ConfigurationBase/Modal/Form/Form.tsx
+++ centreon/www/front_src/src/Configuration/ConfigurationBase/Modal/Form/Form.tsx
@@ -1,24 +1,26 @@
 import { useFormikContext } from 'formik';
 import { useSetAtom } from 'jotai';
 import { equals } from 'ramda';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Form } from '@centreon/ui';
+import { Form, Group, InputProps } from '@centreon/ui';
 import { FormActions, FormActionsProps } from '@centreon/ui/components';
-
 import { CloseModalConfirmation } from '../../Dialogs';
-import useGetDetails from '../../api/useGetOne';
 
 import { isFormDirtyAtom } from '../../atoms';
-
 import { labelCancel, labelCreate, labelUpdate } from '../../translatedLabels';
-
 import { useFormStyles } from './Form.styles';
 
 export type HostGroupFormProps = {
   id?: number;
   onSubmit?;
   mode?: 'add' | 'edit';
+  inputs: InputProps;
+  groups: Array<Group>;
+  validationSchema;
+  initialValues;
+  isLoading: boolean;
 } & Pick<FormActionsProps, 'onCancel'>;
 
 export type ConnectorFormLabels = {
@@ -26,57 +28,34 @@ export type ConnectorFormLabels = {
   entity;
 };
 
-const HostGroupForm = ({
-  id,
-  mode,
-  onSubmit,
-  onCancel,
-  inputs,
-  groups,
-  validationSchema,
-  defaultValues
-}: HostGroupFormProps): JSX.Element => {
-  const { classes } = useFormStyles();
-  const { t } = useTranslation();
+const Actions =
+  ({ onCancel, mode }) =>
+  (): JSX.Element => {
+    const { t } = useTranslation();
 
-  const actionsLabels = {
-    cancel: t(labelCancel),
-    submit: {
-      create: t(labelCreate),
-      update: t(labelUpdate)
-    }
-  };
-
-  // to check later for quit the modal if the id does not exists
-  const { data, isLoading } = useGetDetails({
-    id
-  });
-
-  // const initialValues = data && equals(mode, 'edit') ? data : defaultValues;
-  const initialValues =
-    data && equals(mode, 'edit')
-      ? {
-          ...data,
-          hosts: [{ id: 1, name: 'Host_1' }],
-          resourceAccessRules: [
-            { id: 1, name: 'Rule_1' },
-            { id: 2, name: 'Rule_2' }
-          ]
-        } // for now
-      : defaultValues;
-
-  const Actions = (): JSX.Element => {
     const setIsDirty = useSetAtom(isFormDirtyAtom);
 
     const { dirty } = useFormikContext();
 
-    setIsDirty(dirty);
+    useEffect(() => {
+      setIsDirty(dirty);
+    }, [dirty]);
+
+    const actionsLabels = {
+      cancel: t(labelCancel),
+      submit: {
+        create: t(labelCreate),
+        update: t(labelUpdate)
+      }
+    };
+
+    const variant = equals(mode, 'add') ? 'create' : 'update';
 
     return (
       <>
         <FormActions
           labels={actionsLabels}
-          variant={equals(mode, 'add') ? 'create' : 'update'}
+          variant={variant}
           onCancel={onCancel}
         />
         <CloseModalConfirmation />
@@ -84,21 +63,31 @@ const HostGroupForm = ({
     );
   };
 
+const HostGroupForm = ({
+  mode,
+  onSubmit,
+  onCancel,
+  inputs,
+  groups,
+  validationSchema,
+  initialValues,
+  isLoading
+}: HostGroupFormProps): JSX.Element => {
+  const { classes } = useFormStyles();
+
   return (
-    <div className={classes.form}>
-      <Form
-        Buttons={Actions}
-        isCollapsible
-        areGroupsOpen
-        initialValues={initialValues}
-        inputs={inputs}
-        groups={groups}
-        isLoading={isLoading}
-        submit={(values, bag) => onSubmit?.(values, bag)}
-        validationSchema={validationSchema}
-        groupsClassName={classes.groups}
-      />
-    </div>
+    <Form
+      Buttons={Actions({ onCancel, mode })}
+      isCollapsible
+      areGroupsOpen
+      initialValues={initialValues}
+      inputs={inputs}
+      groups={groups}
+      isLoading={isLoading}
+      submit={(values, bag) => onSubmit?.(values, bag)}
+      validationSchema={validationSchema}
+      groupsClassName={classes.groups}
+    />
   );
 };
 
