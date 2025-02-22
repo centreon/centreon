3e9caf1af60b489eb105de326ae8dff483ff42c9
Tom Darneix <tomdar87@outlook.com>
2025-02-22 20:59:39 +0100

    fix

--- centreon/packages/ui/src/Graph/common/BaseChart/Header/useHeaderStyles.ts
+++ centreon/packages/ui/src/Graph/common/BaseChart/Header/useHeaderStyles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from 'tss-react/mui';
 export const ussHeaderChartStyles = makeStyles()({
   header: {
     display: 'grid',
-    gridTemplateColumns: '0.4fr 1fr 0.4fr',
+    gridTemplateColumns: 'auto 1fr auto',
     width: '100%'
   },
   title: {
