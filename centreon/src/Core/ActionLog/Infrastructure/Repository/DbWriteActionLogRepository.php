d7807ffb0428a678c23e4899c0c19a0960cc0405
Anthony Graule <agraule@centreon.com>
2025-02-24 08:44:28 +0100

    improve performance using batch insert in DbWriteActionLogRepository.php

--- centreon/src/Core/ActionLog/Infrastructure/Repository/DbWriteActionLogRepository.php
+++ centreon/src/Core/ActionLog/Infrastructure/Repository/DbWriteActionLogRepository.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 
 namespace Core\ActionLog\Infrastructure\Repository;
 
+use Adaptation\Database\Connection\Collection\BatchInsertParameters;
 use Adaptation\Database\Connection\Collection\QueryParameters;
 use Adaptation\Database\Connection\Exception\ConnectionException;
 use Adaptation\Database\Connection\ValueObject\QueryParameter;
@@ -131,30 +132,23 @@ class DbWriteActionLogRepository extends AbstractRepositoryRDB implements WriteA
 
         try {
             if (! $isTransactionActive) {
-            $this->db->startTransaction();
-        }
+                $this->db->startTransaction();
+            }
 
-            $request = $this->translateDbName(
-                <<<'SQL'
-                    INSERT INTO `:dbstg`.`log_action_modification` (
-                        `field_name`,
-                        `field_value`,
-                        `action_log_id`
-                    ) VALUES (
-                        :field_name,
-                        :field_value,
-                        :action_log_id
-                    )
-                    SQL
-            );
-
+            $batchQueryParameters = [];
             foreach ($details as $fieldName => $fieldValue) {
-                $this->db->insert($request, QueryParameters::create([
+                $batchQueryParameters[] = QueryParameters::create([
                     QueryParameter::string('field_name', $fieldName),
                     QueryParameter::string('field_value', (string) $fieldValue),
                     QueryParameter::int('action_log_id', (int) $actionLog->getId()),
-                ]));
+                ]);
             }
+
+            $this->db->batchInsert(
+                $this->translateDbName('`:dbstg`.`log_action_modification`'),
+                ['`field_name`', '`field_value`', '`action_log_id`'],
+                BatchInsertParameters::create($batchQueryParameters)
+            );
 
             if (! $isTransactionActive) {
                 $this->db->commitTransaction();
