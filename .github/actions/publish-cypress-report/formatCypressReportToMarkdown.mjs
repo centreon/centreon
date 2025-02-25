67fd3dbdfa4835105b4c009b68e35aae192bdbc8
Kevin Duret <kduret@centreon.com>
2025-02-25 10:40:26 +0100

    enh(ci): group flaky tests summary

--- .github/actions/publish-cypress-report/formatCypressReportToMarkdown.mjs
+++ .github/actions/publish-cypress-report/formatCypressReportToMarkdown.mjs
@@ -39,16 +39,8 @@ const getTestsBySuite = (suite) => {
     return suite.tests;
   }
 
-  return [...suite.tests.filter(({ fail, skipped }) => fail || skipped), ...suite.suites.map((subSuite) => getTestsBySuite(subSuite))];
+  return [...suite.tests.filter(({ fail }) => fail), ...suite.suites.map((subSuite) => getTestsBySuite(subSuite))];
 }
-
-const mapSeries = async ({ array, callback }) => {
-  const result = [];
-  for (const element of array) {
-    result.push(await callback.call(this, await element));
-  }
-  return result;
-};
 
 const testsDetails = report.results.map((result) => ({
   file: result.file,
@@ -56,55 +48,27 @@ const testsDetails = report.results.map((result) => ({
 }));
 
 const details =
-  await mapSeries({ array: testsDetails, callback: async ({ file, tests }) => {
-    let hasFailures = false;
-    for (const test of tests) {
-      if (test.fail) {
-        hasFailures = true;
-        break;
-      }
-    }
-
-    if (!hasFailures) {
-      return;
-    }
-
+  testsDetails.map(({ file, tests }) => {
     return `
-<details>
-  <summary><h2>${file}</h2></summary>
-    ${await mapSeries({ array: tests, callback: async ({ fullTitle, err, fail, duration }) => {
-      if (! fail) {
-        return;
-      }
-      const stackLines = err.estack ? err.estack.split('\n') : [];
-      const localizableFile = stackLines.find((line) => line.includes(file));
+      <details>
+        <summary><h2>${file}</h2></summary>
+        ${tests.map(({ fullTitle, err, fail, duration }) => {
+          if (! fail) {
+            return;
+          }
 
-      if (!localizableFile) {
-        const sanitizedEStack = err.estack ? `<pre>${err.estack}</pre>` : '';
-        return `<h3>${fail ? ':x:' : ':leftwards_arrow_with_hook:'} ${fullTitle}</h3>
-        Duration: ${duration / 1000} seconds
-        ${fail ? `Error stack: ${sanitizedEStack}` : '<br />'}
-        <br />`;
-      }
-      const [,,lineNumber] = localizableFile.split(':');
-      const errorMessage = err.message || '';
-      const response = await fetch(`https://raw.githubusercontent.com/${repo}/${branch}/${urlFilePrefix}/${file}`);
-      const upstreamFile = await response.text();
-      const locatedLine = upstreamFile.split('\n')[lineNumber - 1];
-      const error = `Located at: <a target="_blank" href="https://github.com/${repo}/tree/${branch}/${urlFilePrefix}/${file}#L${lineNumber}">${file}:${lineNumber}</a>`;
-      return `<h3>${fullTitle}</h3>
-        Duration: ${duration / 1000} seconds
-        <div>${error}</div>
-        <div>The following line fails the test: <code>${locatedLine}</code></div>
-        <details>
-          <summary>Complete logs</summary>
-          <pre>${errorMessage.split('\n')[0]}</pre>
-        </details>
-        <br />`;
-    }}).then((v) => v.join(''))}
-</details>`;
-  }});
+          const sanitizedEStack = err.estack ? `<pre>${err.estack}</pre>` : '';
+          return `<h3>:x: ${fullTitle}</h3>
+            Duration: ${duration / 1000} seconds
+            Error: ${err.message || ''}
+            Error stack: ${sanitizedEStack}
+            <br />`;
+        }).join('')}
+      </details>`;
+  });
 
 const newReportContent = `${summary}${details}`;
+
+console.log(newReportContent);
 
 fs.writeFileSync('cypress-report.md', newReportContent);
