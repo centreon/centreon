ef6917ca79fa40aab09a622bef4324cec626a977
pkippes <144150042+pkippes@users.noreply.github.com>
2025-02-26 16:36:19 +0100

    chore(release) bump web 23.04.28 open tickets 23.04.6 (#6791)

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '23.04.27');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '23.04.28');
 
 --
 -- Contenu de la table `contact`
