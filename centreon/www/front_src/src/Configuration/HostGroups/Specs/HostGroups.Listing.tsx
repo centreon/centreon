6484845c28c40813031955a19e2185b8e7219831
Med1233 <melmeziani-ext@centreon.com>
2025-03-01 17:40:11 +0000

    fix

--- centreon/www/front_src/src/Configuration/HostGroups/Specs/HostGroups.Listing.tsx
+++ centreon/www/front_src/src/Configuration/HostGroups/Specs/HostGroups.Listing.tsx
@@ -70,7 +70,7 @@ export default () => {
         cy.waitForRequest('@getAllHostGroups');
 
         cy.findAllByText('0')
-          .eq(isEnabledHost ? 7 : 6)
+          .eq(isEnabledHost ? 7 : 8)
           .trigger('mouseover');
 
         cy.contains(
