2fa659fb61ab371b68aa53d6740bbfcc7aea2f0a
Miloud Lafttah <mlafttah@ext.centreon.com>
2025-03-04 13:28:13 +0000

    [dev-24.04.x] - [MAP]when too many elements are selected in a Geoview the list is out of visibility (#6836)

--- centreon/packages/ui/src/InputField/Select/Autocomplete/Connected/Multi/index.test.tsx
+++ centreon/packages/ui/src/InputField/Select/Autocomplete/Connected/Multi/index.test.tsx
@@ -13,11 +13,12 @@ import { baseEndpoint, getEndpoint, label, optionsData } from './utils';
 
 import MultiConnectedAutocompleteField from '.';
 
-const renderMultiAutocompleteField = (): RenderResult =>
+const renderMultiAutocompleteField = (customRenderTags?): RenderResult =>
   render(
     <TestQueryProvider>
       <MultiConnectedAutocompleteField
         baseEndpoint=""
+        customRenderTags={customRenderTags}
         field="host.name"
         getEndpoint={getEndpoint}
         label={label}
@@ -40,6 +41,25 @@ describe(MultiConnectedAutocompleteField, () => {
 
     await waitFor(() => {
       expect(getFetchCall(0)).toEqual(`${baseEndpoint}?page=1`);
+    });
+  });
+
+  it('display tags when customTagsRender is defined', async () => {
+    const customRender = (tags: React.ReactNode): React.ReactNode => (
+      <div data-testid="custom-tags-wrapper">{tags}</div>
+    );
+
+    const { getByLabelText, getByTestId } =
+      renderMultiAutocompleteField(customRender);
+
+    fireEvent.click(getByLabelText('Open'));
+
+    await waitFor(() => {
+      const tagChip = getByTestId(
+        `tag-option-chip-${optionsData.result[0].id}`
+      );
+      expect(tagChip).toBeVisible();
+      expect(tagChip).toHaveTextContent(optionsData.result[0].name);
     });
   });
 });
