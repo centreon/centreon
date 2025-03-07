b888fd69faa0e1654148ca183ec5287f4551ddb3
selfakiri <selfakiri@ext.centreon.com>
2025-03-07 08:52:00 +0000

    fix(Host Groups): restore icon field for duplicate (#6872)

--- centreon/src/Core/HostGroup/Infrastructure/Repository/DbWriteHostGroupRepository.php
+++ centreon/src/Core/HostGroup/Infrastructure/Repository/DbWriteHostGroupRepository.php
@@ -219,6 +219,7 @@ class DbWriteHostGroupRepository extends AbstractRepositoryDRB implements WriteH
                 hg_alias,
                 geo_coords,
                 hg_comment,
+                hg_icon_image,
                 hg_activate
             )
             SELECT
@@ -226,6 +227,7 @@ class DbWriteHostGroupRepository extends AbstractRepositoryDRB implements WriteH
                 hg_alias,
                 geo_coords,
                 hg_comment,
+                hg_icon_image,
                 hg_activate
             FROM `:db`.`hostgroup`
             WHERE hg_id = :hostgroup_id
