a10e59b9929f5fc8bc6b785d2da3a34f40da7734
s-elhassani <selhassani-ext@centreon.com>
2025-03-11 15:01:20 +0100

    some fixes

--- centreon/tests/e2e/features/Custom-views/common.ts
+++ centreon/tests/e2e/features/Custom-views/common.ts
@@ -1,28 +1,29 @@
 const addPublicCustomView = (): void => {
-  // Visit the page 'Home > Custom Views'
-  cy.visit('/centreon/main.php?p=103')
-  cy.wait('@getTimeZone');
-  // Wait until the "Show/Hide edit mode" icon is visible
-  cy.waitForElementInIframe('#main-content', 'a[title="Show/Hide edit mode"]');
-  cy.getIframeBody().find('a[title="Show/Hide edit mode"]').click();
-  // Wait until the button 'Add view' is visible 
-  cy.waitForElementInIframe(
-    '#main-content',
-    'button:contains("Add view")'
-  );
-  cy.getIframeBody().contains("button", "Add view").click();
-  // Type a value in the 'Name' field
-  cy.getIframeBody().find('input[name="name"]').eq(0).type('public-view');
-  // Check the 'Public' checkbox
-  cy.getIframeBody().find('input[name="public"]').eq(0).click({force: true});
-  // Click on the 'Submit' button
-  cy.getIframeBody().find('input[name="submit"]').eq(0).click();
-  cy.wait('@getViews');
-  cy.exportConfig();
+    // Visit the page 'Home > Custom Views'
+    cy.visit('/centreon/main.php?p=103')
+    cy.wait('@getTimeZone');
+    // Wait until the "Show/Hide edit mode" icon is visible
+    cy.waitForElementInIframe('#main-content', 'a[title="Show/Hide edit mode"]');
+    cy.getIframeBody().find('a[title="Show/Hide edit mode"]').click();
+    // Wait until the button 'Add view' is visible 
+    cy.waitForElementInIframe(
+        '#main-content',
+        'button:contains("Add view")'
+    );
+    cy.getIframeBody().contains("button", "Add view").click({force: true});
+    cy.wait('@action');
+    // Type a value in the 'Name' field
+    cy.getIframeBody().find('input[name="name"]').eq(0).type('public-view');
+    // Check the 'Public' checkbox
+    cy.getIframeBody().find('input[name="public"]').eq(0).click({force: true});
+    // Click on the 'Submit' button
+    cy.getIframeBody().find('input[name="submit"]').eq(0).click();
+    cy.wait('@getViews');
+    cy.exportConfig();
 };
   
-  const addPublicSharedView = () => {
-    cy.getIframeBody().contains("button", "Add view").click();
+const addPublicSharedView = () => {
+    cy.getIframeBody().contains("button", "Add view").click({force: true});
     // Check the 'Load from existing view' radio button
     cy.getIframeBody().find('input[name="create_load[create_load]"]').eq(1).click({force: true});
     // Click on the 'Views' drop down list
@@ -32,7 +33,7 @@ const addPublicCustomView = (): void => {
     // Click on the 'Submit' button
     cy.getIframeBody().find('input[name="submit"]').eq(0).click();
     cy.wait('@getViews');
-  };
+};
   
   export { addPublicCustomView, addPublicSharedView };
   
