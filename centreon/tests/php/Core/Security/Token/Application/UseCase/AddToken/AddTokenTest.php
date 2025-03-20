05d05890622ee11504ce792b89ade31e9a5e584c
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-03-20 09:16:53 +0100

    enh(token): handle new token type CMA and expiration date nullable (#6907)
    Co-authored-by: s-elhassani <selhassani-ext@centreon.com>

--- centreon/tests/php/Core/Security/Token/Application/UseCase/AddToken/AddTokenTest.php
+++ centreon/tests/php/Core/Security/Token/Application/UseCase/AddToken/AddTokenTest.php
@@ -21,17 +21,14 @@
 
 declare(strict_types=1);
 
-namespace Tests\Core\Security\Token\Application\UseCase\AddHostTemplate;
+namespace Tests\Core\Security\Token\Application\UseCase\AddToken;
 
 use Centreon\Domain\Common\Assertion\AssertionException;
 use Centreon\Domain\Contact\Interfaces\ContactInterface;
 use Core\Application\Common\UseCase\ConflictResponse;
 use Core\Application\Common\UseCase\ErrorResponse;
-use Core\Application\Common\UseCase\ForbiddenResponse;
 use Core\Application\Common\UseCase\InvalidArgumentResponse;
 use Core\Common\Domain\TrimmedString;
-use Core\Contact\Application\Repository\ReadContactRepositoryInterface;
-use Core\Infrastructure\Common\Presenter\PresenterFormatterInterface;
 use Core\Security\Authentication\Application\Provider\ProviderAuthenticationFactoryInterface;
 use Core\Security\Authentication\Application\Provider\ProviderAuthenticationInterface;
 use Core\Security\ProviderConfiguration\Domain\Model\Configuration;
@@ -43,13 +40,9 @@ use Core\Security\Token\Application\UseCase\AddToken\AddTokenRequest;
 use Core\Security\Token\Application\UseCase\AddToken\AddTokenResponse;
 use Core\Security\Token\Application\UseCase\AddToken\AddTokenValidation;
 use Core\Security\Token\Domain\Model\Token;
-use Tests\Core\Security\Token\Infrastructure\API\AddToken\AddTokenPresenterStub;
+use Core\Security\Token\Domain\Model\TokenTypeEnum;
 
 beforeEach(function (): void {
-    $this->presenter = new AddTokenPresenterStub(
-        $this->presenterFormatter = $this->createMock(PresenterFormatterInterface::class)
-    );
-
     $this->useCase = new AddToken(
         $this->writeTokenRepository = $this->createMock(WriteTokenRepositoryInterface::class),
         $this->readTokenRepository = $this->createMock(ReadTokenRepositoryInterface::class),
@@ -67,10 +60,12 @@ beforeEach(function (): void {
     $this->linkedUser = ['id' => 23, 'name' => 'Jane Doe'];
     $this->creator = ['id' => 12, 'name' => 'John Doe'];
 
-    $this->request = new AddTokenRequest();
-    $this->request->name = '  token name  ';
-    $this->request->userId = $this->linkedUser['id'];
-    $this->request->expirationDate = $this->expirationDate;
+    $this->request = new AddTokenRequest(
+        name: '  token name  ',
+        type: TokenTypeEnum::CMA,
+        userId: $this->linkedUser['id'],
+        expirationDate: $this->expirationDate
+    );
 
     $this->token = new Token(
         name: new TrimmedString($this->request->name),
@@ -80,69 +75,26 @@ beforeEach(function (): void {
         creatorName: new TrimmedString($this->creator['name']),
         creationDate: $this->creationDate,
         expirationDate: $this->expirationDate,
-        isRevoked: false
+        isRevoked: false,
+        type: TokenTypeEnum::CMA
     );
 });
 
 it('should present an ErrorResponse when a generic exception is thrown', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
-
     $this->providerFactory
         ->expects($this->once())
         ->method('create')
-        ->willReturn($this->localProvider);
-    $this->localProvider
-        ->expects($this->once())
-        ->method('getConfiguration')
-        ->willReturn($this->configurationProvider);
-    $this->configurationProvider
-        ->expects($this->once())
-        ->method('getId')
-        ->willReturn(1);
-    $this->user
-        ->expects($this->once())
-        ->method('getId')
-        ->willReturn($this->creator['id']);
-    $this->user
-        ->expects($this->once())
-        ->method('getName')
-        ->willReturn($this->creator['name']);
-
-    $this->writeTokenRepository
-        ->expects($this->once())
-        ->method('add')
         ->willThrowException(new \Exception());
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(ErrorResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(TokenException::addToken()->getMessage());
 });
 
-it('should present a ForbiddenResponse when a user has insufficient rights', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(false);
-
-    ($this->useCase)($this->request, $this->presenter);
-
-    expect($this->presenter->response)
-        ->toBeInstanceOf(ForbiddenResponse::class)
-        ->and($this->presenter->response->getMessage())
-        ->toBe(TokenException::addNotAllowed()->getMessage());
-});
-
 it('should present a ConflictResponse when name is already used', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
     $this->validation
         ->expects($this->once())
         ->method('assertIsValidName')
@@ -150,21 +102,17 @@ it('should present a ConflictResponse when name is already used', function (): v
             TokenException::nameAlreadyExists(trim($this->request->name))
         );
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(ConflictResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(
             TokenException::nameAlreadyExists(trim($this->request->name))->getMessage()
         );
 });
 
 it('should present a ConflictResponse when user ID is not valid', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
     $this->validation
         ->expects($this->once())
         ->method('assertIsValidUser')
@@ -172,19 +120,15 @@ it('should present a ConflictResponse when user ID is not valid', function (): v
             TokenException::invalidUserId($this->request->userId)
         );
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(ConflictResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(TokenException::invalidUserId($this->request->userId)->getMessage());
 });
 
 it('should present a ConflictResponse when a creator cannot manage user\'s tokens', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
     $this->validation
         ->expects($this->once())
         ->method('assertIsValidUser')
@@ -192,20 +136,15 @@ it('should present a ConflictResponse when a creator cannot manage user\'s token
             TokenException::notAllowedToCreateTokenForUser($this->request->userId)
         );
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(ConflictResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(TokenException::notAllowedToCreateTokenForUser($this->request->userId)->getMessage());
 });
 
 it('should present an InvalidArgumentResponse when a field assert failed', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
-
     $this->providerFactory
         ->expects($this->once())
         ->method('create')
@@ -227,20 +166,15 @@ it('should present an InvalidArgumentResponse when a field assert failed', funct
         ->method('getName')
         ->willReturn('');
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(InvalidArgumentResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(AssertionException::notEmptyString('NewToken::creatorName')->getMessage());
 });
 
 it('should present an ErrorResponse if the newly created token cannot be retrieved', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
-
     $this->providerFactory
         ->expects($this->once())
         ->method('create')
@@ -271,20 +205,15 @@ it('should present an ErrorResponse if the newly created token cannot be retriev
         ->method('find')
         ->willReturn(null);
 
-    ($this->useCase)($this->request, $this->presenter);
+    $response = ($this->useCase)($this->request);
 
-    expect($this->presenter->response)
+    expect($response)
         ->toBeInstanceOf(ErrorResponse::class)
-        ->and($this->presenter->response->getMessage())
+        ->and($response->getMessage())
         ->toBe(TokenException::errorWhileRetrievingObject()->getMessage());
 });
 
 it('should return created object on success', function (): void {
-    $this->user
-        ->expects($this->once())
-        ->method('hasTopologyRole')
-        ->willReturn(true);
-
     $this->validation->expects($this->once())->method('assertIsValidName');
     $this->validation->expects($this->once())->method('assertIsValidUser');
 
@@ -318,25 +247,25 @@ it('should return created object on success', function (): void {
         ->method('find')
         ->willReturn($this->token);
 
-    ($this->useCase)($this->request, $this->presenter);
-
-    $response = $this->presenter->response;
+    $response = ($this->useCase)($this->request);
 
     expect($response)->toBeInstanceOf(AddTokenResponse::class)
-        ->and($response->name)
+        ->and($response->apiToken->getName())
         ->toBe($this->token->getName())
-        ->and($response->userName)
+        ->and($response->apiToken->getUserName())
         ->toBe($this->linkedUser['name'])
-        ->and($response->userId)
+        ->and($response->apiToken->getUserId())
         ->toBe($this->linkedUser['id'])
-        ->and($response->creatorName)
+        ->and($response->apiToken->getCreatorName())
         ->toBe($this->creator['name'])
-        ->and($response->creatorId)
+        ->and($response->apiToken->getCreatorId())
         ->toBe($this->creator['id'])
-        ->and($response->creationDate)
+        ->and($response->apiToken->getCreationDate())
         ->toBe($this->creationDate)
-        ->and($response->expirationDate)
+        ->and($response->apiToken->getExpirationDate())
         ->toBe($this->expirationDate)
-        ->and($response->isRevoked)
-        ->toBe($this->token->isRevoked());
+        ->and($response->apiToken->isRevoked())
+        ->toBe($this->token->isRevoked())
+        ->and($response->apiToken->getType())
+        ->toBe($this->token->getType());
 });
--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -70,10 +70,22 @@ $updateTopologyForHostGroup = function (CentreonDB $pearDB) use (&$errorMessage)
 
 
 
+// -------------------------------------------- Token -------------------------------------------- //
+
+$addCMATypeToTokenTable = function () use ($pearDB, &$errorMessage) {
+    $errorMessage = 'Failed to add column cma type to table security_authentication_tokens';
+    $queryAddApiType = "ALTER TABLE security_authentication_tokens MODIFY COLUMN token_type enum('auto','manual','cma','api') DEFAULT 'auto'";
+    $pearDB->executeQuery($queryAddApiType);
+    $queryUpdateType = "UPDATE security_authentication_tokens SET token_type = 'api' WHERE token_type = 'manual'";
+    $pearDB->executeQuery($queryUpdateType);
+    $queryRemoveManualType = "ALTER TABLE security_authentication_tokens MODIFY COLUMN token_type enum('auto','api','cma') DEFAULT 'auto'";
+    $pearDB->executeQuery($queryRemoveManualType);
+};
+
 try {
     // TODO add your function calls to update the real time database structure here
 
-    // TODO add your function calls to update the configuration database structure here
+    $addCMATypeToTokenTable($pearDB);
 
     // Transactional queries for configuration database
     if (! $pearDB->inTransaction()) {
