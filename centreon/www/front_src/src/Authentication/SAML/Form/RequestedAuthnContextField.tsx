a6c079a678c311c1d4f59de388bd505869b83249
dmyios <diosypenko@centreon.com>
2025-03-27 17:25:16 +0100

    implemented additional field for requested authentication context

---
+++ centreon/www/front_src/src/Authentication/SAML/Form/RequestedAuthnContextField.tsx
@@ -0,0 +1,43 @@
+import type React from 'react';
+import { useFormikContext } from 'formik';
+
+import { SelectEntry, SelectField } from '@centreon/ui';
+
+import { labelRequestedAuthnContext } from '../translatedLabels';
+import { RequestedAuthnContextValue, SAMLConfiguration } from '../models';
+
+const RequestedAuthnContextField = (): React.JSX.Element => {
+  const { values, setFieldValue, errors, touched } =
+    useFormikContext<SAMLConfiguration>();
+
+  const changeValue = (event): void => {
+    setFieldValue('requestedAuthnContext', event.target.value);
+  };
+
+  const options: SelectEntry[] = [
+    { id: 1, name: RequestedAuthnContextValue.Minimum },
+    { id: 2, name: RequestedAuthnContextValue.Exact },
+    { id: 3, name: RequestedAuthnContextValue.Better },
+    { id: 4, name: RequestedAuthnContextValue.Maximum },
+  ];
+
+  const error = touched?.requestedAuthnContext
+    ? errors?.requestedAuthnContext
+    : undefined
+
+  return (
+    <SelectField
+      fullWidth
+      required
+      dataTestId={labelRequestedAuthnContext}
+      error={error as string}
+      label={labelRequestedAuthnContext}
+      name="requestedAuthnContext"
+      options={options}
+      selectedOptionId={values.requestedAuthnContext}
+      onChange={changeValue}
+    />
+  );
+}
+
+export default RequestedAuthnContextField;
