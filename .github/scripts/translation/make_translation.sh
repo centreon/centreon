4b82f11c66f050a97a8dff34ef2b3728dd5583a5
Kevin Duret <kduret@centreon.com>
2025-04-23 16:05:30 +0200

    fix

--- .github/scripts/translation/make_translation.sh
+++ .github/scripts/translation/make_translation.sh
@@ -133,6 +133,8 @@ if [ "$PROJECT" = "centreon" ]; then
     sed -i -r 's/:[0-9]+$//g' $BASE_DIR_PROJECT/lang/messages.pot
     COUNT_ADDED_LINES=$(git diff --numstat | grep "$BASE_DIR_PROJECT/lang/messages.pot" | awk '{print $1;}')
     COUNT_REMOVED_LINES=$(git diff --numstat | grep "$BASE_DIR_PROJECT/lang/messages.pot" | awk '{print $2;}')
+    git diff --numstat | grep "lang/messages.pot"
+    echo -n "::warning::Added lines: $COUNT_ADDED_LINES, Removed lines: $COUNT_REMOVED_LINES"
     if [[ "$COUNT_ADDED_LINES" == "1" && $COUNT_REMOVED_LINES == "1" ]]; then
         git checkout $BASE_DIR_PROJECT/lang/messages.pot
     fi
@@ -165,8 +167,8 @@ if [ "$PROJECT" = "centreon" ]; then
     sed -i -r 's/:[0-9]+$//g' $BASE_DIR_PROJECT/lang/help.pot
     COUNT_ADDED_LINES=$(git diff --numstat | grep "$BASE_DIR_PROJECT/lang/help.pot" | awk '{print $1;}')
     COUNT_REMOVED_LINES=$(git diff --numstat | grep "$BASE_DIR_PROJECT/lang/help.pot" | awk '{print $2;}')
-    git diff --numstat | grep "$BASE_DIR_PROJECT/lang/help.pot"
-    echo -e "::warning::Added lines: $COUNT_ADDED_LINES, Removed lines: $COUNT_REMOVED_LINES"
+    git diff --numstat | grep "lang/help.pot"
+    echo -n "::warning::Added lines: $COUNT_ADDED_LINES, Removed lines: $COUNT_REMOVED_LINES"
     if [[ "$COUNT_ADDED_LINES" == "1" && $COUNT_REMOVED_LINES == "1" ]]; then
         git checkout $BASE_DIR_PROJECT/lang/help.pot
     fi
