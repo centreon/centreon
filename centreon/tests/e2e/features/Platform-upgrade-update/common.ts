2d75152dcd9afaa64c99ced10b40705a8a0c4e01
MedWassCh <wchaabane@centreon.com>
2025-05-02 10:56:02 +0200

    fix(e2e): update package distribution name for version 24.0 in common.ts

--- centreon/tests/e2e/features/Platform-upgrade-update/common.ts
+++ centreon/tests/e2e/features/Platform-upgrade-update/common.ts
@@ -175,7 +175,7 @@ const installCentreon = (version: string): Cypress.Chainable => {
       packageDistribName = Cypress.env('WEB_IMAGE_OS');
     } else if (Number(versionMatches[1]) === 24 && Number(versionMatches[2]) < 10) {
       packageDistribPrefix = '-1+';
-      packageDistribName = 'deb12u1'
+      packageDistribName = 'deb12u1_all'
     } else if (Cypress.env('WEB_IMAGE_OS') === 'bookworm') {
       packageDistribPrefix = '-*+';
       packageDistribName = 'deb12u1';
