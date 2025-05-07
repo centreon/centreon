c264f2b54ed15e7fcd2ffba55c95e478a20bdb34
tuntoja <tuntoja@centreon.com>
2025-05-07 17:19:12 +0200

    force change on web

--- centreon/www/install/php/Update-22.10.30.php
+++ centreon/www/install/php/Update-22.10.30.php
@@ -14,6 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * For more information : contact@centreon.com
+ * For more information : contact@centreon.com .
  *
  */
