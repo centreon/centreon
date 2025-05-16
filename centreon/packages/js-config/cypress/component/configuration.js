8ec5818a468151519c3951c096c1be35455d95d9
Nouha-ElAbrouki <97687698+Noha-ElAbrouki@users.noreply.github.com>
2025-05-16 12:35:10 +0200

    resolve conflict for configuration.js

--- centreon/packages/js-config/cypress/component/configuration.js
+++ centreon/packages/js-config/cypress/component/configuration.js
@@ -1,79 +1,76 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { devServer } = require("cypress-rspack-dev-server");
-const { defineConfig } = require("cypress");
+const { devServer } = require('cypress-rspack-dev-server');
+const { defineConfig } = require('cypress');
 const {
-  addMatchImageSnapshotPlugin,
-} = require("@simonsmith/cypress-image-snapshot/plugin");
-const cypressCodeCoverageTask = require("@cypress/code-coverage/task");
+  addMatchImageSnapshotPlugin
+} = require('@simonsmith/cypress-image-snapshot/plugin');
+const cypressCodeCoverageTask = require('@cypress/code-coverage/task');
 
 module.exports = ({
   rspackConfig,
   cypressFolder,
   specPattern,
   env,
-  excludeSpecPattern,
+  excludeSpecPattern
 }) => {
-  const mainCypressFolder = cypressFolder || "cypress";
+  const mainCypressFolder = cypressFolder || 'cypress';
 
   return defineConfig({
     component: {
       devServer: (devServerConfig) =>
         devServer({
           ...devServerConfig,
-          framework: "react",
-          rspackConfig,
+          framework: 'react',
+          rspackConfig
         }),
       excludeSpecPattern,
       setupNodeEvents: (on, config) => {
         addMatchImageSnapshotPlugin(on, config);
 
         cypressCodeCoverageTask(on, config);
-        on("task", {
+        on('task', {
           coverageReport: () => {
             return null;
-          },
+          }
         });
 
-        on("before:browser:launch", (browser, launchOptions) => {
-          if (
-            ["chrome", "chromium"].includes(browser.name) &&
-            browser.isHeadless
-          ) {
-            launchOptions.args.push("--headless=new");
-            launchOptions.args.push("--force-color-profile=srgb");
-            launchOptions.args.push("--window-size=1400,1200");
+        on('before:browser:launch', (browser, launchOptions) => {
+          if (browser.name === 'chrome' && browser.isHeadless) {
+            launchOptions.args.push('--headless=new');
+            launchOptions.args.push('--force-color-profile=srgb');
+            launchOptions.args.push('--window-size=1400,1200');
           }
 
           return launchOptions;
         });
       },
       specPattern,
-      supportFile: `${mainCypressFolder}/support/component.tsx`,
+      supportFile: `${mainCypressFolder}/support/component.tsx`
     },
     env: {
-      baseUrl: "http://localhost:9092",
+      baseUrl: 'http://localhost:9092',
       codeCoverage: {
         exclude: [
-          "cypress/**/*.*",
-          "packages/**",
-          "node_modules",
-          "**/*.js",
-          "**/*.spec.tsx",
-        ],
+          'cypress/**/*.*',
+          'packages/**',
+          'node_modules',
+          '**/*.js',
+          '**/*.spec.tsx'
+        ]
       },
-      ...env,
+      ...env
     },
-    reporter: "mochawesome",
+    reporter: 'mochawesome',
     reporterOptions: {
       html: false,
       json: true,
       overwrite: true,
       reportDir: `${mainCypressFolder}/results`,
-      reportFilename: "[name]-report.json",
+      reportFilename: '[name]-report.json'
     },
     video: true,
     videosFolder: `${mainCypressFolder}/results/videos`,
     viewportHeight: 590,
-    viewportWidth: 1280,
+    viewportWidth: 1280
   });
 };
