ea3df4be7fbcc12a6fe2003fc551a86b2d76ff01
MedWassCh <wchaabane@centreon.com>
2025-05-19 16:14:36 +0200

    Replace resource cleanup script with a new data purging script and update references

--- centreon/tests/performanceTesting/cleaningUpData.mjs
+++ centreon/tests/performanceTesting/cleaningUpData.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // List of scripts to execute
-const scripts = ["cleaningUpResources.mjs", "cleaningUpUsers.mjs"];
+const scripts = ["deleteAllAddedRessources.mjs", "cleaningUpUsers.mjs"];
 
 // Function to execute a script using Node.js
 function runScript(script) {
