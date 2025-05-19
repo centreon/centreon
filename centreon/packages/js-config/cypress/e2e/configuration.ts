ac9da1ebf6ec7329f5ea10f71c18e0f4b13725fd
hamzabessa <hbessa@ext.centreon.com>
2025-05-19 17:06:43 +0100

    activate videos and remove dockerize for now

--- centreon/packages/js-config/cypress/e2e/configuration.ts
+++ centreon/packages/js-config/cypress/e2e/configuration.ts
@@ -80,7 +80,7 @@ export default ({
       runMode: 2
     },
     screenshotsFolder: `${resultsFolder}/screenshots`,
-    video: isDevelopment,
+    video: true,
     videoCompression: 0,
     videosFolder: `${resultsFolder}/videos`,
     viewportHeight: 1080,
