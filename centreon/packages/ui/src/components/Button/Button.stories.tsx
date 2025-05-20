925d9bbb7df1eb225bcc439e5c0c92276b798631
Tom Darneix <tomdar87@outlook.com>
2025-05-20 17:20:09 +0200

    refactor: move Button to css modules

--- centreon/packages/ui/src/components/Button/Button.stories.tsx
+++ centreon/packages/ui/src/components/Button/Button.stories.tsx
@@ -1,8 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 
-import { Add as AddIcon } from '@mui/icons-material';
+import { Add, Add as AddIcon } from '@mui/icons-material';
 
 import { Button } from './Button';
+import '../../ThemeProvider/tailwindcss.css';
 
 const meta: Meta<typeof Button> = {
   component: Button
@@ -29,6 +30,30 @@ export const WithIcon: Story = {
 export const AsDanger: Story = {
   args: {
     ...Default.args,
+    isDanger: true
+  }
+};
+
+export const small: Story = {
+  args: {
+    ...Default.args,
+    size: 'small'
+  }
+};
+
+export const smallWithIcon: Story = {
+  args: {
+    ...Default.args,
+    size: 'small',
+    icon: <AddIcon />,
+    iconVariant: 'start'
+  }
+};
+
+export const smallDanger: Story = {
+  args: {
+    ...Default.args,
+    size: 'small',
     isDanger: true
   }
 };
