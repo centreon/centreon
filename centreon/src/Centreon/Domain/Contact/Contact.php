c0534ba6b870107608ea8951916b84829d7f28f4
Tom Darneix <tomdar87@outlook.com>
2025-05-21 18:56:51 +0200

    Apply suggestions from code review

--- centreon/src/Centreon/Domain/Contact/Contact.php
+++ centreon/src/Centreon/Domain/Contact/Contact.php
@@ -119,9 +119,9 @@ class Contact implements UserInterface, ContactInterface
     public const ROLE_CONFIGURATION_POLLERS_AGENT_CONFIGURATIONS_RW = 'ROLE_CONFIGURATION_POLLERS_AGENT_CONFIGURATIONS_RW';
 
     /**
-     * @var string|null
+     * @var string
      */
-    public const DEFAULT_LOCALE = null;
+    public const DEFAULT_LOCALE = 'en_US';
 
     /**
      * @var string
