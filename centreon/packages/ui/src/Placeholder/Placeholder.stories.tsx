232f21c848347adb9555db7a9b2c2dd6c06facf9
Tom Darneix <tomdar87@outlook.com>
2025-05-21 14:46:12 +0200

    fix: lint

--- centreon/packages/ui/src/Placeholder/Placeholder.stories.tsx
+++ centreon/packages/ui/src/Placeholder/Placeholder.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react/*';
+import { Meta } from '@storybook/react/*';
 import '../ThemeProvider/tailwindcss.css';
 import Placeholder from './Placeholder';
 
