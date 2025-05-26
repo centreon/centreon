32e2b406b67798bb3a29cb258f4028dcda58d25d
pkippes <144150042+pkippes@users.noreply.github.com>
2025-05-26 10:04:17 +0200

    chore(release) bump update-25.05.0.php

--- centreon/www/install/php/Update-25.05.0.php
+++ centreon/www/install/php/Update-25.05.0.php
@@ -27,7 +27,7 @@ require_once __DIR__ . '/../../../bootstrap.php';
  * This file contains changes to be included in the next version.
  * The actual version number should be added in the variable $version.
  */
-$version = 'xx.xx.x';
+$version = '25.05.0';
 $errorMessage = '';
 
 // -------------------------------------------- Host Group Configuration -------------------------------------------- //
