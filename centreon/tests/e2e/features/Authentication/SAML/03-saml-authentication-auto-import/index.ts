de268c4e556f3fa15c11c56af93e3c24300a25ee
MedWassCh <wchaabane@centreon.com>
2025-05-27 13:19:47 +0200

    refactor: replace logout with logoutViaAPI in authentication tests

--- centreon/tests/e2e/features/Authentication/SAML/03-saml-authentication-auto-import/index.ts
+++ centreon/tests/e2e/features/Authentication/SAML/03-saml-authentication-auto-import/index.ts
@@ -122,7 +122,7 @@ Then(
 
     cy.url().should('include', '/monitoring/resources');
 
-    cy.logout();
+    cy.logoutViaAPI();
     cy.getByLabel({ label: 'Alias', tag: 'input' }).should('exist');
 
     cy.loginByTypeOfUser({ jsonName: 'admin' })
