89ddd5b3fe2b927f8e56daec805c1c07ac709d37
asalhi-ext <asalhi@ext.centreon.com>
2025-05-28 10:20:46 +0100

    Update SqlMultipleBindTrait.php

--- centreon/src/Core/Common/Infrastructure/Repository/SqlMultipleBindTrait.php
+++ centreon/src/Core/Common/Infrastructure/Repository/SqlMultipleBindTrait.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ trait SqlMultipleBindTrait
     }
 
     /**
-     * Create multiple bind parameters compatible with QueryParameterTypeEnum
+     * Create multiple bind parameters compatible with QueryParameterTypeEnum.
      *
      * @param array<int|string, int|string|null> $values Scalar values to bind
      * @param string $prefix Placeholder prefix (no colon)
