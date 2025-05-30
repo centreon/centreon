20b899eaf730ec159a2163682ab85a01df144aba
jeremyjaouen <jjaouen@centreon.com>
2025-05-30 09:29:42 +0200

    indent

--- centreon/src/Core/Common/Infrastructure/RouteChecker.php
+++ centreon/src/Core/Common/Infrastructure/RouteChecker.php
@@ -24,38 +24,38 @@ use Symfony\Component\Process\Process;
 #[AsRoutingConditionService(alias: 'route_checker')]
 final class RouteChecker
 {
-	public function __construct(private ModuleInformationRepositoryInterface $moduleInformationRepository)
-	{
-	}
+    public function __construct(private ModuleInformationRepositoryInterface $moduleInformationRepository)
+    {
+    }
 
-	public function check(Request $request): bool
-	{
-		$path = $request->getPathInfo();
-		if (preg_match('#/api/(latest|v[\d\.]+)/([^/]+)/#', $path, $matches)) {
-		    $modulePath = $matches[2]; // contient "bam"
-		} else {
-			throw new \Exception("unable to retrieve module from URI");
-		}
-		// This Logic should be deported in each modules with a common Interface and services
-		if ($modulePath === "bam") {
-			$moduleName = "centreon-bam-server";
-		}	
-		$moduleInformation = $this->moduleInformationRepository->findByName($moduleName);
-		if ($moduleInformation === null) {
-			throw new \Exception("no module found");
-		}
-		$process = Process::fromShellCommandline("rpm -qa | grep " . escapeshellarg($moduleName));
-		$process->run();
-		$version = $process->getOutput();
-		if (preg_match('/\b(\d+\.\d+\.\d+)\b/', $version, $matches)) {
-			$version = $matches[1];
-		} else {
-    		echo "Version non trouvée.\n";
-		}
-		/**
-		 * If a new version is available we return false as we want the Checker to fail
-		 * If no new version are available so we return true so the route will be available
-		 */
-		return ! $moduleInformation->hasANewVersionAvailable($version);
-	}
+    public function check(Request $request): bool
+    {
+        $path = $request->getPathInfo();
+        if (preg_match('#/api/(latest|v[\d\.]+)/([^/]+)/#', $path, $matches)) {
+            $modulePath = $matches[2]; // contient "bam"
+        } else {
+            throw new \Exception("unable to retrieve module from URI");
+        }
+        // This Logic should be deported in each modules with a common Interface and services
+        if ($modulePath === "bam") {
+            $moduleName = "centreon-bam-server";
+        }
+        $moduleInformation = $this->moduleInformationRepository->findByName($moduleName);
+        if ($moduleInformation === null) {
+            throw new \Exception("no module found");
+        }
+        $process = Process::fromShellCommandline("rpm -qa | grep " . escapeshellarg($moduleName));
+        $process->run();
+        $version = $process->getOutput();
+        if (preg_match('/\b(\d+\.\d+\.\d+)\b/', $version, $matches)) {
+            $version = $matches[1];
+        } else {
+            echo "Version non trouvée.\n";
+        }
+        /**
+         * If a new version is available we return false as we want the Checker to fail
+         * If no new version are available so we return true so the route will be available
+         */
+        return ! $moduleInformation->hasANewVersionAvailable($version);
+    }
 }
