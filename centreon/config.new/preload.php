602237f3026cc15ee8275af5ab6a5596cd4f5526
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-06-05 17:09:26 +0200

    chore(api): add kernel strangler

--- centreon/config.new/preload.php
+++ centreon/config.new/preload.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,11 +18,8 @@ declare(strict_types=1);
  * limitations under the License.
  *
  * For more information : contact@centreon.com
- *
  */
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new \App\Shared\Infrastructure\Symfony\Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+if (file_exists(dirname(__DIR__).'/var/cache/prod/App_Shared_Infrastructure_Symfony_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/App_Shared_Infrastructure_Symfony_KernelProdContainer.preload.php';
+}
