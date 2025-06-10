9b5050b5ed3c9c03bcc87076cb4bf7cd0491dcbf
Tom Darneix <tomdar87@outlook.com>
2025-06-10 16:27:48 +0200

    fix: remove store

--- centreon/www/widgets/src/centreon-widget-batree/src/index.tsx
+++ centreon/www/widgets/src/centreon-widget-batree/src/index.tsx
@@ -1,5 +1,3 @@
-import { Module } from '@centreon/ui';
-
 // eslint-disable-next-line import/no-relative-packages
 import FederatedComponent from '../../../../front_src/src/components/FederatedComponents';
 import NoResources from '../../NoResources';
@@ -7,20 +5,13 @@ import { areResourcesFullfilled } from '../../utils';
 
 import { WidgetProps } from './models';
 
-const Widget = ({ panelData, store, ...rest }: WidgetProps): JSX.Element => {
+const Widget = ({ panelData, ...rest }: WidgetProps): JSX.Element => {
   if (!areResourcesFullfilled(panelData.resources)) {
     return <NoResources />;
   }
 
   return (
-    <Module seedName="centreon-widget-batree" store={store}>
-      <FederatedComponent
-        panelData={panelData}
-        path="/bam/widget"
-        store={store}
-        {...rest}
-      />
-    </Module>
+    <FederatedComponent panelData={panelData} path="/bam/widget" {...rest} />
   );
 };
 
