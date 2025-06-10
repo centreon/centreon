3ef6a0241f550e6e591dbf27f193f1449ad4c77d
Tom Darneix <tomdar87@outlook.com>
2025-06-10 16:58:58 +0200

    feat: add 4 columns on resources table

--- centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/Columns/useColumns.tsx
+++ centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/Columns/useColumns.tsx
@@ -25,12 +25,16 @@ import type { Column } from '@centreon/ui';
 import { DisplayType } from '../models';
 import {
   labelAction,
+  labelAlias,
   labelDuration,
+  labelFqdn,
   labelHost,
   labelInformation,
   labelLastCheck,
+  labelMonitoringServer,
   labelOpenedOn,
   labelParent,
+  labelParentAlias,
   labelResource,
   labelService,
   labelServices,
@@ -258,6 +262,41 @@ const useColumns = ({
       rowMemoProps: ['is_in_downtime', 'is_acknowledged', 'name', 'links'],
       sortable: false,
       type: ColumnType.component
+    },
+    {
+      getFormattedString: ({ alias }): string => alias,
+      id: 'alias',
+      label: t(labelAlias),
+      sortable: true,
+      type: ColumnType.string,
+      width: 'max-content'
+    },
+    {
+      getFormattedString: ({ parent }): string => parent?.alias,
+      id: 'parent_alias',
+      label: t(labelParentAlias),
+      rowMemoProps: ['parent'],
+      sortField: 'parent_alias',
+      sortable: true,
+      type: ColumnType.string,
+      width: 'max-content'
+    },
+    {
+      getFormattedString: ({ fqdn }): string => fqdn,
+      id: 'fqdn',
+      label: t(labelFqdn),
+      sortable: true,
+      type: ColumnType.string,
+      width: 'max-content'
+    },
+    {
+      getFormattedString: ({ monitoring_server_name }): string =>
+        monitoring_server_name,
+      id: 'monitoring_server_name',
+      label: t(labelMonitoringServer),
+      sortable: true,
+      type: ColumnType.string,
+      width: 'max-content'
     },
     ...(areTicketColumnsVisible && !isOnPublicPage
       ? [
