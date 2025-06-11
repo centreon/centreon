173f6b1323838424edcd8851f8f2c7638a854528
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-06-11 10:47:51 +0200

    chore: add castor and php-cs-fixer for the new code

---
+++ centreon/.php-cs-fixer.new.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
+
+declare(strict_types=1);
+
+use PhpCsFixer\{Config, Finder};
+
+$year = date('Y');
+$header = <<<"EOF"
+    Copyright 2005 - ${year} Centreon (https://www.centreon.com/)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    For more information : contact@centreon.com
+    EOF;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__.'/src/App',
+        __DIR__.'/config.new',
+    ]);
+
+return (new Config())
+    // @see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/7777
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'declare_strict_types' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'match', 'parameters']],
+        'header_comment' => ['header' => $header],
+        'concat_space' => ['spacing' => 'one'],
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+    ->setCacheFile('.php-cs-fixer.new.cache');
