6079eaae62a66804a5e3a84682e8f2314bc0bc75
tuntoja <tuntoja@centreon.com>
2025-06-16 16:17:11 +0200

    add test trigger

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * For more information : contact@centreon.com
+ * For more information : contact@centreon.com !!! TRIGGER !!!
  *
  */
 
