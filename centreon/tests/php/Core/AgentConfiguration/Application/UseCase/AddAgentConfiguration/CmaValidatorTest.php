5c77b9c8efb548042af6711b10b24daec79d24cf
alaunois <alaunois@centreon.com>
2025-06-26 10:53:34 +0200

    update unit tests

--- centreon/tests/php/Core/AgentConfiguration/Application/UseCase/AddAgentConfiguration/CmaValidatorTest.php
+++ centreon/tests/php/Core/AgentConfiguration/Application/UseCase/AddAgentConfiguration/CmaValidatorTest.php
@@ -55,15 +55,21 @@ beforeEach(function (): void {
     $this->request->pollerIds = [1];
     $this->request->connectionMode = ConnectionModeEnum::SECURE;
     $this->request->configuration = [
-        'is_reverse' => true,
+        'agent_initiated' => false,
+        'poller_initiated' => false,
         'otel_public_certificate' => '/etc/pki/test.crt',
         'otel_private_key' => '/etc/pki/test.key',
         'otel_ca_certificate' => '/etc/pki/test.cer',
-        'tokens' => [],
+        'tokens' => [
+            [
+                'name' => $this->token->getName(),
+                'creator_id' => $this->token->getCreatorId(),
+            ]
+        ],
         'hosts' => [
             [
                 'id' => 1,
-                'address' => '',
+                'address' => '10.11.12.13',
                 'port' => 0,
                 'poller_ca_certificate' => '/etc/pki/test.cer',
                 'poller_ca_name' => 'poller-name',
@@ -78,6 +84,9 @@ beforeEach(function (): void {
     $this->poller = new Poller(1, 'poller-name');
 });
 
+/** ------------------------------------------ Validator compatibility ------------------------------------------ */
+
+
 it('should correctly identify that it handles CMA type', function (): void {
     $result = $this->cmaValidator->isValidFor(Type::CMA);
     expect($result)->toBeTrue();
@@ -87,6 +96,8 @@ it('should correctly identify that it does not handle other types', function ():
     $result = $this->cmaValidator->isValidFor(Type::TELEGRAF);
     expect($result)->toBeFalse();
 });
+
+/** --------------------------------------- Agent initiated validation --------------------------------------- */
 
 foreach (
     [
@@ -98,12 +109,79 @@ foreach (
         '/etc/pki/test.doc',
     ] as $filename
 ) {
-    it("should throw an exception because of the filename for certificate {$filename} invalidity", function () use ($filename): void {
+    it("should throw an exception because of the filename for certificate {$filename} invalidity (agent_initiated)", function () use ($filename): void {
+        $this->request->configuration['agent_initiated'] = true;
         $this->request->configuration['otel_ca_certificate'] = $filename;
         $this->expectException(AgentConfigurationException::class);
         $this->cmaValidator->validateParametersOrFail($this->request);
     });
 }
+
+foreach (
+    [
+        'invalidfilename',
+        './fileName.key',
+        '../fileName.key',
+        '//fileName.key',
+        '/etc/pki/test.txt',
+        '/etc/pki/test.doc',
+    ] as $filename
+) {
+    it("should throw an exception because of the filename for key {$filename} invalidity (agent_initiated)", function () use ($filename): void {
+        $this->request->configuration['agent_initiated'] = true;
+        $this->request->configuration['otel_private_key'] = $filename;
+        $this->expectException(AgentConfigurationException::class);
+        $this->cmaValidator->validateParametersOrFail($this->request);
+    });
+}
+
+foreach (
+    [
+        '/etc/pki/test.key',
+        'test.key',
+    ] as $filename
+) {
+    it("should not throw an exception when the filename for key {$filename} is valid (agent_initiated)", function () use ($filename): void {
+        $this->request->configuration['agent_initiated'] = true;
+        $this->request->configuration['otel_private_key'] = $filename;
+        $this->user
+            ->method('isAdmin')
+            ->willReturn(true);
+        $this->readHostRepository
+            ->method('exists')
+            ->willReturn(true);
+        $this->readTokenRepository
+            ->method('findByNameAndUserId')
+            ->willReturn($this->token);
+        $this->cmaValidator->validateParametersOrFail($this->request);
+    })->expectNotToPerformAssertions();
+}
+
+
+it("should throw an exception when no token is provided and connection is not no_tls (agent_initiated)", function (): void {
+    $this->request->configuration['agent_initiated'] = true;
+    $this->request->configuration['tokens'] = [];
+
+    $this->expectException(AgentConfigurationException::class);
+    $this->cmaValidator->validateParametersOrFail($this->request);
+});
+
+it("should throw an exception when a token is provided but invalid and connection is not no_tls (agent_initiated)", function (): void {
+    $this->request->configuration['agent_initiated'] = true;
+    $this->user
+        ->expects($this->once())
+        ->method('isAdmin')
+        ->willReturn(true);
+    $this->readTokenRepository
+        ->expects($this->once())
+        ->method('findByNameAndUserId')
+        ->willReturn(null);
+    $this->expectException(AgentConfigurationException::class);
+    $this->cmaValidator->validateParametersOrFail($this->request);
+});
+
+
+/** --------------------------------------- Poller initiated validation --------------------------------------- */
 
 foreach (
     [
@@ -113,7 +191,8 @@ foreach (
         'test.cer',
     ] as $filename
 ) {
-    it("should not throw an exception when the filename for certificate {$filename} is valid", function () use ($filename): void {
+    it("should not throw an exception when the filename for certificate {$filename} is valid (poller_initiated)", function () use ($filename): void {
+        $this->request->configuration['poller_initiated'] = true;
         $this->request->configuration['hosts'][0]['poller_ca_certificate'] = $filename;
         $this->user
             ->method('isAdmin')
@@ -129,66 +208,9 @@ foreach (
         $this->cmaValidator->validateParametersOrFail($this->request);
     });
 }
-foreach (
-    [
-        'invalidfilename',
-        './fileName.key',
-        '../fileName.key',
-        '//fileName.key',
-        '/etc/pki/test.txt',
-        '/etc/pki/test.doc',
-    ] as $filename
-) {
-    it("should throw an exception because of the filename for key {$filename} invalidity", function () use ($filename): void {
-        $this->request->configuration['otel_private_key'] = $filename;
-        $this->expectException(AgentConfigurationException::class);
-        $this->cmaValidator->validateParametersOrFail($this->request);
-    });
-}
 
-foreach (
-    [
-        '/etc/pki/test.key',
-        'test.key',
-    ] as $filename
-) {
-    it("should not throw an exception when the filename for key {$filename} is valid", function () use ($filename): void {
-        $this->request->configuration['otel_private_key'] = $filename;
-        $this->user
-            ->method('isAdmin')
-            ->willReturn(true);
-        $this->readHostRepository
-            ->method('exists')
-            ->willReturn(true);
-        $this->readTokenRepository
-            ->method('findByNameAndUserId')
-            ->willReturn($this->token);
-        $this->cmaValidator->validateParametersOrFail($this->request);
-    })->expectNotToPerformAssertions();
-}
-
-it("should throw an exception when a token is not provided and connection is not no_tls or reverse", function (): void {
-    $this->request->configuration['is_reverse'] = false;
-    $this->expectException(AgentConfigurationException::class);
-    $this->cmaValidator->validateParametersOrFail($this->request);
-});
-
-it("should throw an exception when a token is provided but invalid and connection is not no_tls or reverse", function (): void {
-    $this->request->configuration['is_reverse'] = false;
-    $this->request->configuration['tokens'] = [['name' => 'tokenName', 'creator_id' => 1]];
-    $this->user
-        ->expects($this->once())
-        ->method('isAdmin')
-        ->willReturn(true);
-    $this->readTokenRepository
-        ->expects($this->once())
-        ->method('findByNameAndUserId')
-        ->willReturn(null);
-    $this->expectException(AgentConfigurationException::class);
-    $this->cmaValidator->validateParametersOrFail($this->request);
-});
-
-it('should throw an exception when the host id is invalid', function (): void {
+it('should throw an exception when the host id is invalid (poller_initiated)', function (): void {
+    $this->request->configuration['poller_initiated'] = true;
     $this->request->configuration['hosts'][0]['id'] = 9999;
     $this->readHostRepository
         ->expects($this->once())
@@ -197,13 +219,15 @@ it('should throw an exception when the host id is invalid', function (): void {
     $this->cmaValidator->validateParametersOrFail($this->request);
 })->throws((AgentConfigurationException::invalidHostId(9999)->getMessage()));
 
-it("should throw an exception when a token is not provided for an host and connection is reverse and not no_tls", function (): void { ;
+it("should throw an exception when a token is not provided connection is not no_tls (poller_initiated)", function (): void {
+    $this->request->configuration['poller_initiated'] = true;
     $this->request->configuration['hosts'][0]['token'] = null;
     $this->expectException(AgentConfigurationException::class);
     $this->cmaValidator->validateParametersOrFail($this->request);
 });
 
-it("should throw an exception when a token is provided for an host but invalid and connection is reverse and not no_tls", function (): void {
+it("should throw an exception when a token is provided but invalid and connection is not no_tls (poller_initiated)", function (): void {
+    $this->request->configuration['poller_initiated'] = true;
     $this->user
         ->expects($this->once())
         ->method('isAdmin')
