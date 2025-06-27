f1567e391d2feaf1097690a5d2cf94362cfddac5
Adrien Morais <amorais@centreon.com>
2025-06-27 15:39:45 +0200

    hack for frontend login

--- centreon/www/front_src/src/Login/api/endpoint.ts
+++ centreon/www/front_src/src/Login/api/endpoint.ts
@@ -1,5 +1,5 @@
 import { baseEndpoint } from '../../api/endpoint';
 
-export const loginEndpoint = `${baseEndpoint}/authentication/providers/configurations/local`;
-export const providersConfigurationEndpoint = `${baseEndpoint}/authentication/providers/configurations`;
-export const loginPageCustomisationEndpoint = `${baseEndpoint}/it-edition-extensions/login_page`;
+export const loginEndpoint = '/authentication/providers/configurations/local';
+export const providersConfigurationEndpoint = '/authentication/providers/configurations';
+export const loginPageCustomisationEndpoint = '/it-edition-extensions/login_page';
