553f8bd6af5b50bc87aca18fe4d91f672a261632
Adrien Morais <amorais@centreon.com>
2025-06-27 17:46:22 +0200

    add monolog bundle to new kernel

--- centreon/config.new/bundles.php
+++ centreon/config.new/bundles.php
@@ -24,4 +24,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
