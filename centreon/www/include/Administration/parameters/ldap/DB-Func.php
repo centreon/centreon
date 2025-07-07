e9624f31d7df50b4deaa46b417e3d27e80056dd8
Stéphane Duret <sduret@centreon.com>
2025-07-07 16:45:54 +0200

    Remove useless condition

--- centreon/www/include/Administration/parameters/ldap/DB-Func.php
+++ centreon/www/include/Administration/parameters/ldap/DB-Func.php
@@ -64,10 +64,6 @@ function checkLdapFilterSyntax(string $filterValue): bool
         return false;
     }
 
-    if (! preg_match('/=%s\)/', $filterValue)) {
-        return false;
-    }
-
     // check for parentheses
     if (substr_count($filterValue, '(') !== substr_count($filterValue, ')') ||
         ! str_starts_with($filterValue, '(') ||
