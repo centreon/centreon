f32cfbce828c8564090f894fbcdec6e9dde5ba45
Anthony Graule <agraule@centreon.com>
2025-07-13 11:25:04 +0200

    fix cs fixer

--- centreon-dsm/.php-cs-fixer.legacy.php
+++ centreon-dsm/.php-cs-fixer.legacy.php
@@ -31,7 +31,7 @@ $finder = Finder::create()
     ])
     ->append([
         __DIR__ . '/.php-cs-fixer.legacy.php',
-        __DIR__ . '/rector.php',
+        __DIR__ . '/rector.legacy.php',
     ]);
 
 return $config
