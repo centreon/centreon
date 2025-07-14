006b4cbc1f743939000f8f336308fd3a238b0d0b
Med1233 <melmeziani-ext@centreon.com>
2025-07-14 10:52:26 +0100

    fix

--- centreon/tests/e2e/features/Agent-configuration/commands.ts
+++ centreon/tests/e2e/features/Agent-configuration/commands.ts
@@ -122,8 +122,17 @@ Cypress.Commands.add('addCMAToken', () => {
   cy.contains('Centreon monitoring agent').click();
   cy.contains('button', 'Generate token').click();
   cy.wait('@addToken');
-  cy.contains('button', 'Done').click();
-  cy.logout();
+
+  cy.contains('The token name "CMA-Token-001" already exists').then(
+    (element) => {
+      if (element?.length > 0) {
+        cy.logout();
+      } else {
+        cy.contains('button', 'Done').click();
+        cy.logout();
+      }
+    }
+  );
 });
 
 interface Telegraf {
