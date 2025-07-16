77502664af6dc6e51214f968dd8974e0b32ef5b8
Tom Darneix <tomdar87@outlook.com>
2025-07-16 21:51:36 +0300

    github part 2

---
+++ centreon/www/front_src/src/Configuration/AdditionnalConnectors/Specs/Acc.cypress.spec.tsx
@@ -0,0 +1,9 @@
+import Filters from './Acc.filter';
+import Listing from './Acc.listing';
+import Modal from './Acc.modal';
+
+describe('Additional connectors configuration: ', () => {
+  Listing();
+  Filters();
+  Modal();
+});
