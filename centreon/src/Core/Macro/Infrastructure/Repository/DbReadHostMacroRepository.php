928b14ca086726fd44abf026ddc1d666057590ae
Anthony Graule <agraule@centreon.com>
2025-07-16 18:07:50 +0200

    fix autodisco

--- centreon/src/Core/Macro/Infrastructure/Repository/DbReadHostMacroRepository.php
+++ centreon/src/Core/Macro/Infrastructure/Repository/DbReadHostMacroRepository.php
@@ -199,7 +199,7 @@ class DbReadHostMacroRepository extends AbstractRepositoryRDB implements ReadHos
         );
         $macro->setIsPassword((bool) $data['is_password']);
         $macro->setDescription($data['description'] ?? '');
-        $macro->setOrder($data['macro_order']);
+        $macro->setOrder($data['macro_order'] ?? 0);
 
         return $macro;
     }
