0ecd834b9885541125e8dac7af71eefa9f03c5ca
jeremyjaouen <jjaouen@centreon.com>
2025-07-21 09:00:27 +0200

    Fix update

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -59,16 +59,20 @@ $setEncryptionReadyToFalseByDefaultOnNagiosServer = function () use ($pearDB, &$
  */
 $setEncryptionReadyToFalseByDefaultOnInstances = function () use ($pearDB, $pearDBO, &$errorMessage): void {
     $errorMessage = "Unable to update 'is_encryption_ready' column on 'nagios_server' table";
+
+
     /** @var CentreonDB $pearDB */
-    $configDatabaseName = $pearDB->getConnectionConfig()->getDatabaseNameConfiguration();
-    $statement = $pearDBO->prepare(
+    $instanceIds = $pearDB->fetchFirstColumn(
         <<<'SQL'
-            UPDATE instances SET `is_encryption_ready` = '0' WHERE `instance_id` IN (
-                SELECT `id` FROM `:db`.nagios_server WHERE `localhost` = '0'
-            );
+            SELECT `id` FROM nagios_server WHERE `localhost` = '0';
+        SQL
+    );
+    $instanceIdsAsString = implode(",", $instanceIds);
+    $statement = $pearDBO->prepare(
+        <<<SQL
+            UPDATE instances SET `is_encryption_ready` = '0' WHERE `instance_id` IN ({$instanceIdsAsString});
             SQL
     );
-    $statement->bindValue(':db', $configDatabaseName, PDO::PARAM_STR);
     $statement->execute();
 };
 
