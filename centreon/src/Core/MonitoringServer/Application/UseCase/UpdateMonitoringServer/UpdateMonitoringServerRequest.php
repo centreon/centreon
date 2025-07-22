51c4f4d852b9f3eed78160bc8c5111970c5569fd
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-07-22 07:23:28 +0200

    fix(pollers): Enhance command validation (#7811)
    remove not wanted change
    
    ensure symfony 6.4 compatibility
    
    ensure php 8.1 compat
    
    style
    
    phpstan
    
    style
    
    style

--- centreon/src/Core/MonitoringServer/Application/UseCase/UpdateMonitoringServer/UpdateMonitoringServerRequest.php
+++ centreon/src/Core/MonitoringServer/Application/UseCase/UpdateMonitoringServer/UpdateMonitoringServerRequest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,23 +21,19 @@
 
 declare(strict_types=1);
 
-namespace Core\MonitoringServer\Application\Repository;
+namespace Core\MonitoringServer\Application\UseCase\UpdateMonitoringServer;
 
-use Core\MonitoringServer\Model\MonitoringServer;
-
-interface WriteMonitoringServerRepositoryInterface
+final class UpdateMonitoringServerRequest
 {
-    /**
-     * Define the monitoring server as changed since its last configuration export.
-     *
-     * @param int $monitoringServerId
-     */
-    public function notifyConfigurationChange(int $monitoringServerId): void;
-
-    /**
-     * Update a monitoring server.
-     *
-     * @param MonitoringServer $monitoringServer
-     */
-    public function update(MonitoringServer $monitoringServer): void;
+    public function __construct(
+        public int $id,
+        public string $name,
+        public ?string $engineStartCommand,
+        public ?string $engineStopCommand,
+        public ?string $engineRestartCommand,
+        public ?string $engineReloadCommand,
+        public ?string $brokerReloadCommand,
+    ) {
+    }
 }
+
