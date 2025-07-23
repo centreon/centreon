7ca692f78274060460994cf5d0c28306f6d8edb3
MedWassCh <wchaabane@centreon.com>
2025-07-23 14:44:46 +0200

    test(Dashboards): wait for 'From' element to be visible in metric graph configuration

--- centreon/tests/e2e/features/Dashboards/10-widget-metric-graph-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/10-widget-metric-graph-configuration/index.ts
@@ -695,6 +695,7 @@ When(
 );
 
 Then('the graph updates to reflect data for the selected time period', () => {
+  cy.waitForElementToBeVisible('div[label="From"]');
   cy.getByLabel({
     label: 'From',
     tag: 'div'
