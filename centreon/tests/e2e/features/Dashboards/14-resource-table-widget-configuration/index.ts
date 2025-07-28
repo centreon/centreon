fbf227ee66708911e36e112321436248a6a91a03
MedWassCh <wchaabane@centreon.com>
2025-07-28 17:19:40 +0200

    fix(test): correct indentation in resource table widget test

--- centreon/tests/e2e/features/Dashboards/14-resource-table-widget-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/14-resource-table-widget-configuration/index.ts
@@ -348,9 +348,9 @@ Then(
   'all the resources having the status selected are displayed in the resource table Widget',
   () => {
     cy.getCellContent(1, 2).then((myTableContent) => {
-	 		expect(myTableContent[3]).to.match(/Pending|Unknown|Warning/);
+      expect(myTableContent[3]).to.match(/Pending|Unknown|Warning/);
       expect(myTableContent[6]).to.match(/Pending|Unknown|Warning/);
-		});
+    });
   }
 );
 
