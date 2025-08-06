da7b7071b6d77757a3c05c7eeaf55d8fe2447302
selfakiri <selfakiri@ext.centreon.com>
2025-08-06 20:05:55 +0100

    fix(resource status): notification tab displays wrong contact

--- centreon/src/Core/Infrastructure/Configuration/NotificationPolicy/Repository/LegacyReadServiceNotificationRepository.php
+++ centreon/src/Core/Infrastructure/Configuration/NotificationPolicy/Repository/LegacyReadServiceNotificationRepository.php
@@ -120,9 +120,7 @@ class LegacyReadServiceNotificationRepository extends AbstractDbReadNotification
 
         /**
          * @var array{
-         *      service_use_only_contacts_from_host:string,
-         *      contacts_cache?: int[],
-         *      contact_groups_cache?: int[]
+         *      service_use_only_contacts_from_host:string
          * }|null $service
          */
         $service = $serviceInstance->getServiceFromCache($serviceId);
@@ -132,8 +130,7 @@ class LegacyReadServiceNotificationRepository extends AbstractDbReadNotification
             && (
                 $service['service_use_only_contacts_from_host'] === '1'
                 || (
-                    array_key_exists('contacts_cache', $service) && $service['contacts_cache'] === []
-                    && array_key_exists('contact_groups_cache', $service) && $service['contact_groups_cache'] === []
+                    empty($notifiedContactIds) && empty($notifiedContactGroupIds)
                 )
             )
         ) {
