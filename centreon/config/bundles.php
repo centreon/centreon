579b8100b323704c9825b48a098efc3aff97d557
Anthony Graule <agraule@centreon.com>
2025-08-10 01:32:35 +0200

    test to fix

--- centreon/config/bundles.php
+++ centreon/config/bundles.php
@@ -25,5 +25,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
