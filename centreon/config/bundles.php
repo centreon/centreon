94ee32fdee54db39fe79e79963304f141380d832
Anthony Graule <agraule@centreon.com>
2025-08-11 22:33:37 +0200

    twig removed

--- centreon/config/bundles.php
+++ centreon/config/bundles.php
@@ -26,5 +26,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
 ];
