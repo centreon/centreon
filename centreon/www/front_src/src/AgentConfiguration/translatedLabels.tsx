ac48fac1e1a60c6bbeb038aeeb9850befaa2fb3d
Tom Rabut <trabut@centreon.com>
2025-08-13 11:03:14 +0200

    chore: remove duplicated trans after rebase

--- centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
+++ centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
@@ -77,9 +77,3 @@ export const labelCMAauthenticationToken = 'CMA authentication token(s)';
 export const labelSelectExistingCMATokens = 'Select existing CMA token(s)';
 export const labelSelectExistingCMAToken = 'Select existing CMA token';
 export const labelCreateNewCMAToken = 'Create new CMA token';
-
-export const labelDeletePollerConfirmation =
-  'You are going to delete the configuration for the <strong>{{ poller }}</strong> poller from the <strong>{{ agent }}</strong> agent configuration. All configuration parameters for this poller will be deleted. This action cannot be undone.';
-
-export const labelDeleteAgentConfirmation =
-  'You are going to delete the <strong>{{ agent }}</strong> agent configuration. All parameters for this agent configuration will be deleted. This action cannot be undone.';
