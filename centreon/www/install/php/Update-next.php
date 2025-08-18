d6599ab36f8d0a1c86373711fdced26f7d07859f
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-08-18 13:04:33 +0200

    enh(contact): increase centreon.contact.contact_pager size to 300 (#7967) (#8013)
    Co-authored-by: selfakiri <selfakiri@ext.centreon.com>

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -30,12 +30,24 @@ $errorMessage = '';
 
 // TODO add your functions here
 
+$alterContactPagerSize = function () use ($pearDB, &$errorMessage): void {
+    $errorMessage = 'Unable to alter contact_pager column size in contact table';
+    if ($pearDB->isColumnExist('contact', 'contact_pager')) {
+        $pearDB->executeStatement(
+            <<<'SQL'
+                ALTER TABLE `contact`
+                    MODIFY COLUMN `contact_pager` VARCHAR(300)
+                SQL
+        );
+    }
+};
+
 try {
     // DDL statements for real time database
     // TODO add your function calls to update the real time database structure here
 
     // DDL statements for configuration database
-    // TODO add your function calls to update the configuration database structure here
+    $alterContactPagerSize();
 
     // Transactional queries for configuration database
     if (! $pearDB->inTransaction()) {
