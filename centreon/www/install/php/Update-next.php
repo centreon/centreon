b07baef33e55a0a7d607094628fc607d1f696b65
jeremyjaouen <jjaouen@centreon.com>
2025-08-21 16:31:49 +0200

    fix(AC): Add an upgrade script to realign correctly CMA Configurations

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -19,6 +19,13 @@
  *
  */
 
+use Adaptation\Database\Connection\Collection\QueryParameters;
+use Adaptation\Database\Connection\ValueObject\QueryParameter;
+use Core\Common\Domain\TrimmedString;
+use Core\Security\Token\Domain\Model\NewJwtToken;
+use Core\Security\Token\Domain\Model\TokenFactory;
+use Core\Security\Token\Domain\Model\TokenTypeEnum;
+
 require_once __DIR__ . '/../../../bootstrap.php';
 
 /**
@@ -155,6 +162,122 @@ $alterContactPagerSize = function () use ($pearDB, &$errorMessage): void {
     }
 };
 
+/**
+ * Generate a token based on the first found admin contact to update old agent_configurations
+ *
+ * @return array{token_name: string, creator_id: int}
+ */
+$generateToken = function () use ($pearDB): array {
+    $admin = $pearDB->fetchAssociative(
+        <<<'SQL'
+            SELECT contact_id, contact_name
+            FROM contact
+            WHERE contact_admin = '1'
+            LIMIT 1
+            SQL
+    );
+
+    $token = new NewJwtToken(
+        name: new TrimmedString('cma-hotfix'),
+        creatorId: $admin['contact_id'],
+        creatorName: $admin['contact_name'],
+        expirationDate: null
+    );
+
+    $pearDB->executeStatement(
+        <<<'SQL'
+            INSERT INTO `jwt_tokens` (token_string,token_name,creator_id,creator_name,encoding_key,is_revoked,creation_date,expiration_date)
+            VALUES (:token_string,:token_name,:creator_id,:creator_name,:encoding_key,:is_revoked,:creation_date,:expiration_date)
+            SQL,
+        QueryParameters::create([
+            QueryParameter::string(':token_string', $token->getToken()),
+            QueryParameter::string(':token_name', $token->getName()),
+            QueryParameter::int(':creator_id', $token->getCreatorId()),
+            QueryParameter::string(':creator_name', $token->getCreatorName()),
+            QueryParameter::string(':encoding_key', $token->getEncodingKey()),
+            QueryParameter::bool(':is_revoked', false),
+            QueryParameter::int(':creation_date', $token->getCreationDate()->getTimestamp()),
+            QueryParameter::null(':expiration_date')
+        ])
+    );
+
+    return ['token_name' => 'cma-hotfix', 'creator_id' => $admin['contact_id']];
+};
+
+/**
+ * Align inconsistent Agent Configuration with the new schema:
+ *      - Add a `tokens` key for each configuration in non reverse
+ *      - Add a `token` key for each configuration in reverse
+ *      - Add a `id` key for each hosts in each in reverse configuration
+ *          - host id is based on the first ID found for this address.
+ *          - As many hosts could have the same address, users should validate that the picken host is the good one.
+ */
+$alignCMAAgentConfigurationWithNewSchema = function () use ($pearDB, &$errorMessage, $generateToken): void {
+    $errorMessage = 'Unable to align agent configuration with new schema';
+    $agentConfigurations = $pearDB->fetchAllAssociative(
+        <<<'SQL'
+            SELECT * FROM `agent_configuration`
+            WHERE `type` = 'centreon-agent'
+            SQL
+    );
+    if (empty($agentConfigurations)) {
+        return;
+    }
+    $tokenInformation = $generateToken();
+    foreach ($agentConfigurations as $agentConfiguration) {
+        $configuration = json_decode(
+            json: $agentConfiguration['configuration'],
+            associative: true,
+            flags: JSON_THROW_ON_ERROR
+        );
+        if ($configuration['is_reverse']) {
+            // `tokens` should be an empty array for reverse connection
+            if (! array_key_exists('tokens', $configuration)) {
+                $configuration['tokens'] = [];
+            }
+            foreach ($configuration['hosts'] as &$host) {
+                if (! array_key_exists('token', $host)) {
+                    $host['token'] = $tokenInformation;
+                }
+                if (! array_key_exists('id', $host)) {
+                    $hostId = $pearDB->fetchOne(
+                        <<<SQL
+                            SELECT host_id
+                            FROM host
+                            WHERE host_address = :hostAddress
+                            LIMIT 1
+                            SQL,
+                        QueryParameters::create([QueryParameter::string(':hostAddress', $host['address'])])
+                    );
+                    $host['id'] = $hostId;
+                }
+            }
+        } else {
+            // `hosts` should be an empty array for reverse connection
+            if (! array_key_exists('hosts', $configuration)) {
+                $configuration['hosts'] = [];
+            }
+            if (! array_key_exists('tokens', $configuration)) {
+                $configuration['tokens'] = [$tokenInformation];
+            }
+        }
+
+        $configuration = json_encode($configuration);
+        $pearDB->update(
+            <<<SQL
+                UPDATE agent_configuration
+                SET configuration = :configuration
+                WHERE id = :id
+                SQL,
+            QueryParameters::create([
+                QueryParameter::string(':configuration', json_encode($configuration)),
+                QueryParameter::int(':id', $agentConfiguration['id'])
+            ])
+        );
+    }
+};
+
+
 try {
     // DDL statements for real time database
     // TODO add your function calls to update the real time database structure here
@@ -173,6 +296,7 @@ try {
     $bbdoCfgUpdate();
     $addResourceStatusSearchModeOption();
     $flagContactsAsServiceAccount();
+    $alignCMAAgentConfigurationWithNewSchema();
 
     $pearDB->commit();
 
