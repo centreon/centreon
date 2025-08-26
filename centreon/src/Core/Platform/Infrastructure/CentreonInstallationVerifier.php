40ec9e8acf5b3c6186c938488fcb351a328f83ad
jeremyjaouen <jjaouen@centreon.com>
2025-08-26 15:12:41 +0200

    remove final class

--- centreon/src/Core/Platform/Infrastructure/CentreonInstallationVerifier.php
+++ centreon/src/Core/Platform/Infrastructure/CentreonInstallationVerifier.php
@@ -25,7 +25,7 @@ namespace Core\Platform\Infrastructure;
 
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-final readonly class CentreonInstallationVerifier
+readonly class CentreonInstallationVerifier
 {
     public function __construct(
         #[Autowire(param: 'centreon_etc_path')]
