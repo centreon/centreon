b87237f65d11ab287748ce7b6d051e348f30b70c
dmyios <diosypenko@centreon.com>
2025-08-26 14:05:15 +0200

    cs-fixer iii

--- centreon/src/Core/Common/Infrastructure/Validator/Constraints/Sort.php
+++ centreon/src/Core/Common/Infrastructure/Validator/Constraints/Sort.php
@@ -30,6 +30,7 @@ use Symfony\Component\Validator\Constraint;
 final class Sort extends Constraint
 {
     public string $invalidSortKeyMessage = '{{ key }} is not a valid sort value. Expected values [{{ values }}].';
+
     public string $invalidSortDirectionMesssage
         = '{{ key }} has invalid sort direction ({{ direction }}). Expected directions [{{ values }}].';
 
