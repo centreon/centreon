cd8ea932bbf4b9619e621ed5fc05a3bb406a8ee2
Anthony Graule <agraule@centreon.com>
2025-08-26 21:34:26 +0200

    enh(unit_tests) : php quality tools applied to mock directory (#8090)

--- centreon/tests/php/mock/Object/CentreonMedia.php
+++ centreon/tests/php/mock/Object/CentreonMedia.php
@@ -1,19 +1,24 @@
 <?php
-/**
- * Copyright 2016 Centreon
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
  */
+
 namespace Centreon\Test\Mock\Object;
 
 class CentreonMedia extends BaseObject
@@ -23,7 +28,7 @@ class CentreonMedia extends BaseObject
         return $this->getIncrementedId();
     }
 
-    public function getDirectoryName()
+    public function getDirectoryName(): string
     {
         return 'directory_name';
     }
@@ -32,13 +37,13 @@ class CentreonMedia extends BaseObject
     {
         return $this->getIncrementedId();
     }
-    
+
     public function setMediaDirectory($newMediaDirectory)
     {
-        
+
     }
-    
-    public function getMediaDirectory()
+
+    public function getMediaDirectory(): string
     {
         return 'directory_name';
     }
