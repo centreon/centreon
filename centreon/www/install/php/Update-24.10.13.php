7513ff2396af0920951a63c7e38e50c147242666
pkippes <144150042+pkippes@users.noreply.github.com>
2025-09-01 11:32:14 +0200

    Update centreon/www/install/php/Update-24.10.13.php
    Co-authored-by: selfakiri <selfakiri@ext.centreon.com>

--- centreon/www/install/php/Update-24.10.13.php
+++ centreon/www/install/php/Update-24.10.13.php
@@ -30,7 +30,7 @@ require_once __DIR__ . '/../../../bootstrap.php';
  * This file contains changes to be included in the next version.
  * The actual version number should be added in the variable $version.
  */
-$version = 'xx.xx.x';
+$version = '24.10.13';
 $errorMessage = '';
 
 // TODO add your functions here
