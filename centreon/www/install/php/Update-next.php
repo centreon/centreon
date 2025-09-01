b545fdc4b5014ae4c11f1d70e301ae4c935516c6
pkippes <144150042+pkippes@users.noreply.github.com>
2025-09-01 10:48:08 +0200

    chore(release) Reset update php  template

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -29,24 +29,12 @@ $errorMessage = '';
 
 // TODO add your functions here
 
-$alterContactPagerSize = function () use ($pearDB, &$errorMessage): void {
-    $errorMessage = 'Unable to alter contact_pager column size in contact table';
-    if ($pearDB->isColumnExist('contact', 'contact_pager')) {
-        $pearDB->executeStatement(
-            <<<'SQL'
-                ALTER TABLE `contact`
-                    MODIFY COLUMN `contact_pager` VARCHAR(300)
-                SQL
-        );
-    }
-};
-
 try {
     // DDL statements for real time database
     // TODO add your function calls to update the real time database structure here
 
     // DDL statements for configuration database
-    $alterContactPagerSize();
+    // TODO add your function calls to update the configuration database structure here
 
     // Transactional queries for configuration database
     if (! $pearDB->inTransaction()) {
