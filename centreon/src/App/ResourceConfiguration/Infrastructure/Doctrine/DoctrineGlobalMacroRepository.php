7b9066053738d5d3ec4c29450d22e50f80693f96
jeremyjaouen <jjaouen@centreon.com>
2025-09-02 15:12:00 +0200

    implement list global macros

---
+++ centreon/src/App/ResourceConfiguration/Infrastructure/Doctrine/DoctrineGlobalMacroRepository.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace App\ResourceConfiguration\Infrastructure\Doctrine;
+
+use App\ResourceConfiguration\Domain\Aggregate\GlobalMacro;
+use App\ResourceConfiguration\Domain\Aggregate\GlobalMacroComment;
+use App\ResourceConfiguration\Domain\Aggregate\GlobalMacroId;
+use App\ResourceConfiguration\Domain\Aggregate\GlobalMacroName;
+use App\ResourceConfiguration\Domain\Aggregate\GlobalMacroExpression;
+use App\ResourceConfiguration\Domain\Collection\GlobalMacroCollection;
+use App\ResourceConfiguration\Domain\Repository\GlobalMacroRepository;
+use App\Shared\Infrastructure\Doctrine\DoctrineRepository;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * @phpstan-type RowTypeAlias = array{
+ *  resource_id: int,
+ *  resource_name: string,
+ *  resource_line: string,
+ *  resource_comment: string,
+ *  resource_activate: '0'|'1',
+ *  is_password: 0|1
+ * }
+ */
+final readonly class DoctrineGlobalMacroRepository extends DoctrineRepository implements GlobalMacroRepository
+{
+    private const TABLE_NAME = 'cfg_resource';
+
+    public function __construct(
+        #[Autowire(service: 'doctrine.dbal.default_connection')]
+        private Connection $connection,
+    ) {
+    }
+
+    public function findAll(): GlobalMacroCollection
+    {
+        $qb = $this->connection->createQueryBuilder();
+
+        $qb->select('resource_id', 'resource_name', 'resource_line', 'resource_comment', 'resource_activate', 'is_password')
+            ->from(self::TABLE_NAME);
+
+        /**
+         * @var RowTypeAlias[] $rows
+         */
+        $rows = $qb->executeQuery()->fetchAllAssociative();
+
+        $collection = new GlobalMacroCollection();
+
+        foreach ($rows as $row) {
+            $collection->add($this->createGlobalMacro($row));
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @param RowTypeAlias $row
+     */
+    private function createGlobalMacro(array $row): GlobalMacro
+    {
+        return new GlobalMacro(
+            id: new GlobalMacroId($row['resource_id']),
+            name: new GlobalMacroName($row['resource_name']),
+            expression: new GlobalMacroExpression($row['resource_line']),
+            comment: $row['resource_comment'] !== null ? new GlobalMacroComment($row['resource_comment']) : null,
+            activated: $row['resource_activate'] === '1',
+            isPassword: $row['is_password'] === 1,
+        );
+    }
+}
