0c85046166d7207a937517f6904218926ff85cb7
dmyios <diosypenko@centreon.com>
2025-09-02 15:31:54 +0200

    added CriteriaInterface

--- centreon/tests/php/Core/Common/Infrastructure/Repository/PaginatorTest.php
+++ centreon/tests/php/Core/Common/Infrastructure/Repository/PaginatorTest.php
@@ -1,9 +1,29 @@
 <?php
 
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
 namespace Tests\Core\Common\Infrastructure\Repository;
 
 use Core\Common\Infrastructure\Repository\Paginator;
-use Generator;
 use PHPUnit\Framework\TestCase;
 
 final class PaginatorTest extends TestCase
@@ -68,6 +88,9 @@ final class PaginatorTest extends TestCase
         }
     }
 
+    /**
+     * @return \Generator<array<mixed>>
+     */
     public static function iteratorDataProvider(): \Generator
     {
         yield [
