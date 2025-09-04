7b39c389ad2759cc20974fce6ee43a22c05c56ca
selfakiri <selfakiri@ext.centreon.com>
2025-09-04 10:58:47 +0100

    fix(conf) fix alias field in form (#8076) (#8126)
    Co-authored-by: alaunois <alaunois@centreon.com>

--- centreon-open-tickets/www/modules/centreon-open-tickets/views/rules/form.php
+++ centreon-open-tickets/www/modules/centreon-open-tickets/views/rules/form.php
@@ -41,8 +41,8 @@ $result_rule = $rule->getAliasAndProviderId($ruleId);
 $tpl->assign("page", $p);
 $tpl->assign('rule_id', $ruleId);
 
-$rule_alias_html = '<input size="30" name="rule_alias" type="text" value="' .
-    (isset($result_rule['alias']) ? $result_rule['alias'] : '') . '" />';
+$rule_alias_html = '<input size="30" name="rule_alias" type="text" value="'
+    . (htmlspecialchars($result_rule['alias'] ?? '', ENT_QUOTES, 'UTF-8')) . '" />';
 $provider_html = '<select id="provider_id" name="provider_id"><option value=""></option>';
 ksort($register_providers);
 foreach ($register_providers as $name => $value) {
