838dc73bb687f0a8daa0af4b00617b83ecbd11cb
alaunois <alaunois@centreon.com>
2025-09-05 17:35:40 +0200

    enh(ceip) update api token stat report

--- centreon/www/class/centreonStatistics.class.php
+++ centreon/www/class/centreonStatistics.class.php
@@ -300,18 +300,7 @@ class CentreonStatistics
      */
     public function getApiTokensInfo()
     {
-        $data = [];
-
-        $statementNbTokens = $this->dbConfig->query(
-            <<<'SQL'
-                SELECT count(token_type)
-                FROM security_authentication_tokens
-                WHERE token_type ='manual'
-                SQL
-        );
-        $data['total'] = $statementNbTokens->fetch(PDO::FETCH_COLUMN) ?: 0;
-
-        $statementNbTokens = $this->dbConfig->query(
+        $statementManagerCount = $this->dbConfig->fetchOne(
             <<<'SQL'
                 SELECT count(contact_id)
                 FROM contact
@@ -321,8 +310,7 @@ class CentreonStatistics
                         FROM contact
                         WHERE
                             contact_admin = '1'
-                            AND contact_name != 'centreon-gorgone'
-
+                            AND is_service_account = '0'
                     )
                     OR contact_id IN (
                         SELECT contact_id
@@ -337,9 +325,29 @@ class CentreonStatistics
                     )
                 SQL
         );
-        $data['managers'] = $statementNbTokens->fetch(PDO::FETCH_COLUMN) ?: 0;
 
-        return $data;
+        $statementTokenInfos = $this->dbConfig->fetchAllAssociative(
+            <<<'SQL'
+                SELECT
+                    security_authentication_tokens.token_name as name,
+                    security_authentication_tokens.user_id,
+                    DATEDIFF(
+                        FROM_UNIXTIME(security_token.expiration_date),
+                        FROM_UNIXTIME(security_token.creation_date)
+                    ) as lifespan
+                FROM security_authentication_tokens
+                JOIN security_token
+                    ON security_token.token = security_authentication_tokens.token
+                WHERE
+                    security_authentication_tokens.token_type ='manual'
+                    AND security_authentication_tokens.is_revoked = 0
+                SQL
+        );
+
+        return [
+            'managers' => $statementManagerCount ?: 0,
+            'tokens' => $statementTokenInfos
+        ];
     }
 
     /**
