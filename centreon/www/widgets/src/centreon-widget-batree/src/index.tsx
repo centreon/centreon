bffd8b1c01087657c3b89b85118fdcf3f55cbdb4
Tom Rabut <trabut@centreon.com>
2025-09-05 12:20:56 +0200

    fix: use correct api in public playlist for widget batree

--- centreon/www/widgets/src/centreon-widget-batree/src/index.tsx
+++ centreon/www/widgets/src/centreon-widget-batree/src/index.tsx
@@ -1,22 +1,51 @@
-import { Module } from '@centreon/ui';
+import type { CommonWidgetProps, Data } from '../../models';
 
-// eslint-disable-next-line import/no-relative-packages
+import { Module } from '@centreon/ui';
+import { equals, last, pluck } from 'ramda';
 import FederatedComponent from '../../../../front_src/src/components/FederatedComponents';
 import NoResources from '../../NoResources';
+import { PanelOptions } from './models';
+import { labelSelectBAToDisplayPreview } from './translatedLabels';
 import { areResourcesFullfilled } from '../../utils';
 
-import { WidgetProps } from './models';
-
-const Widget = ({ panelData, store, ...rest }: WidgetProps): JSX.Element => {
+interface Props extends CommonWidgetProps<PanelOptions> {
+  panelData: Data;
+  panelOptions: PanelOptions;
+}
+const Widget = ({
+  panelData,
+  panelOptions,
+  globalRefreshInterval,
+  refreshCount,
+  id,
+  playlistHash,
+  dashboardId,
+  widgetPrefixQuery,
+  queryClient,
+  store
+}: Props): JSX.Element => {
   if (!areResourcesFullfilled(panelData.resources)) {
     return <NoResources />;
   }
 
   return (
     <Module seedName="centreon-widget-batree" store={store}>
-      <FederatedComponent panelData={panelData} path="/bam/widget" {...rest} />
+      <FederatedComponent
+        dashboardId={dashboardId}
+        globalRefreshInterval={globalRefreshInterval}
+        id={id}
+        panelData={panelData}
+        panelOptions={panelOptions}
+        playlistHash={playlistHash}
+        refreshCount={refreshCount}
+        widgetPrefixQuery={widgetPrefixQuery}
+        path="/bam/widget"
+        queryClient={queryClient}
+        store={store}
+      />
     </Module>
   );
 };
 
 export default Widget;
+
