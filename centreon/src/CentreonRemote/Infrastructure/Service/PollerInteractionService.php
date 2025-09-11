2fb7691b5e829d98a9164a9573533a2e43851cf7
Vincent Untz <vuntz@centreon.com>
2025-09-11 10:57:34 +0200

    ai review

--- centreon/src/CentreonRemote/Infrastructure/Service/PollerInteractionService.php
+++ centreon/src/CentreonRemote/Infrastructure/Service/PollerInteractionService.php
@@ -192,7 +192,9 @@ class PollerInteractionService
 
         foreach ($tabServers as $poller) {
             if (isset($poller['localhost']) && $poller['localhost'] == 1) {
-                shell_exec(escapeshellcmd("sudo {$poller['engine_restart_command']}"));
+                if ($poller['engine_restart_command'] != '') {
+                    shell_exec(escapeshellcmd("sudo -n -- {$poller['engine_restart_command']}"));
+                }
             } elseif ($fh = @fopen($centCorePipe, 'a+')) {
                 fwrite($fh, 'RESTART:' . $poller['id'] . "\n");
                 fclose($fh);
