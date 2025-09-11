638c0da471203a19cdd3af9171d7be98e5a0099a
Tpo76 <fthepaut@centreon.com>
2025-09-11 11:53:00 -0400

    fix when php pod recreate

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 chown -R centreon:centreon /var/lib/centreon/centcore /var/lib/centreon/
+chown www-data:www-data /var/cache/centreon/symfony 
 
 chmod -R 775 /var/lib/centreon/centcore /var/lib/centreon/
 # chmod 644 /etc/centreon/config.d/10-database.yaml
