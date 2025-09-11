e38f0c017239ba69eebb54b8c4b5b43b28ffcf90
Tpo76 <fthepaut@centreon.com>
2025-09-11 10:58:23 -0400

    workaround root file creation when using clapi to export configuration

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/70-export.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/70-export.sh
@@ -3,3 +3,4 @@
 php /usr/share/centreon/bin/centreon -u admin -p 'Centreon2025!' -a POLLERGENERATE -v 1 
 php /usr/share/centreon/bin/centreon -u admin -p 'Centreon2025!' -a CFGMOVE -v 1
 php /usr/share/centreon/bin/centreon -u admin -p 'Centreon2025!' -a POLLERRESTART -v 1
+chown -R www-data:www-data /var/cache/centreon/config
