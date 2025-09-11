b44846bb4565588787ede83c1c974425caec97b9
jeremyjaouen <jjaouen@centreon.com>
2025-09-11 17:03:27 +0200

    style

--- centreon/src/Core/Platform/Domain/InstallationVerifierInterface.php
+++ centreon/src/Core/Platform/Domain/InstallationVerifierInterface.php
@@ -4,7 +4,7 @@
  * Copyright 2005 - 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License
  * You may obtain a copy of the License at
  *
  * https://www.apache.org/licenses/LICENSE-2.0
