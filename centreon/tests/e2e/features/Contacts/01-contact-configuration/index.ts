3aff2702117f3cce42c860110d64d41c71feeb4a
s-elhassani <selhassani-ext@centreon.com>
2025-09-15 11:59:52 +0100

    fix login

--- centreon/tests/e2e/features/Contacts/01-contact-configuration/index.ts
+++ centreon/tests/e2e/features/Contacts/01-contact-configuration/index.ts
@@ -264,7 +264,7 @@ When(
     cy.logout();
     //Log in as a non-admin user with READ ONLY rights
     cy.loginByTypeOfUser({
-      jsonName: 'contacts-user-readonly',
+      jsonName: 'contacts-management-acl-user-readonly-rights',
       loginViaApi: false
     });
     contactPageIndex = 0;
