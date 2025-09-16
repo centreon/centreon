d5dd6386a0a9d1234a0c368567ff28989e84ca79
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-09-16 15:26:22 +0200

    Update centreon/src/App/MonitoringConfiguration/Infrastructure/Dbal/DbalOptionRepository.php
    Co-authored-by: alaunois <alaunois@centreon.com>

--- centreon/src/App/MonitoringConfiguration/Infrastructure/Dbal/DbalOptionRepository.php
+++ centreon/src/App/MonitoringConfiguration/Infrastructure/Dbal/DbalOptionRepository.php
@@ -62,6 +62,7 @@ final readonly class DbalOptionRepository extends DoctrineRepository implements 
         if ($row === false) {
             throw new OptionDoesNotExistException(['option_name' => $name->value]);
         }
+
         return $this->createOption($row);
     }
 
