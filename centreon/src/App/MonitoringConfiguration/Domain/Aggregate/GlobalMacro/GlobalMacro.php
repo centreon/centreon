f08e22a1cb17a0e807edd9ccb0dc94857f3669fb
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-09-16 11:14:29 +0200

    feat(macros): Add endpoint to list Global Macros (#8179)
    Co-authored-by: alaunois <alaunois@centreon.com>

---
+++ centreon/src/App/MonitoringConfiguration/Domain/Aggregate/GlobalMacro/GlobalMacro.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace App\MonitoringConfiguration\Domain\Aggregate\GlobalMacro;
+
+use App\Shared\Domain\Aggregate\AggregateRoot;
+
+final class GlobalMacro extends AggregateRoot
+{
+    public function __construct(
+        ?GlobalMacroId $id,
+        public readonly GlobalMacroName $name,
+        public readonly GlobalMacroExpression $expression,
+        public readonly ?GlobalMacroComment $comment,
+        public readonly bool $isPassword,
+        public readonly bool $activated,
+    ) {
+        parent::__construct($id);
+    }
+}
