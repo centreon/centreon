6c4c1b1e5107ac5212015ba7422ba857b698d2ce
Anthony Graule <agraule@centreon.com>
2025-09-16 11:38:31 +0200

    review of exception logging

--- centreon/src/Adaptation/Log/LoggerPassword.php
+++ centreon/src/Adaptation/Log/LoggerPassword.php
@@ -65,6 +65,7 @@ final class LoggerPassword
                 'initiator_user_id' => $initiatorId,
                 'target_user_id' => $targetId,
                 'ip_address' => $_SERVER['REMOTE_ADDR'] ?? 'unknown',
+                'error_message' => $exception?->getMessage() ?? null,
                 'exception' => $exception,
             ]
         );
