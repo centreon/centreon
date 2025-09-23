7ec0b50f8af4897ad89f678dd1e8174557c2f41d
Anthony Graule <agraule@centreon.com>
2025-09-23 10:13:17 +0200

    fix test

--- centreon/www/include/configuration/configObject/contact/DB-Func.php
+++ centreon/www/include/configuration/configObject/contact/DB-Func.php
@@ -1528,7 +1528,7 @@ function sanitizeFormContactParameters(array $ret): array
  *
  * @return array|true
  */
-function validatePasswordCreation(array $fields): true|array
+function validatePasswordCreation(array $fields): bool|array
 {
     global $pearDB;
     $errors = [];
