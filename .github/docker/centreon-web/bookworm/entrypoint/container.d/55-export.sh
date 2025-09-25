ecb7f360de56ba88d676df30e942a039c57a3df4
Tpo76 <fthepaut@centreon.com>
2025-09-25 09:25:15 -0400

    Adjust permissions and export configuration

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/55-export.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/55-export.sh
@@ -5,4 +5,5 @@ php /usr/share/centreon/bin/centreon -u admin -p 'Centreon2025!' -a CFGMOVE -v 1
 php /usr/share/centreon/bin/centreon -u admin -p 'Centreon2025!' -a POLLERRESTART -v 1
 
 chown -R www-data:www-data /var/cache/centreon/config
+chown -R www-data:www-data /var/cache/centreon/
 su - www-data -s /bin/bash -c "php /usr/share/centreon/bin/console cache:clear"
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 chown -R centreon:centreon /var/lib/centreon/centcore /var/lib/centreon/
-chown www-data:www-data /var/cache/centreon/symfony 
+chown www-data:www-data /var/cache/centreon/symfony chown -R www-data:www-data /var/cache/centreon/config
+chown -R www-data:www-data /var/cache/centreon/config
 
 chmod -R 775 /var/lib/centreon/centcore /var/lib/centreon/
+
+su - www-data -s /bin/bash -c "php /usr/share/centreon/bin/console cache:clear"
 # chmod 644 /etc/centreon/config.d/10-database.yaml
 #systemctl start apache2
