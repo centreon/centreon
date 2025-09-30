3e590a73b90e64a4a69bdaf1c9718bfacb7a3a3b
asalhi-ext <asalhi@ext.centreon.com>
2025-09-30 10:29:25 +0100

    fix(Meta Service): cannot disable metaservice on forms (#8332) (#8355)

--- centreon/www/include/configuration/configObject/meta_service/DB-Func.php
+++ centreon/www/include/configuration/configObject/meta_service/DB-Func.php
@@ -1058,12 +1058,12 @@ function getParamValue(
     }
 
     // Handle nested parameter (with subkey)
-    if ($subKey !== null && !empty($params[$key][$subKey])) {
+    if ($subKey !== null && (! empty($params[$key][$subKey]) || $params[$key][$subKey] == 0)) {
         return $sanitize ? sanitize($params[$key][$subKey]) : $params[$key][$subKey];
     }
 
     // Handle first-level parameter
-    if (!empty($params[$key])) {
+    if (! empty($params[$key]) || $params[$key] == 0) {
         return $sanitize ? sanitize($params[$key]) : $params[$key];
     }
 
