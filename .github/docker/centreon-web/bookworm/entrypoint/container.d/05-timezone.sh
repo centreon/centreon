b59224d99f394ac6123c2318723355621d5e2e78
Tpo76 <fthepaut@centreon.com>
2025-10-07 11:27:17 -0400

    Add TZ in php-cli

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/05-timezone.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/05-timezone.sh
@@ -13,3 +13,9 @@ fi
 PHP_INI="/etc/php/8.2/mods-available/timezone.ini"
 printf 'date.timezone = %s\n' "$TZ" > "$PHP_INI" || true
 chmod 644 "$PHP_INI" || true
+
+# write PHP timezone into PHP's conf.d so php-fpm and php-cli see it
+PHP_CONF_DIR="/usr/local/etc/php/conf.d"
+mkdir -p "$PHP_CONF_DIR"
+printf 'date.timezone = %s\n' "$TZ" > "$PHP_CONF_DIR/99-timezone.ini"
+chmod 644 "$PHP_CONF_DIR/99-timezone.ini" || true
