d6afd13bbe3120e7a9f4526bb401528b5b50a4e0
Anthony Graule <agraule@centreon.com>
2025-10-13 09:44:28 +0200

    fix backport

--- centreon/www/class/centreonUser.class.php
+++ centreon/www/class/centreonUser.class.php
@@ -117,7 +117,6 @@ class CentreonUser
         $this->default_page = $user['default_page'] ?? CentreonAuth::DEFAULT_PAGE;
         $this->gmt = $user['contact_location'] ?? null;
         $this->showDeprecatedPages = (bool) ($user['show_deprecated_pages'] ?? false);
-        $this->showDeprecatedCustomViews = (bool) ($user['show_deprecated_custom_views'] ?? false);
         $this->theme = $user['contact_theme'] ?? 'light';
         // Initiate ACL
         $this->access = new CentreonACL($this->user_id, $this->admin);
