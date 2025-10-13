7a987beb41ce3965f2ef650af3b0aa60cfe8132a
cg-tw <83637804+cg-tw@users.noreply.github.com>
2025-10-13 10:00:09 +0200

    Add missing French translations (messages.po) (#8561)
    Co-authored-by: Vincent Untz <vuntz@centreon.com>

--- centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
+++ centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
@@ -88,7 +88,7 @@ export const labelByPollerTooltip =
   'If the agent is not allowed to connect to the poller for security reasons (e.g. when the poller is in a DMZ), you can use a poller-initiated connection.';
 
 export const labelAtLeastOneConnexionMode =
-  'Au moins un des modes de connexion doit être activé.';
+  'At least one connection mode must be enabled.';
 
 export const labelSelectAtLeastOneColumn =
   'At least one column must be selected';
