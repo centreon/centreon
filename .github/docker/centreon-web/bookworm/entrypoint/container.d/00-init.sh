127dae3bcd89735ddf04e08be62808aee55bcfc2
Tpo76 <fthepaut@centreon.com>
2025-10-14 16:35:12 -0400

    Make app data dynamic and fix permissions

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/00-init.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/00-init.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 rm -f /tmp/docker.ready
+
+
+if [ "$(id -u)" = 0 ]; then
+    rsync_options="-rlDog --chown $user:$group"
+else
+    rsync_options="-rlD"
+fi
+rsync $rsync_options --delete /usr/share/centreon/ /var/www/html/centreon/
+chown -R www-data:www-data /var/www/html/centreon
