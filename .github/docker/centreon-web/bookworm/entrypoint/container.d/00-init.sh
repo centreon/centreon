037fbfd4807f0c5c31cb7e6a1ba57a939f6daa47
Tpo76 <fthepaut@centreon.com>
2025-10-14 16:49:38 -0400

    Change permission order to avoid issues with files created by www-data user

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/00-init.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/00-init.sh
@@ -2,9 +2,15 @@
 
 rm -f /tmp/docker.ready
 
+mkdir -p /etc/centreon/config.d /var/cache/centreon/
+chown -R centreon:centreon /etc/centreon
+chown -R www-data:www-data /var/cache/centreon/
+
+chmod 775 /etc/centreon/ /etc/centreon/config.d/
+
 
 if [ "$(id -u)" = 0 ]; then
-    rsync_options="-rlDog --chown $user:$group"
+    rsync_options="-rlDog --chown www-data:www-data"
 else
     rsync_options="-rlD"
 fi
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/60-permissions.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-chown -R centreon:centreon /var/lib/centreon/centcore /var/lib/centreon/
+mkdir -p /var/log/centreon/ /var/lib/centreon/centcore
+chown -R centreon:centreon /var/lib/centreon/centcore /var/lib/centreon/ /var/log/centreon/ /var/log/php8.2-fpm-centreon-error.log /var/log/centreon/centreon-web.log /etc/centreon
 chown www-data:www-data /var/cache/centreon/symfony 
 chown -R www-data:www-data /var/cache/centreon/
 
 chmod -R 775 /var/lib/centreon/centcore /var/lib/centreon/ /var/cache/centreon/
 
+touch /var/log/php8.2-fpm-centreon-error.log /var/log/centreon/centreon-web.log  
+chmod 1230 /var/log/centreon/centreon-web.log
+
 su - www-data -s /bin/bash -c "php /usr/share/centreon/bin/console cache:clear"
-# chmod 644 /etc/centreon/config.d/10-database.yaml
-#systemctl start apache2
