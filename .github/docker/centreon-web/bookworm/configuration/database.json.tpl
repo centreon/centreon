d568b60249337b83b2967a311c35f49f2d972c72
Tpo76 <fthepaut@centreon.com>
2025-10-15 16:39:01 -0400

    Create environment variable for configuration

---
+++ .github/docker/centreon-web/bookworm/configuration/database.json.tpl
@@ -0,0 +1,11 @@
+{
+  "address": "${DB_HOST}",
+  "port": "${DB_PORT}",
+  "root_user": "${DB_ROOT_USER}",
+  "root_password": "${DB_ROOT_PASSWORD}",
+  "db_configuration": "centreon",
+  "db_storage": "centreon_storage",
+  "db_user": "centreon",
+  "db_password": "${CENTREONDB_PWD}",
+  "db_password_confirm": "${CENTREONDB_PWD}"
+}
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/20-installation.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/20-installation.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 # Avoid to display mysql warning: Using a password on the command line interface can be insecure.
-: "${MYSQL_HOST:=db}"
-: "${MYSQL_PWD:=centreon}"
+: "${DB_HOST:=db}"
+: "${DB_PORT:=3306}"
+: "${DB_ROOT_USER:=root}"
+: "${DB_ROOT_PASSWORD:=centreon}"
+: "${CENTREONDB_PWD:=centreon}"
+
 
 
 INSTALL_DIR="/usr/share/centreon/www/install"
 PROCESS_DIR="$INSTALL_DIR/steps/process"
 UPGRADE_DIR="$INSTALL_DIR/step_upgrade/process/"
-if [ -f "$INSTALL_DIR/tmp/database.json" ]; then
-  sed -i "s/localhost/${MYSQL_HOST}/g" "$INSTALL_DIR/tmp/database.json"
+if [ -f "$INSTALL_DIR/tmp/database.json.tpl" ]; then
+  envsubst < "$INSTALL_DIR/tmp/database.json.tpl" > "$INSTALL_DIR/tmp/database.json"
+  envsubst < "$INSTALL_DIR/tmp/container.sql.tpl" > "$INSTALL_DIR/tmp/container.sql"
 fi
 
 # --------------------------
@@ -35,15 +40,15 @@ if [ ! -f /etc/centreon/centreon.conf.php ]; then
   su www-data -s /bin/bash -c "SERVER_ADDR='127.0.0.1' php insertBaseConf.php"
   su www-data -s /bin/bash -c "php partitionTables.php"
 
+  mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} centreon < $INSTALL_DIR/tmp/container.sql
   # Database tweaks
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "UPDATE centreon.cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "UPDATE \`centreon.cfg_centreonbroker_info\` SET \`config_value\`='cbd' WHERE \`config_id\` IN (select \`config_id\` FROM \`cfg_centreonbroker_info\` WHERE \`config_value\` = 'central-module-master-output') AND \`config_key\` = 'host'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "DELETE FROM centreon.cfg_centreonbroker_info WHERE config_group = 'output' AND config_group_id = '1'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "UPDATE centreon.cfg_centreonbroker_info SET config_id = '1', config_group_id = 1 WHERE config_id = '2' and config_group = 'output'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "DELETE FROM centreon.cfg_centreonbroker WHERE config_name = 'central-rrd-master'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "CREATE USER IF NOT EXISTS 'centreon'@'%' IDENTIFIED BY 'centreon'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "UPDATE centreon.options SET \`value\` = 'gorgone' WHERE \`key\` = 'gorgone_api_address'"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} -e "GRANT ALL ON *.* TO 'centreon'@'%'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "UPDATE centreon.cfg_centreonbroker_info SET config_value = '${DB_HOST}' WHERE config_key = 'db_host'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "DELETE FROM centreon.cfg_centreonbroker_info WHERE config_group = 'output' AND config_group_id = '1'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "UPDATE centreon.cfg_centreonbroker_info SET config_id = '1', config_group_id = 1 WHERE config_id = '2' and config_group = 'output'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "DELETE FROM centreon.cfg_centreonbroker WHERE config_name = 'central-rrd-master'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "CREATE USER IF NOT EXISTS 'centreon'@'%' IDENTIFIED BY 'centreon'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "UPDATE centreon.options SET \`value\` = 'gorgone' WHERE \`key\` = 'gorgone_api_address'"
+  # mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} -e "GRANT ALL ON *.* TO 'centreon'@'%'"
 
   # Optional dataset import
   if [ "$CENTREON_DATASET" = "1" ]; then
@@ -51,7 +56,7 @@ if [ ! -f /etc/centreon/centreon.conf.php ]; then
     DATA_DUMP_DIR="/usr/local/src/sql/data"
     for file in "$DATA_DUMP_DIR"/*; do
       echo "Importing $file ..."
-      mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} centreon < "$file"
+      mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} centreon < "$file"
     done
   fi
 
@@ -102,7 +107,7 @@ chmod +x /usr/sbin/centengine
 setAdminLanguage() {
   [ -z "$1" ] && return
   echo "Setting admin language to $1"
-  mysql -h${MYSQL_HOST} -uroot -p${MYSQL_PWD} centreon -e "UPDATE contact SET contact_lang = '$1.UTF-8' WHERE contact_alias = 'admin'"
+  mysql -h${DB_HOST} -ucentreon -p${CENTREONDB_PWD} centreon -e "UPDATE contact SET contact_lang = '$1.UTF-8' WHERE contact_alias = 'admin'"
 }
 
 case "$CENTREON_LANG" in
