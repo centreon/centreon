3ab0c9feefd6b0aea61e4dd4007d8c15f19719e0
alaunois <alaunois@centreon.com>
2025-10-17 15:47:16 +0200

    fix(export) update is_encryption_ready column type (#8626)

--- centreon/www/install/installBroker.sql
+++ centreon/www/install/installBroker.sql
@@ -408,7 +408,7 @@ CREATE TABLE `instances` (
   `version` varchar(16) DEFAULT NULL,
   `deleted` boolean NOT NULL default false,
   `outdated` boolean NOT NULL default false,
-  `is_encryption_ready` enum('0', '1') NOT NULL DEFAULT '0',
+  `is_encryption_ready` BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (`instance_id`),
   KEY `instances_name_idx` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -705,4 +705,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
