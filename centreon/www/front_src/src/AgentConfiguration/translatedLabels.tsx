b7786b8b0a81ed554cdeee84a515c36141c3078a
Med1233 <melmeziani-ext@centreon.com>
2025-10-20 17:48:19 +0100

    fix

--- centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
+++ centreon/www/front_src/src/AgentConfiguration/translatedLabels.tsx
@@ -72,6 +72,7 @@ export const labelWarningEncryptionLevelCMA =
 
 export const labelTLS = 'TLS';
 export const labelNoTLS = 'No TLS';
+export const labelInsecure = 'Insecure TLS';
 
 export const labelCMAauthenticationToken = 'CMA authentication token(s)';
 export const labelSelectExistingCMATokens = 'Select existing CMA token(s)';
