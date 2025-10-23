86aa5c47e989e3943bbf9ffa323faf8fcbefd4d7
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2025-10-23 17:49:46 +0200

    Fix(UI): page content mix up when navigating between host groups and acc pages (#8543)
    Co-authored-by: Med1233 <melmeziani-ext@centreon.com>

--- centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/AdvancedFilters/Fields/Status/useStatus.ts
+++ centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/AdvancedFilters/Fields/Status/useStatus.ts
@@ -1,7 +1,5 @@
-import { useAtom } from 'jotai';
-import { ChangeEvent } from 'react';
-
-import { filtersAtom } from '../../../../atoms';
+import { SetStateAction } from 'jotai';
+import { ChangeEvent, Dispatch } from 'react';
 
 interface UseStatus {
   change: (name: string) => (event: ChangeEvent<HTMLInputElement>) => void;
@@ -9,9 +7,14 @@ interface UseStatus {
   valueDisable: boolean;
 }
 
-const useStatus = (): UseStatus => {
-  const [filters, setFilters] = useAtom(filtersAtom);
-
+interface Props<TFilters> {
+  filters: TFilters;
+  setFilters: Dispatch<SetStateAction<TFilters>>;
+}
+const useStatus = <TFilters>({
+  filters,
+  setFilters
+}: Props<TFilters>): UseStatus => {
   const change =
     (name: string) =>
     (event: ChangeEvent<HTMLInputElement>): void => {
