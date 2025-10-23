0f0314a1d7f2cb7e47f176560e098f8a286f64fb
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2025-10-23 17:49:46 +0200

    Fix(UI): page content mix up when navigating between host groups and acc pages (#8543)
    Co-authored-by: Med1233 <melmeziani-ext@centreon.com>

--- centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/SearchBar.tsx
+++ centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/SearchBar.tsx
@@ -1,3 +1,6 @@
+import { PrimitiveAtom } from 'jotai';
+import { ReactElement, useMemo } from 'react';
+
 import { useTranslation } from 'react-i18next';
 
 import { SearchField } from '@centreon/ui';
@@ -8,18 +11,39 @@ import useSearch from './useSearch';
 
 import AdvancedFilters from './AdvancedFilters';
 
-const Filters = (): JSX.Element => {
+interface Props<TFilters> {
+  filtersAtom: PrimitiveAtom<TFilters>;
+  filtersAtomKey: string;
+}
+
+const Filters = <TFilters,>({
+  filtersAtom,
+  filtersAtomKey
+}: Props<TFilters>): ReactElement => {
   const { classes } = useFilterStyles();
   const { t } = useTranslation();
 
-  const { filters, onChange, areAdvancedFiltersVisible } = useSearch();
+  const { filters, onChange, areAdvancedFiltersVisible } = useSearch<TFilters>({
+    filtersAtom
+  });
+
+  const EndAdornment = useMemo(
+    () => () => (
+      <AdvancedFilters<TFilters>
+        filtersAtomKey={filtersAtomKey}
+        filtersAtom={filtersAtom}
+        areAdvancedFiltersVisible={areAdvancedFiltersVisible}
+      />
+    ),
+    [areAdvancedFiltersVisible, filtersAtom]
+  );
 
   return (
     <div className={classes.filters}>
       <SearchField
         debounced
         fullWidth
-        EndAdornment={areAdvancedFiltersVisible && AdvancedFilters}
+        EndAdornment={EndAdornment}
         dataTestId={'search-bar'}
         placeholder={t(labelSearch)}
         value={filters.name}
