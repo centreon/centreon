8d3c38bb943d0664ba79d6a017a8dc8cc13a0714
asalhi-ext <asalhi@ext.centreon.com>
2025-10-24 09:39:10 +0100

    fix(Traps Groups): enh form s queries

--- centreon/www/include/configuration/configObject/traps-groups/formGroups.php
+++ centreon/www/include/configuration/configObject/traps-groups/formGroups.php
@@ -19,23 +19,53 @@
  *
  */
 
+use Adaptation\Database\Connection\Collection\QueryParameters;
+use Adaptation\Database\Connection\Enum\QueryParameterTypeEnum;
+use Adaptation\Database\Connection\ValueObject\QueryParameter;
+use Core\Common\Domain\Exception\ExceptionFormatter;
+use Core\Common\Domain\Exception\RepositoryException;
+
 //
 // # Database retrieve information for Manufacturer
 //
 
-function myDecodeGroup($arg)
+function myDecodeGroup(?string $arg): string
 {
     return html_entity_decode($arg ?? '', ENT_QUOTES, 'UTF-8');
 }
 
 $group = [];
-if (($o == 'c' || $o == 'w') && $id) {
-    $query = 'SELECT traps_group_name as name, traps_group_id as id FROM traps_group '
-        . "WHERE traps_group_id = '" . $pearDB->escape($id) . "' LIMIT 1";
-    $DBRESULT = $pearDB->query($query);
-    // Set base value
-    $group = array_map('myDecodeGroup', $DBRESULT->fetchRow());
-    $DBRESULT->closeCursor();
+
+try {
+    if (($o === 'c' || $o === 'w') && $id) {
+        $sql = <<<'SQL'
+                SELECT traps_group_name AS name, traps_group_id AS id
+                FROM traps_group
+                WHERE traps_group_id = :id
+                LIMIT 1
+            SQL;
+
+        $params = QueryParameters::create([
+            QueryParameter::create('id', (int) $id, QueryParameterTypeEnum::INTEGER),
+        ]);
+
+        $result = $pearDB->fetchAssociative($sql, $params);
+
+        if ($result !== false) {
+            $group = array_map('myDecodeGroup', $result);
+        }
+    }
+} catch (RepositoryException $exception) {
+    CentreonLog::create()->error(
+        CentreonLog::TYPE_SQL,
+        'Error while retrieving trap group data: ' . $exception->getMessage(),
+        exception: $exception
+    );
+
+    $msg = new CentreonMsg();
+    $msg->setImage('./img/icons/warning.png');
+    $msg->setTextStyle('bold');
+    $msg->setText('Error while retrieving trap group data');
 }
 
 // #########################################################
@@ -43,15 +73,17 @@ if (($o == 'c' || $o == 'w') && $id) {
 //
 $attrsText = ['size' => '50'];
 $attrsTextarea = ['rows' => '5', 'cols' => '40'];
+
 //
 // # Form begin
 //
 $form = new HTML_QuickFormCustom('Form', 'post', '?p=' . $p);
-if ($o == 'a') {
+
+if ($o === 'a') {
     $form->addElement('header', 'title', _('Add Group'));
-} elseif ($o == 'c') {
+} elseif ($o === 'c') {
     $form->addElement('header', 'title', _('Modify Group'));
-} elseif ($o == 'w') {
+} elseif ($o === 'w') {
     $form->addElement('header', 'title', _('View Group'));
 }
 
@@ -63,7 +95,15 @@ $form->addElement('text', 'name', _('Name'), $attrsText);
 $avRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_trap&action=list';
 $deRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_trap'
     . '&action=defaultValues&target=Traps&field=groups&id=' . $id;
-$attrTraps = ['datasourceOrigin' => 'ajax', 'availableDatasetRoute' => $avRoute, 'multiple' => true, 'linkedObject' => 'centreonTraps', 'defaultDatasetRoute' => $deRoute];
+
+$attrTraps = [
+    'datasourceOrigin' => 'ajax',
+    'availableDatasetRoute' => $avRoute,
+    'multiple' => true,
+    'linkedObject' => 'centreonTraps',
+    'defaultDatasetRoute' => $deRoute,
+];
+
 $form->addElement('select2', 'traps', _('Traps'), [], $attrTraps);
 
 //
@@ -83,10 +123,8 @@ $form->addRule('name', _('Name is already in use'), 'exist');
 $form->setRequiredNote("<font style='color: red;'>*</font>&nbsp;" . _('Required fields'));
 
 //
-// #End of form definition
+// # Smarty template initialization
 //
-
-// Smarty template initialization
 $tpl = SmartyBC::createSmartyTemplate($path);
 
 $tpl->assign(
@@ -104,9 +142,8 @@ foreach ($help as $key => $text) {
 }
 $tpl->assign('helptext', $helptext);
 
-// Just watch a Trap Group information
-if ($o == 'w') {
-    if ($centreon->user->access->page($p) != 2) {
+if ($o === 'w') {
+    if ($centreon->user->access->page($p) !== 2) {
         $form->addElement(
             'button',
             'change',
@@ -116,15 +153,13 @@ if ($o == 'w') {
     }
     $form->setDefaults($group);
     $form->freeze();
-} // Modify a Trap Group information
-elseif ($o == 'c') {
-    $subC = $form->addElement('submit', 'submitC', _('Save'), ['class' => 'btc bt_success']);
-    $res = $form->addElement('reset', 'reset', _('Reset'), ['class' => 'btc bt_default']);
+} elseif ($o === 'c') {
+    $form->addElement('submit', 'submitC', _('Save'), ['class' => 'btc bt_success']);
+    $form->addElement('reset', 'reset', _('Reset'), ['class' => 'btc bt_default']);
     $form->setDefaults($group);
-} // Add a Trap Group information
-elseif ($o == 'a') {
-    $subA = $form->addElement('submit', 'submitA', _('Save'), ['class' => 'btc bt_success']);
-    $res = $form->addElement('reset', 'reset', _('Reset'), ['class' => 'btc bt_default']);
+} elseif ($o === 'a') {
+    $form->addElement('submit', 'submitA', _('Save'), ['class' => 'btc bt_success']);
+    $form->addElement('reset', 'reset', _('Reset'), ['class' => 'btc bt_default']);
 }
 
 $valid = false;
