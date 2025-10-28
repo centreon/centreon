0e90be782384f608232e497b172a72cb9975f7be
Kevin Duret <kduret@centreon.com>
2025-10-28 15:27:28 +0100

    fix(packaging): wait php 8.2 configuration before cli usage

--- centreon/packaging/scripts/centreon-web-posttrans.sh
+++ centreon/packaging/scripts/centreon-web-posttrans.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
 rebuildSymfonyCache() {
-  if [ "$1" = "rpm" ]; then
+  if command -v rpm &> /dev/null; then
     echo "Rebuilding Centreon application cache ..."
     rm -rf /var/cache/centreon/symfony
     su - apache -s /bin/bash -c "/usr/share/centreon/bin/console cache:clear"
   fi
 }
 
-package_type="rpm"
-if  [ "$1" = "configure" ]; then
-  package_type="deb"
+if [ -f /etc/centreon/centreon.conf.php ]; then
+  rebuildSymfonyCache
 fi
-
-action="$1"
-if  [ "$1" = "configure" ] && [ -z "$2" ]; then
-  # Alpine linux does not pass args, and deb passes $1=configure
-  action="install"
-elif [ "$1" = "configure" ] && [ -n "$2" ]; then
-  # deb passes $1=configure $2=<current version>
-  action="upgrade"
-fi
-
-case "$action" in
-  "2" | "upgrade")
-    rebuildSymfonyCache $package_type
-    ;;
-esac
