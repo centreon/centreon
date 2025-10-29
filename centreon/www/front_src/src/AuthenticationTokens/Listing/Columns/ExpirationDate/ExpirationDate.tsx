745618fb714570aa6045ede6b0e1744ce18ea40f
Med1233 <melmeziani-ext@centreon.com>
2025-10-29 17:22:51 +0100

    fix

--- centreon/www/front_src/src/AuthenticationTokens/Listing/Columns/ExpirationDate/ExpirationDate.tsx
+++ centreon/www/front_src/src/AuthenticationTokens/Listing/Columns/ExpirationDate/ExpirationDate.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 import { labelNeverExpire } from '../../../translatedLabels';
 import useStyles from './ExpirationDate.styles';
 
-const dateFormat = 'L';
+const dateFormat = 'MM/DD/YYYY HH:mm';
 
 const ExpirationDate = ({
   row,
