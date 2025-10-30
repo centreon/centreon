2490802617f615c4e1b9f70fb92a2a3499c50261
alaunois <alaunois@centreon.com>
2025-10-30 21:45:31 +0100

    fix(cfg) fix herited host macro update (#8759) (#8767)

--- centreon/src/Core/Host/Application/UseCase/PartialUpdateHost/HostMacroFactory.php
+++ centreon/src/Core/Host/Application/UseCase/PartialUpdateHost/HostMacroFactory.php
@@ -49,9 +49,8 @@ final class HostMacroFactory
         array $inheritedMacros,
     ): Macro {
         $macroName = mb_strtoupper($data['name']);
-        $macroValue = self::computeValue($data, $inheritedMacros);
-        $passwordHasNotChanged = (null === $data['value']) && $data['is_password'];
-        // Note: do not handle vault storage at the moment
+        $macroValue = $data['value'] ?? '';
+        $passwordHasNotChanged = ($data['value'] === null) && $data['is_password'];
         if ($passwordHasNotChanged) {
             $macroValue = match (true) {
                 // retrieve actual password value
@@ -70,22 +69,5 @@ final class HostMacroFactory
         $macro->setDescription($data['description'] ?? '');
 
         return $macro;
-    }
-
-    /**
-     * Compute macro value based on the data and inherited macros.
-     *
-     * @param array{name:string,value:string|null,is_password:bool,description:string|null} $data
-     * @param array<string,Macro> $inheritedMacros
-     *
-     * @return string
-     */
-    private static function computeValue(array $data, array $inheritedMacros): string
-    {
-        if (array_key_exists($data['name'], $inheritedMacros)) {
-            return $inheritedMacros[$data['name']]->getValue();
-        }
-
-        return $data['value'] ?? '';
     }
 }
