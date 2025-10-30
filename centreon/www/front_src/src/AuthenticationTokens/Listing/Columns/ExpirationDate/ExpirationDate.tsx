7867d76dd0edea109dbae850bbb0032a889c246c
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2025-10-30 10:46:12 +0100

    Update centreon/www/front_src/src/AuthenticationTokens/Listing/Columns/ExpirationDate/ExpirationDate.tsx
    Co-authored-by: Drokha <45570362+Drokha@users.noreply.github.com>

--- centreon/www/front_src/src/AuthenticationTokens/Listing/Columns/ExpirationDate/ExpirationDate.tsx
+++ centreon/www/front_src/src/AuthenticationTokens/Listing/Columns/ExpirationDate/ExpirationDate.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 import { labelNeverExpire } from '../../../translatedLabels';
 import useStyles from './ExpirationDate.styles';
 
-const dateFormat = 'MM/DD/YYYY HH:mm';
+const dateFormat = 'Pp';
 
 const ExpirationDate = ({
   row,
