927cbab28d757c0b489b7397953500dd70b25d54
s-elhassani <selhassani-ext@centreon.com>
2025-11-03 17:23:21 +0100

    add some automated tests for services macros

--- centreon/tests/e2e/features/Macros/02-services-macros/index.ts
+++ centreon/tests/e2e/features/Macros/02-services-macros/index.ts
@@ -3,12 +3,12 @@ import serviceMacros from '../../../fixtures/macros/services.json';
 
 const clickToAddService = () => {
   cy.waitForElementInIframe('#main-content', 'a:contains("Add")');
-  cy.getIframeBody().contains('a', "Add").click();
+  cy.getIframeBody().contains('a', 'Add').click();
   cy.waitForElementInIframe(
     '#main-content',
     'input[name="service_description"]'
   );
-}
+};
 
 before(() => {
   cy.startContainers();
@@ -30,7 +30,7 @@ beforeEach(() => {
     method: 'GET',
     url: '/centreon/include/common/webServices/rest/internal.php?object=centreon_configuration_host&*'
   }).as('getHostsList');
-   cy.intercept({
+  cy.intercept({
     method: 'GET',
     url: '/centreon/api/latest/configuration/monitoring-servers/generate-and-reload'
   }).as('exportConf');
@@ -48,12 +48,7 @@ Given('a non-admin user is logged into the Centreon server', () => {
 });
 
 Given('the non-admin user is on the "Configuration > services" page', () => {
-  cy.navigateTo({
-    page: 'Services by host',
-    rootItemNumber: 0,
-    subMenu: 'Services'
-  });
-  cy.wait('@getTimeZone');
+  cy.visitServicesListing(0);
 });
 
 When('the non-admin user clicks to add a new service', () => {
@@ -61,17 +56,11 @@ When('the non-admin user clicks to add a new service', () => {
 });
 
 When('the non-admin user fills in all mandatory fields', () => {
-  cy.getIframeBody()
-    .find('input[name="service_description"]')
-    .clear()
-    .type(serviceMacros.default_service.name);
-  cy.getIframeBody().find('input[placeholder="Hosts"]').click();
-  cy.wait('@getHostsList');
-  cy.getIframeBody()
-    .contains('div', serviceMacros.default_service.host)
-    .click();
-  cy.getIframeBody().find('span[title="Check Command"]').click();
-  cy.getIframeBody().contains('div', serviceMacros.default_service.cmd).click();
+  cy.fillServiceMandatoryField(
+    serviceMacros.default_service.name,
+    serviceMacros.default_service.host,
+    serviceMacros.default_service.cmd
+  );
 });
 
 When('the non-admin user adds one normal macro and one password macro', () => {
@@ -227,45 +216,51 @@ Then('the macros are removed from the file {string}', (fileName: string) => {
 Given(
   'a non-admin user is on the "Configuration > services > Templates" page',
   () => {
-    cy.navigateTo({
-      page: 'Templates',
-      rootItemNumber: 0,
-      subMenu: 'Services'
-    });
-    cy.wait('@getTimeZone');
+    cy.visitServiceTemplatesListing(0);
   }
 );
 
-Given('a service Template {string} exists with defined normal and password macros', (name: string) => {
-  clickToAddService();
-  // Fill mandatory fields
-  cy.getIframeBody()
-    .find('input[name="service_description"]')
-    .clear()
-    .type(name);
-  cy.getIframeBody()
-    .find('input[name="service_alias"]')
-    .clear()
-    .type(name);
-  // Fill Service Template Macros (one normal, one of type password)
-  cy.fillMacros(false, serviceMacros.default_service.normalMacro, serviceMacros.default_service.passMacro);
-  // Save the configuration
-  cy.getIframeBody()
+Given(
+  'a service Template {string} exists with defined normal and password macros',
+  (name: string) => {
+    clickToAddService();
+    // Fill mandatory fields
+    cy.getIframeBody()
+      .find('input[name="service_description"]')
+      .clear()
+      .type(name);
+    cy.getIframeBody().find('input[name="service_alias"]').clear().type(name);
+    // Fill Service Template Macros (one normal, one of type password)
+    cy.fillMacros(
+      false,
+      serviceMacros.default_service.normalMacro,
+      serviceMacros.default_service.passMacro
+    );
+    // Save the configuration
+    cy.getIframeBody()
       .find('input.btc.bt_success[name^="submit"]')
       .eq(0)
       .click();
-  cy.wait('@getTimeZone');
-  // Wait until the service template is charged on the DOM
-  cy.waitForElementInIframe('#main-content', `a:contains(${name})`);
-});
+    cy.wait('@getTimeZone');
+    // Wait until the service template is charged on the DOM
+    cy.waitForElementInIframe('#main-content', `a:contains(${name})`);
+  }
+);
 
-Given('a pre-configured service using {string} as its parent template', (parent: string) => {
-  cy.waitForElementInIframe(
+Given(
+  'a pre-configured service using {string} as its parent template',
+  (parent: string) => {
+    cy.waitForElementInIframe(
       '#main-content',
       `a:contains(${serviceMacros.default_service.name})`
     );
-    cy.getIframeBody().contains('a', serviceMacros.default_service.name).click();
-    cy.waitForElementInIframe('#main-content', 'input[name="service_description"]');
+    cy.getIframeBody()
+      .contains('a', serviceMacros.default_service.name)
+      .click();
+    cy.waitForElementInIframe(
+      '#main-content',
+      'input[name="service_description"]'
+    );
     // Add the service template to the service
     cy.getIframeBody().find('span[role="presentation"]').eq(1).click();
     cy.getIframeBody().find(`div[title="${parent}"]`).click();
@@ -280,19 +275,25 @@ Given('a pre-configured service using {string} as its parent template', (parent:
       '#main-content',
       `a:contains(${serviceMacros.default_service.name})`
     );
-});
+  }
+);
 
-Then('the macros should be stored in the service Template configuration file {string}', (file: string) => {
-   cy.checkMacrosFromTheExportFile(
+Then(
+  'the macros should be stored in the service Template configuration file {string}',
+  (file: string) => {
+    cy.checkMacrosFromTheExportFile(
       file,
       false,
       serviceMacros.default_service.normalMacro,
       serviceMacros.default_service.passMacro
     );
-});
+  }
+);
 
-Then('the service configuration file {string} should not contain the inherited macros', (file: string) => {
-  cy.execInContainer({
+Then(
+  'the service configuration file {string} should not contain the inherited macros',
+  (file: string) => {
+    cy.execInContainer({
       command: `cat ${file}`,
       name: 'web'
     }).then((result) => {
@@ -307,7 +308,171 @@ Then('the service configuration file {string} should not contain the inherited m
       );
       expect(output).not.to.match(regexPassword);
     });
-})
+  }
+);
 
+Given(
+  'a pre-configured Service Template {string} that contains defined macros',
+  (name: string) => {
+    cy.waitForElementInIframe('#main-content', `a:contains("${name}")`);
+  }
+);
 
+When(
+  'the non-admin user creates a new Service Template {string} with {string} as its parent',
+  (child: string, parent: string) => {
+    clickToAddService();
+    // Fill mandatory fields
+    cy.getIframeBody()
+      .find('input[name="service_description"]')
+      .clear()
+      .type(child);
+    cy.getIframeBody().find('input[name="service_alias"]').clear().type(child);
+    // Add the parent service template to the child service
+    cy.getIframeBody().find('span[role="presentation"]').eq(0).click();
+    cy.getIframeBody().find(`div[title="${parent}"]`).click();
+  }
+);
 
+When(
+  'he changes the value of the normal macro inherited from Service Template "ST-A"',
+  () => {
+    // Check first that the inherited macros are visible
+    [0, 1].forEach((index) => {
+      cy.getIframeBody().find(`#macroInput_${index}`).should('be.visible');
+    });
+    // Check that the inherited macros are highlighted in orange
+    [0, 1].forEach((index) => {
+      cy.getIframeBody()
+        .find(`#macroInput_${index}`)
+        .should('have.attr', 'style')
+        .and('include', 'var(--custom-macros-template-background-color)');
+    });
+    // Now change the normal macro value
+    cy.getIframeBody()
+      .find('#macroValue_0')
+      .clear()
+      .type(`${serviceMacros.updated_service.normalMacro.value}`);
+  }
+);
+
+Then(
+  'the normal macro value in {string} should be the modified value',
+  (name: string) => {
+    cy.waitForElementInIframe('#main-content', `a:contains(${name})`);
+    cy.getIframeBody().contains('a', name).click();
+    cy.waitForElementInIframe(
+      '#main-content',
+      'input[name="service_description"]'
+    );
+    cy.getIframeBody()
+      .find('#macroValue_0')
+      .should(
+        'have.value',
+        `${serviceMacros.updated_service.normalMacro.value}`
+      );
+  }
+);
+
+Then('the normal macro should not be highlighted in orange', () => {
+  cy.getIframeBody().find('#macroInput_0').should('not.have.attr', 'style');
+});
+
+Then('the password macro should still be highlighted in orange', () => {
+  cy.getIframeBody()
+    .find('#macroInput_1')
+    .should('have.attr', 'style')
+    .and('include', 'var(--custom-macros-template-background-color)');
+});
+
+When(
+  'the user creates a new Service {string} using {string} as its parent template',
+  (child: string, parent: string) => {
+    cy.visitServicesListing(0);
+    clickToAddService();
+    cy.fillServiceMandatoryField(
+      child,
+      serviceMacros.default_service.host,
+      serviceMacros.default_service.cmd
+    );
+    // Add the parent service template to the child service
+    cy.getIframeBody().find('span[role="presentation"]').eq(1).click();
+    cy.getIframeBody().find(`div[title="${parent}"]`).click();
+  }
+);
+
+Then(
+  'the macro values in Service Template {string} should remain unchanged',
+  (name: string) => {
+    cy.visitServicesListing(0);
+    cy.waitForElementInIframe('#main-content', `a:contains(${name})`);
+    cy.getIframeBody().contains('a', name).click();
+    cy.waitForElementInIframe(
+      '#main-content',
+      'input[name="service_description"]'
+    );
+    cy.getIframeBody()
+      .find('#macroValue_0')
+      .should(
+        'have.value',
+        `${serviceMacros.default_service.normalMacro.value}`
+      );
+  }
+);
+
+Given(
+  'a pre-configured Service using a service template with defined macros as its parent template',
+  () => {
+    cy.waitForElementInIframe(
+      '#main-content',
+      `a:contains(${serviceMacros.default_service.name})`
+    );
+  }
+);
+
+When(
+  'the normal macro value in the service should be the modified value',
+  () => {
+    cy.waitForElementInIframe(
+      '#main-content',
+      `a:contains(${serviceMacros.default_service.name})`
+    );
+    cy.getIframeBody()
+      .contains('a', serviceMacros.default_service.name)
+      .click();
+    cy.waitForElementInIframe(
+      '#main-content',
+      'input[name="service_description"]'
+    );
+    cy.getIframeBody()
+      .find('#macroValue_0')
+      .should(
+        'have.value',
+        `${serviceMacros.updated_service.normalMacro.value}`
+      );
+  }
+);
+
+Then(
+  'the new value of the inherited normal macro is exported to the file {string}',
+  (file: string) => {
+    cy.checkMacrosFromTheExportFile(
+      file,
+      true,
+      serviceMacros.updated_service.normalMacro,
+      serviceMacros.updated_service.passMacro
+    );
+  }
+);
+
+Then(
+  'the old values of macros are exported to the service template file {string}',
+  (file: string) => {
+    cy.checkMacrosFromTheExportFile(
+      file,
+      false,
+      serviceMacros.default_service.normalMacro,
+      serviceMacros.default_service.passMacro
+    );
+  }
+);
