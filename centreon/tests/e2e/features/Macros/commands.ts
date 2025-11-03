3af6fa1540066b3f67b4c487a7834fa2147d9e0c
s-elhassani <selhassani-ext@centreon.com>
2025-11-03 15:50:01 +0100

    Automated regression tests on macros (first steps)

---
+++ centreon/tests/e2e/features/Macros/commands.ts
@@ -0,0 +1,109 @@
+Cypress.Commands.add(
+  'fillMacros',
+  (isUpdate: boolean, normalMacro: Macro, passMacro: Macro) => {
+    if (!isUpdate) {
+      cy.getIframeBody().find('#macro_add').click();
+      cy.getIframeBody().find('#macro_add').click();
+      cy.getIframeBody().find('#macroInput_0').clear().type(normalMacro.name);
+      cy.getIframeBody().find('#macroInput_1').clear().type(passMacro.name);
+    }
+    // Add/Update a normal macro
+    cy.getIframeBody().find('#macroValue_0').clear().type(normalMacro.value);
+    // Add/Update a macro of type password
+    cy.getIframeBody().find('#macroValue_1').clear().type(passMacro.value);
+    if (!isUpdate) {
+      cy.getIframeBody().find('#macroPassword_1').click({ force: true });
+    }
+  }
+);
+
+Cypress.Commands.add(
+  'checkMacrosFieldsValues',
+  (normalMacro: Macro, passMacro: Macro) => {
+    // Verify the save of the macros
+    cy.getIframeBody()
+      .find('#macroInput_0')
+      .should('have.value', normalMacro.name);
+    cy.getIframeBody()
+      .find('#macroValue_0')
+      .should('have.value', normalMacro.value);
+
+    cy.getIframeBody()
+      .find('#macroInput_1')
+      .should('have.value', passMacro.name);
+    // Check that the value of the password macro contains just *
+    cy.getIframeBody()
+      .find('#macroValue_1')
+      .invoke('val')
+      .then((value) => {
+        expect(value).to.not.be.empty;
+        expect(value).to.match(/^\*+$/);
+      });
+  }
+);
+
+Cypress.Commands.add('fillHostBasicsInfos', (name: string, alias: string) => {
+  cy.getIframeBody().find('input[name="host_name"]').clear().type(name);
+  cy.getIframeBody().find('input[name="host_alias"]').clear().type(alias);
+});
+
+Cypress.Commands.add(
+  'checkMacrosFromTheExportFile',
+  (
+    fileName: string,
+    isInherited: boolean,
+    normalMacro: Macro,
+    passMacro: Macro
+  ) => {
+    cy.execInContainer({
+      command: `cat ${fileName}`,
+      name: 'web'
+    }).then((result) => {
+      expect(result.exitCode).to.eq(0);
+      const output = result.output;
+      const regexNormal = new RegExp(
+        `${normalMacro.name}\\s+raw::${normalMacro.value}`
+      );
+      expect(output).to.match(regexNormal);
+      const regexPassword = new RegExp(
+        `${passMacro.name}\\s+encrypt::[A-Za-z0-9+/=]+`
+      );
+      if (!isInherited) {
+        expect(output).to.match(regexPassword);
+      } else {
+        expect(output).not.to.match(regexPassword);
+      }
+    });
+  }
+);
+
+interface Macro {
+  name: string;
+  value: string;
+}
+
+declare global {
+  // biome-ignore lint/style/noNamespace: <explanation>
+  namespace Cypress {
+    interface Chainable {
+      fillMacros: (
+        isUpdate: boolean,
+        normalMacro: Macro,
+        passMacro: Macro
+      ) => Cypress.Chainable;
+      checkMacrosFieldsValues: (
+        normalMacro: Macro,
+        passMacro: Macro
+      ) => Cypress.Chainable;
+      fillHostBasicsInfos: (name: string, alias: string) => Cypress.Chainable;
+      checkMacrosFromTheExportFile: (
+        fileName,
+        isInherited: boolean,
+        normalMacro: Macro,
+        passMacro: Macro
+      ) => Cypress.Chainable;
+    }
+  }
+}
+
+export {};
