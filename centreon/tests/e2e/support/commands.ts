f37ca863272fa79ac19fa767d77a7ed8c2144c9d
MedWassCh <144020537+MedWassCh@users.noreply.github.com>
2025-11-06 16:23:08 +0100

    fix(E2E): enhance afterEach cleanup and import additional commands (#8830)

--- centreon/tests/e2e/support/commands.ts
+++ centreon/tests/e2e/support/commands.ts
@@ -19,6 +19,7 @@ import '../features/Commands/commands';
 import '../features/Resources-status/commands';
 import '../features/Platform-upgrade-update/commands';
 import '../features/Additional-connectors/commands';
+import '../../../../centreon/packages/js-config/cypress/e2e/commands'
 
 
 Cypress.Commands.add('refreshListing', (): Cypress.Chainable => {
