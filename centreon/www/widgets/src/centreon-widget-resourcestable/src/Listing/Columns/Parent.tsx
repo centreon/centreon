f033ab0aacc23063a5262eab8b52b319bc27ee29
Med1233 <melmeziani-ext@centreon.com>
2025-11-09 23:31:57 +0100

    fix parent

--- centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/Columns/Parent.tsx
+++ centreon/www/widgets/src/centreon-widget-resourcestable/src/Listing/Columns/Parent.tsx
@@ -1,12 +1,16 @@
-import { type ComponentColumnProps, EllipsisTypography } from '@centreon/ui';
+import { type ComponentColumnProps } from '@centreon/ui';
 
 import { getStatus } from '../utils';
 
+import { useColumnStyles } from './Columns.styles';
 import StatusChip from './ServiceSubItemColumn/StatusChip';
 
 const ParentResourceColumn = ({
-  row
+  row,
+  renderEllipsisTypography
 }: ComponentColumnProps): JSX.Element | null => {
+  const { classes } = useColumnStyles();
+
   const status = row?.parent?.status?.name;
 
   if (!row.parent) {
@@ -15,15 +19,16 @@ const ParentResourceColumn = ({
 
   return (
     <>
-      <div className="flex items-center flex-nowrap">
+      <div className={classes.resourceDetailsCell}>
         <StatusChip
           content={getStatus(status?.toLowerCase())?.label}
           severityCode={getStatus(status?.toLowerCase())?.severity}
         />
       </div>
-      <EllipsisTypography className="pl-1" variant="body2">
-        {row.parent?.name || ''}
-      </EllipsisTypography>
+      {renderEllipsisTypography?.({
+        className: classes.resourceNameText,
+        formattedString: row.parent?.name || ''
+      })}
     </>
   );
 };
