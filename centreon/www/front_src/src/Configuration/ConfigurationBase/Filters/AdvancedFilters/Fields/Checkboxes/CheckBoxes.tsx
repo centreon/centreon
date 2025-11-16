ebb79fa64b83fa045ea55d1ffd5d41d9796bb0d5
Med1233 <melmeziani-ext@centreon.com>
2025-11-16 21:11:24 +0100

    go again

--- centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/AdvancedFilters/Fields/Checkboxes/CheckBoxes.tsx
+++ centreon/www/front_src/src/Configuration/ConfigurationBase/Filters/AdvancedFilters/Fields/Checkboxes/CheckBoxes.tsx
@@ -10,7 +10,7 @@ import { useTranslation } from 'react-i18next';
 
 import { useFilterStyles } from '../../../Filters.styles';
 
-import useCheckboxes from './useCheckBoxes';
+import useCheckboxes from './useCheckboxes';
 
 interface Props<TFilters> {
   name: string;
