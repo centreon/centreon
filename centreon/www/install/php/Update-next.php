d7b82649b953517705bc179ab467c4dab8cf340a
selfakiri <selfakiri@ext.centreon.com>
2025-11-17 14:53:52 +0100

    feat(additional configuration): de-vault username (#8857)

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -23,6 +23,9 @@ use Adaptation\Database\Connection\Collection\QueryParameters;
 use Adaptation\Database\Connection\ConnectionInterface;
 use Adaptation\Database\Connection\Exception\ConnectionException;
 use Adaptation\Database\Connection\ValueObject\QueryParameter;
+use App\Kernel;
+use Core\Security\Vault\Domain\Model\VaultConfiguration;
+use Security\Interfaces\EncryptionInterface;
 
 require_once __DIR__ . '/../../../bootstrap.php';
 
@@ -34,7 +37,6 @@ $errorMessage = '';
  * @var ConnectionInterface $pearDB
  * @var ConnectionInterface $pearDBO
  */
-
 // -------------------------------------- AgentConfiguration updates --------------------------------------
 
 /**
@@ -587,10 +589,82 @@ $addIsEncryptionReadyAsBooleanColumn = function () use ($pearDB, $pearDBO, &$err
     }
 };
 
-try {
-    // DDL statements for real time database
-    // TODO add your function calls to update the real time database structure here
+$migrateAccUsernamesFromVault = function () use ($pearDB, &$errorMessage, $version): void {
+    $errorMessage = 'Failed to migrate Additional Configuration usernames from vault';
+    $secondKey = 'additional_connector_configuration_vmware_v6';
 
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: migrating Additional Configuration usernames from vault"
+    );
+
+    $kernel = Kernel::createForWeb();
+    $container = $kernel->getContainer();
+    $encryption = $container->get(EncryptionInterface::class);
+    $encryption->setSecondKey($secondKey);
+
+    // Retrieve all VMWARE_V6 Additional Configurations
+    $accs = $pearDB->fetchAllAssociative(
+        <<<'SQL'
+            SELECT id, name, type, parameters
+            FROM additional_connector_configuration
+            WHERE type = 'vmware_v6'
+            SQL
+    );
+
+    // If no ACC found, skip migration
+    if ($accs === []) {
+        CentreonLog::create()->info(
+            logTypeId: CentreonLog::TYPE_UPGRADE,
+            message: "UPGRADE - {$version}: no VMWARE_V6 Additional Configurations found, skipping username migration"
+        );
+
+        return;
+    }
+
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: found " . count($accs) . ' VMWARE_V6 Additional Configurations, checking for usernames in vault...'
+    );
+
+    foreach ($accs as $acc) {
+        $parameters = json_decode($acc['parameters'], true, 512, JSON_THROW_ON_ERROR);
+        $updated = false;
+
+        // Retrieve and decrypt usernames from vault
+        foreach ($parameters['vcenters'] as $index => $vcenter) {
+            $parameters['vcenters'][$index]['username'] = str_starts_with(
+                $vcenter['username'],
+                VaultConfiguration::VAULT_PATH_PATTERN
+            ) ? $vcenter['username'] : $encryption->decrypt($vcenter['username']) ?? '';
+
+            $updated = true;
+        }
+
+        // Update ACC in DB
+        if ($updated) {
+            $pearDB->update(
+                <<<'SQL'
+                    UPDATE additional_connector_configuration
+                    SET parameters = :parameters, updated_at = :updatedAt
+                    WHERE id = :id
+                    SQL,
+                QueryParameters::create([
+                    QueryParameter::string(':parameters', json_encode($parameters, JSON_THROW_ON_ERROR)),
+                    QueryParameter::int(':updatedAt', time()),
+                    QueryParameter::int(':id', $acc['id']),
+                ])
+            );
+        }
+    }
+
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: ACC usernames migrated from vault successfully"
+    );
+};
+
+try {
     // DDL statements for configuration database
     $bbdoDefaultUpdate();
     $addOpentelemetryLogLevelColumn();
@@ -601,13 +675,13 @@ try {
         $pearDB->startTransaction();
     }
 
-    // TODO add your function calls to update the configuration database data here
     $alignCMAAgentConfigurationWithNewSchema();
     $cleanGlobalMacrosName();
     $fixTypoInStandardMacroName();
     $fixBrokerConfigTypo();
     $bbdoCfgUpdate();
     $updateSamlProviderConfiguration();
+    $migrateAccUsernamesFromVault();
 
     $pearDB->commitTransaction();
 
