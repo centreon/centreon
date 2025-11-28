49a8154b52bf4729c5756055ed15b2941817cdaa
Nicolas FRAGNET <nfragnet@centreon.com>
2025-11-28 15:46:47 +0100

    Make response status code the HTTP code again

--- centreon/src/App/Shared/Infrastructure/Legacy/LegacyHttpExceptionListener.php
+++ centreon/src/App/Shared/Infrastructure/Legacy/LegacyHttpExceptionListener.php
@@ -95,7 +95,7 @@ final readonly class LegacyHttpExceptionListener
         $event->setResponse(new JsonResponse(
             status: $statusCode,
             data: [
-                'code' => $exception->getCode(),
+                'code' => $statusCode,
                 'message' => $exception->getMessage(),
             ],
         ));
