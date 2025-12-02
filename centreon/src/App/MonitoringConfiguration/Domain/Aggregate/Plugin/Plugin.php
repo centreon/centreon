c3436c52b1b5f7c79494cdd32c69589d9b419543
jeremyjaouen <jjaouen@centreon.com>
2025-12-02 14:27:39 +0100

    revert description management

--- centreon/src/App/MonitoringConfiguration/Domain/Aggregate/Plugin/Plugin.php
+++ centreon/src/App/MonitoringConfiguration/Domain/Aggregate/Plugin/Plugin.php
@@ -23,8 +23,6 @@ declare(strict_types=1);
 
 namespace App\MonitoringConfiguration\Domain\Aggregate\Plugin;
 
-use Symfony\Component\Process\Process;
-
 final class Plugin
 {
     public function __construct(
@@ -32,22 +30,5 @@ final class Plugin
         public readonly PluginCommandLine $commandLine,
         public ?PluginDescription $description = null,
     ) {
-    }
-
-    public function updateDescription(?PluginDescription $description): void
-    {
-        $this->description = $description;
-    }
-
-    public function getDescription(): ?PluginDescription
-    {
-        $process = new Process([$this->commandLine->value, '--help']);
-        $process->run();
-
-        if (! $process->isSuccessful()) {
-            return null;
-        }
-
-        return empty($process->getOutput()) ? null : new PluginDescription(trim($process->getOutput()));
     }
 }
