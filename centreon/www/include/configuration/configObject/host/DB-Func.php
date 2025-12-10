c0a8197d5dd59d7bc6508a02b07a95c845aa07bb
alaunois <alaunois@centreon.com>
2025-12-10 17:09:23 +0100

    fix(configuration H/HT): fix empty template error (#8891) (#9013)
    Co-authored-by: selfakiri <selfakiri@ext.centreon.com>

--- centreon/www/include/configuration/configObject/host/DB-Func.php
+++ centreon/www/include/configuration/configObject/host/DB-Func.php
@@ -3022,7 +3022,10 @@ function getPayloadForHostTemplate(bool $isCloudPlatform, array $formData): arra
         'retry_check_interval' => $formData['host_retry_check_interval'] !== ''
             ? (int) $formData['host_retry_check_interval']
             : null,
-        'templates' => array_map(static fn (string $id): int => (int) $id, array_values($formData['tpSelect'] ?? [])),
+        'templates' => array_map(
+            static fn (string $id): int => (int) $id,
+            array_values(array_filter($formData['tpSelect'] ?? [], static fn ($id) => ! empty($id)))
+        ),
         'categories' => array_map(static fn (string $id): int => (int) $id, $formData['host_hcs'] ?? []),
         'macros' => array_map(
             static function (int|string $key, string $name, string $value) use ($formData): array {
@@ -3159,7 +3162,10 @@ function getPayloadForHost(bool $isCloudPlatform, array $formData): array
             ? (int) $formData['host_retry_check_interval']
             : null,
         'is_activated' => (bool) ($formData['host_activate']['host_activate'] ?: false),
-        'templates' => array_map(static fn (string $id): int => (int) $id, array_values($formData['tpSelect'] ?? [])),
+        'templates' => array_map(
+            static fn (string $id): int => (int) $id,
+            array_values(array_filter($formData['tpSelect'] ?? [], static fn ($id) => ! empty($id)))
+        ),
         'categories' => array_map(static fn (string $id): int => (int) $id, $formData['host_hcs'] ?? []),
         'groups' => array_map(static fn (string $id): int => (int) $id, $formData['host_hgs'] ?? []),
         'macros' => array_map(
