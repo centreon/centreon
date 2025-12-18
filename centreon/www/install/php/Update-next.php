85af985e5c297c4aa0126c310b929c76f7e9faf3
selfakiri <selfakiri@ext.centreon.com>
2025-12-18 14:06:22 +0100

    test

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -127,12 +127,41 @@ $fixBrokerConfigTypo = function () use ($pearDB, &$errorMessage): void {
     );
 };
 
+$fixDuplicateHostGroupTopology = function () use ($pearDB, &$errorMessage, $version): void {
+    $errorMessage = 'Unable to fix duplicate Host Groups topology';
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: [topology] Fixing duplicate Host Groups menu entries",
+    );
+
+    $pearDB->update(
+        <<<'SQL'
+            UPDATE `topology`
+            SET `topology_url` = '/configuration/hosts/groups',
+                `is_react` = '1',
+                `topology_show` = '1'
+            WHERE `topology_page` = 60102
+            SQL
+    );
+
+    // Remove duplicate topology entry 60105 introduced by 25.05 migration
+    $pearDB->delete(
+        <<<'SQL'
+            DELETE FROM `topology`
+            WHERE `topology_page` = 60105
+            SQL
+    );
+
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: [topology] Successfully removed duplicate Host Groups topology entry",
+    );
+};
+
 try {
     // DDL statements for real time database
-    // TODO add your function calls to update the real time database structure here
 
     // DDL statements for configuration database
-    // TODO add your function calls to update the configuration database structure here
 
     // Transactional queries for configuration database
     if (! $pearDB->isTransactionActive()) {
@@ -141,6 +170,7 @@ try {
 
     $fixBrokerConfigTypo();
     $updateSamlProviderConfiguration();
+    $fixDuplicateHostGroupTopology();
 
     $pearDB->commitTransaction();
 
