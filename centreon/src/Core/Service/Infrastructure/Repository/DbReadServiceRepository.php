c0d389bdef8e5f718582e3069ec40452bf65bde4
selfakiri <selfakiri@ext.centreon.com>
2025-12-22 11:32:23 +0100

    Fix(Hosts): remove service only if it is exclusive to deleted host

--- centreon/src/Core/Service/Infrastructure/Repository/DbReadServiceRepository.php
+++ centreon/src/Core/Service/Infrastructure/Repository/DbReadServiceRepository.php
@@ -277,6 +277,39 @@ class DbReadServiceRepository extends AbstractRepositoryRDB implements ReadServi
     /**
      * @inheritDoc
      */
+    public function findServiceIdsExclusivelyLinkedToHostId(int $hostId): array
+    {
+        $request = $this->translateDbName(
+            <<<'SQL'
+                SELECT hsr.service_service_id
+                FROM `:db`.host_service_relation hsr
+                INNER JOIN `:db`.host h
+                    ON h.host_id = hsr.host_host_id
+                WHERE hsr.host_host_id = :host_id
+                    AND h.host_register = '1'
+                    AND (
+                        SELECT COUNT(*)
+                        FROM `:db`.host_service_relation hsr2
+                        WHERE hsr2.service_service_id = hsr.service_service_id
+                    ) = 1
+                SQL
+        );
+
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':host_id', $hostId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        $serviceIds = [];
+        while (($serviceId = $statement->fetchColumn()) !== false) {
+            $serviceIds[] = (int) $serviceId;
+        }
+
+        return $serviceIds;
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function findServiceNamesByHost(int $hostId): ?ServiceNamesByHost
     {
         $request = $this->translateDbName(
