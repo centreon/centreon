9d683c66e581e5aef813f9d50ac1ba63e432f8e1
selfakiri <selfakiri@ext.centreon.com>
2025-12-24 11:25:45 +0100

    merge develop

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -23,6 +23,7 @@ use Adaptation\Database\Connection\Collection\QueryParameters;
 use Adaptation\Database\Connection\ConnectionInterface;
 use Adaptation\Database\Connection\Exception\ConnectionException;
 use Adaptation\Database\Connection\ValueObject\QueryParameter;
+use Core\AgentConfiguration\Domain\Model\AgentConfiguration;
 
 require_once __DIR__ . '/../../../bootstrap.php';
 
@@ -34,6 +35,40 @@ $errorMessage = '';
  * @var ConnectionInterface $pearDB
  * @var ConnectionInterface $pearDBO
  */
+
+/** -------------------------------------- Agent configuration updates -------------------------------------- */
+$addDefaultPortToAgentInitiatedAgentConfiguration = function () use ($pearDB, &$errorMessage, $version): void {
+    $errorMessage = 'Unable to add default port to agent initiated agent configurations';
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: [agent_configuration] Adding default port to agent initiated agent configurations",
+    );
+    $agentConfigurations = $pearDB->fetchAllAssociative(
+        <<<'SQL'
+                SELECT id, configuration FROM agent_configuration
+            SQL
+    );
+    foreach ($agentConfigurations as $configurationJson) {
+        $configuration = json_decode($configurationJson['configuration'], true, JSON_THROW_ON_ERROR);
+        if (! isset($configuration['port'])) {
+            $configuration['port'] = (bool) $configuration['agent_initiated'] === true
+                ? AgentConfiguration::DEFAULT_PORT
+                : null;
+        }
+        $updatedConfigurationJson = json_encode($configuration, JSON_THROW_ON_ERROR);
+        $pearDB->update(
+            <<<'SQL'
+                UPDATE agent_configuration
+                SET configuration = :configuration
+                WHERE id = :id
+                SQL,
+            QueryParameters::create([
+                QueryParameter::string('configuration', $updatedConfigurationJson),
+                QueryParameter::int('id', (int) $configurationJson['id']),
+            ])
+        );
+    }
+};
 
 /** -------------------------------------- Backup updates -------------------------------------- */
 $setBackupMysqlConfDefaultAsEmpty = function () use ($pearDB, &$errorMessage, $version): void {
@@ -262,12 +297,6 @@ $moveCommandACLTopologyIntoACLActions = function () use ($pearDB, &$errorMessage
 };
 
 try {
-    // DDL statements for real time database
-    // TODO add your function calls to update the real time database structure here
-
-    // DDL statements for configuration database
-    // TODO add your function calls to update the configuration database structure here
-
     // Transactional queries for configuration database
     if (! $pearDB->isTransactionActive()) {
         $pearDB->startTransaction();
@@ -282,6 +311,7 @@ try {
     $bbdoCfgUpdate();
     $updateSamlProviderConfiguration();
     $setBackupMysqlConfDefaultAsEmpty();
+    $addDefaultPortToAgentInitiatedAgentConfiguration();
 
     $pearDB->commitTransaction();
 
