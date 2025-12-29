08cb198a65d8dcd5e9c1c67b47159882211bedf6
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2025-12-29 14:39:06 +0100

    Enh(Agent configuration ) Add port in agent-initiated connection (#8945) (#9160)
    Co-authored-by: Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>

--- centreon/www/front_src/src/AgentConfiguration/api/decoders.ts
+++ centreon/www/front_src/src/AgentConfiguration/api/decoders.ts
@@ -56,6 +56,7 @@ const telegrafConfigurationDecoder = JsonDecoder.object<TelegrafConfiguration>(
 
 const cmaConfigurationDecoder = JsonDecoder.object<CMAConfiguration>(
   {
+    port: JsonDecoder.optional(JsonDecoder.nullable(JsonDecoder.number)),
     tokens: JsonDecoder.optional(
       JsonDecoder.array(
         JsonDecoder.object(
