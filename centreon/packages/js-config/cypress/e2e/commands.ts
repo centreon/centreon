bf67c2861095dabb150f07c4efd6be7bcb592321
Kevin Duret <kduret@centreon.com>
2026-01-06 17:28:21 +0100

    enh(test): optimize contact configuration e2e test

--- centreon/packages/js-config/cypress/e2e/commands.ts
+++ centreon/packages/js-config/cypress/e2e/commands.ts
@@ -495,7 +495,7 @@ Cypress.Commands.add(
 
     const webImage = `docker.centreon.com/centreon/${moduleName}${slimSuffix}-${webOs}:${webVersion}`;
 
-    const timeout = 1200_000; // 20 minutes because docker pull can be very slow
+    const timeout = 1200_000; // 20 minutes because docker pull can be very slow (mainly keycloak)
     const cypressTaskTimeout = Cypress.config('taskTimeout') || 600_000;
     Cypress.config('taskTimeout', timeout);
 
