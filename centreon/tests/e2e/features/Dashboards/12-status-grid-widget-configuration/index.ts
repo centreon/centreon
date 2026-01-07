3f2239cadec050142aa52d96085a2db0d9f176c2
Tom Darneix <tomdar87@outlook.com>
2026-01-07 18:41:04 +0100

    use data testid

--- centreon/tests/e2e/features/Dashboards/12-status-grid-widget-configuration/index.ts
+++ centreon/tests/e2e/features/Dashboards/12-status-grid-widget-configuration/index.ts
@@ -336,8 +336,8 @@ When(
     cy.getByLabel({ label: 'RichTextEditor' })
       .eq(0)
       .type(genericTextWidgets.default.description);
-    cy.contains('Select all').eq(0).click();
-    cy.contains('Select all').eq(1).click();
+    cy.get('[data-testid="Select all"]').eq(0).click();
+    cy.get('[data-testid="Select all"]').eq(1).click();
     cy.getByTestId({ testId: 'Resource type' }).realClick();
     cy.getByLabel({ label: 'Host Group' }).click();
     cy.getByTestId({ testId: 'Select resource' }).click();
--- centreon/tests/e2e/features/Dashboards/13-dashboard-all-widgets/index.ts
+++ centreon/tests/e2e/features/Dashboards/13-dashboard-all-widgets/index.ts
@@ -274,8 +274,8 @@ When(
     cy.getByTestId({ testId: 'Widget type' }).click();
     cy.contains('Status grid').click();
     cy.getByLabel({ label: 'Title' }).type(genericTextWidgets.default.title);
-    cy.contains('Select all').eq(0).click();
-    cy.contains('Select all').eq(1).click();
+    cy.get('[data-testid="Select all"]').eq(0).click();
+    cy.get('[data-testid="Select all"]').eq(1).click();
     cy.getByTestId({ testId: 'Resource type' }).realClick();
     cy.getByLabel({ label: 'Host Group' }).click();
     cy.getByTestId({ testId: 'Select resource' }).click();
