27dbf6277806da210333d0c6e53ba849a98c9ad1
Dmytro Iosypenko <108675430+dmyios@users.noreply.github.com>
2026-01-08 11:28:48 +0100

    enh(Dashboards): Added swtiches to diplay buttons for ticket creation for host & service  (#8683)
    Co-authored-by: Drokha <45570362+Drokha@users.noreply.github.com>
    Co-authored-by: Tom Rabut <trabut@centreon.com>
    Co-authored-by: smau <102975713+smau-centreon@users.noreply.github.com>

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-resourcestable/src/Listing/useListing.ts
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-resourcestable/src/Listing/useListing.ts
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 
-import { useAtom } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import { equals, isEmpty, isNotNil } from 'ramda';
 import { useTranslation } from 'react-i18next';
 
@@ -9,6 +9,7 @@ import { type Column, useSnackbar } from '@centreon/ui';
 import type { CommonWidgetProps, Resource, SortOrder } from '../../../models';
 import { getResourcesUrl, goToUrl } from '../../../utils';
 import {
+  openTicketAtom,
   resourcesToAcknowledgeAtom,
   resourcesToOpenTicketAtom,
   resourcesToSetDowntimeAtom,
@@ -24,7 +25,6 @@ import {
   type Ticket
 } from './models';
 import { labelSelectAtLeastThreeColumns } from './translatedLabels';
-import useIsOpenTicketInstalled from './useIsOpenTicketInstalled';
 import useLoadResources from './useLoadResources';
 
 interface UseListingState {
@@ -58,13 +58,9 @@ interface UseListingProps
     'dashboardId' | 'id' | 'playlistHash' | 'widgetPrefixQuery'
   > {
   changeViewMode?: (displayType) => void;
-  displayResources: 'withTicket' | 'withoutTicket';
   displayType: DisplayType;
   hostSeverities: Array<NamedEntity>;
-  isDownHostHidden: boolean;
   isFromPreview?: boolean;
-  isOpenTicketEnabled: boolean;
-  isUnreachableHostHidden: boolean;
   limit?: number;
   provider?: { id: number; name: string };
   refreshCount: number;
@@ -81,7 +77,6 @@ interface UseListingProps
 
 const useListing = ({
   resources,
-  isOpenTicketEnabled,
   states,
   statuses,
   displayType,
@@ -99,11 +94,7 @@ const useListing = ({
   widgetPrefixQuery,
   statusTypes,
   hostSeverities,
-  serviceSeverities,
-  isDownHostHidden,
-  isUnreachableHostHidden,
-  displayResources,
-  provider
+  serviceSeverities
 }: UseListingProps): UseListingState => {
   const { showWarningMessage } = useSnackbar();
   const { t } = useTranslation();
@@ -124,6 +115,9 @@ const useListing = ({
     resourcesToSetDowntimeAtom
   );
 
+  const { isOpenTicketEnabled, isOpenTicketInstalled, provider } =
+    useAtomValue(openTicketAtom);
+
   useEffect(() => {
     if (isOpenTicketEnabled && isFromPreview) {
       setPanelOptions?.({ displayType: DisplayType.Service });
@@ -132,22 +126,16 @@ const useListing = ({
     }
   }, [isOpenTicketEnabled]);
 
-  const isOpenTicketInstalled = useIsOpenTicketInstalled();
-
   const hasProvider = isNotNil(provider) && !isEmpty(provider);
 
   const { data, isLoading } = useLoadResources({
     dashboardId,
-    displayResources,
     displayType,
     hostSeverities,
     id,
-    isDownHostHidden,
-    isUnreachableHostHidden,
     limit,
     page,
     playlistHash,
-    provider,
     refreshCount,
     refreshIntervalToUse,
     resources,
@@ -200,12 +188,7 @@ const useListing = ({
     setPage(updatedPage + 1);
   };
 
-  const { columns, defaultSelectedColumnIds } = useColumns({
-    displayResources,
-    displayType,
-    isOpenTicketEnabled,
-    provider
-  });
+  const { columns, defaultSelectedColumnIds } = useColumns({ displayType });
 
   const selectColumns = (updatedColumnIds: Array<string>): void => {
     if (updatedColumnIds.length < 3) {
