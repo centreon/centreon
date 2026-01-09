73d1e00bbd6e3fe050539cecc5f7e1bf48b6453b
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2026-01-09 17:54:09 +0100

    test(setup): fix command argument pattern matching in setup script

--- centreon/tests/rest_api/collections/configuration/setup.sh
+++ centreon/tests/rest_api/collections/configuration/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == Command* ]]; then
+if [[ "$1" == *Command* ]]; then
   # Import dataset via CLAPI for tests
+  echo "Import dataset via CLAPI for tests"
   docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml cp ./fixtures web:/tmp
   docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml exec web ls -la /tmp/fixtures
   docker compose -f $(dirname $0)/../../../../../.github/docker/docker-compose.yml exec web bash -ex -c "/tmp/fixtures/clapi-import-dataset.sh"
