16b35d51be271ae8118e3f16ea4589db7b1e47af
Tom Darneix <tomdar87@outlook.com>
2026-01-13 16:33:50 +0100

    fix: base 1024 axis unit (#9301)

--- centreon/packages/ui/src/Graph/BarChart/BarChart.cypress.spec.tsx
+++ centreon/packages/ui/src/Graph/BarChart/BarChart.cypress.spec.tsx
@@ -100,7 +100,7 @@ describe('Bar chart', () => {
     cy.contains('Centreon-Server: Round-Trip Maximum Time').should(
       'be.visible'
     );
-    cy.contains('7.47 KB').should('be.visible');
+    cy.contains('7.47 KiB').should('be.visible');
 
     cy.makeSnapshot();
   });
