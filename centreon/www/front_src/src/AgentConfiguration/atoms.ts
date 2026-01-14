4821a1a62de0fb8db6e8bec9115f56b4df4c6a62
Kevin Duret <kduret@centreon.com>
2026-01-14 16:34:36 +0100

    feat(ci): run only e2e tests linked to code changes

--- centreon/www/front_src/src/AgentConfiguration/atoms.ts
+++ centreon/www/front_src/src/AgentConfiguration/atoms.ts
@@ -57,7 +57,6 @@ export const deleteFilterEntryAtom = atom(
   (get, set, { field, entryToDelete }: DeleteFilterProps) => {
     const fieldEntries = get(filtersAtom)[field];
 
-
     const entryToDeleteIndex = findIndex(
       ({ id }) => equals(entryToDelete.id, id),
       fieldEntries
