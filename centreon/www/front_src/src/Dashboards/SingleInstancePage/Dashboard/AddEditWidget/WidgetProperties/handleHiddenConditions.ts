833c6a5800c2451ea927699734984dc57c9324e0
Drokha <45570362+Drokha@users.noreply.github.com>
2026-01-20 13:55:22 +0100

    Feature(WidgetMBI): Mon 168462 feature mbi widget near saturation (#7855)
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    Co-authored-by: Dmytro Iosypenko <108675430+dmyios@users.noreply.github.com>
    Co-authored-by: dmyios <diosypenko@centreon.com>
    Co-authored-by: smau <102975713+smau-centreon@users.noreply.github.com>
    Co-authored-by: Adrien Morais <amorais@CNTR-MAC-A066.local>

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/handleHiddenConditions.ts
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/AddEditWidget/WidgetProperties/handleHiddenConditions.ts
@@ -72,6 +72,10 @@ export const checkHiddenCondition = ({
     return !hasModule || isEmpty(formValue) || isNil(formValue);
   }
 
+  if (equals(method, 'truthy')) {
+    return true;
+  }
+
   return !hasModule || equals(path(when.split('.'), values), matches);
 };
 
