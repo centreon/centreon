72300abcf3faa874bec0d4b3574cebe6bc4ab589
disaster37 <linuxworkgroup@hotmail.com>
2026-01-23 15:11:52 +0000

    feat: add GetHostTemplate API endpoint
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

---
+++ centreon/src/Core/HostTemplate/Infrastructure/API/GetHostTemplate/GetHostTemplateController.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\HostTemplate\Infrastructure\API\GetHostTemplate;
+
+use Centreon\Application\Controller\AbstractController;
+use Centreon\Domain\Log\LoggerTrait;
+use Core\HostTemplate\Application\UseCase\GetHostTemplate\GetHostTemplate;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+final class GetHostTemplateController extends AbstractController
+{
+    use LoggerTrait;
+
+    /**
+     * @param GetHostTemplate $useCase
+     * @param GetHostTemplateSaasPresenter $saasPresenter
+     * @param GetHostTemplateOnPremPresenter $onPremPresenter
+     * @param bool $isCloudPlatform
+     * @param int $hostTemplateId
+     *
+     * @throws AccessDeniedException
+     *
+     * @return Response
+     */
+    public function __invoke(
+        GetHostTemplate $useCase,
+        GetHostTemplateSaasPresenter $saasPresenter,
+        GetHostTemplateOnPremPresenter $onPremPresenter,
+        bool $isCloudPlatform,
+        int $hostTemplateId
+    ): Response {
+        $this->denyAccessUnlessGrantedForApiConfiguration();
+
+        $presenter = $isCloudPlatform ? $saasPresenter : $onPremPresenter;
+        $useCase($presenter, $hostTemplateId);
+
+        return $presenter->show();
+    }
+
+}
