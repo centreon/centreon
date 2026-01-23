dd8270a15af9bd45b541cf0048881ce3c6cc1b2e
Tom Darneix <tomdar87@outlook.com>
2026-01-23 11:44:34 +0100

    fix: e2e tests attempt

--- centreon-open-tickets/tests/e2e/features/Dashboards/01-open-ticket-configuration/index.ts
+++ centreon-open-tickets/tests/e2e/features/Dashboards/01-open-ticket-configuration/index.ts
@@ -269,10 +269,7 @@ Then(
     cy.enterIframe('#open-ticket').within(() => {
       cy.get('td.FormRowField').should('include.text', 'New ticket opened');
     });
-    cy.get('[class$="modalCloseButton"]')
-      .find('[aria-label="close"]')
-      .eq(1)
-      .click();
+    cy.get('[aria-label="close"]').eq(1).click();
     cy.getByLabel({ label: 'Resources linked to a ticket' }).click();
     cy.getByTestId({ testId: 'confirm' }).realClick();
     cy.getByTestId({ testId: 'save_dashboard' }).click();
