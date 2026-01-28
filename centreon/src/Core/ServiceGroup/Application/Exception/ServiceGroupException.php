bcce42d72ce3cd1e5420eef2c73a1e1676e31205
disaster37 <linuxworkgroup@hotmail.com>
2026-01-28 08:35:50 +0000

    fix: some fix after bot review
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- centreon/src/Core/ServiceGroup/Application/Exception/ServiceGroupException.php
+++ centreon/src/Core/ServiceGroup/Application/Exception/ServiceGroupException.php
@@ -90,6 +90,6 @@ class ServiceGroupException extends \Exception
      */
     public static function errorWhileRetrieving(): self
     {
-        return new self(_('Error while retrieving for service groups'));
+        return new self(_('EError while retrieving service group'));
     }
 }
