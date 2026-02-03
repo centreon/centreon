85929c764e23cd4066911dde001db691fa529a40
Tom Darneix <tomdar87@outlook.com>
2026-02-03 11:08:49 +0100

    fix: remove test endpoint (#9504)

--- centreon/www/front_src/src/Resources/Details/tabs/Graph/ChartGraph.tsx
+++ centreon/www/front_src/src/Resources/Details/tabs/Graph/ChartGraph.tsx
@@ -49,8 +49,8 @@ const ChartGraph = ({
 
   const { data } = useFetchQuery<LineChartData>({
     baseEndpoint: '',
-    getEndpoint: () => 'http://localhost:3001/perf2',
-    // `${graphEndpoint}?start=${graphTimeParameters?.start}&end=${graphTimeParameters?.end}`,
+    getEndpoint: () =>
+      `${graphEndpoint}?start=${graphTimeParameters?.start}&end=${graphTimeParameters?.end}`,
     getQueryKey: () => [
       'graphPerformance',
       graphTimeParameters?.start,
