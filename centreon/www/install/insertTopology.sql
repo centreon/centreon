062699e07cda6b374dedb5bde3ef42784d54848b
alaunois <alaunois@centreon.com>
2026-02-03 12:12:47 +0100

    remove legacy commands pages, fix minHelpCommand page

--- centreon/www/install/insertTopology.sql
+++ centreon/www/install/insertTopology.sql
@@ -200,8 +200,8 @@ INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`,
 INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_feature_flag`) VALUES ( 'Vault', '/administration/parameters/vault', '1', '1', 501, 50112, 100, 1, 'vault');
 
 -- add command configuration page
-INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`)
-VALUES ( 'Commands', '/configuration/commands', '1', '1', 608, 60808, 1, 1);
+INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url_substitute`)
+VALUES ( 'Commands', '/configuration/commands', '1', '1', 608, 60808, 1, 1, './include/configuration/configObject/command/command.php');
 
 /*!40000 ALTER TABLE `topology` ENABLE KEYS */;
 UNLOCK TABLES;
