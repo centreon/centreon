38ee123954bfab950a54072e790fe7a9123515ea
alaunois <alaunois@centreon.com>
2026-02-11 17:08:06 +0100

    add missing use

--- centreon/src/Core/Service/Infrastructure/Repository/DbReadServiceRepository.php
+++ centreon/src/Core/Service/Infrastructure/Repository/DbReadServiceRepository.php
@@ -43,6 +43,7 @@ use Core\Service\Domain\Model\Service;
 use Core\Service\Domain\Model\ServiceInheritance;
 use Core\Service\Domain\Model\ServiceLight;
 use Core\Service\Domain\Model\ServiceNamesByHost;
+use Core\Service\Domain\Model\ServiceRelation;
 use Core\ServiceCategory\Infrastructure\Repository\ServiceCategoryRepositoryTrait;
 use Core\ServiceGroup\Domain\Model\ServiceGroupRelation;
 use Utility\SqlConcatenator;
