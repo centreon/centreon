aac9cddf39cb552686ac0c5ba6bd2821d806b6d8
Tom Rabut <trabut@centreon.com>
2026-02-11 15:06:16 +0100

    lint: fix for diff version

--- centreon/www/front_src/src/Resources/Details/tabs/Graph/ChartGraph.tsx
+++ centreon/www/front_src/src/Resources/Details/tabs/Graph/ChartGraph.tsx
@@ -4,20 +4,20 @@ import {
   type LineChartData,
   type Parameters,
   type TooltipData,
-  useFetchQuery,
-} from "@centreon/ui";
-import { path } from "ramda";
-import { type ReactElement, type RefObject, useState } from "react";
+  useFetchQuery
+} from '@centreon/ui';
+import { path } from 'ramda';
+import { type ReactElement, type RefObject, useState } from 'react';
 
-import FederatedComponent from "../../../../components/FederatedComponents";
-import { graphsCapNumber } from "../../../constants";
-import MemoizedGraphActions from "../../../Graph/Performance/GraphActions";
-import type { Resource } from "../../../models";
-import TooManyElementsCard from "../../../TooManyElementsCard";
-import type { ResourceDetails } from "../../models";
-import Comment from "./Comment";
-import { useChartGraphStyles } from "./chartGraph.styles";
-import useRetrieveTimeLine from "./useRetrieveTimeLine";
+import FederatedComponent from '../../../../components/FederatedComponents';
+import MemoizedGraphActions from '../../../Graph/Performance/GraphActions';
+import TooManyElementsCard from '../../../TooManyElementsCard';
+import { graphsCapNumber } from '../../../constants';
+import type { Resource } from '../../../models';
+import type { ResourceDetails } from '../../models';
+import Comment from './Comment';
+import { useChartGraphStyles } from './chartGraph.styles';
+import useRetrieveTimeLine from './useRetrieveTimeLine';
 
 interface Props {
   graphTimeParameters?: Parameters;
@@ -28,7 +28,7 @@ interface Props {
 const ChartGraph = ({
   graphTimeParameters,
   resource,
-  updatedGraphInterval,
+  updatedGraphInterval
 }: Props) => {
   const { classes } = useChartGraphStyles();
 
@@ -37,13 +37,13 @@ const ChartGraph = ({
   const [areaThresholdLines, setAreaThresholdLines] = useState();
 
   const graphEndpoint = path<string>(
-    ["links", "endpoints", "performance_graph"],
-    resource,
+    ['links', 'endpoints', 'performance_graph'],
+    resource
   );
 
   const timelineEndpoint = path<string>(
-    ["links", "endpoints", "timeline"],
-    resource,
+    ['links', 'endpoints', 'timeline'],
+    resource
   );
 
   const { data } = useFetchQuery<LineChartData>({
@@ -51,20 +51,20 @@ const ChartGraph = ({
     getEndpoint: () =>
       `${graphEndpoint}?start=${graphTimeParameters?.start}&end=${graphTimeParameters?.end}`,
     getQueryKey: () => [
-      "graphPerformance",
+      'graphPerformance',
       graphTimeParameters?.start,
       graphTimeParameters?.end,
-      graphEndpoint,
+      graphEndpoint
     ],
     queryOptions: {
       enabled: !!graphTimeParameters && !!graphEndpoint,
-      suspense: false,
-    },
+      suspense: false
+    }
   });
 
   const timeLineData = useRetrieveTimeLine({
     timelineEndpoint,
-    graphTimeParameters,
+    graphTimeParameters
   });
 
   const getInterval = (interval: Interval): void => {
@@ -117,20 +117,20 @@ const ChartGraph = ({
         data={data}
         end={graphTimeParameters?.end}
         height={280}
-        legend={{ mode: "grid", placement: "bottom" }}
+        legend={{ mode: 'grid', placement: 'bottom' }}
         lineStyle={{ lineWidth: 1 }}
         header={{ extraComponent: graphActions }}
         tooltip={{
           renderComponent: ({
             data,
-            hideTooltip,
+            hideTooltip
           }: TooltipData): ReactElement => (
             <Comment
               commentDate={data}
               hideAddCommentTooltip={hideTooltip}
               resource={resource}
             />
-          ),
+          )
         }}
         start={graphTimeParameters?.start}
         timeShiftZones={{ enable: true, getInterval }}
