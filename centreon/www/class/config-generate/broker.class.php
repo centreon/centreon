dd639683d5d4fe318b2ac29a0e96015f58c53cac
jeremyjaouen <jjaouen@centreon.com>
2026-03-12 09:27:07 +0100

    fix(export): validate boolean env vars with FILTER_NULL_ON_FAILURE

--- centreon/www/class/config-generate/broker.class.php
+++ centreon/www/class/config-generate/broker.class.php
@@ -450,12 +450,24 @@ class Broker extends AbstractObjectJSON
                 }
             }
             $shouldBeEncrypted = $this->readMonitoringServerRepository->isEncryptionReady($pollerId);
-            $dbSslEnabled = isset($_ENV['DATABASE_SSL_ENABLED'])
-                ? filter_var($_ENV['DATABASE_SSL_ENABLED'], FILTER_VALIDATE_BOOLEAN)
-                : null;
-            $dbSslVerifyCert = isset($_ENV['DATABASE_VERIFY_SERVER_CERT'])
-                ? filter_var($_ENV['DATABASE_VERIFY_SERVER_CERT'], FILTER_VALIDATE_BOOLEAN)
-                : null;
+            $dbSslEnabled = null;
+            if (isset($_ENV['DATABASE_SSL_ENABLED'])) {
+                $dbSslEnabled = filter_var($_ENV['DATABASE_SSL_ENABLED'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                if ($dbSslEnabled === null) {
+                    throw new InvalidArgumentException(
+                        sprintf('Invalid value "%s" for DATABASE_SSL_ENABLED: expected a boolean value.', $_ENV['DATABASE_SSL_ENABLED'])
+                    );
+                }
+            }
+            $dbSslVerifyCert = null;
+            if (isset($_ENV['DATABASE_VERIFY_SERVER_CERT'])) {
+                $dbSslVerifyCert = filter_var($_ENV['DATABASE_VERIFY_SERVER_CERT'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                if ($dbSslVerifyCert === null) {
+                    throw new InvalidArgumentException(
+                        sprintf('Invalid value "%s" for DATABASE_VERIFY_SERVER_CERT: expected a boolean value.', $_ENV['DATABASE_VERIFY_SERVER_CERT'])
+                    );
+                }
+            }
             $dbSslCa = $_ENV['DATABASE_CA_PATH'] ?? null;
             foreach ($object['output'] as &$output) {
                 if (in_array($output['type'], ['sql', 'storage', 'unified_sql'])) {
