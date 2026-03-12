93403465fbdfbb88f2ee8d019b79219195aa6cb0
jeremyjaouen <jjaouen@centreon.com>
2026-03-12 10:23:25 +0100

    style

--- centreon/src/App/MonitoringConfiguration/Infrastructure/Security/InstallationCommandVoter.php
+++ centreon/src/App/MonitoringConfiguration/Infrastructure/Security/InstallationCommandVoter.php
@@ -25,8 +25,8 @@ namespace App\MonitoringConfiguration\Infrastructure\Security;
 
 use App\MonitoringConfiguration\Domain\Aggregate\Poller\PollerId;
 use App\MonitoringConfiguration\Domain\Security\AgentConfigurationPermissionEnum;
-use App\Security\Domain\Repository\ResourceAccessRepository;
 use App\Security\Domain\Aggregate\Permission;
+use App\Security\Domain\Repository\ResourceAccessRepository;
 use App\Security\Infrastructure\Security\CredentialUser;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Vote;
