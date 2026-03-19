e277396143d6a13accfe6317d2314015ebe7b7bb
Kevin Duret <kduret@centreon.com>
2026-03-19 08:25:08 +0100

    enh(docker): docker web optimizations (startup + runtime) (#9581)

--- .github/docker/centreon-web/alma9/entrypoint/container.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 #set -e
-set -x
+[ "${DEBUG:-0}" = "1" ] && set -x
 
 # Run each startup script located in BASEDIR.
 # ls is required to ensure that the scripts are properly sorted by name.
