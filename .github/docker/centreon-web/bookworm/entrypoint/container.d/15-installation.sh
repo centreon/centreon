3eab91db7165bf60d6440a21b43bdee7b1911481
Kevin Duret <kduret@centreon.com>
2026-03-19 08:25:08 +0100

    enh(docker): docker web optimizations (startup + runtime) (#9581)

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/15-installation.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/15-installation.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
 # Avoid to display mysql warning: Using a password on the command line interface can be insecure.
 export MYSQL_PWD="${MYSQL_ROOT_PASSWORD}"
@@ -8,12 +9,35 @@ sed -i "s/localhost/${MYSQL_HOST}/g" /usr/share/centreon/www/install/tmp/databas
 if [ ! -f /etc/centreon/centreon.conf.php ] && [ -d /usr/share/centreon/www/install ]; then
   cd /usr/share/centreon/www/install/steps/process
 
-  if [ $(mysql -N -s -h${MYSQL_HOST} -u root -e \
-      "SELECT count(*) from information_schema.tables WHERE \
-          table_schema='centreon' and table_name='nagios_server'") -eq 1 ]; then
-      echo "Centreon is already installed."
-      su www-data -s /bin/bash -c "php configFileSetup.php"
-      su www-data -s /bin/bash -c "php createDbUser.php"
+  MYSQL_FK_CHECKS=$(mysql -N -s -h"${MYSQL_HOST}" -uroot -e "SELECT @@GLOBAL.foreign_key_checks")
+  MYSQL_UNIQUE_CHECKS=$(mysql -N -s -h"${MYSQL_HOST}" -uroot -e "SELECT @@GLOBAL.unique_checks")
+  MYSQL_INNODB_FLUSH=$(mysql -N -s -h"${MYSQL_HOST}" -uroot -e "SELECT @@GLOBAL.innodb_flush_log_at_trx_commit")
+  MYSQL_SYNC_BINLOG=$(mysql -N -s -h"${MYSQL_HOST}" -uroot -e "SELECT @@GLOBAL.sync_binlog")
+
+  restore_mysql_settings() {
+    mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL foreign_key_checks=${MYSQL_FK_CHECKS}"
+    mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL unique_checks=${MYSQL_UNIQUE_CHECKS}"
+    mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL innodb_flush_log_at_trx_commit=${MYSQL_INNODB_FLUSH}"
+    mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL sync_binlog=${MYSQL_SYNC_BINLOG}"
+  }
+  trap restore_mysql_settings EXIT
+
+  mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL foreign_key_checks=0"
+  mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL unique_checks=0"
+  mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL innodb_flush_log_at_trx_commit=2"
+  mysql -h"${MYSQL_HOST}" -uroot -e "SET GLOBAL sync_binlog=0"
+
+  echo "Creating Centreon configuration files..."
+  su www-data -s /bin/bash -c "php configFileSetup.php"
+
+  if [ "$(mysql -N -s -h"${MYSQL_HOST}" -u root -e \
+    "SELECT count(*) from information_schema.tables WHERE \
+        table_schema='centreon' and table_name='nagios_server'")" -eq 1
+  ]; then
+    echo "Centreon is already installed."
+
+    echo "Creating Centreon database user..."
+    su www-data -s /bin/bash -c "php createDbUser.php"
   else
     su www-data -s /bin/bash -c "php configFileSetup.php"
     su www-data -s /bin/bash -c "php installConfigurationDb.php"
@@ -22,41 +46,59 @@ if [ ! -f /etc/centreon/centreon.conf.php ] && [ -d /usr/share/centreon/www/inst
     su www-data -s /bin/bash -c "SERVER_ADDR='127.0.0.1' php insertBaseConf.php"
     su www-data -s /bin/bash -c "php partitionTables.php"
 
-    mysql -h${MYSQL_HOST} -uroot centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
-    mysql -h${MYSQL_HOST} -uroot -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
+    mysql -h"${MYSQL_HOST}" -uroot centreon -e "UPDATE cfg_centreonbroker_info SET config_value = '${MYSQL_HOST}' WHERE config_key = 'db_host'"
+    mysql -h"${MYSQL_HOST}" -uroot -e "GRANT ALL ON *.* to 'centreon'@'%' WITH GRANT OPTION"
 
-    if [ "$CENTREON_DATASET" = "1" ]; then
+    if [ "${CENTREON_DATASET:-0}" = "1" ]; then
       echo "CENTREON_DATASET environment variable is set, dump will be inserted."
       DATA_DUMP_DIR="/usr/local/src/sql/data"
-      for file in `ls $DATA_DUMP_DIR` ; do
-        echo "Inserting dump $file ..."
-        mysql -h${MYSQL_HOST} -uroot centreon < $DATA_DUMP_DIR/$file
+      for file in "$DATA_DUMP_DIR"/*; do
+        [ -e "$file" ] || continue
+        echo "Inserting dump $(basename "$file") ..."
+        mysql -h"${MYSQL_HOST}" -uroot centreon < "$file"
       done
     fi
   fi
 
-  su www-data -s /bin/bash -c "php createEngineContextConfiguration.php"
+  echo "Generating Centreon cache..."
   su www-data -s /bin/bash -c "php generationCache.php"
+
+  echo "Creating engine context configuration..."
+  su www-data -s /bin/bash -c "php createEngineContextConfiguration.php"
+
+  echo "Disabling statistics collection..."
+  mysql -h"${MYSQL_HOST}" -uroot centreon -e "DELETE FROM options WHERE \`key\` = 'send_statistics'"
+  mysql -h"${MYSQL_HOST}" -uroot centreon -e "INSERT INTO options (\`key\`, \`value\`) VALUES ('send_statistics', '0')"
+
+  restore_mysql_settings
+  trap - EXIT
+
   cd -
 fi
 
 sed -i 's#severity=error#severity=debug#' /etc/sysconfig/gorgoned
-sed -i "5s/.*/    id: 1/" /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+if [[ -f /etc/centreon-gorgone/config.d/40-gorgoned.yaml ]]; then
+  if grep -Pzo '(?s)gorgonecore:.*?(?=\n\S|\Z)' /etc/centreon-gorgone/config.d/40-gorgoned.yaml | grep -q 'id:'; then
+    sed -Ei '/^gorgonecore:/,/^[^[:space:]]/ s/^([[:space:]]*id:).*/\1 1/' /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+  else
+    sed -Ei '/^gorgonecore:/a\    id: 1' /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+  fi
+fi
 sed -i 's#enable: true#enable: false#' /etc/centreon-gorgone/config.d/50-centreon-audit.yaml
 
 
 setAdminLanguage() {
-  if [ -z "$1" ]; then
+  if [ -z "${1:-}" ]; then
     echo "Language not set"
     return
   fi
 
   echo "Setting language to $1"
 
-  mysql -h${MYSQL_HOST} -uroot centreon -e "UPDATE contact SET contact_lang = '$1.UTF-8' WHERE contact_alias = 'admin'"
+  mysql -h"${MYSQL_HOST}" -uroot centreon -e "UPDATE contact SET contact_lang = '$1.UTF-8' WHERE contact_alias = 'admin'"
 }
 
-case "$CENTREON_LANG" in
+case "${CENTREON_LANG:-}" in
   de*)
     setAdminLanguage "de_DE"
     ;;
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/30-broker-engine_background.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/30-broker-engine_background.sh
@@ -2,3 +2,7 @@
 
 su www-data -s /bin/bash  -c "centreon -u admin -p Centreon\!2021 -a POLLERGENERATE -v 1"
 su www-data -s /bin/bash  -c "centreon -u admin -p Centreon\!2021 -a CFGMOVE -v 1"
+
+# Start Centreon Broker daemons.
+systemctl start cbd
+systemctl start centengine
