f268d50719bc9bb26ff534333b6f1d84fa8ad821
Kevin Duret <kduret@centreon.com>
2026-03-24 14:43:18 +0100

    fix(docker): fix slow startup of docker web image (#9994) (#10003)

--- .github/docker/centreon-web/alma8/entrypoint/container.d/99-logs.sh
+++ .github/docker/centreon-web/alma8/entrypoint/container.d/99-logs.sh
@@ -5,6 +5,4 @@ echo "Centreon is ready"
 
 tail -f \
   /var/log/httpd/error_log \
-  /var/log/php-fpm/centreon-error.log \
-  /var/log/php-fpm/error.log \
-  /var/log/httpd/access_log
+  /var/log/php-fpm/centreon-error.log
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/20-configuration_files.sh
+++ .github/docker/centreon-web/bookworm/entrypoint/container.d/20-configuration_files.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-su www-data -s /bin/bash  -c "centreon -u admin -p Centreon\!2021 -a POLLERGENERATE -v 1"
-su www-data -s /bin/bash  -c "centreon -u admin -p Centreon\!2021 -a CFGMOVE -v 1"
+echo "Generating poller configuration files..."
+su www-data -s /bin/bash -c "centreon -u admin -p Centreon\!2021 -a POLLERGENERATE -v 1" 2>&1
+
+echo "Copying configuration files to engine/broker directories..."
+su www-data -s /bin/bash -c "centreon -u admin -p Centreon\!2021 -a CFGMOVE -v 1"
