f8ace2b60f41a78d884bcc7f5e58ba88ee64260d
alaunois <alaunois@centreon.com>
2026-03-27 10:06:59 +0100

    tmp

--- .github/docker/centreon-web/alma9/entrypoint/container.d/05-php_background.sh
+++ .github/docker/centreon-web/alma9/entrypoint/container.d/05-php_background.sh
@@ -5,5 +5,8 @@ echo "Starting PHP-FPM..."
 # Disable systemd notifications (not available in containers)
 sed -i 's/^systemd_interval\s*=.*/systemd_interval = 0/' /etc/php-fpm.conf 2>/dev/null || true
 
+# Expose _CENTREON_LOG_ as an OS env var so Symfony %env(_CENTREON_LOG_)% can resolve it
+sed -i '/^\[www\]/a env[_CENTREON_LOG_] = /var/log/centreon' /etc/php-fpm.d/www.conf
+
 systemctl start php-fpm
 echo "PHP-FPM started."
