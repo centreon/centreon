c89756cc34c578ce24ee367b9d8649172bf430da
Julien Mathis <jmathis@centreon.com>
2026-03-31 10:41:42 +0200

    feat(configuration): modernize traps, manufacturers and trap groups listings
    Replace the legacy Smarty-rendered SNMP traps, manufacturers and trap
    groups listings with AJAX-driven tables using the shared CentreonListing
    framework. Groups three related pages in a single PR.
    
    Changes per page:
    
    SNMP Traps listing:
    - ajaxTrapsListing.php: ACL (page 61701), OID and status columns,
      sanitized search, prepared statements
    - listTraps.ihtml/php: modern template + simplified controller
    
    Manufacturers listing:
    - ajaxMnftrListing.php: ACL (page 61702), sanitized search
    - listMnftr.ihtml/php: modern template + simplified controller
    
    Trap groups listing:
    - ajaxGroupsListing.php: ACL (page 61705), sanitized search
    - listGroups.ihtml/php: modern template + simplified controller
    
    No toggles — none of these objects have an activate field.
    
    Test coverage (11 Cypress scenarios):
    - Traps: AJAX load, search, duplicate, delete, session persistence
    - Manufacturers: AJAX load, search, duplicate
    - Trap groups: AJAX load, search, duplicate
    
    Depends on: feature/configuration-legacy-listing-shared-library
    
    (cherry picked from commit 845db9eace97851db77c208b0f3b71d3f64d4bdc)

--- centreon/www/include/configuration/configObject/traps-groups/listGroups.php
+++ centreon/www/include/configuration/configObject/traps-groups/listGroups.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
  *
@@ -24,142 +25,56 @@ if (! isset($centreon)) {
 
 include './include/common/autoNumLimit.php';
 
-$mnftr_id = null;
-
-$search = HtmlAnalyzer::sanitizeAndRemoveTags(
-    $_POST['searchTM'] ?? $_GET['searchTM'] ?? null
-);
-
-if (isset($_POST['searchTM']) || isset($_GET['searchTM'])) {
-    // saving filters values
-    $centreon->historySearch[$url] = [];
-    $centreon->historySearch[$url]['search'] = $search;
-} else {
-    // restoring saved values
-    $search = $centreon->historySearch[$url]['search'] ?? null;
-}
-
-$searchTool = null;
-if ($search) {
-    $searchTool = ' WHERE (traps_group_name LIKE :search )';
-}
-$statement = $pearDB->prepare('SELECT SQL_CALC_FOUND_ROWS * FROM traps_group ' . $searchTool
-    . ' ORDER BY traps_group_name LIMIT  :offset, :limit');
-if ($search) {
-    $statement->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
-}
-$statement->bindValue(':offset', (int) $num * (int) $limit, PDO::PARAM_INT);
-$statement->bindValue(':limit', $limit, PDO::PARAM_INT);
-$statement->execute();
-
-$rows = $pearDB->query('SELECT FOUND_ROWS()')->fetchColumn();
-
-include './include/common/checkPagination.php';
-
-// Smarty template initialization
 $tpl = SmartyBC::createSmartyTemplate($path);
 
-// Access level
 $lvl_access = ($centreon->user->access->page($p) == 1) ? 'w' : 'r';
 $tpl->assign('mode_access', $lvl_access);
 
-// start header menu
 $tpl->assign('headerMenu_name', _('Group Name'));
 $tpl->assign('headerMenu_options', _('Options'));
 
-// List of elements - Depends on different criteria
-$form = new HTML_QuickFormCustom('form', 'POST', '?p=' . $p);
+$tpl->assign('grpPage', $p);
 
-// Different style between each lines
-$style = 'one';
-
-$attrBtnSuccess = ['class' => 'btc bt_success', 'onClick' => "window.history.replaceState('', '', '?p=" . $p . "');"];
-$form->addElement('submit', 'Search', _('Search'), $attrBtnSuccess);
-
-// Fill a tab with a multidimensional Array we put in $tpl
-$elemArr = [];
-for ($i = 0; $group = $statement->fetch(PDO::FETCH_ASSOC); $i++) {
-    $moptions = '';
-    $selectedElements = $form->addElement('checkbox', 'select[' . $group['traps_group_id'] . ']');
-    $moptions = '&nbsp;<input onKeypress="if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) '
-        . 'event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;" '
-        . "maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr["
-        . $group['traps_group_id'] . "]' />";
-    $elemArr[$i] = ['MenuClass' => 'list_' . $style, 'RowMenu_select' => $selectedElements->toHtml(), 'RowMenu_name' => myDecode($group['traps_group_name']), 'RowMenu_link' => 'main.php?p=' . $p . '&o=c&id=' . $group['traps_group_id'], 'RowMenu_options' => $moptions];
-    $style = $style != 'two' ? 'two' : 'one';
-}
-$tpl->assign('elemArr', $elemArr);
-
-// Different messages we put in the template
-$tpl->assign(
-    'msg',
-    ['addL' => 'main.php?p=' . $p . '&o=a', 'addT' => _('Add'), 'delConfirm' => _('Do you confirm the deletion ?')]
-);
-
-// Toolbar select
-?>
-<script type="text/javascript">
-    function setO(_i) {
-        document.forms['form'].elements['o'].value = _i;
-    }
-</script>
-<?php
-$attrs1 = ['onchange' => 'javascript: '
-    . ' var bChecked = isChecked(); '
-    . " if (this.form.elements['o1'].selectedIndex != 0 && !bChecked) {"
-    . " alert('" . _('Please select one or more items') . "'); return false;} "
-    . "if (this.form.elements['o1'].selectedIndex == 1 && confirm('"
-    . _('Do you confirm the duplication ?') . "')) {"
-    . "   setO(this.form.elements['o1'].value); submit();} "
-    . "else if (this.form.elements['o1'].selectedIndex == 2 && confirm('"
-    . _('Do you confirm the deletion ?') . "')) {"
-    . "   setO(this.form.elements['o1'].value); submit();} "
-    . "else if (this.form.elements['o1'].selectedIndex == 3) {"
-    . "   setO(this.form.elements['o1'].value); submit();} "
-    . ''];
-$form->addElement(
-    'select',
-    'o1',
-    null,
-    [null => _('More actions...'), 'm' => _('Duplicate'), 'd' => _('Delete')],
-    $attrs1
-);
-$form->setDefaults(['o1' => null]);
-
-$attrs2 = ['onchange' => 'javascript: '
-    . ' var bChecked = isChecked(); '
-    . " if (this.form.elements['o2'].selectedIndex != 0 && !bChecked) {"
-    . " alert('" . _('Please select one or more items') . "'); return false;} "
-    . "if (this.form.elements['o2'].selectedIndex == 1 && confirm('"
-    . _('Do you confirm the duplication ?') . "')) {"
-    . "   setO(this.form.elements['o2'].value); submit();} "
-    . "else if (this.form.elements['o2'].selectedIndex == 2 && confirm('"
-    . _('Do you confirm the deletion ?') . "')) {"
-    . "   setO(this.form.elements['o2'].value); submit();} "
-    . "else if (this.form.elements['o2'].selectedIndex == 3) {"
-    . "   setO(this.form.elements['o2'].value); submit();} "
-    . ''];
-$form->addElement(
-    'select',
-    'o2',
-    null,
-    [null => _('More actions...'), 'm' => _('Duplicate'), 'd' => _('Delete')],
-    $attrs2
-);
-$form->setDefaults(['o2' => null]);
-
-$o1 = $form->getElement('o1');
-$o1->setValue(null);
-$o1->setSelected(null);
-
-$o2 = $form->getElement('o2');
-$o2->setValue(null);
-$o2->setSelected(null);
-
-$tpl->assign('limit', $limit);
+$search = $centreon->historySearch[$url]['search'] ?? '';
 $tpl->assign('searchTM', $search);
 
-// Apply a template definition
+$dbResult = $pearDB->query("SELECT * FROM `options` WHERE `key` = 'maxViewConfiguration'");
+$gopt = $dbResult->fetch();
+$defaultLimit = (int) ($gopt['value'] ?? 30) ?: 30;
+$tpl->assign('defaultLimit', $defaultLimit);
+
+$form = new HTML_QuickFormCustom('select_form', 'POST', '?p=' . $p);
+$attrBtnSuccess = ['class' => 'btc bt_success', 'onClick' => "window.history.replaceState('', '', '?p=" . $p . "');"];
+$form->addElement('submit', 'Search', _('Search'), $attrBtnSuccess);
+$tpl->assign('msg', ['addL' => 'main.php?p=' . $p . '&o=a', 'addT' => _('Add')]);
+
+?>
+<script type="text/javascript">
+    function setO(_i) { document.forms['form'].elements['o'].value = _i; }
+</script>
+<?php
+
+foreach (['o1', 'o2'] as $option) {
+    $attrs = ['onchange' => 'javascript: '
+        . ' var bChecked = isChecked(); '
+        . " if (this.form.elements['" . $option . "'].selectedIndex != 0 && !bChecked) {"
+        . " alert('" . _('Please select one or more items') . "'); return false;} "
+        . "if (this.form.elements['" . $option . "'].selectedIndex == 1 && confirm('"
+        . _('Do you confirm the duplication ?') . "')) {"
+        . "     setO(this.form.elements['" . $option . "'].value); submit();} "
+        . "else if (this.form.elements['" . $option . "'].selectedIndex == 2 && confirm('"
+        . _('Do you confirm the deletion ?') . "')) {"
+        . "     setO(this.form.elements['" . $option . "'].value); submit();} "
+        . "this.form.elements['" . $option . "'].selectedIndex = 0"];
+    $form->addElement('select', $option, null,
+        [null => _('More actions...'), 'm' => _('Duplicate'), 'd' => _('Delete')], $attrs);
+    $form->setDefaults([$option => null]);
+    $el = $form->getElement($option);
+    $el->setValue(null);
+    $el->setSelected(null);
+}
+
+$tpl->assign('limit', $limit);
 $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 $form->accept($renderer);
 $tpl->assign('form', $renderer->toArray());
