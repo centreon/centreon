e55be3816e8e4c50c55267982defb0a92066dd34
pkippes <144150042+pkippes@users.noreply.github.com>
2026-04-01 11:59:29 +0200

    Update insertBaseConf.sql

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.01.1');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.01.2');
 
 --
 -- Contenu de la table `contact`
