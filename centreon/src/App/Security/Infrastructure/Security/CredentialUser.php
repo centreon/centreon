f3905ca52233b85e0c253cd93b2d4e0680cd976e
alaunois <alaunois@centreon.com>
2026-04-07 10:57:50 +0200

    fix(symfony): mark eraseCredentials() as deprecated
    `UserInterface::eraseCredentials()` is deprecated in Symfony 7.3 and
    will be removed from the interface in Symfony 8. Both implementations
    are no-op so there is no logic to move to `__serialize__()`. Adding
    `#[\Deprecated]` signals the deprecation on our side.
    
    Refs: MON-197348

--- centreon/src/App/Security/Infrastructure/Security/CredentialUser.php
+++ centreon/src/App/Security/Infrastructure/Security/CredentialUser.php
@@ -38,6 +38,7 @@ final readonly class CredentialUser implements UserInterface
         return array_map('strval', $this->credential->roles->toArray());
     }
 
+    #[\Deprecated]
     public function eraseCredentials(): void
     {
     }
