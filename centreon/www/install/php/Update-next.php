2cdab00c3b8544977927863acb7369f68627c549
Adrien Morais <amorais@CNTR-MAC-A066.local>
2026-04-08 10:43:53 +0200

    fix(upgrade): move graph template upgrade to 25.10.11

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -32,20 +32,8 @@ $errorMessage = '';
  * @var ConnectionInterface $pearDB
  * @var ConnectionInterface $pearDBO
  */
-$clearDefaultCurveTemplateLegend = function () use ($pearDB, &$errorMessage, $version): void {
-    $errorMessage = 'Unable to clear ds_legend for default curve templates';
-    CentreonLog::create()->info(
-        logTypeId: CentreonLog::TYPE_UPGRADE,
-        message: "UPGRADE - {$version}: Clearing ds_legend for default curve templates",
-    );
-    $pearDB->executeStatement(
-        <<<'SQL'
-            UPDATE `giv_components_template`
-            SET `ds_legend` = NULL
-            WHERE `default_tpl1` = '1'
-            SQL
-    );
-};
+
+// TODO add your functions here
 
 try {
     // DDL statements for real time database
@@ -59,7 +47,7 @@ try {
         $pearDB->startTransaction();
     }
 
-    $clearDefaultCurveTemplateLegend();
+    // TODO add your function calls to update the configuration database data here
 
     $pearDB->commitTransaction();
 
