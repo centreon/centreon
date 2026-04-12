d35e1c3c77bce653937237d611335223c515a2f9
Med1233 <melmeziani-ext@centreon.com>
2026-04-12 22:22:31 +0100

    naming

--- centreon/www/front_src/src/Configuration/AdditionnalConnectors/Specs/ACC.cypress.spec.tsx
+++
@@ -1,9 +0,0 @@
-import Filters from './Acc.Filters';
-import Listing from './Acc.Listing';
-import Modal from './Acc.Modal';
-
-describe('Additional connectors configuration: ', () => {
-  Listing();
-  Filters();
-  Modal();
-});
---
+++ centreon/www/front_src/src/Configuration/AdditionnalConnectors/Specs/AdditionalConnectors.cypress.spec.tsx
@@ -0,0 +1,9 @@
+import Filters from './Filters';
+import Listing from './Listing';
+import Modal from './Modal';
+
+describe('Additional connectors configuration: ', () => {
+  Listing();
+  Filters();
+  Modal();
+});
