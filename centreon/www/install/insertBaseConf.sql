bcedb5a98d85d394156b6707f72b192ee74fe26b
Pieter Kippes <pkippes@centreon.com>
2026-04-13 14:20:15 +0200

    chore(release) bump oss 26.04.0

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.03.0');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.04.0');
 
 --
 -- Contenu de la table `contact`
