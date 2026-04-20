963e3ce8a025864c335930007618489d8936af72
selfakiri <selfakiri@ext.centreon.com>
2026-04-20 16:47:34 +0100

    feat(pollers): add poller_type in nagios_server (#10208)
    Co-authored-by: Sokayna El Hassani <selhassani@ext.centreon.com>

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -863,6 +863,39 @@ $deleteOldCommandsTopologies = function () use ($pearDB, &$errorMessage, $versio
     );
 };
 
+/** ------------------------------------- Pollers ------------------------------------- */
+$addPollerTypeColumn = function () use ($pearDB, &$errorMessage, $version): void {
+    $errorMessage = 'Unable to add poller_type column to nagios_server';
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: Adding poller_type column to nagios_server",
+    );
+
+    if ($pearDB->columnExists(
+        $pearDB->getConnectionConfig()->getDatabaseNameConfiguration(),
+        'nagios_server',
+        'poller_type'
+    )) {
+        CentreonLog::create()->info(
+            logTypeId: CentreonLog::TYPE_UPGRADE,
+            message: "UPGRADE - {$version}: Column poller_type already exists on nagios_server, skipping",
+        );
+
+        return;
+    }
+
+    $pearDB->executeStatement(
+        <<<'SQL'
+            ALTER TABLE `nagios_server` ADD COLUMN `poller_type` enum('vm','docker') NOT NULL DEFAULT 'vm'
+            SQL
+    );
+
+    CentreonLog::create()->info(
+        logTypeId: CentreonLog::TYPE_UPGRADE,
+        message: "UPGRADE - {$version}: Successfully added poller_type column to nagios_server",
+    );
+};
+
 try {
     // DDL statements for real time database
     $pearDBO->executeStatement(
@@ -872,7 +905,7 @@ try {
     );
 
     // DDL statements for configuration database
-    // TODO add your function calls to update the configuration database structure here
+    $addPollerTypeColumn();
 
     // Transactional queries for configuration database
     if (! $pearDB->isTransactionActive()) {
