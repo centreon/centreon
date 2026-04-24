5d6f66b83a9f8f6cd302bfa8b43b8761e1f1f7e0
Kevin Duret <kduret@centreon.com>
2026-04-24 11:58:23 +0200

    fix

--- .github/docker/centreon-web/trixie/entrypoint/container.d/05-php_background.sh
+++ .github/docker/centreon-web/trixie/entrypoint/container.d/05-php_background.sh
@@ -5,7 +5,7 @@ echo "Starting PHP-FPM..."
 mkdir -p /run/php
 
 # Disable systemd notifications (not available in containers)
-sed -i 's/^systemd_interval\s*=.*/systemd_interval = 0/' /etc/php/8.4/fpm/php-fpm.conf 2>/dev/null || true
+sed -i "s/^systemd_interval\s*=.*/systemd_interval = 0/" /etc/php/${PHP_VERSION}/fpm/php-fpm.conf 2>/dev/null || true
 
-systemctl start php8.4-fpm
+systemctl start php${PHP_VERSION}-fpm
 echo "PHP-FPM started."
