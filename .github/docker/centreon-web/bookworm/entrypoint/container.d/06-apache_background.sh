5d8a0f594abc51f0b98483e8cec345b034697cf2
Kevin Duret <kduret@centreon.com>
2026-04-24 14:01:52 +0200

    fix

--- .github/docker/centreon-web/bookworm/entrypoint/container.d/06-apache_background.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Starting Apache (apache2)..."
-systemctl start apache2
-echo "Apache (apache2) started."
--- .github/docker/centreon-web/bookworm/entrypoint/container.d/61-snmpd_background.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Starting SNMP daemon..."
-systemctl start snmpd
-echo "SNMP daemon started."
