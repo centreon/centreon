151cd2acea54c59749536b952a6813881a7a1f97
Maud <110405507+mushroomempires@users.noreply.github.com>
2026-04-27 10:29:48 +0200

    chore(release) bump hotfix web 26.03.3 (#10287)

--- centreon/www/install/insertBaseConf.sql
+++ centreon/www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.03.2');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '26.03.3');
 
 --
 -- Contenu de la table `contact`
