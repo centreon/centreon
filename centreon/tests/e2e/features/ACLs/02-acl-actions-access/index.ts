fb2543eb3e93d51ee24122bcb520dcb6edcd1ae8
hamzabessa <hbessa@ext.centreon.com>
2026-04-28 09:42:36 +0100

    fixes

--- centreon/tests/e2e/features/ACLs/02-acl-actions-access/index.ts
+++ centreon/tests/e2e/features/ACLs/02-acl-actions-access/index.ts
@@ -2,7 +2,6 @@ import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor';
 import { INTERCEPTORS } from 'fixtures/shared/constants/interceptors';
 import { PAGES } from 'fixtures/shared/constants/pages';
 
-import { PAGES } from 'e2e/fixtures/shared/constants/pages';
 import data from '../../../fixtures/acls/acl-data.json';
 import { AclActionType, Action } from '../commands';
 
