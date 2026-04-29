a1e94acd6d49139721c17ccab02f7e6e245affab
Kevin Duret <kduret@centreon.com>
2026-04-29 10:51:52 +0200

    fix

--- .github/docker/glpi/entrypoint.sh
+++ .github/docker/glpi/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+mkdir -p /etc/mysql/conf.d
+printf '[client]\nskip-ssl\n' > /etc/mysql/conf.d/client-no-ssl.cnf
+
+# Wait for the database to be up and running.
+echo "Waiting for DB server to be ready..."
 while true ; do
-  timeout 20 mysql -h${MYSQL_HOST} -uroot -p"${MYSQL_ROOT_PASSWORD}" -e 'SELECT User FROM user' mysql
-  retval=$?
-  if [ "$retval" = 0 ] ; then
-    echo 'DB server is running.'
-    break ;
-  else
-    echo 'DB server is not yet responding.'
-    sleep 1
+  if mysqladmin -h"${MYSQL_HOST}" -uroot -p"${MYSQL_ROOT_PASSWORD}" ping --connect-timeout=5 ; then
+    echo "DB server is running."
+    break
   fi
+  sleep 1
 done
 
 su - www-data -s /bin/bash -c "php /var/www/html/glpi/bin/console database:install --db-host=${MYSQL_HOST} --db-name=glpi --db-user=root --db-password=${MYSQL_ROOT_PASSWORD} --no-interaction"
