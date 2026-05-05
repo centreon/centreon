3d3ae8dd7108a5146dd098d66775377b6068ba8e
jeremyjaouen <jjaouen@centreon.com>
2026-05-05 14:54:30 +0200

    fix(vault): cache vault auth token and GET responses to reduce HTTP calls

--- centreon/www/class/config-generate/broker.class.php
+++ centreon/www/class/config-generate/broker.class.php
@@ -445,7 +445,9 @@ class Broker extends AbstractObjectJSON
             }
 
             if ($this->isVaultEnabled && $this->readVaultRepository !== null) {
-                $this->resolveVaultOutputs($object);
+                foreach ($object['output'] as $outputIndex => $output) {
+                    $this->processVaultOutput($output, $outputIndex, $object);
+                }
             }
             $shouldBeEncrypted = $this->readMonitoringServerRepository->isEncryptionReady($pollerId);
             $dbSslEnabled = null;
@@ -825,55 +827,72 @@ class Broker extends AbstractObjectJSON
     }
 
     /**
-     * Collect all vault paths from broker outputs and resolve them in a single multiplexed request.
+     * Process vault output and update object with vault data.
      *
+     * @param array $output
+     * @param int $outputIndex
      * @param array $object
      * @return void
      */
-    private function resolveVaultOutputs(array &$object): void
+    private function processVaultOutput(array &$output, int $outputIndex, array &$object): void
     {
-        $vaultPaths = [];
-        foreach ($object['output'] as $outputIndex => $output) {
-            foreach ($output as $outputKey => $outputValue) {
-                if (is_string($outputValue) && $this->isAVaultPath($outputValue)) {
-                    $vaultPaths[$outputIndex . '::' . $outputKey] = $outputValue;
-                }
-                if ($outputKey === 'lua_parameter' && is_array($outputValue)) {
-                    foreach ($outputValue as $parameterIndex => $luaParameter) {
-                        if ($luaParameter['type'] === 'password'
-                            && is_string($luaParameter['value'])
-                            && $this->isAVaultPath($luaParameter['value'])
-                        ) {
-                            $vaultPaths[$outputIndex . '::lua::' . $parameterIndex] = $luaParameter['value'];
-                        }
-                    }
-                }
+        foreach ($output as $outputKey => $outputValue) {
+            if (is_string($outputValue) && $this->isAVaultPath($outputValue)) {
+                $this->updateVaultData($output, $outputKey, $outputValue, $object['output'][$outputIndex]);
+            }
+
+            if ($outputKey === 'lua_parameter' && is_array($outputValue)) {
+                $this->processLuaParameters($output, $outputKey, $outputValue, $object['output'][$outputIndex]);
             }
         }
+    }
 
-        if ($vaultPaths === []) {
-            return;
+    /**
+     * Update vault data for a given key.
+     *
+     * @param array $output
+     * @param string $outputKey
+     * @param string $outputValue
+     * @param array $outputReference
+     * @return void
+     */
+    private function updateVaultData(
+        array &$output,
+        string $outputKey,
+        string $outputValue,
+        array &$outputReference,
+    ): void {
+        $vaultData = $this->readVaultRepository->findFromPath($outputValue);
+        $vaultKey = $output['name'] . '_' . $outputKey;
+        if (array_key_exists($vaultKey, $vaultData)) {
+            $outputReference[$outputKey] = $vaultData[$vaultKey];
         }
+    }
 
-        $vaultData = $this->readVaultRepository->findFromPaths($vaultPaths);
-
-        foreach ($vaultData as $compositeKey => $data) {
-            $parts = explode('::', $compositeKey);
-            $outputIndex = (int) $parts[0];
-            $output = $object['output'][$outputIndex];
-
-            if (isset($parts[1]) && $parts[1] === 'lua') {
-                $parameterIndex = (int) $parts[2];
-                $luaParameter = $output['lua_parameter'][$parameterIndex];
-                $vaultKey = $output['name'] . '_lua_parameter_' . $luaParameter['name'];
-                if (array_key_exists($vaultKey, $data)) {
-                    $object['output'][$outputIndex]['lua_parameter'][$parameterIndex]['value'] = $data[$vaultKey];
-                }
-            } else {
-                $outputKey = $parts[1];
-                $vaultKey = $output['name'] . '_' . $outputKey;
-                if (array_key_exists($vaultKey, $data)) {
-                    $object['output'][$outputIndex][$outputKey] = $data[$vaultKey];
+    /**
+     * Process Lua parameters and update with vault data if applicable.
+     *
+     * @param array $output
+     * @param string $outputKey
+     * @param array $luaParameters
+     * @param array $outputReference
+     * @return void
+     */
+    private function processLuaParameters(
+        array &$output,
+        string $outputKey,
+        array $luaParameters,
+        array &$outputReference,
+    ): void {
+        foreach ($luaParameters as $parameterIndex => $luaParameter) {
+            if ($luaParameter['type'] === 'password'
+                && is_string($luaParameter['value'])
+                && $this->isAVaultPath($luaParameter['value'])
+            ) {
+                $vaultData = $this->readVaultRepository->findFromPath($luaParameter['value']);
+                $vaultKey = $output['name'] . '_' . $outputKey . '_' . $luaParameter['name'];
+                if (array_key_exists($vaultKey, $vaultData)) {
+                    $outputReference[$outputKey][$parameterIndex]['value'] = $vaultData[$vaultKey];
                 }
             }
         }
