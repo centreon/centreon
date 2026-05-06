e0610be47a2b25a34c33b75c983d967e8b9f5c5e
Mohamed El Meziani <melmeziani@ext.centreon.com>
2026-05-06 18:15:45 +0100

    add aria labes

--- centreon/www/front_src/src/AgentConfiguration/Form/ConnectionInitiated/Tabs.cypress.spec.tsx
+++ centreon/www/front_src/src/AgentConfiguration/Form/ConnectionInitiated/Tabs.cypress.spec.tsx
@@ -2,8 +2,8 @@ import { Tabs } from './Tabs';
 
 const defaultTabs = [
   { ariaLabel: 'Tab 1', label: 'Tab 1', value: 'tab1' },
-  { ariaLabel: 'Tab 1', label: 'Tab 2', value: 'tab2' },
-  { ariaLabel: 'Tab 1', label: 'Tab 3', value: 'tab3' }
+  { ariaLabel: 'Tab 2', label: 'Tab 2', value: 'tab2' },
+  { ariaLabel: 'Tab 2', label: 'Tab 3', value: 'tab3' }
 ];
 
 const defaultChildren = [
