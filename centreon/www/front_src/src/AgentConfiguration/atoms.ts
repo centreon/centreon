75749721579e32d103b33919f476888d59aada12
Mohamed El Meziani <melmeziani@ext.centreon.com>
2026-05-07 19:42:39 +0100

    fix errors due to new rules

--- centreon/www/front_src/src/AgentConfiguration/atoms.ts
+++ centreon/www/front_src/src/AgentConfiguration/atoms.ts
@@ -67,10 +67,12 @@ interface DeleteFilterProps {
 export const deleteFilterEntryAtom = atom(
   null,
   (get, set, { field, entryToDelete }: DeleteFilterProps) => {
-    const fieldEntries = get(filtersAtom)[field];
+    const fieldEntries = (get(filtersAtom) as Record<string, unknown>)[
+      field
+    ] as Array<SelectEntry>;
 
     const entryToDeleteIndex = findIndex(
-      ({ id }: (typeof fieldEntries)[number]) => equals(entryToDelete.id, id),
+      ({ id }: SelectEntry) => equals(entryToDelete.id, id),
       fieldEntries
     );
 
