a7c24e62c80279fec82c48f9160953adbc268760
jeremyjaouen <jjaouen@centreon.com>
2026-05-20 14:58:47 +0200

    fix(test): revert test infrastructure changes to match develop

--- centreon/tests/php/integration-bootstrap.php
+++ centreon/tests/php/integration-bootstrap.php
@@ -24,7 +24,7 @@ declare(strict_types=1);
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 require dirname(__DIR__, 2) . '/config/bootstrap.php';
 
-if (! defined('INTEGRATION_BOOTSTRAP') || ! INTEGRATION_BOOTSTRAP) {
+if (! defined('INTEGRATION_BOOTSTRAP') || INTEGRATION_BOOTSTRAP !== true) {
     return;
 }
 
