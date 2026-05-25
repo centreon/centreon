90e65cecd70e478b35e93025975afa9b8a2a172f
Anthony Graule <agraule@centreon.com>
2026-05-25 10:21:42 +0200

    feat(dev): enable Symfony DebugBundle in dev/test environments (#10435)

--- centreon/config/bundles.php
+++ centreon/config/bundles.php
@@ -29,6 +29,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
