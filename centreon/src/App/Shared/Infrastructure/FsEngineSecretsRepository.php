b3b5ba741eced9afbb623c683f3024c7c22a23a5
alaunois <alaunois@centreon.com>
2026-06-02 11:41:20 +0200

    feat(MON-197154): add EngineSecretsRepository with filesystem implementation

---
+++ centreon/src/App/Shared/Infrastructure/FsEngineSecretsRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure;
+
+use App\Shared\Domain\Repository\EngineSecretsRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+final readonly class FsEngineSecretsRepository implements EngineSecretsRepository
+{
+    public function __construct(
+        #[Autowire(param: 'upgrade.engine_context_path')]
+        private string $engineContextPath,
+    ) {
+    }
+
+    public function getAppSecret(): string
+    {
+        return $this->readKey('app_secret');
+    }
+
+    public function getSalt(): string
+    {
+        return $this->readKey('salt');
+    }
+
+    private function readKey(string $key): string
+    {
+        $content = file_get_contents($this->engineContextPath);
+        $data = json_decode(is_string($content) ? $content : '', true, flags: JSON_THROW_ON_ERROR);
+
+        if (! is_array($data)) {
+            throw new \JsonException('Engine context file does not contain a valid JSON object.');
+        }
+
+        $value = $data[$key] ?? null;
+
+        if (! is_string($value)) {
+            throw new \JsonException(sprintf('Missing or invalid key "%s" in engine context file.', $key));
+        }
+
+        return $value;
+    }
+}
---
+++ centreon/tests/php/App/Shared/Infrastructure/FsEngineSecretsRepositoryTest.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * Copyright 2005 - 2025 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tests\App\Shared\Infrastructure;
+
+use App\Shared\Infrastructure\FsEngineSecretsRepository;
+use PHPUnit\Framework\TestCase;
+
+final class FsEngineSecretsRepositoryTest extends TestCase
+{
+    private string $tempFile;
+
+    protected function setUp(): void
+    {
+        $this->tempFile = tempnam(sys_get_temp_dir(), 'engine_context_');
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->tempFile)) {
+            unlink($this->tempFile);
+        }
+    }
+
+    public function testGetAppSecretReturnsValue(): void
+    {
+        file_put_contents($this->tempFile, json_encode([
+            'app_secret' => 'my-app-secret',
+            'salt' => 'my-salt',
+        ]));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        self::assertSame('my-app-secret', $repository->getAppSecret());
+    }
+
+    public function testGetSaltReturnsValue(): void
+    {
+        file_put_contents($this->tempFile, json_encode([
+            'app_secret' => 'my-app-secret',
+            'salt' => 'my-salt',
+        ]));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        self::assertSame('my-salt', $repository->getSalt());
+    }
+
+    public function testThrowsWhenFileContainsInvalidJson(): void
+    {
+        file_put_contents($this->tempFile, 'not-valid-json');
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        $this->expectException(\JsonException::class);
+        $repository->getAppSecret();
+    }
+
+    public function testThrowsWhenAppSecretKeyIsMissing(): void
+    {
+        file_put_contents($this->tempFile, json_encode(['salt' => 'my-salt']));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        $this->expectException(\JsonException::class);
+        $repository->getAppSecret();
+    }
+
+    public function testThrowsWhenSaltKeyIsMissing(): void
+    {
+        file_put_contents($this->tempFile, json_encode(['app_secret' => 'my-app-secret']));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        $this->expectException(\JsonException::class);
+        $repository->getSalt();
+    }
+
+    public function testThrowsWhenAppSecretIsNotAString(): void
+    {
+        file_put_contents($this->tempFile, json_encode(['app_secret' => 42, 'salt' => 'my-salt']));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        $this->expectException(\JsonException::class);
+        $repository->getAppSecret();
+    }
+
+    public function testThrowsWhenFileContainsJsonArray(): void
+    {
+        file_put_contents($this->tempFile, json_encode(['my-app-secret', 'my-salt']));
+
+        $repository = new FsEngineSecretsRepository($this->tempFile);
+
+        $this->expectException(\JsonException::class);
+        $repository->getAppSecret();
+    }
+}
