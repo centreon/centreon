a9f8991db4c7d60ffb193a771ac047b3b9885766
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2026-06-03 16:21:06 +0200

    feat(test): api: port legacy API v1 tests to Bruno (backport 25.10) (#10559)

---
+++ centreon/tests/api/web-api-workspace/fixtures/modules/fake-module-to-test-api.conf.php
@@ -0,0 +1,12 @@
+<?php
+
+$module_conf['fake-module-to-test-api']["rname"] = "Fake Module To Test API";
+$module_conf['fake-module-to-test-api']["name"] = "fake-module-to-test-api";
+$module_conf['fake-module-to-test-api']["mod_release"] = "25.10.0";
+$module_conf['fake-module-to-test-api']["infos"] = "Disposable module used only to exercise the module-management API (install/details/remove)";
+$module_conf['fake-module-to-test-api']["is_removeable"] = "1";
+$module_conf['fake-module-to-test-api']["author"] = "Centreon";
+$module_conf['fake-module-to-test-api']["stability"] = "stable";
+$module_conf['fake-module-to-test-api']["last_update"] = "2026-06-02";
+$module_conf['fake-module-to-test-api']["release_note"] = "https://docs.centreon.com";
+$module_conf['fake-module-to-test-api']["images"] = [];
