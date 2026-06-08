3337cab5679a39fa54d4d2cbb0f61a8546f22bcc
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2026-06-08 23:16:09 +0100

    Enh(dashboards) add state to status grid widget (#10536)

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-statusgrid/src/api/endpoints.ts
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-statusgrid/src/api/endpoints.ts
@@ -196,7 +196,8 @@ export const buildCondensedViewEndpoint = ({
   type,
   resources,
   baseEndpoint,
-  statuses
+  statuses,
+  states
 }: BuildResourcesEndpointProps): string => {
   const resourcesToApply = resources.map((resource) => {
     if (!equals(type, resource.resourceType)) {
@@ -230,12 +231,17 @@ export const buildCondensedViewEndpoint = ({
     }
   );
 
+  const state =
+    states && !isEmpty(states) ? [{ name: 'states', value: states }] : [];
+
+  const status =
+    statuses && !isEmpty(statuses)
+      ? [{ name: 'statuses', value: statuses.map(toUpper) }]
+      : [];
+
   return buildListingEndpoint({
     baseEndpoint,
-    customQueryParameters:
-      statuses && !isEmpty(statuses)
-        ? [{ name: 'statuses', value: statuses.map(toUpper) }]
-        : [],
+    customQueryParameters: [...state, ...status],
     parameters: {
       search: {
         conditions: flatten(searchConditions)
