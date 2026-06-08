725eff9c2e9bc49d9e730a3471c2c9f45931227e
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2026-06-08 14:00:51 +0200

    Update centreon/src/App/Shared/Infrastructure/Dbal/TLSConnectionFactory.php

--- centreon/src/App/Shared/Infrastructure/Dbal/TLSConnectionFactory.php
+++ centreon/src/App/Shared/Infrastructure/Dbal/TLSConnectionFactory.php
@@ -33,6 +33,7 @@ use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 #[AsAlias('doctrine.dbal.connection_factory')]
 class TLSConnectionFactory extends ConnectionFactory
 {
+    #[\Override]
     public function createConnection(array $params, Configuration|null $config = null, EventManager|null $eventManager = null, array $mappingTypes = []): Connection
     {
         $tlsOptions = DatabaseTLSResolver::getTLSOptions();
