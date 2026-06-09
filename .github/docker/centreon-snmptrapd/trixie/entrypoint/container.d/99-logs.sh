b63a35aa331fcf0bc649346c8f473a394c1c827d
THEPAUT <fthepaut@centreon.com>
2026-06-09 13:41:20 +0200

    feat(docker): migrate snmptrapd and centreontrapd Docker images to trixie (#10386)

--- .github/docker/centreon-snmptrapd/trixie/entrypoint/container.d/99-logs.sh
+++ .github/docker/centreon-snmptrapd/trixie/entrypoint/container.d/99-logs.sh
@@ -3,3 +3,5 @@ touch /tmp/docker.ready
 echo "[SNMPTRAPD] Centreon snmptrapd is ready"
 echo "[SNMPTRAPD] Listening on UDP/162"
 echo "[SNMPTRAPD] Spool directory: /var/spool/centreontrapd"
+
+exec "$@"
