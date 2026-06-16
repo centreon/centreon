eb531f4b17af61fb4ddfe58bee3638fe85dc3d4c
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2026-06-16 23:21:15 +0200

    test(e2e): awie: remove unused ESLint config from e2e project

--- centreon-awie/tests/e2e/plugins.ts
+++ centreon-awie/tests/e2e/plugins.ts
@@ -1,7 +1,3 @@
-/* eslint-disable default-param-last */
-/* eslint-disable global-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-param-reassign */
 import fs from 'fs';
 import path from 'path';
 
