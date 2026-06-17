ae475bc1c521fd336aac40db577bef8b997add9c
Anthony Graule <agraule@centreon.com>
2026-06-17 15:07:55 +0200

    feat(logging): align authentication events with OWASP security log split (#10413)

--- centreon/www/class/centreon-clapi/centreonAPI.class.php
+++ centreon/www/class/centreon-clapi/centreonAPI.class.php
@@ -21,10 +21,11 @@
 
 namespace CentreonClapi;
 
+use Adaptation\Log\Enum\AuthProviderEnum;
+use Adaptation\Log\LoggerAuthentication;
 use CentreonAuth;
 use CentreonAuthLDAP;
 use CentreonDB;
-use CentreonLog;
 use CentreonUserLog;
 use CentreonXML;
 use DateTime;
@@ -533,10 +534,11 @@ class CentreonAPI
                     );
                     $interval = (date_diff($now, $expirationDate))->format('%Dd %Hh %Im %Ss');
                     echo "Authentication failed.\n";
-                    $CentreonLog = new CentreonLog();
-                    $CentreonLog->insertLog(
-                        1, "Authentication failed for '" . $row['contact_alias'] . "',"
-                        . " max login attempts has been reached. {$interval} left\n"
+                    LoggerAuthentication::create()->loginFailure(
+                        "Authentication failed for '" . $row['contact_alias'] . "',"
+                        . " max login attempts has been reached. {$interval} left",
+                        isset($row['contact_id']) ? (int) $row['contact_id'] : null,
+                        AuthProviderEnum::CLAPI
                     );
 
                     exit(1);
@@ -602,7 +604,8 @@ class CentreonAPI
                 $this->exitOnInvalidCredentials(
                     (int) $row['login_attempts'],
                     (int) $securityPolicy['attempts'],
-                    (int) $securityPolicy['blocking_duration']
+                    (int) $securityPolicy['blocking_duration'],
+                    isset($row['contact_id']) ? (int) $row['contact_id'] : null
                 );
             }
         }
@@ -1366,23 +1369,28 @@ class CentreonAPI
         int $contactLoginAttempts,
         int $securityPolicyAttempts,
         int $blockingDuration,
+        ?int $contactId = null,
     ): void {
-        $CentreonLog = new CentreonLog();
         $loginAttempts = $this->incrementLoginAttempts($contactLoginAttempts);
         if ($loginAttempts === $securityPolicyAttempts) {
             $this->blockLoginForUser();
             echo "Authentication failed.\n";
-            $CentreonLog->insertLog(
-                1,
+            LoggerAuthentication::create()->loginFailure(
                 "Authentication failed. Max attempts has been reached, User can't login for "
-                . "{$blockingDuration} seconds."
+                . "{$blockingDuration} seconds.",
+                $contactId,
+                AuthProviderEnum::CLAPI
             );
 
             exit(1);
         }
         $attemptRemaining = $securityPolicyAttempts - $loginAttempts;
         echo "Authentication failed.\n";
-        $CentreonLog->insertLog(1, "Authentication failed. {$attemptRemaining} attempt(s) remaining");
+        LoggerAuthentication::create()->loginFailure(
+            "Authentication failed. {$attemptRemaining} attempt(s) remaining",
+            $contactId,
+            AuthProviderEnum::CLAPI
+        );
 
         exit(1);
     }
