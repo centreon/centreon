16d8e39e6c22f9248b397a5a3c9ede3d25e6752b
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2026-06-19 10:18:27 +0200

    feat(database): Use TLS Connection (backport #9237) (#10659)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Anthony Graule <agraule@centreon.com>
    Co-authored-by: alaunois <alaunois@centreon.com>

--- centreon/www/install/steps/process/insertBaseConf.php
+++ centreon/www/install/steps/process/insertBaseConf.php
@@ -24,6 +24,7 @@ require_once __DIR__ . '/../../../../bootstrap.php';
 require_once '../functions.php';
 
 use App\Kernel;
+use App\Shared\Infrastructure\Database\DatabaseTLSResolver;
 use CentreonModule\ServiceProvider;
 use Core\AgentConfiguration\Application\UseCase\DeployDefaultAgentConfigurationForPoller\DeployDefaultAgentConfigurationForPoller;
 use Core\AgentConfiguration\Application\UseCase\DeployDefaultAgentConfigurationForPoller\DeployDefaultAgentConfigurationForPollerRequest;
@@ -39,7 +40,8 @@ try {
     $link = new PDO(
         'mysql:host=' . $parameters['address'] . ';port=' . $parameters['port'],
         $parameters['root_user'],
-        $parameters['root_password']
+        $parameters['root_password'],
+        DatabaseTLSResolver::getTLSOptions()
     );
 } catch (PDOException $e) {
     $return['msg'] = $e->getMessage();
