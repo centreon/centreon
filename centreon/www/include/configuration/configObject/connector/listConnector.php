b94da999f066841d3da43be45e2883b4da9f7bb4
Julien Mathis <jmathis@centreon.com>
2026-07-13 00:38:35 +0200

    refactor(configuration): modernize connectors listing with AJAX-driven UI
    Migrate the connectors listing (Configuration > Commands > Connectors)
    to the shared CentreonListing framework, in line with the other
    modernized legacy listings.
    
    - Replace the legacy Smarty table with an AJAX-driven listing:
      ajaxConnectorListing.php serves paginated, searchable rows
      (name / description / command line) and ajaxConnectorToggle.php
      handles enable/disable with CSRF validation and audit logging.
    - Rebuild listConnector.ihtml with the standard toolbar (live search,
      right-aligned pagination), an activation toggle per row and a side
      panel for add / edit.
    - Simplify listConnector.php: a single More actions dropdown, no unused
      search submit and no duplicated bottom toolbar.
    - Read the per-row duplication count from dupNbr in the router.

--- centreon/www/include/configuration/configObject/connector/listConnector.php
+++ centreon/www/include/configuration/configObject/connector/listConnector.php
@@ -19,118 +19,66 @@
  *
  */
 
+if (! isset($centreon)) {
+    exit();
+}
+
 include_once './class/centreonUtils.class.php';
+include './include/common/autoNumLimit.php';
 
-include_once './include/common/autoNumLimit.php';
+$tpl = SmartyBC::createSmartyTemplate($path);
 
-// restoring the pagination if we stay on this menu
-$num = 0;
-if ($centreon->historyLastUrl === $url && isset($_GET['num'])) {
-    $num = $_GET['num'];
-}
+$tpl->assign('mode_access', $lvl_access);
 
-try {
-    $connectorsList = $connectorObj->getList(false, (int) $num, (int) $limit);
+$tpl->assign('headerMenu_name', _('Name'));
+$tpl->assign('headerMenu_description', _('Description'));
+$tpl->assign('headerMenu_command_line', _('Command Line'));
+$tpl->assign('headerMenu_options', _('Options'));
 
-    // Smarty template initialization
-    $tpl = SmartyBC::createSmartyTemplate($path);
+$tpl->assign('connPage', $p);
 
-    $tpl->assign('mode_access', $lvl_access);
+$search = $centreon->historySearch[$url]['search'] ?? '';
+$tpl->assign('searchC', $search);
 
-    $form = new HTML_QuickFormCustom('Form', 'post', '?p=' . $p);
+$dbResult = $pearDB->query("SELECT `value` FROM `options` WHERE `key` = 'maxViewConfiguration'");
+$gopt = $dbResult->fetch();
+$defaultLimit = (int) ($gopt['value'] ?? 30) ?: 30;
+$tpl->assign('defaultLimit', $defaultLimit);
 
-    $tpl->assign(
-        'msg',
-        ['addL' => 'main.php?p=' . $p . '&o=a', 'addT' => _('Add'), 'delConfirm' => _('Do you confirm the deletion ?')]
-    );
+$form = new HTML_QuickFormCustom('select_form', 'POST', '?p=' . $p);
 
-    // Toolbar select
-    foreach (['o1', 'o2'] as $option) {
-        $attrs1 = ['onchange' => 'javascript: '
-            . ' var bChecked = isChecked(); '
-            . " if (this.form.elements['" . $option . "'].selectedIndex != 0 && !bChecked) {"
-            . " alert('" . _('Please select one or more items') . "'); return false;} "
-            . "if (this.form.elements['" . $option . "'].selectedIndex == 1 && confirm('"
-            . _('Do you confirm the duplication ?') . "')) {"
-            . "   setO(this.form.elements['" . $option . "'].value); submit();} "
-            . "else if (this.form.elements['" . $option . "'].selectedIndex == 2 && confirm('"
-            . _('Do you confirm the deletion ?') . "')) {"
-            . "   setO(this.form.elements['" . $option . "'].value); submit();} "
-            . "else if (this.form.elements['" . $option . "'].selectedIndex == 3) {"
-            . "   setO(this.form.elements['" . $option . "'].value); submit();} "
-            . "this.form.elements['" . $option . "'].selectedIndex = 0"];
+$tpl->assign('msg', ['addL' => 'main.php?p=' . $p . '&o=a', 'addT' => _('Add')]);
 
-        $form->addElement(
-            'select',
-            $option,
-            null,
-            [null => _('More actions...'), 'm' => _('Duplicate'), 'd' => _('Delete')],
-            $attrs1
-        );
-        $form->setDefaults([$option => null]);
-        $o1 = $form->getElement($option);
-        $o1->setValue(null);
-        $o1->setSelected(null);
+?>
+<script type="text/javascript">
+    function setO(_i) {
+        document.forms['form'].elements['o'].value = _i;
     }
+</script>
+<?php
 
-    $elemArr = [];
-    $j = 0;
-    $attrsText = ['size' => '2'];
-    $nbConnectors = count($connectorsList);
-    $centreonToken = createCSRFToken();
+// "More actions" dropdown (duplicate / delete) sitting next to the Add button
+$attrs = ['onchange' => 'javascript: '
+    . ' var bChecked = isChecked(); '
+    . " if (this.form.elements['o1'].selectedIndex != 0 && !bChecked) {"
+    . " alert('" . _('Please select one or more items') . "'); return false;} "
+    . "if (this.form.elements['o1'].selectedIndex == 1 && confirm('"
+    . _('Do you confirm the duplication ?') . "')) {"
+    . " 	setO(this.form.elements['o1'].value); submit();} "
+    . "else if (this.form.elements['o1'].selectedIndex == 2 && confirm('"
+    . _('Do you confirm the deletion ?') . "')) {"
+    . " 	setO(this.form.elements['o1'].value); submit();} "
+    . "this.form.elements['o1'].selectedIndex = 0"];
+$form->addElement('select', 'o1', null,
+    [null => _('More actions...'), 'm' => _('Duplicate'), 'd' => _('Delete')], $attrs);
+$form->setDefaults(['o1' => null]);
+$el = $form->getElement('o1');
+$el->setValue(null);
+$el->setSelected(null);
 
-    for ($i = 0; $i < $nbConnectors; $i++) {
-        $result = $connectorsList[$i];
-        $moptions = '';
-        $MyOption = $form->addElement('text', 'options[' . $result['id'] . ']', _('Options'), $attrsText);
-        $form->setDefaults(['options[' . $result['id'] . ']' => '1']);
-        $selectedElements = $form->addElement('checkbox', 'select[' . $result['id'] . ']');
-        if ($result) {
-            if ($lvl_access == 'w') {
-                if ($result['enabled']) {
-                    $moptions = "<a href='main.php?p="
-                        . $p . '&id=' . $result['id'] . '&o=u&limit=' . $limit . '&num=' . $num
-                        . '&centreon_token=' . $centreonToken
-                        . "'><img src='img/icons/disabled.png' class='ico-14 margin_right' border='0' alt='"
-                        . _('Disabled') . "'></a>&nbsp;&nbsp;";
-                } else {
-                    $moptions = "<a href='main.php?p="
-                        . $p . '&id=' . $result['id'] . '&o=s&limit=' . $limit . '&num=' . $num
-                        . '&centreon_token=' . $centreonToken
-                        . "'><img src='img/icons/enabled.png' class='ico-14 margin_right' border='0' alt='"
-                        . _('Enabled') . "'></a>&nbsp;&nbsp;";
-                }
-                $moptions .= '&nbsp;'
-                    . '<input onKeypress="if(event.keyCode > 31 '
-                    . '&& (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false;'
-                    . ' if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;"'
-                    . " maxlength=\"3\" size=\"3\" value='1'"
-                    . " style=\"margin-bottom:0px;\" name='options[" . $result['id'] . "]'></input>";
-                $moptions .= '&nbsp;&nbsp;';
-            } else {
-                $moptions = '&nbsp;';
-            }
+$tpl->assign('limit', $limit);
 
-            $elemArr[$j] = ['RowMenu_select' => $selectedElements->toHtml(), 'RowMenu_link' => 'main.php?p=' . $p . '&o=c&id=' . $result['id'], 'RowMenu_name' => CentreonUtils::escapeSecure($result['name']), 'RowMenu_description' => CentreonUtils::escapeSecure($result['description']), 'RowMenu_command_line' => CentreonUtils::escapeSecure($result['command_line']), 'RowMenu_enabled' => $result['enabled'] ? _('Enabled') : _('Disabled'), 'RowMenu_badge' => $result['enabled'] ? 'service_ok' : 'service_critical', 'RowMenu_options' => $moptions];
-        }
-        $j++;
-    }
-
-    /**
-     * @todo implement
-     */
-    $rows = $connectorObj->count(false);
-
-    include_once './include/common/checkPagination.php';
-
-    $tpl->assign('elemArr', $elemArr);
-    $tpl->assign('p', $p);
-    $tpl->assign('connectorsList', $connectorsList);
-    $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-    $form->accept($renderer);
-    $tpl->assign('form', $renderer->toArray());
-    $tpl->assign('limit', $limit);
-    $tpl->display('listConnector.ihtml');
-} catch (Exception $e) {
-    echo 'Erreur n°' . $e->getCode() . ' : ' . $e->getMessage();
-}
+$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+$form->accept($renderer);
+$tpl->assign('form', $renderer->toArray());
+$tpl->display('listConnector.ihtml');
