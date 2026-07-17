27e1220263d7fbe6c381830be32f2699d074cdd5
Mohamed El Meziani <melmeziani@ext.centreon.com>
2026-07-17 11:46:21 +0100

    use tailwind

--- centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-resourcestable/src/Listing/Columns/Url/index.tsx
+++ centreon/www/front_src/src/Dashboards/SingleInstancePage/Dashboard/Widgets/centreon-widget-resourcestable/src/Listing/Columns/Url/index.tsx
@@ -3,24 +3,6 @@ import { Avatar, Tooltip } from '@mui/material';
 import { IconButton } from '@centreon/ui';
 
 import { isEmpty, isNil } from 'ramda';
-import { makeStyles } from 'tss-react/mui';
-
-const useStyles = makeStyles()((theme) => ({
-  avatar: {
-    backgroundColor: theme.palette.primary.main,
-    fontSize: theme.typography.body2.fontSize,
-    height: theme.spacing(2),
-    width: theme.spacing(2)
-  },
-  button: {
-    padding: 0
-  },
-  column: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    width: '100%'
-  }
-}));
 
 interface Props {
   avatarTitle?: string;
@@ -35,8 +17,6 @@ const UrlColumn = ({
   icon,
   avatarTitle
 }: Props): JSX.Element | null => {
-  const { classes } = useStyles();
-
   const isEndpointEmpty = isNil(endpoint) || isEmpty(endpoint);
   const isTitleEmpty = isNil(title) || isEmpty(title);
 
@@ -46,16 +26,21 @@ const UrlColumn = ({
 
   if (isEndpointEmpty) {
     return (
-      <div className={classes.column}>
-        <Tooltip className={classes.avatar} title={title as string}>
-          <Avatar data-testid={title}>{avatarTitle}</Avatar>
+      <div className="flex w-full justify-start">
+        <Tooltip title={title as string}>
+          <Avatar
+            className="h-4 w-4 bg-primary-main text-xs"
+            data-testid={title}
+          >
+            {avatarTitle}
+          </Avatar>
         </Tooltip>
       </div>
     );
   }
 
   return (
-    <div className={classes.column}>
+    <div className="flex w-full justify-start">
       <a
         href={endpoint}
         onClick={(e): void => {
@@ -64,7 +49,7 @@ const UrlColumn = ({
       >
         <IconButton
           ariaLabel={title}
-          className={classes.button}
+          className="p-0"
           data-testid={title || endpoint}
           onClick={(): null => {
             return null;
