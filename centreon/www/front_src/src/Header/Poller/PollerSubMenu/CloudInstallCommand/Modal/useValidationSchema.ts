391d8194dc80f855044ba8b898fbc79ce930f585
Mohamed El Meziani <melmeziani@ext.centreon.com>
2026-07-24 12:20:23 +0100

    add validation for centralAdress

--- centreon/www/front_src/src/Header/Poller/PollerSubMenu/CloudInstallCommand/Modal/useValidationSchema.ts
+++ centreon/www/front_src/src/Header/Poller/PollerSubMenu/CloudInstallCommand/Modal/useValidationSchema.ts
@@ -1,17 +1,24 @@
+import { platformFeaturesAtom } from '@centreon/ui-context';
+
+import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
 import { mixed, object, string } from 'yup';
 
 import { PollerEnvironment } from '../models';
 
 export const useValidationSchema = () => {
+  const platformFeatures = useAtomValue(platformFeaturesAtom);
+
   return useMemo(
     () =>
       object({
+        centralAddress: platformFeatures?.isCloudPlatform
+          ? string()
+          : string().trim().required(),
         environment: mixed<PollerEnvironment>()
           .oneOf(Object.values(PollerEnvironment))
           .required(),
         pollerAddress: string().trim().required(),
-        centralAddress: string().trim().required(),
         pollerName: string().trim().required(),
         token: object({
           id: string().required(),
