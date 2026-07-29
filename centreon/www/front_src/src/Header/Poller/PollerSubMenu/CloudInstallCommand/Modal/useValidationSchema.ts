8dcd8e79138058294a36d8e2847c23c7492c72ad
Mohamed El Meziani <109956462+Med1233@users.noreply.github.com>
2026-07-29 10:56:24 +0100

    Enh[Configuration] Add central address at poller creation (#11156)
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>

--- centreon/www/front_src/src/Header/Poller/PollerSubMenu/CloudInstallCommand/Modal/useValidationSchema.ts
+++ centreon/www/front_src/src/Header/Poller/PollerSubMenu/CloudInstallCommand/Modal/useValidationSchema.ts
@@ -1,12 +1,20 @@
+import { platformFeaturesAtom } from '@centreon/ui-context';
+
+import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
 import { mixed, object, string } from 'yup';
 
 import { PollerEnvironment } from '../models';
 
 export const useValidationSchema = () => {
+  const platformFeatures = useAtomValue(platformFeaturesAtom);
+
   return useMemo(
     () =>
       object({
+        centralAddress: platformFeatures?.isCloudPlatform
+          ? string()
+          : string().trim().required(),
         environment: mixed<PollerEnvironment>()
           .oneOf(Object.values(PollerEnvironment))
           .required(),
