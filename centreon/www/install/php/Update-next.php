1896e911f5f0662cd3788bbfab94d3f9d34c92a6
jeremyjaouen <jjaouen@centreon.com>
2026-07-29 15:06:45 +0200

    fix(poller): sort imports alphabetically in Update-next

--- centreon/www/install/php/Update-next.php
+++ centreon/www/install/php/Update-next.php
@@ -19,9 +19,9 @@
  *
  */
 
+use Adaptation\Database\Connection\Collection\QueryParameters;
 use Adaptation\Database\Connection\ConnectionInterface;
 use Adaptation\Database\Connection\Exception\ConnectionException;
-use Adaptation\Database\Connection\Collection\QueryParameters;
 use Adaptation\Database\Connection\ValueObject\QueryParameter;
 use Adaptation\Log\LoggerUpgrade;
 
