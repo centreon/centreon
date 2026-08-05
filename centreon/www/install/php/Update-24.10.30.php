9e32334518feba272a88e7bd16b584c9286df140
pkippes <144150042+pkippes@users.noreply.github.com>
2026-08-05 15:21:44 +0200

    Update Update-24.10.30.php

--- centreon/www/install/php/Update-24.10.30.php
+++ centreon/www/install/php/Update-24.10.30.php
@@ -18,3 +18,78 @@
  * For more information : contact@centreon.com
  *
  */
+
+use Adaptation\Database\Connection\ConnectionInterface;
+use Adaptation\Database\Connection\Exception\ConnectionException;
+use Adaptation\Log\LoggerUpgrade;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+$version = '24.10.30';
+
+$errorMessage = '';
+
+/**
+ * @var ConnectionInterface $pearDB
+ * @var ConnectionInterface $pearDBO
+ */
+$removeDebugLevelFromOptions = function () use ($pearDB, &$errorMessage, $version): void {
+    $errorMessage = "Unable to remove 'debug_level' from options table";
+    LoggerUpgrade::create()->info($version, "Removing 'debug_level' from options table");
+
+    $pearDB->executeStatement(
+        <<<'SQL'
+            DELETE FROM `options` WHERE `key` = 'debug_level'
+            SQL
+    );
+
+    LoggerUpgrade::create()->info($version, "Successfully removed 'debug_level' from options table");
+};
+
+try {
+    LoggerUpgrade::create()->info($version, "Starting upgrade script for version {$version}");
+
+    // DDL statements for real time database
+    // TODO add your function calls to update the real time database structure here
+
+    // DDL statements for configuration database
+    // TODO add your function calls to update the configuration database structure here
+
+    // Transactional queries for configuration database
+    $errorMessage = 'Unable to start the configuration database transaction';
+    if (! $pearDB->isTransactionActive()) {
+        $pearDB->startTransaction();
+    }
+
+    $removeDebugLevelFromOptions();
+
+    $errorMessage = 'Unable to commit the configuration database transaction';
+    $pearDB->commitTransaction();
+
+    LoggerUpgrade::create()->info($version, "Upgrade script for version {$version} completed");
+
+} catch (Throwable $throwable) {
+    try {
+        if ($pearDB->isTransactionActive()) {
+            LoggerUpgrade::create()->info($version, "Rolling back transaction after error: {$errorMessage}");
+            $pearDB->rollBackTransaction();
+        }
+    } catch (ConnectionException $rollbackException) {
+        LoggerUpgrade::create()->stepFailure(
+            $version,
+            'php_script_rollback',
+            "UPGRADE - {$version}: error while rolling back the upgrade operation for : {$errorMessage}",
+            $rollbackException
+        );
+
+        throw new RuntimeException(
+            message: "UPGRADE - {$version}: " . $errorMessage,
+            previous: $throwable
+        );
+    }
+
+    throw new RuntimeException(
+        message: "UPGRADE - {$version}: " . $errorMessage,
+        previous: $throwable
+    );
+}
