d0c059eb5175fbd0317cd8eb137d2e8751fbd741
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2026-08-13 14:36:07 +0200

    test(e2e): web: stop pinning centreon-perl-libs in platform update tests (dev-26.10.x) (#11351)

--- centreon/tests/e2e/features/Platform-upgrade-update/common.ts
+++ centreon/tests/e2e/features/Platform-upgrade-update/common.ts
@@ -191,11 +191,11 @@ const installCentreon = (version: string): Cypress.Chainable => {
     }
 
     const packageVersionSuffix = `${version}${packageDistribPrefix}${packageDistribName}`;
+    // centreon-perl-libs is left to apt: it follows the gorgone release cadence
     const packagesToInstall = [
       `centreon-poller='${packageVersionSuffix}'`,
       `centreon-web='${packageVersionSuffix}'`,
-      `centreon-trap='${packageVersionSuffix}'`,
-      `centreon-perl-libs='${packageVersionSuffix}'`
+      `centreon-trap='${packageVersionSuffix}'`
     ];
     if (
       Number(versionMatches[1]) < 24 ||
