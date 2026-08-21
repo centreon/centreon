fc57634caa81e18406107879eda23d923848b2ff
Anthony Graule <agraule@centreon.com>
2026-08-21 10:31:22 +0200

    fix(resources): route empty CSV export columns through a translate() guard

--- centreon/src/Core/Resources/Infrastructure/API/ExportResources/ExportResourcesPresenterCsv.php
+++ centreon/src/Core/Resources/Infrastructure/API/ExportResources/ExportResourcesPresenterCsv.php
@@ -122,12 +122,12 @@ final class ExportResourcesPresenterCsv extends AbstractPresenter implements Exp
         /** @var ResourceEntity $resource */
         foreach ($resources as $resource) {
             $resource = [
-                _('Resource Type') => _($this->formatLabel($resource->getType() ?? '')),
-                _('Resource Name') => _($resource->getName() ?? ''),
-                _('Status') => _($this->formatLabel($resource->getStatus()?->getName() ?? '')),
-                _('Parent Resource Type') => _($this->formatLabel($resource->getParent()?->getType() ?? '')),
-                _('Parent Resource Name') => _($resource->getParent()?->getName() ?? ''),
-                _('Parent Resource Status') => _(
+                _('Resource Type') => $this->translate($this->formatLabel($resource->getType() ?? '')),
+                _('Resource Name') => $resource->getName() ?? '',
+                _('Status') => $this->translate($this->formatLabel($resource->getStatus()?->getName() ?? '')),
+                _('Parent Resource Type') => $this->translate($this->formatLabel($resource->getParent()?->getType() ?? '')),
+                _('Parent Resource Name') => $resource->getParent()?->getName() ?? '',
+                _('Parent Resource Status') => $this->translate(
                     $this->formatLabel($resource->getParent()?->getStatus()?->getName() ?? '')
                 ),
                 _('Duration') => $resource->getDuration() ?? '',
@@ -139,14 +139,14 @@ final class ExportResourcesPresenterCsv extends AbstractPresenter implements Exp
                 _('Action') => $this->formatUrl(
                     $resource->getLinks()->getExternals()->getActionUrl() ?? '', $resource
                 ),
-                _('State') => _($this->getResourceState($resource)),
+                _('State') => $this->translate($this->getResourceState($resource)),
                 _('Alias') => $resource->getAlias() ?? '',
                 _('Parent alias') => $resource->getParent()?->getAlias() ?? '',
                 _('FQDN / Address') => $resource->getFqdn() ?? '',
                 _('Monitoring server') => $resource->getMonitoringServerName(),
                 _('Notif') => $resource->isNotificationEnabled()
                     ? _('Notifications enabled') : _('Notifications disabled'),
-                _('Check') => _($this->getResourceCheck($resource)),
+                _('Check') => $this->translate($this->getResourceCheck($resource)),
             ];
 
             $line = array_map(
@@ -232,6 +232,16 @@ final class ExportResourcesPresenterCsv extends AbstractPresenter implements Exp
         });
 
         return $csvHeader;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    private function translate(string $value): string
+    {
+        return $value !== '' ? _($value) : '';
     }
 
     /**
