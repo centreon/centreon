892f64b8ac507d544d11cbf3328480a067aed115
Mohamed El Meziani <melmeziani@ext.centreon.com>
2026-08-31 16:52:08 +0100

    trigger ci

--- centreon/www/front_src/src/Dashboards/models.ts
+++ centreon/www/front_src/src/Dashboards/models.ts
@@ -1,11 +1,13 @@
-export enum DashboardLayout {
-  Library = 'library',
-  Playlist = 'playlists'
-}
 export enum FavoriteAction {
   add = 0,
   delete = 1
 }
+
+export enum DashboardLayout {
+  Library = 'library',
+  Playlist = 'playlists'
+}
+
 export interface GetPath {
   action: FavoriteAction;
   position: number;
