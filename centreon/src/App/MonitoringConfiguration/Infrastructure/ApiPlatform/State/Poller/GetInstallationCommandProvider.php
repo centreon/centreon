41838a2be5c38803614d21ee8298d3db5e487c4f
Anthony Graule <agraule@centreon.com>
2026-09-07 13:57:19 +0200

    fix(poller): keep base URI in the install command (#11426)
    Co-authored-by: laurent Calvet <lcalvet@centreon.com>
    (cherry picked from commit 7e4d279a42681ce9fb6af5f86eecaeb61e3b47fd)

--- centreon/src/App/MonitoringConfiguration/Infrastructure/ApiPlatform/State/Poller/GetInstallationCommandProvider.php
+++ centreon/src/App/MonitoringConfiguration/Infrastructure/ApiPlatform/State/Poller/GetInstallationCommandProvider.php
@@ -30,6 +30,7 @@ use App\MonitoringConfiguration\Domain\Aggregate\Poller\PollerId;
 use App\MonitoringConfiguration\Domain\Repository\PollerRepository;
 use App\MonitoringConfiguration\Domain\Repository\PollerTokenRepository;
 use App\MonitoringConfiguration\Infrastructure\ApiPlatform\Resource\Poller\InstallationCommandResource;
+use App\MonitoringConfiguration\Infrastructure\CentralUrlFactory;
 use App\MonitoringConfiguration\Infrastructure\PollerInstallationCommandFactory;
 use App\Shared\Domain\Repository\EngineSecretsRepository;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -44,6 +45,7 @@ final readonly class GetInstallationCommandProvider implements ProviderInterface
         private PollerRepository $pollerRepository,
         private PollerTokenRepository $pollerTokenRepository,
         private EngineSecretsRepository $engineSecretsRepository,
+        private CentralUrlFactory $centralUrlFactory,
         #[Autowire(env: 'bool:default::IS_CLOUD_PLATFORM')]
         private bool $isCloudPlatform = false,
     ) {
@@ -73,8 +75,8 @@ final readonly class GetInstallationCommandProvider implements ProviderInterface
             $token,
             $this->engineSecretsRepository->getAppSecret(),
             $this->engineSecretsRepository->getSalt(),
+            $this->centralUrlFactory->create($poller->centralAddress),
             $this->isCloudPlatform,
-            $poller->centralAddress->value,
         );
 
         return new InstallationCommandResource(
