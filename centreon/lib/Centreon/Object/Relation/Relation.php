428509f2c9cf96d8accfd6b80bf7ab07c288f538
Stéphane Chapron <34628915+sc979@users.noreply.github.com>
2026-09-11 15:34:20 +0200

    Apply suggestion from @sc979

--- centreon/lib/Centreon/Object/Relation/Relation.php
+++ centreon/lib/Centreon/Object/Relation/Relation.php
@@ -173,10 +173,11 @@ abstract class Centreon_Object_Relation
         if (! isset($this->firstObject) || ! isset($this->secondObject)) {
             throw new Exception('Unsupported method on this object');
         }
-        if (strtoupper($filterType) !== 'OR' && strtoupper($filterType) !== 'AND') {
+        $filterType = strtoupper($filterType);
+        if ($filterType !== 'OR' && $filterType !== 'AND') {
             throw new InvalidArgumentException('Invalid input');
         }
-        $filterType = strtoupper($filterType);
+
         $fString = '';
         $sString = '';
         foreach ($firstTableParams as $fparams) {
