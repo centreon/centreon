2aa9ceabfee8f3b8af482ba61d1497be0ee671fd
Anthony Graule <agraule@centreon.com>
2026-09-22 12:07:01 +0200

    feat(contact-group): implement GET /configuration/contact_groups (#11627)

--- centreon/src/App/Security/Infrastructure/Dbal/DbalCredentialTransformer.php
+++ centreon/src/App/Security/Infrastructure/Dbal/DbalCredentialTransformer.php
@@ -26,6 +26,7 @@ namespace App\Security\Infrastructure\Dbal;
 use App\MonitoringConfiguration\Domain\Security\AgentConfigurationPermissionEnum;
 use App\MonitoringConfiguration\Domain\Security\CommandPermissionEnum;
 use App\MonitoringConfiguration\Domain\Security\ConnectorPermissionEnum;
+use App\MonitoringConfiguration\Domain\Security\ContactGroupPermissionEnum;
 use App\MonitoringConfiguration\Domain\Security\GlobalMacroPermissionEnum;
 use App\MonitoringConfiguration\Domain\Security\HostCategoryPermissionEnum;
 use App\MonitoringConfiguration\Domain\Security\HostGroupPermissionEnum;
@@ -69,6 +70,8 @@ final readonly class DbalCredentialTransformer implements TransformerInterface
         'ROLE_CONFIGURATION_HOSTS_TEMPLATES_RW' => HostTemplatePermissionEnum::CanReadAndWrite->value,
         'ROLE_CONFIGURATION_HOSTS_CATEGORIES_R' => HostCategoryPermissionEnum::CanRead->value,
         'ROLE_CONFIGURATION_HOSTS_CATEGORIES_RW' => HostCategoryPermissionEnum::CanReadAndWrite->value,
+        'ROLE_CONFIGURATION_USERS_CONTACT_GROUPS_R' => ContactGroupPermissionEnum::CanRead->value,
+        'ROLE_CONFIGURATION_USERS_CONTACT_GROUPS_RW' => ContactGroupPermissionEnum::CanReadAndWrite->value,
         'ROLE_CONFIGURATION_USERS_CONTACTS__USERS_R' => NotificationContactPermissionEnum::CanRead->value,
         'ROLE_CONFIGURATION_USERS_CONTACTS__USERS_RW' => NotificationContactPermissionEnum::CanReadAndWrite->value,
         'ROLE_CONFIGURATION_USERS_TIME_PERIODS_R' => TimePeriodPermissionEnum::CanRead->value,
