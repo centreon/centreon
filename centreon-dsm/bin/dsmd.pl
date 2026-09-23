70829166a63eaa32e7d5c43c18f3d82a8c9ff381
tanguyvda <tcharles@centreon.com>
2026-09-23 11:30:19 +0200

    fix race condition

--- centreon-dsm/bin/dsmd.pl
+++ centreon-dsm/bin/dsmd.pl
@@ -247,20 +247,14 @@ sub get_alarms {
 
     return 1 if (scalar(@{$self->{current_alarms}}) == 0);
 
-    ($status, $sth) = $self->{db_centstorage}->query(
-        "SELECT mdc.`cache_id`, mdc.`host_id`, mdc.`ctime`, mdc.`status`, mdc.`pool_prefix`, mdc.`id`, mdc.`macros`, mdc.`output`
-        FROM mod_dsm_cache mdc, hosts
-        WHERE mdc.host_id = hosts.host_id AND
-            hosts.enabled = '1'
-        GROUP BY mdc.host_id, mdc.pool_prefix
-        "
-    );
-
+    my %pools = ();
     my @sql_where = ();
-    while (my $row = ( shift(@$rows) || # get row from cache, or reload cache:
-                       shift(@{$rows = $sth->fetchall_arrayref(undef, $self->{dsmd_config}->{sql_fetch})||[]})) ) {
+    foreach my $row (@{$self->{current_alarms}}) {
+        next if (defined($pools{$row->[1]}->{$row->[4]}));
+        $pools{$row->[1]}->{$row->[4]} = 1;
         push @sql_where, "(services.host_id = $row->[1] AND services.description LIKE " . $self->{db_centstorage}->quote($row->[4] . '%') . ")";
     }
+    return 1 if (scalar(@sql_where) == 0);
 
     ($status, $sth) = $self->{db_centstorage}->query(
         "SELECT hosts.`name`, hosts.`instance_id`, services.`host_id`, services.`service_id`, services.`description`, services.`last_check`, services.`state`, cv.`value` FROM services " .
